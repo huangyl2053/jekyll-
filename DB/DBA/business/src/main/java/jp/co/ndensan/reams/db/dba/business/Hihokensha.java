744c64cbec0f5e03caa8fb94e9124a6a7d2c5969
三浦 凌 <n3327@ndensan.co.jp>
2014-01-10 14:15:33 +0900

    refs #29201 Hihokenshaクラスを作成。

---
+++ DB/DBA/business/src/main/java/jp/co/ndensan/reams/db/dba/business/Hihokensha.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dba.business;
+
+/**
+ *
+ * @author N3327 三浦 凌
+ */
+public class Hihokensha {
+}
