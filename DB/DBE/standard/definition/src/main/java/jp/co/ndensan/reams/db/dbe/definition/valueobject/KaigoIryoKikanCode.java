c03ac8011d04af17e906e1321f10dd5db7f2db90
城間篤人 <n8178@ndensan.co.jp>
2014-02-07 09:54:43 +0900

    refs #29471 IValueObjectを実装。

--- DB/DBE/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbe/definition/valueobject/KaigoIryoKikanCode.java
+++ DB/DBE/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbe/definition/valueobject/KaigoIryoKikanCode.java
@@ -9,13 +9,14 @@ import jp.co.ndensan.reams.uz.uza.util.db.IDbColumnMappable;
 import java.util.Objects;
 import static java.util.Objects.requireNonNull;
 import jp.co.ndensan.reams.ur.urz.definition.Messages;
+import jp.co.ndensan.reams.uz.uza.biz.IValueObject;
 
 /**
  * 介護医療機関コードを表すクラスです。
  *
  * @author n8178 城間篤人
  */
-public class KaigoIryoKikanCode implements Comparable<KaigoIryoKikanCode>, IDbColumnMappable {
+public class KaigoIryoKikanCode implements Comparable<KaigoIryoKikanCode>, IDbColumnMappable, IValueObject {
 
     private final RString 介護医療機関コード;
 
@@ -60,6 +61,11 @@ public class KaigoIryoKikanCode implements Comparable<KaigoIryoKikanCode>, IDbCo
 
     @Override
     public RString getColumnValue() {
+        return 介護医療機関コード;
+    }
+
+    @Override
+    public Object value() {
         return 介護医療機関コード;
     }
 }
