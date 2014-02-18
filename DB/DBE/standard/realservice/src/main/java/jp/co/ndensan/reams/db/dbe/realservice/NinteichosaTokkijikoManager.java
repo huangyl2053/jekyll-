bcadeee522b6e864361548b6f5b4fb7154fb73bb
城間篤人 <n8178@ndensan.co.jp>
2014-02-18 17:14:03 +0900

    refs #29923 realserviceの実装。

---
+++ DB/DBE/standard/realservice/src/main/java/jp/co/ndensan/reams/db/dbe/realservice/NinteichosaTokkijikoManager.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.realservice;
+
+import java.util.List;
+import jp.co.ndensan.reams.db.dbe.business.NinteichosaTokkijiko;
+import jp.co.ndensan.reams.db.dbe.business.NinteichosaTokkijikoCollection;
+import jp.co.ndensan.reams.db.dbe.definition.valueobject.NinteichosaIraiRirekiNo;
+import jp.co.ndensan.reams.db.dbe.definition.valueobject.ShinseishoKanriNo;
+import jp.co.ndensan.reams.db.dbe.entity.basic.DbT5010NinteichosaTokkijikoEntity;
+import jp.co.ndensan.reams.db.dbe.entity.mapper.NinteichosaTokkijikoMapper;
+import jp.co.ndensan.reams.db.dbe.persistence.basic.INinteichosaTokkijikoDac;
+import jp.co.ndensan.reams.uz.uza.util.di.InstanceCreator;
+
+/**
+ * 認定調査特記事項のManagerクラスです。
+ *
+ * @author n8178 城間篤人
+ */
+public class NinteichosaTokkijikoManager {
+
+    private INinteichosaTokkijikoDac dac;
+
+    /**
+     * デフォルトコンストラクタです。
+     */
+    public NinteichosaTokkijikoManager() {
+        dac = InstanceCreator.create(INinteichosaTokkijikoDac.class);
+    }
+
+    /**
+     * 外部からDacを取得する、テスト用パッケージプライベートコンストラクタです。
+     *
+     * @param dac テスト用Dac
+     */
+    NinteichosaTokkijikoManager(INinteichosaTokkijikoDac dac) {
+        this.dac = dac;
+    }
+
+    /**
+     * 引数の申請書管理番号、認定調査依頼履歴番号に該当する認定調査特記事項を取得します。
+     *
+     * @param 申請書管理番号 申請書管理番号
+     * @param 認定調査依頼履歴番号 認定調査依頼履歴番号
+     * @return 認定調査依頼履歴番号
+     */
+    public NinteichosaTokkijiko get認定調査特記事項(ShinseishoKanriNo 申請書管理番号, NinteichosaIraiRirekiNo 認定調査依頼履歴番号) {
+        DbT5010NinteichosaTokkijikoEntity entity = dac.select(申請書管理番号, 認定調査依頼履歴番号);
+        return NinteichosaTokkijikoMapper.to認定調査特記事項(entity);
+    }
+
+    /**
+     * 引数の申請書管理番号に該当する、認定調査特記事項のCollectionを返します。
+     *
+     * @param 申請書管理番号 申請書管理番号
+     * @return 認定調査特記事項Collection
+     */
+    public NinteichosaTokkijikoCollection get認定調査特記事項Collection(ShinseishoKanriNo 申請書管理番号) {
+        List<DbT5010NinteichosaTokkijikoEntity> entities = dac.select(申請書管理番号);
+        return NinteichosaTokkijikoMapper.to認定調査特記事項Collection(entities);
+    }
+
+    /**
+     * 指定した認定調査特記事項を保存します。
+     *
+     * @param 認定調査特記事項 認定調査特記事項
+     * @return 保存に成功したらtrue、失敗したらfalse
+     */
+    public boolean save(NinteichosaTokkijiko 認定調査特記事項) {
+        DbT5010NinteichosaTokkijikoEntity entity = NinteichosaTokkijikoMapper.to認定調査特記事項Entity(認定調査特記事項);
+        return dac.insertOrUpdate(entity) == 1 ? true : false;
+    }
+
+    /**
+     * 指定した認定調査特記事項を削除します。
+     *
+     * @param 認定調査特記事項 認定調査特記事項
+     * @return 削除に成功したらtrue、失敗したらfalse
+     */
+    public boolean remove(NinteichosaTokkijiko 認定調査特記事項) {
+        DbT5010NinteichosaTokkijikoEntity entity = NinteichosaTokkijikoMapper.to認定調査特記事項Entity(認定調査特記事項);
+        return dac.delete(entity) == 1 ? true : false;
+    }
+}
---
+++ DB/DBE/standard/realservice/src/test/java/jp/co/ndensan/reams/db/dbe/realservice/NinteichosaTokkijikoManagerTest.java
@@ -0,0 +1,150 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.realservice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import jp.co.ndensan.reams.db.dbe.business.NinteichosaTokkijiko;
+import jp.co.ndensan.reams.db.dbe.definition.valueobject.NinteichosaIraiRirekiNo;
+import jp.co.ndensan.reams.db.dbe.definition.valueobject.ShinseishoKanriNo;
+import jp.co.ndensan.reams.db.dbe.entity.basic.DbT5010NinteichosaTokkijikoEntity;
+import jp.co.ndensan.reams.db.dbe.persistence.basic.INinteichosaTokkijikoDac;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.testhelper.TestBase;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+
+/**
+ * 認定調査特記事項Managerのテストです。
+ *
+ * @author n8178 城間篤人
+ */
+@RunWith(Enclosed.class)
+public class NinteichosaTokkijikoManagerTest extends TestBase {
+
+    private static NinteichosaTokkijikoManager sut;
+
+    public static class get認定調査特記事項のテスト extends TestBase {
+
+        @Test
+        public void 申請書管理番号_A001_認定調査依頼履歴番号_1を指定したとき_特記事項としてAAAを持つ認定調査特記事項が取得できる() {
+            DbT5010NinteichosaTokkijikoEntity entity = create認定調査特記事項Entity("A001", 1, "AAA");
+            sut = new NinteichosaTokkijikoManager(createMockDac(entity));
+
+            ShinseishoKanriNo 申請書管理番号 = new ShinseishoKanriNo(new RString("A001"));
+            NinteichosaIraiRirekiNo 認定調査依頼履歴番号 = new NinteichosaIraiRirekiNo(1);
+            assertThat(sut.get認定調査特記事項(申請書管理番号, 認定調査依頼履歴番号).get特記事項(), is(new RString("AAA")));
+        }
+
+        private INinteichosaTokkijikoDac createMockDac(DbT5010NinteichosaTokkijikoEntity entity) {
+            INinteichosaTokkijikoDac dac = mock(INinteichosaTokkijikoDac.class);
+            when(dac.select(any(ShinseishoKanriNo.class), any(NinteichosaIraiRirekiNo.class))).thenReturn(entity);
+            return dac;
+        }
+    }
+
+    public static class get認定調査特記事項Collectionのテスト extends TestBase {
+
+        private ShinseishoKanriNo 申請書管理番号;
+
+        @Override
+        public void setUp() {
+            申請書管理番号 = new ShinseishoKanriNo(new RString("A001"));
+        }
+
+        @Test
+        public void DBからデータを3件取得した場合_3件の要素を持つ認定調査特記事項Collectionを返す() {
+            sut = new NinteichosaTokkijikoManager(createMockDac(create件数分のList(3)));
+            assertThat(sut.get認定調査特記事項Collection(申請書管理番号).size(), is(3));
+        }
+
+        @Test
+        public void DBからデータを取得できなかった場合_空の認定調査特記事項Collectionを返す() {
+            sut = new NinteichosaTokkijikoManager(createMockDac(Collections.EMPTY_LIST));
+            assertThat(sut.get認定調査特記事項Collection(申請書管理番号).isEmpty(), is(true));
+        }
+
+        private INinteichosaTokkijikoDac createMockDac(List<DbT5010NinteichosaTokkijikoEntity> entities) {
+            INinteichosaTokkijikoDac dac = mock(INinteichosaTokkijikoDac.class);
+            when(dac.select(any(ShinseishoKanriNo.class))).thenReturn(entities);
+            return dac;
+        }
+
+        private List<DbT5010NinteichosaTokkijikoEntity> create件数分のList(int 件数) {
+            List<DbT5010NinteichosaTokkijikoEntity> list = new ArrayList<>();
+            for (int i = 0; i < 件数; i++) {
+                list.add(create認定調査特記事項Entity("A001", i, "特記事項"));
+            }
+            return list;
+        }
+    }
+
+    public static class saveのテスト {
+
+        @Test
+        public void saveに成功した場合_trueを返す() {
+            sut = new NinteichosaTokkijikoManager(createMockDac(1));
+            assertThat(sut.save(create認定調査特記事項()), is(true));
+        }
+
+        @Test
+        public void saveに失敗した場合_falseを返す() {
+            sut = new NinteichosaTokkijikoManager(createMockDac(0));
+            assertThat(sut.save(create認定調査特記事項()), is(false));
+        }
+
+        private INinteichosaTokkijikoDac createMockDac(int 更新件数) {
+            INinteichosaTokkijikoDac dac = mock(INinteichosaTokkijikoDac.class);
+            when(dac.insertOrUpdate(any(DbT5010NinteichosaTokkijikoEntity.class))).thenReturn(更新件数);
+            return dac;
+        }
+    }
+
+    public static class removeのテスト {
+
+        @Test
+        public void removeに成功した場合_trueを返す() {
+            sut = new NinteichosaTokkijikoManager(createMockDac(1));
+            assertThat(sut.remove(create認定調査特記事項()), is(true));
+        }
+
+        @Test
+        public void removeに失敗した場合_falseを返す() {
+            sut = new NinteichosaTokkijikoManager(createMockDac(0));
+            assertThat(sut.remove(create認定調査特記事項()), is(false));
+        }
+
+        private INinteichosaTokkijikoDac createMockDac(int 削除件数) {
+            INinteichosaTokkijikoDac dac = mock(INinteichosaTokkijikoDac.class);
+            when(dac.delete(any(DbT5010NinteichosaTokkijikoEntity.class))).thenReturn(削除件数);
+            return dac;
+        }
+    }
+
+    private static NinteichosaTokkijiko create認定調査特記事項(String 申請書管理番号, int 認定調査依頼履歴番号, String 特記事項) {
+        return new NinteichosaTokkijiko(new ShinseishoKanriNo(new RString(申請書管理番号)),
+                new NinteichosaIraiRirekiNo(認定調査依頼履歴番号), new RString("B001"), new RString(特記事項));
+    }
+
+    private static NinteichosaTokkijiko create認定調査特記事項() {
+        return create認定調査特記事項("A001", 1, "特記事項を記入しましょう");
+    }
+
+    private static DbT5010NinteichosaTokkijikoEntity create認定調査特記事項Entity(String 申請書管理番号, int 認定調査依頼履歴番号, String 特記事項) {
+        DbT5010NinteichosaTokkijikoEntity entity = new DbT5010NinteichosaTokkijikoEntity();
+        entity.setShinseishoKanriNo(new ShinseishoKanriNo(new RString(申請書管理番号)));
+        entity.setNinteichosaRirekiNo(new NinteichosaIraiRirekiNo(認定調査依頼履歴番号));
+        entity.setNinteichosaTokkijikoNo(new RString("B001"));
+        entity.setNinteichosaTokkijiko(new RString(特記事項));
+        return entity;
+    }
+}
