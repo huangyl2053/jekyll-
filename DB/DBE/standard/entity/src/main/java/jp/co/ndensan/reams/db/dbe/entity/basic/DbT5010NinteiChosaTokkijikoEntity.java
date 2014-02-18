ca72e821495b4b16077e73ee6bea9620aa135805
城間篤人 <n8178@ndensan.co.jp>
2014-02-18 11:29:36 +0900

    refs #29923 Entity自動生成。

---
+++ DB/DBE/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbe/entity/basic/DbT5010NinteiChosaTokkijikoEntity.java
@@ -0,0 +1,154 @@
+package jp.co.ndensan.reams.db.dbe.entity.basic;
+
+import jp.co.ndensan.reams.uz.uza.util.db.IDbAccessable;
+import jp.co.ndensan.reams.uz.uza.util.db.PrimaryKey;
+import jp.co.ndensan.reams.uz.uza.util.db.TableName;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.lang.RDateTime;
+import java.util.UUID;
+import jp.co.ndensan.reams.db.dbe.definition.valueobject.ShinseishoKanriNo;
+import jp.co.ndensan.reams.db.dbe.definition.valueobject.NinteichosaIraiRirekiNo;
+
+/**
+ * DbT5010NinteiChosaTokkijikoの項目定義クラスです
+ *
+ * @author n8178 城間篤人
+ */
+public class DbT5010NinteiChosaTokkijikoEntity implements IDbAccessable {
+// <editor-fold defaultstate="collapsed" desc="Generated Code">
+
+    @TableName
+    public static final RString TABLE_NAME = new RString("DbT5010NinteiChosaTokkijiko");
+    private RString insertDantaiCd = RString.EMPTY;
+    private RDateTime insertTimestamp;
+    private RString insertReamsLoginId = RString.EMPTY;
+    private UUID insertContextId;
+    private boolean isDeleted = false;
+    private int updateCount = 0;
+    private RDateTime lastUpdateTimestamp;
+    private RString lastUpdateReamsLoginId = RString.EMPTY;
+    @PrimaryKey
+    private ShinseishoKanriNo shinseishoKanriNo;
+    @PrimaryKey
+    private NinteichosaIraiRirekiNo ninteichosaRirekiNo;
+    private RString ninteichosaTokkijikoNo;
+    private RString ninteichosaTokkijiko;
+
+    /**
+     * getInsertDantaiCd
+     *
+     * @return insertDantaiCd
+     */
+    public RString getInsertDantaiCd() {
+        return insertDantaiCd;
+    }
+
+    /**
+     * setInsertDantaiCd
+     *
+     * @param insertDantaiCd insertDantaiCd
+     */
+    public void setInsertDantaiCd(RString insertDantaiCd) {
+        this.insertDantaiCd = insertDantaiCd;
+    }
+
+    /**
+     * getIsDeleted
+     *
+     * @return isDeleted
+     */
+    public boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    /**
+     * setIsDeleted
+     *
+     * @param isDeleted isDeleted
+     */
+    public void setIsDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    /**
+     * setLastUpdateReamsLoginId
+     *
+     * @param lastUpdateReamsLoginId lastUpdateReamsLoginId
+     */
+    public void setLastUpdateReamsLoginId(RString lastUpdateReamsLoginId) {
+        this.lastUpdateReamsLoginId = lastUpdateReamsLoginId;
+    }
+
+    /**
+     * getShinseishoKanriNo
+     *
+     * @return shinseishoKanriNo
+     */
+    public ShinseishoKanriNo getShinseishoKanriNo() {
+        return shinseishoKanriNo;
+    }
+
+    /**
+     * setShinseishoKanriNo
+     *
+     * @param shinseishoKanriNo shinseishoKanriNo
+     */
+    public void setShinseishoKanriNo(ShinseishoKanriNo shinseishoKanriNo) {
+        this.shinseishoKanriNo = shinseishoKanriNo;
+    }
+
+    /**
+     * getNinteichosaRirekiNo
+     *
+     * @return ninteichosaRirekiNo
+     */
+    public NinteichosaIraiRirekiNo getNinteichosaRirekiNo() {
+        return ninteichosaRirekiNo;
+    }
+
+    /**
+     * setNinteichosaRirekiNo
+     *
+     * @param ninteichosaRirekiNo ninteichosaRirekiNo
+     */
+    public void setNinteichosaRirekiNo(NinteichosaIraiRirekiNo ninteichosaRirekiNo) {
+        this.ninteichosaRirekiNo = ninteichosaRirekiNo;
+    }
+
+    /**
+     * getNinteichosaTokkijikoNo
+     *
+     * @return ninteichosaTokkijikoNo
+     */
+    public RString getNinteichosaTokkijikoNo() {
+        return ninteichosaTokkijikoNo;
+    }
+
+    /**
+     * setNinteichosaTokkijikoNo
+     *
+     * @param ninteichosaTokkijikoNo ninteichosaTokkijikoNo
+     */
+    public void setNinteichosaTokkijikoNo(RString ninteichosaTokkijikoNo) {
+        this.ninteichosaTokkijikoNo = ninteichosaTokkijikoNo;
+    }
+
+    /**
+     * getNinteichosaTokkijiko
+     *
+     * @return ninteichosaTokkijiko
+     */
+    public RString getNinteichosaTokkijiko() {
+        return ninteichosaTokkijiko;
+    }
+
+    /**
+     * setNinteichosaTokkijiko
+     *
+     * @param ninteichosaTokkijiko ninteichosaTokkijiko
+     */
+    public void setNinteichosaTokkijiko(RString ninteichosaTokkijiko) {
+        this.ninteichosaTokkijiko = ninteichosaTokkijiko;
+    }
+// </editor-fold>
+}
---
+++ DB/DBE/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbe/entity/typehandler/ShinseishoKanriNoTypeHandler.java
@@ -0,0 +1,39 @@
+package jp.co.ndensan.reams.db.dbe.entity.typehandler;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import jp.co.ndensan.reams.db.dbe.definition.valueobject.ShinseishoKanriNo;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+public class ShinseishoKanriNoTypeHandler extends BaseTypeHandler<ShinseishoKanriNo> {
+// <editor-fold defaultstate="collapsed" desc="Generated Code">
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, ShinseishoKanriNo parameter, JdbcType jdbcType) throws SQLException {
+        ps.setString(i, parameter == null ? null : parameter.getColumnValue().toString());
+    }
+
+    @Override
+    public ShinseishoKanriNo getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return new ShinseishoKanriNo(getResultRString(rs.getString(columnName)));
+    }
+
+    @Override
+    public ShinseishoKanriNo getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return new ShinseishoKanriNo(getResultRString(rs.getString(columnIndex)));
+    }
+
+    @Override
+    public ShinseishoKanriNo getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return new ShinseishoKanriNo(getResultRString(cs.getString(columnIndex)));
+    }
+
+    private RString getResultRString(String result) {
+        return result == null ? null : new RString(result);
+    }
+
+// </editor-fold>
+}
