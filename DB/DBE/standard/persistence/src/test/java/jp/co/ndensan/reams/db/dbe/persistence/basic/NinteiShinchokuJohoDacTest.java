99208f24f836e3dcd46c74c5bca15d6fc2e9ed62
久保田　英男 <n8187@ndensan.co.jp>
2014-03-14 18:58:09 +0900

    refs #30384 DACのテスト追加

--- DB/DBE/standard/persistence/src/test/java/jp/co/ndensan/reams/db/dbe/persistence/basic/NinteiShinchokuJohoDacTest.java
+++ DB/DBE/standard/persistence/src/test/java/jp/co/ndensan/reams/db/dbe/persistence/basic/NinteiShinchokuJohoDacTest.java
@@ -81,6 +81,28 @@ public class NinteiShinchokuJohoDacTest extends DbeTestDacBase {
         }
     }
 
+    public static class update_戻り値が1 extends DbeTestDacBase {
+
+        @Test
+        public void updateに成功し_1が返る() {
+            ninteiShinchokuJohoDacMock.insert(create認定進捗情報Entity(new RString("0001"), new FlexibleDate("00000000"), new FlexibleDate("00000000")));
+            ninteiShinchokuJohoDacMock.insert(create認定進捗情報Entity(new RString("0002"), new FlexibleDate("20140101"), new FlexibleDate("20140102")));
+            int result = sut.update(create認定進捗情報Entity(new RString("0001"), new FlexibleDate("20140101"), new FlexibleDate("00000000")));
+            assertThat(result, is(1));
+        }
+    }
+
+    public static class update_DB登録が更新される extends DbeTestDacBase {
+
+        @Test
+        public void updateに成功し_select依頼済認定調査未完了で取得した認定進捗情報の_認定調査依頼完了年月日が_20140101に変更される() {
+            ninteiShinchokuJohoDacMock.insert(create認定進捗情報Entity(new RString("0001"), new FlexibleDate("00000000"), new FlexibleDate("00000000")));
+            ninteiShinchokuJohoDacMock.insert(create認定進捗情報Entity(new RString("0002"), new FlexibleDate("20140101"), new FlexibleDate("20140102")));
+            sut.update(create認定進捗情報Entity(new RString("0001"), new FlexibleDate("20140101"), new FlexibleDate("00000000")));
+            assertThat(sut.select依頼済認定調査未完了().get(0).getNinteichosaIraiKanryoYMD().toString(), is("20140101"));
+        }
+    }
+
     private static DbT5005NinteiShinchokuJohoEntity create認定進捗情報Entity(RString 申請書管理番号, FlexibleDate 認定調査依頼完了年月日, FlexibleDate 認定調査完了年月日) {
         FlexibleDate testDate = new FlexibleDate("20140101");
 
