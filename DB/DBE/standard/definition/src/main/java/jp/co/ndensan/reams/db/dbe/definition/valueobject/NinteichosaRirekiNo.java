ce5a439b9da5ec2ed6b348c91e903d0ff9b705dd
城間篤人 <n8178@ndensan.co.jp>
2014-03-28 09:28:08 +0900

    refs #30425 チェックスタイルの再確認他

--- DB/DBE/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbe/definition/valueobject/NinteichosaRirekiNo.java
+++ DB/DBE/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbe/definition/valueobject/NinteichosaRirekiNo.java
@@ -18,7 +18,7 @@ public class NinteichosaRirekiNo implements IValueObject<Integer> {
     /**
      * 引数から履歴を表す数値を受け取り、インスタンスを生成します。
      *
-     * @param 履歴番号
+     * @param 履歴番号 履歴番号
      */
     public NinteichosaRirekiNo(int 履歴番号) {
         this.履歴番号 = 履歴番号;
