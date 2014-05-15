71dfa55c1edc60a00fe740f1380ff1c49f5e34ab
塚田萌 <n3317@ndensan.co.jp>
2014-05-15 15:35:48 +0900

    refs #36549 entityパッケージクラス自動生成。

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/dgHihokenshaSearchGaitosha_Row.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/dgHihokenshaSearchGaitosha_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbc.divcontroller.entity;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Tue May 13 18:53:31 JST 2014 
+ * Thu May 15 15:14:47 JST 2014 
  */
 
 
