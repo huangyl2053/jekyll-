b08182dd93627df978cdd292b383899a574da512
塚田萌 <n3317@ndensan.co.jp>
2014-05-15 15:07:03 +0900

    refs #36549 レイアウト修正 １つの共有子Divを３つに分割

---
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/dgSeikyuDetail_Row.java
@@ -0,0 +1,131 @@
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity;
+/**
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+ * コードが再生成されるときに損失したりします。
+ * Thu May 15 12:08:12 JST 2014 
+ */
+
+
+
+import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Button;
+import jp.co.ndensan.reams.uz.uza.ui.binding.TextBox;
+import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxDate;
+import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxNum;
+
+
+/**
+ * dgSeikyuDetail_Row のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+
+public class dgSeikyuDetail_Row extends DataRow {
+
+    private Button btnModify;
+    private Button btnDelete;
+    private TextBox txtRowState;
+    private TextBoxDate txtBuyYMD;
+    private TextBox txtShumoku;
+    private TextBox txtShohinName;
+    private TextBoxNum txtBuyAmount;
+    private TextBox txtShinsaMethod;
+
+    public dgSeikyuDetail_Row() {
+        super();
+    }
+
+    public dgSeikyuDetail_Row(Button btnModify, Button btnDelete, TextBox txtRowState, TextBoxDate txtBuyYMD, TextBox txtShumoku, TextBox txtShohinName, TextBoxNum txtBuyAmount, TextBox txtShinsaMethod) {
+        super();
+        this.setOriginalData("btnModify", btnModify);
+        this.setOriginalData("btnDelete", btnDelete);
+        this.setOriginalData("txtRowState", txtRowState);
+        this.setOriginalData("txtBuyYMD", txtBuyYMD);
+        this.setOriginalData("txtShumoku", txtShumoku);
+        this.setOriginalData("txtShohinName", txtShohinName);
+        this.setOriginalData("txtBuyAmount", txtBuyAmount);
+        this.setOriginalData("txtShinsaMethod", txtShinsaMethod);
+        this.btnModify = btnModify;
+        this.btnDelete = btnDelete;
+        this.txtRowState = txtRowState;
+        this.txtBuyYMD = txtBuyYMD;
+        this.txtShumoku = txtShumoku;
+        this.txtShohinName = txtShohinName;
+        this.txtBuyAmount = txtBuyAmount;
+        this.txtShinsaMethod = txtShinsaMethod;
+    }
+
+    public Button getBtnModify() {
+        return btnModify;
+    }
+
+    public Button getBtnDelete() {
+        return btnDelete;
+    }
+
+    public TextBox getTxtRowState() {
+        return txtRowState;
+    }
+
+    public TextBoxDate getTxtBuyYMD() {
+        return txtBuyYMD;
+    }
+
+    public TextBox getTxtShumoku() {
+        return txtShumoku;
+    }
+
+    public TextBox getTxtShohinName() {
+        return txtShohinName;
+    }
+
+    public TextBoxNum getTxtBuyAmount() {
+        return txtBuyAmount;
+    }
+
+    public TextBox getTxtShinsaMethod() {
+        return txtShinsaMethod;
+    }
+
+    public void setBtnModify(Button btnModify) {
+        this.setOriginalData("btnModify", btnModify);
+        this.btnModify = btnModify;
+    }
+
+    public void setBtnDelete(Button btnDelete) {
+        this.setOriginalData("btnDelete", btnDelete);
+        this.btnDelete = btnDelete;
+    }
+
+    public void setTxtRowState(TextBox txtRowState) {
+        this.setOriginalData("txtRowState", txtRowState);
+        this.txtRowState = txtRowState;
+    }
+
+    public void setTxtBuyYMD(TextBoxDate txtBuyYMD) {
+        this.setOriginalData("txtBuyYMD", txtBuyYMD);
+        this.txtBuyYMD = txtBuyYMD;
+    }
+
+    public void setTxtShumoku(TextBox txtShumoku) {
+        this.setOriginalData("txtShumoku", txtShumoku);
+        this.txtShumoku = txtShumoku;
+    }
+
+    public void setTxtShohinName(TextBox txtShohinName) {
+        this.setOriginalData("txtShohinName", txtShohinName);
+        this.txtShohinName = txtShohinName;
+    }
+
+    public void setTxtBuyAmount(TextBoxNum txtBuyAmount) {
+        this.setOriginalData("txtBuyAmount", txtBuyAmount);
+        this.txtBuyAmount = txtBuyAmount;
+    }
+
+    public void setTxtShinsaMethod(TextBox txtShinsaMethod) {
+        this.setOriginalData("txtShinsaMethod", txtShinsaMethod);
+        this.txtShinsaMethod = txtShinsaMethod;
+    }
+
+}
