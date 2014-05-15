324b5e8dabcd3702a98ecfd7728f05959ae45669
城間篤人 <n8178@ndensan.co.jp>
2014-05-15 10:57:42 +0900

    refs #36093 コミット漏れ

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/helper/YamlLoader.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/helper/YamlLoader.java
@@ -24,23 +24,41 @@ import org.yaml.snakeyaml.Yaml;
  *
  * @author n8178 城間篤人
  */
-public class YamlLoader {
+public final class YamlLoader {
 
-    private static final RString RootPath = new RString("db/");
+    private static final RString ROOT_PATH = new RString("db/");
 
+    /**
+     * インスタンス化防止のためのプライベートコンストラクタです。
+     */
+    private YamlLoader() {
+    }
+
+    /**
+     * 引数に渡したURLからYamlファイルをロードして、その内容をList型で返します。
+     *
+     * @param fileName ファイルの位置情報
+     * @return List化したYamlファイル
+     */
     public static List<HashMap> loadAsList(RString fileName) {
         return toList(load(fileName));
     }
 
+    /**
+     * 引数に渡したURLからYamlファイルをロードして、その内容をMap型で返します。
+     *
+     * @param fileName ファイルの位置情報
+     * @return Map化したYamlファイル
+     */
     public static Map loadAsMap(RString fileName) {
         return toHashMap(load(fileName));
     }
 
     private static Object load(RString fileName) {
         InputStream input = null;
-        String urlHeader = ((_IServletControlData) _ControlDataHolder.getControlData()).getUrlHeader();
+        RString urlHeader = new RString(((_IServletControlData) _ControlDataHolder.getControlData()).getUrlHeader());
         try {
-            URL url = new URL(urlHeader.concat(RootPath.toString()).concat(fileName.toString()));
+            URL url = new URL(urlHeader.toString().concat(ROOT_PATH.toString()).concat(fileName.toString()));
             input = url.openStream();
         } catch (FileNotFoundException e) {
             throw new SystemException("fileが見つかりません :" + fileName.toString(), e);
