8d7ed8bfdb85da645767cfc8473b2ad91012a921
宮本 康 <n8156@ndensan.co.jp>
2014-05-22 10:48:33 +0900

    refs #36873 レビュー指摘反映

---
+++ DB/DBC/standard/CommonChildDiv/ServiceTeikyoShomeishoNyushoList.ts
@@ -0,0 +1,8 @@
+/// <reference path='ServiceTeikyoShomeishoNyushoList_Design.ts' />
+module DBC
+{
+    export class ServiceTeikyoShomeishoNyushoList extends ServiceTeikyoShomeishoNyushoList_Design
+    {
+    }
+}
+
