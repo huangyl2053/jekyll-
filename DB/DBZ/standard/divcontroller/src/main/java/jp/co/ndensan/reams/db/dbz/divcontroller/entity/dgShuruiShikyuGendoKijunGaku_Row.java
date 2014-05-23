d9bb6bdc4f32007038ab01fe6c1e111df1eda35f
城間篤人 <n8178@ndensan.co.jp>
2014-05-23 15:54:06 +0900

    refs #37121 資格者証発行画面レイアウト作成。 レビュー対応後コミット。

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/dgShuruiShikyuGendoKijunGaku_Row.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/dgShuruiShikyuGendoKijunGaku_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.entity;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Fri May 23 14:22:47 JST 2014 
+ * Fri May 23 15:52:30 JST 2014 
  */
 
 
