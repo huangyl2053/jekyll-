1aa8050e250833feac7b851c7cdc25def6665ea6
塚田萌 <n3317@ndensan.co.jp>
2014-05-23 15:28:25 +0900

    refs #37126 レイアウト完了。

---
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/dgHihokenshaSearchGaitosha_Row.java
@@ -0,0 +1,167 @@
+package jp.co.ndensan.reams.db.dbz.divcontroller.entity;
+/**
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+ * コードが再生成されるときに損失したりします。
+ * Fri May 23 15:23:31 JST 2014 
+ */
+
+
+
+import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Button;
+import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxCode;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxDate;
+
+
+/**
+ * dgHihokenshaSearchGaitosha_Row のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+
+public class dgHihokenshaSearchGaitosha_Row extends DataRow {
+
+    private Boolean chkCreateMoshitatesho;
+    private Button btnSelect;
+    private TextBoxCode txtJigyoshaNo;
+    private RString txtJigyoshaName;
+    private TextBoxCode txtHihoNo;
+    private RString txtHihoName;
+    private RString txtKagoForm;
+    private TextBoxDate txtTeikyoYM;
+    private RString txtKyufuJissekiSakuseiKubun;
+    private RString txtKyufuKubun;
+    private TextBoxDate txtShinsaYM;
+
+    public dgHihokenshaSearchGaitosha_Row() {
+        super();
+    }
+
+    public dgHihokenshaSearchGaitosha_Row(Boolean chkCreateMoshitatesho, Button btnSelect, TextBoxCode txtJigyoshaNo, RString txtJigyoshaName, TextBoxCode txtHihoNo, RString txtHihoName, RString txtKagoForm, TextBoxDate txtTeikyoYM, RString txtKyufuJissekiSakuseiKubun, RString txtKyufuKubun, TextBoxDate txtShinsaYM) {
+        super();
+        this.setOriginalData("chkCreateMoshitatesho", chkCreateMoshitatesho);
+        this.setOriginalData("btnSelect", btnSelect);
+        this.setOriginalData("txtJigyoshaNo", txtJigyoshaNo);
+        this.setOriginalData("txtJigyoshaName", txtJigyoshaName);
+        this.setOriginalData("txtHihoNo", txtHihoNo);
+        this.setOriginalData("txtHihoName", txtHihoName);
+        this.setOriginalData("txtKagoForm", txtKagoForm);
+        this.setOriginalData("txtTeikyoYM", txtTeikyoYM);
+        this.setOriginalData("txtKyufuJissekiSakuseiKubun", txtKyufuJissekiSakuseiKubun);
+        this.setOriginalData("txtKyufuKubun", txtKyufuKubun);
+        this.setOriginalData("txtShinsaYM", txtShinsaYM);
+        this.chkCreateMoshitatesho = chkCreateMoshitatesho;
+        this.btnSelect = btnSelect;
+        this.txtJigyoshaNo = txtJigyoshaNo;
+        this.txtJigyoshaName = txtJigyoshaName;
+        this.txtHihoNo = txtHihoNo;
+        this.txtHihoName = txtHihoName;
+        this.txtKagoForm = txtKagoForm;
+        this.txtTeikyoYM = txtTeikyoYM;
+        this.txtKyufuJissekiSakuseiKubun = txtKyufuJissekiSakuseiKubun;
+        this.txtKyufuKubun = txtKyufuKubun;
+        this.txtShinsaYM = txtShinsaYM;
+    }
+
+    public Boolean getChkCreateMoshitatesho() {
+        return chkCreateMoshitatesho;
+    }
+
+    public Button getBtnSelect() {
+        return btnSelect;
+    }
+
+    public TextBoxCode getTxtJigyoshaNo() {
+        return txtJigyoshaNo;
+    }
+
+    public RString getTxtJigyoshaName() {
+        return txtJigyoshaName;
+    }
+
+    public TextBoxCode getTxtHihoNo() {
+        return txtHihoNo;
+    }
+
+    public RString getTxtHihoName() {
+        return txtHihoName;
+    }
+
+    public RString getTxtKagoForm() {
+        return txtKagoForm;
+    }
+
+    public TextBoxDate getTxtTeikyoYM() {
+        return txtTeikyoYM;
+    }
+
+    public RString getTxtKyufuJissekiSakuseiKubun() {
+        return txtKyufuJissekiSakuseiKubun;
+    }
+
+    public RString getTxtKyufuKubun() {
+        return txtKyufuKubun;
+    }
+
+    public TextBoxDate getTxtShinsaYM() {
+        return txtShinsaYM;
+    }
+
+    public void setChkCreateMoshitatesho(Boolean chkCreateMoshitatesho) {
+        this.setOriginalData("chkCreateMoshitatesho", chkCreateMoshitatesho);
+        this.chkCreateMoshitatesho = chkCreateMoshitatesho;
+    }
+
+    public void setBtnSelect(Button btnSelect) {
+        this.setOriginalData("btnSelect", btnSelect);
+        this.btnSelect = btnSelect;
+    }
+
+    public void setTxtJigyoshaNo(TextBoxCode txtJigyoshaNo) {
+        this.setOriginalData("txtJigyoshaNo", txtJigyoshaNo);
+        this.txtJigyoshaNo = txtJigyoshaNo;
+    }
+
+    public void setTxtJigyoshaName(RString txtJigyoshaName) {
+        this.setOriginalData("txtJigyoshaName", txtJigyoshaName);
+        this.txtJigyoshaName = txtJigyoshaName;
+    }
+
+    public void setTxtHihoNo(TextBoxCode txtHihoNo) {
+        this.setOriginalData("txtHihoNo", txtHihoNo);
+        this.txtHihoNo = txtHihoNo;
+    }
+
+    public void setTxtHihoName(RString txtHihoName) {
+        this.setOriginalData("txtHihoName", txtHihoName);
+        this.txtHihoName = txtHihoName;
+    }
+
+    public void setTxtKagoForm(RString txtKagoForm) {
+        this.setOriginalData("txtKagoForm", txtKagoForm);
+        this.txtKagoForm = txtKagoForm;
+    }
+
+    public void setTxtTeikyoYM(TextBoxDate txtTeikyoYM) {
+        this.setOriginalData("txtTeikyoYM", txtTeikyoYM);
+        this.txtTeikyoYM = txtTeikyoYM;
+    }
+
+    public void setTxtKyufuJissekiSakuseiKubun(RString txtKyufuJissekiSakuseiKubun) {
+        this.setOriginalData("txtKyufuJissekiSakuseiKubun", txtKyufuJissekiSakuseiKubun);
+        this.txtKyufuJissekiSakuseiKubun = txtKyufuJissekiSakuseiKubun;
+    }
+
+    public void setTxtKyufuKubun(RString txtKyufuKubun) {
+        this.setOriginalData("txtKyufuKubun", txtKyufuKubun);
+        this.txtKyufuKubun = txtKyufuKubun;
+    }
+
+    public void setTxtShinsaYM(TextBoxDate txtShinsaYM) {
+        this.setOriginalData("txtShinsaYM", txtShinsaYM);
+        this.txtShinsaYM = txtShinsaYM;
+    }
+
+}
