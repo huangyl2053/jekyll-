86cd587797728be9f8e504bcaa3fa37d7f76145f
城間篤人 <n8178@ndensan.co.jp>
2014-05-24 15:32:53 +0900

    refs #37150 証回収記録管理レイアウト、レビュー対応完了。

---
+++ DB/DBZ/standard/CommonChildDiv/ShoKaishuKirokuKanri_Design.js
@@ -0,0 +1,174 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var DBZ;
+(function (DBZ) {
+    var ShoKaishuKirokuKanri_Design = (function (_super) {
+        __extends(ShoKaishuKirokuKanri_Design, _super);
+        function ShoKaishuKirokuKanri_Design($parentElement, isDesignMode, fieldName) {
+            _super.call(this, $parentElement, isDesignMode, ShoKaishuKirokuKanri_Design.myLayout, fieldName);
+        }
+        ShoKaishuKirokuKanri_Design.prototype.registProperty = function () {
+            _super.prototype.registProperty.call(this);
+        };
+
+        ShoKaishuKirokuKanri_Design.prototype.getEditablePropertyInfo = function () {
+            var editablePropertyInfo = _super.prototype.getEditablePropertyInfo.call(this);
+
+            return editablePropertyInfo;
+        };
+        ShoKaishuKirokuKanri_Design.myLayout = {
+            "fieldName": "defaultLayout",
+            "items": [
+                {
+                    "fieldName": "ShoKaishuKirokuKanri",
+                    "items": [
+                        {
+                            "fieldName": "ShoKaishuJokyoList",
+                            "items": [],
+                            "controlType": "CommonChildDiv",
+                            "width": "XS",
+                            "visible": true,
+                            "displayNone": false,
+                            "disabled": false,
+                            "accessKey": "",
+                            "nextFocusFieldName": "",
+                            "wrap": true,
+                            "dependencies": [],
+                            "float": 0,
+                            "toolTip": "",
+                            "authorityMode": 0,
+                            "marginLeft": 0,
+                            "marginRight": 0,
+                            "relation": [],
+                            "businessId": "DBZ",
+                            "controlName": "ShoKaishuJokyoList",
+                            "marginTop": 0,
+                            "marginBottom": 0,
+                            "originalProperty": [],
+                            "dataPassingForDialog": [],
+                            "dialogOkEventNameForDialog": "",
+                            "dialogCancelEventNameForDialog": ""
+                        },
+                        {
+                            "fieldName": "ShoKaishuJokyoShosai",
+                            "items": [],
+                            "controlType": "CommonChildDiv",
+                            "width": "XS",
+                            "visible": true,
+                            "displayNone": false,
+                            "disabled": false,
+                            "accessKey": "",
+                            "nextFocusFieldName": "",
+                            "wrap": false,
+                            "dependencies": [],
+                            "float": 0,
+                            "toolTip": "",
+                            "authorityMode": 0,
+                            "marginLeft": 0,
+                            "marginRight": 0,
+                            "relation": [],
+                            "businessId": "DBZ",
+                            "controlName": "ShoKaishuJokyoShosai",
+                            "marginTop": 0,
+                            "marginBottom": 0,
+                            "originalProperty": [],
+                            "dataPassingForDialog": [],
+                            "dialogOkEventNameForDialog": "",
+                            "dialogCancelEventNameForDialog": ""
+                        },
+                        {
+                            "fieldName": "btnUpdateShoKaishuJokyo",
+                            "items": [],
+                            "controlType": "Button",
+                            "width": "150",
+                            "visible": true,
+                            "displayNone": false,
+                            "disabled": false,
+                            "accessKey": "",
+                            "nextFocusFieldName": "",
+                            "wrap": true,
+                            "dependencies": [],
+                            "float": 2,
+                            "toolTip": "",
+                            "authorityMode": 0,
+                            "marginLeft": "XS",
+                            "marginRight": "XS",
+                            "onClick": "",
+                            "text": "交付情報を確定する",
+                            "appearance": 0,
+                            "imageFileUrl": "",
+                            "imageWidth": "",
+                            "imageHeight": ""
+                        }
+                    ],
+                    "controlType": "Panel",
+                    "width": "1030",
+                    "visible": true,
+                    "displayNone": false,
+                    "disabled": false,
+                    "accessKey": "",
+                    "nextFocusFieldName": "",
+                    "wrap": false,
+                    "dependencies": [],
+                    "float": 0,
+                    "toolTip": "",
+                    "authorityMode": 0,
+                    "marginLeft": "XS",
+                    "marginRight": "XS",
+                    "onLoad": "",
+                    "title": "",
+                    "marginTop": "Default",
+                    "marginBottom": "Default",
+                    "isOpen": true,
+                    "canOpenAndClose": true,
+                    "postParameterPanelNames": [
+                        {
+                            "postParameterPanelNames": "ShoKaishuKirokuKanri"
+                        }
+                    ],
+                    "requestSettings": [],
+                    "hiddenInput": [],
+                    "onOpen": "",
+                    "onClose": "",
+                    "session": {},
+                    "eraseBorderTop": true,
+                    "eraseBorderBottom": true,
+                    "eraseBorderRight": true,
+                    "eraseBorderLeft": true,
+                    "backgroundColor": 0,
+                    "widthAuto": false,
+                    "isGroupBox": false
+                }
+            ],
+            "controlType": "CommonChildDiv",
+            "width": "XS",
+            "visible": true,
+            "displayNone": false,
+            "disabled": false,
+            "accessKey": "",
+            "nextFocusFieldName": "",
+            "wrap": false,
+            "dependencies": [],
+            "float": 0,
+            "toolTip": "",
+            "authorityMode": 0,
+            "marginLeft": 0,
+            "marginRight": 0,
+            "relation": [],
+            "businessId": "DBZ",
+            "controlName": "ShoKaishuKirokuKanri",
+            "marginTop": 0,
+            "marginBottom": 0,
+            "originalProperty": [],
+            "dataPassingForDialog": [],
+            "dialogOkEventNameForDialog": "",
+            "dialogCancelEventNameForDialog": ""
+        };
+        return ShoKaishuKirokuKanri_Design;
+    })(Uz.CommonChildDiv);
+    DBZ.ShoKaishuKirokuKanri_Design = ShoKaishuKirokuKanri_Design;
+})(DBZ || (DBZ = {}));
