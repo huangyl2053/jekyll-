d8c8a1d6ffa8c1c43111a75e2d4a92b9cac45600
朴　義一 <n8223@ndensan.co.jp>
2014-05-27 15:44:16 +0900

    refs #36918 住宅改修費支給申請 画面作成

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/dgJutakuKaishuDetail_Row.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/dgJutakuKaishuDetail_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbc.divcontroller.entity;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Fri May 23 13:54:59 JST 2014 
+ * Tue May 27 14:01:30 JST 2014 
  */
 
 
