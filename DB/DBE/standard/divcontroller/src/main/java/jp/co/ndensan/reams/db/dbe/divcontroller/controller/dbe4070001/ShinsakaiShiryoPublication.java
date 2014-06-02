1d1150ebacad9e3041129647262943fa256f58e8
松本直樹 <n1013@ndensan.co.jp>
2014-06-02 21:04:27 +0900

    refs #37342 審査会資料作成 イメージマスク手前まで

---
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/controller/dbe4070001/ShinsakaiShiryoPublication.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.divcontroller.controller.dbe4070001;
+
+import java.math.BigDecimal;
+import jp.co.ndensan.reams.db.dbe.divcontroller.entity.dbe4070001.ShinsakaiSearchDiv;
+import jp.co.ndensan.reams.db.dbe.divcontroller.entity.dbe4070001.ShinsakaiShiryoPublicationDiv;
+import jp.co.ndensan.reams.db.dbe.divcontroller.entity.dbe4070001.dgShinsakaiList_Row;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.lang.RTime;
+import jp.co.ndensan.reams.uz.uza.math.Decimal;
+
+/**
+ * 審査会資料作成時の資料発行Divの処理を表します。
+ *
+ * @author N1013 松本直樹
+ */
+public class ShinsakaiShiryoPublication {
+
+    /**
+     * 審査会選択Divの審査会一覧の審査会が選択された際の資料発行Divの処理を表します。
+     *
+     * @param div 資料発行Div
+     * @param searchDiv 審査会選択Div
+     * @return ResponseData
+     */
+    public ResponseData onClick_btnSentaku(ShinsakaiShiryoPublicationDiv div,
+            ShinsakaiSearchDiv searchDiv) {
+        ResponseData<ShinsakaiShiryoPublicationDiv> response = new ResponseData<>();
+
+        dgShinsakaiList_Row dataRow = searchDiv.getShinsakaiList().getDgShinsakaiList().getClickedItem();
+
+        div.getPublicationTargetShinsakai().getTxtGogitaiName().setValue(dataRow.getGogitaiName());
+        div.getPublicationTargetShinsakai().getTxtGogitaiNo().setValue(dataRow.getGogitaiNo());
+        div.getPublicationTargetShinsakai().getTxtShinsakaiKaijo().setValue(dataRow.getShinsakaiKaijo());
+        div.getPublicationTargetShinsakai().getTxtShinsakaiKaisaiNo().setValue(dataRow.getKaisaiNo());
+        div.getPublicationTargetShinsakai().getTxtShinsakaiKaishiYoteiTime().setValue(
+                RTime.parse(dataRow.getKaishiYoteiTime().toString()));
+        div.getPublicationTargetShinsakai().getTxtShinsakaiYoteiDate().setValue(dataRow.getKaisaiYoteiDate().getValue());
+        div.getPublicationTargetShinsakai().getTxtShiryoSakusei().setValue(dataRow.getShiryoSakuseiKubun());
+        Decimal wariateNinzu = new Decimal(dataRow.getWariateNinzu().toString());
+        Decimal yoteiTeiin = new Decimal(dataRow.getYoteiTeiin().toString());
+        div.getPublicationTargetShinsakai().getTxtWariateNinzu().setValue(wariateNinzu);
+        div.getPublicationTargetShinsakai().getTxtYoteiTeiin().setValue(yoteiTeiin);
+
+        div.getLatestPublication().getTxtOperationDate().setValue(FlexibleDate.EMPTY);
+        div.getLatestPublication().getTxtShoriKubun().setValue(RString.EMPTY);
+        div.getLatestPublication().getLatestShinsakai().getTxtLatestGogitaiName().setValue(RString.EMPTY);
+        div.getLatestPublication().getLatestShinsakai().getTxtLatestGogitaiNo().setValue(RString.EMPTY);
+        div.getLatestPublication().getLatestShinsakai().getTxtLatestShinsakaiKaijo().setValue(RString.EMPTY);
+        div.getLatestPublication().getLatestShinsakai().getTxtLatestShinsakaiKaisaiNo().setValue(RString.EMPTY);
+        div.getLatestPublication().getLatestShinsakai().getTxtLatestShinsakaiKaishiYoteiTime().setValue(null);
+        div.getLatestPublication().getLatestShinsakai().getTxtLatestShinsakaiYoteiDate().setValue(FlexibleDate.EMPTY);
+
+        div.getPublishingCondition1().getPublishingConditionForJimukyoku1().getTxtCopyNumForJimukyoku1().setValue(Decimal.ONE);
+        div.getPublishingCondition1().getPublishingConditionForShinsakaiIin1().getTxtCopyNumForShinsakaiIin1().setValue(Decimal.ONE);
+
+        response.data = div;
+        return response;
+    }
+
+    /**
+     * 資料発行Divの「発行の範囲を指定する」ボタン押下時の処理を表します。
+     *
+     * @param div 資料発行Div
+     * @param searchDiv 審査会選択Div
+     * @return ResponseData
+     */
+    public ResponseData onClick_btnToChangeCondition2(ShinsakaiShiryoPublicationDiv div,
+            ShinsakaiSearchDiv searchDiv) {
+        ResponseData<ShinsakaiShiryoPublicationDiv> response = new ResponseData<>();
+
+        div.getPublishingCondition2().getPublishingConditionForJimukyoku2().getTtxtCopyNumForJimukyoku2().setValue(Decimal.ONE);
+        div.getPublishingCondition2().getPublishingConditionForJimukyoku2().getPageRangeForJimukyoku().setTxtPageRangeForJimukyokuFrom(null);
+        div.getPublishingCondition2().getPublishingConditionForJimukyoku2().getPageRangeForJimukyoku().setTxtPageRangeForJimukyokuTo(null);
+
+        div.getPublishingCondition2().getPublishingConditionForShinsakaiIin2().getTxtCopyNumForShinsakaiIin2().setValue(Decimal.ONE);
+        div.getPublishingCondition2().getPublishingConditionForShinsakaiIin2().getPageRangeAssignmentForShinsakaiIin().
+                setTxtPageRangeAssignmentForShinsakaiIinFrom(null);
+        div.getPublishingCondition2().getPublishingConditionForShinsakaiIin2().getPageRangeAssignmentForShinsakaiIin().
+                setTxtPageRangeAssignmentForShinsakaiIinTo(null);
+
+        response.data = div;
+        return response;
+    }
+
+    /**
+     * 資料発行Divの作成条件指定画面の「戻る」ボタン押下時の処理を表します。
+     *
+     * @param div 資料発行Div
+     * @param searchDiv 審査会選択Div
+     * @return ResponseData
+     */
+    public ResponseData onClick_btnToReturn(ShinsakaiShiryoPublicationDiv div,
+            ShinsakaiSearchDiv searchDiv) {
+        ResponseData<ShinsakaiShiryoPublicationDiv> response = new ResponseData<>();
+
+        div.getPublishingCondition1().getPublishingConditionForJimukyoku1().getTxtCopyNumForJimukyoku1().setValue(Decimal.ONE);
+        div.getPublishingCondition1().getPublishingConditionForShinsakaiIin1().getTxtCopyNumForShinsakaiIin1().setValue(Decimal.ONE);
+
+        response.data = div;
+        return response;
+    }
+
+}
---
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/dbe4070001/SearchCriteriaForShinsakaiDiv.java
@@ -0,0 +1,68 @@
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.dbe4070001;
+/**
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.db.dbe.divcontroller.entity.dbe4070001.ButtonsForShinsakaiSearchDiv;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+import jp.co.ndensan.reams.uz.uza.ui.binding.domain.*;
+
+/**
+ * SearchCriteriaForShinsakai のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class SearchCriteriaForShinsakaiDiv extends Panel {
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("lblKaisaiDateRange")
+    private Label lblKaisaiDateRange;
+    @JsonProperty("txtKaisaiDateRange")
+    private TextBoxDateRange txtKaisaiDateRange;
+    @JsonProperty("ButtonsForShinsakaiSearch")
+    private ButtonsForShinsakaiSearchDiv ButtonsForShinsakaiSearch;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    @JsonProperty("lblKaisaiDateRange")
+    public Label getLblKaisaiDateRange() {
+        return lblKaisaiDateRange;
+    }
+
+    @JsonProperty("lblKaisaiDateRange")
+    public void setLblKaisaiDateRange(Label lblKaisaiDateRange) {
+        this.lblKaisaiDateRange=lblKaisaiDateRange;
+    }
+
+    @JsonProperty("txtKaisaiDateRange")
+    public TextBoxDateRange getTxtKaisaiDateRange() {
+        return txtKaisaiDateRange;
+    }
+
+    @JsonProperty("txtKaisaiDateRange")
+    public void setTxtKaisaiDateRange(TextBoxDateRange txtKaisaiDateRange) {
+        this.txtKaisaiDateRange=txtKaisaiDateRange;
+    }
+
+    @JsonProperty("ButtonsForShinsakaiSearch")
+    public ButtonsForShinsakaiSearchDiv getButtonsForShinsakaiSearch() {
+        return ButtonsForShinsakaiSearch;
+    }
+
+    @JsonProperty("ButtonsForShinsakaiSearch")
+    public void setButtonsForShinsakaiSearch(ButtonsForShinsakaiSearchDiv ButtonsForShinsakaiSearch) {
+        this.ButtonsForShinsakaiSearch=ButtonsForShinsakaiSearch;
+    }
+
+}
