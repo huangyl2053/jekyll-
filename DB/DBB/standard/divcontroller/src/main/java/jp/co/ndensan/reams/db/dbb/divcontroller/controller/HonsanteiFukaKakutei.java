3c1865b98ac572f63c41079afc35031963a13386
田辺 紘一 <n8211@ndensan.co.jp>
2014-06-03 19:26:46 +0900

    refs #35381 本算定賦課確定 PH2実装

---
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/controller/HonsanteiFukaKakutei.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jp.co.ndensan.reams.db.dbb.divcontroller.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import jp.co.ndensan.reams.db.dbb.divcontroller.entity.DBB0310001.HonsanteiFukaKakuteiDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.helper.YamlLoader;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+/**
+ *
+ * @author n8211
+ */
+public class HonsanteiFukaKakutei {
+    public ResponseData<HonsanteiFukaKakuteiDiv> onLoad(HonsanteiFukaKakuteiDiv honsanteiFukaKakuteiDiv) {
+        ResponseData<HonsanteiFukaKakuteiDiv> response = new ResponseData<>();
+
+        setParam(honsanteiFukaKakuteiDiv);
+
+        response.data = honsanteiFukaKakuteiDiv;
+        return response;
+    }
+
+    private void setParam(HonsanteiFukaKakuteiDiv honsanteiFukaKakuteiDiv) {
+        List<HashMap> fukaKakuteiData = YamlLoader.FOR_DBB.loadAsList(new RString("FukaKakutei.yml"));
+        
+        
+        String 処理日 = (String) fukaKakuteiData.get(0).get("処理日");
+        String 処理時 = (String) fukaKakuteiData.get(0).get("処理時");
+        String 賦課年度 = (String) fukaKakuteiData.get(0).get("賦課年度");
+        
+        StringBuilder buf = new StringBuilder();
+        buf.append(処理日);
+        buf.append(" ");
+        buf.append(処理時);
+        
+        String 処理日時 = buf.toString();
+        
+        honsanteiFukaKakuteiDiv.getHonsanteiShoriNichiji().getTxtKakuteiFukaNendo().setValue(new RString(賦課年度));
+        honsanteiFukaKakuteiDiv.getHonsanteiShoriNichiji().getTxtHonsanteiShoriNichiji().setValue(new RString(処理日時));
+    }
+}
