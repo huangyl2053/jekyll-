c0be6dec64e7291e4ec73a1dd22d6e5e78148cfe
城間篤人 <n8178@ndensan.co.jp>
2014-06-12 16:25:28 +0900

    refs #37335 pull後に、ファイル構成が変わったためエラー発生。エラー箇所のインポート文を修正。

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/dbzu020001/HihokenshaSearchDiv.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/dbzu020001/HihokenshaSearchDiv.java
@@ -4,8 +4,8 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.entity.dbzu020001;
  * このコードはツールによって生成されました。 このファイルへの変更は、再生成時には損失するため 不正な動作の原因になります。
  */
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jp.co.ndensan.reams.db.dbz.divcontroller.entity.HihokenshaFinderDiv;
-import jp.co.ndensan.reams.db.dbz.divcontroller.entity.SearchResultOfHihokenshaDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.hihokenshaFinder.HihokenshaFinderDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.searchResultOfHihokensha.SearchResultOfHihokenshaDiv;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
 
 /**
