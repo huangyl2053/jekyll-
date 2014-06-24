0d3641e0068cc09958553a32d674d26bde791576
朴　義一 <n8223@ndensan.co.jp>
2014-06-24 12:05:32 +0900

    refs #37986 介護給付費過誤申立書登録 プラッシュアップ 対応　(yml漏れ） refs #37973 高額サービス費支給決定通知書作成　プラッシュアップ 対応

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/KogakuShikyuKetteiTsuchiSakusei.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/KogakuShikyuKetteiTsuchiSakusei.java
@@ -7,9 +7,12 @@
 package jp.co.ndensan.reams.db.dbc.divcontroller.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import java.util.List;
-import jp.co.ndensan.reams.db.dbc.divcontroller.entity.KogakuShikyuKetteiTsuchiSakuseiDiv;
+import jp.co.ndensan.reams.db.dbc.divcontroller.entity.DBC0490011.KogakuShikyuKetteiTsuchiSakuseiDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.helper.ControlGenerator;
+import jp.co.ndensan.reams.db.dbz.divcontroller.helper.YamlLoader;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
 import jp.co.ndensan.reams.uz.uza.lang.RDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
@@ -24,27 +27,53 @@ public class KogakuShikyuKetteiTsuchiSakusei {
     public  ResponseData<KogakuShikyuKetteiTsuchiSakuseiDiv> onLoad(KogakuShikyuKetteiTsuchiSakuseiDiv panel) {
          ResponseData<KogakuShikyuKetteiTsuchiSakuseiDiv> response = new ResponseData<>();
 
+        setKogakuShikyuKetteiTsuchiSakusei(panel);  
+         
         //初期値を設定したいものに値を入れる。決定年月　通知送達日　
-        panel.getTxtKetteiYM().setValue(new RDate("201406"));
-        panel.getTxtTsuchiSotatsuYMD().setValue(new RDate("20140701"));
-        
-        //初期値を設定したいものに値を入れる。出力対象区分（
-        List<KeyValueDataSource> dataSource = new ArrayList<>();
-        dataSource.add(new KeyValueDataSource(new RString("ForRiyosha"), new RString("利用者向け決定通知書")));
-        dataSource.add(new KeyValueDataSource(new RString("ForJuryoininsha"), new RString("受領委任者向け決定通知書")));
-        panel.getChkOutputTargetKubun().setSelectedItems(dataSource);
-        
+        //初期値を設定したいものに値を入れる。出力対象区分
         //初期値を設定したいものに値を入れる。  高額介護サービス費支給（不支給）決定者一覧表       
-        panel.getKariPanel().getTxtNewPageItem().setValue(new RString("氏名５０音カナ"));
-        panel.getKariPanel().getTxt1().setValue(new RString("被保険者番号"));
-        panel.getKariPanel().getTxt2().setValue(new RString("行政区コード"));
-        panel.getKariPanel().getTxt3().setValue(new RString("氏名５０音カナ"));
-        panel.getKariPanel().getTxt4().setValue(new RString("決定通知番号"));
-        panel.getKariPanel().getTxt5().setValue(new RString("市町村コード"));
+//        panel.getKariPanel().getTxtNewPageItem().setValue(new RString("氏名５０音カナ"));
+//        panel.getKariPanel().getTxt1().setValue(new RString("被保険者番号"));
+//        panel.getKariPanel().getTxt2().setValue(new RString("行政区コード"));
+//        panel.getKariPanel().getTxt3().setValue(new RString("氏名５０音カナ"));
+//        panel.getKariPanel().getTxt4().setValue(new RString("決定通知番号"));
+//        panel.getKariPanel().getTxt5().setValue(new RString("市町村コード"));
 
         response.data = panel;        
         return response;
     }
+
+    private void setKogakuShikyuKetteiTsuchiSakusei(KogakuShikyuKetteiTsuchiSakuseiDiv panel) {
+        //初期値を設定したいものに値を入れる。出力対象区分
+        List<KeyValueDataSource> dataSource = new ArrayList<>();
+        dataSource.add(new KeyValueDataSource(new RString("ForRiyosha"), new RString("利用者向け決定通知書")));
+        dataSource.add(new KeyValueDataSource(new RString("ForJuryoininsha"), new RString("受領委任者向け決定通知書")));
+        panel.getChkOutputTargetKubun().setSelectedItems(dataSource);
+ 
+        List<HashMap> ymlData = ymlData("dbc0490011/KogakuShikyuKetteiTsuchiSakusei.yml");
+        HashMap hashMap = ymlData.get(0);
+        ControlGenerator ymlDt = new ControlGenerator(hashMap);
+        
+        //初期値を設定したいものに値を入れる。決定年月　通知送達日　
+        panel.getTxtKetteiYM().setValue(ymlDt.getAsRDate("KetteiYM"));
+        panel.getTxtTsuchiSotatsuYMD().setValue(ymlDt.getAsRDate("TsuchiSotatsuYMD"));
+        
+        panel.getCommonKariChildDiv1().getCcdChohyoShutsuryokujun().getTxtSortName()
+                .setValue(ymlDt.getAsRString("SortName"));
+        panel.getCommonKariChildDiv1().getCcdChohyoShutsuryokujun().getTxtKaiPage().
+                setValue(ymlDt.getAsRString("KaiPage"));
+        panel.getCommonKariChildDiv1().getCcdChohyoShutsuryokujun().getTxtSort().
+                setValue(ymlDt.getAsRString("Sort"));
+       
+        
+        
+    }
+
+    private List<HashMap> ymlData(String ymlName) {
+        return YamlLoader.DBC.loadAsList(new RString(ymlName));
+    }
+
+    
 
  
     
---
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/DBC0490011/KogakuShikyuKetteiTsuchiSakuseiDiv.java
@@ -0,0 +1,116 @@
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.DBC0490011;
+/**
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.chohyoShutsuryokujun.KaigoChohyoShutsuryokujunDiv;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+import jp.co.ndensan.reams.uz.uza.ui.binding.domain.*;
+
+/**
+ * KogakuShikyuKetteiTsuchiSakusei のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class KogakuShikyuKetteiTsuchiSakuseiDiv extends Panel {
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("txtKetteiYM")
+    private TextBoxDate txtKetteiYM;
+    @JsonProperty("txtTsuchiSotatsuYMD")
+    private TextBoxDate txtTsuchiSotatsuYMD;
+    @JsonProperty("chkOutputTargetKubun")
+    private CheckBoxList chkOutputTargetKubun;
+    @JsonProperty("lblReportCreateKubun")
+    private Label lblReportCreateKubun;
+    @JsonProperty("radIsCreateIchiranhyo")
+    private RadioButton radIsCreateIchiranhyo;
+    @JsonProperty("CommonKariChildDiv1")
+    private KaigoChohyoShutsuryokujunDiv CommonKariChildDiv1;
+    @JsonProperty("btnCreate")
+    private Button btnCreate;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    @JsonProperty("txtKetteiYM")
+    public TextBoxDate getTxtKetteiYM() {
+        return txtKetteiYM;
+    }
+
+    @JsonProperty("txtKetteiYM")
+    public void setTxtKetteiYM(TextBoxDate txtKetteiYM) {
+        this.txtKetteiYM=txtKetteiYM;
+    }
+
+    @JsonProperty("txtTsuchiSotatsuYMD")
+    public TextBoxDate getTxtTsuchiSotatsuYMD() {
+        return txtTsuchiSotatsuYMD;
+    }
+
+    @JsonProperty("txtTsuchiSotatsuYMD")
+    public void setTxtTsuchiSotatsuYMD(TextBoxDate txtTsuchiSotatsuYMD) {
+        this.txtTsuchiSotatsuYMD=txtTsuchiSotatsuYMD;
+    }
+
+    @JsonProperty("chkOutputTargetKubun")
+    public CheckBoxList getChkOutputTargetKubun() {
+        return chkOutputTargetKubun;
+    }
+
+    @JsonProperty("chkOutputTargetKubun")
+    public void setChkOutputTargetKubun(CheckBoxList chkOutputTargetKubun) {
+        this.chkOutputTargetKubun=chkOutputTargetKubun;
+    }
+
+    @JsonProperty("lblReportCreateKubun")
+    public Label getLblReportCreateKubun() {
+        return lblReportCreateKubun;
+    }
+
+    @JsonProperty("lblReportCreateKubun")
+    public void setLblReportCreateKubun(Label lblReportCreateKubun) {
+        this.lblReportCreateKubun=lblReportCreateKubun;
+    }
+
+    @JsonProperty("radIsCreateIchiranhyo")
+    public RadioButton getRadIsCreateIchiranhyo() {
+        return radIsCreateIchiranhyo;
+    }
+
+    @JsonProperty("radIsCreateIchiranhyo")
+    public void setRadIsCreateIchiranhyo(RadioButton radIsCreateIchiranhyo) {
+        this.radIsCreateIchiranhyo=radIsCreateIchiranhyo;
+    }
+
+    @JsonProperty("CommonKariChildDiv1")
+    public KaigoChohyoShutsuryokujunDiv getCommonKariChildDiv1() {
+        return CommonKariChildDiv1;
+    }
+
+    @JsonProperty("CommonKariChildDiv1")
+    public void setCommonKariChildDiv1(KaigoChohyoShutsuryokujunDiv CommonKariChildDiv1) {
+        this.CommonKariChildDiv1=CommonKariChildDiv1;
+    }
+
+    @JsonProperty("btnCreate")
+    public Button getBtnCreate() {
+        return btnCreate;
+    }
+
+    @JsonProperty("btnCreate")
+    public void setBtnCreate(Button btnCreate) {
+        this.btnCreate=btnCreate;
+    }
+
+}
