79a468cdedd66dd4769975df1d0b9564728de291
立野　啓子 <n2006@ndensan.co.jp>
2014-06-24 11:21:52 +0900

    refs #37736 補足情報追加 共有子Div（連帯納付義務者、収納情報、滞納情報）pushu漏れ

---
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/ShunoJoho/ShunoJohoTokuchoDiv.java
@@ -0,0 +1,68 @@
+package jp.co.ndensan.reams.db.dbb.divcontroller.entity.ShunoJoho;
+/**
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.db.dbb.divcontroller.entity.ShunoJoho.dgTokucho_Row;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+import jp.co.ndensan.reams.uz.uza.ui.binding.domain.*;
+
+/**
+ * ShunoJohoTokucho のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class ShunoJohoTokuchoDiv extends Panel {
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("dgTokucho")
+    private DataGrid<dgTokucho_Row> dgTokucho;
+    @JsonProperty("txtTokuChoteiGeoukei")
+    private TextBoxNum txtTokuChoteiGeoukei;
+    @JsonProperty("txtTokuShunyuGeoukei")
+    private TextBoxNum txtTokuShunyuGeoukei;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    @JsonProperty("dgTokucho")
+    public DataGrid<dgTokucho_Row> getDgTokucho() {
+        return dgTokucho;
+    }
+
+    @JsonProperty("dgTokucho")
+    public void setDgTokucho(DataGrid<dgTokucho_Row> dgTokucho) {
+        this.dgTokucho=dgTokucho;
+    }
+
+    @JsonProperty("txtTokuChoteiGeoukei")
+    public TextBoxNum getTxtTokuChoteiGeoukei() {
+        return txtTokuChoteiGeoukei;
+    }
+
+    @JsonProperty("txtTokuChoteiGeoukei")
+    public void setTxtTokuChoteiGeoukei(TextBoxNum txtTokuChoteiGeoukei) {
+        this.txtTokuChoteiGeoukei=txtTokuChoteiGeoukei;
+    }
+
+    @JsonProperty("txtTokuShunyuGeoukei")
+    public TextBoxNum getTxtTokuShunyuGeoukei() {
+        return txtTokuShunyuGeoukei;
+    }
+
+    @JsonProperty("txtTokuShunyuGeoukei")
+    public void setTxtTokuShunyuGeoukei(TextBoxNum txtTokuShunyuGeoukei) {
+        this.txtTokuShunyuGeoukei=txtTokuShunyuGeoukei;
+    }
+
+}
---
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/ShunoJoho/dgTokucho_Row.java
@@ -0,0 +1,149 @@
+package jp.co.ndensan.reams.db.dbb.divcontroller.entity.ShunoJoho;
+/**
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+ * コードが再生成されるときに損失したりします。
+ * Tue Jun 24 09:38:16 JST 2014 
+ */
+
+
+
+import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+
+/**
+ * dgTokucho_Row のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+
+public class dgTokucho_Row extends DataRow {
+
+    private RString txtKi;
+    private RString txtChoteiGaku;
+    private RString txtShunyuGaku;
+    private RString txtShunyuYMD;
+    private RString txtRyosyuYMD;
+    private RString txtTokusokuTesuryo;
+    private RString txtEntaikin;
+    private RString txtChoteiYMD;
+    private RString txtNokigen;
+
+    public dgTokucho_Row() {
+        super();
+        this.txtKi = RString.EMPTY;
+        this.txtChoteiGaku = RString.EMPTY;
+        this.txtShunyuGaku = RString.EMPTY;
+        this.txtShunyuYMD = RString.EMPTY;
+        this.txtRyosyuYMD = RString.EMPTY;
+        this.txtTokusokuTesuryo = RString.EMPTY;
+        this.txtEntaikin = RString.EMPTY;
+        this.txtChoteiYMD = RString.EMPTY;
+        this.txtNokigen = RString.EMPTY;
+    }
+
+    public dgTokucho_Row(RString txtKi, RString txtChoteiGaku, RString txtShunyuGaku, RString txtShunyuYMD, RString txtRyosyuYMD, RString txtTokusokuTesuryo, RString txtEntaikin, RString txtChoteiYMD, RString txtNokigen) {
+        super();
+        this.setOriginalData("txtKi", txtKi);
+        this.setOriginalData("txtChoteiGaku", txtChoteiGaku);
+        this.setOriginalData("txtShunyuGaku", txtShunyuGaku);
+        this.setOriginalData("txtShunyuYMD", txtShunyuYMD);
+        this.setOriginalData("txtRyosyuYMD", txtRyosyuYMD);
+        this.setOriginalData("txtTokusokuTesuryo", txtTokusokuTesuryo);
+        this.setOriginalData("txtEntaikin", txtEntaikin);
+        this.setOriginalData("txtChoteiYMD", txtChoteiYMD);
+        this.setOriginalData("txtNokigen", txtNokigen);
+        this.txtKi = txtKi;
+        this.txtChoteiGaku = txtChoteiGaku;
+        this.txtShunyuGaku = txtShunyuGaku;
+        this.txtShunyuYMD = txtShunyuYMD;
+        this.txtRyosyuYMD = txtRyosyuYMD;
+        this.txtTokusokuTesuryo = txtTokusokuTesuryo;
+        this.txtEntaikin = txtEntaikin;
+        this.txtChoteiYMD = txtChoteiYMD;
+        this.txtNokigen = txtNokigen;
+    }
+
+    public RString getTxtKi() {
+        return txtKi;
+    }
+
+    public RString getTxtChoteiGaku() {
+        return txtChoteiGaku;
+    }
+
+    public RString getTxtShunyuGaku() {
+        return txtShunyuGaku;
+    }
+
+    public RString getTxtShunyuYMD() {
+        return txtShunyuYMD;
+    }
+
+    public RString getTxtRyosyuYMD() {
+        return txtRyosyuYMD;
+    }
+
+    public RString getTxtTokusokuTesuryo() {
+        return txtTokusokuTesuryo;
+    }
+
+    public RString getTxtEntaikin() {
+        return txtEntaikin;
+    }
+
+    public RString getTxtChoteiYMD() {
+        return txtChoteiYMD;
+    }
+
+    public RString getTxtNokigen() {
+        return txtNokigen;
+    }
+
+    public void setTxtKi(RString txtKi) {
+        this.setOriginalData("txtKi", txtKi);
+        this.txtKi = txtKi;
+    }
+
+    public void setTxtChoteiGaku(RString txtChoteiGaku) {
+        this.setOriginalData("txtChoteiGaku", txtChoteiGaku);
+        this.txtChoteiGaku = txtChoteiGaku;
+    }
+
+    public void setTxtShunyuGaku(RString txtShunyuGaku) {
+        this.setOriginalData("txtShunyuGaku", txtShunyuGaku);
+        this.txtShunyuGaku = txtShunyuGaku;
+    }
+
+    public void setTxtShunyuYMD(RString txtShunyuYMD) {
+        this.setOriginalData("txtShunyuYMD", txtShunyuYMD);
+        this.txtShunyuYMD = txtShunyuYMD;
+    }
+
+    public void setTxtRyosyuYMD(RString txtRyosyuYMD) {
+        this.setOriginalData("txtRyosyuYMD", txtRyosyuYMD);
+        this.txtRyosyuYMD = txtRyosyuYMD;
+    }
+
+    public void setTxtTokusokuTesuryo(RString txtTokusokuTesuryo) {
+        this.setOriginalData("txtTokusokuTesuryo", txtTokusokuTesuryo);
+        this.txtTokusokuTesuryo = txtTokusokuTesuryo;
+    }
+
+    public void setTxtEntaikin(RString txtEntaikin) {
+        this.setOriginalData("txtEntaikin", txtEntaikin);
+        this.txtEntaikin = txtEntaikin;
+    }
+
+    public void setTxtChoteiYMD(RString txtChoteiYMD) {
+        this.setOriginalData("txtChoteiYMD", txtChoteiYMD);
+        this.txtChoteiYMD = txtChoteiYMD;
+    }
+
+    public void setTxtNokigen(RString txtNokigen) {
+        this.setOriginalData("txtNokigen", txtNokigen);
+        this.txtNokigen = txtNokigen;
+    }
+
+}
