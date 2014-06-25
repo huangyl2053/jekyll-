c96f63e25914137444f3a573430fe34e78e2430e
城間篤人 <n8178@ndensan.co.jp>
2014-06-25 21:33:08 +0900

    refs #37335 被保険者照会 証回収管理共有子Divの修正に伴うimport文の変更。

--- DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/controller/HihokenshaShokaiPanelSelect.java
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/controller/HihokenshaShokaiPanelSelect.java
@@ -23,7 +23,7 @@ import jp.co.ndensan.reams.db.dba.divcontroller.entity.dba4010011.dgSeikatsuHogo
 import jp.co.ndensan.reams.db.dba.divcontroller.entity.dba4010011.dgSetaiJoho_Row;
 import jp.co.ndensan.reams.db.dba.divcontroller.entity.dba4010011.dgShinseishoTodokede_Row;
 import jp.co.ndensan.reams.db.dbz.divcontroller.controller.ShisetsuNyutaishoRirekiKanri;
-import jp.co.ndensan.reams.db.dbz.divcontroller.ShoKaishuKirokuKanri;
+import jp.co.ndensan.reams.db.dbz.divcontroller.controller.ShoKaishuKirokuKanri;
 import jp.co.ndensan.reams.db.dbz.divcontroller.controller.IryoHokenRireki;
 import jp.co.ndensan.reams.db.dbz.divcontroller.controller.ShikakuTokusoRireki;
 import jp.co.ndensan.reams.db.dbz.divcontroller.entity.iryohokenrireki.dgIryoHokenRireki_Row;
