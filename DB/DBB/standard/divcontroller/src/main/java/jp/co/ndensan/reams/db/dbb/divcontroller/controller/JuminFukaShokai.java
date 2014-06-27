4f0c7aaf1bbd3fb2df1252a17e72128d5958149e
大谷 和弘 <n3336@ndensan.co.jp>
2014-06-27 16:41:48 +0900

    refs#DBB8110001 個別通知コード追加

--- DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/controller/JuminFukaShokai.java
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/controller/JuminFukaShokai.java
@@ -6,11 +6,16 @@
 
 package jp.co.ndensan.reams.db.dbb.divcontroller.controller;
 
+import java.util.HashMap;
+import java.util.List;
 import jp.co.ndensan.reams.db.dbb.divcontroller.entity.JuminFukaShokaiDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.helper.YamlLoader;
 import jp.co.ndensan.reams.ur.ura.divcontroller.controller.AtenaShokaiSimple;
 import jp.co.ndensan.reams.ur.ura.divcontroller.entity.AtenaShokaiSimpleDiv;
 import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
  *
@@ -23,8 +28,16 @@ public class JuminFukaShokai {
     public ResponseData<JuminFukaShokaiDiv> onLoad_JuminFukaShokai(JuminFukaShokaiDiv panel) {
         ResponseData<JuminFukaShokaiDiv> response = new ResponseData<>();
         
-        AtenaShokaiSimpleDiv a = panel.getKaigoatena().getAtenaInfo();
-        AtenaShokaiSimple.setData(a, new ShikibetsuCode("000000000000094"));
+        AtenaShokaiSimple.setData(panel.getKaigoatena().getAtenaInfo(), new ShikibetsuCode("000000000000094"));
+        
+        List<HashMap> SokujiKouseiKihonData = YamlLoader.DBB.loadAsList(new RString("DBB8120001/SokujiKouseiKihon.yml"));
+        panel.getKaigoKihon().getTxtTsuchishoNo().setValue(new RString(SokujiKouseiKihonData.get(0).get("通知書番号").toString()));
+        panel.getKaigoKihon().getTxtHihokenshaNo().setValue(new RString(SokujiKouseiKihonData.get(0).get("被保番号").toString()));
+        panel.getKaigoKihon().getTxtHokenryoDankai().setValue(new RString(SokujiKouseiKihonData.get(0).get("保険料段階").toString()));
+        panel.getKaigoKihon().getTxtShutokuYmd().setValue(new RDate(SokujiKouseiKihonData.get(0).get("資格取得日").toString()));
+        panel.getKaigoKihon().getTxtShutokuJiyu().setValue(new RString(SokujiKouseiKihonData.get(0).get("資格取得事由").toString()));
+        panel.getKaigoKihon().getTxtSoshitsuYmd().clearValue();
+        panel.getKaigoKihon().getTxtSoshitsuJiyu().setValue(new RString(SokujiKouseiKihonData.get(0).get("資格喪失事由").toString()));   
             
         response.data = panel;
         return response;
--- DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/controller/TsuchishoSakuseiKobetsu.java
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/controller/TsuchishoSakuseiKobetsu.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package jp.co.ndensan.reams.db.dbb.divcontroller.controller;
 
 import java.util.ArrayList;
@@ -18,28 +17,147 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
  * @author n3412
  */
 public class TsuchishoSakuseiKobetsu {
+
     public ResponseData<TsuchishoSakuseiKobetsuDiv> onLoad_TsuchishoSakuseiKobetsu(TsuchishoSakuseiKobetsuDiv panel) {
         ResponseData<TsuchishoSakuseiKobetsuDiv> response = new ResponseData<>();
+
+        init(panel);
+
         List<dgChohyoSentaku_Row> arrayData = new ArrayList<>();
-        
-        arrayData.add( set_dgChohyoSentaku_Row(new RString("決定通知書")) );
-        arrayData.add( set_dgChohyoSentaku_Row(new RString("変更兼特徴中止通知書")) );
-        arrayData.add( set_dgChohyoSentaku_Row(new RString("納入通知書")) );
-        arrayData.add( set_dgChohyoSentaku_Row(new RString("郵便振替納付書")) );
-        arrayData.add( set_dgChohyoSentaku_Row(new RString("賦課台帳")) );
-         
+
+        arrayData.add(set_dgChohyoSentaku_Row(new RString("決定通知書")));
+        arrayData.add(set_dgChohyoSentaku_Row(new RString("変更兼特徴中止通知書")));
+        arrayData.add(set_dgChohyoSentaku_Row(new RString("納入通知書")));
+        arrayData.add(set_dgChohyoSentaku_Row(new RString("郵便振替納付書")));
+        arrayData.add(set_dgChohyoSentaku_Row(new RString("賦課台帳")));
+
         panel.getDgChohyoSentaku().setDataSource(arrayData);
-        
+
         response.data = panel;
         return response;
     }
-    
-    
-    
-    
-    private dgChohyoSentaku_Row set_dgChohyoSentaku_Row(RString s){
+
+    private dgChohyoSentaku_Row set_dgChohyoSentaku_Row(RString s) {
         dgChohyoSentaku_Row row = new dgChohyoSentaku_Row(RString.EMPTY);
         row.setTxtChohyoSentaku(s);
         return row;
     }
+
+    private void init(TsuchishoSakuseiKobetsuDiv panel) {
+        panel.getTokuKaishiTsuchiKobetsu().setIsOpen(false);
+        
+        panel.getKetteiTsuchiKobetsu().setIsOpen(false);
+        panel.getKetteiTsuchiKobetsu().setIsPublish(false);
+
+        panel.getHenkoTsuchiKobetsu().setIsOpen(false);
+        panel.getHenkoTsuchiKobetsu().setIsPublish(false);
+
+        panel.getNotsuKobetsu().setIsOpen(false);
+        panel.getNotsuKobetsu().setIsPublish(false);
+
+        panel.getGemmenTsuchiKobetsu().setIsOpen(false);
+        
+        panel.getYufuriKobetsu().setIsOpen(false);
+        panel.getYufuriKobetsu().setIsPublish(false);
+        
+        panel.getChoshuYuyoTsuchiKobetsu().setIsOpen(false);
+
+        panel.getFukadaichoKobetsu().setIsOpen(false);
+        panel.getFukadaichoKobetsu().setIsPublish(false);
+    }
+
+    public ResponseData<TsuchishoSakuseiKobetsuDiv> onSelect_dgChohyoSentaku_Row(TsuchishoSakuseiKobetsuDiv panel) { 
+
+        ResponseData<TsuchishoSakuseiKobetsuDiv> response = new ResponseData<>();
+
+        change_KetteiTsuchiKobetsu(panel);
+        change_HenkoTsuchiKobetsu(panel);
+        change_NotsuKobetsu(panel);
+        change_YufuriKobetsu(panel);
+        change_FukadaichoKobetsu(panel);
+
+        response.data = panel;
+        return response;
+    }
+    
+    private void change_KetteiTsuchiKobetsu(TsuchishoSakuseiKobetsuDiv panel) {
+
+        if (panel.getDgChohyoSentaku().getClickedItem().getTxtChohyoSentaku().equals(new RString("決定通知書"))) {
+            if (panel.getKetteiTsuchiKobetsu().isIsOpen() == false) {
+                panel.getKetteiTsuchiKobetsu().setIsOpen(true);
+            } else {
+                panel.getKetteiTsuchiKobetsu().setIsOpen(false);
+            }
+            if (panel.getKetteiTsuchiKobetsu().isIsPublish() == false) {
+                panel.getKetteiTsuchiKobetsu().setIsPublish(true);
+            } else {
+                panel.getKetteiTsuchiKobetsu().setIsPublish(false);
+            }
+        }
+    }
+
+    private void change_HenkoTsuchiKobetsu(TsuchishoSakuseiKobetsuDiv panel) {
+
+        if (panel.getDgChohyoSentaku().getClickedItem().getTxtChohyoSentaku().equals(new RString("変更兼特徴中止通知書"))) {
+            if (panel.getHenkoTsuchiKobetsu().isIsOpen() == false) {
+                panel.getHenkoTsuchiKobetsu().setIsOpen(true);
+            } else {
+                panel.getHenkoTsuchiKobetsu().setIsOpen(false);
+            }
+            if (panel.getHenkoTsuchiKobetsu().isIsPublish() == false) {
+                panel.getHenkoTsuchiKobetsu().setIsPublish(true);
+            } else {
+                panel.getHenkoTsuchiKobetsu().setIsPublish(false);
+            }
+        }
+    }
+
+    private void change_NotsuKobetsu(TsuchishoSakuseiKobetsuDiv panel) {
+
+        if (panel.getDgChohyoSentaku().getClickedItem().getTxtChohyoSentaku().equals(new RString("納入通知書"))) {
+            if (panel.getNotsuKobetsu().isIsOpen() == false) {
+                panel.getNotsuKobetsu().setIsOpen(true);
+            } else {
+                panel.getNotsuKobetsu().setIsOpen(false);
+            }
+            if (panel.getNotsuKobetsu().isIsPublish() == false) {
+                panel.getNotsuKobetsu().setIsPublish(true);
+            } else {
+                panel.getNotsuKobetsu().setIsPublish(false);
+            }
+        }
+    }
+
+    private void change_YufuriKobetsu(TsuchishoSakuseiKobetsuDiv panel) {
+
+        if (panel.getDgChohyoSentaku().getClickedItem().getTxtChohyoSentaku().equals(new RString("郵便振替納付書"))) {
+            if (panel.getYufuriKobetsu().isIsOpen() == false) {
+                panel.getYufuriKobetsu().setIsOpen(true);
+            } else {
+                panel.getYufuriKobetsu().setIsOpen(false);
+            }
+            if (panel.getYufuriKobetsu().isIsPublish() == false) {
+                panel.getYufuriKobetsu().setIsPublish(true);
+            } else {
+                panel.getYufuriKobetsu().setIsPublish(false);
+            }
+        }
+    }
+
+    private void change_FukadaichoKobetsu(TsuchishoSakuseiKobetsuDiv panel) {
+
+        if (panel.getDgChohyoSentaku().getClickedItem().getTxtChohyoSentaku().equals(new RString("賦課台帳"))) {
+            if (panel.getFukadaichoKobetsu().isIsOpen() == false) {
+                panel.getFukadaichoKobetsu().setIsOpen(true);
+            } else {
+                panel.getFukadaichoKobetsu().setIsOpen(false);
+            }
+            if (panel.getFukadaichoKobetsu().isIsPublish() == false) {
+                panel.getFukadaichoKobetsu().setIsPublish(true);
+            } else {
+                panel.getFukadaichoKobetsu().setIsPublish(false);
+            }
+        }
+    }
+
 }
