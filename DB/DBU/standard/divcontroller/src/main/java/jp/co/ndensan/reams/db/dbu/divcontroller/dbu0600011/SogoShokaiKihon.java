62490fc60c5450e09c2d5761d0ecec8893a804e3
漢那憲作 <n9606@ndensan.co.jp>
2014-06-30 11:02:58 +0900

    refs #37336 総合照会 親Div分割対応

--- DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/dbu0600011/SogoShokaiKihon.java
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/dbu0600011/SogoShokaiKihon.java
@@ -7,11 +7,9 @@ package jp.co.ndensan.reams.db.dbu.divcontroller.dbu0600011;
 
 import java.util.HashMap;
 import java.util.List;
-import jp.co.ndensan.reams.db.dbu.divcontroller.entity.dbu0600011.SogoShokaiSearchDiv;
 import jp.co.ndensan.reams.db.dbu.divcontroller.entity.dbu0600011.SogoShokaiKihonDiv;
 import jp.co.ndensan.reams.db.dbz.divcontroller.helper.ControlGenerator;
 import jp.co.ndensan.reams.db.dbz.divcontroller.helper.YamlLoader;
-import jp.co.ndensan.reams.uz.uza.biz.YubinNo;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 
@@ -26,66 +24,59 @@ public class SogoShokaiKihon {
      * 検索結果画面-「選択」押下時の処理を表します。
      *
      * @param panel SogoShokaiKihonDiv
-     * @param panel2 SogoShokaiSearchDiv
      * @return ResponseData
      */
     public ResponseData<SogoShokaiKihonDiv> dispTaishoshaJoho(
-            SogoShokaiKihonDiv panel, SogoShokaiSearchDiv panel2) {
+            SogoShokaiKihonDiv panel) {
         ResponseData<SogoShokaiKihonDiv> response = new ResponseData<>();
 
         List<HashMap> hihokenshashoHakkoTaishoshaJohoList = YamlLoader.DBU.loadAsList(
                 new RString("dbu0600011/AtenaKihonData.yml"));
 
-        HashMap hashMap = hihokenshashoHakkoTaishoshaJohoList.get(
-                panel2.getSogoShokaiGaitoshaListInfo().getDgSearchResult().getClickedRowId());
-
+//        HashMap hashMap = hihokenshashoHakkoTaishoshaJohoList.get(
+//                panel2.getSogoShokaiGaitoshaListInfo().getDgSearchResult().getClickedRowId());
+        HashMap hashMap = hihokenshashoHakkoTaishoshaJohoList.get(0);
         ControlGenerator ymlData = new ControlGenerator(hashMap);
 
         //宛名基本情報の出力
         panel.getSogoShokaiKihonInfo().getTxtAtenaMeisho().
-                setValue(panel2.getSogoShokaiGaitoshaListInfo().getDgSearchResult().getClickedItem().getShimei());
+                setValue(ymlData.getAsRString("氏名"));
 
         panel.getSogoShokaiKihonInfo().getTxtAtenaKanaMeisho().
                 setValue(ymlData.getAsRString("カナ氏名"));
 
         panel.getSogoShokaiKihonInfo().getTxtJusho().
-                setValue(panel2.getSogoShokaiGaitoshaListInfo().getDgSearchResult().getClickedItem().getJusho());
+                setValue(ymlData.getAsRString("住所"));
 
         panel.getSogoShokaiKihonInfo().getTxtSeinengappiYMD().
-                setValue(panel2.getSogoShokaiGaitoshaListInfo().getDgSearchResult().
-                        getClickedItem().getBirthDay().getValue());
+                setValue(ymlData.getAsRDate("生年月日"));
 
         panel.getSogoShokaiKihonInfo().getTxtNenrei().
                 setValue(ymlData.getAsRString("年齢"));
 
         panel.getSogoShokaiKihonInfo().getTxtSeibetsu().
-                setValue(panel2.getSogoShokaiGaitoshaListInfo().getDgSearchResult().
-                        getClickedItem().getGender());
+                setValue(ymlData.getAsRString("性別"));
 
         panel.getSogoShokaiKihonInfo().getTxtGyoseiku().
                 setValue(ymlData.getAsRString("行政区"));
 
         panel.getSogoShokaiKihonInfo().getTxtYubinNo().
-                setValue(new YubinNo(panel2.getSogoShokaiGaitoshaListInfo().getDgSearchResult().
-                                getClickedItem().getYubinNo()));
+                setValue(ymlData.getAsYubinNo("郵便番号"));
 
         panel.getSogoShokaiKihonInfo().getTxtNihonjinGaikokujin().
-                setValue(panel2.getSogoShokaiGaitoshaListInfo().getDgSearchResult().
-                        getClickedItem().getJuminShubetsu());
+                setValue(ymlData.getAsRString("住民種別"));
 
         panel.getSogoShokaiKihonInfo().getTxtJuminJotai().
                 setValue(ymlData.getAsRString("住民状態"));
 
         panel.getSogoShokaiKihonInfo().getTxtShikibetsuCode().
-                setValue(panel2.getSogoShokaiGaitoshaListInfo().getDgSearchResult().
-                        getClickedItem().getShikibetsuCode());
+                setValue(ymlData.getAsRString("識別コード"));
 
         panel.getSogoShokaiKihonInfo().getTxtKojinHojinCode().
                 setValue(ymlData.getAsRString("個人番号"));
 
         panel.getSogoShokaiKihonInfo().getTxtSetaiCode().
-                setValue(panel2.getSogoShokaiGaitoshaListInfo().getDgSearchResult().
-                        getClickedItem().getSetaiCode());
+                setValue(ymlData.getAsRString("世帯コード"));
 
         response.data = panel;
         return response;
