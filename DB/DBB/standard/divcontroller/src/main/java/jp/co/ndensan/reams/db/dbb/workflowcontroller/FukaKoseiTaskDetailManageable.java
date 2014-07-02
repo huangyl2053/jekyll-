e52b0d382073d99d107fe3aef32ac72a526a8158
田辺 紘一 <n8211@ndensan.co.jp>
2014-07-02 21:37:30 +0900

    refs #37736 タスクの氏名表示対応

--- DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/workflowcontroller/FukaKoseiTaskDetailManageable.java
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/workflowcontroller/FukaKoseiTaskDetailManageable.java
@@ -6,6 +6,7 @@
 package jp.co.ndensan.reams.db.dbb.workflowcontroller;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.lang.RStringBuilder;
 import jp.co.ndensan.reams.uz.uza.workflow.flow.task.ITaskDetailManageable;
 
 /**
@@ -16,7 +17,16 @@ public class FukaKoseiTaskDetailManageable implements ITaskDetailManageable {
 
     @Override
     public RString getDetailString() {
-        return new RString("test");
+
+        RString 被保番号 = new RString("0000000000009401");
+        RString 氏名 = new RString("電算　次郎");
+
+        RStringBuilder rsb = new RStringBuilder();
+        rsb.append(被保番号);
+        rsb.append("<br>");
+        rsb.append(氏名);
+
+        return rsb.toRString();
     }
 
 }
