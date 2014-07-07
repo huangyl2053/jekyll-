8fb47910b08c18818b4953fc1b36e0683bf8b40c
城間篤人 <n8178@ndensan.co.jp>
2014-07-07 22:17:31 +0900

    refs #40981 福祉用具購入費支給申請審査・住宅改修費支給申請審査 デモで使用される識別コードを、固定値ではなくYamlから参照するように修正。

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/dbc0720011/JutakuKaishuShinsaHihokenshaPanel.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/dbc0720011/JutakuKaishuShinsaHihokenshaPanel.java
@@ -5,10 +5,15 @@
  */
 package jp.co.ndensan.reams.db.dbc.divcontroller.controller.dbc0720011;
 
+import java.util.HashMap;
+import java.util.List;
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.JutakuKaishuShinsaHihokenshaPanelDiv;
 import jp.co.ndensan.reams.db.dbz.divcontroller.controller.KaigoShikakuKihon;
+import jp.co.ndensan.reams.db.dbz.divcontroller.helper.ControlGenerator;
+import jp.co.ndensan.reams.db.dbz.divcontroller.helper.YamlLoader;
 import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
  * 住宅改修費支給申請審査の被保険者パネルです。
@@ -26,7 +31,10 @@ public class JutakuKaishuShinsaHihokenshaPanel {
     public ResponseData<JutakuKaishuShinsaHihokenshaPanelDiv> onLoad(JutakuKaishuShinsaHihokenshaPanelDiv panel) {
         ResponseData<JutakuKaishuShinsaHihokenshaPanelDiv> response = new ResponseData<>();
 
-        ShikibetsuCode 識別コード = new ShikibetsuCode("012345678901234");
+        List<HashMap> targetSourceList = YamlLoader.DBC.loadAsList(new RString("dbc0720011/demoKojin.yml"));
+        ControlGenerator targetSource = new ControlGenerator(targetSourceList.get(0));
+
+        ShikibetsuCode 識別コード = new ShikibetsuCode(targetSource.getAsRString("識別コード"));
         int rowId = 0;
         KaigoShikakuKihon.setData(panel.getKaigoAtena(), panel.getKaigoShikakuKihon(), 識別コード, rowId);
         response.data = panel;
