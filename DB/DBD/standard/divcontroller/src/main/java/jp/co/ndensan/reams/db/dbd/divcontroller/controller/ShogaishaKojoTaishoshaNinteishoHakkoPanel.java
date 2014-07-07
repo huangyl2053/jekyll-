541f6479877a08bc60115537b3c75ac10f585d80
久保田　英男 <n8187@ndensan.co.jp>
2014-07-07 20:32:14 +0900

    refs #40981 障害者控除対象者認定書発行 Yaml修正

--- DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/controller/ShogaishaKojoTaishoshaNinteishoHakkoPanel.java
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/controller/ShogaishaKojoTaishoshaNinteishoHakkoPanel.java
@@ -11,9 +11,9 @@ import java.util.List;
 import jp.co.ndensan.reams.db.dbd.divcontroller.entity.dbd4040011.ShogaishaKojoShinseishaDiv;
 import jp.co.ndensan.reams.db.dbd.divcontroller.entity.dbd4040011.ShogaishaKojoTaishoshaDetailDiv;
 import jp.co.ndensan.reams.db.dbd.divcontroller.entity.dbd4040011.ShogaishaKojoTaishoshaNinteishoHakkoPanelDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.controller.KaigoShikakuKihon;
 import jp.co.ndensan.reams.db.dbz.divcontroller.helper.ControlGenerator;
 import jp.co.ndensan.reams.db.dbz.divcontroller.helper.YamlLoader;
-import jp.co.ndensan.reams.ur.ura.divcontroller.controller.AtenaShokaiSimple;
 import jp.co.ndensan.reams.ur.ura.divcontroller.entity.AtenaShokaiSimpleDiv;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
@@ -61,28 +61,16 @@ public class ShogaishaKojoTaishoshaNinteishoHakkoPanel {
 
     private void set障害者控除対象者情報(ShogaishaKojoTaishoshaNinteishoHakkoPanelDiv panel) {
         set基本情報(panel);
-        set介護情報(panel);
         set障害者控除明細(panel);
         clear申請者情報(panel);
     }
 
     private void set基本情報(ShogaishaKojoTaishoshaNinteishoHakkoPanelDiv panel) {
-        AtenaShokaiSimple.setData(panel.getTaishoshaAtena().getAtenaInfo(), new ShikibetsuCode(new RString("012345678901234")));
-    }
-
-    private void set介護情報(ShogaishaKojoTaishoshaNinteishoHakkoPanelDiv panel) {
-        List<HashMap> targetSource = YamlLoader.DBD.loadAsList(new RString("dbd4040011/KaigoShikakuKihon.yml"));
+        List<HashMap> targetSource = YamlLoader.DBZ.loadAsList(new RString("KaigoShikakuKihon.yml"));
         ControlGenerator cg = new ControlGenerator(targetSource.get(0));
-
-        panel.getTaishoshaKaigoShikaku().getTxtHihokenshaNo().setValue(cg.getAsRString("被保番号"));
-        panel.getTaishoshaKaigoShikaku().getTxtShutokuYmd().setValue(cg.getAsRDate("取得日"));
-        panel.getTaishoshaKaigoShikaku().getTxtSoshitsuYmd().setValue(cg.getAsRDate("喪失日"));
-        panel.getTaishoshaKaigoShikaku().getTxtShutokuJiyu().setValue(cg.getAsRString("取得事由"));
-        panel.getTaishoshaKaigoShikaku().getTxtSoshitsuJiyu().setValue(cg.getAsRString("喪失事由"));
-        panel.getTaishoshaKaigoShikaku().getTxtJutokuKubun().setValue(cg.getAsRString("住特区分"));
-        panel.getTaishoshaKaigoShikaku().getTxtYokaigoJotaiKubun().setValue(cg.getAsRString("要介護度"));
-        panel.getTaishoshaKaigoShikaku().getTxtNinteiKaishiYmd().setValue(cg.getAsRDate("認定開始日"));
-        panel.getTaishoshaKaigoShikaku().getTxtNinteiShuryoYmd().setValue(cg.getAsRDate("認定終了日"));
+        ShikibetsuCode 識別コード = new ShikibetsuCode(cg.getAsRString("識別コード"));
+        int rowId = 0;
+        KaigoShikakuKihon.setData(panel.getTaishoshaAtena(), panel.getTaishoshaKaigoShikaku(), 識別コード, rowId);
     }
 
     private void set障害者控除明細(ShogaishaKojoTaishoshaNinteishoHakkoPanelDiv panel) {
