238abdac7584f46fb4eae2b8ad0ba78f85f45985
城間篤人 <n8178@ndensan.co.jp>
2014-07-07 19:10:56 +0900

    refs #40981 該当者一覧レイアウト修正、コミット忘れ。

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/searchResultOfHihokensha/dgSearchResult_Row.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/searchResultOfHihokensha/dgSearchResult_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.entity.searchResultOfHihokensha
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Mon Jul 07 15:01:22 JST 2014 
+ * Mon Jul 07 19:02:08 JST 2014 
  */
 
 
