6cb3f5b61be7cc9a12b27c9ace50b11b663b301d
城間篤人 <n8178@ndensan.co.jp>
2014-07-08 00:14:09 +0900

    refs #40981 福祉用具購入費支給申請審査・住宅改修費支給申請審査 共有子DivパネルのwidthAuto指定を、falseに変更。

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/dgSeikyuDetail_Row.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/dgSeikyuDetail_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbc.divcontroller.entity;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Fri Jul 04 17:58:57 JST 2014 
+ * Mon Jul 07 23:16:05 JST 2014 
  */
 
 
