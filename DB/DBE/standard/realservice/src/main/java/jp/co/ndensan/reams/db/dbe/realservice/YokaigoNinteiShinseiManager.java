8035d2e7609b63aa5bc7a2ab982e644eac6e81dc
三浦 凌 <n3327@ndensan.co.jp>
2014-07-22 13:46:42 +0900

    refs #4106 YokaigoNinteiShinseiManger修正。同クラスのTestを作成。

--- DB/DBE/standard/realservice/src/main/java/jp/co/ndensan/reams/db/dbe/realservice/YokaigoNinteiShinseiManager.java
+++ DB/DBE/standard/realservice/src/main/java/jp/co/ndensan/reams/db/dbe/realservice/YokaigoNinteiShinseiManager.java
@@ -8,9 +8,11 @@ package jp.co.ndensan.reams.db.dbe.realservice;
 import jp.co.ndensan.reams.db.dbe.business.YokaigoNinteiShinsei;
 import jp.co.ndensan.reams.db.dbe.entity.mapper.YokaigoNinteiShinseiMapper;
 import jp.co.ndensan.reams.db.dbe.persistence.basic.YokaigoNinteiShinseiDac;
+import jp.co.ndensan.reams.db.dbz.definition.valueobject.ShinseishoKanriNo;
 import jp.co.ndensan.reams.uz.uza.util.di.InstanceProvider;
 
 /**
+ * 要介護認定の登録・修正・削除を行うためのクラスです。
  *
  * @author N3327 三浦 凌
  */
@@ -18,12 +20,34 @@ public class YokaigoNinteiShinseiManager {
 
     private final YokaigoNinteiShinseiDac dac;
 
+    /**
+     * 新しいYokaigoNinteiShinseiManagerのインスタンスを生成します。
+     */
     public YokaigoNinteiShinseiManager() {
         dac = InstanceProvider.create(YokaigoNinteiShinseiDac.class);
     }
 
+    /**
+     * Constructor for Test
+     *
+     * @param dac YokaigoNinteiShinseiDac
+     */
+    YokaigoNinteiShinseiManager(YokaigoNinteiShinseiDac dac) {
+        this.dac = dac;
+    }
+
+    /**
+     * YokaigoNinteiShinseiの内容でDBへ保存します。
+     *
+     * @param 要介護認定申請 要介護認定申請
+     * @return 成功したときはtrue, 失敗したときはfalse
+     */
     public boolean save(YokaigoNinteiShinsei 要介護認定申請) {
-        dac.insertOrUpdate(YokaigoNinteiShinseiMapper.toDbT5001NinteiShinseiJohoEntity(要介護認定申請));
-        return true;
+        int result = dac.insertOrUpdate(YokaigoNinteiShinseiMapper.toDbT5001NinteiShinseiJohoEntity(要介護認定申請));
+        return isSuccess(result);
+    }
+
+    private boolean isSuccess(int result) {
+        return result != 0;
     }
 }
