814268b4c4609964833569d2c70a9b9f709e91d7
城間篤人 <n8178@ndensan.co.jp>
2014-08-07 14:04:37 +0900

    refs #41064 javaDocを修正。 みなし2号→みなし2号被保険者区分に修正

--- DB/DBE/standard/realservice/src/main/java/jp/co/ndensan/reams/db/dbe/realservice/search/Minashi2GoshaDaichoSearchItem.java
+++ DB/DBE/standard/realservice/src/main/java/jp/co/ndensan/reams/db/dbe/realservice/search/Minashi2GoshaDaichoSearchItem.java
@@ -5,12 +5,12 @@
  */
 package jp.co.ndensan.reams.db.dbe.realservice.search;
 
-import jp.co.ndensan.reams.db.dbe.entity.basic.DbT1012Minashi2GoshaDaicho;
 import jp.co.ndensan.reams.ur.urz.realservice.search.ISearchItem;
 import jp.co.ndensan.reams.uz.uza.util.db.IColumnDefinition;
 import static jp.co.ndensan.reams.db.dbe.entity.basic.DbT1012Minashi2GoshaDaicho.*;
 
 /**
+ * みなし2号被保険者区分者台帳を検索する際に使用するSearchItemです。
  *
  * @author N8211 田辺 紘一
  */
@@ -20,9 +20,9 @@ public enum Minashi2GoshaDaichoSearchItem implements ISearchItem {
     市町村コード(shichosonCode),
     識別コード(shikibetsuCode),
     被保険者番号(hihokenshaNo),
-    みなし2号(hihokenshaKubunCode);
+    みなし2号被保険者区分(hihokenshaKubunCode);
 
-    private IColumnDefinition columnDefinition;
+    private final IColumnDefinition columnDefinition;
 
     private Minashi2GoshaDaichoSearchItem(IColumnDefinition columnDefinition) {
         this.columnDefinition = columnDefinition;
