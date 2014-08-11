1622b93e5ad7bbf12e82684136f92b712d06cf9c
城間篤人 <n8178@ndensan.co.jp>
2014-08-11 18:09:46 +0900

    refs #41695 介護資格のDivControllerをビルドが通るように修正。

--- DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/dba1010011/ShikakuShutokuTaishoshaJohoDiv.java
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/dba1010011/ShikakuShutokuTaishoshaJohoDiv.java
@@ -1,17 +1,16 @@
 package jp.co.ndensan.reams.db.dba.divcontroller.entity.dba1010011;
+
 /**
- * このコードはツールによって生成されました。
- * このファイルへの変更は、再生成時には損失するため
- * 不正な動作の原因になります。
+ * このコードはツールによって生成されました。 このファイルへの変更は、再生成時には損失するため 不正な動作の原因になります。
  */
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jp.co.ndensan.reams.db.dbz.divcontroller.entity.KaigoAtenaInfoDiv;
-import jp.co.ndensan.reams.db.dbz.divcontroller.entity.KaigoShikakuKihonDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.KaigoShikakuKihon.KaigoShikakuKihonDiv;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
 
 /**
- * ShikakuShutokuTaishoshaJoho のクラスファイル 
- * 
+ * ShikakuShutokuTaishoshaJoho のクラスファイル
+ *
  * @author 自動生成
  */
 public class ShikakuShutokuTaishoshaJohoDiv extends Panel {
@@ -21,6 +20,7 @@ public class ShikakuShutokuTaishoshaJohoDiv extends Panel {
      * コントロール名とフィールド名を取得する
      * private + コントロール名 + フィールド名 の文字列を作成
      */
+
     @JsonProperty("ShikakuShutokuCommonJoho")
     private KaigoAtenaInfoDiv ShikakuShutokuCommonJoho;
     @JsonProperty("ShikakuShutokuDbJoho")
@@ -39,7 +39,7 @@ public class ShikakuShutokuTaishoshaJohoDiv extends Panel {
 
     @JsonProperty("ShikakuShutokuCommonJoho")
     public void setShikakuShutokuCommonJoho(KaigoAtenaInfoDiv ShikakuShutokuCommonJoho) {
-        this.ShikakuShutokuCommonJoho=ShikakuShutokuCommonJoho;
+        this.ShikakuShutokuCommonJoho = ShikakuShutokuCommonJoho;
     }
 
     @JsonProperty("ShikakuShutokuDbJoho")
@@ -49,7 +49,7 @@ public class ShikakuShutokuTaishoshaJohoDiv extends Panel {
 
     @JsonProperty("ShikakuShutokuDbJoho")
     public void setShikakuShutokuDbJoho(KaigoShikakuKihonDiv ShikakuShutokuDbJoho) {
-        this.ShikakuShutokuDbJoho=ShikakuShutokuDbJoho;
+        this.ShikakuShutokuDbJoho = ShikakuShutokuDbJoho;
     }
 
 }
--- DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/dba2040011/TajutokuTaishosaJohoDiv.java
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/dba2040011/TajutokuTaishosaJohoDiv.java
@@ -1,17 +1,16 @@
 package jp.co.ndensan.reams.db.dba.divcontroller.entity.dba2040011;
+
 /**
- * このコードはツールによって生成されました。
- * このファイルへの変更は、再生成時には損失するため
- * 不正な動作の原因になります。
+ * このコードはツールによって生成されました。 このファイルへの変更は、再生成時には損失するため 不正な動作の原因になります。
  */
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jp.co.ndensan.reams.db.dbz.divcontroller.entity.KaigoShikakuKihonDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.KaigoShikakuKihon.KaigoShikakuKihonDiv;
 import jp.co.ndensan.reams.ur.ura.divcontroller.entity.AtenaShokaiSimpleDiv;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
 
 /**
- * TajutokuTaishosaJoho のクラスファイル 
- * 
+ * TajutokuTaishosaJoho のクラスファイル
+ *
  * @author 自動生成
  */
 public class TajutokuTaishosaJohoDiv extends Panel {
@@ -21,6 +20,7 @@ public class TajutokuTaishosaJohoDiv extends Panel {
      * コントロール名とフィールド名を取得する
      * private + コントロール名 + フィールド名 の文字列を作成
      */
+
     @JsonProperty("AtenaJoho")
     private AtenaShokaiSimpleDiv AtenaJoho;
     @JsonProperty("KaigoShikakuJoho")
@@ -39,7 +39,7 @@ public class TajutokuTaishosaJohoDiv extends Panel {
 
     @JsonProperty("AtenaJoho")
     public void setAtenaJoho(AtenaShokaiSimpleDiv AtenaJoho) {
-        this.AtenaJoho=AtenaJoho;
+        this.AtenaJoho = AtenaJoho;
     }
 
     @JsonProperty("KaigoShikakuJoho")
@@ -49,7 +49,7 @@ public class TajutokuTaishosaJohoDiv extends Panel {
 
     @JsonProperty("KaigoShikakuJoho")
     public void setKaigoShikakuJoho(KaigoShikakuKihonDiv KaigoShikakuJoho) {
-        this.KaigoShikakuJoho=KaigoShikakuJoho;
+        this.KaigoShikakuJoho = KaigoShikakuJoho;
     }
 
 }
