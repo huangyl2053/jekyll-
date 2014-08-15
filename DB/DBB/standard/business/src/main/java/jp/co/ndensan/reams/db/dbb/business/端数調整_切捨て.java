a242f6d28c2d3b3a43011791fb7e19172e4ef882
大谷 和弘 <n3336@ndensan.co.jp>
2014-08-15 10:12:01 +0900

    #年額計算 クラス修正

---
+++ DB/DBB/standard/business/src/main/java/jp/co/ndensan/reams/db/dbb/business/端数調整_切捨て.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jp.co.ndensan.reams.db.dbb.business;
+
+import jp.co.ndensan.reams.uz.uza.math.Decimal;
+
+/**
+ *
+ * @author n3336
+ */
+public class 端数調整_切捨て implements I端数調整{
+     private String tanni;
+
+    public 端数調整_切捨て(String tanni) {
+        this.tanni = tanni;
+    }
+     
+      public Decimal Calc端数(Decimal 年額保険料) {
+        return 年額保険料.subtract(年額保険料.remainder(new Decimal(this.tanni)));    
+    }   
+    
+}
