14042c2741931e5c90f580ecd85611d6fda87431
宮本 康 <n8156@ndensan.co.jp>
2014-10-03 09:09:04 +0900

    refs #45514 他市町村住所地特例異動 見直し

---
+++ DB/DBZ/standard/CommonChildDiv/JushochiTokureiRireki.js
@@ -0,0 +1,47 @@
+var DBZ;
+(function (DBZ) {
+    (function (JushochiTokureiRireki) {
+        var ModeController = (function () {
+            function ModeController(fieldName) {
+                this.controls = new JushochiTokureiRireki.Controls(fieldName);
+            }
+            ModeController.prototype.DisplayMode = function () {
+                return new Modes.DisplayMode(this.controls);
+            };
+            return ModeController;
+        })();
+        JushochiTokureiRireki.ModeController = ModeController;
+        (function (Modes) {
+            var DisplayMode = (function () {
+                function DisplayMode(controls) {
+                    this.controls = controls;
+                }
+                DisplayMode.prototype.Shokai = function () {
+                    this.controls.btnAdd().visible = false;
+                    this.controls.btnAdd().displayNone = true;
+                    this.controls.dgJushochiTokureiRireki().readOnly = true;
+
+                    this.controls.dgJushochiTokureiRireki().gridSetting.isShowSelectButtonColumn = false;
+                    this.controls.dgJushochiTokureiRireki().gridSetting.isShowModifyButtonColumn = false;
+                    this.controls.dgJushochiTokureiRireki().gridSetting.isShowDeleteButtonColumn = false;
+                    this.controls.dgJushochiTokureiRireki()._control.afterPropertiesSet();
+                };
+
+                DisplayMode.prototype.IdoToroku = function () {
+                    this.controls.btnAdd().visible = true;
+                    this.controls.btnAdd().displayNone = false;
+                    this.controls.dgJushochiTokureiRireki().readOnly = false;
+
+                    this.controls.dgJushochiTokureiRireki().gridSetting.isShowSelectButtonColumn = false;
+                    this.controls.dgJushochiTokureiRireki().gridSetting.isShowModifyButtonColumn = true;
+                    this.controls.dgJushochiTokureiRireki().gridSetting.isShowDeleteButtonColumn = false;
+                    this.controls.dgJushochiTokureiRireki()._control.afterPropertiesSet();
+                };
+                return DisplayMode;
+            })();
+            Modes.DisplayMode = DisplayMode;
+        })(JushochiTokureiRireki.Modes || (JushochiTokureiRireki.Modes = {}));
+        var Modes = JushochiTokureiRireki.Modes;
+    })(DBZ.JushochiTokureiRireki || (DBZ.JushochiTokureiRireki = {}));
+    var JushochiTokureiRireki = DBZ.JushochiTokureiRireki;
+})(DBZ || (DBZ = {}));
---
+++ DB/DBZ/standard/CommonChildDiv/JushochiTokureiRireki.ts
@@ -0,0 +1,52 @@
+/// <reference path='JushochiTokureiRireki_Design.ts' />
+module DBZ
+{
+    export module JushochiTokureiRireki
+    {
+        export class ModeController {
+            private controls: Controls;
+            private fieldName: string;
+
+            constructor(fieldName: string) {
+                this.controls = new Controls(fieldName);
+            }
+
+            public DisplayMode() {
+                return new Modes.DisplayMode(this.controls);
+            }
+        }
+        export module Modes {
+
+            export class DisplayMode {
+                private controls: Controls;
+
+                constructor(controls: Controls) {
+                    this.controls = controls;
+                }
+
+                public Shokai(): void {
+                    this.controls.btnAdd().visible = false;
+                    this.controls.btnAdd().displayNone = true;
+                    this.controls.dgJushochiTokureiRireki().readOnly = true;
+
+                    this.controls.dgJushochiTokureiRireki().gridSetting.isShowSelectButtonColumn = false;
+                    this.controls.dgJushochiTokureiRireki().gridSetting.isShowModifyButtonColumn = false;
+                    this.controls.dgJushochiTokureiRireki().gridSetting.isShowDeleteButtonColumn = false;
+                    this.controls.dgJushochiTokureiRireki()._control.afterPropertiesSet();
+                }
+
+                public IdoToroku(): void {
+                    this.controls.btnAdd().visible = true;
+                    this.controls.btnAdd().displayNone = false;
+                    this.controls.dgJushochiTokureiRireki().readOnly = false;
+
+                    this.controls.dgJushochiTokureiRireki().gridSetting.isShowSelectButtonColumn = false;
+                    this.controls.dgJushochiTokureiRireki().gridSetting.isShowModifyButtonColumn = true;
+                    this.controls.dgJushochiTokureiRireki().gridSetting.isShowDeleteButtonColumn = false;
+                    this.controls.dgJushochiTokureiRireki()._control.afterPropertiesSet();
+                }
+            }
+        }
+    }
+}
+
