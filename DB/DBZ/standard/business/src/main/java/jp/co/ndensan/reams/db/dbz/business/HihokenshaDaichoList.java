8a901a27f8b4c34355c6eb46675c921fc5f24f45
城間篤人 <n8178@ndensan.co.jp>
2014-10-27 20:37:54 +0900

    refs #52997 レビュー指摘対応 TODOの記述を修正。

--- DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/HihokenshaDaichoList.java
+++ DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/HihokenshaDaichoList.java
@@ -43,17 +43,18 @@ public class HihokenshaDaichoList implements Iterable<HihokenshaDaichoModel> {
         List<HihokenshaDaichoModel> daichoList = new ArrayList<>();
         //TODO #52997
         //資格得喪を表現するためには、被保険者台帳から、
-        //a, 同じ取得日を持つ情報の内、最新のデータ
+        //  a, 同じ取得日を持つ情報の内、最新のデータ
         //を抽出する必要がある。
         //必要な被保険者台帳情報を、以下の手順で取得する。
         //
         //1, 被保険者台帳の取得日を格納する変数shutokuDateを用意（初期値 = null)。
         //2, 被保険者台帳Listの各要素daichoに対して以下の処理を繰り返し実行し、対象の情報を抽出する。
-        //2-1, if !daicho.get取得日().equals(shutokuDate)
-        //3, 2-1の判定結果がtrueの場合、以下の処理を実行する。
-        //3-1, daichoList.add(daicho)
-        //3-2, shutokuDate = daicho.get取得日()
-        //4, 抽出した結果を戻り値として返す。
+        //  2-1, 次の判定を行う。
+        //      2-1-1, !daicho.get取得日().equals(shutokuDate)
+        //  2-2, 2-1の判定結果がtrueの場合、以下の処理を実行する。
+        //      2-2-1, daichoList.add(daicho)
+        //      2-2-2, shutokuDate = daicho.get取得日()
+        //3, 抽出した結果を戻り値として返す。
         return ItemList.of(daichoList);
     }
 
@@ -67,29 +68,30 @@ public class HihokenshaDaichoList implements Iterable<HihokenshaDaichoModel> {
         //TODO #52997
         //住所地特例を表現するためには、適用情報と解除情報をそれぞれ取得する必要がある。
         //解除情報を持つ被保険者台帳は、
-        //a, 被保険者台帳の解除日にデータを持つもの
+        //  a, 被保険者台帳の解除日にデータを持つもの
         //を抽出することで取得できる。
         //適用情報を持つ被保険者台帳は、
-        //b, 被保険者台帳の適用日にデータを持つもの
-        //c, 前の適用日から、データが更新されているもの
+        //  b, 被保険者台帳の適用日にデータを持つもの
+        //  c, 前の適用日から、データが更新されているもの
         //の条件を満たす台帳情報を抽出することで取得できる。
         //
         //必要な被保険者台帳情報を、以下の手順で取得する。
         //
         //1, 被保険者台帳の適用日を格納する変数tekiyoDateを用意（初期値 = null)。
         //2, 被保険者台帳Listの各要素daichoに対して以下の処理を繰り返し実行し、対象の情報を抽出する。
-        //2-1, if daicho.get解除日() != null
-        //3, 2-1の判定結果がtrueの場合、以下の処理を実行する。
-        //3-1, daichoList.add(daicho)
-        //3-2, continueで、以降の処理を省略して次のループ処理に移行する。
-        //4, 解除情報が取得できなかった場合、以下の判定を行う。
-        //4-1, if daicho.get適用日() != null
-        //5, 4-1の判定結果がtrueの場合、さらに以下の判定を行う。
-        //5-1, if !daicho.get適用日().equals(tekiyoDate)
-        //6, 5-1の判定結果がtrueの場合、以下の処理を行う。
-        //6-1, daichoList.add(daicho)
-        //6-2, tekiyoDate = daicho.get変更日()
-        //7, 抽出した結果を戻り値として返す。
+        //  2-1, 次の判定を行う。
+        //      2-1-1, daicho.get解除日() != null
+        //  2-2, 2-1の判定結果がtrueの場合、以下の処理を実行する。
+        //      2-2-1, daichoList.add(daicho)
+        //      2-2-2, continueで、以降の処理を省略して次のループ処理に移行する。
+        //  2-3, 解除情報が取得できなかった場合、以下の判定を行う。
+        //      2-3-1, daicho.get適用日() != null
+        //  2-4, 2-3の判定結果がtrueの場合、さらに以下の判定を行う。
+        //      2-4-1, !daicho.get適用日().equals(tekiyoDate)
+        //  2-5, 5-1の判定結果がtrueの場合、以下の処理を行う。
+        //      2-5-1, daichoList.add(daicho)
+        //      2-5-2, tekiyoDate = daicho.get変更日()
+        //3, 抽出した結果を戻り値として返す。
         return ItemList.of(daichoList);
     }
 
@@ -102,20 +104,20 @@ public class HihokenshaDaichoList implements Iterable<HihokenshaDaichoModel> {
         List<HihokenshaDaichoModel> daichoList = new ArrayList<>();
         //TODO #52997
         //資格関連異動を表現するためには、被保険者台帳から、
-        //a, 被保険者台帳の変更日にデータを持つもの
-        //b, 前の変更日から、データが更新されているもの
+        //  a, 被保険者台帳の変更日にデータを持つもの
+        //  b, 前の変更日から、データが更新されているもの
         //の条件を満たす台帳情報を抽出する必要がある。
         //必要な被保険者台帳情報を、以下の手順で取得する。
         //
         //1, 被保険者台帳の変更日を格納する変数henkoDateを用意（初期値 = null)。
         //2, 被保険者台帳Listの各要素daichoに対して以下の処理を繰り返し実行し、対象の情報を抽出する。
-        //2-1, if daicho.get変更日() != null
-        //3, 2-1の判定結果がtrueの場合、さらに以下の判定を行う。
-        //3-1, if !daicho.get変更日().equals(henkoDate)
-        //4, 3-1の判定結果がtrueの場合、以下の処理を行う。
-        //4-1, daichoList.add(daicho)
-        //4-2, henkoDate = daicho.get変更日()
-        //5, 抽出した結果を戻り値として返す。
+        //  2-1, 次の判定を行う。
+        //      2-1-1, daicho.get変更日() != null
+        //      2-1-2, !daicho.get変更日().equals(henkoDate)
+        //  2-2, 2-1の判定結果が両方ともtrueの場合、以下の処理を行う。
+        //      2-2-1, daichoList.add(daicho)
+        //      2-2-2, henkoDate = daicho.get変更日()
+        //3, 抽出した結果を戻り値として返す。
         return ItemList.of(daichoList);
     }
 
