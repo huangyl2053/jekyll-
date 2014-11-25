7339f040d29a8845a6316407e160d46d7f63fa89
城間篤人 <n8178@ndensan.co.jp>
2014-11-25 19:43:14 +0900

    refs #55510 DBU共有子Divに対して、PublickProperty変換ツールを適用。

--- DB/DBU/standard/CommonChildDiv/HihokenshaShikakuHakko.js
+++ DB/DBU/standard/CommonChildDiv/HihokenshaShikakuHakko.js
@@ -1,17 +1,58 @@
-var __extends = this.__extends || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    __.prototype = b.prototype;
-    d.prototype = new __();
-};
 var DBU;
 (function (DBU) {
-    var HihokenshaShikakuHakko = (function (_super) {
-        __extends(HihokenshaShikakuHakko, _super);
-        function HihokenshaShikakuHakko() {
-            _super.apply(this, arguments);
-        }
-        return HihokenshaShikakuHakko;
-    })(DBU.HihokenshaShikakuHakko_Design);
-    DBU.HihokenshaShikakuHakko = HihokenshaShikakuHakko;
+    (function (HihokenshaShikakuHakko) {
+        var ModeController = (function () {
+            function ModeController(fieldName) {
+                this.fieldName = fieldName;
+                this.controls = new HihokenshaShikakuHakko.Controls(fieldName);
+            }
+            ModeController.prototype.priorities = function () {
+                return [];
+            };
+
+            ModeController.prototype.Properties = function () {
+                return new UZA.CommonChildDiv(this.fieldName);
+            };
+            return ModeController;
+        })();
+        HihokenshaShikakuHakko.ModeController = ModeController;
+    })(DBU.HihokenshaShikakuHakko || (DBU.HihokenshaShikakuHakko = {}));
+    var HihokenshaShikakuHakko = DBU.HihokenshaShikakuHakko;
+})(DBU || (DBU = {}));
+
+var DBU;
+(function (DBU) {
+    (function (HihokenshaShikakuHakko) {
+        var PublicProperties = (function () {
+            function PublicProperties(fieldName) {
+                this.controls = new HihokenshaShikakuHakko.Controls(fieldName);
+            }
+            PublicProperties.prototype.getEditTypes = function () {
+                var editTypes = new UZA.EditTypeForPublicProperty();
+
+                editTypes.addEditType("txtYukoKigen_displayNone", UZA.EditTypeEnumForPublicProperty.BooleanType);
+                editTypes.addEditType("radInjiIchi_displayNone", UZA.EditTypeEnumForPublicProperty.BooleanType);
+                return editTypes;
+            };
+
+            PublicProperties.prototype.gettxtYukoKigen_displayNone = function () {
+                return this.controls.txtYukoKigen().displayNone;
+            };
+
+            PublicProperties.prototype.settxtYukoKigen_displayNone = function (value) {
+                this.controls.txtYukoKigen().displayNone = value;
+            };
+
+            PublicProperties.prototype.getradInjiIchi_displayNone = function () {
+                return this.controls.radInjiIchi().displayNone;
+            };
+
+            PublicProperties.prototype.setradInjiIchi_displayNone = function (value) {
+                this.controls.radInjiIchi().displayNone = value;
+            };
+            return PublicProperties;
+        })();
+        HihokenshaShikakuHakko.PublicProperties = PublicProperties;
+    })(DBU.HihokenshaShikakuHakko || (DBU.HihokenshaShikakuHakko = {}));
+    var HihokenshaShikakuHakko = DBU.HihokenshaShikakuHakko;
 })(DBU || (DBU = {}));
--- DB/DBU/standard/CommonChildDiv/HihokenshaShikakuHakko.ts
+++ DB/DBU/standard/CommonChildDiv/HihokenshaShikakuHakko.ts
@@ -1,8 +1,73 @@
-/// <reference path='HihokenshaShikakuHakko_Design.ts' />
+/// <reference path="HihokenshaShikakuHakko_Design.ts" />
+
+// 共有子Divモード記述TypeScriptソースです。
+
 module DBU
 {
-    export class HihokenshaShikakuHakko extends HihokenshaShikakuHakko_Design
-    {
+    export module HihokenshaShikakuHakko {
+
+        export class ModeController{
+            private controls: Controls;
+            private fieldName: string;
+
+            constructor(fieldName: string) {
+                this.fieldName = fieldName;
+                this.controls = new Controls(fieldName);
+            }
+ 
+            public priorities(): Array<string> {
+                return [];
+            }
+
+            public Properties() {
+                return new UZA.CommonChildDiv(this.fieldName);
+            }
+        }
+
     }
 }
+
+
+
+module DBU {
+
+    export module HihokenshaShikakuHakko {
+
+        export class PublicProperties {
+            private controls: Controls;
+
+            constructor(fieldName: string) {
+                this.controls = new Controls(fieldName);
+            }
+
+            public getEditTypes(): UZA.EditTypeForPublicProperty {
+                var editTypes = new UZA.EditTypeForPublicProperty();
+
+                editTypes.addEditType("txtYukoKigen_displayNone", UZA.EditTypeEnumForPublicProperty.BooleanType);
+                editTypes.addEditType("radInjiIchi_displayNone", UZA.EditTypeEnumForPublicProperty.BooleanType);
+                return editTypes;
+            }
+
+            public gettxtYukoKigen_displayNone() {
+                return this.controls.txtYukoKigen().displayNone;
+            }
+
+            public settxtYukoKigen_displayNone(value) {
+                this.controls.txtYukoKigen().displayNone = value;
+            }
+
+            public getradInjiIchi_displayNone() {
+                return this.controls.radInjiIchi().displayNone;
+            }
+
+            public setradInjiIchi_displayNone(value) {
+                this.controls.radInjiIchi().displayNone = value;
+            }
+
+        }
+    }
+}
+
+
+
 
