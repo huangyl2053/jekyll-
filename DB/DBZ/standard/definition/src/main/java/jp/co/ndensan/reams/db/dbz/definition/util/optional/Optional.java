4a74c5e04c36a6a2729c000af0339074dc20abb9
三浦 凌 <n3327@ndensan.co.jp>
2015-01-14 22:00:58 +0900

    IOptionalを削除。

--- DB/DBZ/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbz/definition/util/optional/Optional.java
+++ DB/DBZ/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbz/definition/util/optional/Optional.java
@@ -5,6 +5,7 @@
  */
 package jp.co.ndensan.reams.db.dbz.definition.util.optional;
 
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import jp.co.ndensan.reams.db.dbz.definition.util.function.IPredicate;
 import jp.co.ndensan.reams.db.dbz.definition.util.function.IConsumer;
@@ -12,12 +13,12 @@ import jp.co.ndensan.reams.db.dbz.definition.util.function.IFunction;
 import jp.co.ndensan.reams.db.dbz.definition.util.function.ISupplier;
 
 /**
- * {@link Optional Optional}を生成します。
+ * nullかもしれない値を保持するオブジェクトです。保持する値があるか、または、emptyか、2つの状態のどちらかにあります。
  *
  * @param <T> Optional が保持する値
  * @author N3327 三浦 凌
  */
-public final class Optional<T> implements IOptional<T> {
+public final class Optional<T> {
 
     private static final Optional<?> THE_EMPTY;
 
@@ -48,7 +49,7 @@ public final class Optional<T> implements IOptional<T> {
     }
 
     /**
-     * 引数の値を保持した{@link Optional Optional}を生成します。<br/>
+     * 指定の非{@code null}値を保持した{@link Optional Optional}を生成します。<br/>
      * 引数がnullかもしれない場合は、{@link #ofNullable(java.lang.Object) ofNullable()}を使用してください。
      *
      * @param <T> 保持するオブジェクトの型
@@ -72,7 +73,12 @@ public final class Optional<T> implements IOptional<T> {
         return null == value ? Optional.<T>empty() : of(value);
     }
 
-    @Override
+    /**
+     * 保持する値を返します。emptyの場合は{@link NoSuchElementException}をスローします。 <br/>
+     * 使用前に{@link #isPresent() isPresent()}でemptyでないことを確かめてください。
+     *
+     * @return 保持するオブジェクト
+     */
     public T get() {
         if (this.value == null) {
             throw new java.util.NoSuchElementException("value is not present.");
@@ -80,17 +86,33 @@ public final class Optional<T> implements IOptional<T> {
         return value;
     }
 
-    @Override
+    /**
+     * 保持する値があれば{@literal true}を返します。
+     *
+     * @return 保持する値があれば{@literal true}、emptyなら{@literal false}
+     */
     public boolean isPresent() {
         return this.value != null;
     }
 
-    @Override
+    /**
+     * 保持する値があればその値、emptyなら引数の値を返します。
+     *
+     * @param other 保持する値がない時に返す値
+     * @return 保持する値があればその値、emptyなら引数の値
+     */
     public T orElse(T other) {
         return this.value != null ? this.value : other;
     }
 
-    @Override
+    /**
+     * 保持する値があればその値、emptyなら引数の{@link ISupplier ISupplier}が生成するexceptionをスローします。
+     *
+     * @param <X> スローされるexception
+     * @param exceptionSupplier exceptionを生成するsupplier
+     * @return 保持する値があればその値
+     * @throws X emptyの時
+     */
     public <X extends Throwable> T orElseThrow(ISupplier<X> exceptionSupplier) throws X {
         if (value != null) {
             return this.value;
@@ -99,6 +121,11 @@ public final class Optional<T> implements IOptional<T> {
         }
     }
 
+    /**
+     * デバッグ用の文字列を返します。
+     *
+     * @return デバッグ用の文字列
+     */
     @Override
     public String toString() {
         return this.value != null
@@ -123,7 +150,15 @@ public final class Optional<T> implements IOptional<T> {
         return Objects.equals(this.value, other.value);
     }
 
-    @Override
+    /**
+     * 保持する値を指定の{@link IFunction mapper}により変換し、その結果を持った新しい{@link Optional Optional}として返します。
+     * emptyの場合は、戻り値の{@link Optional Optional}もemptyになります。
+     *
+     * @param <R> 変換後の{@link Optional Optional}が保持する型
+     * @param mapper 変換に用いる{@link IFunction mapper}
+     * @return 保持する値を変換した結果を持った{@link Optional Optional},
+     * emptyの場合はemptyな{@link Optional Optional}
+     */
     public <R> Optional<R> map(IFunction<? super T, ? extends R> mapper) {
         Objects.requireNonNull(mapper);
         if (!isPresent()) {
@@ -133,7 +168,15 @@ public final class Optional<T> implements IOptional<T> {
         }
     }
 
-    @Override
+    /**
+     * 保持する値を指定の{@link IFunction mapper}により、別の{@link Optional Optional}として返します。
+     * emptyの場合は、戻り値の{@link Optional Optional}もemptyになります。
+     *
+     * @param <R> 変換後の{@link Optional Optional}が保持する型
+     * @param mapper 変換に用いる{@link IFunction mapper}
+     * @return 保持する値を変換した結果を持った{@link Optional Optional},
+     * emptyの場合はemptyな{@link Optional Optional}
+     */
     public <R> Optional<R> flatMap(IFunction<? super T, Optional<R>> mapper) {
         Objects.requireNonNull(mapper);
         if (!isPresent()) {
@@ -143,7 +186,13 @@ public final class Optional<T> implements IOptional<T> {
         }
     }
 
-    @Override
+    /**
+     * 保持する値が指定の{@link IPredicate IConditon}に当てはまるときは自身を、
+     * 当てはまらない時はemptyな{@link Optional Optional}を返します。
+     *
+     * @param predicate {@link IPredicate IPredicate}に定義される条件
+     * @return 指定の条件にあてはまる時は自身をそのまま、あてはまらない時はemptyな{@link Optional Optional}
+     */
     public Optional<T> filter(IPredicate<? super T> predicate) {
         Objects.requireNonNull(predicate);
         if (!isPresent()) {
@@ -155,7 +204,12 @@ public final class Optional<T> implements IOptional<T> {
         return empty();
     }
 
-    @Override
+    /**
+     * 保持する値があれば、指定の{@code consumer}に定義された処理を実行します。
+     *
+     * @param consumer 保持する値があるときに実行する、指定の処理
+     * @throws NullPointerException 保持する値が存在していて、かつ、引数の{@code consumer}がnullの時
+     */
     public void ifPresent(IConsumer<? super T> consumer) {
         if (this.value != null) {
             consumer.accept(value);
