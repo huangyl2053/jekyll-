9bdab89fe37b98754cc50e3fcd15fe721a316df3
城間篤人 <n8178@ndensan.co.jp>
2015-01-19 11:38:55 +0900

    refs #55856 DBAのエラーメッセージに住所地特例に関する項目を追加。 各サブ業務に用意されていた、まったく同じ処理を行うクラスをDBZに統合（TODOあり）

--- DB/DBD/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbd/definition/enumeratedtype/message/DbdErrorMessages.java
+++ DB/DBD/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbd/definition/enumeratedtype/message/DbdErrorMessages.java
@@ -8,7 +8,7 @@ package jp.co.ndensan.reams.db.dbd.definition.enumeratedtype.message;
 import jp.co.ndensan.reams.uz.uza.message.ErrorMessage;
 import jp.co.ndensan.reams.uz.uza.message.IMessageGettable;
 import jp.co.ndensan.reams.uz.uza.message.Message;
-import static jp.co.ndensan.reams.db.dbd.definition.enumeratedtype.message.MessageCreateHelper.toCode;
+import static jp.co.ndensan.reams.db.dbz.definition.enumeratedtype.message.MessageCreateHelper.toCode;
 
 /**
  * DBDのエラーメッセージ定義列挙型です。
