4a8c8f5021a02aaad6e16441c15043543da4200b
久保田　英男 <n8187@ndensan.co.jp>
2015-01-19 20:16:05 +0900

    refs #55509 レビュー指摘対応(HihokenshaShikakuHakkoModelのListのget/setにArrayListを使用するよう修正：共有・被保険者証・資格者証発行)

--- DB/DBZ/standard/model/src/main/java/jp/co/ndensan/reams/db/dbz/model/hihokenshashikakuhakko/HihokenshaShikakuHakkoModel.java
+++ DB/DBZ/standard/model/src/main/java/jp/co/ndensan/reams/db/dbz/model/hihokenshashikakuhakko/HihokenshaShikakuHakkoModel.java
@@ -6,6 +6,7 @@
 package jp.co.ndensan.reams.db.dbz.model.hihokenshashikakuhakko;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import jp.co.ndensan.reams.db.dbz.definition.util.optional.Optional;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
@@ -355,7 +356,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @return 限度基準額サービス種類
      */
     public List<RString> get限度基準額サービス種類() {
-        return 限度基準額サービス種類;
+        return new ArrayList<>(限度基準額サービス種類);
     }
 
     /**
@@ -364,7 +365,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @param 限度基準額サービス種類 限度基準額サービス種類
      */
     public void set限度基準額サービス種類(List<RString> 限度基準額サービス種類) {
-        this.限度基準額サービス種類 = 限度基準額サービス種類;
+        this.限度基準額サービス種類 = new ArrayList<>(限度基準額サービス種類);
     }
 
     /**
@@ -373,7 +374,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @return 限度基準額サービス額
      */
     public List<RString> get限度基準額サービス額() {
-        return 限度基準額サービス額;
+        return new ArrayList<>(限度基準額サービス額);
     }
 
     /**
@@ -382,7 +383,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @param 限度基準額サービス額 限度基準額サービス額
      */
     public void set限度基準額サービス額(List<RString> 限度基準額サービス額) {
-        this.限度基準額サービス額 = 限度基準額サービス額;
+        this.限度基準額サービス額 = new ArrayList<>(限度基準額サービス額);
     }
 
     /**
@@ -409,7 +410,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @return 給付制限内容
      */
     public List<RString> get給付制限内容() {
-        return 給付制限内容;
+        return new ArrayList<>(給付制限内容);
     }
 
     /**
@@ -418,7 +419,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @param 給付制限内容 給付制限内容
      */
     public void set給付制限内容(List<RString> 給付制限内容) {
-        this.給付制限内容 = 給付制限内容;
+        this.給付制限内容 = new ArrayList<>(給付制限内容);
     }
 
     /**
@@ -427,7 +428,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @return 給付制限開始日
      */
     public List<FlexibleDate> get給付制限開始日() {
-        return 給付制限開始日;
+        return new ArrayList<>(給付制限開始日);
     }
 
     /**
@@ -436,7 +437,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @param 給付制限開始日 給付制限開始日
      */
     public void set給付制限開始日(List<FlexibleDate> 給付制限開始日) {
-        this.給付制限開始日 = 給付制限開始日;
+        this.給付制限開始日 = new ArrayList<>(給付制限開始日);
     }
 
     /**
@@ -445,7 +446,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @return 給付制限終了日
      */
     public List<FlexibleDate> get給付制限終了日() {
-        return 給付制限終了日;
+        return new ArrayList<>(給付制限終了日);
     }
 
     /**
@@ -454,7 +455,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @param 給付制限終了日 給付制限終了日
      */
     public void set給付制限終了日(List<FlexibleDate> 給付制限終了日) {
-        this.給付制限終了日 = 給付制限終了日;
+        this.給付制限終了日 = new ArrayList<>(給付制限終了日);
     }
 
     /**
@@ -463,7 +464,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @return 支援事業者名称
      */
     public List<RString> get支援事業者名称() {
-        return 支援事業者名称;
+        return new ArrayList<>(支援事業者名称);
     }
 
     /**
@@ -472,7 +473,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @param 支援事業者名称 支援事業者名称
      */
     public void set支援事業者名称(List<RString> 支援事業者名称) {
-        this.支援事業者名称 = 支援事業者名称;
+        this.支援事業者名称 = new ArrayList<>(支援事業者名称);
     }
 
     /**
@@ -481,7 +482,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @return 支援事業者届出日
      */
     public List<FlexibleDate> get支援事業者届出日() {
-        return 支援事業者届出日;
+        return new ArrayList<>(支援事業者届出日);
     }
 
     /**
@@ -490,7 +491,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @param 支援事業者届出日 支援事業者届出日
      */
     public void set支援事業者届出日(List<FlexibleDate> 支援事業者届出日) {
-        this.支援事業者届出日 = 支援事業者届出日;
+        this.支援事業者届出日 = new ArrayList<>(支援事業者届出日);
     }
 
     /**
@@ -499,7 +500,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @return 支援事業者適用開始日
      */
     public List<FlexibleDate> get支援事業者適用開始日() {
-        return 支援事業者適用開始日;
+        return new ArrayList<>(支援事業者適用開始日);
     }
 
     /**
@@ -508,7 +509,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @param 支援事業者適用開始日 支援事業者適用開始日
      */
     public void set支援事業者適用開始日(List<FlexibleDate> 支援事業者適用開始日) {
-        this.支援事業者適用開始日 = 支援事業者適用開始日;
+        this.支援事業者適用開始日 = new ArrayList<>(支援事業者適用開始日);
     }
 
     /**
@@ -517,7 +518,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @return 支援事業者適用終了日
      */
     public List<FlexibleDate> get支援事業者適用終了日() {
-        return 支援事業者適用終了日;
+        return new ArrayList<>(支援事業者適用終了日);
     }
 
     /**
@@ -526,7 +527,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @param 支援事業者適用終了日 支援事業者適用終了日
      */
     public void set支援事業者適用終了日(List<FlexibleDate> 支援事業者適用終了日) {
-        this.支援事業者適用終了日 = 支援事業者適用終了日;
+        this.支援事業者適用終了日 = new ArrayList<>(支援事業者適用終了日);
     }
 
     /**
@@ -535,7 +536,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @return 施設名
      */
     public List<RString> get施設名() {
-        return 施設名;
+        return new ArrayList<>(施設名);
     }
 
     /**
@@ -544,7 +545,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @param 施設名 施設名
      */
     public void set施設名(List<RString> 施設名) {
-        this.施設名 = 施設名;
+        this.施設名 = new ArrayList<>(施設名);
     }
 
     /**
@@ -553,7 +554,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @return 施設入所日
      */
     public List<FlexibleDate> get施設入所日() {
-        return 施設入所日;
+        return new ArrayList<>(施設入所日);
     }
 
     /**
@@ -562,7 +563,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @param 施設入所日 施設入所日
      */
     public void set施設入所日(List<FlexibleDate> 施設入所日) {
-        this.施設入所日 = 施設入所日;
+        this.施設入所日 = new ArrayList<>(施設入所日);
     }
 
     /**
@@ -571,7 +572,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @return 施設退所日
      */
     public List<FlexibleDate> get施設退所日() {
-        return 施設退所日;
+        return new ArrayList<>(施設退所日);
     }
 
     /**
@@ -580,7 +581,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @param 施設退所日 施設退所日
      */
     public void set施設退所日(List<FlexibleDate> 施設退所日) {
-        this.施設退所日 = 施設退所日;
+        this.施設退所日 = new ArrayList<>(施設退所日);
     }
 
     /**
@@ -589,7 +590,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @return 入所施設コード
      */
     public List<RString> get入所施設コード() {
-        return 入所施設コード;
+        return new ArrayList<>(入所施設コード);
     }
 
     /**
@@ -598,7 +599,7 @@ public class HihokenshaShikakuHakkoModel implements Serializable {
      * @param 入所施設コード List<RString>
      */
     public void set入所施設コード(List<RString> 入所施設コード) {
-        this.入所施設コード = 入所施設コード;
+        this.入所施設コード = new ArrayList<>(入所施設コード);
     }
 
     /**
