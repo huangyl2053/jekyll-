f9fa341f195e7609ae0f3c5fc68418667c215126
船山洋介 <n8235@ndensan.co.jp>
2015-01-19 19:43:31 +0900

    refs #55853 コメントアウトしていたエラーを解消

--- DB/DBZ/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbz/persistence/basic/HihokenshaDaichoDac.java
+++ DB/DBZ/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbz/persistence/basic/HihokenshaDaichoDac.java
@@ -16,8 +16,10 @@ import jp.co.ndensan.reams.db.dbz.entity.basic.DbT1001HihokenshaDaichoEntity;
 import jp.co.ndensan.reams.ur.urz.definition.enumeratedtype.message.UrSystemErrorMessages;
 import jp.co.ndensan.reams.uz.uza.core.mybatis.SqlSession;
 import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
+import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
 import jp.co.ndensan.reams.uz.uza.biz.YMDHMS;
 import jp.co.ndensan.reams.uz.uza.util.db.DbAccessorNormalType;
+import jp.co.ndensan.reams.uz.uza.util.db.ITrueFalseCriteria;
 import jp.co.ndensan.reams.uz.uza.util.db.Order;
 import jp.co.ndensan.reams.uz.uza.util.di.InjectSession;
 import static jp.co.ndensan.reams.uz.uza.util.db.Restrictions.*;
@@ -116,6 +118,103 @@ public class HihokenshaDaichoDac implements IPersistable<DbT1001HihokenshaDaicho
         return accessor.select().
                 table(DbT1001HihokenshaDaicho.class).
                 toList(DbT1001HihokenshaDaichoEntity.class);
+    }
+
+    /**
+     * 市町村コードから、指定した市町村の被保険者台帳情報を検索します。
+     *
+     * @param 市町村コード {@link LasdecCode 市町村コード}
+     * @return
+     * {@link DbT1001HihokenshaDaichoEntity 被保険者台帳管理Entity}の{@link List リスト}
+     */
+    @Transaction
+    public List<DbT1001HihokenshaDaichoEntity> selectAll(LasdecCode 市町村コード) {
+        DbAccessorNormalType accessor = new DbAccessorNormalType(session);
+        List<DbT1001HihokenshaDaichoEntity> entities = accessor.
+                select().
+                table(DbT1001HihokenshaDaicho.class).
+                where(eq(shichosonCode, 市町村コード)).
+                order(by(shoriTimestamp, Order.DESC)).
+                toList(DbT1001HihokenshaDaichoEntity.class);
+        return entities;
+    }
+
+    /**
+     * 検索条件を指定して、その条件に合致する被保険者台帳情報を検索します。
+     *
+     * @param 検索条件 {@link ITrueFalseCriteria 検索条件}
+     * @return
+     * {@link DbT1001HihokenshaDaichoEntity 被保険者台帳管理Entity}の{@link List リスト}
+     */
+    @Transaction
+    public List<DbT1001HihokenshaDaichoEntity> selectAll(ITrueFalseCriteria 検索条件) {
+        DbAccessorNormalType accessor = new DbAccessorNormalType(session);
+        List<DbT1001HihokenshaDaichoEntity> entities = accessor.
+                select().
+                table(DbT1001HihokenshaDaicho.class).
+                where(検索条件).
+                order(by(shoriTimestamp, Order.DESC)).
+                toList(DbT1001HihokenshaDaichoEntity.class);
+        return entities;
+    }
+
+    /**
+     * 指定の{@link LasdecCode 市町村コード}と{@link ShikibetsuCode 識別コード}から特定される個人の、
+     * 直近の資格情報を検索します。
+     *
+     * @param 市町村コード {@link LasdecCode 市町村コード}
+     * @param 識別コード {@link ShikibetsuCode 識別コード}
+     * @return 直近の資格情報を保持した{@link DbT1001HihokenshaDaichoEntity 被保険者台帳Entity}
+     */
+    @Transaction
+    public DbT1001HihokenshaDaichoEntity selectLatestOfPerson(LasdecCode 市町村コード, ShikibetsuCode 識別コード) {
+        DbAccessorNormalType accessor = new DbAccessorNormalType(session);
+        List<DbT1001HihokenshaDaichoEntity> entities = accessor.
+                select().
+                table(DbT1001HihokenshaDaicho.class).
+                where(and(eq(shichosonCode, 市町村コード), eq(shikibetsuCode, 識別コード))).
+                order(by(shoriTimestamp, Order.DESC)).
+                toList(DbT1001HihokenshaDaichoEntity.class);
+        return entities.isEmpty() ? null : entities.get(0);
+    }
+
+    /**
+     * 指定の{@link LasdecCode 市町村コード}と{@link HihokenshaNo 被保険者番号}から特定される被保険者の、
+     * 直近の資格情報を検索します。
+     *
+     * @param 市町村コード {@link LasdecCode 市町村コード}
+     * @param 被保険者番号 {@link HihokenshaNo 被保険者番号}
+     * @return 直近の資格情報を保持した{@link DbT1001HihokenshaDaichoEntity 被保険者台帳Entity}
+     */
+    public DbT1001HihokenshaDaichoEntity selectLatestOfPerson(LasdecCode 市町村コード, HihokenshaNo 被保険者番号) {
+        DbAccessorNormalType accessor = new DbAccessorNormalType(session);
+        List<DbT1001HihokenshaDaichoEntity> entities = accessor.
+                select().
+                table(DbT1001HihokenshaDaicho.class).
+                where(and(eq(shichosonCode, 市町村コード), eq(hihokenshaNo, 被保険者番号))).
+                order(by(shoriTimestamp, Order.DESC)).
+                toList(DbT1001HihokenshaDaichoEntity.class);
+        return entities.isEmpty() ? null : entities.get(0);
+    }
+
+    /**
+     * 主キーから被保険者台帳を検索します。
+     *
+     * @param 市町村コード {@link LasdecCode 市町村コード}
+     * @param 識別コード {@link ShikibetsuCode 識別コード}
+     * @param 処理日時 {@link RDateTime 処理日時}
+     * @return {@link DbT1001HihokenshaDaichoEntity 被保険者台帳Entity}
+     */
+    @Transaction
+    public DbT1001HihokenshaDaichoEntity selectFromKey(LasdecCode 市町村コード, ShikibetsuCode 識別コード, YMDHMS 処理日時) {
+        DbAccessorNormalType accessor = new DbAccessorNormalType(session);
+        DbT1001HihokenshaDaichoEntity entity = accessor.
+                select().
+                table(DbT1001HihokenshaDaicho.class).
+                where(and(eq(shichosonCode, 市町村コード), eq(shikibetsuCode, 識別コード), eq(shoriTimestamp, 処理日時))).
+                order(by(shoriTimestamp, Order.DESC)).
+                toObject(DbT1001HihokenshaDaichoEntity.class);
+        return entity;
     }
 
     @Override
