5702328d636401d371f328b8d2dc0df1181d8434
塚田萌 <n3317@ndensan.co.jp>
2015-01-21 18:39:38 +0900

    refs #55858 特徴情報画面実装

--- DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/controller/dbb0320003/Tokucho.java
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/controller/dbb0320003/Tokucho.java
@@ -5,15 +5,30 @@
  */
 package jp.co.ndensan.reams.db.dbb.divcontroller.controller.dbb0320003;
 
+import java.util.ArrayList;
+import java.util.List;
 import jp.co.ndensan.reams.db.dbb.divcontroller.controller.fuka.FukaMapper;
 import jp.co.ndensan.reams.db.dbb.divcontroller.controller.fuka.FukaShokaiController;
+import jp.co.ndensan.reams.db.dbb.divcontroller.entity.DBB0320003.TokuChoIdoAndIraiDiv;
+import jp.co.ndensan.reams.db.dbb.divcontroller.entity.DBB0320003.TokuChoKekkaDiv;
 import jp.co.ndensan.reams.db.dbb.divcontroller.entity.DBB0320003.TokuchoDiv;
+import jp.co.ndensan.reams.db.dbb.divcontroller.entity.DBB0320003.dgTokuChoIdoAndIrai_Row;
+import jp.co.ndensan.reams.db.dbb.divcontroller.entity.DBB0320003.dgTokuchoKekka_Row;
 import jp.co.ndensan.reams.db.dbz.business.viewstate.FukaShokaiKey;
+import jp.co.ndensan.reams.db.dbz.definition.enumeratedtype.configkeys.ConfigKeysTokuchoHosoku;
+import jp.co.ndensan.reams.db.dbz.definition.enumeratedtype.configkeys.TokuchoHosokuConfig;
 import jp.co.ndensan.reams.db.dbz.definition.enumeratedtype.fuka.SanteiState;
+import jp.co.ndensan.reams.db.dbz.definition.valueobject.FukaNendo;
 import jp.co.ndensan.reams.db.dbz.model.fuka.ChoshuHohoModel;
+import jp.co.ndensan.reams.ur.urz.definition.nenkintokucho.KakushuKubun;
 import jp.co.ndensan.reams.ur.urz.definition.valueobject.code.NenkinCode;
+import jp.co.ndensan.reams.ur.urz.model.NenkinTokuchoKaifuJohoModel;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleYear;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.lang.RYearMonth;
+import jp.co.ndensan.reams.uz.uza.math.Decimal;
 
 /**
  * 賦課照会の特別徴収Divのコントローラです。
@@ -65,49 +80,64 @@ public class Tokucho {
     private void setDivFor仮算定(TokuchoDiv div, ChoshuHohoModel model) {
         div.getTxtKisoNenkinNo2().setValue(model.get仮徴収_基礎年金番号());
         div.getTxtNenkinCode2().setValue(model.get仮徴収_年金コード());
-        //TODO 取得した特徴回付の処理年度＋捕捉月
-//        div.getTxtHosokuYM().setDomain(FukaMapper.toRYearMonth(model.get仮徴収_捕捉月()));
-//TODO コンフィグから取得
-//        div.getTxtKaishiYM().setDomain();
+
+        div.getTxtKaishiYM().setDomain(new RYearMonth(build開始年月(model.get仮徴収_捕捉月(), model.get賦課年度().value().minusYear(1))));
 
         NenkinCode nenkinCode = createNenkinCode(model.get仮徴収_年金コード());
         div.getTxtTokubetsuChoshuTaishoNenkin().setValue(nenkinCode.getRyakusho());
 
-        //TODO 回付情報から＋コードマスタ
-//        div.getTxtTokuChoGimusha().setValue(model);
-        set年金特徴回付情報(div, model.get仮徴収_基礎年金番号(), model.get仮徴収_年金コード(), model.get仮徴収_捕捉月());
+        set年金保険者突合Div(div, model.get賦課年度(), model.get仮徴収_基礎年金番号(), model.get仮徴収_年金コード(), model.get仮徴収_捕捉月());
+        set特徴異動and依頼Div(div.getTokuChoIdoAndIrai(), model);
+        set特徴結果Div(div.getTokuChoKekka(), model);
     }
 
     private void setDivFor本算定(TokuchoDiv div, ChoshuHohoModel model) {
         div.getTxtKisoNenkinNo2().setValue(model.get本徴収_基礎年金番号());
         div.getTxtNenkinCode2().setValue(model.get本徴収_年金コード());
-        //TODO 取得した特徴回付の処理年度＋捕捉月
-//        div.getTxtHosokuYM().setDomain(FukaMapper.toRYearMonth(model.get本徴収_捕捉月()));
-//TODO コンフィグから取得
-//        div.getTxtKaishiYM().setDomain();
+
+        div.getTxtKaishiYM().setDomain(new RYearMonth(build開始年月(model.get本徴収_捕捉月(), model.get賦課年度().value())));
 
         NenkinCode nenkinCode = createNenkinCode(model.get本徴収_年金コード());
         div.getTxtTokubetsuChoshuTaishoNenkin().setValue(nenkinCode.getRyakusho());
 
-        //TODO 回付情報から＋コードマスタ
-//        div.getTxtTokuChoGimusha().setValue(model);
-        set年金特徴回付情報(div, model.get本徴収_基礎年金番号(), model.get本徴収_年金コード(), model.get本徴収_捕捉月());
+        set年金保険者突合Div(div, model.get賦課年度(), model.get本徴収_基礎年金番号(), model.get本徴収_年金コード(), model.get本徴収_捕捉月());
+        set特徴異動and依頼Div(div.getTokuChoIdoAndIrai(), model);
+        set特徴結果Div(div.getTokuChoKekka(), model);
     }
 
     private void setDivFor翌年度(TokuchoDiv div, ChoshuHohoModel model) {
         div.getTxtKisoNenkinNo2().setValue(model.get翌年度仮徴収_基礎年金番号());
         div.getTxtNenkinCode2().setValue(model.get翌年度仮徴収_年金コード());
-        //TODO 取得した特徴回付の処理年度＋捕捉月
-//        div.getTxtHosokuYM().setDomain(FukaMapper.toRYearMonth(model.get翌年度仮徴収_捕捉月()));
-//TODO コンフィグから取得
-//        div.getTxtKaishiYM().setDomain();
+
+        div.getTxtKaishiYM().setDomain(new RYearMonth(build開始年月(model.get翌年度仮徴収_捕捉月(), model.get賦課年度().value())));
 
         NenkinCode nenkinCode = createNenkinCode(model.get翌年度仮徴収_年金コード());
         div.getTxtTokubetsuChoshuTaishoNenkin().setValue(nenkinCode.getRyakusho());
 
-        //TODO 回付情報から＋コードマスタ
-//        div.getTxtTokuChoGimusha().setValue(model);
-        set年金特徴回付情報(div, model.get翌年度仮徴収_基礎年金番号(), model.get翌年度仮徴収_年金コード(), model.get翌年度仮徴収_捕捉月());
+        set年金保険者突合Div(div, model.get賦課年度(), model.get翌年度仮徴収_基礎年金番号(), model.get翌年度仮徴収_年金コード(), model.get翌年度仮徴収_捕捉月());
+        set特徴異動and依頼Div(div.getTokuChoIdoAndIrai(), model);
+        set特徴結果Div(div.getTokuChoKekka(), model);
+    }
+
+    private RString build開始年月(RString 捕捉月, FlexibleYear 年度) {
+        ConfigKeysTokuchoHosoku configKey = ConfigKeysTokuchoHosoku.toValue(捕捉月);
+
+        TokuchoHosokuConfig config = new TokuchoHosokuConfig();
+        RString 開始月 = config.get(configKey, toEffectiveDate(年度));
+
+        if (is開始月between01to08(開始月)) {
+            年度.plusYear(1);
+        }
+
+        return 年度.toDateString().concat(開始月);
+    }
+
+    private boolean is開始月between01to08(RString 開始月) {
+        return 開始月.compareTo("01") >= 0 && 開始月.compareTo("08") <= 0;
+    }
+
+    private RDate toEffectiveDate(FlexibleYear 年度) {
+        return new RDate(年度.getYearValue(), 04, 01);
     }
 
     /**
@@ -123,118 +153,77 @@ public class Tokucho {
         return new NenkinCode(年金コード上3桁);
     }
 
-    private void set年金特徴回付情報(TokuchoDiv div, RString 基礎年金番号, RString 年金コード, RString 捕捉月) {
-        //TODO 保留　各メソッド（仮、本、翌年度）からキーを渡すので、ここではキーを使ってモデル取得＋セットすればよい
+    private void set年金保険者突合Div(TokuchoDiv div,
+            FukaNendo 賦課年度, RString 基礎年金番号, RString 年金コード, RString 捕捉月) {
 
-//    set年金保険者突合Div(div.getNenkinHokenshaTotsugoJoho());
-//    set特徴異動and依頼Div(div.getTokuChoIdoAndIrai());
-//    set特徴結果Div(div.getTokuChoKekka());
+        //TODO メソッド1を呼ぶ　get年金特徴対象者情報("DB",) モデル取れたてい
+        NenkinTokuchoKaifuJohoModel model = null;
+
+        div.getTxtHosokuYM().setDomain(new RYearMonth(model.get処理年度().toDateString().concat(捕捉月)));
+        //TODO 特徴義務者名　回付情報から＋コードマスタ 修正がマージされればgetRyakushoが取れるはず
+        div.getTxtTokuChoGimusha().setValue(model.getDT特別徴収義務者コード());
+
+        div.getNenkinHokenshaTotsugoJoho().getTxtShimeiKana().setValue(model.getDTカナ氏名());
+        div.getNenkinHokenshaTotsugoJoho().getTxtShimeiKanji().setValue(model.getDT漢字氏名());
+        div.getNenkinHokenshaTotsugoJoho().getTxtJushoKana().setValue(model.getDTカナ住所());
+        div.getNenkinHokenshaTotsugoJoho().getTxtJushoKanji().setValue(model.getDT漢字住所());
+        div.getNenkinHokenshaTotsugoJoho().getTxtSex().setValue(model.getDT性別());
+        div.getNenkinHokenshaTotsugoJoho().getTxtBirthYMD().setValue(model.getDT生年月日());
     }
 
-//       private void set年金保険者突合Div(NenkinHokenshaTotsugoJohoDiv div) {
-//
-//        div.getTxtShimeiKana().setValue(cg.getAsRString("氏名カナ"));
-//        div.getTxtShimeiKanji().setValue(cg.getAsRString("氏名漢字"));
-//        div.getTxtJushoKana().setValue(cg.getAsRString("住所カナ"));
-//        div.getTxtJushoKanji().setValue(cg.getAsRString("住所漢字"));
-//        div.getTxtSex().setValue(cg.getAsRString("性別"));
-//        div.getTxtBirthYMD().setValue(cg.getAsRString("生年月日"));
-//    }
-//    /**
-//     * 異動・依頼DataGridに値をセット
-//     *
-//     * @param div
-//     * @param formatData
-//     */
-//    private void set特徴異動and依頼Div(TokuChoIdoAndIraiDiv div) {
-//        List dataGridAll = new ArrayList();
-//
-//        for (int i = 0; i < formatData.size(); i++) {
-//            List dataGrid = (List) formatData.get(i);
-//            dgTokuChoIdoAndIrai_Row row = createRowTokuchoIdoAndIrai(dataGrid);
-//
-//            dataGridAll.add(row);
-//        }
-//
-//        DataGrid<dgTokuChoIdoAndIrai_Row> grid = div.getTokuChoIdoAndIrai().getDgTokuChoIdoAndIrai();
-//        grid.setDataSource(dataGridAll);
-//    }
-//
-//    /**
-//     * 異動・依頼DataGridの行を生成
-//     *
-//     * @param div
-//     * @param dataGrid
-//     * @return
-//     */
-//    private dgTokuChoIdoAndIrai_Row createRowTokuchoIdoAndIrai(List dataGrid) {
-//        dgTokuChoIdoAndIrai_Row row
-//                = new dgTokuChoIdoAndIrai_Row(RString.EMPTY, RString.EMPTY, RString.EMPTY, RString.EMPTY, RString.EMPTY, RString.EMPTY);
-//
-//        row.setTxtSofuYM((RString) dataGrid.get(1));
-//        row.setTxtTsuchiNaiyo((RString) dataGrid.get(2));
-//        row.setTxtKakushuKbn((RString) dataGrid.get(3));
-//        row.setTxtKakushuKingaku1((RString) dataGrid.get(4));
-//        row.setTxtKakushuKingaku2((RString) dataGrid.get(5));
-//        row.setTxtKakushuKingaku3((RString) dataGrid.get(6));
-//
-//        return row;
-//    }
-//
-//    /**
-//     * 特徴結果DataGridをロード
-//     *
-//     * @param div
-//     * @param formatData
-//     */
-//    private void set特徴結果Div(TokuChoKekkaDiv div, List formatData) {
-//        List dataGridAll = new ArrayList();
-//
-//        for (int i = 0; i < formatData.size(); i++) {
-//            List dataGrid = (List) formatData.get(i);
-//            dgTokuchoKekka_Row row = createRowTokuchoResult(dataGrid);
-//
-//            dataGridAll.add(row);
-//        }
-//
-//        DataGrid<dgTokuchoKekka_Row> grid = div.getTokuChoKekka().getDgTokuchoKekka();
-//        grid.setDataSource(dataGridAll);
-//    }
-//
-//    /**
-//     * 特徴結果DataGridの行を生成
-//     *
-//     * @param dataGrid
-//     * @return
-//     */
-//    private dgTokuchoKekka_Row createRowTokuchoResult(List dataGrid) {
-//        dgTokuchoKekka_Row row
-//                = new dgTokuchoKekka_Row(RString.EMPTY, RString.EMPTY, RString.EMPTY, RString.EMPTY, RString.EMPTY, RString.EMPTY, RString.EMPTY);
-//
-//        if ((RString) dataGrid.get(0) != null) {
-//            row.setTxtUketoriYM((RString) dataGrid.get(0));
-//        }
-//        if ((RString) dataGrid.get(1) != null) {
-//            row.setTxtTsuchiNaiyo((RString) dataGrid.get(1));
-//        }
-//        if ((RString) dataGrid.get(2) != null) {
-//            row.setTxtKakushuKbn((RString) dataGrid.get(2));
-//        }
-//        if ((RString) dataGrid.get(3) != null) {
-//            row.setTxtKakushuKingaku1((RString) dataGrid.get(3));
-//        }
-//        if ((RString) dataGrid.get(4) != null) {
-//            row.setTxtKakushuKingaku2((RString) dataGrid.get(4));
-//        }
-//        if ((RString) dataGrid.get(5) != null) {
-//            row.setTxtKakushuKingaku3((RString) dataGrid.get(5));
-//        }
-//        if ((RString) dataGrid.get(6) != null) {
-//            row.setTxtShoriKekka((RString) dataGrid.get(6));
-//        }
-//
-//        return row;
-//    }
+    private void set特徴異動and依頼Div(TokuChoIdoAndIraiDiv div, ChoshuHohoModel model) {
+        //TODO メソッド2を呼ぶ　リスト取れたてい
+        List<NenkinTokuchoKaifuJohoModel> 年金特徴送付List = null;
+        List dataSource = new ArrayList();
+
+        for (NenkinTokuchoKaifuJohoModel tokuchoKaifuModel : 年金特徴送付List) {
+            dataSource.add(toDgTokuChoIdoAndIrai_Row(tokuchoKaifuModel));
+        }
+
+        div.getDgTokuChoIdoAndIrai().setDataSource(dataSource);
+    }
+
+    private dgTokuChoIdoAndIrai_Row toDgTokuChoIdoAndIrai_Row(NenkinTokuchoKaifuJohoModel model) {
+
+        return new dgTokuChoIdoAndIrai_Row(
+                model.get処理対象年月().wareki().toDateString(),
+                //TODO enumになってるはず
+                model.get通知内容コード(),
+                //TODO 通知内容コードを渡す
+                KakushuKubun.search各種区分名称(model.get通知内容コード(), model.getDT各種区分()).get各種区分名称(),
+                FukaMapper.addComma(new Decimal(model.getDT各種金額欄１().toString())),
+                FukaMapper.addComma(new Decimal(model.getDT各種金額欄２().toString())),
+                FukaMapper.addComma(new Decimal(model.getDT各種金額欄３().toString())));
+
+    }
+
+    private void set特徴結果Div(TokuChoKekkaDiv div, ChoshuHohoModel model) {
+        //TODO メソッド3を呼ぶ　リスト取れたてい
+        List<NenkinTokuchoKaifuJohoModel> 年金特徴受取List = null;
+        List dataSource = new ArrayList();
+
+        for (NenkinTokuchoKaifuJohoModel tokuchoKaifuModel : 年金特徴受取List) {
+            dataSource.add(toDgTokuchoKekka_Row(tokuchoKaifuModel));
+        }
+
+        div.getDgTokuchoKekka().setDataSource(dataSource);
+    }
+
+    private dgTokuchoKekka_Row toDgTokuchoKekka_Row(NenkinTokuchoKaifuJohoModel model) {
+
+        return new dgTokuchoKekka_Row(
+                model.get処理対象年月().wareki().toDateString(),
+                //TODO enumになってるはず
+                model.get通知内容コード(),
+                //TODO 通知内容コードを渡す
+                KakushuKubun.search各種区分名称(model.get通知内容コード(), model.getDT各種区分()).get各種区分名称(),
+                FukaMapper.addComma(new Decimal(model.getDT各種金額欄１().toString())),
+                FukaMapper.addComma(new Decimal(model.getDT各種金額欄２().toString())),
+                FukaMapper.addComma(new Decimal(model.getDT各種金額欄３().toString())),
+                //TODO enumのはず
+                model.getDT処理結果());
+    }
+
     private ResponseData<TokuchoDiv> createResponseData(TokuchoDiv div) {
         ResponseData<TokuchoDiv> response = new ResponseData<>();
         response.data = div;
---
+++ DB/DBZ/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbz/definition/enumeratedtype/configkeys/TokuchoHosokuConfig.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbz.definition.enumeratedtype.configkeys;
+
+import java.util.HashMap;
+import java.util.Map;
+import static java.util.Objects.requireNonNull;
+import jp.co.ndensan.reams.ur.urz.definition.enumeratedtype.message.UrSystemErrorMessages;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.util.config.BusinessConfig;
+
+/**
+ * 特別徴収捕捉コンフィグを取得するためのクラスです。
+ *
+ * @author n3317 塚田 萌
+ */
+public class TokuchoHosokuConfig {
+
+    Map<ConfigKeysTokuchoHosoku, RString> configs;
+
+    /**
+     * コンストラクタです。
+     *
+     */
+    public TokuchoHosokuConfig() {
+        this.configs = getValue(RDate.getNowDate());
+    }
+
+    /**
+     * 特別徴収捕捉に関する業務コンフィグを取得します。
+     *
+     * @param key ConfigKeysTokuchoHosoku 特別徴収捕捉コンフィグキー
+     * @param effectiveDate RDate 適用基準日
+     * @return Rstring 業務コンフィグの値
+     * @throws NullPointerException 引数がnullの場合
+     */
+    public RString get(ConfigKeysTokuchoHosoku key, RDate effectiveDate) throws NullPointerException {
+        requireNonNull(key, UrSystemErrorMessages.値がnull.getReplacedMessage("特別徴収捕捉"));
+        requireNonNull(effectiveDate, UrSystemErrorMessages.値がnull.getReplacedMessage("適用基準日"));
+
+        this.configs = getValue(effectiveDate);
+
+        return new RString(configs.get(key).toString());
+    }
+
+    private Map<ConfigKeysTokuchoHosoku, RString> getValue(RDate effectiveDate) {
+
+        Map<ConfigKeysTokuchoHosoku, RString> map = new HashMap();
+        for (ConfigKeysTokuchoHosoku target : ConfigKeysTokuchoHosoku.values()) {
+            map.put(target, BusinessConfig.get(target, effectiveDate));
+        }
+
+        return map;
+    }
+}
