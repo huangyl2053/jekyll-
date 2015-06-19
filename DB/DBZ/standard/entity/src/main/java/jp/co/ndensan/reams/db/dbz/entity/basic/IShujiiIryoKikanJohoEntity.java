1224b3237f6aef0c59d3bc2d1452a627d7059f50
船山洋介 <n8235@ndensan.co.jp>
2015-06-19 13:11:32 +0900

    refs #61466 パッケージ構成変更対応(DBD⇒DBZ)

---
+++ DB/DBZ/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbz/entity/basic/IShujiiIryoKikanJohoEntity.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbz.entity.basic;
+
+import jp.co.ndensan.reams.uz.uza.util.db.IDbAccessable;
+
+/**
+ * 主治医情報エンティティのインターフェースです。
+ *
+ * @author n8235 船山洋介
+ */
+public interface IShujiiIryoKikanJohoEntity extends IDbAccessable {
+
+}
