9e651545b9c01d15bc6b3aa639775d1b5d9a7fa0
高松 由紀子 <n2206@ndensan.co.jp>
2015-07-02 13:06:56 +0900

    UIデザイン DBD5330001 要介護認定結果通知（主治医） ※No67

---
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/DBD5330001/dgResultList_Row.java
@@ -0,0 +1,229 @@
+package jp.co.ndensan.reams.db.dbd.divcontroller.entity.DBD5330001;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+ * コードが再生成されるときに損失したりします。
+ * Thu Jul 02 13:02:44 JST 2015 
+ */
+
+
+
+import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+
+/**
+ * dgResultList_Row のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+
+public class dgResultList_Row extends DataRow {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：Uz-master-47">
+
+    private RString no;
+    private RString hihokenshaNo;
+    private RString hihokenshaName;
+    private RString seibetsu;
+    private RString birthDay;
+    private RString age;
+    private RString shinseiYMD;
+    private RString shinseiKubunOnShinsei;
+    private RString shinseiKubunOnHorei;
+    private RString nijiHanteiKekka;
+    private RString yukoKikan;
+    private RString yukoKikanFrom;
+    private RString yukoKikanTo;
+    private RString nijiHanteiYMD;
+    private RString ninteiJokyoTeikyoYMD;
+
+    public dgResultList_Row() {
+        super();
+        this.no = RString.EMPTY;
+        this.hihokenshaNo = RString.EMPTY;
+        this.hihokenshaName = RString.EMPTY;
+        this.seibetsu = RString.EMPTY;
+        this.birthDay = RString.EMPTY;
+        this.age = RString.EMPTY;
+        this.shinseiYMD = RString.EMPTY;
+        this.shinseiKubunOnShinsei = RString.EMPTY;
+        this.shinseiKubunOnHorei = RString.EMPTY;
+        this.nijiHanteiKekka = RString.EMPTY;
+        this.yukoKikan = RString.EMPTY;
+        this.yukoKikanFrom = RString.EMPTY;
+        this.yukoKikanTo = RString.EMPTY;
+        this.nijiHanteiYMD = RString.EMPTY;
+        this.ninteiJokyoTeikyoYMD = RString.EMPTY;
+    }
+
+    public dgResultList_Row(RString no, RString hihokenshaNo, RString hihokenshaName, RString seibetsu, RString birthDay, RString age, RString shinseiYMD, RString shinseiKubunOnShinsei, RString shinseiKubunOnHorei, RString nijiHanteiKekka, RString yukoKikan, RString yukoKikanFrom, RString yukoKikanTo, RString nijiHanteiYMD, RString ninteiJokyoTeikyoYMD) {
+        super();
+        this.setOriginalData("no", no);
+        this.setOriginalData("hihokenshaNo", hihokenshaNo);
+        this.setOriginalData("hihokenshaName", hihokenshaName);
+        this.setOriginalData("seibetsu", seibetsu);
+        this.setOriginalData("birthDay", birthDay);
+        this.setOriginalData("age", age);
+        this.setOriginalData("shinseiYMD", shinseiYMD);
+        this.setOriginalData("shinseiKubunOnShinsei", shinseiKubunOnShinsei);
+        this.setOriginalData("shinseiKubunOnHorei", shinseiKubunOnHorei);
+        this.setOriginalData("nijiHanteiKekka", nijiHanteiKekka);
+        this.setOriginalData("yukoKikan", yukoKikan);
+        this.setOriginalData("yukoKikanFrom", yukoKikanFrom);
+        this.setOriginalData("yukoKikanTo", yukoKikanTo);
+        this.setOriginalData("nijiHanteiYMD", nijiHanteiYMD);
+        this.setOriginalData("ninteiJokyoTeikyoYMD", ninteiJokyoTeikyoYMD);
+        this.no = no;
+        this.hihokenshaNo = hihokenshaNo;
+        this.hihokenshaName = hihokenshaName;
+        this.seibetsu = seibetsu;
+        this.birthDay = birthDay;
+        this.age = age;
+        this.shinseiYMD = shinseiYMD;
+        this.shinseiKubunOnShinsei = shinseiKubunOnShinsei;
+        this.shinseiKubunOnHorei = shinseiKubunOnHorei;
+        this.nijiHanteiKekka = nijiHanteiKekka;
+        this.yukoKikan = yukoKikan;
+        this.yukoKikanFrom = yukoKikanFrom;
+        this.yukoKikanTo = yukoKikanTo;
+        this.nijiHanteiYMD = nijiHanteiYMD;
+        this.ninteiJokyoTeikyoYMD = ninteiJokyoTeikyoYMD;
+    }
+
+    public RString getNo() {
+        return no;
+    }
+
+    public RString getHihokenshaNo() {
+        return hihokenshaNo;
+    }
+
+    public RString getHihokenshaName() {
+        return hihokenshaName;
+    }
+
+    public RString getSeibetsu() {
+        return seibetsu;
+    }
+
+    public RString getBirthDay() {
+        return birthDay;
+    }
+
+    public RString getAge() {
+        return age;
+    }
+
+    public RString getShinseiYMD() {
+        return shinseiYMD;
+    }
+
+    public RString getShinseiKubunOnShinsei() {
+        return shinseiKubunOnShinsei;
+    }
+
+    public RString getShinseiKubunOnHorei() {
+        return shinseiKubunOnHorei;
+    }
+
+    public RString getNijiHanteiKekka() {
+        return nijiHanteiKekka;
+    }
+
+    public RString getYukoKikan() {
+        return yukoKikan;
+    }
+
+    public RString getYukoKikanFrom() {
+        return yukoKikanFrom;
+    }
+
+    public RString getYukoKikanTo() {
+        return yukoKikanTo;
+    }
+
+    public RString getNijiHanteiYMD() {
+        return nijiHanteiYMD;
+    }
+
+    public RString getNinteiJokyoTeikyoYMD() {
+        return ninteiJokyoTeikyoYMD;
+    }
+
+    public void setNo(RString no) {
+        this.setOriginalData("no", no);
+        this.no = no;
+    }
+
+    public void setHihokenshaNo(RString hihokenshaNo) {
+        this.setOriginalData("hihokenshaNo", hihokenshaNo);
+        this.hihokenshaNo = hihokenshaNo;
+    }
+
+    public void setHihokenshaName(RString hihokenshaName) {
+        this.setOriginalData("hihokenshaName", hihokenshaName);
+        this.hihokenshaName = hihokenshaName;
+    }
+
+    public void setSeibetsu(RString seibetsu) {
+        this.setOriginalData("seibetsu", seibetsu);
+        this.seibetsu = seibetsu;
+    }
+
+    public void setBirthDay(RString birthDay) {
+        this.setOriginalData("birthDay", birthDay);
+        this.birthDay = birthDay;
+    }
+
+    public void setAge(RString age) {
+        this.setOriginalData("age", age);
+        this.age = age;
+    }
+
+    public void setShinseiYMD(RString shinseiYMD) {
+        this.setOriginalData("shinseiYMD", shinseiYMD);
+        this.shinseiYMD = shinseiYMD;
+    }
+
+    public void setShinseiKubunOnShinsei(RString shinseiKubunOnShinsei) {
+        this.setOriginalData("shinseiKubunOnShinsei", shinseiKubunOnShinsei);
+        this.shinseiKubunOnShinsei = shinseiKubunOnShinsei;
+    }
+
+    public void setShinseiKubunOnHorei(RString shinseiKubunOnHorei) {
+        this.setOriginalData("shinseiKubunOnHorei", shinseiKubunOnHorei);
+        this.shinseiKubunOnHorei = shinseiKubunOnHorei;
+    }
+
+    public void setNijiHanteiKekka(RString nijiHanteiKekka) {
+        this.setOriginalData("nijiHanteiKekka", nijiHanteiKekka);
+        this.nijiHanteiKekka = nijiHanteiKekka;
+    }
+
+    public void setYukoKikan(RString yukoKikan) {
+        this.setOriginalData("yukoKikan", yukoKikan);
+        this.yukoKikan = yukoKikan;
+    }
+
+    public void setYukoKikanFrom(RString yukoKikanFrom) {
+        this.setOriginalData("yukoKikanFrom", yukoKikanFrom);
+        this.yukoKikanFrom = yukoKikanFrom;
+    }
+
+    public void setYukoKikanTo(RString yukoKikanTo) {
+        this.setOriginalData("yukoKikanTo", yukoKikanTo);
+        this.yukoKikanTo = yukoKikanTo;
+    }
+
+    public void setNijiHanteiYMD(RString nijiHanteiYMD) {
+        this.setOriginalData("nijiHanteiYMD", nijiHanteiYMD);
+        this.nijiHanteiYMD = nijiHanteiYMD;
+    }
+
+    public void setNinteiJokyoTeikyoYMD(RString ninteiJokyoTeikyoYMD) {
+        this.setOriginalData("ninteiJokyoTeikyoYMD", ninteiJokyoTeikyoYMD);
+        this.ninteiJokyoTeikyoYMD = ninteiJokyoTeikyoYMD;
+    }
+
+    // </editor-fold>
+}
