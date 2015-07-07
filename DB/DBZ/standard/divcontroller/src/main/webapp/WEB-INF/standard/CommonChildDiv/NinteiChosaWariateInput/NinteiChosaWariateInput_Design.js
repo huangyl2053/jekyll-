fed5862194b8ab537eb8ea9f1018416035d8778c
中島　慎也 <n3015@ndensan.co.jp>
2015-07-07 14:16:53 +0900

    UIデザイン作成（DBE20200002 認定調査スケジュール詳細）

---
+++ DB/DBZ/standard/divcontroller/src/main/webapp/WEB-INF/standard/CommonChildDiv/NinteiChosaWariateInput/NinteiChosaWariateInput_Design.js
@@ -0,0 +1,147 @@
+var DBZ;
+(function (DBZ) {
+    (function (NinteiChosaWariateInput) {
+        var Events = (function () {
+            function Events() {
+            }
+            return Events;
+        })();
+        NinteiChosaWariateInput.Events = Events;
+
+        var Controls = (function () {
+            function Controls(fieldName) {
+                this._myName = fieldName;
+            }
+            Controls.myType = function () {
+                return "NinteiChosaWariateInput";
+            };
+
+            Controls.prototype.convFiledNameSelf = function () {
+                return this._myName + "_" + DBZ.NinteiChosaWariateInput.Controls.myType();
+            };
+
+            Controls.prototype.convFiledName = function (fieldName) {
+                return this._myName + "_" + DBZ.NinteiChosaWariateInput.Controls.myType() + "_" + fieldName;
+            };
+
+            Controls.prototype.NinteiChosaWariateInput = function () {
+                return new UZA.Panel(this.convFiledNameSelf());
+            };
+
+            Controls.prototype.WariateJokyo = function () {
+                return new UZA.Panel(this.convFiledName("WariateJokyo"));
+            };
+
+            Controls.prototype.txtNinteiChosainCode = function () {
+                return new UZA.TextBoxCode(this.convFiledName("txtNinteiChosainCode"));
+            };
+
+            Controls.prototype.txtNinteiChosainName = function () {
+                return new UZA.TextBox(this.convFiledName("txtNinteiChosainName"));
+            };
+
+            Controls.prototype.txtNinteiChosaItakusakiCode = function () {
+                return new UZA.TextBoxCode(this.convFiledName("txtNinteiChosaItakusakiCode"));
+            };
+
+            Controls.prototype.txtNinteiChosaItakusakiName = function () {
+                return new UZA.TextBox(this.convFiledName("txtNinteiChosaItakusakiName"));
+            };
+
+            Controls.prototype.txtNinteiChosaDate = function () {
+                return new UZA.TextBoxDate(this.convFiledName("txtNinteiChosaDate"));
+            };
+
+            Controls.prototype.txtNinteiChosaTime = function () {
+                return new UZA.TextBox(this.convFiledName("txtNinteiChosaTime"));
+            };
+
+            Controls.prototype.txtNinteiChosaTimeFrame = function () {
+                return new UZA.TextBox(this.convFiledName("txtNinteiChosaTimeFrame"));
+            };
+
+            Controls.prototype.radYoyakuKahi = function () {
+                return new UZA.RadioButton(this.convFiledName("radYoyakuKahi"));
+            };
+
+            Controls.prototype.txtNinteiChosaBiko = function () {
+                return new UZA.TextBox(this.convFiledName("txtNinteiChosaBiko"));
+            };
+
+            Controls.prototype.radYoyakuJokyo = function () {
+                return new UZA.RadioButton(this.convFiledName("radYoyakuJokyo"));
+            };
+
+            Controls.prototype.btnSearchTaishosha = function () {
+                return new UZA.ButtonDialog(this.convFiledName("btnSearchTaishosha"));
+            };
+
+            Controls.prototype.btnDisplayLatestInformation = function () {
+                return new UZA.Button(this.convFiledName("btnDisplayLatestInformation"));
+            };
+
+            Controls.prototype.TaishoshaShosai = function () {
+                return new UZA.Panel(this.convFiledName("TaishoshaShosai"));
+            };
+
+            Controls.prototype.txtHihoBangoNumber = function () {
+                return new UZA.TextBox(this.convFiledName("txtHihoBangoNumber"));
+            };
+
+            Controls.prototype.txtHihoBangoGosu = function () {
+                return new UZA.TextBox(this.convFiledName("txtHihoBangoGosu"));
+            };
+
+            Controls.prototype.txtHokenshaName = function () {
+                return new UZA.TextBox(this.convFiledName("txtHokenshaName"));
+            };
+
+            Controls.prototype.txtNinteiShinseiDate = function () {
+                return new UZA.TextBoxDate(this.convFiledName("txtNinteiShinseiDate"));
+            };
+
+            Controls.prototype.txtNinteiShinseiKubun = function () {
+                return new UZA.TextBox(this.convFiledName("txtNinteiShinseiKubun"));
+            };
+
+            Controls.prototype.txtTaishoshaName = function () {
+                return new UZA.TextBox(this.convFiledName("txtTaishoshaName"));
+            };
+
+            Controls.prototype.txtTaishoshaNameKana = function () {
+                return new UZA.TextBox(this.convFiledName("txtTaishoshaNameKana"));
+            };
+
+            Controls.prototype.txtBasho = function () {
+                return new UZA.TextBox(this.convFiledName("txtBasho"));
+            };
+
+            Controls.prototype.txtChushajo = function () {
+                return new UZA.TextBox(this.convFiledName("txtChushajo"));
+            };
+
+            Controls.prototype.txtTachiainin1 = function () {
+                return new UZA.TextBox(this.convFiledName("txtTachiainin1"));
+            };
+
+            Controls.prototype.txtRenrakusaki1 = function () {
+                return new UZA.TextBox(this.convFiledName("txtRenrakusaki1"));
+            };
+
+            Controls.prototype.txtTachiainin2 = function () {
+                return new UZA.TextBox(this.convFiledName("txtTachiainin2"));
+            };
+
+            Controls.prototype.txtRenrakusaki2 = function () {
+                return new UZA.TextBox(this.convFiledName("txtRenrakusaki2"));
+            };
+
+            Controls.prototype.txtTaishoshaShosaiMemo = function () {
+                return new UZA.TextBoxMultiLine(this.convFiledName("txtTaishoshaShosaiMemo"));
+            };
+            return Controls;
+        })();
+        NinteiChosaWariateInput.Controls = Controls;
+    })(DBZ.NinteiChosaWariateInput || (DBZ.NinteiChosaWariateInput = {}));
+    var NinteiChosaWariateInput = DBZ.NinteiChosaWariateInput;
+})(DBZ || (DBZ = {}));
---
+++ DB/DBZ/standard/divcontroller/src/main/webapp/WEB-INF/standard/CommonChildDiv/NinteiChosaWariateInput/NinteiChosaWariateInput_Design.ts
@@ -0,0 +1,153 @@
+/// <reference path="../../d.ts/jquery.d.ts" />
+/// <reference path="../../d.ts/UzViewControls.d.ts" />
+/// <reference path="../../d.ts/UzaConverter.d.ts" />
+
+module DBZ {
+
+     export module NinteiChosaWariateInput {
+
+        export class Events {
+
+        }
+
+        export class Controls {
+            private _myName: string;
+
+            public static myType(): string {
+                return "NinteiChosaWariateInput";
+            }
+
+            constructor(fieldName: string) {
+                this._myName = fieldName;
+            }
+
+            public convFiledNameSelf(): string {
+                return this._myName + "_" + DBZ.NinteiChosaWariateInput.Controls.myType();
+            }
+
+            public convFiledName(fieldName: string): string {
+                return this._myName + "_" + DBZ.NinteiChosaWariateInput.Controls.myType() + "_" + fieldName;
+            }
+
+            public NinteiChosaWariateInput(): UZA.Panel {
+                return new UZA.Panel(this.convFiledNameSelf());
+            }
+
+            public WariateJokyo(): UZA.Panel {
+                return new UZA.Panel(this.convFiledName("WariateJokyo"));
+            }
+
+            public txtNinteiChosainCode(): UZA.TextBoxCode {
+                return new UZA.TextBoxCode(this.convFiledName("txtNinteiChosainCode"));
+            }
+
+            public txtNinteiChosainName(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtNinteiChosainName"));
+            }
+
+            public txtNinteiChosaItakusakiCode(): UZA.TextBoxCode {
+                return new UZA.TextBoxCode(this.convFiledName("txtNinteiChosaItakusakiCode"));
+            }
+
+            public txtNinteiChosaItakusakiName(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtNinteiChosaItakusakiName"));
+            }
+
+            public txtNinteiChosaDate(): UZA.TextBoxDate {
+                return new UZA.TextBoxDate(this.convFiledName("txtNinteiChosaDate"));
+            }
+
+            public txtNinteiChosaTime(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtNinteiChosaTime"));
+            }
+
+            public txtNinteiChosaTimeFrame(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtNinteiChosaTimeFrame"));
+            }
+
+            public radYoyakuKahi(): UZA.RadioButton {
+                return new UZA.RadioButton(this.convFiledName("radYoyakuKahi"));
+            }
+
+            public txtNinteiChosaBiko(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtNinteiChosaBiko"));
+            }
+
+            public radYoyakuJokyo(): UZA.RadioButton {
+                return new UZA.RadioButton(this.convFiledName("radYoyakuJokyo"));
+            }
+
+            public btnSearchTaishosha(): UZA.ButtonDialog {
+                return new UZA.ButtonDialog(this.convFiledName("btnSearchTaishosha"));
+            }
+
+            public btnDisplayLatestInformation(): UZA.Button {
+                return new UZA.Button(this.convFiledName("btnDisplayLatestInformation"));
+            }
+
+            public TaishoshaShosai(): UZA.Panel {
+                return new UZA.Panel(this.convFiledName("TaishoshaShosai"));
+            }
+
+            public txtHihoBangoNumber(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtHihoBangoNumber"));
+            }
+
+            public txtHihoBangoGosu(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtHihoBangoGosu"));
+            }
+
+            public txtHokenshaName(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtHokenshaName"));
+            }
+
+            public txtNinteiShinseiDate(): UZA.TextBoxDate {
+                return new UZA.TextBoxDate(this.convFiledName("txtNinteiShinseiDate"));
+            }
+
+            public txtNinteiShinseiKubun(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtNinteiShinseiKubun"));
+            }
+
+            public txtTaishoshaName(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtTaishoshaName"));
+            }
+
+            public txtTaishoshaNameKana(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtTaishoshaNameKana"));
+            }
+
+            public txtBasho(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtBasho"));
+            }
+
+            public txtChushajo(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtChushajo"));
+            }
+
+            public txtTachiainin1(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtTachiainin1"));
+            }
+
+            public txtRenrakusaki1(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtRenrakusaki1"));
+            }
+
+            public txtTachiainin2(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtTachiainin2"));
+            }
+
+            public txtRenrakusaki2(): UZA.TextBox {
+                return new UZA.TextBox(this.convFiledName("txtRenrakusaki2"));
+            }
+
+            public txtTaishoshaShosaiMemo(): UZA.TextBoxMultiLine {
+                return new UZA.TextBoxMultiLine(this.convFiledName("txtTaishoshaShosaiMemo"));
+            }
+
+        }
+
+     }
+
+}
+
