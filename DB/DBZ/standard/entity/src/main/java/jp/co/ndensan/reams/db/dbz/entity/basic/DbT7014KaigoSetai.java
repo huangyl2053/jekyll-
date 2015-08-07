055a052952e9baa994bd7f1c4c5d7e141c960c9d
姜　井図 <n8300@ndensan.co.jp>
2015-08-07 15:27:08 +0900

    エンティティ作成_エンティティ修正

--- DB/DBZ/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbz/entity/basic/DbT7014KaigoSetai.java
+++ DB/DBZ/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbz/entity/basic/DbT7014KaigoSetai.java
@@ -45,21 +45,44 @@ public enum DbT7014KaigoSetai implements IColumnDefinition {
      */
     hihokenshaNo(2147483647, 0),
     /**
-     * 世帯基準年月日
+     * 管理識別区分
+     * <br/>1：賦課、2：高額介護、3：負担限度額、4：社福軽減
      */
-    setaiKijunYMD(2147483647, 0),
+    kanriShikibetsuKubun(1, 0),
     /**
-     * 連番
+     * 世帯把握基準年月日
      */
-    renban(10, 0),
+    setaiHaakuKijunYMD(2147483647, 0),
     /**
-     * 処理日時
+     * 世帯員管理連番
      */
-    shoriTimestamp(29, 0),
+    setaiInkanriRenban(10, 0),
     /**
      * 世帯員識別コード
      */
-    setaiInshikibetsuCode(2147483647, 0);
+    setaiInshikibetsuCode(2147483647, 0),
+    /**
+     * 本人区分
+     * <br/>1：本人、2：世帯構成員
+     */
+    honninKubun(1, 0),
+    /**
+     * 課税年度
+     */
+    kazeiNendo(2147483647, 0),
+    /**
+     * 課税非課税区分
+     * <br/>1:課税　2:非課税　3:未申告　4:所得調査中
+     */
+    kazeiHikazeiKubun(1, 0),
+    /**
+     * 合計所得金額
+     */
+    gokeiShotokuKingaku(15, 0),
+    /**
+     * 課税年金収入額
+     */
+    kazeiNenkinShunyugaku(15, 0);
 
     private final int maxLength;
     private final int scale;
