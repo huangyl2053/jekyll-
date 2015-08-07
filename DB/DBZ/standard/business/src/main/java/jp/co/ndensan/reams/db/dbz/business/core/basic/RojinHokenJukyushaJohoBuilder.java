1193ee50d759535713826af0c7c6223074114e25
姜　井図 <n8300@ndensan.co.jp>
2015-08-07 16:34:29 +0900

    Dac作成_Dbt7005

---
+++ DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/core/basic/RojinHokenJukyushaJohoBuilder.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbz.business.core.basic;
+
+import static java.util.Objects.requireNonNull;
+import jp.co.ndensan.reams.db.dbz.definition.valueobject.domain.HihokenshaNo;
+import jp.co.ndensan.reams.db.dbz.entity.basic.DbT7005RojinHokenJukyushaJohoEntity;
+import jp.co.ndensan.reams.ur.urz.definition.enumeratedtype.message.UrSystemErrorMessages;
+import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
+import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
+
+/**
+ * {@link RojinHokenJukyushaJoho}の編集を行うビルダークラスです。
+ */
+public class RojinHokenJukyushaJohoBuilder {
+
+    private final DbT7005RojinHokenJukyushaJohoEntity entity;
+    private final RojinHokenJukyushaJohoIdentifier id;
+
+    /**
+     * {@link DbT7005RojinHokenJukyushaJohoEntity}より{@link RojinHokenJukyushaJoho}の編集用Builderクラスを生成します。
+     *
+     * @param entity {@link DbT7005RojinHokenJukyushaJohoEntity}
+     * @param id {@link RojinHokenJukyushaJohoIdentifier}
+     *
+     */
+    RojinHokenJukyushaJohoBuilder(
+            DbT7005RojinHokenJukyushaJohoEntity entity,
+            RojinHokenJukyushaJohoIdentifier id
+    ) {
+        this.entity = entity.clone();
+        this.id = id;
+
+    }
+
+//TODO Key項目のsetterメソッドは削除してください。
+//TODO 一緒に置換される値のまとまりで不変なクラスを作成し、その単位でsetterを作る様に見直してください。
+    /**
+     * 識別コードを設定します。
+     *
+     * @param 識別コード 識別コード
+     * @return {@link RojinHokenJukyushaJohoBuilder}
+     */
+    public RojinHokenJukyushaJohoBuilder set識別コード(ShikibetsuCode 識別コード) {
+        requireNonNull(識別コード, UrSystemErrorMessages.値がnull.getReplacedMessage("識別コード"));
+        entity.setShikibetsuCode(識別コード);
+        return this;
+    }
+
+    /**
+     * 市町村コードを設定します。
+     *
+     * @param 市町村コード 市町村コード
+     * @return {@link RojinHokenJukyushaJohoBuilder}
+     */
+    public RojinHokenJukyushaJohoBuilder set市町村コード(LasdecCode 市町村コード) {
+        requireNonNull(市町村コード, UrSystemErrorMessages.値がnull.getReplacedMessage("市町村コード"));
+        entity.setShichosonCode(市町村コード);
+        return this;
+    }
+
+    /**
+     * 被保険者番号を設定します。
+     *
+     * @param 被保険者番号 被保険者番号
+     * @return {@link RojinHokenJukyushaJohoBuilder}
+     */
+    public RojinHokenJukyushaJohoBuilder set被保険者番号(HihokenshaNo 被保険者番号) {
+        requireNonNull(被保険者番号, UrSystemErrorMessages.値がnull.getReplacedMessage("被保険者番号"));
+        entity.setHihokenshaNo(被保険者番号);
+        return this;
+    }
+
+    /**
+     * 老人保健市町村コードを設定します。
+     *
+     * @param 老人保健市町村コード 老人保健市町村コード
+     * @return {@link RojinHokenJukyushaJohoBuilder}
+     */
+    public RojinHokenJukyushaJohoBuilder set老人保健市町村コード(LasdecCode 老人保健市町村コード) {
+        requireNonNull(老人保健市町村コード, UrSystemErrorMessages.値がnull.getReplacedMessage("老人保健市町村コード"));
+        entity.setRojinHokenShichosonCode(老人保健市町村コード);
+        return this;
+    }
+
+    /**
+     * 老人保健受給者番号を設定します。
+     *
+     * @param 老人保健受給者番号 老人保健受給者番号
+     * @return {@link RojinHokenJukyushaJohoBuilder}
+     */
+    public RojinHokenJukyushaJohoBuilder set老人保健受給者番号(Rstring 老人保健受給者番号) {
+        requireNonNull(老人保健受給者番号, UrSystemErrorMessages.値がnull.getReplacedMessage("老人保健受給者番号"));
+        entity.setRojinHokenJukyushaNo(老人保健受給者番号);
+        return this;
+    }
+
+    /**
+     * {@link RojinHokenJukyushaJoho}のインスタンスを生成します。
+     *
+     * @return {@link RojinHokenJukyushaJoho}のインスタンス
+     */
+    public RojinHokenJukyushaJoho build() {
+        return new RojinHokenJukyushaJoho(entity, id);
+    }
+}
---
+++ DB/DBZ/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbz/persistence/db/basic/DbT7005RojinHokenJukyushaJohoDac.java
@@ -0,0 +1,80 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbz.persistence.db.basic;
+
+import java.util.List;
+import static java.util.Objects.requireNonNull;
+import static jp.co.ndensan.reams.db.dbz.entity.basic.DbT1001HihokenshaDaicho.shikibetsuCode;
+import jp.co.ndensan.reams.db.dbz.entity.basic.DbT7005RojinHokenJukyushaJoho;
+import jp.co.ndensan.reams.db.dbz.entity.basic.DbT7005RojinHokenJukyushaJohoEntity;
+import static jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT7005RojinHokenJukyushaJoho.*;
+import jp.co.ndensan.reams.ur.urz.definition.enumeratedtype.message.UrSystemErrorMessages;
+import jp.co.ndensan.reams.ur.urz.persistence.basic.ISaveable;
+import jp.co.ndensan.reams.uz.uza.core.mybatis.SqlSession;
+import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
+import jp.co.ndensan.reams.uz.uza.util.db.DbAccessorNormalType;
+import static jp.co.ndensan.reams.uz.uza.util.db.Restrictions.eq;
+import jp.co.ndensan.reams.uz.uza.util.db.util.DbAccessorMethodSelector;
+import jp.co.ndensan.reams.uz.uza.util.di.InjectSession;
+import jp.co.ndensan.reams.uz.uza.util.di.Transaction;
+
+/**
+ * 老人保健受給者情報のデータアクセスクラスです。
+ */
+public class DbT7005RojinHokenJukyushaJohoDac implements ISaveable<DbT7005RojinHokenJukyushaJohoEntity> {
+
+    @InjectSession
+    private SqlSession session;
+
+    /**
+     * 主キーで老人保健受給者情報を取得します。
+     *
+     * @param 識別コード ShikibetsuCode
+     * @return DbT7005RojinHokenJukyushaJohoEntity
+     * @throws NullPointerException 引数のいずれかがnullの場合
+     */
+    @Transaction
+    public DbT7005RojinHokenJukyushaJohoEntity selectByKey(
+            ShikibetsuCode 識別コード) throws NullPointerException {
+        requireNonNull(識別コード, UrSystemErrorMessages.値がnull.getReplacedMessage("識別コード"));
+
+        DbAccessorNormalType accessor = new DbAccessorNormalType(session);
+
+        return accessor.select().
+                table(DbT7005RojinHokenJukyushaJoho.class).
+                where(and(
+                                eq(shikibetsuCode, 識別コード))).
+                toObject(DbT7005RojinHokenJukyushaJohoEntity.class);
+    }
+
+    /**
+     * 老人保健受給者情報を全件返します。
+     *
+     * @return List<DbT7005RojinHokenJukyushaJohoEntity>
+     */
+    @Transaction
+    public List<DbT7005RojinHokenJukyushaJohoEntity> selectAll() {
+        DbAccessorNormalType accessor = new DbAccessorNormalType(session);
+
+        return accessor.select().
+                table(DbT7005RojinHokenJukyushaJoho.class).
+                toList(DbT7005RojinHokenJukyushaJohoEntity.class);
+    }
+
+    /**
+     * DbT7005RojinHokenJukyushaJohoEntityを登録します。状態によってinsert/update/delete処理に振り分けられます。
+     *
+     * @param entity entity
+     * @return 登録件数
+     */
+    @Transaction
+    @Override
+    public int save(DbT7005RojinHokenJukyushaJohoEntity entity) {
+        requireNonNull(entity, UrSystemErrorMessages.値がnull.getReplacedMessage("老人保健受給者情報エンティティ"));
+        // TODO 物理削除であるかは業務ごとに検討してください。
+        //return DbAccessorMethodSelector.saveByForDeletePhysical(new DbAccessorNormalType(session), entity);
+        return DbAccessorMethodSelector.saveBy(new DbAccessorNormalType(session), entity);
+    }
+}
