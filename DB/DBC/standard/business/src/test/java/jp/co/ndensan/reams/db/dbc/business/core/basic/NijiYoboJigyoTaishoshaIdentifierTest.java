82e1c8dd2f235fcb42ed13ea4439a33cbd0ff468
姜　井図 <n8300@ndensan.co.jp>
2015-08-20 17:10:00 +0900

    ビルドエラー解消_dbc

--- DB/DBC/standard/business/src/test/java/jp/co/ndensan/reams/db/dbc/business/core/basic/NijiYoboJigyoTaishoshaIdentifierTest.java
+++ DB/DBC/standard/business/src/test/java/jp/co/ndensan/reams/db/dbc/business/core/basic/NijiYoboJigyoTaishoshaIdentifierTest.java
@@ -4,12 +4,15 @@
  */
 package jp.co.ndensan.reams.db.dbc.business.core.basic;
 
-import jp.co.ndensan.reams.db.dbc.testhelper.DbcTestBase;
-import static jp.co.ndensan.reams.db.dbc.testhelper.matcher.IsSerializable.serializable;
-import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
-import jp.co.ndensan.reams.uz.uza.lang.RDateTime;
+import jp.co.ndensan.reams.db.dbc.entity.basic.helper.DbT3100NijiYoboJigyoTaishoshaEntityGenerator;
+import jp.co.ndensan.reams.db.dbx.definition.valueobject.domain.HihokenshaNo;
+import jp.co.ndensan.reams.db.dbx.definition.valueobject.domain.HokenshaNo;
+import static jp.co.ndensan.reams.db.dbx.testhelper.matcher.IsSerializable.serializable;
+import jp.co.ndensan.reams.db.dbz.testhelper.DbcTestBase;
+import jp.co.ndensan.reams.uz.uza.math.Decimal;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
@@ -22,21 +25,23 @@ public class NijiYoboJigyoTaishoshaIdentifierTest extends DbcTestBase {
 
 //TODO 主キー型と変数名を置換してください
 //TODO 主キーの数が足りない場合、追加してください。
-    private static 主キー型1 主キー名1;
-    private static 主キー型2 主キー名2;
+    private static HokenshaNo 証記載保険者番号;
+    private static HihokenshaNo 被保険者番号;
+    private static Decimal 履歴番号;
 
     @BeforeClass
     public static void setUpClass() {
 //TODO 主キー値を適切な値に置換してください
-        主キー名1 = DbT3100NijiYoboJigyoTaishoshaEntityGenerator.DEFAULT_主キー名1;
-        主キー名2 = DbT3100NijiYoboJigyoTaishoshaEntityGenerator.DEFAULT_主キー名2;
+        証記載保険者番号 = DbT3100NijiYoboJigyoTaishoshaEntityGenerator.DEFAULT_証記載保険者番号;
+        被保険者番号 = DbT3100NijiYoboJigyoTaishoshaEntityGenerator.DEFAULT_被保険者番号;
+        履歴番号 = DbT3100NijiYoboJigyoTaishoshaEntityGenerator.DEFAULT_履歴番号;
     }
 
     public static class シリアライズテスト extends DbcTestBase {
 
         @Test
         public void シリアライズできる() {
-            NijiYoboJigyoTaishoshaIdentifier sut = new NijiYoboJigyoTaishoshaIdentifier(主キー名1, 主キー名2);
+            NijiYoboJigyoTaishoshaIdentifier sut = new NijiYoboJigyoTaishoshaIdentifier(証記載保険者番号, 被保険者番号, 履歴番号);
             assertThat(sut, is(serializable()));
         }
     }
