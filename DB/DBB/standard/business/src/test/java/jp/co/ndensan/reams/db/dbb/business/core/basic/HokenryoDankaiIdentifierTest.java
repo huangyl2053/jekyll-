9c625007ff5c0b51a2cb87cdac5b5ae8cdb2c8c5
久保田　英男 <n8187@ndensan.co.jp>
2015-08-20 09:17:25 +0900

    ビルドエラー対応 dbb.business

--- DB/DBB/standard/business/src/test/java/jp/co/ndensan/reams/db/dbb/business/core/basic/HokenryoDankaiIdentifierTest.java
+++ DB/DBB/standard/business/src/test/java/jp/co/ndensan/reams/db/dbb/business/core/basic/HokenryoDankaiIdentifierTest.java
@@ -5,14 +5,11 @@
 package jp.co.ndensan.reams.db.dbb.business.core.basic;
 
 import jp.co.ndensan.reams.db.dbb.entity.basic.helper.DbT2013HokenryoDankaiEntityGenerator;
+import jp.co.ndensan.reams.db.dbz.definition.valueobject.DankaiIndex;
+import jp.co.ndensan.reams.db.dbz.definition.valueobject.RankKubun;
 import jp.co.ndensan.reams.db.dbz.testhelper.DbbTestBase;
 //import static jp.co.ndensan.reams.db.dbb.testhelper.matcher.IsSerializable.serializable;
-import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleYear;
-import jp.co.ndensan.reams.uz.uza.lang.RDateTime;
-import jp.co.ndensan.reams.uz.uza.lang.RString;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
@@ -27,8 +24,8 @@ public class HokenryoDankaiIdentifierTest extends DbbTestBase {
 //TODO 主キー型と変数名を置換してください
 //TODO 主キーの数が足りない場合、追加してください。
     private static FlexibleYear 賦課年度;
-    private static RString 段階index;
-    private static RString ランク区分;
+    private static DankaiIndex 段階index;
+    private static RankKubun ランク区分;
 
     @BeforeClass
     public static void setUpClass() {
