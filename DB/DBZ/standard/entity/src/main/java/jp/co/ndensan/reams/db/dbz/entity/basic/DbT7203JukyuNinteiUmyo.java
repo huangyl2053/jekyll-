0bb8810a1b8d9c2b8ac1ed111c6d9eea71c21dee
船山洋介 <n8235@ndensan.co.jp>
2015-08-27 16:19:54 +0900

    DbT7203JukyuNinteiUmyoのエンティティ再生成　使用するドメインを変更

--- DB/DBZ/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbz/entity/basic/DbT7203JukyuNinteiUmyo.java
+++ DB/DBZ/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbz/entity/basic/DbT7203JukyuNinteiUmyo.java
@@ -6,8 +6,7 @@ import jp.co.ndensan.reams.uz.uza.util.db.IColumnDefinition;
  * 受給認定運用管理テーブルの項目定義クラスです。
  */
 public enum DbT7203JukyuNinteiUmyo implements IColumnDefinition {
-// <editor-fold defaultstate="collapsed" desc="Created By POJO Tool ver 1.3.9">
-
+// <editor-fold defaultstate="collapsed" desc="Created By POJO Tool ver 1.4.2">
     /**
      * insertDantaiCd
      */
@@ -63,7 +62,7 @@ public enum DbT7203JukyuNinteiUmyo implements IColumnDefinition {
 
     /**
      * 項目の最大長のgetメソッドです。
-     *
+     * 
      * @return 項目の最大長
      */
     public int getMaxLength() {
@@ -72,7 +71,7 @@ public enum DbT7203JukyuNinteiUmyo implements IColumnDefinition {
 
     /**
      * 小数点以下の桁数のgetメソッドです。
-     *
+     * 
      * @return 小数点以下の桁数
      */
     public int getScale() {
