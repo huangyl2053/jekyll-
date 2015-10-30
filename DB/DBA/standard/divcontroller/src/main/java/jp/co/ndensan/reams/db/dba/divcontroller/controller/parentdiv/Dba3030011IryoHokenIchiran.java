7fd68166601081df384c370ce6993f5643bba355
chenlei <chenlei@liandisys.com.cn>
2015-10-30 10:14:38 +0800

    onLoad対応

--- DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/controller/parentdiv/Dba3030011IryoHokenIchiran.java
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/controller/parentdiv/Dba3030011IryoHokenIchiran.java
@@ -25,7 +25,7 @@ public class Dba3030011IryoHokenIchiran {
      */
     public ResponseData<Dba3030011IryoHokenIchiranDiv> onLoad(Dba3030011IryoHokenIchiranDiv panel) {
         IIryoHokenRirekiDiv iryoHokenRireki = panel.getCcdryoHokenRireki();
-        iryoHokenRireki.load(LasdecCode.EMPTY, ShikibetsuCode.EMPTY);
+        iryoHokenRireki.initialize(LasdecCode.EMPTY, ShikibetsuCode.EMPTY);
         return Response(panel);
     }
 
