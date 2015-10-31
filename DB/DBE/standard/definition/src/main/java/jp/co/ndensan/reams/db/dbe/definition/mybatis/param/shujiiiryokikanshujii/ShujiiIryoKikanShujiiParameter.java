de1b91b2077758da5e8f4e8c354ea6c9684e8cbe
燕京 <yanjing@liandisys.com.cn>
2015-10-31 14:23:30 +0800

    課題５＿主治医医療機関・主治医

---
+++ DB/DBE/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbe/definition/mybatis/param/shujiiiryokikanshujii/ShujiiIryoKikanShujiiParameter.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.definition.mybatisprm.shujiiiryokikanshujii;
+
+import static java.util.Objects.requireNonNull;
+import jp.co.ndensan.reams.ur.urz.definition.message.UrSystemErrorMessages;
+import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+/**
+ * 構成市町村マスタを特定するためのMyBatis用パラメータクラスです。
+ */
+@lombok.Getter
+public class ShujiiIryoKikanShujiiParameter {
+
+    private final RString shichosonShokibetsuID;
+    private final LasdecCode shichosonCode;
+
+    /**
+     * コンストラクタです。
+     *
+     * @param RString shichosonShokibetsuID
+     * @param LasdecCode shichosonCode
+     * @throws NullPointerException 引数のいずれかが{@code null}の場合
+     */
+    private ShujiiIryoKikanShujiiParameter(
+            RString shichosonShokibetsuID,
+            LasdecCode shichosonCode
+    ) {
+        this.shichosonShokibetsuID = requireNonNull(shichosonShokibetsuID, UrSystemErrorMessages.値がnull.getReplacedMessage("市町村識別ID"));
+        this.shichosonCode = requireNonNull(shichosonCode, UrSystemErrorMessages.値がnull.getReplacedMessage("市町村コード"));
+    }
+
+    /**
+     * キー検索用のパラメータを生成します。
+     *
+     * @param shichosonShokibetsuID
+     * @param shichosonCode
+     * @return 身体手帳検索パラメータ
+     */
+    public static ShujiiIryoKikanShujiiParameter createSelectByKeyParam(
+            RString shichosonShokibetsuID,
+            LasdecCode shichosonCode) {
+        return new ShujiiIryoKikanShujiiParameter(shichosonShokibetsuID, shichosonCode);
+    }
+
+}
---
+++ DB/DBE/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbe/persistence/db/mapper/shujiiiryokikanshujii/IShujiiIryoKikanShujiiMapper.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.persistence.db.mapper.relate.shujiiiryokikanshujii;
+
+import jp.co.ndensan.reams.db.dbe.definition.mybatisprm.shujiiiryokikanshujii.ShujiiIryoKikanShujiiParameter;
+import jp.co.ndensan.reams.db.dbe.entity.db.relate.syujii.shujiiiryokikanjoho.ShujiiIryoKikanJohoRelateEntity;
+
+/**
+ * 構成市町村マスタのマッパーインタフェースです。
+ */
+public interface IShujiiIryoKikanShujiiMapper {
+
+    /**
+     * 構成市町村マスタ情報をキー検索で１件取得します。
+     *
+     * @param 構成市町村マスタ検索条件 構成市町村マスタ検索条件
+     * @return KoseiShichosonMasterEntity
+     */
+    ShujiiIryoKikanJohoRelateEntity getShujiiIryoKikanShujiiRelateEntity(ShujiiIryoKikanShujiiParameter 構成市町村マスタ検索条件);
+}
