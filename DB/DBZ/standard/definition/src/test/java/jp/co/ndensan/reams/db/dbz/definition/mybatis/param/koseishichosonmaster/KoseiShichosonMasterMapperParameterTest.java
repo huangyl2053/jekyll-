014a9e03f19cf4284635644c2d8dd02b5385b4bd
sunhadi <sunhaidi@liandisys.com.cn>
2015-12-01 14:39:54 +0800

    コンパイルエラー対応

--- DB/DBZ/standard/definition/src/test/java/jp/co/ndensan/reams/db/dbz/definition/mybatis/param/koseishichosonmaster/KoseiShichosonMasterMapperParameterTest.java
+++ DB/DBZ/standard/definition/src/test/java/jp/co/ndensan/reams/db/dbz/definition/mybatis/param/koseishichosonmaster/KoseiShichosonMasterMapperParameterTest.java
@@ -6,7 +6,6 @@
 package jp.co.ndensan.reams.db.dbz.definition.mybatis.param.koseishichosonmaster;
 
 import jp.co.ndensan.reams.db.dbz.testhelper.DbzTestBase;
-import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -20,23 +19,19 @@ import org.junit.runner.RunWith;
 @RunWith(Enclosed.class)
 public class KoseiShichosonMasterMapperParameterTest extends DbzTestBase {
 
-    private static final LasdecCode shichonCode = new LasdecCode(new RString("302302"));
-    private static final boolean usesshichonCode = true;
+    private static final RString shichonCode = new RString("302302");
 
-// テストクラス名は適切に変更してください
     public static class createSelectByKeyParamテスト extends DbzTestBase {
 
         @Test(expected = NullPointerException.class)
         public void 主キー1にNullを指定すると_NullPointerExceptionが発生する() {
-            KoseiShichosonMasterMapperParameter sut = KoseiShichosonMasterMapperParameter.createSelectByKeyParam(null, usesshichonCode);
+            KoseiShichosonMasterMapperParameter sut = KoseiShichosonMasterMapperParameter.createSelectByKeyParam(null);
         }
 
         @Test
         public void 引数にNull以外を指定すると_パラメータが生成できる() {
-            KoseiShichosonMasterMapperParameter sut = KoseiShichosonMasterMapperParameter.createSelectByKeyParam(shichonCode, usesshichonCode);
-
-            assertThat(sut.getShichonCode(), is(shichonCode));
-            assertThat(sut.isUsesshichonCode(), is(true));
+            KoseiShichosonMasterMapperParameter sut = KoseiShichosonMasterMapperParameter.createSelectByKeyParam(shichonCode);
+            assertThat(sut.isUseShichosonShokibetsuID(), is(true));
         }
     }
 
