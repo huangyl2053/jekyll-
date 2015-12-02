0ca2c81b11cf6c71ae8956a1d66a349559fdaa67
zhangguopeng <zhangguopeng@liandisys.com.cn>
2015-12-02 13:41:06 +0800

    DB DBAMN00000_被保詳細ビジネスの実装

--- DB/DBE/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbe/persistence/db/basic/DbT5051KoseiShichosonMasterDac.java
+++ DB/DBE/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbe/persistence/db/basic/DbT5051KoseiShichosonMasterDac.java
@@ -7,14 +7,20 @@ package jp.co.ndensan.reams.db.dbe.persistence.db.basic;
 import java.util.List;
 import static java.util.Objects.requireNonNull;
 import jp.co.ndensan.reams.db.dbe.entity.db.basic.DbT5051KoseiShichosonMaster;
+import static jp.co.ndensan.reams.db.dbe.entity.db.basic.DbT5051KoseiShichosonMaster.gappeiKyuShichosonKubun;
+import static jp.co.ndensan.reams.db.dbe.entity.db.basic.DbT5051KoseiShichosonMaster.kanyuYMD;
+import static jp.co.ndensan.reams.db.dbe.entity.db.basic.DbT5051KoseiShichosonMaster.ridatsuYMD;
 import static jp.co.ndensan.reams.db.dbe.entity.db.basic.DbT5051KoseiShichosonMaster.shichosonShokibetsuID;
 import jp.co.ndensan.reams.db.dbe.entity.db.basic.DbT5051KoseiShichosonMasterEntity;
 import jp.co.ndensan.reams.db.dbz.persistence.db.basic.ISaveable;
 import jp.co.ndensan.reams.ur.urz.definition.message.UrSystemErrorMessages;
 import jp.co.ndensan.reams.uz.uza.core.mybatis.SqlSession;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.util.db.DbAccessorNormalType;
+import static jp.co.ndensan.reams.uz.uza.util.db.Restrictions.and;
 import static jp.co.ndensan.reams.uz.uza.util.db.Restrictions.eq;
+import static jp.co.ndensan.reams.uz.uza.util.db.Restrictions.leq;
 import jp.co.ndensan.reams.uz.uza.util.db.util.DbAccessors;
 import jp.co.ndensan.reams.uz.uza.util.di.InjectSession;
 import jp.co.ndensan.reams.uz.uza.util.di.Transaction;
@@ -74,5 +80,26 @@ public class DbT5051KoseiShichosonMasterDac implements ISaveable<DbT5051KoseiShi
         requireNonNull(entity, UrSystemErrorMessages.値がnull.getReplacedMessage("構成市町村マスタエンティティ"));
 
         return DbAccessors.saveBy(new DbAccessorNormalType(session), entity);
+    }
+
+    /**
+     * 所在保険者リスト情報取得。
+     *
+     * @param systemDate システム年月日
+     * @return @return List<DbT7051KoseiShichosonMasterEntity>
+     * @throws NullPointerException 引数のいずれかがnullの場合
+     */
+    @Transaction
+    public List<DbT5051KoseiShichosonMasterEntity> selectByGappeiKyuShichosonKubun(
+            RDate systemDate) throws NullPointerException {
+        requireNonNull(systemDate, UrSystemErrorMessages.値がnull.getReplacedMessage("システム年月日"));
+
+        DbAccessorNormalType accessor = new DbAccessorNormalType(session);
+
+        return accessor.select().table(DbT5051KoseiShichosonMaster.class).
+                where(and(eq(gappeiKyuShichosonKubun, "0"),
+                                leq(kanyuYMD, systemDate),
+                                leq(systemDate, ridatsuYMD))).
+                toList(DbT5051KoseiShichosonMasterEntity.class);
     }
 }
