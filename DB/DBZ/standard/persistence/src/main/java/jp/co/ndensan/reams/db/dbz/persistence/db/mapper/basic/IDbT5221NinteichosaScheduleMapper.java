1ed47a8c1d7434b1d7bf018b01b05270b4b63af0
testUser <testUser@liandisys.com.cn>
2015-12-02 12:03:07 +0800

    IDbT5221NinteichosaScheduleMapperの改修

--- DB/DBZ/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbz/persistence/db/mapper/basic/IDbT5221NinteichosaScheduleMapper.java
+++ DB/DBZ/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbz/persistence/db/mapper/basic/IDbT5221NinteichosaScheduleMapper.java
@@ -6,6 +6,10 @@
 package jp.co.ndensan.reams.db.dbz.persistence.db.mapper.basic;
 
 import jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT5221NinteichosaScheduleEntity;
+import jp.co.ndensan.reams.uz.uza.biz.Code;
+import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
  *
@@ -20,4 +24,47 @@ public interface IDbT5221NinteichosaScheduleMapper {
      * @deprecated マッピング検証用のメソッドであるため、テストコード以外から使用しないで下さい。
      */
     DbT5221NinteichosaScheduleEntity getEntity();
+
+    /**
+     * 認定調査スケジュール情報件数取得する
+     *
+     * @param 認定調査予定年月日
+     * @param 調査地区コード
+     * @param 認定調査委託先コード
+     * @param 認定調査員コード
+     * @param 市町村コード
+     * @return DbT5221NinteichosaScheduleEntity
+     */
+    DbT5221NinteichosaScheduleEntity getCount(FlexibleDate 認定調査予定年月日, Code 調査地区コード, RString 認定調査委託先コード,
+            RString 認定調査員コード, LasdecCode 市町村コード);
+
+    /**
+     * 認定調査予定情報削除する
+     *
+     * @param 認定調査予定年月日
+     * @param 調査地区コード
+     * @param 認定調査委託先コード
+     * @param 認定調査員コード
+     * @param 市町村コード
+     * @return DbT5221NinteichosaScheduleEntity
+     */
+    DbT5221NinteichosaScheduleEntity getDelete(FlexibleDate 認定調査予定年月日, Code 調査地区コード, RString 認定調査委託先コード,
+            RString 認定調査員コード, LasdecCode 市町村コード);
+
+    /**
+     * 認定調査スケジュール情報インサート処理
+     *
+     * @param 認定調査予定年月日
+     * @param 認定調査予定開始時間
+     * @param 認定調査予定終了時間
+     * @param 認定調査時間枠
+     * @param 調査地区コード
+     * @param 認定調査委託先コード
+     * @param 認定調査員コード
+     * @param 市町村コード
+     * @return DbT5221NinteichosaScheduleEntity
+     */
+    DbT5221NinteichosaScheduleEntity getInsert(FlexibleDate 認定調査予定年月日, RString 認定調査予定開始時間, RString 認定調査予定終了時間,
+            Code 認定調査時間枠, Code 調査地区コード, RString 認定調査委託先コード, RString 認定調査員コード, LasdecCode 市町村コード);
+
 }
