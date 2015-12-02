c7c29701aa4f633310363d9c6f5a9228d83089b9
lijunchao <lijunchao@liandisys.com.cn>
2015-12-02 11:47:18 +0800

    DBU　広住特者住民コードSQL取得　新規

---
+++ DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/loiki/KoikiJushochiTokureishaEnum.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbu.business.loiki;
+
+/**
+ *
+ * @author soft863
+ */
+public enum KoikiJushochiTokureishaEnum {
+
+    広域内住所地特例者検索制御_措置元_措置先区分_介護共通;
+
+//    DBU("DBU", "広域内住所地特例者検索制御_措置元_措置先区分_介護共通");
+//
+//    private final RString code;
+//    private final RString name;
+//
+//    private KoikiJushochiTokureishaEnum(String code, String name) {
+//        this.code = new RString(code);
+//
+//        this.name = new RString(name);
+//    }
+}
