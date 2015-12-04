75003f856a466ceba7447e875e6512cfc4551a08
zhangguopeng <zhangguopeng@liandisys.com.cn>
2015-12-04 11:53:17 +0800

    テープル：DbT5120ShinseitodokedeJohoはentity生成、関連ソース修正

--- DB/DBE/standard/entity/src/test/java/jp/co/ndensan/reams/db/dbe/entity/db/basic/helper/DbT5120ShinseitodokedeJohoEntityGenerator.java
+++ DB/DBE/standard/entity/src/test/java/jp/co/ndensan/reams/db/dbe/entity/db/basic/helper/DbT5120ShinseitodokedeJohoEntityGenerator.java
@@ -7,9 +7,6 @@ package jp.co.ndensan.reams.db.dbe.entity.db.basic.helper;
 import jp.co.ndensan.reams.db.dbe.entity.db.basic.DbT5120ShinseitodokedeJohoEntity;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.JigyoshaNo;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShinseishoKanriNo;
-import jp.co.ndensan.reams.uz.uza.biz.AtenaJusho;
-import jp.co.ndensan.reams.uz.uza.biz.AtenaKanaMeisho;
-import jp.co.ndensan.reams.uz.uza.biz.AtenaMeisho;
 import jp.co.ndensan.reams.uz.uza.biz.Code;
 import jp.co.ndensan.reams.uz.uza.biz.TelNo;
 import jp.co.ndensan.reams.uz.uza.biz.YubinNo;
@@ -24,13 +21,13 @@ public final class DbT5120ShinseitodokedeJohoEntityGenerator {
 
     public static final ShinseishoKanriNo DEFAULT_申請書管理番号 = new ShinseishoKanriNo("0000000001");
     public static final Code DEFAULT_申請届出代行区分コード = new Code("1");
-    public static final AtenaMeisho DEFAULT_申請届出者氏名 = new AtenaMeisho("申請届出者氏名");
-    public static final AtenaKanaMeisho DEFAULT_申請届出者氏名カナ = new AtenaKanaMeisho("申請届出者カナ");
+    public static final RString DEFAULT_申請届出者氏名 = new RString("申請届出者氏名");
+    public static final RString DEFAULT_申請届出者氏名カナ = new RString("申請届出者カナ");
     public static final RString DEFAULT_申請届出者続柄コード = new RString("01");
     public static final JigyoshaNo DEFAULT_申請届出代行事業者番号 = new JigyoshaNo("1234567890");
     public static final RString DEFAULT_事業者区分 = new RString("01");
     public static final YubinNo DEFAULT_申請届出者郵便番号 = new YubinNo("123-4567");
-    public static final AtenaJusho DEFAULT_申請届出者住所 = new AtenaJusho("申請届出者住所");
+    public static final RString DEFAULT_申請届出者住所 = new RString("申請届出者住所");
     public static final TelNo DEFAULT_申請届出者電話番号 = new TelNo("1234567890");
 
     /**
@@ -45,7 +42,7 @@ public final class DbT5120ShinseitodokedeJohoEntityGenerator {
         entity.setShinseiTodokedeDaikoKubunCode(DEFAULT_申請届出代行区分コード);
         entity.setShinseiTodokedeshaShimei(DEFAULT_申請届出者氏名);
         entity.setShinseiTodokedeshaKanaShimei(DEFAULT_申請届出者氏名カナ);
-        entity.setShinseiTodokedeshaTsuzukigaraCode(DEFAULT_申請届出者続柄コード);
+        entity.setShinseiTodokedeshaTsuzukigara(DEFAULT_申請届出者続柄コード);
         entity.setShinseiTodokedeDaikoJigyoshaNo(DEFAULT_申請届出代行事業者番号);
         entity.setJigyoshaKubun(DEFAULT_事業者区分);
         entity.setShinseiTodokedeshaYubinNo(DEFAULT_申請届出者郵便番号);
