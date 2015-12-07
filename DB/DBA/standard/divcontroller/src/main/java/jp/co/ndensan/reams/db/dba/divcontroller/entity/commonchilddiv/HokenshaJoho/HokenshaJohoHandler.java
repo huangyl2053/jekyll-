ac90b8e91ac0a231cbd526b9b60ffee35941ef90
lishengli <lishengli@liandisys.com.cn>
2015-12-07 20:24:31 +0800

    DB DBAMN00000 保険者入力補助修正

--- DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/commonchilddiv/HokenshaJoho/HokenshaJohoHandler.java
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/commonchilddiv/HokenshaJoho/HokenshaJohoHandler.java
@@ -8,10 +8,12 @@ package jp.co.ndensan.reams.db.dba.divcontroller.entity.commonchilddiv.HokenshaJ
 import java.util.List;
 import jp.co.ndensan.reams.db.dba.business.core.hokensha.Hokensha;
 import jp.co.ndensan.reams.db.dba.business.core.hokensha.KenCodeJigyoshaInputGuide;
-import jp.co.ndensan.reams.db.dba.definition.mybatis.param.hokensha.HokenjaMapperParameter;
-import jp.co.ndensan.reams.db.dba.divcontroller.entity.parentdiv.DBA6010012.DataGrid1_Row;
+import jp.co.ndensan.reams.db.dba.definition.mybatis.param.hokensha.HokenshaMapperParameter;
+//import jp.co.ndensan.reams.db.dba.divcontroller.entity.parentdiv.DBA6010012.DataGrid1_Row;
 import jp.co.ndensan.reams.db.dba.service.core.hokensha.HokenshaNyuryokuHojoFinder;
+import jp.co.ndensan.reams.ur.urz.business.core.association.Association;
 import jp.co.ndensan.reams.ur.urz.definition.core.hokenja.HokenjaNo;
+import jp.co.ndensan.reams.ur.urz.service.core.association.AssociationFinderFactory;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.ui.binding.DropDownList;
@@ -30,7 +32,7 @@ public class HokenshaJohoHandler {
     }
 
     public void onBlur_txtHokenshaNo(HokenshaJohoDiv div) {
-        HokenjaMapperParameter parameter = HokenjaMapperParameter.createHokenjaNoParam(
+        HokenshaMapperParameter parameter = HokenshaMapperParameter.createHokenjaNoParam(
                 new HokenjaNo(div.getTxtHokenshaNo().getValue()));
         Hokensha ent = HokenshaNyuryokuHojoFinder.createInstance().getHokensha(parameter);
         if (ent == null) {
@@ -43,20 +45,13 @@ public class HokenshaJohoHandler {
     public ResponseData<HokenshaJohoDiv> on保険者検索(HokenshaJohoDiv div) {
         ResponseData<HokenshaJohoDiv> responseData = new ResponseData<>();
         if (div.getTxtHokenshaNo().getValue().isEmpty()) {
+            Association association = AssociationFinderFactory.createInstance().getAssociation();
+            kenCode = association.get市町村名();
             kenCode = new RString("");
         } else {
             kenCode = new RString(div.getTxtHokenshaNo().getValue().toString().substring(0, 2));
         }
-        HokenjaMapperParameter parameter = HokenjaMapperParameter.createKenCodeParam(kenCode);
-        List<Hokensha> hokenjaList = HokenshaNyuryokuHojoFinder.createInstance().getHokenshaList(parameter);
-//        List<DataGrid1_Row> list = new ArrayList<>();
-        DataGrid1_Row row = new DataGrid1_Row();
-        for (Hokensha guide : hokenjaList) {
-//            row.setDefaultDataName0(guide.get保険者番号());
-//            row.setDefaultDataName1(guide.get保険者名());
-        }
-//        list.add(row);
-        //div.getDataGrid1().setDataSource(list);
+        set保険者(div, kenCode);
         List<KenCodeJigyoshaInputGuide> KenCodeList = HokenshaNyuryokuHojoFinder.createInstance().getKenCodeJigyoshaInputGuide();
         DropDownList dropdownlist = new DropDownList();
         for (KenCodeJigyoshaInputGuide guide : KenCodeList) {
@@ -69,16 +64,7 @@ public class HokenshaJohoHandler {
     public ResponseData<HokenshaJohoDiv> on保険者を表示する(HokenshaJohoDiv div) {
         ResponseData<HokenshaJohoDiv> responseData = new ResponseData<>();
 //        kenCode = div.getDropDownList().getValue();
-        HokenjaMapperParameter parameter = HokenjaMapperParameter.createKenCodeParam(kenCode);
-        List<Hokensha> hokenjaList = HokenshaNyuryokuHojoFinder.createInstance().getHokenshaList(parameter);
-//        List<DataGrid1_Row> list = new ArrayList<>();
-        DataGrid1_Row row = new DataGrid1_Row();
-        for (Hokensha guide : hokenjaList) {
-//            row.setDefaultDataName0(guide.get保険者番号());
-//            row.setDefaultDataName1(guide.get保険者名());
-        }
-//        list.add(row);
-        //div.getDataGrid1().setDataSource(list);
+        set保険者(div, kenCode);
         return responseData;
     }
 
@@ -86,6 +72,19 @@ public class HokenshaJohoHandler {
         ResponseData<HokenshaJohoDiv> responseData = new ResponseData<>();
 
         return responseData;
+    }
+
+    private void set保険者(HokenshaJohoDiv div, RString kenCode) {
+        HokenshaMapperParameter parameter = HokenshaMapperParameter.createKenCodeParam(kenCode);
+        List<Hokensha> hokenjaList = HokenshaNyuryokuHojoFinder.createInstance().getHokenshaList(parameter);
+//        List<DataGrid1_Row> list = new ArrayList<>();
+//        DataGrid1_Row row = new DataGrid1_Row();
+        for (Hokensha guide : hokenjaList) {
+//            row.setDefaultDataName0(guide.get保険者番号());
+//            row.setDefaultDataName1(guide.get保険者名());
+        }
+//        list.add(row);
+        //div.getDataGrid1().setDataSource(list);
     }
 
 }
