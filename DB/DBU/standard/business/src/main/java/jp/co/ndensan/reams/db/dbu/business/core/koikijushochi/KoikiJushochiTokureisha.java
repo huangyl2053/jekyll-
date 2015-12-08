9436a40cf3f18b5aa14f343c148eb437b8044d57
lijunchao <lijunchao.@liandisys.com.cn>
2015-12-08 17:30:34 +0800

    DBU 広住特者住民コードSQL取得  改正

--- DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/core/koikijushochi/KoikiJushochiTokureisha.java
+++ DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/core/koikijushochi/KoikiJushochiTokureisha.java
@@ -5,8 +5,7 @@
  */
 package jp.co.ndensan.reams.db.dbu.business.core.koikijushochi;
 
-import jp.co.ndensan.reams.db.dbu.business.loiki.KoikiJushochiTokureishaEnum;
-import jp.co.ndensan.reams.uz.uza.ControlDataHolder;
+import jp.co.ndensan.reams.db.dbx.definition.core.kensakuseigyo.KoikinaiJushochitokureiKensakuSeigyo;
 import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
 import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
@@ -20,75 +19,72 @@ public class KoikiJushochiTokureisha {
 
     /**
      *
-     * @param 市町村コード ShichosonCodeLasdecCode
-     * @return HihokenshaDaicho
+     * @param 市町村コード ShichosonCode
+     *
+     * @return KoikinaiJutokuTaishoshaSql
      */
     public KoikinaiJutokuTaishoshaSql getShikibetsuCodeSql(LasdecCode 市町村コード) {
-        //requireNonNull(市町村コード, UrSystemErrorMessages.値がnull.getReplacedMessage("市町村コード"));
-        SubGyomuCode subGyomuCd = ControlDataHolder.getSubGyomuCD();
-        RString value = BusinessConfig.get(KoikiJushochiTokureishaEnum.広域内住所地特例者検索制御_措置元_措置先区分_介護共通, subGyomuCd.DBU介護統計報告);
 
-        StringBuffer sb1 = new StringBuffer("SELECT DbT1001HihokenshaDaicho.\"shikibetsuCode\"");
+        RString value = BusinessConfig.get(KoikinaiJushochitokureiKensakuSeigyo.広域内住所地特例者検索制御_措置元_措置先区分_介護共通, SubGyomuCode.DBU介護統計報告);
+
+        StringBuffer sb1 = new StringBuffer("SELECT DbT1001HihokenshaDaicho.\"shikibetsuCode\" ");
         sb1.append("FROM rgdb.\"DbT1001HihokenshaDaicho\" DbT1001HihokenshaDaicho,");
         sb1.append("(SELECT HihokenshaDaichoA.\"hihokenshaNo\"");
-        sb1.append(", MAX(HihokenshaDaichoA.\"idoYMD\" || HihokenshaDaichoA.\"edaNo\") AS \"maxEdaMo\"");
-        sb1.append("FROM rgdb.\"DbT1001HihokenshaDaicho\"  HihokenshaDaichoA");
-        sb1.append("WHERE  HihokenshaDaichoA.\"logicalDeletedFlag\"<> TRUE");
-        sb1.append("GROUP BY HihokenshaDaichoA.\"hihokenshaNo\" ) HihokenshaDaichoT");
+        sb1.append(", MAX(HihokenshaDaichoA.\"idoYMD\" || HihokenshaDaichoA.\"edaNo\") AS \"maxEdaMo\" ");
+        sb1.append("FROM rgdb.\"DbT1001HihokenshaDaicho\"  HihokenshaDaichoA ");
+        sb1.append("WHERE  HihokenshaDaichoA.\"logicalDeletedFlag\" <> TRUE ");
+        sb1.append("GROUP BY HihokenshaDaichoA.\"hihokenshaNo\" ) HihokenshaDaichoT ");
         sb1.append("WHERE DbT1001HihokenshaDaicho.\"hihokenshaNo\" = HihokenshaDaichoT.\"hihokenshaNo\" ");
         sb1.append("AND DbT1001HihokenshaDaicho.\"idoYMD\" || DbT1001HihokenshaDaicho.\"edaNo\"=HihokenshaDaichoT.\"maxEdaMo\" ");
         sb1.append("AND DbT1001HihokenshaDaicho.\"koikinaiJushochiTokureiFlag\"= '1' ");
         sb1.append("AND DbT1001HihokenshaDaicho.\"shichosonCode\" = ");
-        sb1.append("666666");
-        //sb1.append(市町村コード.toString());
+        sb1.append(市町村コード.toString());
 
-        StringBuffer sb2 = new StringBuffer("SELECT DbT1001HihokenshaDaicho.\"shikibetsuCode\"");
-        sb2.append("FROM rgdb.\"DbT1001HihokenshaDaicho\" DbT1001HihokenshaDaicho,");
+        StringBuffer sb2 = new StringBuffer("SELECT DbT1001HihokenshaDaicho.\"shikibetsuCode\" ");
+        sb2.append("FROM rgdb.\"DbT1001HihokenshaDaicho\" DbT1001HihokenshaDaicho, ");
         sb2.append("(SELECT HihokenshaDaichoA.\"hihokenshaNo\"");
-        sb2.append(", MAX(HihokenshaDaichoA.\"idoYMD\" || HihokenshaDaichoA.\"edaNo\") AS \"maxEdaMo\"");
-        sb2.append("FROM rgdb.\"DbT1001HihokenshaDaicho\"  HihokenshaDaichoA");
-        sb2.append("WHERE  HihokenshaDaichoA.\"logicalDeletedFlag\"<> TRUE");
-        sb2.append("GROUP BY HihokenshaDaichoA.\"hihokenshaNo\" ) HihokenshaDaichoT");
+        sb2.append(", MAX(HihokenshaDaichoA.\"idoYMD\" || HihokenshaDaichoA.\"edaNo\") AS \"maxEdaMo\" ");
+        sb2.append("FROM rgdb.\"DbT1001HihokenshaDaicho\"  HihokenshaDaichoA ");
+        sb2.append("WHERE  HihokenshaDaichoA.\"logicalDeletedFlag\" <> TRUE ");
+        sb2.append("GROUP BY HihokenshaDaichoA.\"hihokenshaNo\" ) HihokenshaDaichoT ");
         sb2.append("WHERE DbT1001HihokenshaDaicho.\"hihokenshaNo\" = HihokenshaDaichoT.\"hihokenshaNo\" ");
         sb2.append("AND DbT1001HihokenshaDaicho.\"idoYMD\" || DbT1001HihokenshaDaicho.\"edaNo\"=HihokenshaDaichoT.\"maxEdaMo\" ");
         sb2.append("AND DbT1001HihokenshaDaicho.\"koikinaiJushochiTokureiFlag\"= '1' ");
         sb2.append("AND DbT1001HihokenshaDaicho.\"koikinaiTokureiSochimotoShichosonCode\" = ");
-        sb2.append("666666");
-        //sb2.append(市町村コード.toString());
+        sb2.append(市町村コード.toString());
 
-        StringBuffer sb3 = new StringBuffer("SELECT DbT1001HihokenshaDaicho.\"shikibetsuCode\"");
-        sb3.append("FROM rgdb.\"DbT1001HihokenshaDaicho\" DbT1001HihokenshaDaicho,");
-        sb3.append("(SELECT HihokenshaDaichoA.\"hihokenshaNo\"");
-        sb3.append(", MAX(HihokenshaDaichoA.\"idoYMD\" || HihokenshaDaichoA.\"edaNo\") AS \"maxEdaMo\"");
-        sb3.append("FROM rgdb.\"DbT1001HihokenshaDaicho\"  HihokenshaDaichoA");
-        sb3.append("WHERE  HihokenshaDaichoA.\"logicalDeletedFlag\"<> TRUE");
-        sb3.append("GROUP BY HihokenshaDaichoA.\"hihokenshaNo\" ) HihokenshaDaichoT");
+        StringBuffer sb3 = new StringBuffer("SELECT DbT1001HihokenshaDaicho.\"shikibetsuCode\" ");
+        sb3.append("FROM rgdb.\"DbT1001HihokenshaDaicho\" DbT1001HihokenshaDaicho, ");
+        sb3.append("(SELECT HihokenshaDaichoA.\"hihokenshaNo\" ");
+        sb3.append(", MAX(HihokenshaDaichoA.\"idoYMD\" || HihokenshaDaichoA.\"edaNo\") AS \"maxEdaMo\" ");
+        sb3.append("FROM rgdb.\"DbT1001HihokenshaDaicho\"  HihokenshaDaichoA ");
+        sb3.append("WHERE  HihokenshaDaichoA.\"logicalDeletedFlag\" <> TRUE ");
+        sb3.append("GROUP BY HihokenshaDaichoA.\"hihokenshaNo\" ) HihokenshaDaichoT ");
         sb3.append("WHERE DbT1001HihokenshaDaicho.\"hihokenshaNo\" = HihokenshaDaichoT.\"hihokenshaNo\" ");
         sb3.append("AND DbT1001HihokenshaDaicho.\"idoYMD\" || DbT1001HihokenshaDaicho.\"edaNo\"=HihokenshaDaichoT.\"maxEdaMo\" ");
         sb3.append("AND DbT1001HihokenshaDaicho.\"koikinaiJushochiTokureiFlag\"= '1' ");
-        sb3.append("AND DbT1001HihokenshaDaicho.\"koikinaiTokureiSochimotoShichosonCode\" = ");
-        sb3.append("666666");
-        // sb3.append(市町村コード.toString());
-        sb3.append("OR  DbT1001HihokenshaDaicho.\"shichosonCode\" =  ");
-        sb3.append("666666");
-        // sb3.append(市町村コード.toString());
+        sb3.append("AND (DbT1001HihokenshaDaicho.\"koikinaiTokureiSochimotoShichosonCode\" = ");
+        sb3.append(市町村コード.toString());
+        sb3.append(" OR DbT1001HihokenshaDaicho.\"shichosonCode\" = ");
+        sb3.append(市町村コード.toString());
+        sb3.append(")");
 
         KoikinaiJutokuTaishoshaSql koikinaiJutokuTaishoshaSql = new KoikinaiJutokuTaishoshaSql();
 
-        if ("1".equals(value)) {
+        if (new RString("1").equals(value)) {
 
             koikinaiJutokuTaishoshaSql.set対象者SQL1(new RString(sb1.toString()));
             koikinaiJutokuTaishoshaSql.set対象者SQL2(new RString(sb2.toString()));
 
-        } else if ("2".equals(value)) {
+        } else if (new RString("2").equals(value)) {
 
             koikinaiJutokuTaishoshaSql.set対象者SQL1(new RString(sb1.toString()));
             koikinaiJutokuTaishoshaSql.set対象者SQL2(new RString(sb1.toString()));
 
-        } else if ("3".equals(value)) {
+        } else if (new RString("3").equals(value)) {
 
             koikinaiJutokuTaishoshaSql.set対象者SQL1(new RString(sb1.toString()));
-            koikinaiJutokuTaishoshaSql.set対象者SQL1(new RString(sb3.toString()));
+            koikinaiJutokuTaishoshaSql.set対象者SQL2(new RString(sb3.toString()));
         }
 
         return koikinaiJutokuTaishoshaSql;
--- DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/core/koikijushochi/KoikinaiJutokuTaishoshaSql.java
+++ DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/core/koikijushochi/KoikinaiJutokuTaishoshaSql.java
@@ -17,46 +17,10 @@ public class KoikinaiJutokuTaishoshaSql {
 
     private RString 対象者SQL2;
 
-    private RString sql1 = new RString("SELECT DbT1001HihokenshaDaicho.\"shikibetsuCode\" "
-            + "FROM rgdb.\"DbT1001HihokenshaDaicho\"  DbT1001HihokenshaDaicho\n"
-            + ",(SELECT HihokenshaDaichoA.\"hihokenshaNo\"\n"
-            + ", MAX(HihokenshaDaichoA.\"idoYMD\" || HihokenshaDaichoA.\"edaNo\") AS \"maxEdaMo\" \n"
-            + "FROM  rgdb.\"DbT1001HihokenshaDaicho\"  HihokenshaDaichoA \n"
-            + "WHERE  HihokenshaDaichoA.\"logicalDeletedFlag\"<> TRUE \n"
-            + "GROUP BY  HihokenshaDaichoA.\"hihokenshaNo\" ) HihokenshaDaichoT \n"
-            + "WHERE  DbT1001HihokenshaDaicho.\"hihokenshaNo\" =  HihokenshaDaichoT.\"hihokenshaNo\" \n"
-            + "  AND DbT1001HihokenshaDaicho.\"idoYMD\" \n"
-            + "  || DbT1001HihokenshaDaicho.\"edaNo\" =  HihokenshaDaichoT.\"maxEdaMo\" \n"
-            + "  AND DbT1001HihokenshaDaicho.\"koikinaiJushochiTokureiFlag\" = '1'"
-            + "AND  DbT1001HihokenshaDaicho.\"shichosonCode\" =666666");
-    private RString sql2 = new RString("SELECT DbT1001HihokenshaDaicho.\"shikibetsuCode\" "
-            + "FROM rgdb.\"DbT1001HihokenshaDaicho\"  DbT1001HihokenshaDaicho\n"
-            + ",(SELECT HihokenshaDaichoA.\"hihokenshaNo\"\n"
-            + ", MAX(HihokenshaDaichoA.\"idoYMD\" || HihokenshaDaichoA.\"edaNo\") AS \"maxEdaMo\" \n"
-            + "FROM  rgdb.\"DbT1001HihokenshaDaicho\"  HihokenshaDaichoA \n"
-            + "WHERE  HihokenshaDaichoA.\"logicalDeletedFlag\"<> TRUE \n"
-            + "GROUP BY  HihokenshaDaichoA.\"hihokenshaNo\" ) HihokenshaDaichoT \n"
-            + "WHERE  DbT1001HihokenshaDaicho.\"hihokenshaNo\" =  HihokenshaDaichoT.\"hihokenshaNo\" \n"
-            + "  AND DbT1001HihokenshaDaicho.\"idoYMD\" \n"
-            + "  || DbT1001HihokenshaDaicho.\"edaNo\" =  HihokenshaDaichoT.\"maxEdaMo\" \n"
-            + "  AND DbT1001HihokenshaDaicho.\"koikinaiJushochiTokureiFlag\" = '1'"
-            + "AND  DbT1001HihokenshaDaicho.\"koikinaiTokureiSochimotoShichosonCode\" =666666");
-    private RString sql3 = new RString("SELECT DbT1001HihokenshaDaicho.\"shikibetsuCode\" "
-            + "FROM rgdb.\"DbT1001HihokenshaDaicho\""
-            + ",(SELECT HihokenshaDaichoA.\"hihokenshaNo\""
-            + ", MAX(HihokenshaDaichoA.\"idoYMD\" || HihokenshaDaichoA.\"edaNo\") AS \"maxEdaMo\"\n"
-            + "FROM rgdb.\"DbT1001HihokenshaDaicho\"  HihokenshaDaichoA "
-            + " WHERE  HihokenshaDaichoA.\"logicalDeletedFlag\"<> TRUE\n"
-            + "GROUP BY HihokenshaDaichoA.\"hihokenshaNo\" ) HihokenshaDaichoT  "
-            + "WHERE DbT1001HihokenshaDaicho.\"hihokenshaNo\" = HihokenshaDaichoT.\"hihokenshaNo\" \n"
-            + "AND DbT1001HihokenshaDaicho.\"idoYMD\" || "
-            + "DbT1001HihokenshaDaicho.\"edaNo\"=HihokenshaDaichoT.\"maxEdaMo\"\n"
-            + "AND DbT1001HihokenshaDaicho.\"koikinaiJushochiTokureiFlag\"= '1'"
-            + "AND  DbT1001HihokenshaDaicho.\"koikinaiTokureiSochimotoShichosonCode\" =666666 "
-            + "OR  DbT1001HihokenshaDaicho.\"shichosonCode\" =66666");
-
     /**
-     * @return the 対象者SQL1
+     * 対象者SQL1を返します
+     *
+     * @return 対象者SQL1
      */
     public RString get対象者SQL1() {
         return 対象者SQL1;
@@ -70,7 +34,9 @@ public class KoikinaiJutokuTaishoshaSql {
     }
 
     /**
-     * @return the 対象者SQL2
+     * 対象者SQL2を返します
+     *
+     * @return 対象者SQL2
      */
     public RString get対象者SQL2() {
         return 対象者SQL2;
@@ -82,47 +48,4 @@ public class KoikinaiJutokuTaishoshaSql {
     public void set対象者SQL2(RString 対象者SQL2) {
         this.対象者SQL2 = 対象者SQL2;
     }
-
-    /**
-     * @return the sql2
-     */
-    public RString getSql2() {
-        return sql2;
-    }
-
-    /**
-     * @param sql2 the sql2 to set
-     */
-    public void setSql2(RString sql2) {
-        this.sql2 = sql2;
-    }
-
-    /**
-     * @return the sql3
-     */
-    public RString getSql3() {
-        return sql3;
-    }
-
-    /**
-     * @param sql3 the sql3 to set
-     */
-    public void setSql3(RString sql3) {
-        this.sql3 = sql3;
-    }
-
-    /**
-     * @return the sql1
-     */
-    public RString getSql1() {
-        return sql1;
-    }
-
-    /**
-     * @param sql1 the sql1 to set
-     */
-    public void setSql1(RString sql1) {
-        this.sql1 = sql1;
-    }
-
 }
