ca5a61a51ffea69bfd925cf1c1ce80a7d53e7eb6
n8300姜井図 <n8300@ndensan.co.jp>
2015-12-10 11:38:46 +0900

    介護画面レイアウト修正 DBA4010012_被保険者資格詳細

---
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/parentdiv/DBA4010012/DBA4010012StateName.java
@@ -0,0 +1,36 @@
+package jp.co.ndensan.reams.db.dba.divcontroller.entity.parentdiv.DBA4010012;
+
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+
+/**
+ * このコードはツールによって生成されました。</br>
+ * このファイルへの変更は、再生成時には損失するため</br>
+ * 不正な動作の原因になります。</br>
+ * DBA4010012の状態名定義クラスです
+ *
+ * @author 自動生成
+ */
+public enum DBA4010012StateName implements IStateEnumerations {
+// <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
+
+    NoChange(""),
+    初期状態("初期状態");
+
+    private final RString name;
+
+    private DBA4010012StateName(String name) {
+        this.name = new RString(name);
+    }
+
+    /**
+     * getName
+     *
+     * @return 名前
+     */
+    @Override
+    public RString getName() {
+        return name;
+    }
+// </editor-fold>
+}
