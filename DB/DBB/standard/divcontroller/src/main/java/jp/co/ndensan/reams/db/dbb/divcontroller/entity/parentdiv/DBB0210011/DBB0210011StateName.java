94a1e717c668a2c71df3829760a6a7640157910a
三浦 凌 <n3327@ndensan.co.jp>
2015-12-15 19:33:59 +0900

    画面レイアウトを修正 ・特徴仮算定賦課 ・特別徴収平準化計算確定 ・介護保険料月別推移表作成

--- DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB0210011/DBB0210011StateName.java
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB0210011/DBB0210011StateName.java
@@ -1,4 +1,4 @@
-package jp.co.ndensan.reams.db.dbb.divcontroller.entity.parentdiv.DBB0130002;
+package jp.co.ndensan.reams.db.dbb.divcontroller.entity.parentdiv.DBB0210011;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
@@ -7,20 +7,19 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBB0130002の状態名定義クラスです
+ * DBB0210011の状態名定義クラスです
  *
  * @author 自動生成
  */
-public enum DBB0130002StateName implements IStateEnumerations {
+public enum DBB0210011StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
     NoChange(""),
-    平準化確定リアル("平準化確定リアル"),
-    平準化確定バッチ("平準化確定バッチ");
+    Default("default");
 
     private final RString name;
 
-    private DBB0130002StateName(String name) {
+    private DBB0210011StateName(String name) {
         this.name = new RString(name);
     }
 
