04fd614c0d8349c077f14a20d6adb99893df71d1
houtianpeng <houtianpeng@liandisys.com.cn>
2015-12-15 23:29:06 +0800

    ソース修正

--- DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/commonchilddiv/shisetsuJohoCommonChildDiv/IShisetsuJohoCommonChildDiv.java
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/commonchilddiv/shisetsuJohoCommonChildDiv/IShisetsuJohoCommonChildDiv.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dba.divcontroller.entity.commonchilddiv.ShisetsuJ
 import jp.co.ndensan.reams.uz.uza.ui.binding.ICommonChildDivBaseProperties;
 
 /*
- * このコードはツールによって生成されました。
+ * このコードはツールによって生成されました。 
  */
 public interface IShisetsuJohoCommonChildDiv extends ICommonChildDivBaseProperties {
 
