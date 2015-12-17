aa9073efc5d175c870eec9c56ad3ead2e2cf4e6c
lizhuoxuan <lizhuoxuan@liandisys.com.cn>
2015-12-17 20:52:22 +0800

    証回収状記録管理を修正

--- DB/DBA/standard/service/src/main/java/jp/co/ndensan/reams/db/dba/service/core/shoKofuKaishuJoho/ShoKofuKaishuJohoManager.java
+++ DB/DBA/standard/service/src/main/java/jp/co/ndensan/reams/db/dba/service/core/shoKofuKaishuJoho/ShoKofuKaishuJohoManager.java
@@ -5,16 +5,20 @@
  */
 package jp.co.ndensan.reams.db.dba.service.core.shoKofuKaishuJoho;
 
+import jp.co.ndensan.reams.db.dba.persistence.mapper.shoKofuKaishuJoho.ShoKofuKaishuJohoMapper;
 import java.util.ArrayList;
 import java.util.List;
 import jp.co.ndensan.reams.db.dba.business.ShoKofuKaishuJohoModel;
 import jp.co.ndensan.reams.db.dba.definition.mybatis.param.shokofukaishujoho.ShoKofuKaishuJohoParameter;
+import jp.co.ndensan.reams.db.dba.entity.ShoKofuKaishuJohoEntity;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
 import jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT7037ShoKofuKaishuEntity;
 import jp.co.ndensan.reams.db.dbz.persistence.db.basic.DbT7037ShoKofuKaishuDac;
+import jp.co.ndensan.reams.db.dbz.service.core.MapperProvider;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
 import jp.co.ndensan.reams.uz.uza.lang.RDateTime;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.util.db.SearchResult;
 import jp.co.ndensan.reams.uz.uza.util.di.InstanceProvider;
 import jp.co.ndensan.reams.uz.uza.util.di.Transaction;
 
@@ -22,25 +26,41 @@ import jp.co.ndensan.reams.uz.uza.util.di.Transaction;
  *
  * 証交付回収情報の取得処理です。
  */
-public class ShoKofuKaishuJohoFinder {
+public class ShoKofuKaishuJohoManager {
 
+    private final MapperProvider mapperProvider;
     private final DbT7037ShoKofuKaishuDac dac;
 
-    /**
+     /**
      * コンストラクタ
-     *
      */
-    public ShoKofuKaishuJohoFinder() {
-        this.dac = InstanceProvider.create(DbT7037ShoKofuKaishuDac.class);
+    ShoKofuKaishuJohoManager(){
+    this.mapperProvider = InstanceProvider.create(MapperProvider.class);
+    this.dac = InstanceProvider.create(DbT7037ShoKofuKaishuDac.class);
+    }
+    
+     /**
+     * 単体テスト用のコンストラクタです。
+     *
+     * @param mapperProvider mapperProvider
+     * @param dac dac
+     */
+    ShoKofuKaishuJohoManager(
+            MapperProvider mapperProvider,
+            DbT7037ShoKofuKaishuDac dac
+    ) {
+        this.mapperProvider = mapperProvider;
+        this.dac = dac;
     }
 
-    /**
-     * クラスをcreateメソッドです。
+     /**
+     * {@link InstanceProvider#create}にて生成した{@link ShoKofuKaishuJohoManager}のインスタンスを返します。
      *
-     * @return 証交付回収情報の取得処理するクラス
+     * @return
+     * {@link InstanceProvider#create}にて生成した{@link ShoKofuKaishuJohoManager}のインスタンス
      */
-    public static ShoKofuKaishuJohoFinder createInstance() {
-        return InstanceProvider.create(ShoKofuKaishuJohoFinder.class);
+    public static ShoKofuKaishuJohoManager createInstance() {
+        return InstanceProvider.create(ShoKofuKaishuJohoManager.class);
     }
 
     /**
@@ -48,16 +68,40 @@ public class ShoKofuKaishuJohoFinder {
      *
      * @param 被保険者番号 HihokenshaNo
      * @param 論理削除フラグ boolean
-     * @return List<ShoKofuKaishuJohoModel>
+     * @return SearchResult<ShoKofuKaishuJohoModel>
      */
-    public List<ShoKofuKaishuJohoModel> getShoKofuKaishuJohoList(HihokenshaNo 被保険者番号, boolean 論理削除フラグ) {
+    public SearchResult<ShoKofuKaishuJohoModel> getShoKofuKaishuJohoList(HihokenshaNo 被保険者番号, boolean 論理削除フラグ) {
         List<ShoKofuKaishuJohoModel> businessList = new ArrayList<>();
         List<DbT7037ShoKofuKaishuEntity> dbT7037ShoKofuKaishuEntity = dac.select一覧取得(被保険者番号, 論理削除フラグ);
         for (DbT7037ShoKofuKaishuEntity fufukuMoshitateEntity : dbT7037ShoKofuKaishuEntity) {
             businessList.add(new ShoKofuKaishuJohoModel(fufukuMoshitateEntity));
         }
 
-        return businessList;
+        return SearchResult.of(businessList, 0, false);
+    }
+    
+     /**
+     * 証交付回収の更新処理します。
+     *
+     * @param model
+     * @return count
+     */
+    @Transaction
+    public int updShoKofuKaishuJoho(ShoKofuKaishuJohoModel model) {
+        ShoKofuKaishuJohoMapper mapper = mapperProvider.create(ShoKofuKaishuJohoMapper.class);
+        ShoKofuKaishuJohoEntity entity = new ShoKofuKaishuJohoEntity();
+        entity.setHihokenshaNo(model.get被保険者番号());
+        entity.setHakkoShoriTimestamp(model.get処理日時());
+        entity.setKaishuJiyu(model.get回収事由コード());
+        entity.setKaishuRiyu(model.get回収理由());
+        entity.setKaishuYMD(model.get回収年月日());
+        entity.setKofuJiyu(model.get交付事由コード());
+        entity.setKofuRiyu(model.get交付理由());
+        entity.setKofuShoShurui(model.get交付証種類コード());
+        entity.setKofuYMD(model.get交付年月日());
+        entity.setYukoKigenYMD(model.get有効期限());
+        int count = mapper.updShoKofuKaishuJoho(entity);
+        return count;
     }
 
     /**
