4d047888a5a7d2031533539e1476847d46c66d39
wanghui <wanghui@liandisys.com.cn>
2015-12-17 14:50:47 +0800

    資格得喪履歴

--- DB/DBA/standard/service/src/main/java/jp/co/ndensan/reams/db/dba/service/shikakutokuso/ShikakuTokusoFinder.java
+++ DB/DBA/standard/service/src/main/java/jp/co/ndensan/reams/db/dba/service/shikakutokuso/ShikakuTokusoFinder.java
@@ -8,6 +8,7 @@ package jp.co.ndensan.reams.db.dba.service.shikakutokuso;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import jp.co.ndensan.reams.db.dba.business.core.shikakutokuso.ShikakuTokuSoa;
 import jp.co.ndensan.reams.db.dba.business.core.shikakutokuso.ShikakuTokuso;
 import jp.co.ndensan.reams.db.dba.definition.shikakutokuso.ShikakuTokusoParameter;
 import jp.co.ndensan.reams.db.dba.entity.db.relate.shikakutoku.shikakutokuso.ServiceShikakuRelateEntity;
@@ -16,8 +17,12 @@ import jp.co.ndensan.reams.db.dba.persistence.db.basic.shikakutokuso.IShikakuTok
 import jp.co.ndensan.reams.db.dbz.persistence.db.basic.DbT1001HihokenshaDaichoDac;
 import jp.co.ndensan.reams.db.dbz.service.core.MapperProvider;
 import jp.co.ndensan.reams.ur.urz.definition.message.UrErrorMessages;
+import jp.co.ndensan.reams.uz.uza.biz.CodeShubetsu;
+import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
 import jp.co.ndensan.reams.uz.uza.lang.ApplicationException;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
+import jp.co.ndensan.reams.uz.uza.util.code.CodeMaster;
+import jp.co.ndensan.reams.uz.uza.util.code.entity.UzT0007CodeEntity;
 import jp.co.ndensan.reams.uz.uza.util.db.SearchResult;
 import jp.co.ndensan.reams.uz.uza.util.di.InstanceProvider;
 
@@ -29,6 +34,8 @@ public class ShikakuTokusoFinder {
 
     private final MapperProvider mapperProvider;
     private final DbT1001HihokenshaDaichoDac dbT1001Dac;
+    private final CodeShubetsu CHIKU_CODE_SHUBETSU = new CodeShubetsu("0117");
+    private final CodeShubetsu CHIKU_CODE_SHUBETSu = new CodeShubetsu("0117");
 
     /**
      * コンストラクタです。
@@ -86,14 +93,21 @@ public class ShikakuTokusoFinder {
      * @param 住所地特例適用事由コード 住所地特例適用事由コード
      * @return List<ShikakuTokuso>コード種別　=　'0117'取得取得リスト
      *///TODD:QA未回
-//    public SearchResult<ShikakuTokuso> getShutokuJiyuDDL(RString 住所地特例適用事由コード) {
-//        List<ShikakuTokuso> serviceShuruiList = new ArrayList<>();
-//        List<DbT1001HihokenshaDaichoEntity> list = dbT1001Dac.selectByShutokuJiyuDDL(住所地特例適用事由コード);
-//        if (list == null || list.isEmpty()) {
-//            return SearchResult.of(Collections.<ShikakuTokuso>emptyList(), 0, false);
-//        }
-//        return SearchResult.of(serviceShuruiList, 0, false);
-//    }
+    public SearchResult<ShikakuTokuSoa> getShutokuJiyuDDL() {
+        List<UzT0007CodeEntity> codeList = CodeMaster.getCodeRireki(SubGyomuCode.DBA介護資格, CHIKU_CODE_SHUBETSU);
+        List<ShikakuTokuSoa> serviceShuruiList = new ArrayList<>();
+        if (codeList == null || codeList.isEmpty()) {
+            return SearchResult.of(Collections.<ShikakuTokuso>emptyList(), 0, false);
+        }
+        for (UzT0007CodeEntity entity : codeList) {
+            ShikakuTokuSoa List = new ShikakuTokuSoa();
+            List.setCode(entity.getコード());
+            List.setCodeRyakusho(entity.getコード略称());
+            serviceShuruiList.add(List);
+        }
+        return SearchResult.of(serviceShuruiList, 0, false);
+    }
+
     /**
      * コードマスタから喪失事由ドロップダウンリストに表示する用のデータを取得する。
      *
@@ -101,13 +115,18 @@ public class ShikakuTokusoFinder {
      * @return List<ShikakuTokuso>コード種別　=　'0121'取得取得リスト
      */
     //TODD:QA未回
-//    public SearchResult<ShikakuTokuso> getSoshitsuJiyuDDL(RString 住所地特例解除事由コード) {
-//        List<ShikakuTokuso> serviceShuruiList = new ArrayList<>();
-//        List<DbT1001HihokenshaDaichoEntity> entity = dbT1001Dac.selectBySoshitsuJiyuDDL(住所地特例解除事由コード);
-//        if (entity.isEmpty()) {
-//            return SearchResult.of(Collections.<ShikakuTokuso>emptyList(), 0, false);
-//        }
-//
-//        return SearchResult.of(serviceShuruiList, 0, false);
-//    }
+    public SearchResult<ShikakuTokuSoa> getSoshitsuJiyuDDL() {
+        List<UzT0007CodeEntity> codeList = CodeMaster.getCodeRireki(SubGyomuCode.DBA介護資格, CHIKU_CODE_SHUBETSu);
+        List<ShikakuTokuSoa> serviceShuruiList = new ArrayList<>();
+        if (codeList.isEmpty()) {
+            return SearchResult.of(Collections.<ShikakuTokuso>emptyList(), 0, false);
+        }
+        for (UzT0007CodeEntity entity : codeList) {
+            ShikakuTokuSoa List = new ShikakuTokuSoa();
+            List.setCode(entity.getコード());
+            List.setCodeRyakusho(entity.getコード略称());
+            serviceShuruiList.add(List);
+        }
+        return SearchResult.of(serviceShuruiList, 0, false);
+    }
 }
