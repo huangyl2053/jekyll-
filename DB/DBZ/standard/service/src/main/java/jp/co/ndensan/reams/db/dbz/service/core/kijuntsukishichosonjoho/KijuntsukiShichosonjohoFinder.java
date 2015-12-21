2647c6639f3e7923de6c7217ab6ccd6d70031d65
lijunchao <lijunchao@liandisys.com.cn>
2015-12-21 19:33:07 +0800

    DBU 基準月市町村情報取得  修正

--- DB/DBZ/standard/service/src/main/java/jp/co/ndensan/reams/db/dbz/service/core/kijuntsukishichosonjoho/KijuntsukiShichosonjohoFinder.java
+++ DB/DBZ/standard/service/src/main/java/jp/co/ndensan/reams/db/dbz/service/core/kijuntsukishichosonjoho/KijuntsukiShichosonjohoFinder.java
@@ -35,12 +35,13 @@ public class KijuntsukiShichosonjohoFinder {
     private final MapperProvider mapperProvider;
     private static final RString 合併なし = new RString("0");
     private static final RString 合併あり = new RString("1");
+    private static final RString 日 = new RString("01");
     private static final RString 事務構成市町村 = new RString("112");
     private static final RString 事務単一 = new RString("120");
     private static final RString 認定単一 = new RString("220");
     private static final RString 事務広域 = new RString("111");
     private static final RString 認定広域 = new RString("211");
-    private static RString 内部開始日フラグ = new RString("1");
+    private static RString 内部開始日フラグ = RString.EMPTY;
 
     /**
      * コンストラクタです。
@@ -83,18 +84,19 @@ public class KijuntsukiShichosonjohoFinder {
             } else if (事務広域.equals(導入形態コード) || 認定広域.equals(導入形態コード)) {
                 SearchResult<KoikiZenShichosonJoho> searchResult = KoikiShichosonJohoFinder.createInstance().koseiShichosonJoho();
                 // TODO 市町村情報取得_広域
-                return null;
             }
         } else if (合併あり.equals(gappeijohokubun)) {
             if (事務構成市町村.equals(導入形態コード) || 事務単一.equals(導入形態コード)
                     || 認定単一.equals(導入形態コード)) {
                 KijuntsukiShichosonjohMapperParameter parameter = KijuntsukiShichosonjohMapperParameter.yearMonthcreateKaokuParam(基準年月);
-                get新合併市町村の取得(parameter);
-                return koseiShiList;
+                List<IKoseiShichosonMaster> 新合併市町村 = get新合併市町村の取得(parameter);
+                koseiShiList.addAll(新合併市町村);
             } else if (事務広域.equals(導入形態コード) || 認定広域.equals(導入形態コード)) {
                 KijuntsukiShichosonjohMapperParameter parameter = KijuntsukiShichosonjohMapperParameter.yearMonthcreateKaokuParam(基準年月);
-                get広域の旧合併市町村情報(parameter);
-                get最新の広域構成市町村(parameter);
+                List<IKoseiShichosonMaster> 広域の旧合併市町村情報 = get広域の旧合併市町村情報(parameter);
+                List<IKoseiShichosonMaster> 最新の広域構成市町村 = get最新の広域構成市町村(parameter);
+                koseiShiList.addAll(広域の旧合併市町村情報);
+                koseiShiList.addAll(最新の広域構成市町村);
             }
         }
         return koseiShiList;
@@ -108,15 +110,16 @@ public class KijuntsukiShichosonjohoFinder {
      */
     public List<IKoseiShichosonMaster> get新合併市町村の取得(KijuntsukiShichosonjohMapperParameter 基準年月) {
         requireNonNull(基準年月, UrSystemErrorMessages.値がnull.getReplacedMessage("基準年月"));
+        List<IKoseiShichosonMaster> koseiShiList = new ArrayList<>();
         IKijuntsukiShichosonjohoMapper mapper = mapperProvider.create(IKijuntsukiShichosonjohoMapper.class);
         DbT7055GappeiJohoEntity dbt7055Entity = mapper.get新合併市町村の取得(基準年月);
         if (dbt7055Entity != null) {
-            List<IKoseiShichosonMaster> koseiShiList = get単一全合併市町村情報(基準年月);
-            return koseiShiList;
+            List<IKoseiShichosonMaster> 単一全合併市町村情報 = get単一全合併市町村情報(基準年月);
+            koseiShiList.addAll(単一全合併市町村情報);
         }
         //TODO 取得した結果NULL以外場合
         //TODO 市町村情報取得_単一
-        return null;
+        return koseiShiList;
     }
 
     /**
@@ -130,23 +133,22 @@ public class KijuntsukiShichosonjohoFinder {
         List<IKoseiShichosonMaster> koseiShiList = new ArrayList<>();
         IKijuntsukiShichosonjohoMapper mapper = mapperProvider.create(IKijuntsukiShichosonjohoMapper.class);
         List<GappeiShichosonEntity> entityList = mapper.get旧市町村情報を取得(基準年月);
-        if (entityList != null) {
+        if (!entityList.isEmpty()) {
             for (GappeiShichosonEntity entity : entityList) {
-                if (entity.getGappeiYMD().getDayValue() != 1) {
-                    if (entity.getKokuhorenDataFromYMD().getYearMonth() == entity.getGappeiYMD().getYearMonth()
-                            && entity.getUnyoKaishiYMD().getYearMonth() == 基準年月.get基準年月().getYearMonth()) {
+                if (!日.equals(entity.getGappeiYMD().getDayValue())) {
+                    if (entity.getKokuhorenDataFromYMD().getYearMonth().equals(entity.getGappeiYMD().getYearMonth())
+                            && entity.getUnyoShuryoYMD().getYearMonth().equals(基準年月.get基準年月().getYearMonth())) {
                         continue;
                     } else if (entity.getKokuhorenDataFromYMD().getYearMonth().isBefore(entity.getGappeiYMD().getYearMonth())
-                            && entity.getUnyoKaishiYMD().getYearMonth() == 基準年月.get基準年月().getYearMonth()) {
+                            && entity.getUnyoShuryoYMD().getYearMonth().equals(基準年月.get基準年月().getYearMonth())) {
                         koseiShiList.add(new GappeiShichoson(entity));
                         内部開始日フラグ = new RString("2");
                         continue;
                     }
                 }
-                if (new RString("2") == 内部開始日フラグ && entity.getUnyoShuryoYMD().getDayValue() != 1) {
-                    内部開始日フラグ = null;
-                    if (entity.getUnyoKaishiYMD().getYearMonth() == 基準年月.get基準年月().getYearMonth()) {
-                        continue;
+                if (new RString("2") == 内部開始日フラグ && !日.equals(entity.getUnyoKaishiYMD().getDayValue())) {
+                    内部開始日フラグ = RString.EMPTY;
+                    if (entity.getUnyoKaishiYMD().getYearMonth().equals(基準年月.get基準年月().getYearMonth())) {
                         // TODO QA-234
                     }
                 }
@@ -166,14 +168,14 @@ public class KijuntsukiShichosonjohoFinder {
         List<IKoseiShichosonMaster> koseiShiList = new ArrayList<>();
         IKijuntsukiShichosonjohoMapper mapper = mapperProvider.create(IKijuntsukiShichosonjohoMapper.class);
         List<KoseiShichosonMasterEntity> koseiList = mapper.get広域の旧合併市町村情報を取得(基準年月);
-        if (koseiList.isEmpty()) {
+        if (!koseiList.isEmpty()) {
             for (KoseiShichosonMasterEntity entity : koseiList) {
-                if (entity.getGappeiYMD().getDayValue() != 1) {
-                    if (entity.getKokuhorenDataFromYMD().getYearMonth() == entity.getGappeiYMD().getYearMonth()
-                            && entity.getRidatsuYMD().getYearMonth() == 基準年月.get基準年月().getYearMonth()) {
+                if (!日.equals(entity.getGappeiYMD().getDayValue())) {
+                    if (entity.getKokuhorenDataFromYMD().getYearMonth().equals(entity.getGappeiYMD().getYearMonth())
+                            && entity.getRidatsuYMD().getYearMonth().equals(基準年月.get基準年月().getYearMonth())) {
                         continue;
                     } else if (entity.getKokuhorenDataFromYMD().getYearMonth().isBefore(entity.getGappeiYMD().getYearMonth())
-                            && entity.getKanyuYMD().getYearMonth() == 基準年月.get基準年月().getYearMonth()) {
+                            && entity.getKanyuYMD().getYearMonth().equals(基準年月.get基準年月().getYearMonth())) {
                         continue;
                     }
                 }
@@ -194,11 +196,11 @@ public class KijuntsukiShichosonjohoFinder {
         List<IKoseiShichosonMaster> koseiShiList = new ArrayList<>();
         IKijuntsukiShichosonjohoMapper mapper = mapperProvider.create(IKijuntsukiShichosonjohoMapper.class);
         List<KoseiShichosonMasterEntity> koseishiList = mapper.get最新の広域構成市町村の取得(基準年月);
-        if (koseishiList == null) {
+        if (!koseishiList.isEmpty()) {
             for (KoseiShichosonMasterEntity entity : koseishiList) {
-                if (entity.getGappeiYMD().getDayValue() != 1) {
+                if (!日.equals(entity.getGappeiYMD().getDayValue())) {
                     if (entity.getKokuhorenDataFromYMD().getYearMonth().isBefore(entity.getGappeiYMD().getYearMonth())
-                            && entity.getKanyuYMD().getYearMonth() == 基準年月.get基準年月().getYearMonth()) {
+                            && entity.getKanyuYMD().getYearMonth().equals(基準年月.get基準年月().getYearMonth())) {
                         continue;
                     }
                 }
