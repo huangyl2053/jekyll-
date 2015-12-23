f26cad3abd0ca0238b9c07fd59f1caa587519ea2
zuotao <zuotao@liandisys.com.cn>
2015-12-23 14:08:05 +0800

    メソッドcreateProcessParameter削除

--- DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/flow/ikenshoShujiiIchiran/IkenshoShujiiIchiranFlow.java
+++ DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/flow/ikenshoShujiiIchiran/IkenshoShujiiIchiranFlow.java
@@ -5,14 +5,11 @@
  */
 package jp.co.ndensan.reams.db.dbe.batchcontroller.flow.ikenshoShujiiIchiran;
 
-import java.util.HashMap;
-import java.util.Map;
 import jp.co.ndensan.reams.db.dbe.batchcontroller.step.ikenshoShujiiIchiran.IkenshoShujiiIchiranProcess;
 import jp.co.ndensan.reams.db.dbe.definition.batchprm.ikenshoshujiiichiran.IkenshoShujiiIchiranParameter;
 import jp.co.ndensan.reams.uz.uza.batch.Step;
 import jp.co.ndensan.reams.uz.uza.batch.flow.BatchFlowBase;
 import jp.co.ndensan.reams.uz.uza.batch.flow.IBatchFlowCommand;
-import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
  * 医療機関・主治医一覧表_バッチフロークラスです
@@ -27,15 +24,9 @@ public class IkenshoShujiiIchiranFlow extends BatchFlowBase<IkenshoShujiiIchiran
     private static final String REPORT_PROCESS = "reportProcess"; //@Stepの定数はメソッドの近くに置くと見やすくていい。
 
     @Step(REPORT_PROCESS)
-    private IBatchFlowCommand reportProcess() {
+    protected IBatchFlowCommand reportProcess() {
         return loopBatch(IkenshoShujiiIchiranProcess.class)
                 .arguments(getParameter().to主治医一覧情報プロセスparameter())
                 .define();
-    }
-
-    private Map<RString, Object> createProcessParameter() {
-        Map<RString, Object> processParameter = new HashMap<>();
-        processParameter.put(new RString("parameterClass"), getParameter().to主治医一覧情報プロセスparameter());
-        return processParameter;
     }
 }
