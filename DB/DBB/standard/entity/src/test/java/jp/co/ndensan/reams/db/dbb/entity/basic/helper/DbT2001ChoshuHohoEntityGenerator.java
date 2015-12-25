548a90aa8cabd5c8fb609b1c5abcb5b9fa59e63d
n8300姜井図 <n8300@ndensan.co.jp>
2015-12-25 10:40:15 +0900

    Entity等を最新化する_DBB介護賦課

--- DB/DBB/standard/entity/src/test/java/jp/co/ndensan/reams/db/dbb/entity/basic/helper/DbT2001ChoshuHohoEntityGenerator.java
+++ DB/DBB/standard/entity/src/test/java/jp/co/ndensan/reams/db/dbb/entity/basic/helper/DbT2001ChoshuHohoEntityGenerator.java
@@ -91,7 +91,7 @@ public final class DbT2001ChoshuHohoEntityGenerator {
         entity.setYokunendoKariHosokuM(DEFAULT_翌年度仮徴収捕捉月);
         entity.setIraiSohuzumiFlag(DEFAULT_依頼情報送付済みフラグ);
         entity.setTsuikaIraiSohuzumiFlag(DEFAULT_追加依頼情報送付済みフラグ);
-        entity.setTokuchoTeishiTimestamp(DEFAULT_特別徴収停止日時);
+        entity.setTokuchoTeishiNichiji(DEFAULT_特別徴収停止日時);
         entity.setTokuchoTeishiJiyuCode(DEFAULT_特別徴収停止事由コード);
         return entity;
     }
