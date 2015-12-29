acf55a2476ab654bdb70d464c6f33162b8927b62
zuotao <zuotao@liandisys.com.cn>
2015-12-29 14:19:16 +0800

    市町村を保険者に変更

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/controller/dbe5910001/IkenshoShujiiIchiranHandler.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/controller/dbe5910001/IkenshoShujiiIchiranHandler.java
@@ -8,11 +8,9 @@ package jp.co.ndensan.reams.db.dbe.divcontroller.controller.dbe5910001;
 import java.util.ArrayList;
 import java.util.List;
 import jp.co.ndensan.reams.db.dbe.definition.batchprm.ikenshoshujiiichiran.IkenshoShujiiIchiranParameter;
-import jp.co.ndensan.reams.db.dbe.definition.enumeratedtype.Dokuji.ShujiiHateiJokyo;
 import jp.co.ndensan.reams.db.dbe.definition.enumeratedtype.Dokuji.ShujiiOutputPage;
 import jp.co.ndensan.reams.db.dbe.definition.enumeratedtype.Dokuji.ShujiiOutputSort;
 import jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE5910001.IkenshoShujiiIchiranDiv;
-import jp.co.ndensan.reams.db.dbx.definition.core.shichosonsecurity.DonyukeitaiCode;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.ui.binding.KeyValueDataSource;
 
@@ -34,21 +32,10 @@ public class IkenshoShujiiIchiranHandler {
 
     /**
      * 主治医医療機関・主治医一覧表初期化の設定します
-     *
-     * @param 導入形態コード 導入形態コード
      */
-    public void onLoad(RString 導入形態コード) {
+    public void onLoad() {
 
-        // TODO 左涛 QA153　「市町村ドロップダウンリスト」の表示内容および設定方法の確認　12/25まで
-        //div.getDdlShichosonCode().setSelectedValue(RString.EMPTY);
-        div.getRadJyokyo().setSelectedKey(ShujiiHateiJokyo.有効のみ.getコード());
-        if (DonyukeitaiCode.事務広域.getコード().equals(導入形態コード) || DonyukeitaiCode.事務構成市町村.getコード().equals(導入形態コード)
-                || DonyukeitaiCode.認定広域.getコード().equals(導入形態コード)) {
-            // div.getDdlShichosonCode().setSelectedValue(RString.EMPTY);
-        } else if (DonyukeitaiCode.事務単一.getコード().equals(導入形態コード) || DonyukeitaiCode.認定単一.getコード().equals(導入形態コード)) {
-            div.getDdlShichosonCode().setSelectedValue(RString.EMPTY);
-        }
-
+        div.getCcdHokensha().loadHokenshaList();
         div.getDdlOutputSort().setDataSource(set出力順());
         List<KeyValueDataSource> 改頁 = new ArrayList();
         改頁.add(new KeyValueDataSource(ShujiiOutputPage.医療機関コード.getコード(), ShujiiOutputPage.医療機関コード.get名称()));
@@ -84,8 +71,8 @@ public class IkenshoShujiiIchiranHandler {
     public IkenshoShujiiIchiranParameter onClick_btnReportPublish() {
 
         IkenshoShujiiIchiranParameter batchParameter = new IkenshoShujiiIchiranParameter();
-        batchParameter.setShichosonCode(div.getDdlShichosonCode().getSelectedKey());
-        batchParameter.setShichosonName(div.getDdlShichosonCode().getSelectedValue());
+        batchParameter.setShichosonCode(div.getCcdHokensha().getSelectedItem().get市町村コード().value());
+        batchParameter.setShichosonName(div.getCcdHokensha().getSelectedItem().get市町村名称());
         batchParameter.setIryoKikanCodeFrom(div.getTxtIryoKikanCodeFrom().getValue());
         batchParameter.setIryoKikanCodeTo(div.getTxtIryoKikanCodeTo().getValue());
         batchParameter.setShujiiCodeFrom(div.getTxtShujiiCodeFrom().getValue());
