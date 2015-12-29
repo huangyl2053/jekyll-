e0b5e6908fa84de4a3f1438f1fd0501dd1c8496f
wangkun <wangkun@liandisys.com.cn>
2015-12-29 16:01:34 +0800

    開催場所情報一覧ソースの疎通。

--- DB/DBE/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbe/persistence/db/basic/DbT5592ShinsakaiKaisaiBashoJohoDac.java
+++ DB/DBE/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbe/persistence/db/basic/DbT5592ShinsakaiKaisaiBashoJohoDac.java
@@ -11,7 +11,6 @@ import static jp.co.ndensan.reams.db.dbe.entity.db.basic.DbT5592ShinsakaiKaisaiB
 import jp.co.ndensan.reams.db.dbe.entity.db.basic.DbT5592ShinsakaiKaisaiBashoJohoEntity;
 import jp.co.ndensan.reams.db.dbz.persistence.db.basic.ISaveable;
 import jp.co.ndensan.reams.ur.urz.definition.message.UrSystemErrorMessages;
-import jp.co.ndensan.reams.ur.urz.persistence.util.DbAccessorMethodSelector;
 import jp.co.ndensan.reams.uz.uza.core.mybatis.SqlSession;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.util.db.DbAccessorNormalType;
@@ -71,6 +70,7 @@ public class DbT5592ShinsakaiKaisaiBashoJohoDac implements ISaveable<DbT5592Shin
     public int save(DbT5592ShinsakaiKaisaiBashoJohoEntity entity) {
         requireNonNull(entity, 
                 UrSystemErrorMessages.値がnull.getReplacedMessage("介護認定審査会開催場所情報エンティティ"));
-        return DbAccessorMethodSelector.saveByForDeletePhysical(new DbAccessorNormalType(session), entity);
+         DbAccessorNormalType accessor = new DbAccessorNormalType(session);
+        return accessor.deletePhysical(entity).execute();
     }
 }
