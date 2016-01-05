3a612e8101333d48bb41d49eb9a22eb6fe368d50
zhangguopeng <zhangguopeng@liandisys.com.cn>
2016-01-05 13:43:13 +0800

    介護住民票個別事項連携情報作成【他社住基】　画面ソース

--- DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/controller/dbu0210011/KobetsuJikoRenkeiInfoSakusei.java
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/controller/dbu0210011/KobetsuJikoRenkeiInfoSakusei.java
@@ -3,12 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbu.divcontroller.controller.dbu0210011;
+package jp.co.ndensan.reams.db.dbu.divcontroller.controller.DBU0210011;
 
+import jp.co.ndensan.reams.db.dbu.business.core.kaigojuminhyo.ChushutsuKikanJohoData;
+import jp.co.ndensan.reams.db.dbu.definition.batchprm.kaigojuminhyo.KaigoJuminhyoBatchParameter;
 import jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0210011.KobetsuJikoRenkeiInfoSakuseiDiv;
+import jp.co.ndensan.reams.db.dbu.divcontroller.handler.DBU0210011.KobetsuJikoRenkeiInfoSakuseiHandler;
+import jp.co.ndensan.reams.db.dbu.service.core.basic.kaigojuminhyo.KaigoJuminhyoKobetsuJikouBatchParameterSakuseiFinder;
+import jp.co.ndensan.reams.db.dbz.definition.message.DbzErrorMessages;
+import jp.co.ndensan.reams.ur.urz.definition.message.UrQuestionMessages;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
+import jp.co.ndensan.reams.uz.uza.lang.ApplicationException;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.ui.servlets.CommonButtonHolder;
+import jp.co.ndensan.reams.uz.uza.lang.RTime;
 
 /**
  *
@@ -17,15 +25,74 @@ import jp.co.ndensan.reams.uz.uza.ui.servlets.CommonButtonHolder;
 public class KobetsuJikoRenkeiInfoSakusei {
 
     /**
-     * 画面初期化表示、画面項目に設定されている値をクリアする。
+     * 介護住民票個別事項連携情報作成【他社住基】を画面初期化処理しました。
      *
-     * @param div div
-     * @return ResponseData
+     * @param div
+     * {@link KobetsuJikoRenkeiInfoSakuseiDiv 介護住民票個別事項連携情報作成【他社住基】情報Div}
+     * @return 介護住民票個別事項連携情報作成【他社住基】情報Divを持つResponseData
      */
-    public ResponseData onLoad(KobetsuJikoRenkeiInfoSakuseiDiv div) {
-        ResponseData<KobetsuJikoRenkeiInfoSakuseiDiv> response = new ResponseData<>();
-        CommonButtonHolder.setDisabledByCommonButtonFieldName(new RString("btnJikko"), true);
+    public ResponseData<KobetsuJikoRenkeiInfoSakuseiDiv> onLoad(KobetsuJikoRenkeiInfoSakuseiDiv div) {
+        ChushutsuKikanJohoData chushutsuKikanJohoData = KaigoJuminhyoKobetsuJikouBatchParameterSakuseiFinder.createInstance().getChushutsukikanJoho();
+        getHandler(div).initialize(chushutsuKikanJohoData);
+        return ResponseData.of(div).respond();
+    }
 
-        return response;
+    /**
+     * 介護住民票個別事項連携情報作成【他社住基】を「全件作成する」を押下する。<br/>
+     *
+     * @param div
+     * {@link KobetsuJikoRenkeiInfoSakuseiDiv 介護住民票個別事項連携情報作成【他社住基】情報Div}
+     * @return 介護住民票個別事項連携情報作成【他社住基】情報Divを持つResponseData
+     */
+    public ResponseData<KobetsuJikoRenkeiInfoSakuseiDiv> onClick_chkZenken(KobetsuJikoRenkeiInfoSakuseiDiv div) {
+        return ResponseData.of(div).respond();
+    }
+
+    /**
+     * 介護住民票個別事項連携情報作成【他社住基】を「今回開始日時を修正する]」を押下する。<br/>
+     *
+     * @param div
+     * {@link KobetsuJikoRenkeiInfoSakuseiDiv 介護住民票個別事項連携情報作成【他社住基】情報Div}
+     * @return 介護住民票個別事項連携情報作成【他社住基】情報Divを持つResponseData
+     */
+    public ResponseData<KobetsuJikoRenkeiInfoSakuseiDiv> onClick_chkKikanHenko(KobetsuJikoRenkeiInfoSakuseiDiv div) {
+        if (div.getKobetsuJikoRenkeiInfoSakuseiBP().getChkKikanHenko().isAllSelected() == true) {
+            div.getTblChushutsuKikan().getTxtKonkaiChushutsuFromYMD().setReadOnly(false);
+            div.getTblChushutsuKikan().getTxtKonkaiChushutsuFromTime().setReadOnly(false);
+        } else {
+            div.getTblChushutsuKikan().getTxtKonkaiChushutsuFromYMD().setReadOnly(true);
+            div.getTblChushutsuKikan().getTxtKonkaiChushutsuFromTime().setReadOnly(true);
+        }
+        return ResponseData.of(div).respond();
+    }
+
+    /**
+     * 介護住民票個別事項連携情報作成【他社住基】を「実行する」を押下する。<br/>
+     *
+     * @param div
+     * {@link KobetsuJikoRenkeiInfoSakuseiDiv 介護住民票個別事項連携情報作成【他社住基】情報Div}
+     * @return 介護住民票個別事項連携情報作成【他社住基】情報Divを持つResponseData
+     */
+    public ResponseData<KaigoJuminhyoBatchParameter> onClick_btnJikko(KobetsuJikoRenkeiInfoSakuseiDiv div) {
+        KaigoJuminhyoBatchParameter parameter = new KaigoJuminhyoBatchParameter();
+        ResponseData.of(div).addMessage(UrQuestionMessages.処理実行の確認.getMessage()).respond();
+        if (true) {
+            RDate konkaiFromYMD = div.getChushutsuKikan().getTxtKonkaiChushutsuFromYMD().getValue();
+            RTime konkaiFromTime = div.getTblChushutsuKikan().getTxtKonkaiChushutsuFromTime().getValue();
+            RDate zenkaiFromYMD = div.getChushutsuKikan().getTxtZenkaiChushutsuFromYMD().getValue();
+            RTime zenkaiFromTime = div.getTblChushutsuKikan().getTxtZenkaiChushutsuFromTime().getValue();
+            if (konkaiFromYMD.isBefore(zenkaiFromYMD) || (konkaiFromYMD.isBefore(zenkaiFromYMD) && konkaiFromTime.isBefore(zenkaiFromTime))) {
+                throw new ApplicationException(DbzErrorMessages.期間が不正_未来日付不可.getMessage());
+            } else {
+                parameter = KaigoJuminhyoKobetsuJikouBatchParameterSakuseiFinder.createInstance()
+                        .getKaigoJuminhyoKobetsuJikouBatchParameter(konkaiFromYMD.toDateString(), new RString(konkaiFromTime.toString()));
+            }
+        }
+
+        return ResponseData.of(parameter).respond();
+    }
+
+    private KobetsuJikoRenkeiInfoSakuseiHandler getHandler(KobetsuJikoRenkeiInfoSakuseiDiv div) {
+        return new KobetsuJikoRenkeiInfoSakuseiHandler(div);
     }
 }
--- DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/handler/dbu0210011/KobetsuJikoRenkeiInfoSakuseiHandler.java
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/handler/dbu0210011/KobetsuJikoRenkeiInfoSakuseiHandler.java
@@ -3,7 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbu.divcontroller.handler.dbu0210011;
+package jp.co.ndensan.reams.db.dbu.divcontroller.handler.DBU0210011;
+
+import jp.co.ndensan.reams.db.dbu.business.core.kaigojuminhyo.ChushutsuKikanJohoData;
+import jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0210011.KobetsuJikoRenkeiInfoSakuseiDiv;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
 
 /**
  *
@@ -11,4 +15,36 @@ package jp.co.ndensan.reams.db.dbu.divcontroller.handler.dbu0210011;
  */
 public class KobetsuJikoRenkeiInfoSakuseiHandler {
 
+    private final KobetsuJikoRenkeiInfoSakuseiDiv div;
+
+    /**
+     * コンストラクタです。
+     *
+     * @param div 介護住民票個別事項連携情報作成【他社住基】情報Div
+     */
+    public KobetsuJikoRenkeiInfoSakuseiHandler(KobetsuJikoRenkeiInfoSakuseiDiv div) {
+        this.div = div;
+    }
+
+    /**
+     * 画面初期化処理します。
+     *
+     * @param chushutsuKikanJohoData 抽出期間情報Entity
+     */
+    public void initialize(ChushutsuKikanJohoData chushutsuKikanJohoData) {
+
+        if (chushutsuKikanJohoData != null) {
+            div.getTblChushutsuKikan().getTxtKonkaiChushutsuFromYMD().setValue(chushutsuKikanJohoData.get対象開始日時().getDate());
+            div.getTblChushutsuKikan().getTxtKonkaiChushutsuFromTime().setValue(chushutsuKikanJohoData.get対象開始日時().getRDateTime().getTime());
+            div.getTblChushutsuKikan().getTxtZenkaiChushutsuFromYMD().setValue(chushutsuKikanJohoData.get対象終了日時().getDate());
+            div.getTblChushutsuKikan().getTxtZenkaiChushutsuFromTime().setValue(chushutsuKikanJohoData.get対象開始日時().getRDateTime().getTime());
+            div.getTblChushutsuKikan().getTxtZenkaiChushutsuToYMD().setValue(chushutsuKikanJohoData.get対象終了日時().getDate());
+            div.getTblChushutsuKikan().getTxtZenkaiChushutsuToTime().setValue(chushutsuKikanJohoData.get対象開始日時().getRDateTime().getTime());
+        } else {
+            div.getTblChushutsuKikan().getTxtZenkaiChushutsuFromYMD().setValue(RDate.getNowDate());
+            div.getTblChushutsuKikan().getTxtZenkaiChushutsuFromTime().setValue(RDate.getNowTime());
+            div.getTblChushutsuKikan().getTxtZenkaiChushutsuToYMD().setValue(RDate.getNowDate());
+            div.getTblChushutsuKikan().getTxtZenkaiChushutsuToTime().setValue(RDate.getNowTime());
+        }
+    }
 }
