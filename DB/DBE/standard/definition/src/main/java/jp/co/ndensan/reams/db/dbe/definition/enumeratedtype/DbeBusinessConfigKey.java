7a4aacdb6ba4b7f03dc5c7019cd42cd423ee8913
梁百川 <pangsw@liandisys.com.cn>
2016-01-08 09:56:05 +0800

    DBZ 介護宛名基本情報 新規作成

---
+++ DB/DBE/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbe/definition/enumeratedtype/DbeBusinessConfigKey.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.definition.enumeratedtype;
+
+/**
+ * DBEカスタムコンフィグkey
+ */
+public enum DbeBusinessConfigKey {
+
+    認定調査督促期限日数;
+}
