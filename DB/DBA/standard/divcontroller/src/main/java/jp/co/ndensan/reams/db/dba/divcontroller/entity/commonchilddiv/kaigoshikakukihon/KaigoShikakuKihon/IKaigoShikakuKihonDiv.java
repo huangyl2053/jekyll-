e5ddaca3a1ef1be3931df6325a37fe2033552bdd
梁百川 <pangsw@liandisys.com.cn>
2016-01-08 09:53:59 +0800

    DBA 介護資格基本 新規作成

---
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/commonchilddiv/kaigoshikakukihon/KaigoShikakuKihon/IKaigoShikakuKihonDiv.java
@@ -0,0 +1,12 @@
+package jp.co.ndensan.reams.db.dba.divcontroller.entity.commonchilddiv.kaigoshikakukihon.KaigoShikakuKihon;
+
+import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
+import jp.co.ndensan.reams.uz.uza.ui.binding.ICommonChildDivBaseProperties;
+
+/*
+ * このコードはツールによって生成されました。
+ */
+public interface IKaigoShikakuKihonDiv extends ICommonChildDivBaseProperties {
+
+    void onLoad(ShikibetsuCode 識別コード);
+}
---
+++ DB/DBA/standard/divcontroller/src/main/webapp/WEB-INF/standard/CommonChildDiv/kaigoshikakukihon/KaigoShikakuKihon.ts
@@ -0,0 +1,82 @@
+﻿/// <reference path='KaigoShikakuKihon_Design.ts' />
+module DBZ {
+    export module KaigoShikakuKihon {
+        export class ModeController {
+            private controls: Controls;
+            private fieldName: string;
+
+            constructor(fieldName: string) {
+                this.fieldName = fieldName;
+                this.controls = new Controls(fieldName);
+            }
+
+            public 連絡先ボタンを() {
+                return new Modes.連絡先ボタンを(this.controls);
+            }
+            
+            public 被保履歴ボタンを() {
+                return new Modes.被保履歴ボタンを(this.controls);
+            }
+
+            public 認定履歴ボタンを() {
+                return new Modes.認定履歴ボタンを(this.controls);
+            }
+        }
+
+        export module Modes {
+
+            export class 連絡先ボタンを {
+                private controls: Controls;
+
+                constructor(controls: Controls) {
+                    this.controls = controls;
+                }
+
+                public 表示する(): void {
+                    this.controls.btnRenrakusaki().displayNone = false;
+                }
+
+                public 表示しない(): void {
+                    this.controls.btnRenrakusaki().displayNone = true;
+                }
+            }
+
+            export class 被保履歴ボタンを {
+                private controls: Controls;
+
+                constructor(controls: Controls) {
+                    this.controls = controls;
+                }
+
+                public 表示する(): void {
+                    this.controls.btnHihoRireki().displayNone = false;
+                }
+                
+                public 表示しない(): void {
+                    this.controls.btnHihoRireki().displayNone = true;
+                }
+            }
+
+            export class 認定履歴ボタンを {
+                private controls: Controls;
+
+                constructor(controls: Controls) {
+                    this.controls = controls;
+                }
+
+                public 表示する(): void {
+                    this.controls.btnNinteiRireki().displayNone = false;
+                }
+                
+                public 表示しない(): void {
+                    this.controls.btnNinteiRireki().displayNone = true;
+                }
+            }
+        }
+    }
+
+}
+
+
+
+
