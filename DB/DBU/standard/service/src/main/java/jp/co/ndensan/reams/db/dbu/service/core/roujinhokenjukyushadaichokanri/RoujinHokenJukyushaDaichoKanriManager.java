87097549470d9bcb3fe27484f3a76ade75bb8de8
zhangzhiming <zhangzhiming@liandisys.com.cn>
2016-01-12 14:21:05 +0800

    ソース修正

--- DB/DBU/standard/service/src/main/java/jp/co/ndensan/reams/db/dbu/service/core/roujinhokenjukyushadaichokanri/RoujinHokenJukyushaDaichoKanriManager.java
+++ DB/DBU/standard/service/src/main/java/jp/co/ndensan/reams/db/dbu/service/core/roujinhokenjukyushadaichokanri/RoujinHokenJukyushaDaichoKanriManager.java
@@ -5,16 +5,12 @@
  */
 package jp.co.ndensan.reams.db.dbu.service.core.roujinhokenjukyushadaichokanri;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import static java.util.Objects.requireNonNull;
 import jp.co.ndensan.reams.db.dbz.business.core.basic.RojinHokenJukyushaJoho;
 import jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT7005RojinHokenJukyushaJohoEntity;
 import jp.co.ndensan.reams.db.dbz.persistence.db.basic.DbT7005RojinHokenJukyushaJohoDac;
 import jp.co.ndensan.reams.ur.urz.definition.message.UrSystemErrorMessages;
 import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
-import jp.co.ndensan.reams.uz.uza.util.db.SearchResult;
 import jp.co.ndensan.reams.uz.uza.util.di.InstanceProvider;
 import jp.co.ndensan.reams.uz.uza.util.di.Transaction;
 
@@ -55,20 +51,14 @@ public class RoujinHokenJukyushaDaichoKanriManager {
      * 老人保健受給者情報テーブルから老健受給情報老健受給情報を処理します。
      *
      * @param shikibetsuCode 識別コード
-     * @return SearchResult<RojinHokenJukyushaJoho> 老健受給情報老健受給情報
+     * @return RojinHokenJukyushaJoho 老健受給情報老健受給情報
      */
     @Transaction
-    public SearchResult<RojinHokenJukyushaJoho> getRoukenJukyuJoho(ShikibetsuCode shikibetsuCode) {
-        List<RojinHokenJukyushaJoho> buinessList = new ArrayList<>();
-        List<DbT7005RojinHokenJukyushaJohoEntity> entity = dac.selectRoukenJukyuJoho(shikibetsuCode);
-        if (entity == null || entity.isEmpty()) {
-            return SearchResult.of(Collections.<RojinHokenJukyushaJoho>emptyList(), 0, false);
-        }
-        for (DbT7005RojinHokenJukyushaJohoEntity entityList : entity) {
-            entityList.initializeMd5();
-            buinessList.add(new RojinHokenJukyushaJoho(entityList));
-        }
-        return SearchResult.of(buinessList, 0, false);
+    public RojinHokenJukyushaJoho getRoukenJukyuJoho(ShikibetsuCode shikibetsuCode) {
+        DbT7005RojinHokenJukyushaJohoEntity entity = dac.selectRoukenJukyuJoho(shikibetsuCode);
+        entity.initializeMd5();
+        RojinHokenJukyushaJoho buinessList = new RojinHokenJukyushaJoho(entity);
+        return buinessList;
     }
 
     /**
