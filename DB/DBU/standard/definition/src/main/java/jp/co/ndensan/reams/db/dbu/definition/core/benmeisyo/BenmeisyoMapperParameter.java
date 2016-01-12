bcb65ab086bc3c3384fc2c40e87a9fc817f4ce96
zhengshukai <zhengshukai@liandisys.com.cn>
2016-01-12 10:40:48 +0800

    弁明書　修正

--- DB/DBU/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbu/definition/core/benmeisyo/BenmeisyoMapperParameter.java
+++ DB/DBU/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbu/definition/core/benmeisyo/BenmeisyoMapperParameter.java
@@ -5,6 +5,7 @@
  */
 package jp.co.ndensan.reams.db.dbu.definition.core.benmeisyo;
 
+import edu.umd.cs.findbugs.annotations.SuppressWarnings;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
 import jp.co.ndensan.reams.uz.uza.batch.parameter.IMyBatisParameter;
 import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
@@ -21,12 +22,13 @@ public final class BenmeisyoMapperParameter implements IMyBatisParameter {
     private final FlexibleDate 審査請求届出日;
 
     /**
-     * コンストラクタ。
+     * コンストラクタです。
      *
      * @param 識別コード 識別コード
      * @param 被保険者番号 被保険者番号
      * @param 審査請求届出日 審査請求届出日
      */
+    @SuppressWarnings("URF_UNREAD_FIELD")
     public BenmeisyoMapperParameter(ShikibetsuCode 識別コード, HihokenshaNo 被保険者番号, FlexibleDate 審査請求届出日) {
         this.識別コード = 識別コード;
         this.被保険者番号 = 被保険者番号;
