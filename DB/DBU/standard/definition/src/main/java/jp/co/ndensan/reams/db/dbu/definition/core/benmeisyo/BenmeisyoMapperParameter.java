a0b66e8884c1a56327f340dd3f921ec90032e179
testUser <testUser@liandisys.com.cn>
2016-01-12 10:35:52 +0800

    弁明書　新規

---
+++ DB/DBU/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbu/definition/core/benmeisyo/BenmeisyoMapperParameter.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbu.definition.core.benmeisyo;
+
+import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
+import jp.co.ndensan.reams.uz.uza.batch.parameter.IMyBatisParameter;
+import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
+
+/**
+ *
+ * 弁明情報検索用パラメータです。
+ */
+public final class BenmeisyoMapperParameter implements IMyBatisParameter {
+
+    private final ShikibetsuCode 識別コード;
+    private final HihokenshaNo 被保険者番号;
+    private final FlexibleDate 審査請求届出日;
+
+    /**
+     * コンストラクタ。
+     *
+     * @param 識別コード 識別コード
+     * @param 被保険者番号 被保険者番号
+     * @param 審査請求届出日 審査請求届出日
+     */
+    public BenmeisyoMapperParameter(ShikibetsuCode 識別コード, HihokenshaNo 被保険者番号, FlexibleDate 審査請求届出日) {
+        this.識別コード = 識別コード;
+        this.被保険者番号 = 被保険者番号;
+        this.審査請求届出日 = 審査請求届出日;
+    }
+
+    public static BenmeisyoMapperParameter createSelectByKeyParam(ShikibetsuCode 識別コード, HihokenshaNo 被保険者番号, FlexibleDate 審査請求届出日) {
+        return new BenmeisyoMapperParameter(識別コード, 被保険者番号, 審査請求届出日);
+    }
+
+}
---
+++ DB/DBU/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbu/entity/db/benmeisyo/BenmeiJohoResultEntity.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbu.entity.db.benmeisyo;
+
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+/**
+ *
+ * 弁明情報取得のマッパーの戻るEntityです。
+ */
+@lombok.Getter
+@lombok.Setter
+@SuppressWarnings("PMD.UnusedPrivateField")
+public class BenmeiJohoResultEntity {
+
+    private RString 処分種類コード;
+    private FlexibleDate 弁明書作成日;
+    private RString 弁明内容;
+
+}
