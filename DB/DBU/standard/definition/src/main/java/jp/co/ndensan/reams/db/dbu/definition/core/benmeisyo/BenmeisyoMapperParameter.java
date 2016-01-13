81d4da4fd1afc55d2f6ec16518772b1eb3418bc2
zhengshukai <zhengshukai@liandisys.com.cn>
2016-01-13 15:13:05 +0800

    弁明書　修正

--- DB/DBU/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbu/definition/core/benmeisyo/BenmeisyoMapperParameter.java
+++ DB/DBU/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbu/definition/core/benmeisyo/BenmeisyoMapperParameter.java
@@ -5,7 +5,6 @@
  */
 package jp.co.ndensan.reams.db.dbu.definition.core.benmeisyo;
 
-import edu.umd.cs.findbugs.annotations.SuppressWarnings;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
 import jp.co.ndensan.reams.uz.uza.batch.parameter.IMyBatisParameter;
 import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
@@ -15,26 +14,28 @@ import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
  *
  * 弁明情報検索用パラメータです。
  */
+@java.lang.SuppressWarnings("PMD.UnusedPrivateField")
+@lombok.Getter
 public final class BenmeisyoMapperParameter implements IMyBatisParameter {
 
     private final ShikibetsuCode 識別コード;
     private final HihokenshaNo 被保険者番号;
     private final FlexibleDate 審査請求届出日;
 
-    /**
-     * コンストラクタです。
-     *
-     * @param 識別コード 識別コード
-     * @param 被保険者番号 被保険者番号
-     * @param 審査請求届出日 審査請求届出日
-     */
-    @SuppressWarnings("URF_UNREAD_FIELD")
-    public BenmeisyoMapperParameter(ShikibetsuCode 識別コード, HihokenshaNo 被保険者番号, FlexibleDate 審査請求届出日) {
+    private BenmeisyoMapperParameter(ShikibetsuCode 識別コード, HihokenshaNo 被保険者番号, FlexibleDate 審査請求届出日) {
         this.識別コード = 識別コード;
         this.被保険者番号 = 被保険者番号;
         this.審査請求届出日 = 審査請求届出日;
     }
 
+    /**
+     * mybatisのパラメータを生成します。
+     *
+     * @param 識別コード 識別コード
+     * @param 被保険者番号 被保険者番号
+     * @param 審査請求届出日 審査請求届出日
+     * @return BenmeisyoMapperParameter 弁明情報検索用パラメータ
+     */
     public static BenmeisyoMapperParameter createSelectByKeyParam(ShikibetsuCode 識別コード, HihokenshaNo 被保険者番号, FlexibleDate 審査請求届出日) {
         return new BenmeisyoMapperParameter(識別コード, 被保険者番号, 審査請求届出日);
     }
