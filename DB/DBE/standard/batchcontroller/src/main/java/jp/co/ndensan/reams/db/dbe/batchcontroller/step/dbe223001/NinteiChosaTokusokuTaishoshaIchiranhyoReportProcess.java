6e34acb4349ac4229dec459be247ef342fb86078
梁百川 <pangsw@liandisys.com.cn>
2016-01-14 10:46:14 +0800

    QA #73519 対応

--- DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/step/dbe223001/NinteiChosaTokusokuTaishoshaIchiranhyoReportProcess.java
+++ DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/step/dbe223001/NinteiChosaTokusokuTaishoshaIchiranhyoReportProcess.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package jp.co.ndensan.reams.db.dbe.batchcontroller.step.dbe223001;
 
 import java.util.ArrayList;
@@ -27,35 +26,33 @@ import jp.co.ndensan.reams.uz.uza.lang.FillType;
 import jp.co.ndensan.reams.uz.uza.lang.FirstYear;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.lang.Separator;
-import jp.co.ndensan.reams.uz.uza.report.BreakerCatalog;
 import jp.co.ndensan.reams.uz.uza.report.ReportSourceWriter;
 
 /**
  * 処理詳細_認定調査督促対象者一覧表の作成クラスです。
  */
 public class NinteiChosaTokusokuTaishoshaIchiranhyoReportProcess extends BatchProcessBase<NinteiChosaTokusokuTaishoshaIchiranhyoRelateEntity> {
-    
-   /**
+
+    /**
      * OutputParameter用キー outDataList
      */
     public static final RString OUT_DATA_LIST;
     private NinteiChosaTokusokuTaishoshaIchiranhyoProcessParameter parameter;
-    
+
     private static final ReportId REPORT_DBE223001 = new ReportId("DBE223002_NinteiChosaTokusokuTaishoshaIchiranhyo");
     @BatchWriter
     private BatchReportWriter<NinteiChosaTokusokuTaishoshaIchiranhyoReportSource> batchWrite;
     private ReportSourceWriter<NinteiChosaTokusokuTaishoshaIchiranhyoReportSource> reportSourceWriter;
     private int 帳票データの行番号 = 1;
-    
-    
+
     NinteiChosaTokusokuTaishoshaIchiranhyoItem item;
     private List<NinteiChosaTokusokuTaishoshaIchiranhyoItem> itemList;
-    
+
     static {
         OUT_DATA_LIST = new RString("outDataList");
     }
     private OutputParameter<List<NinteiChosaTokusokuTaishoshaIchiranhyoItem>> outDataList;
-        
+
     @Override
     protected void initialize() {
         itemList = new ArrayList();
@@ -65,49 +62,47 @@ public class NinteiChosaTokusokuTaishoshaIchiranhyoReportProcess extends BatchPr
 
     @Override
     protected IBatchReader createReader() {
-        return new BatchDbReader(DbeMapperInterfaces.認定調査督促対象者一覧表データの抽出.getFullPath(), 
+        return new BatchDbReader(DbeMapperInterfaces.認定調査督促対象者一覧表データの抽出.getFullPath(),
                 parameter.toNinteiChosaTokusokuTaishoshaIchiranhyoMybatisParameter());
     }
-    
+
     @Override
     protected void createWriter() {
-        batchWrite = BatchReportFactory.createBatchReportWriter(REPORT_DBE223001.value()).
-                 addBreak(new BreakerCatalog<NinteiChosaTokusokuTaishoshaIchiranhyoReportSource>().simplePageBreaker(parameter.getTemp_改頁()))
-                 .create();
+        batchWrite = BatchReportFactory.createBatchReportWriter(REPORT_DBE223001.value()).create();
         reportSourceWriter = new ReportSourceWriter(batchWrite);
     }
 
     @Override
     protected void process(NinteiChosaTokusokuTaishoshaIchiranhyoRelateEntity entity) {
-        
-        item =  new NinteiChosaTokusokuTaishoshaIchiranhyoItem(
-                entity.getTemp_市町村コード() == null ? RString.EMPTY : entity.getTemp_市町村コード().getColumnValue(),  //cityCode
-                entity.getTemp_市町村名称(),  //cityName
-                new RString("調査機関名称"),  //kikanNameTitle
-                new RString("調査機関住所"),  //kikanJushoTitle
-                new RString("調査員氏名"),  // nameTitle
-                new RString("調査機関TEL"),  //kikanTelTitle
-                new RString(String.valueOf(帳票データの行番号++)),  //listNo_1
-                parameter.getTemp_保険者名称(),  //listUpper1_1 TODO QA N0.22
+
+        item = new NinteiChosaTokusokuTaishoshaIchiranhyoItem(
+                entity.getTemp_市町村コード() == null ? RString.EMPTY : entity.getTemp_市町村コード().getColumnValue(), //cityCode
+                entity.getTemp_市町村名称(), //cityName
+                new RString("調査機関名称"), //kikanNameTitle
+                new RString("調査機関住所"), //kikanJushoTitle
+                new RString("調査員氏名"), // nameTitle
+                new RString("調査機関TEL"), //kikanTelTitle
+                new RString(String.valueOf(帳票データの行番号++)), //listNo_1
+                parameter.getTemp_保険者名称(), //listUpper1_1 TODO QA N0.22
                 entity.getTemp_被保険者氏名カナ() == null ? RString.EMPTY
-                        : entity.getTemp_被保険者氏名カナ().getColumnValue(),  //listUpper1_2
-                entity.getTemp_申請日() == null ? RString.EMPTY :  entity.getTemp_申請日().
-                        wareki().eraType(EraType.KANJI_RYAKU).firstYear(FirstYear.GAN_NEN).
-                        separator(Separator.PERIOD).fillType(FillType.BLANK).toDateString(),  // listShinseiYMD_1
+                : entity.getTemp_被保険者氏名カナ().getColumnValue(), //listUpper1_2
+                entity.getTemp_申請日() == null ? RString.EMPTY : entity.getTemp_申請日().
+                wareki().eraType(EraType.KANJI_RYAKU).firstYear(FirstYear.GAN_NEN).
+                separator(Separator.PERIOD).fillType(FillType.BLANK).toDateString(), // listShinseiYMD_1
                 entity.getTemp_督促状発行日() == null ? RString.EMPTY : entity.getTemp_督促状発行日().
-                        wareki().eraType(EraType.KANJI_RYAKU).firstYear(FirstYear.GAN_NEN).
-                        separator(Separator.PERIOD).fillType(FillType.BLANK).toDateString(),  // listTokusokujoHakkoYMD_1
-                entity.getTemp_事業者コード() == null ? RString.EMPTY 
-                        : entity.getTemp_事業者コード().getColumnValue(),  //listUpper2_1 TODO  QA  帳票レイアウトに当該項目が存在しない
-                entity.getTemp_事業者住所(),  // listUpper2_2
-                entity.getTemp_被保険者番号(),  //listLower1_1
-                entity.getTemp_被保険者氏名() == null ? RString.EMPTY : entity.getTemp_被保険者氏名().getColumnValue(),  //listLower1_2
-                entity.getTemp_事業者名称(),  //listLower2_1 
-                entity.getTemp_事業者電話番号() == null ? RString.EMPTY : entity.getTemp_事業者電話番号().getColumnValue()  //listLower2_2
+                wareki().eraType(EraType.KANJI_RYAKU).firstYear(FirstYear.GAN_NEN).
+                separator(Separator.PERIOD).fillType(FillType.BLANK).toDateString(), // listTokusokujoHakkoYMD_1
+                entity.getTemp_事業者コード() == null ? RString.EMPTY
+                : entity.getTemp_事業者コード().getColumnValue(), //listUpper2_1 TODO  QA  帳票レイアウトに当該項目が存在しない
+                entity.getTemp_事業者住所(), // listUpper2_2
+                entity.getTemp_被保険者番号(), //listLower1_1
+                entity.getTemp_被保険者氏名() == null ? RString.EMPTY : entity.getTemp_被保険者氏名().getColumnValue(), //listLower1_2
+                entity.getTemp_事業者名称(), //listLower2_1
+                entity.getTemp_事業者電話番号() == null ? RString.EMPTY : entity.getTemp_事業者電話番号().getColumnValue() //listLower2_2
         );
         itemList.add(item);
     }
-    
+
     @Override
     protected void afterExecute() {
         NinteiChosaTokusokuTaishoshaIchiranhyoReport report = NinteiChosaTokusokuTaishoshaIchiranhyoReport.createFrom(itemList);
