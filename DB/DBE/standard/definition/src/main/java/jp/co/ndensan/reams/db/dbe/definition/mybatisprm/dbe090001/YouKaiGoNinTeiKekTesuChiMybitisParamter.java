db4736944ac2f2c0712630531a7990af9087d141
wangkun <wangkun@liandisys.com.cn>
2016-01-19 13:49:26 +0800

    ソースを修正しました。

---
+++ DB/DBE/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbe/definition/mybatisprm/dbe090001/YouKaiGoNinTeiKekTesuChiMybitisParamter.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.definition.mybatisprm.dbe090001;
+
+import jp.co.ndensan.reams.uz.uza.batch.parameter.IMyBatisParameter;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 主治医への結果通知書のMyBatis用パラメータクラスです。
+ */
+@SuppressWarnings("PMD.UnusedPrivateField")
+@Getter
+@Setter
+public class YouKaiGoNinTeiKekTesuChiMybitisParamter implements IMyBatisParameter {
+
+    private final RString 二次判定期間From;
+    private final RString 二次判定期間To;
+    private final RString 未出力フラグ;
+    private final RString 主治医情報;
+    private final RString 申請書管理番号;
+    private final RString 証記載保険者番号;
+    private final RString 支所コード;
+    private final RString 認定状況提供日;
+
+    private final boolean use未出力フラグ;
+    private final boolean use支所コード;
+    private final boolean use二次判定期間From;
+    private final boolean use二次判定期間To;
+    private final boolean use申請書管理番号;
+    private final boolean use主治医情報;
+
+    /**
+     * コンストラクタです。
+     *
+     * @param 二次判定期間From 二次判定期間From
+     * @param 二次判定期間To 二次判定期間To
+     * @param 未出力フラグ 未出力フラグ
+     * @param 主治医情報 主治医情報
+     * @param 申請書管理番号 申請書管理番号
+     * @param 証記載保険者番号 証記載保険者番号
+     * @param 支所コード 支所コード
+     * @param 認定状況提供日 認定状況提供日
+     * @param use未出力フラグ use未出力フラグ
+     * @param use支所コード use支所コード
+     * @param use二次判定期間From use二次判定期間From
+     * @param use二次判定期間To use二次判定期間To
+     * @param use申請書管理番号 use申請書管理番号
+     * @param use主治医情報 use主治医情報
+     */
+    private YouKaiGoNinTeiKekTesuChiMybitisParamter(RString 二次判定期間From,
+            RString 二次判定期間To,
+            RString 未出力フラグ,
+            RString 主治医情報,
+            RString 申請書管理番号,
+            RString 証記載保険者番号,
+            RString 支所コード,
+            RString 認定状況提供日,
+            boolean use未出力フラグ,
+            boolean use支所コード,
+            boolean use二次判定期間From,
+            boolean use二次判定期間To,
+            boolean use申請書管理番号,
+            boolean use主治医情報) {
+        this.二次判定期間From = 二次判定期間From;
+        this.二次判定期間To = 二次判定期間To;
+        this.未出力フラグ = 未出力フラグ;
+        this.主治医情報 = 主治医情報;
+        this.申請書管理番号 = 申請書管理番号;
+        this.証記載保険者番号 = 証記載保険者番号;
+        this.支所コード = 支所コード;
+        this.認定状況提供日 = 認定状況提供日;
+
+        this.use未出力フラグ = use未出力フラグ;
+        this.use支所コード = use支所コード;
+        this.use二次判定期間From = use二次判定期間From;
+        this.use二次判定期間To = use二次判定期間To;
+        this.use申請書管理番号 = use申請書管理番号;
+        this.use主治医情報 = use主治医情報;
+    }
+
+    /**
+     * 主治医選択一覧検索用のパラメータを生成します。
+     *
+     * @param 二次判定期間From 二次判定期間From
+     * @param 二次判定期間To 二次判定期間To
+     * @param 未出力フラグ １：未出力のみ(画面出力条件「未出力のみ」選択した場合)、２：条件なし
+     * @param 主治医情報 主治医医療機関コード+主治医コード
+     * @param 申請書管理番号 申請書管理番号
+     * @param 証記載保険者番号 証記載保険者番号
+     * @param 支所コード 支所コード
+     * @param 認定状況提供日 認定状況提供日
+     *
+     * @return YouKaiGoNinTeiKekTesuChiMapperProcessParameter 主治医への結果通知書のMyBatis用パラメータクラスです。
+     */
+    public static YouKaiGoNinTeiKekTesuChiMybitisParamter create主治医への結果通知書Parameter(RString 二次判定期間From,
+            RString 二次判定期間To,
+            RString 未出力フラグ,
+            RString 主治医情報,
+            RString 申請書管理番号,
+            RString 証記載保険者番号,
+            RString 支所コード,
+            RString 認定状況提供日) {
+        boolean useTemp未出力フラグ = true;
+        boolean useTemp支所コード = true;
+        boolean useTemp二次判定期間From = true;
+        boolean useTemp二次判定期間To = true;
+        boolean useTemp申請書管理番号 = true;
+        boolean useTemp主治医情報 = true;
+        if (未出力フラグ.isEmpty() || !new RString("1").equals(未出力フラグ)) {
+            useTemp未出力フラグ = false;
+        }
+        if (支所コード.isEmpty()) {
+            useTemp支所コード = false;
+        }
+        if (二次判定期間From.isEmpty()) {
+            useTemp二次判定期間From = false;
+        }
+        if (二次判定期間To.isEmpty()) {
+            useTemp二次判定期間To = false;
+        }
+        if (申請書管理番号.isEmpty()) {
+            useTemp申請書管理番号 = false;
+        }
+        if (主治医情報.isEmpty()) {
+            useTemp主治医情報 = false;
+        }
+        return new YouKaiGoNinTeiKekTesuChiMybitisParamter(二次判定期間From, 二次判定期間To, 未出力フラグ,
+                主治医情報, 申請書管理番号, 証記載保険者番号, 支所コード, 認定状況提供日,
+                useTemp未出力フラグ, useTemp支所コード, useTemp二次判定期間From, useTemp二次判定期間To, useTemp申請書管理番号, useTemp主治医情報);
+    }
+}
--- DB/DBE/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbe/persistence/db/mapper/basic/youkaigoninteikekktesuchi/IYouKaiGoNinTeiKekTesuChiMapper.java
+++ DB/DBE/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbe/persistence/db/mapper/basic/youkaigoninteikekktesuchi/IYouKaiGoNinTeiKekTesuChiMapper.java
@@ -7,7 +7,9 @@ package jp.co.ndensan.reams.db.dbe.persistence.db.mapper.basic.youkaigoninteikek
 
 import java.util.List;
 import jp.co.ndensan.reams.db.dbe.definition.mybatis.param.youkaigoninteikekktesuchi.YouKaiGoNinTeiKekTesuChiMapperParameter;
+import jp.co.ndensan.reams.db.dbe.definition.mybatisprm.dbe090001.YouKaiGoNinTeiKekTesuChiMybitisParamter;
 import jp.co.ndensan.reams.db.dbe.entity.db.relate.youkaigoninteikekktesuchi.YouKaiGoNinTeiKekTesuChiRelateEntity;
+import jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT5301ShujiiIkenshoIraiJohoEntity;
 
 /**
  * 要介護認定結果情報のマッパーインタフェースです。
@@ -28,7 +30,23 @@ public interface IYouKaiGoNinTeiKekTesuChiMapper {
      * @param param 要介護認定結果情報を特定するためのMyBatis用パラメータクラスです。
      * @return YouKaiGoNinTeiKekTesuChiRelateEntity 要介護認定結果情報RelateEntityクラスです。
      */
-    List<YouKaiGoNinTeiKekTesuChiRelateEntity> 
-        getKeKaTsuChiSyuTsuRyoKuTaiSyou(YouKaiGoNinTeiKekTesuChiMapperParameter param);
+    List<YouKaiGoNinTeiKekTesuChiRelateEntity> getKeKaTsuChiSyuTsuRyoKuTaiSyou(YouKaiGoNinTeiKekTesuChiMapperParameter param);
 
+    /**
+     * 主治医への結果通知書を発行します。
+     *
+     * @param param 主治医への結果通知書のMyBatis用パラメータクラスです。
+     * @return YouKaiGoNinTeiKekTesuChiRelateEntity 要介護認定結果情報RelateEntityクラスです。
+     */
+    List<YouKaiGoNinTeiKekTesuChiRelateEntity> getCyouHyouSyuTsuRyoKu(YouKaiGoNinTeiKekTesuChiMybitisParamter param);
+    
+    
+    /**
+     * 主治医意見書作成依頼情報を更新します。
+     *
+     * @param entity entity
+     * @return 更新件数
+     */
+    int updateShuJiIkenSyoSaKuSeiIraiJyouHou (DbT5301ShujiiIkenshoIraiJohoEntity entity);
+    
 }
