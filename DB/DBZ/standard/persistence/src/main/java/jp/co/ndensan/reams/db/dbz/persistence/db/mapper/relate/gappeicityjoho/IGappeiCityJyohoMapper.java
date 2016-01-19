bb3f56ee7a09ab7542431bd6fdfe0233ad4d9a19
Kubo Satoshi <n2810@ndensan.co.jp>
2016-01-19 19:20:51 +0900

    Mapper.XML修正２

--- DB/DBZ/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbz/persistence/db/mapper/relate/gappeicityjoho/IGappeiCityJyohoMapper.java
+++ DB/DBZ/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbz/persistence/db/mapper/relate/gappeicityjoho/IGappeiCityJyohoMapper.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbz.persistence.db.mapper.relate.gappeijoho;
+package jp.co.ndensan.reams.db.dbz.persistence.db.mapper.relate.gappeicityjoho;
 
 import java.util.List;
 import jp.co.ndensan.reams.db.dbz.definition.mybatis.param.gappeijoho.GappeiJyohoSpecificParameter;
