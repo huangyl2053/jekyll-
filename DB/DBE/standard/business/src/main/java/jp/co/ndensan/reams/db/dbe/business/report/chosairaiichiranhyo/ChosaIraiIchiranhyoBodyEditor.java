5c73cdb0a08825721e19ade16e1663333f7b3d77
lishengli <lishengli@liandisys.com.cn>
2016-01-22 11:07:01 +0800

    帳票設計_DBE220002_認定調査依頼一覧表　指摘対応しました。

--- DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/chosairaiichiranhyo/ChosaIraiIchiranhyoBodyEditor.java
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/chosairaiichiranhyo/ChosaIraiIchiranhyoBodyEditor.java
@@ -6,9 +6,12 @@
 package jp.co.ndensan.reams.db.dbe.business.report.chosairaiichiranhyo;
 
 import jp.co.ndensan.reams.db.dbe.entity.report.source.chosairaiichiranhyo.ChosaIraiIchiranhyoReportSource;
+import jp.co.ndensan.reams.uz.uza.lang.EraType;
 import jp.co.ndensan.reams.uz.uza.lang.FillType;
+import jp.co.ndensan.reams.uz.uza.lang.FirstYear;
 import jp.co.ndensan.reams.uz.uza.lang.RDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.lang.Separator;
 
 /**
  *
@@ -44,7 +47,7 @@ class ChosaIraiIchiranhyoBodyEditor implements IChosaIraiIchiranhyoEditor {
         if (item.getSinnseyibi() == null || item.getSinnseyibi().isEmpty()) {
             source.listIchiranhyo_4 = RString.EMPTY;
         } else {
-            source.listIchiranhyo_4 = new RDate(item.getSinnseyibi().toString()).wareki().fillType(FillType.BLANK).toDateString();
+            source.listIchiranhyo_4 = パターン4(new RDate(item.getSinnseyibi().toString()));
         }
         source.listIchiranhyo_5 = item.getSinnseyikubunn();
         source.listIchiranhyo_6 = item.getHihokennsyameyi();
@@ -53,7 +56,7 @@ class ChosaIraiIchiranhyoBodyEditor implements IChosaIraiIchiranhyoEditor {
         if (item.getBirthYMD() == null || item.getBirthYMD().isEmpty()) {
             source.listIchiranhyo_9 = RString.EMPTY;
         } else {
-            source.listIchiranhyo_9 = new RDate(item.getBirthYMD().toString()).wareki().fillType(FillType.BLANK).toDateString();
+            source.listIchiranhyo_9 = パターン4(new RDate(item.getBirthYMD().toString()));
         }
         source.listIchiranhyo_10 = item.getJyuusyo();
         source.listIchiranhyo_11 = item.getTelNo();
@@ -61,8 +64,15 @@ class ChosaIraiIchiranhyoBodyEditor implements IChosaIraiIchiranhyoEditor {
         if (item.getTeyisyukigenn() == null || item.getTeyisyukigenn().isEmpty()) {
             source.listIchiranhyo_12 = RString.EMPTY;
         } else {
-            source.listIchiranhyo_12 = new RDate(item.getTeyisyukigenn().toString()).wareki().fillType(FillType.BLANK).toDateString();
+            source.listIchiranhyo_12 = パターン4(new RDate(item.getTeyisyukigenn().toString()));
         }
         return source;
+    }
+
+    // TODO QA541 共通ポリシー　確認
+    private RString パターン4(RDate rDate) {
+        return rDate.wareki().eraType(EraType.KANJI_RYAKU)
+                .firstYear(FirstYear.GAN_NEN).separator(Separator.PERIOD)
+                .fillType(FillType.BLANK).toDateString();
     }
 }
