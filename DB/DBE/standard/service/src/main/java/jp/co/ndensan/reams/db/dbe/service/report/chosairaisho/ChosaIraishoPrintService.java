e721ca3f716596297a6c802c20432da8ed5ecdd2
lishengli <lishengli@liandisys.com.cn>
2016-01-24 18:53:43 +0800

    帳票設計_DBE220001_要介護認定調査依頼書 修正しました

--- DB/DBE/standard/service/src/main/java/jp/co/ndensan/reams/db/dbe/service/report/chosairaisho/ChosaIraishoPrintService.java
+++ DB/DBE/standard/service/src/main/java/jp/co/ndensan/reams/db/dbe/service/report/chosairaisho/ChosaIraishoPrintService.java
@@ -37,76 +37,78 @@ public class ChosaIraishoPrintService {
     /**
      * 要介護認定調査依頼書を印刷します。
      *
-     * @param headItem 要介護認定調査依頼書ヘッダのITEMです。
+     * @param headItemlist 要介護認定調査依頼書ヘッダのITEMリストです。
      * @return {@link SourceDataCollection}
      */
-    public SourceDataCollection print(ChosaIraishoHeadItem headItem) {
+    public SourceDataCollection print(List<ChosaIraishoHeadItem> headItemlist) {
         ChosaIraishoProperty property = new ChosaIraishoProperty();
         try (ReportManager reportManager = new ReportManager()) {
             try (ReportAssembler<ChosaIraishoReportSource> assembler = createAssembler(property, reportManager)) {
                 INinshoshaSourceBuilderCreator ninshoshaSourceBuilderCreator = ReportSourceBuilders.ninshoshaSourceBuilder();
                 INinshoshaSourceBuilder ninshoshaSourceBuilder = ninshoshaSourceBuilderCreator.create(GyomuCode.DB介護保険, RString.EMPTY,
                         RDate.getNowDate(), assembler.getImageFolderPath());
-
-                headItem = new ChosaIraishoHeadItem(
-                        headItem.getShomeiHakkoYMD(),
-                        ninshoshaSourceBuilder.buildSource().denshiKoin,
-                        headItem.getShichosonMei(),
-                        headItem.getShuchoMei(),
-                        ninshoshaSourceBuilder.buildSource().koinShoryaku,
-                        headItem.getBunshoNo(),
-                        ninshoshaSourceBuilder.buildSource().hakkoYMD,
-                        headItem.getHihokenshaNo1(),
-                        headItem.getHihokenshaNo2(),
-                        headItem.getHihokenshaNo3(),
-                        headItem.getHihokenshaNo4(),
-                        headItem.getHihokenshaNo5(),
-                        headItem.getHihokenshaNo6(),
-                        headItem.getHihokenshaNo7(),
-                        headItem.getHihokenshaNo8(),
-                        headItem.getHihokenshaNo9(),
-                        headItem.getHihokenshaNo10(),
-                        headItem.getHihokenshaNameKana(),
-                        headItem.getBirthGengoMeiji(),
-                        headItem.getBirthGengoTaisho(),
-                        headItem.getBirthGengoShowa(),
-                        headItem.getBirthYMD(),
-                        headItem.getHihokenshaName(),
-                        headItem.getSeibetsuMan(),
-                        headItem.getSeibetsuWoman(),
-                        headItem.getYubinNo(),
-                        headItem.getJusho(),
-                        headItem.getTelNo(),
-                        headItem.getHomonChosasakiYubinNo(),
-                        headItem.getHomonChosasakiJusho1(),
-                        headItem.getHomonChosasakiJusho2(),
-                        headItem.getHomonChosasakiJusho3(),
-                        headItem.getHomonChosasakiTelNo(),
-                        headItem.getShinseiYMD(),
-                        headItem.getTeishutsuKigen(),
-                        headItem.getTsuchibun1(),
-                        headItem.getTsuchibun2(),
-                        headItem.getTsuchibun3(),
-                        headItem.getTsuchibun4(),
-                        headItem.getTsuchibun5(),
-                        headItem.getTsuchibun6(),
-                        headItem.getTsuchibun7(),
-                        headItem.getTsuchibun8(),
-                        headItem.getTsuchibun9(),
-                        headItem.getTsuchibun10(),
-                        headItem.getTsuchibun11(),
-                        headItem.getTsuchibun12(),
-                        headItem.getTsuchibun13(),
-                        headItem.getTsuchibun14(),
-                        headItem.getTsuchibun15(),
-                        headItem.getTsuchibun16(),
-                        headItem.getTsuchibun17(),
-                        headItem.getTsuchibun18(),
-                        headItem.getTsuchibun19(),
-                        headItem.getRemban());
-                for (ChosaIraishoReport report : toReports(new ChosaIraishoReportJoho(headItem))) {
-                    ReportSourceWriter<ChosaIraishoReportSource> reportSourceWriter = new ReportSourceWriter(assembler);
-                    report.writeBy(reportSourceWriter);
+                for (ChosaIraishoHeadItem headItem : headItemlist) {
+                    headItem = new ChosaIraishoHeadItem(
+                            // TODO CompNinshoshaの出力項目 技術点NO.47
+                            ninshoshaSourceBuilder.buildSource().hakkoYMD,
+                            ninshoshaSourceBuilder.buildSource().denshiKoin,
+                            headItem.getShichosonMei(),
+                            headItem.getShuchoMei(),
+                            ninshoshaSourceBuilder.buildSource().koinShoryaku,
+                            headItem.getBunshoNo(),
+                            ninshoshaSourceBuilder.buildSource().hakkoYMD,
+                            headItem.getHihokenshaNo1(),
+                            headItem.getHihokenshaNo2(),
+                            headItem.getHihokenshaNo3(),
+                            headItem.getHihokenshaNo4(),
+                            headItem.getHihokenshaNo5(),
+                            headItem.getHihokenshaNo6(),
+                            headItem.getHihokenshaNo7(),
+                            headItem.getHihokenshaNo8(),
+                            headItem.getHihokenshaNo9(),
+                            headItem.getHihokenshaNo10(),
+                            headItem.getHihokenshaNameKana(),
+                            headItem.getBirthGengoMeiji(),
+                            headItem.getBirthGengoTaisho(),
+                            headItem.getBirthGengoShowa(),
+                            headItem.getBirthYMD(),
+                            headItem.getHihokenshaName(),
+                            headItem.getSeibetsuMan(),
+                            headItem.getSeibetsuWoman(),
+                            headItem.getYubinNo(),
+                            headItem.getJusho(),
+                            headItem.getTelNo(),
+                            headItem.getHomonChosasakiYubinNo(),
+                            headItem.getHomonChosasakiJusho1(),
+                            headItem.getHomonChosasakiJusho2(),
+                            headItem.getHomonChosasakiJusho3(),
+                            headItem.getHomonChosasakiTelNo(),
+                            headItem.getShinseiYMD(),
+                            headItem.getTeishutsuKigen(),
+                            headItem.getTsuchibun1(),
+                            headItem.getTsuchibun2(),
+                            headItem.getTsuchibun3(),
+                            headItem.getTsuchibun4(),
+                            headItem.getTsuchibun5(),
+                            headItem.getTsuchibun6(),
+                            headItem.getTsuchibun7(),
+                            headItem.getTsuchibun8(),
+                            headItem.getTsuchibun9(),
+                            headItem.getTsuchibun10(),
+                            headItem.getTsuchibun11(),
+                            headItem.getTsuchibun12(),
+                            headItem.getTsuchibun13(),
+                            headItem.getTsuchibun14(),
+                            headItem.getTsuchibun15(),
+                            headItem.getTsuchibun16(),
+                            headItem.getTsuchibun17(),
+                            headItem.getTsuchibun18(),
+                            headItem.getTsuchibun19(),
+                            headItem.getRemban());
+                    for (ChosaIraishoReport report : toReports(new ChosaIraishoReportJoho(headItem))) {
+                        ReportSourceWriter<ChosaIraishoReportSource> reportSourceWriter = new ReportSourceWriter(assembler);
+                        report.writeBy(reportSourceWriter);
+                    }
                 }
             }
             return reportManager.publish();
