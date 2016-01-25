39a326fad00306780f59934fde6cccf1748b8657
wangkun <wangkun@liandisys.com.cn>
2016-01-25 20:56:52 +0800

    ソースを修正しました。

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBD5330001/dgDoctorSelection_Row.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBD5330001/dgDoctorSelection_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBD5330001;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Thu Jan 21 17:18:12 CST 2016 
+ * Mon Jan 25 16:49:45 CST 2016 
  */
 
 
