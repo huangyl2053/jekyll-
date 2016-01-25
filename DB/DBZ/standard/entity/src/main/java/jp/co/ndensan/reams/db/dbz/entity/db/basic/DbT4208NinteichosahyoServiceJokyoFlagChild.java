3c2768528e55504c24a92af266a1263429510f17
西澤 貴幸 <n2818@ndensan.co.jp>
2016-01-25 22:50:30 +0900

    DBZテーブルEntity修正_DbT4209NinteichosahyoServiceJokyoFlagChild

---
+++ DB/DBZ/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbz/entity/db/basic/DbT4208NinteichosahyoServiceJokyoFlagChild.java
@@ -0,0 +1,92 @@
+package jp.co.ndensan.reams.db.dbz.entity.db.basic;
+
+import jp.co.ndensan.reams.uz.uza.util.db.IColumnDefinition;
+
+/**
+ * 認定調査票（概況調査）サービスの状況フラグ（子）テーブルの項目定義クラスです。
+ */
+public enum DbT4208NinteichosahyoServiceJokyoFlagChild implements IColumnDefinition {
+// <editor-fold defaultstate="collapsed" desc="Created By POJO Tool ver 1.4.2">
+    /**
+     * insertDantaiCd
+     */
+    insertDantaiCd(2147483647, 0),
+    /**
+     * insertTimestamp
+     */
+    insertTimestamp(29, 0),
+    /**
+     * insertReamsLoginId
+     */
+    insertReamsLoginId(2147483647, 0),
+    /**
+     * insertContextId
+     */
+    insertContextId(2147483647, 0),
+    /**
+     * isDeleted
+     */
+    isDeleted(1, 0),
+    /**
+     * updateCount
+     */
+    updateCount(10, 0),
+    /**
+     * lastUpdateTimestamp
+     */
+    lastUpdateTimestamp(29, 0),
+    /**
+     * lastUpdateReamsLoginId
+     */
+    lastUpdateReamsLoginId(2147483647, 0),
+    /**
+     * 申請書管理番号
+     * <br/>要介護認定申請情報
+     */
+    shinseishoKanriNo(2147483647, 0),
+    /**
+     * 認定調査依頼履歴番号
+     * <br/>認定調査依頼情報
+     */
+    ninteichosaRirekiNo(5, 0),
+    /**
+     * 連番
+     */
+    remban(5, 0),
+    /**
+     * 厚労省IF識別コード
+     */
+    koroshoIfShikibetsuCode(2147483647, 0),
+    /**
+     * サービスの状況フラグ
+     */
+    serviceJokyoFlag(1, 0);
+
+    private final int maxLength;
+    private final int scale;
+
+    private DbT4208NinteichosahyoServiceJokyoFlagChild(int maxLength, int scale) {
+        this.maxLength = maxLength;
+        this.scale = scale;
+    }
+
+    /**
+     * 項目の最大長のgetメソッドです。
+     * 
+     * @return 項目の最大長
+     */
+    public int getMaxLength() {
+        return maxLength;
+    }
+
+    /**
+     * 小数点以下の桁数のgetメソッドです。
+     * 
+     * @return 小数点以下の桁数
+     */
+    public int getScale() {
+        return scale;
+    }
+
+// </editor-fold>
+}
