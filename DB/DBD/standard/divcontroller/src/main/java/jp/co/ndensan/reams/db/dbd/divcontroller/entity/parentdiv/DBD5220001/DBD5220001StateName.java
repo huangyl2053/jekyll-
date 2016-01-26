69297766ed6a59f560c4cf8848e461da19b94310
unknown <ds_kobayashi@LDNS-DT-1590.sunjapan.com.cn>
2016-01-26 13:09:13 +0800

    介護認定（画面修正）

--- DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD5220001/DBD5220001StateName.java
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD5220001/DBD5220001StateName.java
@@ -15,7 +15,8 @@ public enum DBD5220001StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
     NoChange(""),
-    初期表示("初期表示");
+    検索("検索"),
+    通知書("通知書");
 
     private final RString name;
 
