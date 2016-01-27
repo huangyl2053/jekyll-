e988d511eb4820c33e406a00198f0e034d215c2b
sunhadi <sunhaidi@liandisys.com.cn>
2016-01-27 19:58:39 +0800

    技術点No19よりViewStateKeysを作成

---
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/viewbox/ViewStateKeys.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbz.divcontroller.viewbox;
+
+/**
+ * ViewStateKeys
+ */
+public enum ViewStateKeys {
+
+}
