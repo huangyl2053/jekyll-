7f686ab8a5cb646c89ce045465ab698c34848900
孫輝 <sunhui@ustcsoft.com>
2016-01-29 11:44:47 +0800

    no message

---
+++ DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/jigyohokokugeppohoseihako/JigyoHokokuGeppoHoseiHakoResult.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jp.co.ndensan.reams.db.dbu.business.jigyohokokugeppohoseihako;
+
+import jp.co.ndensan.reams.db.dbu.entity.db.jigyohokokugeppohoseihako.JigyoHokokuGeppoHoseiHakoEntity;
+
+/**
+ *
+ * @author sunhui
+ */
+public class JigyoHokokuGeppoHoseiHakoResult {
+    JigyoHokokuGeppoHoseiHakoEntity entity;
+    public JigyoHokokuGeppoHoseiHakoResult(JigyoHokokuGeppoHoseiHakoEntity entity){
+        this.entity=entity;
+    }
+}
---
+++ DB/DBU/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbu/entity/db/jigyohokokugeppohoseihako/JigyoHokokuGeppoHoseiHakoEntity.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jp.co.ndensan.reams.db.dbu.entity.db.jigyohokokugeppohoseihako;
+
+import jp.co.ndensan.reams.db.dbu.entity.db.basic.DbT7021JigyoHokokuTokeiDataEntity;
+import jp.co.ndensan.reams.uz.uza.math.Decimal;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author sunhui
+ */
+@Getter
+@Setter
+public class JigyoHokokuGeppoHoseiHakoEntity {
+    private DbT7021JigyoHokokuTokeiDataEntity entity;
+    private Decimal yoshiCode;
+
+    public JigyoHokokuGeppoHoseiHakoEntity(DbT7021JigyoHokokuTokeiDataEntity entity, Decimal yoshiCode) {
+        this.entity = entity;
+        this.yoshiCode = yoshiCode;
+    }
+    
+}
