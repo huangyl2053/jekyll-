662a318296cf952a8f8616a6d2998d9a6b0473d6
zhengshukai <zhengshukai@liandisys.com.cn>
2016-01-29 09:57:02 +0800

    事業報告（年報）補正、発行_様式３　ソースの修正

--- DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/controller/parentdiv/dbu0060041/NenpoYoushi3.java
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/controller/parentdiv/dbu0060041/NenpoYoushi3.java
@@ -63,17 +63,6 @@ public class NenpoYoushi3 {
      * @return ResponseData<NenpoYoushi3Div>
      */
     public ResponseData<NenpoYoushi3Div> onLoad(NenpoYoushi3Div div) {
-        ZigyouHoukokuNenpouHoseihakouKensakuRelateEntity entity1 = new ZigyouHoukokuNenpouHoseihakouKensakuRelateEntity();
-        entity1.set画面報告年度(new RString("19900101"));
-        entity1.set画面集計年度(new RString("20100202"));
-        entity1.set保険者コード(new RString("123456"));
-        entity1.set市町村名称(new RString("市町村名称"));
-        entity1.set補正フラグ(new RString("修正"));
-        entity1.set行報告年(new RString("1990"));
-        entity1.set行集計対象年(new RString("2016"));
-        entity1.set行市町村コード(new RString("123456"));
-        entity1.set事業報告年報補正表示のコード(new RString("003"));
-        ViewStateHolder.put(DbuViewStateKey.補正検索画面情報, entity1);
         ZigyouHoukokuNenpouHoseihakouKensakuRelateEntity entity
                 = ViewStateHolder.get(DbuViewStateKey.補正検索画面情報, ZigyouHoukokuNenpouHoseihakouKensakuRelateEntity.class);
         報告年度 = new FlexibleDate(entity.get画面報告年度());
@@ -106,7 +95,8 @@ public class NenpoYoushi3 {
     public ResponseData<NenpoYoushi3Div> onChange_tab(NenpoYoushi3Div div) {
         RString title = div.getShokuhikyojunofutannintei().getTabShokuhikyojunofutannintei().getSelectedItem().getTitle();
         if (title.equals(保険料収納状況)) {
-            onLoad(div);
+            get保険料収納状況データ(div);
+            return ResponseData.of(div).respond();
         }
         if (title.equals(保険給付支払状況)) {
             get保険給付支払状況データ(div);
