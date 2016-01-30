e7d03f38b001bc06f3347561436029534bb91867
lijunjun <lijunjun@ustcsoft.com>
2016-01-30 09:24:17 +0800

    画面設計_DBUMN91003_事業報告（月報）補正発行_様式１(別紙）

---
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/controller/parentdiv/DBU0020061/MainPanel.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbu.divcontroller.controller.parentdiv.DBU0020061;
+
+import java.util.List;
+import jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0020061.DBU0020061TransitionEventName;
+import jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0020061.MainPanelDiv;
+import jp.co.ndensan.reams.db.dbu.divcontroller.handler.dbu0020061.MainPanelHandler;
+import jp.co.ndensan.reams.db.dbu.entity.db.basic.DbT7021JigyoHokokuTokeiDataEntity;
+import jp.co.ndensan.reams.db.dbz.definition.core.ViewStateKeys;
+import jp.co.ndensan.reams.db.dbz.definition.message.DbzErrorMessages;
+import jp.co.ndensan.reams.ur.urz.definition.message.UrQuestionMessages;
+import jp.co.ndensan.reams.ur.urz.definition.message.UrWarningMessages;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
+import jp.co.ndensan.reams.uz.uza.lang.ApplicationException;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.ui.servlets.ViewStateHolder;
+
+/**
+ *
+ * @author LDNS lijunjun
+ */
+public class MainPanel {
+
+    private static final RString 修正 = new RString("修正");
+    private static final RString 削除 = new RString("削除");
+    private static final RString 完了 = new RString("完了");
+
+    /**
+     * 画面ロードメソッド
+     *
+     * @param div
+     * @return ResponseData<MainPanelDiv>
+     */
+    public ResponseData<MainPanelDiv> onLoad(MainPanelDiv div) {
+        MainPanelHandler handler = getHandler(div);
+        // TODO 引き継ぎデータの取得
+//        RString entity = ViewStateHolder.get(ViewStateKeys.償還払請求パラメータ, XXX.class);
+        RString 引き継ぎデータEntity = new RString("");
+        List<DbT7021JigyoHokokuTokeiDataEntity> 更新前データリスト = handler.get更新前データリスト(引き継ぎデータEntity);
+        handler.更新前データリスト初期化(更新前データリスト.get(0));
+        return ResponseData.of(div).respond();
+    }
+
+    /**
+     * 保存ボタンークをリックして時実行するメソッド
+     *
+     * @param div
+     * @return ResponseData<MainPanelDiv>
+     */
+    public ResponseData<MainPanelDiv> onClick_btnSave(MainPanelDiv div) {
+        MainPanelHandler handler = getHandler(div);
+        // TODO 引き継ぎデータの取得
+        // RString entity = ViewStateHolder.get(ViewStateKeys.償還払請求パラメータ, XXX.class);
+        RString 引き継ぎデータEntity = new RString("");
+        //TODO ビジネス設計_DBUMN91003_事業報告（月報）補正発行.xlsxの「事業報告月報詳細データの取得」を呼び出す
+        //DbT7021JigyoHokokuTokeiDataEntity 事業報告統計データEntityリスト = getJigyoHokokuGeppoDetal(報告年, 報告月, 集計対象年, 集計対象月, 統計対象区分, 市町村コード, 表番号, 集計番号);
+        if (修正.equals(ViewStateHolder.get(ViewStateKeys.状態, RString.class))) {
+            if (handler.get修正データ(引き継ぎデータEntity).isEmpty()) {
+                throw new ApplicationException(DbzErrorMessages.編集なしで更新不可.getMessage());
+            }
+            if (!div.getDaiichigohiHokenshazogenUchiWake().getTblSetaiSu().getTxtTonenZoTennyu().getValue().add(
+                    div.getDaiichigohiHokenshazogenUchiWake().getTxtTonenZoShokkenfukkatsu().getValue()).subtract(
+                            div.getDaiichigohiHokenshazogenUchiWake().getTblSetaiSu().getTxtTonenZorokugosaitotatsu().getValue()).equals(
+                            div.getDaiichigohiHokenshazogenUchiWake().getTblSetaiSu().getTxtTonenZoTekiyojogaihi().getValue())) {
+                throw new ApplicationException(UrWarningMessages.相違.getMessage().replace("当月末現在の世帯数", "前月末世帯数から増減した世帯数の計算結果"));
+            }
+            if (handler.get修正データ(引き継ぎデータEntity).size() > 0) {
+                return ResponseData.of(div).addMessage(UrQuestionMessages.処理実行の確認.getMessage()).respond();
+            }
+        } else if (削除.equals(ViewStateHolder.get(ViewStateKeys.状態, RString.class))) {
+            // 「ビジネス設計_DBUMN91003_事業報告（月報）補正発行.xlsx」の「事業報告月報詳細データの削除」を呼び出す
+            // 引数：削除条件オブジェクト
+        }
+        return ResponseData.of(div).respond();
+    }
+
+    /**
+     * 戻るボタンークをリックして時実行するメソッド
+     *
+     * @param div
+     * @return ResponseData<MainPanelDiv>
+     */
+    public ResponseData<MainPanelDiv> onClick_btnBack(MainPanelDiv div) {
+        MainPanelHandler handler = getHandler(div);
+        // TODO 引き継ぎデータの取得
+        RString 引き継ぎデータentity = new RString("");
+        if (handler.get修正データ(引き継ぎデータentity).isEmpty()) {
+            return ResponseData.of(div).forwardWithEventName(DBU0020061TransitionEventName.補正発行検索に戻る).respond();
+        } else if (handler.get修正データ(引き継ぎデータentity).size() > 0) {
+            return ResponseData.of(div).addMessage(UrQuestionMessages.入力内容の破棄.getMessage()).respond();
+        }
+        return ResponseData.of(div).respond();
+    }
+
+    private MainPanelHandler getHandler(MainPanelDiv div) {
+        return MainPanelHandler.of(div);
+    }
+
+}
---
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/entity/parentdiv/DBU0020061/tblSetaiSuDiv.java
@@ -0,0 +1,563 @@
+package jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0020061;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.TableCell;
+import jp.co.ndensan.reams.uz.uza.ui.binding.TablePanel;
+
+/**
+ * tblSetaiSu のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class tblSetaiSuDiv extends TablePanel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2015-11-16_12-38-19">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("TableCell31")
+    private TableCell31AttblSetaiSu TableCell31;
+    @JsonProperty("TableCell32")
+    private TableCell32AttblSetaiSu TableCell32;
+    @JsonProperty("TableCell33")
+    private TableCell33AttblSetaiSu TableCell33;
+    @JsonProperty("TableCell40")
+    private TableCell40AttblSetaiSu TableCell40;
+    @JsonProperty("TableCell41")
+    private TableCell41AttblSetaiSu TableCell41;
+    @JsonProperty("TableCell34")
+    private TableCell34AttblSetaiSu TableCell34;
+    @JsonProperty("TableCell35")
+    private TableCell35AttblSetaiSu TableCell35;
+    @JsonProperty("TableCell36")
+    private TableCell36AttblSetaiSu TableCell36;
+    @JsonProperty("TableCell44")
+    private TableCell44AttblSetaiSu TableCell44;
+    @JsonProperty("TableCell45")
+    private TableCell45AttblSetaiSu TableCell45;
+
+    /*
+     * [ Helperの作成 ]
+     * TablePanelコントロールに特化したヘルパーメソッドを作成
+     */
+    @JsonIgnore
+    public Label getLblTonenZoTennyu() {
+        return this.TableCell32.getLblTonenZoTennyu();
+    }
+
+    @JsonIgnore
+    public Label getLblTonenZoShokkenfukkatsu() {
+        return this.TableCell33.getLblTonenZoShokkenfukkatsu();
+    }
+
+    @JsonIgnore
+    public Label getLblTonenZorokugosaitotatsu() {
+        return this.TableCell40.getLblTonenZorokugosaitotatsu();
+    }
+
+    @JsonIgnore
+    public Label getLblTonenZoTekiyojogaihi() {
+        return this.TableCell41.getLblTonenZoTekiyojogaihi();
+    }
+
+    @JsonIgnore
+    public Label getLblKei() {
+        return this.TableCell34.getLblKei();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtTonenZoTennyu() {
+        return this.TableCell35.getTxtTonenZoTennyu();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtTonenZoShokkenfukkatsu() {
+        return this.TableCell36.getTxtTonenZoShokkenfukkatsu();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtTonenZorokugosaitotatsu() {
+        return this.TableCell44.getTxtTonenZorokugosaitotatsu();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtTonenZoTekiyojogaihi() {
+        return this.TableCell45.getTxtTonenZoTekiyojogaihi();
+    }
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * getTableCell31
+     * @return TableCell31
+     */
+    @JsonProperty("TableCell31")
+    private TableCell31AttblSetaiSu getTableCell31() {
+        return TableCell31;
+    }
+
+    /*
+     * setTableCell31
+     * @param TableCell31 TableCell31
+     */
+    @JsonProperty("TableCell31")
+    private void setTableCell31(TableCell31AttblSetaiSu TableCell31) {
+        this.TableCell31 = TableCell31;
+    }
+
+    /*
+     * getTableCell32
+     * @return TableCell32
+     */
+    @JsonProperty("TableCell32")
+    private TableCell32AttblSetaiSu getTableCell32() {
+        return TableCell32;
+    }
+
+    /*
+     * setTableCell32
+     * @param TableCell32 TableCell32
+     */
+    @JsonProperty("TableCell32")
+    private void setTableCell32(TableCell32AttblSetaiSu TableCell32) {
+        this.TableCell32 = TableCell32;
+    }
+
+    /*
+     * getTableCell33
+     * @return TableCell33
+     */
+    @JsonProperty("TableCell33")
+    private TableCell33AttblSetaiSu getTableCell33() {
+        return TableCell33;
+    }
+
+    /*
+     * setTableCell33
+     * @param TableCell33 TableCell33
+     */
+    @JsonProperty("TableCell33")
+    private void setTableCell33(TableCell33AttblSetaiSu TableCell33) {
+        this.TableCell33 = TableCell33;
+    }
+
+    /*
+     * getTableCell40
+     * @return TableCell40
+     */
+    @JsonProperty("TableCell40")
+    private TableCell40AttblSetaiSu getTableCell40() {
+        return TableCell40;
+    }
+
+    /*
+     * setTableCell40
+     * @param TableCell40 TableCell40
+     */
+    @JsonProperty("TableCell40")
+    private void setTableCell40(TableCell40AttblSetaiSu TableCell40) {
+        this.TableCell40 = TableCell40;
+    }
+
+    /*
+     * getTableCell41
+     * @return TableCell41
+     */
+    @JsonProperty("TableCell41")
+    private TableCell41AttblSetaiSu getTableCell41() {
+        return TableCell41;
+    }
+
+    /*
+     * setTableCell41
+     * @param TableCell41 TableCell41
+     */
+    @JsonProperty("TableCell41")
+    private void setTableCell41(TableCell41AttblSetaiSu TableCell41) {
+        this.TableCell41 = TableCell41;
+    }
+
+    /*
+     * getTableCell34
+     * @return TableCell34
+     */
+    @JsonProperty("TableCell34")
+    private TableCell34AttblSetaiSu getTableCell34() {
+        return TableCell34;
+    }
+
+    /*
+     * setTableCell34
+     * @param TableCell34 TableCell34
+     */
+    @JsonProperty("TableCell34")
+    private void setTableCell34(TableCell34AttblSetaiSu TableCell34) {
+        this.TableCell34 = TableCell34;
+    }
+
+    /*
+     * getTableCell35
+     * @return TableCell35
+     */
+    @JsonProperty("TableCell35")
+    private TableCell35AttblSetaiSu getTableCell35() {
+        return TableCell35;
+    }
+
+    /*
+     * setTableCell35
+     * @param TableCell35 TableCell35
+     */
+    @JsonProperty("TableCell35")
+    private void setTableCell35(TableCell35AttblSetaiSu TableCell35) {
+        this.TableCell35 = TableCell35;
+    }
+
+    /*
+     * getTableCell36
+     * @return TableCell36
+     */
+    @JsonProperty("TableCell36")
+    private TableCell36AttblSetaiSu getTableCell36() {
+        return TableCell36;
+    }
+
+    /*
+     * setTableCell36
+     * @param TableCell36 TableCell36
+     */
+    @JsonProperty("TableCell36")
+    private void setTableCell36(TableCell36AttblSetaiSu TableCell36) {
+        this.TableCell36 = TableCell36;
+    }
+
+    /*
+     * getTableCell44
+     * @return TableCell44
+     */
+    @JsonProperty("TableCell44")
+    private TableCell44AttblSetaiSu getTableCell44() {
+        return TableCell44;
+    }
+
+    /*
+     * setTableCell44
+     * @param TableCell44 TableCell44
+     */
+    @JsonProperty("TableCell44")
+    private void setTableCell44(TableCell44AttblSetaiSu TableCell44) {
+        this.TableCell44 = TableCell44;
+    }
+
+    /*
+     * getTableCell45
+     * @return TableCell45
+     */
+    @JsonProperty("TableCell45")
+    private TableCell45AttblSetaiSu getTableCell45() {
+        return TableCell45;
+    }
+
+    /*
+     * setTableCell45
+     * @param TableCell45 TableCell45
+     */
+    @JsonProperty("TableCell45")
+    private void setTableCell45(TableCell45AttblSetaiSu TableCell45) {
+        this.TableCell45 = TableCell45;
+    }
+
+    // </editor-fold>
+}
+/**
+ * TableCell31 のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+class TableCell31AttblSetaiSu extends TableCell {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2015-11-16_12-38-19">
+
+    // </editor-fold>
+}
+/**
+ * TableCell32 のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+class TableCell32AttblSetaiSu extends TableCell {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2015-11-16_12-38-19">
+    @JsonProperty("lblTonenZoTennyu")
+    private Label lblTonenZoTennyu;
+
+    /*
+     * getlblTonenZoTennyu
+     * @return lblTonenZoTennyu
+     */
+    @JsonProperty("lblTonenZoTennyu")
+    public Label getLblTonenZoTennyu() {
+        return lblTonenZoTennyu;
+    }
+
+    /*
+     * setlblTonenZoTennyu
+     * @param lblTonenZoTennyu lblTonenZoTennyu
+     */
+    @JsonProperty("lblTonenZoTennyu")
+    public void setLblTonenZoTennyu(Label lblTonenZoTennyu) {
+        this.lblTonenZoTennyu = lblTonenZoTennyu;
+    }
+
+    // </editor-fold>
+}
+/**
+ * TableCell33 のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+class TableCell33AttblSetaiSu extends TableCell {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2015-11-16_12-38-19">
+    @JsonProperty("lblTonenZoShokkenfukkatsu")
+    private Label lblTonenZoShokkenfukkatsu;
+
+    /*
+     * getlblTonenZoShokkenfukkatsu
+     * @return lblTonenZoShokkenfukkatsu
+     */
+    @JsonProperty("lblTonenZoShokkenfukkatsu")
+    public Label getLblTonenZoShokkenfukkatsu() {
+        return lblTonenZoShokkenfukkatsu;
+    }
+
+    /*
+     * setlblTonenZoShokkenfukkatsu
+     * @param lblTonenZoShokkenfukkatsu lblTonenZoShokkenfukkatsu
+     */
+    @JsonProperty("lblTonenZoShokkenfukkatsu")
+    public void setLblTonenZoShokkenfukkatsu(Label lblTonenZoShokkenfukkatsu) {
+        this.lblTonenZoShokkenfukkatsu = lblTonenZoShokkenfukkatsu;
+    }
+
+    // </editor-fold>
+}
+/**
+ * TableCell40 のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+class TableCell40AttblSetaiSu extends TableCell {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2015-11-16_12-38-19">
+    @JsonProperty("lblTonenZorokugosaitotatsu")
+    private Label lblTonenZorokugosaitotatsu;
+
+    /*
+     * getlblTonenZorokugosaitotatsu
+     * @return lblTonenZorokugosaitotatsu
+     */
+    @JsonProperty("lblTonenZorokugosaitotatsu")
+    public Label getLblTonenZorokugosaitotatsu() {
+        return lblTonenZorokugosaitotatsu;
+    }
+
+    /*
+     * setlblTonenZorokugosaitotatsu
+     * @param lblTonenZorokugosaitotatsu lblTonenZorokugosaitotatsu
+     */
+    @JsonProperty("lblTonenZorokugosaitotatsu")
+    public void setLblTonenZorokugosaitotatsu(Label lblTonenZorokugosaitotatsu) {
+        this.lblTonenZorokugosaitotatsu = lblTonenZorokugosaitotatsu;
+    }
+
+    // </editor-fold>
+}
+/**
+ * TableCell41 のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+class TableCell41AttblSetaiSu extends TableCell {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2015-11-16_12-38-19">
+    @JsonProperty("lblTonenZoTekiyojogaihi")
+    private Label lblTonenZoTekiyojogaihi;
+
+    /*
+     * getlblTonenZoTekiyojogaihi
+     * @return lblTonenZoTekiyojogaihi
+     */
+    @JsonProperty("lblTonenZoTekiyojogaihi")
+    public Label getLblTonenZoTekiyojogaihi() {
+        return lblTonenZoTekiyojogaihi;
+    }
+
+    /*
+     * setlblTonenZoTekiyojogaihi
+     * @param lblTonenZoTekiyojogaihi lblTonenZoTekiyojogaihi
+     */
+    @JsonProperty("lblTonenZoTekiyojogaihi")
+    public void setLblTonenZoTekiyojogaihi(Label lblTonenZoTekiyojogaihi) {
+        this.lblTonenZoTekiyojogaihi = lblTonenZoTekiyojogaihi;
+    }
+
+    // </editor-fold>
+}
+/**
+ * TableCell34 のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+class TableCell34AttblSetaiSu extends TableCell {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2015-11-16_12-38-19">
+    @JsonProperty("lblKei")
+    private Label lblKei;
+
+    /*
+     * getlblKei
+     * @return lblKei
+     */
+    @JsonProperty("lblKei")
+    public Label getLblKei() {
+        return lblKei;
+    }
+
+    /*
+     * setlblKei
+     * @param lblKei lblKei
+     */
+    @JsonProperty("lblKei")
+    public void setLblKei(Label lblKei) {
+        this.lblKei = lblKei;
+    }
+
+    // </editor-fold>
+}
+/**
+ * TableCell35 のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+class TableCell35AttblSetaiSu extends TableCell {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2015-11-16_12-38-19">
+    @JsonProperty("txtTonenZoTennyu")
+    private TextBoxNum txtTonenZoTennyu;
+
+    /*
+     * gettxtTonenZoTennyu
+     * @return txtTonenZoTennyu
+     */
+    @JsonProperty("txtTonenZoTennyu")
+    public TextBoxNum getTxtTonenZoTennyu() {
+        return txtTonenZoTennyu;
+    }
+
+    /*
+     * settxtTonenZoTennyu
+     * @param txtTonenZoTennyu txtTonenZoTennyu
+     */
+    @JsonProperty("txtTonenZoTennyu")
+    public void setTxtTonenZoTennyu(TextBoxNum txtTonenZoTennyu) {
+        this.txtTonenZoTennyu = txtTonenZoTennyu;
+    }
+
+    // </editor-fold>
+}
+/**
+ * TableCell36 のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+class TableCell36AttblSetaiSu extends TableCell {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2015-11-16_12-38-19">
+    @JsonProperty("txtTonenZoShokkenfukkatsu")
+    private TextBoxNum txtTonenZoShokkenfukkatsu;
+
+    /*
+     * gettxtTonenZoShokkenfukkatsu
+     * @return txtTonenZoShokkenfukkatsu
+     */
+    @JsonProperty("txtTonenZoShokkenfukkatsu")
+    public TextBoxNum getTxtTonenZoShokkenfukkatsu() {
+        return txtTonenZoShokkenfukkatsu;
+    }
+
+    /*
+     * settxtTonenZoShokkenfukkatsu
+     * @param txtTonenZoShokkenfukkatsu txtTonenZoShokkenfukkatsu
+     */
+    @JsonProperty("txtTonenZoShokkenfukkatsu")
+    public void setTxtTonenZoShokkenfukkatsu(TextBoxNum txtTonenZoShokkenfukkatsu) {
+        this.txtTonenZoShokkenfukkatsu = txtTonenZoShokkenfukkatsu;
+    }
+
+    // </editor-fold>
+}
+/**
+ * TableCell44 のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+class TableCell44AttblSetaiSu extends TableCell {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2015-11-16_12-38-19">
+    @JsonProperty("txtTonenZorokugosaitotatsu")
+    private TextBoxNum txtTonenZorokugosaitotatsu;
+
+    /*
+     * gettxtTonenZorokugosaitotatsu
+     * @return txtTonenZorokugosaitotatsu
+     */
+    @JsonProperty("txtTonenZorokugosaitotatsu")
+    public TextBoxNum getTxtTonenZorokugosaitotatsu() {
+        return txtTonenZorokugosaitotatsu;
+    }
+
+    /*
+     * settxtTonenZorokugosaitotatsu
+     * @param txtTonenZorokugosaitotatsu txtTonenZorokugosaitotatsu
+     */
+    @JsonProperty("txtTonenZorokugosaitotatsu")
+    public void setTxtTonenZorokugosaitotatsu(TextBoxNum txtTonenZorokugosaitotatsu) {
+        this.txtTonenZorokugosaitotatsu = txtTonenZorokugosaitotatsu;
+    }
+
+    // </editor-fold>
+}
+/**
+ * TableCell45 のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+class TableCell45AttblSetaiSu extends TableCell {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2015-11-16_12-38-19">
+    @JsonProperty("txtTonenZoTekiyojogaihi")
+    private TextBoxNum txtTonenZoTekiyojogaihi;
+
+    /*
+     * gettxtTonenZoTekiyojogaihi
+     * @return txtTonenZoTekiyojogaihi
+     */
+    @JsonProperty("txtTonenZoTekiyojogaihi")
+    public TextBoxNum getTxtTonenZoTekiyojogaihi() {
+        return txtTonenZoTekiyojogaihi;
+    }
+
+    /*
+     * settxtTonenZoTekiyojogaihi
+     * @param txtTonenZoTekiyojogaihi txtTonenZoTekiyojogaihi
+     */
+    @JsonProperty("txtTonenZoTekiyojogaihi")
+    public void setTxtTonenZoTekiyojogaihi(TextBoxNum txtTonenZoTekiyojogaihi) {
+        this.txtTonenZoTekiyojogaihi = txtTonenZoTekiyojogaihi;
+    }
+
+    // </editor-fold>
+}
