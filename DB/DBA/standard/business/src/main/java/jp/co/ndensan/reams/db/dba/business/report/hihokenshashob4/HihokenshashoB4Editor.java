c5bf5960c8fe6b556c970209fb63125a86060553
dongyabin <dongyabin@liandisys.com.cn>
2016-02-01 17:00:58 +0800

    介護保険被保険者証（B4版）　レビュー対応

--- DB/DBA/standard/business/src/main/java/jp/co/ndensan/reams/db/dba/business/report/hihokenshashob4/HihokenshashoB4Editor.java
+++ DB/DBA/standard/business/src/main/java/jp/co/ndensan/reams/db/dba/business/report/hihokenshashob4/HihokenshashoB4Editor.java
@@ -16,17 +16,17 @@ import jp.co.ndensan.reams.uz.uza.lang.Separator;
 
 /**
  *
- * 被保険者証発行一覧表Editorです。
+ * 介護保険被保険者証（B4版）Editorです。
  */
 public class HihokenshashoB4Editor implements IHihokenshashoB4Editor {
     
-    private final HihokenshashoB4Item joho;
+    private final HihokenshashoB4BodyItem joho;
     
     /**
      * コンストラクタです。
      * @param joho 一覧表証発行者Entityリストです
      */
-    public HihokenshashoB4Editor(HihokenshashoB4Item joho) {
+    public HihokenshashoB4Editor(HihokenshashoB4BodyItem joho) {
         this.joho = joho;
     }
 
@@ -179,129 +179,129 @@ public class HihokenshashoB4Editor implements IHihokenshashoB4Editor {
     
     private HihokenshashoB4ReportSource editBody2(HihokenshashoB4ReportSource source) {
         source.seigen22 = joho.getSeigen2();
-        source.seigenLong22 = joho.getSeigenLong2();
-        source.seigenEd12 = joho.getSeigenEd1();
-        source.ninteiymd2 = joho.getNinteiymd();
+        source.seigenLong22 = joho.getSeigenLong22();
+        source.seigenEd12 = joho.getSeigenEd12();
+        source.ninteiymd2 = joho.getNinteiymd2();
         // TODO
-        source.yukokigen22 = joho.getYukokigen();
-        source.yukokigen2 = joho.getYukokigen();
-        source.seigen32 = joho.getSeigen3();
-        source.seigenLong32 = joho.getSeigenLong3();
-        source.seigenSt22 = joho.getSeigenSt2();
-        source.yukokikanst2 = joho.getYukokikanst();
-        source.yukokikaned2 = joho.getYukokikaned();
-        source.seigen42 = joho.getSeigen4();
-        source.seigenLong42 = joho.getSeigenLong4();
-        source.seigenEd22 = joho.getSeigenEd2();
-        source.hihokenshanno2 = joho.getHihokenshanno();
-        source.seigen52 = joho.getSeigen5();
-        source.seigenLong52 = joho.getSeigenLong5();
-        source.seigenSt32 = joho.getSeigenSt3();
-        source.yubinno2 = joho.getYubinno();
-        source.homonkikanst2 = joho.getHomonkikanst();
-        source.homonkikaned2 = joho.getHomonkikaned();
-        source.gyoseikuS2 = joho.getGyoseikuS();
-        source.seigen62 = joho.getSeigen6();
-        source.gyoseiku2 = joho.getGyoseiku();
-        source.seigenEd32 = joho.getSeigenEd3();
-        source.seigenLong62 = joho.getSeigenLong6();
-        source.hihojusho2 = joho.getHihojusho();
-        source.service12 = joho.getService1();
-        source.ten02 = joho.getTen0();
-        source.servicetani2 = joho.getServicetani();
-        source.gyosha4 = joho.getGyosha();
-        source.todokeYmd12 = joho.getTodokeYmd1();
-        source.shuruigendo12 = joho.getShuruigendo1();
-        source.ten12 = joho.getTen1();
-        source.shurui12 = joho.getShurui1();
-        source.gyosha5 = joho.getGyosha2();
+        source.yukokigen22 = joho.getYukokigen2();
+        source.yukokigen2 = joho.getYukokigen2();
+        source.seigen32 = joho.getSeigen32();
+        source.seigenLong32 = joho.getSeigenLong32();
+        source.seigenSt22 = joho.getSeigenSt22();
+        source.yukokikanst2 = joho.getYukokikanst2();
+        source.yukokikaned2 = joho.getYukokikaned2();
+        source.seigen42 = joho.getSeigen42();
+        source.seigenLong42 = joho.getSeigenLong42();
+        source.seigenEd22 = joho.getSeigenEd22();
+        source.hihokenshanno2 = joho.getHihokenshanno2();
+        source.seigen52 = joho.getSeigen52();
+        source.seigenLong52 = joho.getSeigenLong52();
+        source.seigenSt32 = joho.getSeigenSt32();
+        source.yubinno2 = joho.getYubinno2();
+        source.homonkikanst2 = joho.getHomonkikanst2();
+        source.homonkikaned2 = joho.getHomonkikaned2();
+        source.gyoseikuS2 = joho.getGyoseikuS2();
+        source.seigen62 = joho.getSeigen62();
+        source.gyoseiku2 = joho.getGyoseiku2();
+        source.seigenEd32 = joho.getSeigenEd32();
+        source.seigenLong62 = joho.getSeigenLong62();
+        source.hihojusho2 = joho.getHihojusho2();
+        source.service12 = joho.getService12();
+        source.ten02 = joho.getTen02();
+        source.servicetani2 = joho.getServicetani2();
+        source.gyosha4 = joho.getGyosha2();
+        source.todokeYmd12 = joho.getTodokeYmd12();
+        source.shuruigendo12 = joho.getShuruigendo12();
+        source.ten12 = joho.getTen12();
+        source.shurui12 = joho.getShurui12();
+        source.gyosha5 = joho.getGyosha22();
         source.gyosha5Asa = new RString("*");
-        source.gyosha5Massho = joho.getGyosha2();
-        source.shuruigendo22 = joho.getShuruigendo2();
-        source.ten22 = joho.getTen2();
-        source.shurui22 = joho.getShurui2();
-        source.shuruigendo32 = joho.getShuruigendo3();
-        source.ten32 = joho.getTen3();
-        source.shurui32 = joho.getShurui3();
-        source.hihokana2 = joho.getHihokana();
-        source.todokeYmd22 = joho.getTodokeYmd2();
+        source.gyosha5Massho = joho.getGyosha22();
+        source.shuruigendo22 = joho.getShuruigendo22();
+        source.ten22 = joho.getTen22();
+        source.shurui22 = joho.getShurui22();
+        source.shuruigendo32 = joho.getShuruigendo32();
+        source.ten32 = joho.getTen32();
+        source.shurui32 = joho.getShurui32();
+        source.hihokana2 = joho.getHihokana2();
+        source.todokeYmd22 = joho.getTodokeYmd22();
         source.todokeYmd2Asa2 = new RString("*");
-        source.todokeYmd2Massho2 = joho.getTodokeYmd2();
-        source.shuruigendo42 = joho.getShuruigendo4();
-        source.ten42 = joho.getTen4();
-        source.shurui42 = joho.getShurui4();
-        source.hihoname2 = joho.getHihoname();
-        source.gyosha6 = joho.getGyosha3();
+        source.todokeYmd2Massho2 = joho.getTodokeYmd22();
+        source.shuruigendo42 = joho.getShuruigendo42();
+        source.ten42 = joho.getTen42();
+        source.shurui42 = joho.getShurui42();
+        source.hihoname2 = joho.getHihoname2();
+        source.gyosha6 = joho.getGyosha32();
         source.gyosha6Asa = new RString("*");
-        source.gyosha6Massho = joho.getGyosha3();
-        source.shuruigendo52 = joho.getShuruigendo5();
-        source.ten52 = joho.getTen5();
-        source.shurui52 = joho.getShurui5();
-        source.shuruigendo62 = joho.getShuruigendo6();
-        source.ten62 = joho.getTen6();
-        source.shurui62 = joho.getShurui6();
-        source.ryui2 = joho.getRyui();
-        source.todokeYmd32 = joho.getTodokeYmd3();
+        source.gyosha6Massho = joho.getGyosha32();
+        source.shuruigendo52 = joho.getShuruigendo52();
+        source.ten52 = joho.getTen52();
+        source.shurui52 = joho.getShurui52();
+        source.shuruigendo62 = joho.getShuruigendo62();
+        source.ten62 = joho.getTen62();
+        source.shurui62 = joho.getShurui62();
+        source.ryui2 = joho.getRyui2();
+        source.todokeYmd32 = joho.getTodokeYmd32();
         source.todokeYmd3Asa2 = new RString("*");
-        source.todokeYmd3Massho2 = joho.getTodokeYmd3();
-        source.shisetsuSyu12 = joho.getShisetsuSyu1();
-        source.seibetsuWoman2 = joho.getSeibetsuWoman();
-        source.seibetsuMan2 = joho.getSeibetsuMan();
-        source.umareMeiji2 = joho.getUmareMeiji();
-        source.umareTaisho2 = joho.getUmareTaisho();
-        source.umareShowa2 = joho.getUmareShowa();
-        source.umareMm2 = joho.getUmareYy().isEmpty() ? RString.EMPTY : new RDate(joho
-                .getUmareYy().toString()).wareki().eraType(EraType.KANJI)
+        source.todokeYmd3Massho2 = joho.getTodokeYmd32();
+        source.shisetsuSyu12 = joho.getShisetsuSyu12();
+        source.seibetsuWoman2 = joho.getSeibetsuWoman2();
+        source.seibetsuMan2 = joho.getSeibetsuMan2();
+        source.umareMeiji2 = joho.getUmareMeiji2();
+        source.umareTaisho2 = joho.getUmareTaisho2();
+        source.umareShowa2 = joho.getUmareShowa2();
+        source.umareMm2 = joho.getUmareYy2().isEmpty() ? RString.EMPTY : new RDate(joho
+                .getUmareYy2().toString()).wareki().eraType(EraType.KANJI)
                 .firstYear(FirstYear.GAN_NEN).separator(Separator.JAPANESE)
                 .fillType(FillType.BLANK).getMonth();
-        source.umareDd2 = joho.getUmareYy().isEmpty() ? RString.EMPTY : new RDate(joho
-                .getUmareYy().toString()).wareki().eraType(EraType.KANJI)
+        source.umareDd2 = joho.getUmareYy2().isEmpty() ? RString.EMPTY : new RDate(joho
+                .getUmareYy2().toString()).wareki().eraType(EraType.KANJI)
                 .firstYear(FirstYear.GAN_NEN).separator(Separator.JAPANESE)
                 .fillType(FillType.BLANK).getDay();
-        source.umareMmS2 = joho.getUmareYy().isEmpty() ? RString.EMPTY : new RDate(joho
-                .getUmareYy().toString()).seireki().getMonth();
-        source.umareDdS2 = joho.getUmareYy().isEmpty() ? RString.EMPTY : new RDate(joho
-                .getUmareYy().toString()).seireki().getDay();
-        source.umareYy2 = joho.getUmareYy().isEmpty() ? RString.EMPTY : new RDate(joho
-                .getUmareYy().toString()).wareki().eraType(EraType.KANJI)
+        source.umareMmS2 = joho.getUmareYy2().isEmpty() ? RString.EMPTY : new RDate(joho
+                .getUmareYy2().toString()).seireki().getMonth();
+        source.umareDdS2 = joho.getUmareYy2().isEmpty() ? RString.EMPTY : new RDate(joho
+                .getUmareYy2().toString()).seireki().getDay();
+        source.umareYy2 = joho.getUmareYy2().isEmpty() ? RString.EMPTY : new RDate(joho
+                .getUmareYy2().toString()).wareki().eraType(EraType.KANJI)
                 .firstYear(FirstYear.GAN_NEN).separator(Separator.JAPANESE).fillType(FillType.BLANK).getYear();
-        source.umareYyyy2 = joho.getUmareYy().isEmpty() ? RString.EMPTY : new RDate(joho.getUmareYy().toString()).seireki().getYear();
-        source.saikofu12 = joho.getSaikofu1();
-        source.saikofu22 = joho.getSaikofu2();
-        source.shisetsuname12 = joho.getShisetsuname1();
-        source.kofuymdYy2 = joho.getKofuymdGengo().isEmpty() ? RString.EMPTY : new RDate(joho
-                .getKofuymdGengo().toString()).wareki().eraType(EraType.KANJI)
+        source.umareYyyy2 = joho.getUmareYy2().isEmpty() ? RString.EMPTY : new RDate(joho.getUmareYy().toString()).seireki().getYear();
+        source.saikofu12 = joho.getSaikofu12();
+        source.saikofu22 = joho.getSaikofu22();
+        source.shisetsuname12 = joho.getShisetsuname12();
+        source.kofuymdYy2 = joho.getKofuymdGengo2().isEmpty() ? RString.EMPTY : new RDate(joho
+                .getKofuymdGengo2().toString()).wareki().eraType(EraType.KANJI)
                 .firstYear(FirstYear.GAN_NEN).separator(Separator.JAPANESE).fillType(FillType.BLANK).getYear();
-        source.kofuymdMm2 = joho.getKofuymdGengo().isEmpty() ? RString.EMPTY : new RDate(joho
-                .getKofuymdGengo().toString()).wareki().eraType(EraType.KANJI)
+        source.kofuymdMm2 = joho.getKofuymdGengo2().isEmpty() ? RString.EMPTY : new RDate(joho
+                .getKofuymdGengo2().toString()).wareki().eraType(EraType.KANJI)
                 .firstYear(FirstYear.GAN_NEN).separator(Separator.JAPANESE).fillType(FillType.BLANK).getMonth();
-        source.kofuymdDd2 = joho.getKofuymdGengo().isEmpty() ? RString.EMPTY : new RDate(joho
-                .getKofuymdGengo().toString()).wareki().eraType(EraType.KANJI)
+        source.kofuymdDd2 = joho.getKofuymdGengo2().isEmpty() ? RString.EMPTY : new RDate(joho
+                .getKofuymdGengo2().toString()).wareki().eraType(EraType.KANJI)
                 .firstYear(FirstYear.GAN_NEN).separator(Separator.JAPANESE).fillType(FillType.BLANK).getDay();
-        source.kofuymdGengo2 = joho.getKofuymdGengo().isEmpty() ? RString.EMPTY : new RDate(joho
-                .getKofuymdGengo().toString()).wareki().eraType(EraType.KANJI)
+        source.kofuymdGengo2 = joho.getKofuymdGengo2().isEmpty() ? RString.EMPTY : new RDate(joho
+                .getKofuymdGengo2().toString()).wareki().eraType(EraType.KANJI)
                 .firstYear(FirstYear.GAN_NEN).separator(Separator.JAPANESE).fillType(FillType.BLANK).getEra();
-        source.nyushochk12 = joho.getNyushochk1();
-        source.nyuinchk12 = joho.getNyuinchk1();
-        source.nyushoymd12 = joho.getNyushoymd1();
-        source.taiinchk12 = joho.getTaiinchk1();
-        source.taishochk12 = joho.getTaishochk1();
-        source.taiinymd12 = joho.getTaiinymd1();
-        source.hokenshano12 = joho.getHokenshano1();
-        source.hokenshano22 = joho.getHokenshano2();
-        source.hokenshano32 = joho.getHokenshano3();
-        source.hokenshano42 = joho.getHokenshano4();
-        source.hokenshano52 = joho.getHokenshano5();
-        source.hokenshano62 = joho.getHokenshano6();
-        source.shisetsuSyu22 = joho.getShisetsuSyu2();
-        source.shisetsuname22 = joho.getShisetsuname2();
-        source.nyushoymd22 = joho.getNyushoymd2();
-        source.nyushochk22 = joho.getNyushochk2();
-        source.nyuinchk22 = joho.getNyuinchk2();
-        source.taiinymd22 = joho.getTaiinymd2();
-        source.renban2 = joho.getRenban();
-        source.taishochk22 = joho.getTaishochk2();
-        source.taiinchk22 = joho.getTaiinchk2();
+        source.nyushochk12 = joho.getNyushochk12();
+        source.nyuinchk12 = joho.getNyuinchk12();
+        source.nyushoymd12 = joho.getNyushoymd12();
+        source.taiinchk12 = joho.getTaiinchk12();
+        source.taishochk12 = joho.getTaishochk12();
+        source.taiinymd12 = joho.getTaiinymd12();
+        source.hokenshano12 = joho.getHokenshano12();
+        source.hokenshano22 = joho.getHokenshano22();
+        source.hokenshano32 = joho.getHokenshano32();
+        source.hokenshano42 = joho.getHokenshano42();
+        source.hokenshano52 = joho.getHokenshano52();
+        source.hokenshano62 = joho.getHokenshano62();
+        source.shisetsuSyu22 = joho.getShisetsuSyu22();
+        source.shisetsuname22 = joho.getShisetsuname22();
+        source.nyushoymd22 = joho.getNyushoymd22();
+        source.nyushochk22 = joho.getNyushochk22();
+        source.nyuinchk22 = joho.getNyuinchk22();
+        source.taiinymd22 = joho.getTaiinymd22();
+        source.renban2 = joho.getRenban2();
+        source.taishochk22 = joho.getTaishochk22();
+        source.taiinchk22 = joho.getTaiinchk22();
         source.itakuGyosha1 = joho.getItakuGyosha1();
         source.itakuGyosha2 = joho.getItakuGyosha2();
         source.itakuGyosha2Asa = new RString("*");
@@ -309,21 +309,21 @@ public class HihokenshashoB4Editor implements IHihokenshashoB4Editor {
         source.itakuGyosha3 = joho.getItakuGyosha3();
         source.itakuGyosha3Asa = new RString("*");
         source.itakuGyosha3Massho = joho.getItakuGyosha3();
-        source.seiShogai11 = joho.getSeiShogai1();
-        source.seiShogai21 = joho.getSeiShogai2();
-        source.kosekiSeibetsu11 = joho.getKosekiSeibetsu1();
-        source.kosekiSeibetsu21 = joho.getKosekiSeibetsu2();
-        source.itakuGyosha4 = joho.getItakuGyosha1();
-        source.itakuGyosha5 = joho.getItakuGyosha2();
+        source.seiShogai11 = joho.getSeiShogai12();
+        source.seiShogai21 = joho.getSeiShogai22();
+        source.kosekiSeibetsu11 = joho.getKosekiSeibetsu12();
+        source.kosekiSeibetsu21 = joho.getKosekiSeibetsu22();
+        source.itakuGyosha4 = joho.getItakuGyosha12();
+        source.itakuGyosha5 = joho.getItakuGyosha22();
         source.itakuGyosha5Asa = new RString("*");
-        source.itakuGyosha5Massho = joho.getItakuGyosha2();
-        source.itakuGyosha6 = joho.getItakuGyosha3();
+        source.itakuGyosha5Massho = joho.getItakuGyosha22();
+        source.itakuGyosha6 = joho.getItakuGyosha32();
         source.itakuGyosha6Asa = new RString("*");
-        source.itakuGyosha6Massho = joho.getItakuGyosha3();
-        source.seiShogai12 = joho.getSeiShogai1();
-        source.seiShogai22 = joho.getSeiShogai2();
-        source.kosekiSeibetsu12 = joho.getKosekiSeibetsu1();
-        source.kosekiSeibetsu22 = joho.getKosekiSeibetsu2();
+        source.itakuGyosha6Massho = joho.getItakuGyosha32();
+        source.seiShogai12 = joho.getSeiShogai12();
+        source.seiShogai22 = joho.getSeiShogai22();
+        source.kosekiSeibetsu12 = joho.getKosekiSeibetsu12();
+        source.kosekiSeibetsu22 = joho.getKosekiSeibetsu22();
         return source;
     }
     
