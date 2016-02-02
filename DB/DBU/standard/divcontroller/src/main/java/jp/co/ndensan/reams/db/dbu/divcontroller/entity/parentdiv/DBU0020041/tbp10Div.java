6fb955c579ff8a2650233fc8db862eb7697d2fd0
n8300姜井図 <n8300@ndensan.co.jp>
2016-02-02 14:19:05 +0900

    QA #74696回答（再）

---
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/entity/parentdiv/DBU0020041/tbp10Div.java
@@ -0,0 +1,67 @@
+package jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0020041;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.TableCell;
+import jp.co.ndensan.reams.uz.uza.ui.binding.TablePanel;
+
+/**
+ * tbp10 のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class tbp10Div extends TablePanel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：バージョン情報無し">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("cell211")
+    private cell211Attbp10 cell211;
+
+    /*
+     * [ Helperの作成 ]
+     * TablePanelコントロールに特化したヘルパーメソッドを作成
+     */
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * getcell211
+     * @return cell211
+     */
+    @JsonProperty("cell211")
+    private cell211Attbp10 getCell211() {
+        return cell211;
+    }
+
+    /*
+     * setcell211
+     * @param cell211 cell211
+     */
+    @JsonProperty("cell211")
+    private void setCell211(cell211Attbp10 cell211) {
+        this.cell211 = cell211;
+    }
+
+    // </editor-fold>
+}
+/**
+ * cell211 のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+class cell211Attbp10 extends TableCell {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：バージョン情報無し">
+
+    // </editor-fold>
+}
