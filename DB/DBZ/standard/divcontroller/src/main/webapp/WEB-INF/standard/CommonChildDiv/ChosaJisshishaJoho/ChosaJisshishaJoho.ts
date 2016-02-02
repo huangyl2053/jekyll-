3bd2ef3ccb7f7e03355b09b9b7d09ac809b0e61d
湯亜芳 <tangyf@liandisys.com.cn>
2016-02-02 18:27:08 +0800

    共有子divモード修正

--- DB/DBZ/standard/divcontroller/src/main/webapp/WEB-INF/standard/CommonChildDiv/ChosaJisshishaJoho/ChosaJisshishaJoho.ts
+++ DB/DBZ/standard/divcontroller/src/main/webapp/WEB-INF/standard/CommonChildDiv/ChosaJisshishaJoho/ChosaJisshishaJoho.ts
@@ -19,11 +19,54 @@ module DBZ
             public PublicProperties() {
                 return new PublicProperties(this.fieldName);
             }
+
+            public State() {
+                return new Modes.State(this.controls);
+            }
+
+
         }
 
         export module Modes {
+           export class State {
+                private controls: Controls;
+
+                constructor(controls: Controls) {
+                    this.controls = controls;
+                }
+                
+                public Input():void {
+                	this.controls.txtChosaJisshiDate ().readOnly = false;
+                    this.controls.ddlChosaJisshiBasho().readOnly = false;
+                    this.controls.txtJisshiBashoMeisho().readOnly = false;
+                    this.controls.ddlShozokuKikan().readOnly = false;
+                    this.controls.ddlKinyusha().readOnly = false;
+                    this.controls.txtChosaKubun().readOnly = true;
+                    this.controls.txtChosaJisshiDate().required = true;
+                    this.controls.ddlChosaJisshiBasho().required = true;
+                    this.controls.txtJisshiBashoMeisho().required = true;
+                    this.controls.ddlShozokuKikan().required = true;
+                    this.controls.ddlKinyusha().required = true;
+                }
+                
+                 public Shokai(): void {
+                    this.controls.txtChosaJisshiDate().readOnly = true;
+                    this.controls.ddlChosaJisshiBasho().readOnly = true;
+                    this.controls.txtJisshiBashoMeisho().readOnly = true;
+                    this.controls.ddlShozokuKikan().readOnly = true;
+                    this.controls.ddlKinyusha().readOnly = true;
+                    this.controls.txtChosaKubun().readOnly = true;
+                    this.controls.txtChosaJisshiDate().required = false;
+                    this.controls.ddlChosaJisshiBasho().required = false;
+                    this.controls.txtJisshiBashoMeisho().required = false;
+                    this.controls.ddlShozokuKikan().required = false;
+                    this.controls.ddlKinyusha().required = false;
+                }
+
+            }
+
         }
-    }
+ }
 }
 
 module DBZ
