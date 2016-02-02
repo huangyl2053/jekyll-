d85a2753a4b3055b362ebbad9a0d765da8bf21bc
wangchao <wangchao@liandisys.com.cn>
2016-02-02 10:13:59 +0800

    QA #71309の回答と伴って、仕様変更「1.01版」を対応

--- DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/core/kaigoconfiglistshutoku/KaigoConfigListShutoku.java
+++ DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/core/kaigoconfiglistshutoku/KaigoConfigListShutoku.java
@@ -6,13 +6,14 @@
 package jp.co.ndensan.reams.db.dbz.business.core.kaigoconfiglistshutoku;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.util.config.BusinessConfig;
 
 /**
- * 介護Config取得クラスです。
+ * 介護Config取得のビジネスクラスです。
  *
  */
 public class KaigoConfigListShutoku {
@@ -20,14 +21,13 @@ public class KaigoConfigListShutoku {
     /**
      * 介護Configリスト取得処理。
      *
-     * @param コンフィグ コンフィグ
+     * @param enumList Enum コンフィグのリスト
      * @return Map (コンフィグ名,コンフィグ値)
      */
-    public Map<Enum, Object> getKaigoConfigList(Class コンフィグ) {
-            Map<Enum, Object> kaigoConfigMap = new HashMap();
-        for (Enum コンフィグキー : ((Class<Enum>) コンフィグ).getEnumConstants()) {
+    public Map<Enum, Object> getKaigoConfigList(List<Enum> enumList) {
+        Map<Enum, Object> kaigoConfigMap = new HashMap();
+        for (Enum コンフィグキー : enumList) {
             RString コンフィグ名称 = BusinessConfig.get(コンフィグキー);
-
             kaigoConfigMap.put(コンフィグキー, コンフィグ名称);
         }
         return kaigoConfigMap;
@@ -36,13 +36,13 @@ public class KaigoConfigListShutoku {
     /**
      * 介護Config取得処理（サブ業務指定）。
      *
-     * @param コンフィグ コンフィグ
+     * @param enumList Enum コンフィグのリスト
      * @param サブ業務コード サブ業務コード
      * @return Map (コンフィグ名,コンフィグ値)
      */
-    public Map<Enum, Object> getKaigoConfigSbuList(Class コンフィグ, SubGyomuCode サブ業務コード) {
+    public Map<Enum, Object> getKaigoConfigSbuList(List<Enum> enumList, SubGyomuCode サブ業務コード) {
         Map<Enum, Object> kaigoConfigMap = new HashMap();
-        for (Enum コンフィグキー : ((Class<Enum>) コンフィグ).getEnumConstants()) {
+        for (Enum コンフィグキー : enumList) {
             RString コンフィグ名称 = BusinessConfig.get(コンフィグキー, サブ業務コード);
             kaigoConfigMap.put(コンフィグキー, コンフィグ名称);
         }
