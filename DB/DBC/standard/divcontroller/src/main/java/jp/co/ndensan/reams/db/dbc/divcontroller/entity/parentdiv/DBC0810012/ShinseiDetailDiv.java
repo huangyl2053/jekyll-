e825ea0714b01d064c3fcf704a71d5cf3dfec9dc
許鵬 <pxu@sinoi.com.cn>
2016-02-02 10:12:53 +0800

    DBC-1010-170 償還払い状況照会_申請情報照会 修正

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0810012/ShinseiDetailDiv.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0810012/ShinseiDetailDiv.java
@@ -4,11 +4,8 @@ package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0810012;
  * このファイルへの変更は、再生成時には損失するため
  * 不正な動作の原因になります。
  */
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jp.co.ndensan.reams.uz.uza.ui.binding.*;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
-import jp.co.ndensan.reams.uz.uza.ui.binding.domain.*;
 
 /**
  * ShinseiDetail のクラスファイル 
@@ -88,139 +85,6 @@ public class ShinseiDetailDiv extends Panel {
     @JsonProperty("pnlShinsei")
     public void setPnlShinsei(pnlShinseiDiv pnlShinsei) {
         this.pnlShinsei = pnlShinsei;
-    }
-
-    /*
-     * [ ショートカットの作成 ]
-     */
-    @JsonIgnore
-    public TextBoxDate getTxtShinseiYMD() {
-        return this.getPnlShinsei().getTxtShinseiYMD();
-    }
-
-    @JsonIgnore
-    public void  setTxtShinseiYMD(TextBoxDate txtShinseiYMD) {
-        this.getPnlShinsei().setTxtShinseiYMD(txtShinseiYMD);
-    }
-
-    @JsonIgnore
-    public TextBoxDate getTxtUketsukeYMD() {
-        return this.getPnlShinsei().getTxtUketsukeYMD();
-    }
-
-    @JsonIgnore
-    public void  setTxtUketsukeYMD(TextBoxDate txtUketsukeYMD) {
-        this.getPnlShinsei().setTxtUketsukeYMD(txtUketsukeYMD);
-    }
-
-    @JsonIgnore
-    public RadioButton getRdoShinseisyaKubun() {
-        return this.getPnlShinsei().getRdoShinseisyaKubun();
-    }
-
-    @JsonIgnore
-    public void  setRdoShinseisyaKubun(RadioButton rdoShinseisyaKubun) {
-        this.getPnlShinsei().setRdoShinseisyaKubun(rdoShinseisyaKubun);
-    }
-
-    @JsonIgnore
-    public TextBoxCode getTxtKisaiHokensyaBango() {
-        return this.getPnlShinsei().getTxtKisaiHokensyaBango();
-    }
-
-    @JsonIgnore
-    public void  setTxtKisaiHokensyaBango(TextBoxCode txtKisaiHokensyaBango) {
-        this.getPnlShinsei().setTxtKisaiHokensyaBango(txtKisaiHokensyaBango);
-    }
-
-    @JsonIgnore
-    public CheckBoxList getChkKokuhorenSaiso() {
-        return this.getPnlShinsei().getChkKokuhorenSaiso();
-    }
-
-    @JsonIgnore
-    public void  setChkKokuhorenSaiso(CheckBoxList chkKokuhorenSaiso) {
-        this.getPnlShinsei().setChkKokuhorenSaiso(chkKokuhorenSaiso);
-    }
-
-    @JsonIgnore
-    public TextBoxKana getTxtShimeikana() {
-        return this.getPnlShinsei().getTxtShimeikana();
-    }
-
-    @JsonIgnore
-    public void  setTxtShimeikana(TextBoxKana txtShimeikana) {
-        this.getPnlShinsei().setTxtShimeikana(txtShimeikana);
-    }
-
-    @JsonIgnore
-    public TextBox getTxtShimeiKanji() {
-        return this.getPnlShinsei().getTxtShimeiKanji();
-    }
-
-    @JsonIgnore
-    public void  setTxtShimeiKanji(TextBox txtShimeiKanji) {
-        this.getPnlShinsei().setTxtShimeiKanji(txtShimeiKanji);
-    }
-
-    @JsonIgnore
-    public TextBoxTelNo getTxtTelNo() {
-        return this.getPnlShinsei().getTxtTelNo();
-    }
-
-    @JsonIgnore
-    public void  setTxtTelNo(TextBoxTelNo txtTelNo) {
-        this.getPnlShinsei().setTxtTelNo(txtTelNo);
-    }
-
-    @JsonIgnore
-    public TextBoxMultiLine getTxtMulShinseiRiyu() {
-        return this.getPnlShinsei().getTxtMulShinseiRiyu();
-    }
-
-    @JsonIgnore
-    public void  setTxtMulShinseiRiyu(TextBoxMultiLine txtMulShinseiRiyu) {
-        this.getPnlShinsei().setTxtMulShinseiRiyu(txtMulShinseiRiyu);
-    }
-
-    @JsonIgnore
-    public TextBoxNum getTxtNumShiharaKingakuGk() {
-        return this.getPnlShinsei().getTxtNumShiharaKingakuGk();
-    }
-
-    @JsonIgnore
-    public void  setTxtNumShiharaKingakuGk(TextBoxNum txtNumShiharaKingakuGk) {
-        this.getPnlShinsei().setTxtNumShiharaKingakuGk(txtNumShiharaKingakuGk);
-    }
-
-    @JsonIgnore
-    public TextBoxNum getTxtNumHokentaisyoHiyouGaku() {
-        return this.getPnlShinsei().getTxtNumHokentaisyoHiyouGaku();
-    }
-
-    @JsonIgnore
-    public void  setTxtNumHokentaisyoHiyouGaku(TextBoxNum txtNumHokentaisyoHiyouGaku) {
-        this.getPnlShinsei().setTxtNumHokentaisyoHiyouGaku(txtNumHokentaisyoHiyouGaku);
-    }
-
-    @JsonIgnore
-    public TextBoxNum getTxtNumHokenKyufuGaku() {
-        return this.getPnlShinsei().getTxtNumHokenKyufuGaku();
-    }
-
-    @JsonIgnore
-    public void  setTxtNumHokenKyufuGaku(TextBoxNum txtNumHokenKyufuGaku) {
-        this.getPnlShinsei().setTxtNumHokenKyufuGaku(txtNumHokenKyufuGaku);
-    }
-
-    @JsonIgnore
-    public TextBoxNum getTxtNumRiyoshaFutanGaku() {
-        return this.getPnlShinsei().getTxtNumRiyoshaFutanGaku();
-    }
-
-    @JsonIgnore
-    public void  setTxtNumRiyoshaFutanGaku(TextBoxNum txtNumRiyoshaFutanGaku) {
-        this.getPnlShinsei().setTxtNumRiyoshaFutanGaku(txtNumRiyoshaFutanGaku);
     }
 
     // </editor-fold>
