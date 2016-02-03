e30145c279db8ecfe50c695c44b18b95c3561c52
huangh <huangh@liandisys.com>
2016-02-03 10:40:10 +0800

    entity追加

--- DB/DBZ/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbz/entity/db/hihokenshoshikakushohakko/ServiceTypeListEntity.java
+++ DB/DBZ/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbz/entity/db/hihokenshoshikakushohakko/ServiceTypeListEntity.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dba.entity.db.hihokenshoshikakushohakko;
+package jp.co.ndensan.reams.db.dbz.entity.db.hihokenshoshikakushohakko;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 
