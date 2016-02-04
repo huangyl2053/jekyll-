bbe5d7dfc448b546255b5875e2804c236528d84e
陳奥奇 <luaoqi@ustcsoft.com>
2016-02-04 11:45:28 +0800

    DBC-1020-060 福祉用具購入費支給限度額判定

--- DB/DBC/standard/service/src/main/java/jp/co/ndensan/reams/db/dbc/service/core/fukushiyogukonyuhishikyushisei/FukushiyoguKonyuhiShikyuGendogakuValidate.java
+++ DB/DBC/standard/service/src/main/java/jp/co/ndensan/reams/db/dbc/service/core/fukushiyogukonyuhishikyushisei/FukushiyoguKonyuhiShikyuGendogakuValidate.java
@@ -40,9 +40,8 @@ public class FukushiyoguKonyuhiShikyuGendogakuValidate {
             RString 様式番号,
             RString 明細番号, Decimal 今回の保険対象費用額) {
         boolean flag = false;
-        if (被保険者番号 == null || サービス提供年月 == null
-                || 事業者番号 == null || 様式番号 == null
-                || 明細番号 == null || 今回の保険対象費用額 == null) {
+        if (被保険者番号 == null || サービス提供年月 == null || 事業者番号 == null
+                || 様式番号 == null || 明細番号 == null || 今回の保険対象費用額 == null) {
             return flag;
         }
         Decimal 支給限度額 = sut.getShikyuGendogaku(被保険者番号, サービス提供年月);
@@ -53,18 +52,15 @@ public class FukushiyoguKonyuhiShikyuGendogakuValidate {
                 様式番号,
                 明細番号);
         Decimal 今までの保険者対象費用額 = entity.get保険対象費用額();
-        Decimal 今回登録済みの保険対象費用額 = new Decimal(0);
-        Decimal 前回までの保険対象費用額 = new Decimal(0);
-        if (整理番号 == null || 整理番号.isEmpty()) {
-            前回までの保険対象費用額 = 今までの保険者対象費用額;
-        } else {
+        Decimal 前回までの保険対象費用額 = 今までの保険者対象費用額;
+        if (整理番号 != null && !整理番号.isEmpty()) {
             entity = sut.getShokanShiharaiKekka(被保険者番号,
                     サービス提供年月,
                     整理番号,
                     事業者番号,
                     様式番号,
                     明細番号);
-            今回登録済みの保険対象費用額 = entity.get保険対象費用額();
+            Decimal 今回登録済みの保険対象費用額 = entity.get保険対象費用額();
             前回までの保険対象費用額 = 今までの保険者対象費用額.subtract(今回登録済みの保険対象費用額);
         }
         Decimal 保険対象費用額 = 前回までの保険対象費用額.add(今回の保険対象費用額);
