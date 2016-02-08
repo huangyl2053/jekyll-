2cada853ce8b014f330e482b361cb33ff21bc998
n8300姜井図 <n8300@ndensan.co.jp>
2016-02-08 15:26:57 +0900

    QA#75469回答

--- DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/entity/parentdiv/DBU0020021/FirstHihokensyasuDiv.java
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/entity/parentdiv/DBU0020021/FirstHihokensyasuDiv.java
@@ -4,7 +4,9 @@ package jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0020021;
  * このファイルへの変更は、再生成時には損失するため
  * 不正な動作の原因になります。
  */
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
 
 /**
@@ -45,6 +47,79 @@ public class FirstHihokensyasuDiv extends Panel {
     @JsonProperty("tplFirstHihokensyasu")
     public void setTplFirstHihokensyasu(tplFirstHihokensyasuDiv tplFirstHihokensyasu) {
         this.tplFirstHihokensyasu = tplFirstHihokensyasu;
+    }
+
+    /*
+     * [ ショートカットの作成 ]
+     */
+    @JsonIgnore
+    public TextBoxNum getTxtZenGetsumatsugenzailrokugoijonanagomiman() {
+        return this.getTplFirstHihokensyasu().getTxtZenGetsumatsugenzailrokugoijonanagomiman();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtToGetsumatsugenzailrokugoijonanagomiman() {
+        return this.getTplFirstHihokensyasu().getTxtToGetsumatsugenzailrokugoijonanagomiman();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtZennGetsumatsugenzaiKoki() {
+        return this.getTplFirstHihokensyasu().getTxtZennGetsumatsugenzaiKoki();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtToGetsumatsugenzaiKoki() {
+        return this.getTplFirstHihokensyasu().getTxtToGetsumatsugenzaiKoki();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtZenGetsumatsugenzaiGai() {
+        return this.getTplFirstHihokensyasu().getTxtZenGetsumatsugenzaiGai();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtToGetsumatsugenzaiGai() {
+        return this.getTplFirstHihokensyasu().getTxtToGetsumatsugenzaiGai();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtZenGetsumatsugenzaiJusho() {
+        return this.getTplFirstHihokensyasu().getTxtZenGetsumatsugenzaiJusho();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtToGetsumatsugenzaiJusho() {
+        return this.getTplFirstHihokensyasu().getTxtToGetsumatsugenzaiJusho();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtZenGetsumatsugenzaiKei() {
+        return this.getTplFirstHihokensyasu().getTxtZenGetsumatsugenzaiKei();
+    }
+
+    @JsonIgnore
+    public Label getLblSanko1() {
+        return this.getTplFirstHihokensyasu().getLblSanko1();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtSanko1() {
+        return this.getTplFirstHihokensyasu().getTxtSanko1();
+    }
+
+    @JsonIgnore
+    public Label getLblSanko2() {
+        return this.getTplFirstHihokensyasu().getLblSanko2();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtSanko2() {
+        return this.getTplFirstHihokensyasu().getTxtSanko2();
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtToGetsumatsugenzaiKei() {
+        return this.getTplFirstHihokensyasu().getTxtToGetsumatsugenzaiKei();
     }
 
     // </editor-fold>
