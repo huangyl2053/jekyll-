7c634beb4d33ac09bc87ff87d98f9238d7fb0919
久保田　英男 <n8187@ndensan.co.jp>
2016-02-13 13:07:32 +0900

    QA#75416対応 DBE2020004_認定調査スケジュール登録4 完了メッセージ追加、完了ボタン追加

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE2020004/DBE2020004TransitionEventName.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE2020004/DBE2020004TransitionEventName.java
@@ -1,26 +1,24 @@
 package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE2020004;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 
 /**
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBE2020004の状態名定義クラスです
+ * DBE2020004のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBE2020004StateName implements IStateEnumerations {
+public enum DBE2020004TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    NoChange(""),
-    スケジュールメモ("スケジュールメモ"),
-    完了メッセージ表示("完了メッセージ表示");
+    戻る("戻る");
 
     private final RString name;
 
-    private DBE2020004StateName(String name) {
+    private DBE2020004TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
