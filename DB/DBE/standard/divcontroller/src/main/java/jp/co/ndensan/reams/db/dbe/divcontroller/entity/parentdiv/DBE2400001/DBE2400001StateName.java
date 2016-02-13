f1468294d7181ed10c0fc1644aa4cc6f5e3d736e
久保田　英男 <n8187@ndensan.co.jp>
2016-02-13 15:21:20 +0900

    QA#75435対応 DBE2400001_依頼書一括発行

---
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE2400001/DBE2400001StateName.java
@@ -0,0 +1,37 @@
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE2400001;
+
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+
+/**
+ * このコードはツールによって生成されました。</br>
+ * このファイルへの変更は、再生成時には損失するため</br>
+ * 不正な動作の原因になります。</br>
+ * DBE2400001の状態名定義クラスです
+ *
+ * @author 自動生成
+ */
+public enum DBE2400001StateName implements IStateEnumerations {
+// <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
+
+    NoChange(""),
+    認定調査依頼("認定調査依頼"),
+    主治医意見書作成依頼("主治医意見書作成依頼");
+
+    private final RString name;
+
+    private DBE2400001StateName(String name) {
+        this.name = new RString(name);
+    }
+
+    /**
+     * getName
+     *
+     * @return 名前
+     */
+    @Override
+    public RString getName() {
+        return name;
+    }
+// </editor-fold>
+}
