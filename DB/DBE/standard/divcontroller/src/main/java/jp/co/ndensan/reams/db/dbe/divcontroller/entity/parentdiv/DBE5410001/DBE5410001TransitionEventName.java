f76735d657d085f225f9ef7f1116d47898d87d9f
久保田　英男 <n8187@ndensan.co.jp>
2016-02-15 14:44:46 +0900

    QA#74663対応 DBE5510001_要介護認定進捗状況照会 DBE5410001_要介護認定個人状況照会

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE5410001/DBE5410001TransitionEventName.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE5410001/DBE5410001TransitionEventName.java
@@ -1,4 +1,4 @@
-package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE5510001;
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE5410001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
@@ -7,19 +7,21 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBE5510001のイベント名定義クラスです</br>
+ * DBE5410001のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBE5510001TransitionEventName implements IContainerEvents {
+public enum DBE5410001TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
     処理完了("処理完了"),
-    個人状況照会に遷移する("個人状況照会に遷移する");
+    検索に戻る("検索に戻る"),
+    一覧に戻る("一覧に戻る"),
+    進捗状況照会に遷移する("進捗状況照会に遷移する");
 
     private final RString name;
 
-    private DBE5510001TransitionEventName(String name) {
+    private DBE5410001TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
