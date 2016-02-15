ee1e68f6f69b8c568f0b8340f348d06fa1310719
houtianpeng <houtianpeng@liandisys.com.cn>
2016-02-15 09:31:13 +0800

    認定調査スケジュール登録4

--- DB/DBE/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbe/definition/enumeratedtype/importance/Importance.java
+++ DB/DBE/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbe/definition/enumeratedtype/importance/Importance.java
@@ -1,63 +1,63 @@
-package jp.co.ndensan.reams.db.dbe.definition.enumeratedtype.Shinsei;
+package jp.co.ndensan.reams.db.dbe.definition.enumeratedtype.importance;
 
 import jp.co.ndensan.reams.ur.urz.definition.message.UrSystemErrorMessages;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
- * 調査区分を表す列挙型です。
+ * メモ重要度を表す列挙型です。
  *
- * @author LDNS
+ *
  */
-public enum ChosaKubun {
+public enum Importance {
 
     /**
-     * コード:0 名称:新規調査 略称:定義なし
+     * コード:1 名称:重要
      */
-    新規調査("0", "新規調査"),
+    重要("1", "重要"),
     /**
-     * コード:1 名称:再調査 略称:定義なし
+     * コード:2 名称:通常
      */
-    再調査("1", "再調査");
+    通常("2", "通常");
 
     private final RString code;
     private final RString fullName;
 
-    private ChosaKubun(String code, String fullname) {
+    private Importance(String code, String fullname) {
         this.code = new RString(code);
         this.fullName = new RString(fullname);
     }
 
     /**
-     * 調査区分のコードを返します。
+     * メモ重要度のコードを返します。
      *
-     * @return 調査区分のコード
+     * @return メモ重要度のコード
      */
     public RString getコード() {
         return code;
     }
 
     /**
-     * 調査区分の名称を返します。
+     * メモ重要度の名称を返します。
      *
-     * @return 調査区分の名称
+     * @return メモ重要度の名称
      */
     public RString get名称() {
         return fullName;
     }
 
     /**
-     * 調査区分のコードと一致する内容を探します。
+     * メモ重要度のコードと一致する内容を探します。
      *
-     * @param code 調査区分のコード
-     * @return {@code code} に対応する調査区分
+     * @param code メモ重要度のコード
+     * @return {@code code} に対応するメモ重要度
      */
-    public static ChosaKubun toValue(RString code) {
+    public static Importance toValue(RString code) {
 
-        for (ChosaKubun chosaKubun : ChosaKubun.values()) {
+        for (Importance chosaKubun : Importance.values()) {
             if (chosaKubun.code.equals(code)) {
                 return chosaKubun;
             }
         }
-        throw new IllegalArgumentException(UrSystemErrorMessages.変換不可.getReplacedMessage("調査区分"));
+        throw new IllegalArgumentException(UrSystemErrorMessages.変換不可.getReplacedMessage("メモ重要度"));
     }
 }
