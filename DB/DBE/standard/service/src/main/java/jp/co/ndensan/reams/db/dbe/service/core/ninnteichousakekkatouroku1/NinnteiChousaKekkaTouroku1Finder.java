e3bad8f59432bf163182172eaa37debf6a8bdbfe
xuyue <xuyue@liandisys.com.cn>
2016-02-17 08:59:45 +0800

    ソース修正

--- DB/DBE/standard/service/src/main/java/jp/co/ndensan/reams/db/dbe/service/core/ninnteichousakekkatouroku1/NinnteiChousaKekkaTouroku1Finder.java
+++ DB/DBE/standard/service/src/main/java/jp/co/ndensan/reams/db/dbe/service/core/ninnteichousakekkatouroku1/NinnteiChousaKekkaTouroku1Finder.java
@@ -5,11 +5,17 @@
  */
 package jp.co.ndensan.reams.db.dbe.service.core.ninnteichousakekkatouroku1;
 
+import jp.co.ndensan.reams.db.dbe.business.core.ninnteichousakekkatouroku1.TempData;
+import jp.co.ndensan.reams.db.dbe.definition.mybatisprm.dbe2210001.NinteiChosaJohoMybatisParameter;
+import jp.co.ndensan.reams.db.dbe.entity.ninnteichousakekkatouroku1.TempDataEntity;
 import jp.co.ndensan.reams.db.dbe.persistence.core.basic.MapperProvider;
+import jp.co.ndensan.reams.db.dbe.persistence.db.mapper.relate.dbe2210001.IDbe2210001RelateMapper;
+import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShinseishoKanriNo;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.util.di.InstanceProvider;
 
 /**
- * 認定調査結果登録1検索用クラスです
+ * 認定調査結果登録1検索用クラスです。
  */
 public class NinnteiChousaKekkaTouroku1Finder {
 
@@ -22,5 +28,19 @@ public class NinnteiChousaKekkaTouroku1Finder {
     public static NinnteiChousaKekkaTouroku1Finder createInstance() {
         return InstanceProvider.create(NinnteiChousaKekkaTouroku1Finder.class);
 
+    }
+
+    public TempData get認定調査情報(int temp_認定調査履歴番号,
+            RString temp_認定調査委託先コード,
+            ShinseishoKanriNo temp_申請書管理番号) {
+        IDbe2210001RelateMapper mapper = mapperProvider.create(IDbe2210001RelateMapper.class);
+        TempDataEntity entity = mapper.get認定調査情報(new NinteiChosaJohoMybatisParameter(temp_認定調査履歴番号, temp_認定調査委託先コード, temp_申請書管理番号));
+        
+        TempData data = new TempData();
+        data.setTemp_厚労省IF識別コード(entity.getTemp_厚労省IF識別コード());
+        data.setTemp_認定調査依頼区分コード(entity.getTemp_認定調査依頼区分コード());   
+        data.setTemp_認定調査回数(entity.getTemp_認定調査回数());
+        data.setTemp_認定調査委託区分コード(entity.getTemp_認定調査依頼区分コード());
+        return data;
     }
 }
