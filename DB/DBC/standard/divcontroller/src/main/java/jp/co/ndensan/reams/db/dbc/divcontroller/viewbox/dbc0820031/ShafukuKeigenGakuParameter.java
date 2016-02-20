a7dfcab756743080be05f7b0af48c0be8446f2fd
徐浩 <xuhao@ustcsoft.com>
2016-02-20 17:52:57 +0800

    画面設計_DBCMN53001_償還払い費支給申請決定_サービス提供証明書(社福軽減額）改正

---
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/viewbox/dbc0820031/ShafukuKeigenGakuParameter.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jp.co.ndensan.reams.db.dbc.divcontroller.viewbox.dbc0820031;
+
+import java.io.Serializable;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import lombok.Getter;
+
+/**
+ *
+ * @author yebangqiang
+ */
+@SuppressWarnings("PMD.UnusedPrivateField")
+@Getter
+public class ShafukuKeigenGakuParameter implements Serializable {
+    
+    private static final long serialVersionUID = -6680438140785085186L;
+    
+    private final RString hiHokenshaNo;
+    private final RDate serviceTeikyoYM;
+    private final RString seiriNp;
+    private final RString jigyoshaNo;
+    private final RString yoshikiNo;
+    private final RString meisaiNo;
+
+    public ShafukuKeigenGakuParameter(RString hiHokenshaNo, RDate serviceTeikyoYM, RString seiriNp, RString jigyoshaNo, RString yoshikiNo, RString meisaiNo) {
+        this.hiHokenshaNo = hiHokenshaNo;
+        this.serviceTeikyoYM = serviceTeikyoYM;
+        this.seiriNp = seiriNp;
+        this.jigyoshaNo = jigyoshaNo;
+        this.yoshikiNo = yoshikiNo;
+        this.meisaiNo = meisaiNo;
+    }
+    
+    
+}
