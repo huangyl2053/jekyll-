4e4ffacb7bad78a35543cda1ace98646d156ac37
龚亮 <gongliang@ustcsoft.com>
2016-02-21 15:19:04 +0800

    画面設計_DBUMN22002_広域内転居結果一覧表  コード  更新します

--- DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/controller/parentdiv/dbu0700011/BatchParamterInfo.java
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/controller/parentdiv/dbu0700011/BatchParamterInfo.java
@@ -5,24 +5,26 @@
  */
 package jp.co.ndensan.reams.db.dbu.divcontroller.controller.parentdiv.dbu0700011;
 
+import jp.co.ndensan.reams.db.dbu.definition.batchprm.hiroshimadomain.HiroshimaDomainBatchParameter;
 import jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0700011.BatchParamterInfoDiv;
 import jp.co.ndensan.reams.db.dbu.divcontroller.handler.parentdiv.dbu0700011.BatchParamterInfoHandler;
 import jp.co.ndensan.reams.db.dbz.business.core.koikizenshichosonjoho.KoikiZenShichosonJoho;
 import jp.co.ndensan.reams.db.dbz.service.core.basic.koikishichosonjoho.KoikiShichosonJohoFinder;
-import jp.co.ndensan.reams.ur.urz.definition.message.UrQuestionMessages;
+import jp.co.ndensan.reams.ur.urz.definition.message.UrErrorMessages;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
+import jp.co.ndensan.reams.uz.uza.lang.ApplicationException;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.message.MessageDialogSelectedResult;
-import jp.co.ndensan.reams.uz.uza.message.QuestionMessage;
-import jp.co.ndensan.reams.uz.uza.ui.servlets.ResponseHolder;
+import jp.co.ndensan.reams.uz.uza.ui.servlets.CommonButtonHolder;
 import jp.co.ndensan.reams.uz.uza.util.db.SearchResult;
 
 /**
  * 広域内転居結果一覧表作成画面Divを制御します。
  *
- * @author きょう亮
  */
 public class BatchParamterInfo {
+
+    private final RString 実行ボタン = new RString("BatchRegister");
 
     public ResponseData<BatchParamterInfoDiv> onLoad(BatchParamterInfoDiv div) {
         SearchResult<KoikiZenShichosonJoho> 市町村List = KoikiShichosonJohoFinder.createInstance().getGenShichosonJoho();
@@ -30,19 +32,25 @@ public class BatchParamterInfo {
         return ResponseData.of(div).respond();
     }
 
-    public ResponseData<BatchParamterInfoDiv> onClick(BatchParamterInfoDiv div) {
-        getHandler(div).is入力チェック();
-        if (!ResponseHolder.isReRequest()) {
-            QuestionMessage message = new QuestionMessage(UrQuestionMessages.処理実行の確認.getMessage().getCode(),
-                    UrQuestionMessages.処理実行の確認.getMessage().evaluate());
-            return ResponseData.of(div).addMessage(message).respond();
+    public ResponseData<BatchParamterInfoDiv> onClick_btnCheck(BatchParamterInfoDiv div) {
+        FlexibleDate kaishihiValue = div.getTxtkaishihi().getValue();
+        FlexibleDate shohiValue = div.getTxtshohi().getValue();
+        if (null == kaishihiValue || kaishihiValue.isEmpty()
+                || shohiValue == null || shohiValue.isEmpty()) {
+            CommonButtonHolder.setDisabledByCommonButtonFieldName(実行ボタン, true);
+            throw new ApplicationException(UrErrorMessages.期間が不正.getMessage());
+        } else if (shohiValue.isBefore(kaishihiValue)) {
+            CommonButtonHolder.setDisabledByCommonButtonFieldName(実行ボタン, true);
+            throw new ApplicationException(UrErrorMessages.期間が不正.getMessage());
         }
-        if (new RString(UrQuestionMessages.処理実行の確認.getMessage().getCode())
-                .equals(ResponseHolder.getMessageCode())
-                && ResponseHolder.getButtonType() == MessageDialogSelectedResult.Yes) {
-//            getHandler(div).click実行();
-        } 
         return ResponseData.of(div).respond();
+    }
+
+    public ResponseData<HiroshimaDomainBatchParameter> click実行(BatchParamterInfoDiv div) {
+        HiroshimaDomainBatchParameter paramter = getHandler(div).setBatchParamter();
+        ResponseData<HiroshimaDomainBatchParameter> responseData = new ResponseData<>();
+        responseData.data = getHandler(div).setBatchParamter();
+        return ResponseData.of(paramter).respond();
     }
 
     private BatchParamterInfoHandler getHandler(BatchParamterInfoDiv div) {
--- DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/handler/parentdiv/dbu0700011/BatchParamterInfoHandler.java
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/handler/parentdiv/dbu0700011/BatchParamterInfoHandler.java
@@ -3,35 +3,38 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package jp.co.ndensan.reams.db.dbu.divcontroller.handler.parentdiv.dbu0700011;
 
 import java.util.ArrayList;
 import java.util.List;
 import jp.co.ndensan.reams.db.dbu.definition.batchprm.hiroshimadomain.HiroshimaDomainBatchParameter;
-import jp.co.ndensan.reams.db.dbu.definition.batchprm.kouikitenkyoresultlist.KouikiTenkyoResultListSakuseiParameter;
-import jp.co.ndensan.reams.db.dbu.definition.batchprm.kouikitenkyoresultlist.KouikiTenkyoResultListBatchParameterSakusei;
 import jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0700011.BatchParamterInfoDiv;
 import jp.co.ndensan.reams.db.dbz.business.core.koikizenshichosonjoho.KoikiZenShichosonJoho;
+import jp.co.ndensan.reams.ua.uax.business.core.shikibetsutaisho.search.ShikibetsuTaishoPSMSearchKeyBuilder;
+import jp.co.ndensan.reams.ua.uax.definition.core.enumeratedtype.shikibetsutaisho.KensakuYusenKubun;
+import jp.co.ndensan.reams.ua.uax.definition.core.enumeratedtype.shikibetsutaisho.psm.DataShutokuKubun;
 import jp.co.ndensan.reams.ua.uax.definition.mybatisprm.shikibetsutaisho.IShikibetsuTaishoPSMSearchKey;
-import jp.co.ndensan.reams.ur.urz.definition.message.UrErrorMessages;
+import jp.co.ndensan.reams.ur.urz.definition.core.shikibetsutaisho.JuminJotai;
+import jp.co.ndensan.reams.ur.urz.definition.core.shikibetsutaisho.JuminShubetsu;
+import jp.co.ndensan.reams.uz.uza.biz.GyomuCode;
 import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
-import jp.co.ndensan.reams.uz.uza.lang.ApplicationException;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.lang.RStringBuilder;
 import jp.co.ndensan.reams.uz.uza.util.db.SearchResult;
 import jp.co.ndensan.reams.uz.uza.ui.binding.KeyValueDataSource;
+
 /**
  * 広域内転居結果一覧表Handlerクラスです。
- * 
- * @author きょう亮
+ *
  */
 public class BatchParamterInfoHandler {
-    
+
     private static final RString 市町村DDL1件目コード = new RString("000000");
     private static final RString 市町村DDL1件目名称 = new RString("全市町村");
-    
+    private List<JuminShubetsu> 住民種別List;
+    private List<JuminJotai> 住民状態List;
+
     private final BatchParamterInfoDiv div;
 
     public BatchParamterInfoHandler(BatchParamterInfoDiv div) {
@@ -60,33 +63,42 @@ public class BatchParamterInfoHandler {
         div.getTxtshohi().setValue(FlexibleDate.getNowDate());
     }
 
-    public void is入力チェック() {
-        FlexibleDate kaishihiValue = div.getTxtkaishihi().getValue();
-        FlexibleDate shohiValue = div.getTxtshohi().getValue();
-        if (null == kaishihiValue || kaishihiValue.isEmpty()
-                    || shohiValue == null || shohiValue.isEmpty()) {
-                throw new ApplicationException(UrErrorMessages.期間が不正.getMessage());
-        }else if (shohiValue.isBefore(kaishihiValue)) {
-                throw new ApplicationException(UrErrorMessages.期間が不正.getMessage());
-        }
+    public HiroshimaDomainBatchParameter setBatchParamter() {
+        HiroshimaDomainBatchParameter paramter = new HiroshimaDomainBatchParameter();
+
+        住民種別List = get住民種別List();
+        住民状態List = get住民状態List();
+        ShikibetsuTaishoPSMSearchKeyBuilder builder = new ShikibetsuTaishoPSMSearchKeyBuilder(GyomuCode.DB介護保険,
+                KensakuYusenKubun.住登外優先);
+        builder.set住民種別(住民種別List);
+        builder.set住民状態(住民状態List);
+        builder.setデータ取得区分(DataShutokuKubun.基準日時点の最新のレコード);
+        IShikibetsuTaishoPSMSearchKey searchKey = builder.build();
+        paramter.set宛名検索条件(searchKey);
+        String[] 市町村 = div.getDdlshichosonshitei().getSelectedValue().toString().split(" ");
+        paramter.set市町村コード(new LasdecCode(市町村[0]));
+        paramter.set市町村名称(new RString(市町村[1]));
+        paramter.set日付From(div.getTxtkaishihi().getValue());
+        paramter.set日付To(div.getTxtshohi().getValue());
+        return paramter;
     }
 
-    /**
-     * 「実行する」ボタンを押下
-     * @return KoikinaiTenkyoBatchParameter
-     */
-//    public KouikiTenkyoResultListSakuseiParameter click実行() {
-//        HiroshimaDomainBatchParameter paramter = new HiroshimaDomainBatchParameter();
-//        KouikiTenkyoResultListSakuseiParameter param = new KouikiTenkyoResultListSakuseiParameter();
-//        param.setKaishihi(div.getTxtkaishihi().getValue());
-//        param.setShohi(div.getTxtshohi().getValue());
-//        param.setShichosonshitei(new RString(市町村[0]));
-//        String[] 市町村 = div.getDdlshichosonshitei().getSelectedValue().toString().split(" ");
-//        paramter.set市町村コード(new LasdecCode(市町村[0]));
-//        paramter.set市町村名称(new RString(市町村[1]));
-//        KouikiTenkyoResultListBatchParameterSakusei batchParam =
-//                KouikiTenkyoResultListBatchParameterSakusei.getKouikiTenkyoResultListBatchParameter(param);
-//        // TODO バッチを起動する
-//        return param;
-//    }
+    private List<JuminShubetsu> get住民種別List() {
+        List<JuminShubetsu> 住民種別 = new ArrayList<>();
+        住民種別.add(JuminShubetsu.日本人);
+        住民種別.add(JuminShubetsu.外国人);
+
+        return 住民種別;
+    }
+
+    private List<JuminJotai> get住民状態List() {
+        List<JuminJotai> 住民状態 = new ArrayList<>();
+        住民状態.add(JuminJotai.住民);
+        住民状態.add(JuminJotai.住登外);
+        住民状態.add(JuminJotai.転出者);
+        住民状態.add(JuminJotai.消除者);
+        住民状態.add(JuminJotai.死亡者);
+        return 住民状態;
+    }
+
 }
