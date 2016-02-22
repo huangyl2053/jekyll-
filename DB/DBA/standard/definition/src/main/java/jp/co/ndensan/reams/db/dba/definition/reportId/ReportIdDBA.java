fc17a0378a629e10b13eb5fe86a98c0c2be9a07e
wangkun <wangkun@liandisys.com.cn>
2016-02-22 11:30:34 +0800

    介護保険受給資格証明書交付申請書のソースを修正しました。

--- DB/DBA/standard/definition/src/main/java/jp/co/ndensan/reams/db/dba/definition/reportId/ReportIdDBA.java
+++ DB/DBA/standard/definition/src/main/java/jp/co/ndensan/reams/db/dba/definition/reportId/ReportIdDBA.java
@@ -154,7 +154,15 @@ public enum ReportIdDBA {
     /**
      * 介護保険給付費借入申請書の帳票ＩＤです。
      */
-    DBC800018(new ReportId("DBC800018_kyufuhiKariireiShinseisho"), new RString("介護保険給付費借入申請書"));
+    DBC800018(new ReportId("DBC800018_kyufuhiKariireiShinseisho"), new RString("介護保険給付費借入申請書")),
+     /**
+     * 第三者行為による被害届（介護保険用）の帳票ＩＤです。
+     */
+    DBC800020(new ReportId("DBC800020_DaisanshaKouiHigaitodokeKaigoHokenyo"), new RString("第三者行為による被害届（介護保険用）")),
+    /**
+     * 介護保険受領委任払い契約申請書の帳票ＩＤです。
+     */
+    DBC800002(new ReportId("DBC800002_JuryoIninharaiKeiyakuShinseishoFukushiYogu"), new RString("介護保険受領委任払い契約申請書"));
 
     private final ReportId reportId;
     private final RString reportName;
