7a2177174e7e4fd57350bd8a669a398db859c4e8
wangkun <wangkun@liandisys.com.cn>
2016-02-22 10:51:20 +0800

    ソースを修正しました。

--- DB/DBA/standard/business/src/main/java/jp/co/ndensan/reams/db/dba/business/core/kaigohohenshisetsu/KaigoHohenShisetsuBusiness.java
+++ DB/DBA/standard/business/src/main/java/jp/co/ndensan/reams/db/dba/business/core/kaigohohenshisetsu/KaigoHohenShisetsuBusiness.java
@@ -5,8 +5,8 @@
  */
 package jp.co.ndensan.reams.db.dba.business.core.kaigohohenshisetsu;
 
-import jp.co.ndensan.reams.db.dba.entity.db.relate.kaigohohenshisetsu.KaigoHohenShisetsuRelateEntity;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.JigyoshaNo;
+import jp.co.ndensan.reams.db.dbz.entity.db.relate.kaigohohenshisetsu.KaigoHohenShisetsuRelateEntity;
 import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
 import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
