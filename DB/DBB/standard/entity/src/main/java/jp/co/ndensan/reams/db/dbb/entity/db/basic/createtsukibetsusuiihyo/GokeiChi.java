10d5a1d19af18000b4595b8c678951ba4e5efe3a
lishengli <lishengli@liandisys.com.cn>
2016-02-23 13:30:56 +0800

    ビジネス設計_DBBBZ21002_1_月別推移表集計（編集(バッチ)）　新規作成

---
+++ DB/DBB/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbb/entity/db/basic/createtsukibetsusuiihyo/GokeiChi.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jp.co.ndensan.reams.db.dbb.entity.db.basic.createtsukibetsusuiihyo;
+
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * 合計値Entityクラスです。
+ */
+@Getter
+@Setter
+@SuppressWarnings("PMD.UnusedPrivateField")
+public class GokeiChi {
+    private RString 段階;
+    private RString 徴収方法;
+    private int yoGetuNinsuu;
+    private int goGetuNinsuu;
+    private int rokuGetuNinsuu;
+    private int nanaGetuNinsuu;
+    private int hakuGetuNinsuu;
+    private int kyuGetuNinsuu;
+    private int jyuGetuNinsuu;
+    private int jyuitiGetuNinsuu;
+    private int jyuniGetuNinsuu;
+    private int itiGetuNinsuu;
+    private int niGetuNinsuu;
+    private int sanGetuNinsuu;
+    private int 現年随時の人数;
+    private int 過年度の人数;
+    private int 歳出還付の人数;
+    private int 該当段階の人数合計;
+    private int 総人数合計;
+    private int yoGetuKinkaku;
+    private int goGetuKinkaku;
+    private int rokuGetuKinkaku;
+    private int nanaGetuKinkaku;
+    private int hakuGetuKinkaku;
+    private int kyuGetuKinkaku;
+    private int jyuGetuKinkaku;
+    private int jyuitiGetuKinkaku;
+    private int jyuniGetuKinkaku;
+    private int itiGetuKinkaku;
+    private int niGetuKinkaku;
+    private int sanGetuKinkaku;
+    private int 現年随時の金額;
+    private int 過年度の金額;
+    private int 歳出還付の金額;
+    private int 該当段階の金額合計;
+    private int 総金額合計;
+    
+}
