379a50fbb494a751a9d38e7bb3fba463d296b2ee
許鵬 <pxu@sinoi.com.cn>
2016-02-24 13:05:20 +0800

    DBC-1010-170 償還払い状況照会_申請情報照会 修正

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/dbc0810012/ShinseiDetailHandler.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/dbc0810012/ShinseiDetailHandler.java
@@ -65,7 +65,7 @@ public final class ShinseiDetailHandler {
         }
         div.getPnlShinsei().getTxtKisaiHokensyaBango().setValue(new RString(shokanShinsei
                 .get証記載保険者番号().value().toString()));
-        if (shokanShinsei.get国保連再送付フラグ()) {
+        if (shokanShinsei.is国保連再送付フラグ()) {
             List<RString> list = new ArrayList<>();
             list.add(new RString("key0"));
             div.getPnlShinsei().getChkKokuhorenSaiso().setSelectedItemsByKey(list);
