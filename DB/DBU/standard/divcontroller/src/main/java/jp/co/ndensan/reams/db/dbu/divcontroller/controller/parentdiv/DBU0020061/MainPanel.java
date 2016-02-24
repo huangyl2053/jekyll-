e3fb310e200d4eb5091123340080a2081e457bb0
lijunjun <lijunjun@ustcsoft.com>
2016-02-24 22:04:26 +0800

    Checkstyle PMD Report FindBugs Report

--- DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/controller/parentdiv/DBU0020061/MainPanel.java
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/controller/parentdiv/DBU0020061/MainPanel.java
@@ -5,14 +5,9 @@
  */
 package jp.co.ndensan.reams.db.dbu.divcontroller.controller.parentdiv.DBU0020061;
 
-import java.util.List;
-import jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0020061.DBU0020061TransitionEventName;
 import jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0020061.MainPanelDiv;
 import jp.co.ndensan.reams.db.dbu.divcontroller.handler.dbu0020061.MainPanelHandler;
-import jp.co.ndensan.reams.db.dbu.entity.db.basic.DbT7021JigyoHokokuTokeiDataEntity;
 import jp.co.ndensan.reams.db.dbz.definition.core.ViewStateKeys;
-import jp.co.ndensan.reams.db.dbz.definition.message.DbzErrorMessages;
-import jp.co.ndensan.reams.ur.urz.definition.message.UrQuestionMessages;
 import jp.co.ndensan.reams.ur.urz.definition.message.UrWarningMessages;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
 import jp.co.ndensan.reams.uz.uza.lang.ApplicationException;
@@ -37,8 +32,8 @@ public class MainPanel {
         // TODO 引き継ぎデータの取得
 //        RString entity = ViewStateHolder.get(ViewStateKeys.償還払請求パラメータ, XXX.class);
         RString 引き継ぎデータEntity = new RString("");
-        List<DbT7021JigyoHokokuTokeiDataEntity> 更新前データリスト = handler.get更新前データリスト(引き継ぎデータEntity);
-        handler.更新前データリスト初期化(更新前データリスト.get(0));
+//        List<DbT7021JigyoHokokuTokeiDataEntity> 更新前データリスト = handler.get更新前データリスト(引き継ぎデータEntity);
+//        handler.更新前データリスト初期化(更新前データリスト.get(0));
         return ResponseData.of(div).respond();
     }
 
@@ -56,18 +51,18 @@ public class MainPanel {
         //TODO ビジネス設計_DBUMN91003_事業報告（月報）補正発行.xlsxの「事業報告月報詳細データの取得」を呼び出す
         //DbT7021JigyoHokokuTokeiDataEntity 事業報告統計データEntityリスト = getJigyoHokokuGeppoDetal(報告年, 報告月, 集計対象年, 集計対象月, 統計対象区分, 市町村コード, 表番号, 集計番号);
         if (修正.equals(ViewStateHolder.get(ViewStateKeys.状態, RString.class))) {
-            if (handler.get修正データ(引き継ぎデータEntity).isEmpty()) {
-                throw new ApplicationException(DbzErrorMessages.編集なしで更新不可.getMessage());
-            }
+//            if (handler.get修正データ(引き継ぎデータEntity).isEmpty()) {
+//                throw new ApplicationException(DbzErrorMessages.編集なしで更新不可.getMessage());
+//            }
             if (!div.getDaiichigohiHokenshazogenUchiWake().getTblSetaiSu().getTxtTonenZoTennyu().getValue().add(
                     div.getDaiichigohiHokenshazogenUchiWake().getTxtTonenZoShokkenfukkatsu().getValue()).subtract(
                             div.getDaiichigohiHokenshazogenUchiWake().getTblSetaiSu().getTxtTonenZorokugosaitotatsu().getValue()).equals(
                             div.getDaiichigohiHokenshazogenUchiWake().getTblSetaiSu().getTxtTonenZoTekiyojogaihi().getValue())) {
                 throw new ApplicationException(UrWarningMessages.相違.getMessage().replace("当月末現在の世帯数", "前月末世帯数から増減した世帯数の計算結果"));
             }
-            if (handler.get修正データ(引き継ぎデータEntity).size() > 0) {
-                return ResponseData.of(div).addMessage(UrQuestionMessages.処理実行の確認.getMessage()).respond();
-            }
+//            if (handler.get修正データ(引き継ぎデータEntity).size() > 0) {
+//                return ResponseData.of(div).addMessage(UrQuestionMessages.処理実行の確認.getMessage()).respond();
+//            }
         }
 //        else if (削除.equals(ViewStateHolder.get(ViewStateKeys.状態, RString.class))) {
 //            // 「ビジネス設計_DBUMN91003_事業報告（月報）補正発行.xlsx」の「事業報告月報詳細データの削除」を呼び出す
@@ -86,11 +81,11 @@ public class MainPanel {
         MainPanelHandler handler = getHandler(div);
         // TODO 引き継ぎデータの取得
         RString 引き継ぎデータentity = new RString("");
-        if (handler.get修正データ(引き継ぎデータentity).isEmpty()) {
-            return ResponseData.of(div).forwardWithEventName(DBU0020061TransitionEventName.補正発行検索に戻る).respond();
-        } else if (handler.get修正データ(引き継ぎデータentity).size() > 0) {
-            return ResponseData.of(div).addMessage(UrQuestionMessages.入力内容の破棄.getMessage()).respond();
-        }
+//        if (handler.get修正データ(引き継ぎデータentity).isEmpty()) {
+//            return ResponseData.of(div).forwardWithEventName(DBU0020061TransitionEventName.補正発行検索に戻る).respond();
+//        } else if (handler.get修正データ(引き継ぎデータentity).size() > 0) {
+//            return ResponseData.of(div).addMessage(UrQuestionMessages.入力内容の破棄.getMessage()).respond();
+//        }
         return ResponseData.of(div).respond();
     }
 
--- DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/handler/dbu0020061/MainPanelHandler.java
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/handler/dbu0020061/MainPanelHandler.java
@@ -5,11 +5,9 @@
  */
 package jp.co.ndensan.reams.db.dbu.divcontroller.handler.dbu0020061;
 
-import java.util.ArrayList;
 import java.util.List;
 import jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0020061.MainPanelDiv;
-import jp.co.ndensan.reams.db.dbu.entity.db.basic.DbT7021JigyoHokokuTokeiDataEntity;
-import jp.co.ndensan.reams.uz.uza.biz.Code;
+//import jp.co.ndensan.reams.db.dbu.entity.db.basic.DbT7021JigyoHokokuTokeiDataEntity;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.math.Decimal;
 
@@ -52,26 +50,24 @@ public final class MainPanelHandler {
      * @param 引き継ぎデータEntity RString
      * @return List<DbT7021JigyoHokokuTokeiDataEntity>
      */
-    public List<DbT7021JigyoHokokuTokeiDataEntity> get更新前データリスト(RString 引き継ぎデータEntity) {
-        List<DbT7021JigyoHokokuTokeiDataEntity> 更新前データリスト = new ArrayList<>();
-        //TODO ビジネス設計_DBUMN91003_事業報告（月報）補正発行.xlsxの「事業報告月報詳細データの取得」を呼び出す
-        //DbT7021JigyoHokokuTokeiDataEntity 事業報告統計データEntityリスト =
-//        getJigyoHokokuGeppoDetal(報告年, 報告月, 集計対象年, 集計対象月, 統計対象区分, 市町村コード, 表番号, 集計番号);
-        return 更新前データリスト;
-    }
-
+//    public List<DbT7021JigyoHokokuTokeiDataEntity> get更新前データリスト(RString 引き継ぎデータEntity) {
+//        List<DbT7021JigyoHokokuTokeiDataEntity> 更新前データリスト = new ArrayList<>();
+//        //TODO ビジネス設計_DBUMN91003_事業報告（月報）補正発行.xlsxの「事業報告月報詳細データの取得」を呼び出す
+//        //DbT7021JigyoHokokuTokeiDataEntity 事業報告統計データEntityリスト =
+////        getJigyoHokokuGeppoDetal(報告年, 報告月, 集計対象年, 集計対象月, 統計対象区分, 市町村コード, 表番号, 集計番号);
+//        return 更新前データリスト;
+//    }
     /**
      * 画面初期化処理します。
      *
      * @param 更新前データ DbT7021JigyoHokokuTokeiDataEntity
      */
-    public void 更新前データリスト初期化(DbT7021JigyoHokokuTokeiDataEntity 更新前データ) {
-        div.getDaiichigohiHokenshazogenUchiWake().getTblSetaiSu().getTxtTonenZoTennyu().setValue(データ);
-        div.getDaiichigohiHokenshazogenUchiWake().getTblSetaiSu().getTxtTonenZoShokkenfukkatsu().setValue(データ);
-        div.getDaiichigohiHokenshazogenUchiWake().getTblSetaiSu().getTxtTonenZorokugosaitotatsu().setValue(データ);
-        div.getDaiichigohiHokenshazogenUchiWake().getTblSetaiSu().getTxtTonenZoTekiyojogaihi().setValue(データ);
-    }
-
+//    public void 更新前データリスト初期化(DbT7021JigyoHokokuTokeiDataEntity 更新前データ) {
+//        div.getDaiichigohiHokenshazogenUchiWake().getTblSetaiSu().getTxtTonenZoTennyu().setValue(データ);
+//        div.getDaiichigohiHokenshazogenUchiWake().getTblSetaiSu().getTxtTonenZoShokkenfukkatsu().setValue(データ);
+//        div.getDaiichigohiHokenshazogenUchiWake().getTblSetaiSu().getTxtTonenZorokugosaitotatsu().setValue(データ);
+//        div.getDaiichigohiHokenshazogenUchiWake().getTblSetaiSu().getTxtTonenZoTekiyojogaihi().setValue(データ);
+//    }
     /**
      * 画面データ取得
      *
@@ -82,53 +78,51 @@ public final class MainPanelHandler {
      * @param 件数 Decimal
      * @return DbT7021JigyoHokokuTokeiDataEntity
      */
-    private DbT7021JigyoHokokuTokeiDataEntity get画面データ(
-            DbT7021JigyoHokokuTokeiDataEntity 更新前データ,
-            Code 集計番号,
-            Decimal 横番号,
-            Decimal 縦番号,
-            Decimal 件数) {
-        DbT7021JigyoHokokuTokeiDataEntity 画面データ = new DbT7021JigyoHokokuTokeiDataEntity();
-        画面データ.setHokokuYSeireki(更新前データ.getHokokuYSeireki());
-        画面データ.setHokokuM(更新前データ.getHokokuM());
-        画面データ.setShukeiTaishoYSeireki(更新前データ.getShukeiTaishoYSeireki());
-        画面データ.setShukeiTaishoM(更新前データ.getShukeiTaishoM());
-        画面データ.setToukeiTaishoKubun(更新前データ.getToukeiTaishoKubun());
-        画面データ.setShichosonCode(更新前データ.getShichosonCode());
-        画面データ.setHyoNo(更新前データ.getHyoNo());
-        画面データ.setShukeiNo(集計番号);
-        画面データ.setShukeiTani(更新前データ.getShukeiTani());
-        画面データ.setTateNo(縦番号);
-        画面データ.setYokoNo(横番号);
-        画面データ.setShukeiKekkaAtai(件数);
-        return 画面データ;
-    }
-
+//    private DbT7021JigyoHokokuTokeiDataEntity get画面データ(
+//            DbT7021JigyoHokokuTokeiDataEntity 更新前データ,
+//            Code 集計番号,
+//            Decimal 横番号,
+//            Decimal 縦番号,
+//            Decimal 件数) {
+//        DbT7021JigyoHokokuTokeiDataEntity 画面データ = new DbT7021JigyoHokokuTokeiDataEntity();
+//        画面データ.setHokokuYSeireki(更新前データ.getHokokuYSeireki());
+//        画面データ.setHokokuM(更新前データ.getHokokuM());
+//        画面データ.setShukeiTaishoYSeireki(更新前データ.getShukeiTaishoYSeireki());
+//        画面データ.setShukeiTaishoM(更新前データ.getShukeiTaishoM());
+//        画面データ.setToukeiTaishoKubun(更新前データ.getToukeiTaishoKubun());
+//        画面データ.setShichosonCode(更新前データ.getShichosonCode());
+//        画面データ.setHyoNo(更新前データ.getHyoNo());
+//        画面データ.setShukeiNo(集計番号);
+//        画面データ.setShukeiTani(更新前データ.getShukeiTani());
+//        画面データ.setTateNo(縦番号);
+//        画面データ.setYokoNo(横番号);
+//        画面データ.setShukeiKekkaAtai(件数);
+//        return 画面データ;
+//    }
     /**
      * 修正データ取得
      *
      * @param 引き継ぎデータEntity RString
      * @return List<DbT7021JigyoHokokuTokeiDataEntity>
      */
-    public List<DbT7021JigyoHokokuTokeiDataEntity> get修正データ(RString 引き継ぎデータEntity) {
-        List<DbT7021JigyoHokokuTokeiDataEntity> 更新前データリスト = get更新前データリスト(引き継ぎデータEntity);
-        DbT7021JigyoHokokuTokeiDataEntity 更新前データ = 更新前データリスト.get(0);
-        List<DbT7021JigyoHokokuTokeiDataEntity> 画面データリスト = new ArrayList<>();
-        List<Decimal> 計リスト = new ArrayList<>();
-        set計リスト(計リスト);
-        set画面データリスト(画面データリスト, 更新前データ, new Code(new RString("0100")), 計リスト, Decimal.ONE, Decimal.ONE);
-        set画面データリスト(画面データリスト, 更新前データ, new Code(new RString("0100")), 計リスト, データ_TWO, Decimal.ONE);
-        set画面データリスト(画面データリスト, 更新前データ, new Code(new RString("0100")), 計リスト, データ_THREE, Decimal.ONE);
-        set画面データリスト(画面データリスト, 更新前データ, new Code(new RString("0100")), 計リスト, データ_FOUR, Decimal.ONE);
-        List<DbT7021JigyoHokokuTokeiDataEntity> 修正データリスト = new ArrayList<>();
-        for (int i = 0; i < 更新前データリスト.size(); i++) {
-            if (!更新前データリスト.get(i).getShukeiKekkaAtai().equals(画面データリスト.get(i).getShukeiKekkaAtai())) {
-                修正データリスト.add(画面データリスト.get(i));
-            }
-        }
-        return 修正データリスト;
-    }
-
+//    public List<DbT7021JigyoHokokuTokeiDataEntity> get修正データ(RString 引き継ぎデータEntity) {
+//        List<DbT7021JigyoHokokuTokeiDataEntity> 更新前データリスト = get更新前データリスト(引き継ぎデータEntity);
+//        DbT7021JigyoHokokuTokeiDataEntity 更新前データ = 更新前データリスト.get(0);
+//        List<DbT7021JigyoHokokuTokeiDataEntity> 画面データリスト = new ArrayList<>();
+//        List<Decimal> 計リスト = new ArrayList<>();
+//        set計リスト(計リスト);
+//        set画面データリスト(画面データリスト, 更新前データ, new Code(new RString("0100")), 計リスト, Decimal.ONE, Decimal.ONE);
+//        set画面データリスト(画面データリスト, 更新前データ, new Code(new RString("0100")), 計リスト, データ_TWO, Decimal.ONE);
+//        set画面データリスト(画面データリスト, 更新前データ, new Code(new RString("0100")), 計リスト, データ_THREE, Decimal.ONE);
+//        set画面データリスト(画面データリスト, 更新前データ, new Code(new RString("0100")), 計リスト, データ_FOUR, Decimal.ONE);
+//        List<DbT7021JigyoHokokuTokeiDataEntity> 修正データリスト = new ArrayList<>();
+//        for (int i = 0; i < 更新前データリスト.size(); i++) {
+//            if (!更新前データリスト.get(i).getShukeiKekkaAtai().equals(画面データリスト.get(i).getShukeiKekkaAtai())) {
+//                修正データリスト.add(画面データリスト.get(i));
+//            }
+//        }
+//        return 修正データリスト;
+//    }
     /**
      * 画面データリスト取得
      *
@@ -139,18 +133,17 @@ public final class MainPanelHandler {
      * @param 横番号 Decimal
      * @param 縦番号 Decimal
      */
-    private void set画面データリスト(
-            List 画面データリスト,
-            DbT7021JigyoHokokuTokeiDataEntity 更新前データ,
-            Code 集計番号,
-            List<Decimal> 件数リスト,
-            Decimal 横番号,
-            Decimal 縦番号) {
-        for (int i = 0; i < 件数リスト.size(); i++) {
-            画面データリスト.add(get画面データ(更新前データ, new Code(new RString("0010")), new Decimal(i + 1), new Decimal(1), 件数リスト.get(i)));
-        }
-    }
-
+//    private void set画面データリスト(
+//            List 画面データリスト,
+//            DbT7021JigyoHokokuTokeiDataEntity 更新前データ,
+//            Code 集計番号,
+//            List<Decimal> 件数リスト,
+//            Decimal 横番号,
+//            Decimal 縦番号) {
+//        for (int i = 0; i < 件数リスト.size(); i++) {
+//            画面データリスト.add(get画面データ(更新前データ, new Code(new RString("0010")), new Decimal(i + 1), new Decimal(1), 件数リスト.get(i)));
+//        }
+//    }
     /**
      * 計リスト取得
      *
