984db07506ca4823d6c109d9809ba8fcb22c5760
zhangzhiming <zhangzhiming@liandisys.com.cn>
2016-02-25 14:51:53 +0800

    資格取得除外者登録　新規

---
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/controller/parentdiv/DBA1080011/JogaishaToroku.java
@@ -0,0 +1,214 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dba.divcontroller.controller.parentdiv.DBA1080011;
+
+import java.util.List;
+import jp.co.ndensan.reams.db.dba.business.core.shikakushutokujogaishakanri.ShikakuShutokuJogaishaKanri;
+import static jp.co.ndensan.reams.db.dba.definition.enumeratedtype.config.ConfigKeysJukyuShikakuShomeishoHakko.資格取得除外者登録キー;
+import jp.co.ndensan.reams.db.dba.divcontroller.entity.parentdiv.DBA1080011.DBA1080011StateName;
+import jp.co.ndensan.reams.db.dba.divcontroller.entity.parentdiv.DBA1080011.JogaishaTorokuDiv;
+import jp.co.ndensan.reams.db.dba.divcontroller.entity.parentdiv.DBA1080011.dgNenreiTotatshusha_Row;
+import jp.co.ndensan.reams.db.dba.divcontroller.handler.parentdiv.DBA1080011.JogaishaTorokuHandler;
+import jp.co.ndensan.reams.db.dba.service.core.shikakushutokujogaishakanri.ShikakuShutokuJogaishaKanriManager;
+import jp.co.ndensan.reams.db.dbz.business.core.ShikakuShutokuJogaisha;
+import jp.co.ndensan.reams.db.dbz.business.core.ShikakuShutokuJogaishaBuilder;
+import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
+import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
+import jp.co.ndensan.reams.uz.uza.exclusion.LockingKey;
+import jp.co.ndensan.reams.uz.uza.exclusion.RealInitialLocker;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.ui.binding.RowState;
+import jp.co.ndensan.reams.uz.uza.ui.servlets.ValidationMessageControlPairs;
+import jp.co.ndensan.reams.uz.uza.ui.servlets.ViewStateHolder;
+import jp.co.ndensan.reams.uz.uza.util.db.SearchResult;
+
+/**
+ *
+ * 資格取得除外者登録のクラス。
+ */
+public class JogaishaToroku {
+
+    /**
+     * 資格取得除外者登録の初期化。<br/>
+     *
+     * @param requestDiv JogaishaTorokuDiv
+     * @return ResponseData<JogaishaTorokuDiv>
+     */
+    public ResponseData<JogaishaTorokuDiv> onLoad(JogaishaTorokuDiv requestDiv) {
+
+        if (ViewStateHolder.get(資格取得除外者登録キー, RString.class) == null || ViewStateHolder.get(資格取得除外者登録キー, RString.class).isEmpty()) {
+            SearchResult<ShikakuShutokuJogaishaKanri> 資格取得除外者情報
+                    = ShikakuShutokuJogaishaKanriManager.createInstance().getTennyuHoryuTokuteiJushoIchiran();
+            if (資格取得除外者情報.records() == null || 資格取得除外者情報.records().isEmpty()) {
+                return ResponseData.of(requestDiv).respond();
+            }
+            getHandler(requestDiv).onLoad(資格取得除外者情報.records());
+        } else {
+            getHandler(requestDiv).onLoadKen();
+        }
+        return ResponseData.of(requestDiv).setState(DBA1080011StateName.初期状態);
+    }
+
+    /**
+     * 追加ボタンを押下します。
+     *
+     * @param requestDiv JogaishaTorokuDiv
+     * @return ResponseData<JogaishaTorokuDiv>
+     */
+    public ResponseData<JogaishaTorokuDiv> onClick_btnAdd(JogaishaTorokuDiv requestDiv) {
+        getHandler(requestDiv).onClick_btnAdd();
+        return ResponseData.of(requestDiv).setState(DBA1080011StateName.初期状態);
+    }
+
+    /**
+     * 修正ボタンを押下します。
+     *
+     * @param requestDiv JogaishaTorokuDiv
+     * @return ResponseData<JogaishaTorokuDiv>
+     */
+    public ResponseData<JogaishaTorokuDiv> onClick_btnUpda(JogaishaTorokuDiv requestDiv) {
+        getHandler(requestDiv).onClick_btnUpda();
+        return ResponseData.of(requestDiv).setState(DBA1080011StateName.初期状態);
+    }
+
+    /**
+     * 削除ボタンを押下します。
+     *
+     * @param requestDiv JogaishaTorokuDiv
+     * @return ResponseData<JogaishaTorokuDiv>
+     */
+    public ResponseData<JogaishaTorokuDiv> onClick_btnDele(JogaishaTorokuDiv requestDiv) {
+        getHandler(requestDiv).onClick_btnDele();
+        return ResponseData.of(requestDiv).setState(DBA1080011StateName.初期状態);
+    }
+
+    /**
+     * 取消ボタンを押下します。
+     *
+     * @param requestDiv JogaishaTorokuDiv
+     * @return ResponseData<JogaishaTorokuDiv>
+     */
+    public ResponseData<JogaishaTorokuDiv> onClick_btnCacel(JogaishaTorokuDiv requestDiv) {
+        getHandler(requestDiv).onClick_btnCacel();
+        return ResponseData.of(requestDiv).setState(DBA1080011StateName.初期状態);
+    }
+
+    /**
+     * 確定ボタンを押下します。
+     *
+     * @param requestDiv JogaishaTorokuDiv
+     * @return ResponseData<JogaishaTorokuDiv>
+     */
+    public ResponseData<JogaishaTorokuDiv> onClick_btnKaku(JogaishaTorokuDiv requestDiv) {
+        getHandler(requestDiv).onClick_btnKaku();
+        return ResponseData.of(requestDiv).setState(DBA1080011StateName.初期状態);
+    }
+
+    /**
+     * 識別コードLostFocusする。
+     *
+     * @param requestDiv JogaishaTorokuDiv
+     * @return ResponseData<JogaishaTorokuDiv>
+     */
+    public ResponseData<JogaishaTorokuDiv> onFocus_shikibetsuCode(JogaishaTorokuDiv requestDiv) {
+        getHandler(requestDiv).onFocus_shikibetsuCode();
+        return ResponseData.of(requestDiv).setState(DBA1080011StateName.初期状態);
+    }
+
+    /**
+     * 識別対象検索ボタンを押下します。
+     *
+     * @param requestDiv JogaishaTorokuDiv
+     * @return ResponseData<JogaishaTorokuDiv>
+     */
+    public ResponseData<JogaishaTorokuDiv> onClick_Search(JogaishaTorokuDiv requestDiv) {
+        getHandler(requestDiv).onClick_Search();
+        return ResponseData.of(requestDiv).setState(DBA1080011StateName.初期状態);
+    }
+
+    /**
+     * 完了するボタンを押下します。
+     *
+     * @param requestDiv JogaishaTorokuDiv
+     * @return ResponseData<JogaishaTorokuDiv>
+     */
+    public ResponseData<JogaishaTorokuDiv> onClick_btnComplete(JogaishaTorokuDiv requestDiv) {
+        前排他処理();
+        return ResponseData.of(requestDiv).setState(DBA1080011StateName.完了状態);
+    }
+
+    /**
+     * 保存するボタン押下、データがDBに更新します。
+     *
+     * @param requestDiv JogaishaTorokuDiv
+     * @return ResponseData<JogaishaTorokuDiv>
+     */
+    public ResponseData<JogaishaTorokuDiv> onClick_btnUpdate(JogaishaTorokuDiv requestDiv) {
+        List<dgNenreiTotatshusha_Row> dgRowList = requestDiv.getJogaishaTorokuIchiran().getNenreiTotatsh().getDgNenreiTotatshusha().getDataSource();
+        if (重複チェック(requestDiv)) {
+            ValidationMessageControlPairs validationMessageControlPairs = getHandler(requestDiv).validateCheck();
+            return ResponseData.of(requestDiv).addValidationMessages(validationMessageControlPairs).respond();
+        }
+        for (int i = 0; i < dgRowList.size(); i++) {
+            ShikakuShutokuJogaisha busniess = new ShikakuShutokuJogaisha(new ShikibetsuCode(dgRowList.get(i).getShikibetsuCode()),
+                    Integer.parseInt(dgRowList.get(i).getRirekiNo().toString()));
+            ShikakuShutokuJogaishaBuilder builder = busniess.createBuilderForEdit();
+            builder.set市町村コード(new LasdecCode(dgRowList.get(i).getLasdecCode()));
+            builder.set識別コード(new ShikibetsuCode(dgRowList.get(i).getShikibetsuCode()));
+            builder.set資格取得除外年月日(dgRowList.get(i).getJogaiTekiyoDate().getValue() == null
+                    ? FlexibleDate.EMPTY : new FlexibleDate(dgRowList.get(i).getJogaiTekiyoDate().getValue().toDateString()));
+            builder.set資格取得除外理由(dgRowList.get(i).getJogaiRiyu());
+            builder.set資格取得除外解除年月日(dgRowList.get(i).getJogaiKaijyoDate().getValue() == null
+                    ? FlexibleDate.EMPTY : new FlexibleDate(dgRowList.get(i).getJogaiKaijyoDate().getValue().toDateString()));
+            if (RowState.Deleted.equals(dgRowList.get(i).getRowState())) {
+                builder.set履歴番号(Integer.parseInt(dgRowList.get(i).getRirekiNo().toString()));
+                ShikakuShutokuJogaishaKanriManager.createInstance().delShikakuShutokuJogaisha(builder.build().toEntity());
+            }
+            if (RowState.Added.equals(dgRowList.get(i).getRowState())) {
+                ShikakuShutokuJogaishaKanriManager.createInstance().insertShikakuShutokuJogaisha(builder.build().toEntity());
+            }
+            if (RowState.Modified.equals(dgRowList.get(i).getRowState())) {
+                builder.set履歴番号(Integer.parseInt(dgRowList.get(i).getRirekiNo().toString()));
+                ShikakuShutokuJogaishaKanriManager.createInstance().updateShikakuShutokuJogaisha(builder.build().toEntity());
+            }
+        }
+        前排他処理();
+        return ResponseData.of(requestDiv).setState(DBA1080011StateName.完了状態);
+    }
+
+    private void 前排他処理() {
+        LockingKey lockingKey = new LockingKey(new RString("ShikakuShutokuJogaishaToroku"));
+        RealInitialLocker.release(lockingKey);
+    }
+
+    private boolean 重複チェック(JogaishaTorokuDiv div) {
+        List<dgNenreiTotatshusha_Row> dgRowList = div.getJogaishaTorokuIchiran().getNenreiTotatsh().getDgNenreiTotatshusha().getDataSource();
+        for (int i = 0; i < dgRowList.size(); i++) {
+            if (RowState.Deleted.equals(dgRowList.get(i).getRowState())) {
+                continue;
+            }
+            ShikakuShutokuJogaisha busniess = new ShikakuShutokuJogaisha(new ShikibetsuCode(dgRowList.get(i).getShikibetsuCode()),
+                    Integer.parseInt(dgRowList.get(i).getRirekiNo().toString()));
+            ShikakuShutokuJogaishaBuilder builder = busniess.createBuilderForEdit();
+            builder.set識別コード(new ShikibetsuCode(dgRowList.get(i).getShikibetsuCode()));
+            builder.set資格取得除外年月日(dgRowList.get(i).getJogaiTekiyoDate().getValue() == null
+                    ? FlexibleDate.EMPTY : new FlexibleDate(dgRowList.get(i).getJogaiTekiyoDate().getValue().toDateString()));
+            builder.set資格取得除外解除年月日(dgRowList.get(i).getJogaiKaijyoDate().getValue() == null
+                    ? FlexibleDate.EMPTY : new FlexibleDate(dgRowList.get(i).getJogaiKaijyoDate().getValue().toDateString()));
+            boolean flag = ShikakuShutokuJogaishaKanriManager.createInstance().jogaiKikanJufukuCheck(builder.build().toEntity());
+            if (flag) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private JogaishaTorokuHandler getHandler(JogaishaTorokuDiv requestDiv) {
+        return new JogaishaTorokuHandler(requestDiv);
+    }
+}
---
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/parentdiv/DBA1080011/JogaiTaishoIchiranDiv.java
@@ -0,0 +1,294 @@
+package jp.co.ndensan.reams.db.dba.divcontroller.entity.parentdiv.DBA1080011;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+import jp.co.ndensan.reams.uz.uza.ui.binding.domain.*;
+
+/**
+ * JogaiTaishoIchiran のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class JogaiTaishoIchiranDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：バージョン情報無し">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("txtShikibetsuCode")
+    private TextBoxShikibetsuCode txtShikibetsuCode;
+    @JsonProperty("btnShikibetsuSearch")
+    private Button btnShikibetsuSearch;
+    @JsonProperty("txtShikibetsuCodeName")
+    private TextBox txtShikibetsuCodeName;
+    @JsonProperty("txtJogaiRiyu")
+    private TextBox txtJogaiRiyu;
+    @JsonProperty("txtJogaiTekiyoYMD")
+    private TextBoxDate txtJogaiTekiyoYMD;
+    @JsonProperty("txtJogaiKaijyoYMD")
+    private TextBoxDate txtJogaiKaijyoYMD;
+    @JsonProperty("btnConfirm")
+    private Button btnConfirm;
+    @JsonProperty("btnCacel")
+    private Button btnCacel;
+    @JsonProperty("txtDateOfBirth")
+    private RString txtDateOfBirth;
+    @JsonProperty("txtGender")
+    private RString txtGender;
+    @JsonProperty("txtJuminJotai")
+    private RString txtJuminJotai;
+    @JsonProperty("txtLasdecMei")
+    private RString txtLasdecMei;
+    @JsonProperty("txtLasdecCode")
+    private RString txtLasdecCode;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * gettxtShikibetsuCode
+     * @return txtShikibetsuCode
+     */
+    @JsonProperty("txtShikibetsuCode")
+    public TextBoxShikibetsuCode getTxtShikibetsuCode() {
+        return txtShikibetsuCode;
+    }
+
+    /*
+     * settxtShikibetsuCode
+     * @param txtShikibetsuCode txtShikibetsuCode
+     */
+    @JsonProperty("txtShikibetsuCode")
+    public void setTxtShikibetsuCode(TextBoxShikibetsuCode txtShikibetsuCode) {
+        this.txtShikibetsuCode = txtShikibetsuCode;
+    }
+
+    /*
+     * getbtnShikibetsuSearch
+     * @return btnShikibetsuSearch
+     */
+    @JsonProperty("btnShikibetsuSearch")
+    public Button getBtnShikibetsuSearch() {
+        return btnShikibetsuSearch;
+    }
+
+    /*
+     * setbtnShikibetsuSearch
+     * @param btnShikibetsuSearch btnShikibetsuSearch
+     */
+    @JsonProperty("btnShikibetsuSearch")
+    public void setBtnShikibetsuSearch(Button btnShikibetsuSearch) {
+        this.btnShikibetsuSearch = btnShikibetsuSearch;
+    }
+
+    /*
+     * gettxtShikibetsuCodeName
+     * @return txtShikibetsuCodeName
+     */
+    @JsonProperty("txtShikibetsuCodeName")
+    public TextBox getTxtShikibetsuCodeName() {
+        return txtShikibetsuCodeName;
+    }
+
+    /*
+     * settxtShikibetsuCodeName
+     * @param txtShikibetsuCodeName txtShikibetsuCodeName
+     */
+    @JsonProperty("txtShikibetsuCodeName")
+    public void setTxtShikibetsuCodeName(TextBox txtShikibetsuCodeName) {
+        this.txtShikibetsuCodeName = txtShikibetsuCodeName;
+    }
+
+    /*
+     * gettxtJogaiRiyu
+     * @return txtJogaiRiyu
+     */
+    @JsonProperty("txtJogaiRiyu")
+    public TextBox getTxtJogaiRiyu() {
+        return txtJogaiRiyu;
+    }
+
+    /*
+     * settxtJogaiRiyu
+     * @param txtJogaiRiyu txtJogaiRiyu
+     */
+    @JsonProperty("txtJogaiRiyu")
+    public void setTxtJogaiRiyu(TextBox txtJogaiRiyu) {
+        this.txtJogaiRiyu = txtJogaiRiyu;
+    }
+
+    /*
+     * gettxtJogaiTekiyoYMD
+     * @return txtJogaiTekiyoYMD
+     */
+    @JsonProperty("txtJogaiTekiyoYMD")
+    public TextBoxDate getTxtJogaiTekiyoYMD() {
+        return txtJogaiTekiyoYMD;
+    }
+
+    /*
+     * settxtJogaiTekiyoYMD
+     * @param txtJogaiTekiyoYMD txtJogaiTekiyoYMD
+     */
+    @JsonProperty("txtJogaiTekiyoYMD")
+    public void setTxtJogaiTekiyoYMD(TextBoxDate txtJogaiTekiyoYMD) {
+        this.txtJogaiTekiyoYMD = txtJogaiTekiyoYMD;
+    }
+
+    /*
+     * gettxtJogaiKaijyoYMD
+     * @return txtJogaiKaijyoYMD
+     */
+    @JsonProperty("txtJogaiKaijyoYMD")
+    public TextBoxDate getTxtJogaiKaijyoYMD() {
+        return txtJogaiKaijyoYMD;
+    }
+
+    /*
+     * settxtJogaiKaijyoYMD
+     * @param txtJogaiKaijyoYMD txtJogaiKaijyoYMD
+     */
+    @JsonProperty("txtJogaiKaijyoYMD")
+    public void setTxtJogaiKaijyoYMD(TextBoxDate txtJogaiKaijyoYMD) {
+        this.txtJogaiKaijyoYMD = txtJogaiKaijyoYMD;
+    }
+
+    /*
+     * getbtnConfirm
+     * @return btnConfirm
+     */
+    @JsonProperty("btnConfirm")
+    public Button getBtnConfirm() {
+        return btnConfirm;
+    }
+
+    /*
+     * setbtnConfirm
+     * @param btnConfirm btnConfirm
+     */
+    @JsonProperty("btnConfirm")
+    public void setBtnConfirm(Button btnConfirm) {
+        this.btnConfirm = btnConfirm;
+    }
+
+    /*
+     * getbtnCacel
+     * @return btnCacel
+     */
+    @JsonProperty("btnCacel")
+    public Button getBtnCacel() {
+        return btnCacel;
+    }
+
+    /*
+     * setbtnCacel
+     * @param btnCacel btnCacel
+     */
+    @JsonProperty("btnCacel")
+    public void setBtnCacel(Button btnCacel) {
+        this.btnCacel = btnCacel;
+    }
+
+    /*
+     * gettxtDateOfBirth
+     * @return txtDateOfBirth
+     */
+    @JsonProperty("txtDateOfBirth")
+    public RString getTxtDateOfBirth() {
+        return txtDateOfBirth;
+    }
+
+    /*
+     * settxtDateOfBirth
+     * @param txtDateOfBirth txtDateOfBirth
+     */
+    @JsonProperty("txtDateOfBirth")
+    public void setTxtDateOfBirth(RString txtDateOfBirth) {
+        this.txtDateOfBirth = txtDateOfBirth;
+    }
+
+    /*
+     * gettxtGender
+     * @return txtGender
+     */
+    @JsonProperty("txtGender")
+    public RString getTxtGender() {
+        return txtGender;
+    }
+
+    /*
+     * settxtGender
+     * @param txtGender txtGender
+     */
+    @JsonProperty("txtGender")
+    public void setTxtGender(RString txtGender) {
+        this.txtGender = txtGender;
+    }
+
+    /*
+     * gettxtJuminJotai
+     * @return txtJuminJotai
+     */
+    @JsonProperty("txtJuminJotai")
+    public RString getTxtJuminJotai() {
+        return txtJuminJotai;
+    }
+
+    /*
+     * settxtJuminJotai
+     * @param txtJuminJotai txtJuminJotai
+     */
+    @JsonProperty("txtJuminJotai")
+    public void setTxtJuminJotai(RString txtJuminJotai) {
+        this.txtJuminJotai = txtJuminJotai;
+    }
+
+    /*
+     * gettxtLasdecMei
+     * @return txtLasdecMei
+     */
+    @JsonProperty("txtLasdecMei")
+    public RString getTxtLasdecMei() {
+        return txtLasdecMei;
+    }
+
+    /*
+     * settxtLasdecMei
+     * @param txtLasdecMei txtLasdecMei
+     */
+    @JsonProperty("txtLasdecMei")
+    public void setTxtLasdecMei(RString txtLasdecMei) {
+        this.txtLasdecMei = txtLasdecMei;
+    }
+
+    /*
+     * gettxtLasdecCode
+     * @return txtLasdecCode
+     */
+    @JsonProperty("txtLasdecCode")
+    public RString getTxtLasdecCode() {
+        return txtLasdecCode;
+    }
+
+    /*
+     * settxtLasdecCode
+     * @param txtLasdecCode txtLasdecCode
+     */
+    @JsonProperty("txtLasdecCode")
+    public void setTxtLasdecCode(RString txtLasdecCode) {
+        this.txtLasdecCode = txtLasdecCode;
+    }
+
+    // </editor-fold>
+}
