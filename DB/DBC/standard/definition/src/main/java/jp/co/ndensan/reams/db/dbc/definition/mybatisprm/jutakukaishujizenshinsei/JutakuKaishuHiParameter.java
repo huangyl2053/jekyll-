291c21fe2765da4f4b5ae796f684faffb92d8f42
xicongwang <cwxi@sinoi.com.cn>
2016-02-25 13:38:36 +0800

    checkstyle対応

--- DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/mybatisprm/jutakukaishujizenshinsei/JutakuKaishuHiParameter.java
+++ DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/mybatisprm/jutakukaishujizenshinsei/JutakuKaishuHiParameter.java
@@ -14,6 +14,7 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
  *
  */
 @lombok.Getter
+@SuppressWarnings("PMD.UnusedPrivateField")
 public final class JutakuKaishuHiParameter {
 
     private final HihokenshaNo 被保険者番号;
