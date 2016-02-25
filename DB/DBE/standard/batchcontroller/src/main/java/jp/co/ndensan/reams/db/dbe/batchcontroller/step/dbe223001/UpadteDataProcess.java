babca2d342ac5f4179c6ffae6288cc0f0b1b4ef1
xuyue <xuyue@liandisys.com.cn>
2016-02-25 10:41:35 +0800

    DEB223001 排他を削除

--- DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/step/dbe223001/UpadteDataProcess.java
+++ DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/step/dbe223001/UpadteDataProcess.java
@@ -8,12 +8,7 @@ package jp.co.ndensan.reams.db.dbe.batchcontroller.step.dbe223001;
 import java.util.List;
 import jp.co.ndensan.reams.db.dbe.definition.processprm.dbe223001.UpdateProcessParameter;
 import jp.co.ndensan.reams.db.dbe.persistence.db.mapper.basic.IDbT5201NinteichosaIraiJohoMapper;
-import jp.co.ndensan.reams.ur.urz.definition.message.UrErrorMessages;
 import jp.co.ndensan.reams.uz.uza.batch.process.SimpleBatchProcessBase;
-import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
-import jp.co.ndensan.reams.uz.uza.exclusion.LockingKey;
-import jp.co.ndensan.reams.uz.uza.exclusion.RealInitialLocker;
-import jp.co.ndensan.reams.uz.uza.lang.ApplicationException;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.util.di.Transaction;
 
@@ -37,21 +32,7 @@ public class UpadteDataProcess extends SimpleBatchProcessBase {
 
         List<RString> noList = processParameter.get申請書管理番号List();
         for (RString shinseishoKanriNo : noList) {
-
-            LockingKey 排他キー = new LockingKey(SubGyomuCode.DBE認定支援.getGyomuCode().getColumnValue()
-                    .concat(new RString("ShinseishoKanriNo")).concat(shinseishoKanriNo));
-            boolean getLock = RealInitialLocker.tryGetLock(排他キー);
-            if (getLock) {
-                try {
-                    mapper.update認定調査依頼情報BY申請書管理番号(processParameter.toNinteiChosaTokusokujoMybatisParameter(shinseishoKanriNo));
-                    RealInitialLocker.release(排他キー);
-                } catch (Exception e) {
-                    RealInitialLocker.release(排他キー);
-                    throw new ApplicationException(UrErrorMessages.更新に失敗.getMessage());
-                }
-            } else {
-                throw new ApplicationException(UrErrorMessages.排他_バッチ実行中で更新不可.getMessage());
-            }
+            mapper.update認定調査依頼情報BY申請書管理番号(processParameter.toNinteiChosaTokusokujoMybatisParameter(shinseishoKanriNo));
         }
     }
 
