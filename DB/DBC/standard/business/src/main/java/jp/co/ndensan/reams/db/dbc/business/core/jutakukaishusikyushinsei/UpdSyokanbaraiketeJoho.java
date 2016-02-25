e094c4e6ea70779c1ab6e9a1bd9a61c310b6c8d7
xicongwang <cwxi@sinoi.com.cn>
2016-02-25 16:45:26 +0800

    DBC-0990-150 住宅改修費支給申請(修正)

--- DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/core/jutakukaishusikyushinsei/UpdSyokanbaraiketeJoho.java
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/core/jutakukaishusikyushinsei/UpdSyokanbaraiketeJoho.java
@@ -3,27 +3,33 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbc.definition.core.jutakukaishusikyushinsei;
+package jp.co.ndensan.reams.db.dbc.business.core.jutakukaishusikyushinsei;
 
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
-import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShoKisaiHokenshaNo;
+import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HokenshaNo;
+import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleYearMonth;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.math.Decimal;
 
 /**
- * 償還払支給判定結果MyBatisパラメータクラスです。
+ *
+ * 償還払決定情報更新パラメータクラスです。
  */
 @lombok.Getter
 @lombok.Setter
 @SuppressWarnings("PMD.UnusedPrivateField")
-public final class UpShokanHanteiKekkaParameter {
+public class UpdSyokanbaraiketeJoho {
 
     private final HihokenshaNo 被保険者番号;
     private final FlexibleYearMonth サービス提供年月;
     private final RString 整理番号;
-    private final ShoKisaiHokenshaNo 証記載保険者番号;
+    private final RString 証明書;
+    private final ShikibetsuCode 識別コード;
+    private final HokenshaNo 証記載保険者番号;
+    private final FlexibleDate 決定日;
+    private final RString 支給区分;
     private final FlexibleDate 決定年月日;
     private final RString 支給決定区分;
     private final Decimal 支払金額;
@@ -34,10 +40,14 @@ public final class UpShokanHanteiKekkaParameter {
      * コンストラクタです。
      *
      */
-    private UpShokanHanteiKekkaParameter(HihokenshaNo 被保険者番号,
+    private UpdSyokanbaraiketeJoho(HihokenshaNo 被保険者番号,
             FlexibleYearMonth サービス提供年月,
             RString 整理番号,
-            ShoKisaiHokenshaNo 証記載保険者番号,
+            RString 証明書,
+            ShikibetsuCode 識別コード,
+            HokenshaNo 証記載保険者番号,
+            FlexibleDate 決定日,
+            RString 支給区分,
             FlexibleDate 決定年月日,
             RString 支給決定区分,
             Decimal 支払金額,
@@ -46,12 +56,17 @@ public final class UpShokanHanteiKekkaParameter {
         this.被保険者番号 = 被保険者番号;
         this.サービス提供年月 = サービス提供年月;
         this.整理番号 = 整理番号;
+        this.証明書 = 証明書;
+        this.識別コード = 識別コード;
         this.証記載保険者番号 = 証記載保険者番号;
+        this.決定日 = 決定日;
+        this.支給区分 = 支給区分;
         this.決定年月日 = 決定年月日;
         this.支給決定区分 = 支給決定区分;
         this.支払金額 = 支払金額;
         this.前回支払金額 = 前回支払金額;
         this.差額金額合計 = 差額金額合計;
+
     }
 
     /**
@@ -60,24 +75,32 @@ public final class UpShokanHanteiKekkaParameter {
      * @param 被保険者番号 被保険者番号
      * @param サービス提供年月 サービス提供年月
      * @param 整理番号 整理番号
+     * @param 証明書 証明書
+     * @param 識別コード 識別コード
      * @param 証記載保険者番号 証記載保険者番号
+     * @param 決定日 決定日
+     * @param 支給区分 支給区分
      * @param 決定年月日 決定年月日
      * @param 支給決定区分 支給決定区分
      * @param 支払金額 支払金額
      * @param 前回支払金額 前回支払金額
      * @param 差額金額合計 差額金額合計
-     * @return UpShokanHanteiKekkaParameter
+     * @return UpdSyokanbaraiketeJoho
      */
-    public static UpShokanHanteiKekkaParameter createSelectByKeyParam(HihokenshaNo 被保険者番号,
+    public static UpdSyokanbaraiketeJoho createSelectByKeyParam(HihokenshaNo 被保険者番号,
             FlexibleYearMonth サービス提供年月,
             RString 整理番号,
-            ShoKisaiHokenshaNo 証記載保険者番号,
+            RString 証明書,
+            ShikibetsuCode 識別コード,
+            HokenshaNo 証記載保険者番号,
+            FlexibleDate 決定日,
+            RString 支給区分,
             FlexibleDate 決定年月日,
             RString 支給決定区分,
             Decimal 支払金額,
             Decimal 前回支払金額,
             Decimal 差額金額合計) {
-        return new UpShokanHanteiKekkaParameter(被保険者番号, サービス提供年月, 整理番号, 証記載保険者番号,
-                決定年月日, 支給決定区分, 支払金額, 前回支払金額, 差額金額合計);
+        return new UpdSyokanbaraiketeJoho(被保険者番号, サービス提供年月, 整理番号, 証明書, 識別コード,
+                証記載保険者番号, 決定日, 支給区分, 決定年月日, 支給決定区分, 支払金額, 前回支払金額, 差額金額合計);
     }
 }
