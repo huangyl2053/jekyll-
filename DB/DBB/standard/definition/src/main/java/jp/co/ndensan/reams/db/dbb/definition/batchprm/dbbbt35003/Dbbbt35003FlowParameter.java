3314062ea23429ba46712d0a9f213a3242e80f96
xuyue <xuyue@liandisys.com.cn>
2016-02-26 11:09:51 +0800

    「特徴平準化（特徴6月分）通知書一括発行」checkstyleの対応

--- DB/DBB/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbb/definition/batchprm/dbbbt35003/Dbbbt35003FlowParameter.java
+++ DB/DBB/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbb/definition/batchprm/dbbbt35003/Dbbbt35003FlowParameter.java
@@ -5,7 +5,7 @@
  */
 package jp.co.ndensan.reams.db.dbb.definition.batchprm.dbbbt35003;
 
-import jp.co.ndensan.reams.db.dbb.definition.core.valueobject.tokuchoheijunka6gatsutsuchishoikkatsuhakko.TokuchoHeijunka6gatsuTsuchishoIkkatsuHakkoTempData;
+import jp.co.ndensan.reams.db.dbb.definition.core.valueobject.tokuchoheijunka6gatsutsuchishoikkatsuhakko.TsuchishoIkkatsuHakkoTempData;
 import jp.co.ndensan.reams.uz.uza.batch.flow.BatchParameterBase;
 import jp.co.ndensan.reams.uz.uza.lang.RDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
@@ -34,7 +34,7 @@ public class Dbbbt35003FlowParameter extends BatchParameterBase {
      *
      * @param tempData 画面データ
      */
-    public void toDbbt35003Parameter(TokuchoHeijunka6gatsuTsuchishoIkkatsuHakkoTempData tempData) {
+    public void toDbbt35003Parameter(TsuchishoIkkatsuHakkoTempData tempData) {
         this.調定年度 = tempData.get調定年度();
         this.賦課年度 = tempData.get賦課年度();
         this.出力帳票一覧List = tempData.get出力帳票一覧List();
--- DB/DBB/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbb/definition/processprm/dbbbt35003/TsuchishoIdoshaTorokuProcessParameter.java
+++ DB/DBB/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbb/definition/processprm/dbbbt35003/TsuchishoIdoshaTorokuProcessParameter.java
@@ -3,23 +3,20 @@
  * To change this template file, choose Tools | templates
  * and open the template in the editor.
  */
-
 package jp.co.ndensan.reams.db.dbb.definition.processprm.dbbbt35003;
 
 import jp.co.ndensan.reams.uz.uza.batch.parameter.IBatchProcessParameter;
-import jp.co.ndensan.reams.uz.uza.lang.RDateTime;
-import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
- *　
+ *
  * 通知書発行後異動者の登録_バッチ処理クラスパラメータクラスです。
  */
 @lombok.Getter
 @lombok.Setter
 public class TsuchishoIdoshaTorokuProcessParameter implements IBatchProcessParameter {
-    
-    private RString 出力対象区分;
-    private RDateTime 帳票作成日時;
-    private Object 出力帳票一覧List;
+    // TODO DBBBZ35003_特徴平準化（特徴6月分）通知書一括発行.xlsxの「通知書発行後異動者登録」メソッド実装　待ち。
 
+//    private RString 出力対象区分;
+//    private RDateTime 帳票作成日時;
+//    private Object 出力帳票一覧List;
 }
