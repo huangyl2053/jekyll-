44eabcee21474ea4e426023c1ffb5da70fb3745a
梁百川 <pangsw@liandisys.com.cn>
2016-02-26 11:36:59 +0800

    DbT5120Entity移動

--- DB/DBE/standard/persistence/src/test/java/jp/co/ndensan/reams/db/dbe/persistence/db/basic/DbT5120ShinsakaiShinseitodokedeJohoDacTest.java
+++ DB/DBE/standard/persistence/src/test/java/jp/co/ndensan/reams/db/dbe/persistence/db/basic/DbT5120ShinsakaiShinseitodokedeJohoDacTest.java
@@ -9,7 +9,7 @@ import jp.co.ndensan.reams.db.dbe.entity.basic.helper.DbT5120ShinseitodokedeJoho
 import static jp.co.ndensan.reams.db.dbe.entity.basic.helper.DbT5120ShinseitodokedeJohoEntityGenerator.DEFAULT_申請書管理番号;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShinseishoKanriNo;
 import jp.co.ndensan.reams.db.dbz.definition.core.util.optional.Optional;
-import jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT5120ShinseitodokedeJohoEntity;
+import jp.co.ndensan.reams.db.dbe.entity.db.basic.DbT5120ShinseitodokedeJohoEntity;
 import jp.co.ndensan.reams.db.dbz.entity.db.basic.IShinseitodokedeJohoEntity;
 import jp.co.ndensan.reams.db.dbz.testhelper.DbdTestDacBase;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
