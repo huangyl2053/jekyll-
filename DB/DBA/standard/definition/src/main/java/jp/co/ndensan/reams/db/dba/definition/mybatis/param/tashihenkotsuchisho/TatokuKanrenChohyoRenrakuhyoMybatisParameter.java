281b5db079d28c7b533f918e28392b5361213297
huangh <huangh@liandisys.com>
2016-02-27 20:32:05 +0800

    checkstyle対応

--- DB/DBA/standard/definition/src/main/java/jp/co/ndensan/reams/db/dba/definition/mybatis/param/tashihenkotsuchisho/TatokuKanrenChohyoRenrakuhyoMybatisParameter.java
+++ DB/DBA/standard/definition/src/main/java/jp/co/ndensan/reams/db/dba/definition/mybatis/param/tashihenkotsuchisho/TatokuKanrenChohyoRenrakuhyoMybatisParameter.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dba.definition.mybatis.param.TaShichosonJushochiTokureiShisetsuHenkoTsuchisho;
+package jp.co.ndensan.reams.db.dba.definition.mybatis.param.tashihenkotsuchisho;
 
 import jp.co.ndensan.reams.ua.uax.definition.mybatisprm.shikibetsutaisho.IShikibetsuTaishoPSMSearchKey;
 import jp.co.ndensan.reams.ua.uax.definition.mybatisprm.shikibetsutaisho.UaFt200FindShikibetsuTaishoParam;
@@ -18,7 +18,7 @@ public class TatokuKanrenChohyoRenrakuhyoMybatisParameter extends UaFt200FindShi
     /**
      * 共通「宛名取得」情報paramter
      *
-     * @param searchKey
+     * @param searchKey searchKey
      */
     public TatokuKanrenChohyoRenrakuhyoMybatisParameter(IShikibetsuTaishoPSMSearchKey searchKey) {
         super(searchKey);
