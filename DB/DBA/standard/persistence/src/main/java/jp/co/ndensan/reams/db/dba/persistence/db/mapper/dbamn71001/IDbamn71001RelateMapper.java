a43b53f2266433befcf5b99f3df4fc2aad5e4746
xuyue <xuyue@liandisys.com.cn>
2016-02-27 19:41:51 +0800

    checkstyle

--- DB/DBA/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dba/persistence/db/mapper/dbamn71001/IDbamn71001RelateMapper.java
+++ DB/DBA/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dba/persistence/db/mapper/dbamn71001/IDbamn71001RelateMapper.java
@@ -19,53 +19,53 @@ import jp.co.ndensan.reams.ua.uax.entity.db.basic.UaFt200FindShikibetsuTaishoEnt
  * ６５歳年齢到達資格異動のマッパーインタフェースです。
  */
 public interface IDbamn71001RelateMapper {
-    
+
     /**
      * 識別コードを取得します。
-     * 
+     *
      * @param parameter 検索条件
      * @return List<UaFt200FindShikibetsuTaishoEntity>
      */
     List<UaFt200FindShikibetsuTaishoEntity> select識別コード(AtenaMybatisParameter parameter);
-    
-   /**
+
+    /**
      * 資格異動対象者のデータを取得します。
      *
      * @param parameter 検索条件
      * @return 資格異動対象者リスト
      */
     List<ShikakuIdoTaishoshaEntity> select資格異動対象者(AtenaMybatisParameter parameter);
-    
+
     /**
      * 他市町村住所地特例のデータを取得します。
-     * 
+     *
      * @param param 検索条件
      * @return 他市町村住所地特例のデータ
      */
     List<DbT1003TashichosonJushochiTokureiEntity> select他市町村住所地特例(ShikakuIdoTaishoshaShutokuMybatisParameter param);
-    
+
     /**
      * 適用除外者のデータを取得します。
-     * 
+     *
      * @param param 検索条件
      * @return 適用除外者のデータ
      */
     List<DbT1002TekiyoJogaishaEntity> select適用除外者(ShikakuIdoTaishoshaShutokuMybatisParameter param);
-    
+
     /**
      * 資格取得除外者のデータを取得します。
-     * 
-     * @param param
+     *
+     * @param param 検索条件
      * @return 資格取得除外者のデータ
      */
     List<DbT1009ShikakuShutokuJogaishaEntity> select資格取得除外者(ShikakuIdoTaishoshaShutokuMybatisParameter param);
-    
+
     /**
      * 転入保留対象者のデータを取得します。
-     * 
-     * @param param
+     *
+     * @param param 検索条件
      * @return 転入保留対象者のデータ
      */
     List<DbT1010TennyushutsuHoryuTaishoshaEntity> select転入保留対象者(ShikakuIdoTaishoshaShutokuMybatisParameter param);
-    
+
 }
