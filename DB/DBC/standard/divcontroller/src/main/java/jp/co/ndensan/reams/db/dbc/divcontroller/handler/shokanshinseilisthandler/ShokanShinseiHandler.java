e8d1a59cd601fb06a8f8beafb29fea121865fe9a
hezhenzhen <hezhenzhen@liandisys.com.cn>
2016-02-27 19:13:36 +0800

    画面償還払申請一覧の更新

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/shokanshinseilisthandler/ShokanShinseiHandler.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/shokanshinseilisthandler/ShokanShinseiHandler.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbc.divcontroller.handler.ShokanShinseiList;
+package jp.co.ndensan.reams.db.dbc.divcontroller.handler.shokanshinseilisthandler;
 
 import java.util.ArrayList;
 import java.util.List;
