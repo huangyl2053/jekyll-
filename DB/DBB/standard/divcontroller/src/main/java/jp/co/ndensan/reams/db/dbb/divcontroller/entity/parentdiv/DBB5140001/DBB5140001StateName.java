677c9f643875fb26f45cc1f48fb8354b0afad16a
城間 篤人 <n8178@ndensan.co.jp>
2016-02-29 10:30:50 +0900

    レイアウトファイルの修正

--- DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB5140001/DBB5140001StateName.java
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB5140001/DBB5140001StateName.java
@@ -1,4 +1,4 @@
-package jp.co.ndensan.reams.db.dbz.divcontroller.entity.parentdiv.DBZA010001;
+package jp.co.ndensan.reams.db.dbb.divcontroller.entity.parentdiv.DBB5140001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
@@ -7,22 +7,20 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBZA010001の状態名定義クラスです
+ * DBB5140001の状態名定義クラスです
  *
  * @author 自動生成
  */
-public enum DBZA010001StateName implements IStateEnumerations {
+public enum DBB5140001StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
     NoChange(""),
-    不整合一覧("不整合一覧"),
-    資格修正("資格修正"),
-    除外者修正("除外者修正"),
-    他特修正("他特修正");
+    バッチ起動_単一("バッチ起動_単一"),
+    バッチ起動_広域("バッチ起動_広域");
 
     private final RString name;
 
-    private DBZA010001StateName(String name) {
+    private DBB5140001StateName(String name) {
         this.name = new RString(name);
     }
 
