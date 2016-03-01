91d015c678e73ec932a2ae4d758874329f9d0180
n8300姜井図 <n8300@ndensan.co.jp>
2016-03-01 20:58:02 +0900

    ramlと設計書見直し ・DBA_0402_DBAMN11003_他市町村住所地特例者照会 ・DBA_0412_DBAMN11004_適用除外者照会

--- DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/parentdiv/DBA4030011/DBA4030011StateName.java
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/parentdiv/DBA4030011/DBA4030011StateName.java
@@ -1,4 +1,4 @@
-package jp.co.ndensan.reams.db.dba.divcontroller.entity.parentdiv.DBA4020011;
+package jp.co.ndensan.reams.db.dba.divcontroller.entity.parentdiv.DBA4030011;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
@@ -7,20 +7,20 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBA4020011の状態名定義クラスです
+ * DBA4030011の状態名定義クラスです
  *
  * @author 自動生成
  */
-public enum DBA4020011StateName implements IStateEnumerations {
+public enum DBA4030011StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
     NoChange(""),
-    他市町村住所特例者照会("他市町村住所特例者照会"),
+    適用除外者照会("適用除外者照会"),
     完了状態("完了状態");
 
     private final RString name;
 
-    private DBA4020011StateName(String name) {
+    private DBA4030011StateName(String name) {
         this.name = new RString(name);
     }
 
--- DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/parentdiv/DBA4030011/DBA4030011TransitionEventName.java
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/parentdiv/DBA4030011/DBA4030011TransitionEventName.java
@@ -1,26 +1,26 @@
-package jp.co.ndensan.reams.db.dba.divcontroller.entity.parentdiv.DBA4020011;
+package jp.co.ndensan.reams.db.dba.divcontroller.entity.parentdiv.DBA4030011;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 
 /**
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBA4020011の状態名定義クラスです
+ * DBA4030011のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBA4020011StateName implements IStateEnumerations {
+public enum DBA4030011TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    NoChange(""),
-    他市町村住所特例者照会("他市町村住所特例者照会"),
-    完了状態("完了状態");
+    検索条件("検索条件"),
+    完了("完了"),
+    該当者一覧("該当者一覧");
 
     private final RString name;
 
-    private DBA4020011StateName(String name) {
+    private DBA4030011TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
