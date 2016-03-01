996599755f7ab1f2a93c9834cace4ea5148d40fc
sunhadi <sunhaidi@liandisys.com.cn>
2016-03-01 17:31:47 +0800

    DBE-0010-010 認定調査依頼 修正

--- DB/DBE/standard/service/src/main/java/jp/co/ndensan/reams/db/dbe/service/report/chosairaisho/ChosaIraishoPrintService.java
+++ DB/DBE/standard/service/src/main/java/jp/co/ndensan/reams/db/dbe/service/report/chosairaisho/ChosaIraishoPrintService.java
@@ -11,12 +11,12 @@ import jp.co.ndensan.reams.db.dbe.business.report.chosairaisho.ChosaIraishoHeadI
 import jp.co.ndensan.reams.db.dbe.business.report.chosairaisho.ChosaIraishoProperty;
 import jp.co.ndensan.reams.db.dbe.business.report.chosairaisho.ChosaIraishoReport;
 import jp.co.ndensan.reams.db.dbe.business.report.chosairaisho.ChosaIraishoReportJoho;
+import jp.co.ndensan.reams.db.dbe.definition.core.reportId.ReportIdDBE;
 import jp.co.ndensan.reams.db.dbe.entity.report.source.chosairaisho.ChosaIraishoReportSource;
-import jp.co.ndensan.reams.ur.urz.business.report.parts.ninshosha.INinshoshaSourceBuilder;
-import jp.co.ndensan.reams.ur.urz.service.report.parts.ninshosha.INinshoshaSourceBuilderCreator;
-import jp.co.ndensan.reams.ur.urz.service.report.sourcebuilder.ReportSourceBuilders;
-import jp.co.ndensan.reams.uz.uza.biz.GyomuCode;
-import jp.co.ndensan.reams.uz.uza.lang.RDate;
+import jp.co.ndensan.reams.db.dbz.service.util.report.ReportUtil;
+import jp.co.ndensan.reams.ur.urz.entity.report.parts.ninshosha.NinshoshaSource;
+import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.report.IReportProperty;
 import jp.co.ndensan.reams.uz.uza.report.IReportSource;
@@ -44,21 +44,27 @@ public class ChosaIraishoPrintService {
         ChosaIraishoProperty property = new ChosaIraishoProperty();
         try (ReportManager reportManager = new ReportManager()) {
             try (ReportAssembler<ChosaIraishoReportSource> assembler = createAssembler(property, reportManager)) {
-                INinshoshaSourceBuilderCreator ninshoshaSourceBuilderCreator = ReportSourceBuilders.ninshoshaSourceBuilder();
-                INinshoshaSourceBuilder ninshoshaSourceBuilder = ninshoshaSourceBuilderCreator.create(GyomuCode.DB介護保険, RString.EMPTY,
-                        RDate.getNowDate(), assembler.getImageFolderPath());
+                FlexibleDate hakkoYMD = FlexibleDate.getNowDate();
+                if (!headItemlist.isEmpty()) {
+                    hakkoYMD = new FlexibleDate(headItemlist.get(0).getHakkoYMD1());
+                }
+
+                ReportSourceWriter<ChosaIraishoReportSource> reportSourceWriter = new ReportSourceWriter(assembler);
+                NinshoshaSource ninshoshaSource = ReportUtil.get認証者情報(SubGyomuCode.DBE認定支援, ReportIdDBE.DBE220001.getReportId(),
+                        hakkoYMD, reportSourceWriter);
+                RString bunshoNo = ReportUtil.get文書番号(SubGyomuCode.DBE認定支援, ReportIdDBE.DBE220001.getReportId(), hakkoYMD);
                 for (ChosaIraishoHeadItem headItem : headItemlist) {
                     headItem = new ChosaIraishoHeadItem(
-                            ninshoshaSourceBuilder.buildSource().hakkoYMD,
-                            ninshoshaSourceBuilder.buildSource().denshiKoin,
-                            ninshoshaSourceBuilder.buildSource().ninshoshaYakushokuMei,
-                            ninshoshaSourceBuilder.buildSource().ninshoshaYakushokuMei2,
-                            ninshoshaSourceBuilder.buildSource().ninshoshaYakushokuMei1,
-                            ninshoshaSourceBuilder.buildSource().koinMojiretsu,
-                            ninshoshaSourceBuilder.buildSource().ninshoshaShimeiKakeru,
-                            ninshoshaSourceBuilder.buildSource().ninshoshaShimeiKakenai,
-                            ninshoshaSourceBuilder.buildSource().koinShoryaku,
-                            headItem.getBunshoNo(),
+                            ninshoshaSource.hakkoYMD,
+                            ninshoshaSource.denshiKoin,
+                            ninshoshaSource.ninshoshaYakushokuMei,
+                            ninshoshaSource.ninshoshaYakushokuMei2,
+                            ninshoshaSource.ninshoshaYakushokuMei1,
+                            ninshoshaSource.koinMojiretsu,
+                            ninshoshaSource.ninshoshaShimeiKakeru,
+                            ninshoshaSource.ninshoshaShimeiKakenai,
+                            ninshoshaSource.koinShoryaku,
+                            bunshoNo,
                             headItem.getYubinNo1(),
                             headItem.getJushoText(),
                             headItem.getKikanNameText(),
@@ -99,7 +105,7 @@ public class ChosaIraishoPrintService {
                             headItem.getTsuchibun2(),
                             headItem.getRemban());
                     for (ChosaIraishoReport report : toReports(new ChosaIraishoReportJoho(headItem))) {
-                        ReportSourceWriter<ChosaIraishoReportSource> reportSourceWriter = new ReportSourceWriter(assembler);
+
                         report.writeBy(reportSourceWriter);
                     }
                 }
