4f60f4cd6a5d2c0037402fab56ef573a852a6c89
n8300姜井図 <n8300@ndensan.co.jp>
2016-03-02 11:50:05 +0900

    ramlと設計書見直し ・DBA_0500_DBAMN11002_被保険者照会 ・DBA_0220_DBAMN00000_老齢福祉年金 ・DBA_0230_DBAMN00000_医療保険履歴

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/RoreiFukushiNenkinShokai/datagridRireki_Row.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/RoreiFukushiNenkinShokai/datagridRireki_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.RoreiFuku
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Mon Dec 21 10:37:34 JST 2015 
+ * Wed Mar 02 11:18:38 JST 2016 
  */
 
 
