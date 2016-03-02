a306d2d639b500aaa1fccd01a28a02b6067a61ec
yaodongsheng <yaodongsheng@liandisys.com.cn>
2016-03-02 10:50:35 +0800

    14D203D-5147対応

--- DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/shinsakaisukejuruhyo/ShinsakaisukejuruhyoBodyEditor.java
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/shinsakaisukejuruhyo/ShinsakaisukejuruhyoBodyEditor.java
@@ -16,7 +16,7 @@ import jp.co.ndensan.reams.uz.uza.lang.RStringBuilder;
 class ShinsakaisukejuruhyoBodyEditor implements IShinsakaisukejuruhyoEditor {
 
     private static final int INDEX_2 = 2;
-    private static final int INDEX_9 = 9;
+    private static final int INDEX_8 = 8;
     private static final RString FUNN = new RString(":");
     private final ShinsakaisukejuruhyoBodyItem item;
 
@@ -40,7 +40,7 @@ class ShinsakaisukejuruhyoBodyEditor implements IShinsakaisukejuruhyoEditor {
         source.listShinsakaiSukejuru_3 = new FlexibleDate(item.get開催日()).wareki().toDateString();
         RStringBuilder 開催時間 = new RStringBuilder(item.get開催時間());
         開催時間.insert(INDEX_2, FUNN);
-        開催時間.insert(INDEX_9, FUNN);
+        開催時間.insert(INDEX_8, FUNN);
         source.listShinsakaiSukejuru_4 = 開催時間.toRString();
         source.listShinsakaiSukejuru_5 = item.get合議体();
         source.listShinsakaiSukejuru_6 = item.get会場();
