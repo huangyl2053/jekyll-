50794c4dd42cde98d0c334789a59ddd5aaa9f170
unknown <ds_kobayashi@LDNS-DT-1590.sunjapan.com.cn>
2016-03-02 14:16:53 +0800

    介護認定（画面修正）

---
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/commonchilddiv/IchijiHanteiKekkaInfo/IIchijiHanteiKekkaInfoDiv.java
@@ -0,0 +1,10 @@
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.commonchilddiv.IchijiHanteiKekkaInfo;
+
+import jp.co.ndensan.reams.uz.uza.ui.binding.ICommonChildDivBaseProperties;
+
+/*
+ * このコードはツールによって生成されました。
+ */
+public interface IIchijiHanteiKekkaInfoDiv extends ICommonChildDivBaseProperties {
+
+}
