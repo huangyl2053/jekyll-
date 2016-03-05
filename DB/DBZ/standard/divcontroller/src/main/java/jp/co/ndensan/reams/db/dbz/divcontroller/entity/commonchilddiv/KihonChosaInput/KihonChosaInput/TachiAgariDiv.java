6fb67a9d984cc3ebdff94d5385699bc72836721d
wangjie2 <wangjie2@liandisys.com.cn>
2016-03-05 16:56:29 +0800

    認定基本調査入力画面のファイル追加

---
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/KihonChosaInput/KihonChosaInput/TachiAgariDiv.java
@@ -0,0 +1,93 @@
+package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.KihonChosaInput.KihonChosaInput;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+
+/**
+ * TachiAgari のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class TachiAgariDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-01-15_09-59-03">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("btnTachiAgari")
+    private ButtonDialog btnTachiAgari;
+    @JsonProperty("radTachiAgari")
+    private RadioButton radTachiAgari;
+    @JsonProperty("TachiAgariShinseishoKanriNo")
+    private RString TachiAgariShinseishoKanriNo;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * getbtnTachiAgari
+     * @return btnTachiAgari
+     */
+    @JsonProperty("btnTachiAgari")
+    public ButtonDialog getBtnTachiAgari() {
+        return btnTachiAgari;
+    }
+
+    /*
+     * setbtnTachiAgari
+     * @param btnTachiAgari btnTachiAgari
+     */
+    @JsonProperty("btnTachiAgari")
+    public void setBtnTachiAgari(ButtonDialog btnTachiAgari) {
+        this.btnTachiAgari = btnTachiAgari;
+    }
+
+    /*
+     * getradTachiAgari
+     * @return radTachiAgari
+     */
+    @JsonProperty("radTachiAgari")
+    public RadioButton getRadTachiAgari() {
+        return radTachiAgari;
+    }
+
+    /*
+     * setradTachiAgari
+     * @param radTachiAgari radTachiAgari
+     */
+    @JsonProperty("radTachiAgari")
+    public void setRadTachiAgari(RadioButton radTachiAgari) {
+        this.radTachiAgari = radTachiAgari;
+    }
+
+    /*
+     * getTachiAgariShinseishoKanriNo
+     * @return TachiAgariShinseishoKanriNo
+     */
+    @JsonProperty("TachiAgariShinseishoKanriNo")
+    public RString getTachiAgariShinseishoKanriNo() {
+        return TachiAgariShinseishoKanriNo;
+    }
+
+    /*
+     * setTachiAgariShinseishoKanriNo
+     * @param TachiAgariShinseishoKanriNo TachiAgariShinseishoKanriNo
+     */
+    @JsonProperty("TachiAgariShinseishoKanriNo")
+    public void setTachiAgariShinseishoKanriNo(RString TachiAgariShinseishoKanriNo) {
+        this.TachiAgariShinseishoKanriNo = TachiAgariShinseishoKanriNo;
+    }
+
+    // </editor-fold>
+}
