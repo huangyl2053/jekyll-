4d247b16e36c1ea8b998c03517b61785e196c8ed
王康磊 <wangkanglei@ustcsoft.com>
2016-03-07 18:39:51 +0800

    下記一つ機能初版作成：画面設計_DBCMNF2002_国保連情報データ取込

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/dbc0410011/KokuhorenTorikomiList.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/dbc0410011/KokuhorenTorikomiList.java
@@ -5,29 +5,41 @@
  */
 package jp.co.ndensan.reams.db.dbc.divcontroller.controller.parentdiv.dbc0410011;
 
-import java.util.ArrayList;
-import java.util.List;
+import jp.co.ndensan.reams.db.dbc.business.core.basic.KokuhorenTorikomiJohoKey;
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.DBC0410011.KokuhorenTorikomiListDiv;
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.DBC0410011.dgKokuhorenTorikomiList_Row;
+import jp.co.ndensan.reams.db.dbc.divcontroller.handler.dbc0410011.KokuhorenTorikomiListHandler;
+import jp.co.ndensan.reams.db.dbc.service.core.basic.IryoHokenRirekiManager;
+import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.KokanShikibetsuNo;
+import jp.co.ndensan.reams.db.dbz.divcontroller.util.viewstate.ViewStateKey;
+import jp.co.ndensan.reams.ur.urz.definition.message.UrErrorMessages;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
-import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.lang.ApplicationException;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleYearMonth;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
 import jp.co.ndensan.reams.uz.uza.lang.RYearMonth;
+import jp.co.ndensan.reams.uz.uza.lang.Separator;
+import jp.co.ndensan.reams.uz.uza.ui.servlets.ViewStateHolder;
 
 /**
- *
- * @author N2810 久保 里史
+ * 国保連情報データ取込のクラスです
  */
 public class KokuhorenTorikomiList {
 
-    private final RString グリッドソート条件１ = new RString("ichiranHyojijun");
-
+    /**
+     * 初期化
+     *
+     * @param panel KokuhorenTorikomiListDiv
+     * @return response
+     */
     public ResponseData<KokuhorenTorikomiListDiv> onLoad(KokuhorenTorikomiListDiv panel) {
 
-        // TODO n8300姜　ビルドエラー回避のために暫定対応
-//        KokuhorenTorikomiJohoManager realservice = new KokuhorenTorikomiJohoManager();
-//        RYearMonth max処理年月 = realservice.get国保連IF管理Max処理年月();
-//        panel.getTxtShoriYM().setValue(new RDate(max処理年月.getYearValue(), max処理年月.getMonthValue()));
-//        loadGrid(panel, max処理年月);
+        FlexibleYearMonth max処理年月 = IryoHokenRirekiManager.createInstance().getMaxShoriYearMonth();
+        if (max処理年月 == null) {
+            max処理年月 = new FlexibleYearMonth(RDate.getNowDate().getYearMonth().toDateString());
+        }
+        panel.getTxtShoriYM().setValue(new RDate(max処理年月.getYearValue(), max処理年月.getMonthValue()));
+        getHandler(panel).load(panel, new RYearMonth(max処理年月.toDateString()));
         return ResponseData.of(panel).respond();
     }
 
@@ -35,63 +47,38 @@ public class KokuhorenTorikomiList {
      * 処理年月テキストボックス変更時にリストを更新します。
      *
      * @param panel KokuhorenTorikomiListDiv
-     * @return response
+     * @return ResponseData
      */
     public ResponseData<KokuhorenTorikomiListDiv> onChange_txtShoriYM(KokuhorenTorikomiListDiv panel) {
-        loadGrid(panel, panel.getTxtShoriYM().getValue().getYearMonth());
-
+        if (panel.getTxtShoriYM().getValue() == null) {
+            throw new ApplicationException(UrErrorMessages.必須.getMessage().replace("処理年月"));
+        }
+        getHandler(panel).load(panel, panel.getTxtShoriYM().getValue().getYearMonth());
         return ResponseData.of(panel).respond();
     }
 
-    private void loadGrid(KokuhorenTorikomiListDiv panel, RYearMonth 処理年月) {
-
-//        KokuhorenTorikomiJohoManager realservice = new KokuhorenTorikomiJohoManager();
-//        List<KokuhorenTorikomiJohoModel> KokuhorenTorikomiJoho = realservice.get国保連取込情報(処理年月);
-        List<dgKokuhorenTorikomiList_Row> kokuhorenTorikomiListDataSource = new ArrayList<>();
-
-//        for (KokuhorenTorikomiJohoModel model : KokuhorenTorikomiJoho) {
-//            KokuhorenTorikomiListDataSource.add(createKokuhorenTorikomiRow(model));
-//        }
-        panel.getDgKokuhorenTorikomiList().setDataSource(kokuhorenTorikomiListDataSource);
-
-        panel.getDgKokuhorenTorikomiList().setSortOrder(グリッドソート条件１);
-    }
-
-    public ResponseData<KokuhorenTorikomiListDiv> onSelect_dgKokuhorenTorikomiList_Row(KokuhorenTorikomiListDiv panel) {
+    /**
+     * onSelect_dgKokuhorenTorikomiList_Row
+     *
+     * @param panel KokuhorenTorikomiListDiv
+     * @return ResponseData
+     */
+    public ResponseData<KokuhorenTorikomiListDiv> onSelect_dgKokuhorenTorikomiList_Row(
+            KokuhorenTorikomiListDiv panel) {
 
         dgKokuhorenTorikomiList_Row row = panel.getDgKokuhorenTorikomiList().getSelectedItems().get(0);
-//        KokuhorenJohoTorikomiBatchKidoModel viewStateHolder = new KokuhorenJohoTorikomiBatchKidoModel();
-        //      viewStateHolder.setバッチID(row.getBatchID());
-        //      viewStateHolder.set交換識別番号(row.getKokanShikibetsuNo());
-        //      viewStateHolder.set再処理フラグ(row.getSaishoriFlag());
-        //      viewStateHolder.set処理名(row.getTxtTorikomiJoho());
-        //     viewStateHolder.set処理年月(panel.getTxtShoriYM().getValue().seireki().separator(Separator.NONE).getYearMonth());
-//        ViewStateHolder.put(ViewStateHolderName.国保連取込情報, viewStateHolder);
+        KokuhorenTorikomiJohoKey viewStateHolder = new KokuhorenTorikomiJohoKey(
+                panel.getTxtShoriYM().getValue().seireki().separator(Separator.NONE).getYearMonth(),
+                new KokanShikibetsuNo(row.getKokanShikibetsuNo()),
+                row.getTxtTorikomiJoho(),
+                row.getSaishoriFlag(),
+                row.getBatchID());
+        ViewStateHolder.put(ViewStateKey.国保連取込情報, viewStateHolder);
 
         return ResponseData.of(panel).respond();
     }
 
-//    private dgKokuhorenTorikomiList_Row createKokuhorenTorikomiRow(KokuhorenTorikomiJohoModel model) {
-//        dgKokuhorenTorikomiList_Row row = new dgKokuhorenTorikomiList_Row(RString.EMPTY, RString.EMPTY, RString.EMPTY, RString.EMPTY, RString.EMPTY, RString.EMPTY, RString.EMPTY, RString.EMPTY, new TextBoxNum(), new TextBoxDate(), RString.EMPTY);
-//        KokuhorenJohoTorikomi business = new KokuhorenJohoTorikomi();
-//        RStringBuilder rsb = new RStringBuilder();
-//        row.setTorikomiFlag(business.get取込フラグ(model.get処理名()));
-//        rsb.append("[");
-//        rsb.append(model.get交換識別番号());
-//        rsb.append("]");
-//        rsb.append(model.get処理名());
-//        row.setTxtTorikomiJoho(rsb.toRString());
-//        row.setTxtZenZengetsu(business.get処理状態(model.get前々月処理状態()));
-//        row.setTxtZengetsu(business.get処理状態(model.get前月処理状態()));
-//        row.setTxtTogetsuJotai(model.get当月処理状態());
-//        row.setTxtShoriNichiji(model.get当月処理日時().toDateString());
-//        row.setSaishoriFlag(business.get再処理フラグコード(model.get処理名()));
-//        row.getIchiranHyojijun().setValue(new Decimal(model.get一覧表示順().toString()));
-//        row.setBatchID(model.getバッチID());
-//        row.setKokanShikibetsuNo(model.get交換識別番号());
-//TODO n3317塚田　画面遷移させるために一時的に処理を修正
-//        row.setSelectButtonState(business.get選択ボタン状態(row.getTorikomiFlag(), row.getTxtTogetsuJotai()));
-//        row.setSelectButtonState(DataGridButtonState.Enabled);
-//        return row;
-//    }
+    private KokuhorenTorikomiListHandler getHandler(KokuhorenTorikomiListDiv div) {
+        return new KokuhorenTorikomiListHandler(div);
+    }
 }
