6a0eb77da55fd1f2e110964e56f082547580e7cc
wangjie2 <wangjie2@liandisys.com.cn>
2016-03-07 09:58:56 +0800

    画面設計_KihonChosaInput_認定基本調査入力の訂正

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/KihonChosaInput/KihonChosaInput/IKihonChosaInputDiv.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/KihonChosaInput/KihonChosaInput/IKihonChosaInputDiv.java
@@ -15,7 +15,9 @@ public interface IKihonChosaInputDiv extends ICommonChildDivBaseProperties, IDia
      * 共有子Divの状態を初期化します。
      *
      * @param 申請書管理番号 申請書管理番号
+     * @param 認定調査依頼履歴番号 認定調査依頼履歴番号
+     * @param 初期状態モード 初期状態モード
      */
-    void onLoad(ShinseishoKanriNo 申請書管理番号, RString 認定調査依頼履歴番号);
+    void onLoad(ShinseishoKanriNo 申請書管理番号, RString 認定調査依頼履歴番号, RString 初期状態モード);
 
 }
