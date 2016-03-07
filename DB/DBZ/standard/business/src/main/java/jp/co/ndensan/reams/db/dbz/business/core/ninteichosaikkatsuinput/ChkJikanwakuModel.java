914fc2c7e678adbd4cef7195944b08b25ebc78f9
zhengshukai <zhengshukai@liandisys.com.cn>
2016-03-07 09:37:02 +0800

    調査時間一括設定　ソース修正

---
+++ DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/core/ninteichosaikkatsuinput/ChkJikanwakuModel.java
@@ -0,0 +1,224 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbz.business.core.ninteichosaikkatsuinput;
+
+/**
+ *
+ * 調査時間一括設定のダイアログパラメータクラスです。
+ */
+public class ChkJikanwakuModel {
+
+    private boolean 時間枠1;
+    private boolean 時間枠2;
+    private boolean 時間枠3;
+    private boolean 時間枠4;
+    private boolean 時間枠5;
+    private boolean 時間枠6;
+    private boolean 時間枠7;
+    private boolean 時間枠8;
+    private boolean 時間枠9;
+    private boolean 時間枠10;
+    private boolean 既に設定済みの場合上書きする;
+
+    /**
+     * 既に設定済みの場合上書きするを返します。
+     *
+     * @return 既に設定済みの場合上書きする
+     */
+    public boolean is既に設定済みの場合上書きする() {
+        return 既に設定済みの場合上書きする;
+    }
+
+    /**
+     * 時間枠1を返します。
+     *
+     * @return 時間枠1
+     */
+    public boolean is時間枠1() {
+        return 時間枠1;
+    }
+
+    /**
+     * 時間枠2を返します。
+     *
+     * @return 時間枠2
+     */
+    public boolean is時間枠2() {
+        return 時間枠2;
+    }
+
+    /**
+     * 時間枠3を返します。
+     *
+     * @return 時間枠3
+     */
+    public boolean is時間枠3() {
+        return 時間枠3;
+    }
+
+    /**
+     * 時間枠4を返します。
+     *
+     * @return 時間枠4
+     */
+    public boolean is時間枠4() {
+        return 時間枠4;
+    }
+
+    /**
+     * 時間枠5を返します。
+     *
+     * @return 時間枠5
+     */
+    public boolean is時間枠5() {
+        return 時間枠5;
+    }
+
+    /**
+     * 時間枠6を返します。
+     *
+     * @return 時間枠6
+     */
+    public boolean is時間枠6() {
+        return 時間枠6;
+    }
+
+    /**
+     * 時間枠7を返します。
+     *
+     * @return 時間枠7
+     */
+    public boolean is時間枠7() {
+        return 時間枠7;
+    }
+
+    /**
+     * 時間枠8を返します。
+     *
+     * @return 時間枠8
+     */
+    public boolean is時間枠8() {
+        return 時間枠8;
+    }
+
+    /**
+     * 時間枠9を返します。
+     *
+     * @return 時間枠9
+     */
+    public boolean is時間枠9() {
+        return 時間枠9;
+    }
+
+    /**
+     * 時間枠10を返します。
+     *
+     * @return 時間枠10
+     */
+    public boolean is時間枠10() {
+        return 時間枠10;
+    }
+
+    /**
+     * 時間枠1を設定します。
+     *
+     * @param 時間枠1 boolean
+     */
+    public void set時間枠1(boolean 時間枠1) {
+        this.時間枠1 = 時間枠1;
+    }
+
+    /**
+     * 時間枠2を設定します。
+     *
+     * @param 時間枠2 boolean
+     */
+    public void set時間枠2(boolean 時間枠2) {
+        this.時間枠2 = 時間枠2;
+    }
+
+    /**
+     * 時間枠3を設定します。
+     *
+     * @param 時間枠3 boolean
+     */
+    public void set時間枠3(boolean 時間枠3) {
+        this.時間枠3 = 時間枠3;
+    }
+
+    /**
+     * 時間枠4を設定します。
+     *
+     * @param 時間枠4 boolean
+     */
+    public void set時間枠4(boolean 時間枠4) {
+        this.時間枠4 = 時間枠4;
+    }
+
+    /**
+     * 時間枠5を設定します。
+     *
+     * @param 時間枠5 boolean
+     */
+    public void set時間枠5(boolean 時間枠5) {
+        this.時間枠5 = 時間枠5;
+    }
+
+    /**
+     * 時間枠6を設定します。
+     *
+     * @param 時間枠6 boolean
+     */
+    public void set時間枠6(boolean 時間枠6) {
+        this.時間枠6 = 時間枠6;
+    }
+
+    /**
+     * 時間枠7を設定します。
+     *
+     * @param 時間枠7 boolean
+     */
+    public void set時間枠7(boolean 時間枠7) {
+        this.時間枠7 = 時間枠7;
+    }
+
+    /**
+     * 時間枠8を設定します。
+     *
+     * @param 時間枠8 boolean
+     */
+    public void set時間枠8(boolean 時間枠8) {
+        this.時間枠8 = 時間枠8;
+    }
+
+    /**
+     * 時間枠9を設定します。
+     *
+     * @param 時間枠9 boolean
+     */
+    public void set時間枠9(boolean 時間枠9) {
+        this.時間枠9 = 時間枠9;
+    }
+
+    /**
+     * 時間枠10を設定します。
+     *
+     * @param 時間枠10 boolean
+     */
+    public void set時間枠10(boolean 時間枠10) {
+        this.時間枠10 = 時間枠10;
+    }
+
+    /**
+     * 既に設定済みの場合上書きするを設定します。
+     *
+     * @param 既に設定済みの場合上書きする boolean
+     */
+    public void set既に設定済みの場合上書きする(boolean 既に設定済みの場合上書きする) {
+        this.既に設定済みの場合上書きする = 既に設定済みの場合上書きする;
+    }
+
+}
--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/handler/commonchilddiv/ninteichosaikkatsuinput/NinteiChosaIkkatsuInputHandler.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/handler/commonchilddiv/ninteichosaikkatsuinput/NinteiChosaIkkatsuInputHandler.java
@@ -7,6 +7,7 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.handler.commonchilddiv.ninteich
 
 import java.util.ArrayList;
 import java.util.List;
+import jp.co.ndensan.reams.db.dbz.business.core.ninteichosaikkatsuinput.ChkJikanwakuModel;
 import jp.co.ndensan.reams.db.dbz.definition.core.configkeys.ConfigNameDBE;
 import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.NinteiChosaIkkatsuInput.NinteiChosaIkkatsuInput.NinteiChosaIkkatsuInputDiv;
 import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
@@ -47,10 +48,12 @@ public class NinteiChosaIkkatsuInputHandler {
     /**
      * 共通子DIVを初期化します。
      *
+     * @return ChkJikanwakuModel
      */
-    public void initialize() {
+    public ChkJikanwakuModel initialize() {
         set非活性();
-        set時間();
+        return set時間();
+
     }
 
     private void set非活性() {
@@ -86,201 +89,214 @@ public class NinteiChosaIkkatsuInputHandler {
         div.getTblJikanwaku2().getTxtEndTime10().setDisabled(true);
     }
 
-    private void set時間() {
+    private ChkJikanwakuModel set時間() {
         List<RString> keylist = new ArrayList<>();
         keylist.add(KEY_0);
+        ChkJikanwakuModel chkJikanwakuModel = new ChkJikanwakuModel();
         RString 調査スケジュール最大時間枠 = BusinessConfig.get(ConfigNameDBE.調査スケジュール最大時間枠, SubGyomuCode.DBE認定支援);
         if (Integer.valueOf(調査スケジュール最大時間枠.toString()) >= 最大時間枠_1) {
-            div.getTblJikanwaku1().getChkJikanwaku1().setDisabled(false);
-            set時間枠1(keylist);
+            set時間枠1(keylist, chkJikanwakuModel);
         }
         if (Integer.valueOf(調査スケジュール最大時間枠.toString()) >= 最大時間枠_2) {
-            div.getTblJikanwaku1().getChkJikanwaku2().setDisabled(false);
-            set時間枠2(keylist);
+            set時間枠2(keylist, chkJikanwakuModel);
         }
         if (Integer.valueOf(調査スケジュール最大時間枠.toString()) >= 最大時間枠_3) {
-            div.getTblJikanwaku1().getChkJikanwaku3().setDisabled(false);
-            set時間枠3(keylist);
+            set時間枠3(keylist, chkJikanwakuModel);
         }
         if (Integer.valueOf(調査スケジュール最大時間枠.toString()) >= 最大時間枠_4) {
-            div.getTblJikanwaku1().getChkJikanwaku4().setDisabled(false);
-            set時間枠4(keylist);
+            set時間枠4(keylist, chkJikanwakuModel);
         }
         if (Integer.valueOf(調査スケジュール最大時間枠.toString()) >= 最大時間枠_5) {
-            div.getTblJikanwaku1().getChkJikanwaku5().setDisabled(false);
-            set時間枠5(keylist);
+            set時間枠5(keylist, chkJikanwakuModel);
         }
         if (Integer.valueOf(調査スケジュール最大時間枠.toString()) >= 最大時間枠_6) {
-            div.getTblJikanwaku2().getChkJikanwaku6().setDisabled(true);
-            set時間枠6(keylist);
+            set時間枠6(keylist, chkJikanwakuModel);
         }
         if (Integer.valueOf(調査スケジュール最大時間枠.toString()) >= 最大時間枠_7) {
-            div.getTblJikanwaku2().getChkJikanwaku7().setDisabled(true);
-            set時間枠7(keylist);
+            set時間枠7(keylist, chkJikanwakuModel);
         }
         if (Integer.valueOf(調査スケジュール最大時間枠.toString()) >= 最大時間枠_8) {
-            div.getTblJikanwaku2().getChkJikanwaku8().setDisabled(true);
-            set時間枠8(keylist);
+            set時間枠8(keylist, chkJikanwakuModel);
         }
         if (Integer.valueOf(調査スケジュール最大時間枠.toString()) >= 最大時間枠_9) {
-            div.getTblJikanwaku2().getChkJikanwaku9().setDisabled(true);
-            set時間枠9(keylist);
+            set時間枠9(keylist, chkJikanwakuModel);
         }
         if (Integer.valueOf(調査スケジュール最大時間枠.toString()) >= 最大時間枠_10) {
-            div.getTblJikanwaku2().getChkJikanwaku10().setDisabled(true);
-            set時間枠10(keylist);
+            set時間枠10(keylist, chkJikanwakuModel);
         }
         div.getChkUpdate().setSelectedItemsByKey(keylist);
-
+        chkJikanwakuModel.set既に設定済みの場合上書きする(true);
+        return chkJikanwakuModel;
     }
 
-    private void set時間枠1(List<RString> keylist) {
+    private void set時間枠1(List<RString> keylist, ChkJikanwakuModel chkJikanwakuModel) {
         RString 時間枠1 = BusinessConfig.get(ConfigNameDBE.調査スケジュール時間枠1, SubGyomuCode.DBE認定支援);
-        if (時間枠1 != null && !時間枠1.isEmpty()) {
+        if (!時間枠1.isNullOrEmpty()) {
             List<RString> start = 時間枠1.split(LINE.toString()).get(0).split(COLON.toString());
             int starthour = Integer.valueOf(start.get(0).toString());
             int startminute = Integer.valueOf(start.get(1).toString());
             List<RString> end = 時間枠1.split(LINE.toString()).get(1).split(COLON.toString());
             int endhour = Integer.valueOf(end.get(0).toString());
             int endminute = Integer.valueOf(end.get(1).toString());
+            div.getTblJikanwaku1().getChkJikanwaku1().setDisabled(false);
             div.getTblJikanwaku1().getChkJikanwaku1().setSelectedItemsByKey(keylist);
             div.getTblJikanwaku1().getTxtStartTime1().setValue(RTime.of(starthour, startminute));
             div.getTblJikanwaku1().getTxtEndTime1().setValue(RTime.of(endhour, endminute));
+            chkJikanwakuModel.set時間枠1(true);
         }
     }
 
-    private void set時間枠2(List<RString> keylist) {
+    private void set時間枠2(List<RString> keylist, ChkJikanwakuModel chkJikanwakuModel) {
         RString 時間枠2 = BusinessConfig.get(ConfigNameDBE.調査スケジュール時間枠2, SubGyomuCode.DBE認定支援);
-        if (時間枠2 != null && !時間枠2.isEmpty()) {
+        if (!時間枠2.isNullOrEmpty()) {
             List<RString> start = 時間枠2.split(LINE.toString()).get(0).split(COLON.toString());
             int starthour = Integer.valueOf(start.get(0).toString());
             int startminute = Integer.valueOf(start.get(1).toString());
             List<RString> end = 時間枠2.split(LINE.toString()).get(1).split(COLON.toString());
             int endhour = Integer.valueOf(end.get(0).toString());
             int endminute = Integer.valueOf(end.get(1).toString());
+            div.getTblJikanwaku1().getChkJikanwaku2().setDisabled(false);
             div.getTblJikanwaku1().getChkJikanwaku2().setSelectedItemsByKey(keylist);
             div.getTblJikanwaku1().getTxtStartTime2().setValue(RTime.of(starthour, startminute));
             div.getTblJikanwaku1().getTxtEndTime2().setValue(RTime.of(endhour, endminute));
+            chkJikanwakuModel.set時間枠2(true);
         }
     }
 
-    private void set時間枠3(List<RString> keylist) {
+    private void set時間枠3(List<RString> keylist, ChkJikanwakuModel chkJikanwakuModel) {
         RString 時間枠3 = BusinessConfig.get(ConfigNameDBE.調査スケジュール時間枠3, SubGyomuCode.DBE認定支援);
-        if (時間枠3 != null && !時間枠3.isEmpty()) {
+        if (!時間枠3.isNullOrEmpty()) {
             List<RString> start = 時間枠3.split(LINE.toString()).get(0).split(COLON.toString());
             int starthour = Integer.valueOf(start.get(0).toString());
             int startminute = Integer.valueOf(start.get(1).toString());
             List<RString> end = 時間枠3.split(LINE.toString()).get(1).split(COLON.toString());
             int endhour = Integer.valueOf(end.get(0).toString());
             int endminute = Integer.valueOf(end.get(1).toString());
+            div.getTblJikanwaku1().getChkJikanwaku3().setDisabled(false);
             div.getTblJikanwaku1().getChkJikanwaku3().setSelectedItemsByKey(keylist);
             div.getTblJikanwaku1().getTxtStartTime3().setValue(RTime.of(starthour, startminute));
             div.getTblJikanwaku1().getTxtEndTime3().setValue(RTime.of(endhour, endminute));
+            chkJikanwakuModel.set時間枠3(true);
         }
     }
 
-    private void set時間枠4(List<RString> keylist) {
+    private void set時間枠4(List<RString> keylist, ChkJikanwakuModel chkJikanwakuModel) {
         RString 時間枠4 = BusinessConfig.get(ConfigNameDBE.調査スケジュール時間枠4, SubGyomuCode.DBE認定支援);
-        if (時間枠4 != null && !時間枠4.isEmpty()) {
+        if (!時間枠4.isNullOrEmpty()) {
             List<RString> start = 時間枠4.split(LINE.toString()).get(0).split(COLON.toString());
             int starthour = Integer.valueOf(start.get(0).toString());
             int startminute = Integer.valueOf(start.get(1).toString());
             List<RString> end = 時間枠4.split(LINE.toString()).get(1).split(COLON.toString());
             int endhour = Integer.valueOf(end.get(0).toString());
             int endminute = Integer.valueOf(end.get(1).toString());
+            div.getTblJikanwaku1().getChkJikanwaku4().setDisabled(false);
             div.getTblJikanwaku1().getChkJikanwaku4().setSelectedItemsByKey(keylist);
             div.getTblJikanwaku1().getTxtStartTime4().setValue(RTime.of(starthour, startminute));
             div.getTblJikanwaku1().getTxtEndTime4().setValue(RTime.of(endhour, endminute));
+            chkJikanwakuModel.set時間枠4(true);
         }
     }
 
-    private void set時間枠5(List<RString> keylist) {
+    private void set時間枠5(List<RString> keylist, ChkJikanwakuModel chkJikanwakuModel) {
         RString 時間枠5 = BusinessConfig.get(ConfigNameDBE.調査スケジュール時間枠5, SubGyomuCode.DBE認定支援);
-        if (時間枠5 != null && !時間枠5.isEmpty()) {
+        if (!時間枠5.isNullOrEmpty()) {
             List<RString> start = 時間枠5.split(LINE.toString()).get(0).split(COLON.toString());
             int starthour = Integer.valueOf(start.get(0).toString());
             int startminute = Integer.valueOf(start.get(1).toString());
             List<RString> end = 時間枠5.split(LINE.toString()).get(1).split(COLON.toString());
             int endhour = Integer.valueOf(end.get(0).toString());
             int endminute = Integer.valueOf(end.get(1).toString());
+            div.getTblJikanwaku1().getChkJikanwaku5().setDisabled(false);
             div.getTblJikanwaku1().getChkJikanwaku5().setSelectedItemsByKey(keylist);
             div.getTblJikanwaku1().getTxtStartTime5().setValue(RTime.of(starthour, startminute));
             div.getTblJikanwaku1().getTxtEndTime5().setValue(RTime.of(endhour, endminute));
+            chkJikanwakuModel.set時間枠5(true);
         }
     }
 
-    private void set時間枠6(List<RString> keylist) {
+    private void set時間枠6(List<RString> keylist, ChkJikanwakuModel chkJikanwakuModel) {
         RString 時間枠6 = BusinessConfig.get(ConfigNameDBE.調査スケジュール時間枠6, SubGyomuCode.DBE認定支援);
-        if (時間枠6 != null && !時間枠6.isEmpty()) {
+        if (!時間枠6.isNullOrEmpty()) {
             List<RString> start = 時間枠6.split(LINE.toString()).get(0).split(COLON.toString());
             int starthour = Integer.valueOf(start.get(0).toString());
             int startminute = Integer.valueOf(start.get(1).toString());
             List<RString> end = 時間枠6.split(LINE.toString()).get(1).split(COLON.toString());
             int endhour = Integer.valueOf(end.get(0).toString());
             int endminute = Integer.valueOf(end.get(1).toString());
+            div.getTblJikanwaku2().getChkJikanwaku6().setDisabled(false);
             div.getTblJikanwaku2().getChkJikanwaku6().setSelectedItemsByKey(keylist);
             div.getTblJikanwaku2().getTxtStartTime6().setValue(RTime.of(starthour, startminute));
             div.getTblJikanwaku2().getTxtEndTime6().setValue(RTime.of(endhour, endminute));
+            chkJikanwakuModel.set時間枠6(true);
         }
     }
 
-    private void set時間枠7(List<RString> keylist) {
+    private void set時間枠7(List<RString> keylist, ChkJikanwakuModel chkJikanwakuModel) {
         RString 時間枠7 = BusinessConfig.get(ConfigNameDBE.調査スケジュール時間枠7, SubGyomuCode.DBE認定支援);
-        if (時間枠7 != null && !時間枠7.isEmpty()) {
+        if (!時間枠7.isNullOrEmpty()) {
             List<RString> start = 時間枠7.split(LINE.toString()).get(0).split(COLON.toString());
             int starthour = Integer.valueOf(start.get(0).toString());
             int startminute = Integer.valueOf(start.get(1).toString());
             List<RString> end = 時間枠7.split(LINE.toString()).get(1).split(COLON.toString());
             int endhour = Integer.valueOf(end.get(0).toString());
             int endminute = Integer.valueOf(end.get(1).toString());
+            div.getTblJikanwaku2().getChkJikanwaku7().setDisabled(false);
             div.getTblJikanwaku2().getChkJikanwaku7().setSelectedItemsByKey(keylist);
             div.getTblJikanwaku2().getTxtStartTime7().setValue(RTime.of(starthour, startminute));
             div.getTblJikanwaku2().getTxtEndTime7().setValue(RTime.of(endhour, endminute));
+            chkJikanwakuModel.set時間枠7(true);
         }
     }
 
-    private void set時間枠8(List<RString> keylist) {
+    private void set時間枠8(List<RString> keylist, ChkJikanwakuModel chkJikanwakuModel) {
         RString 時間枠8 = BusinessConfig.get(ConfigNameDBE.調査スケジュール時間枠8, SubGyomuCode.DBE認定支援);
-        if (時間枠8 != null && !時間枠8.isEmpty()) {
+        if (!時間枠8.isNullOrEmpty()) {
             List<RString> start = 時間枠8.split(LINE.toString()).get(0).split(COLON.toString());
             int starthour = Integer.valueOf(start.get(0).toString());
             int startminute = Integer.valueOf(start.get(1).toString());
             List<RString> end = 時間枠8.split(LINE.toString()).get(1).split(COLON.toString());
             int endhour = Integer.valueOf(end.get(0).toString());
             int endminute = Integer.valueOf(end.get(1).toString());
+            div.getTblJikanwaku2().getChkJikanwaku8().setDisabled(false);
             div.getTblJikanwaku2().getChkJikanwaku8().setSelectedItemsByKey(keylist);
             div.getTblJikanwaku2().getTxtStartTime8().setValue(RTime.of(starthour, startminute));
             div.getTblJikanwaku2().getTxtEndTime8().setValue(RTime.of(endhour, endminute));
+            chkJikanwakuModel.set時間枠8(true);
         }
     }
 
-    private void set時間枠9(List<RString> keylist) {
+    private void set時間枠9(List<RString> keylist, ChkJikanwakuModel chkJikanwakuModel) {
         RString 時間枠9 = BusinessConfig.get(ConfigNameDBE.調査スケジュール時間枠9, SubGyomuCode.DBE認定支援);
-        if (時間枠9 != null && !時間枠9.isEmpty()) {
+        if (!時間枠9.isNullOrEmpty()) {
             List<RString> start = 時間枠9.split(LINE.toString()).get(0).split(COLON.toString());
             int starthour = Integer.valueOf(start.get(0).toString());
             int startminute = Integer.valueOf(start.get(1).toString());
             List<RString> end = 時間枠9.split(LINE.toString()).get(1).split(COLON.toString());
             int endhour = Integer.valueOf(end.get(0).toString());
             int endminute = Integer.valueOf(end.get(1).toString());
+            div.getTblJikanwaku2().getChkJikanwaku9().setDisabled(false);
             div.getTblJikanwaku2().getChkJikanwaku9().setSelectedItemsByKey(keylist);
             div.getTblJikanwaku2().getTxtStartTime9().setValue(RTime.of(starthour, startminute));
             div.getTblJikanwaku2().getTxtEndTime9().setValue(RTime.of(endhour, endminute));
+            chkJikanwakuModel.set時間枠9(true);
         }
     }
 
-    private void set時間枠10(List<RString> keylist) {
+    private void set時間枠10(List<RString> keylist, ChkJikanwakuModel chkJikanwakuModel) {
         RString 時間枠10 = BusinessConfig.get(ConfigNameDBE.調査スケジュール時間枠10, SubGyomuCode.DBE認定支援);
-        if (時間枠10 != null && !時間枠10.isEmpty()) {
+        if (!時間枠10.isNullOrEmpty()) {
             List<RString> start = 時間枠10.split(LINE.toString()).get(0).split(COLON.toString());
             int starthour = Integer.valueOf(start.get(0).toString());
             int startminute = Integer.valueOf(start.get(1).toString());
             List<RString> end = 時間枠10.split(LINE.toString()).get(1).split(COLON.toString());
             int endhour = Integer.valueOf(end.get(0).toString());
             int endminute = Integer.valueOf(end.get(1).toString());
+            div.getTblJikanwaku2().getChkJikanwaku10().setDisabled(false);
             div.getTblJikanwaku2().getChkJikanwaku10().setSelectedItemsByKey(keylist);
             div.getTblJikanwaku2().getTxtStartTime10().setValue(RTime.of(starthour, startminute));
             div.getTblJikanwaku2().getTxtEndTime10().setValue(RTime.of(endhour, endminute));
+            chkJikanwakuModel.set時間枠10(true);
         }
     }
+
 }
