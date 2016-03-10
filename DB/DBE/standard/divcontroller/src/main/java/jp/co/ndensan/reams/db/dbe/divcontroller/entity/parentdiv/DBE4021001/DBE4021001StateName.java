49265c4b02df19d843df8fc7525f415fa38ade95
unknown <ds_kobayashi@LDNS-DT-1590.sunjapan.com.cn>
2016-03-10 15:00:32 +0800

    介護認定（画面修正）

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE4021001/DBE4021001StateName.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE4021001/DBE4021001StateName.java
@@ -1,4 +1,4 @@
-package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE2070001;
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE4021001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
@@ -7,20 +7,19 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBE2070001の状態名定義クラスです
+ * DBE4021001の状態名定義クラスです
  *
  * @author 自動生成
  */
-public enum DBE2070001StateName implements IStateEnumerations {
+public enum DBE4021001StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
     NoChange(""),
-    登録("登録"),
-    完了("完了");
+    Default("default");
 
     private final RString name;
 
-    private DBE2070001StateName(String name) {
+    private DBE4021001StateName(String name) {
         this.name = new RString(name);
     }
 
