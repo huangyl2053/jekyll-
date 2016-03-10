97946ff1df12f04b2782bcdcee52ed31e1e872ce
wangkun <wangkun@liandisys.com.cn>
2016-03-10 15:28:19 +0800

    KaigoNinteiShinseishaInfo_介護認定申請者を新規作成しました。

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/KaigoNinteiShinseishaInfo/KaigoNinteiShinseishaInfo/KaigoNinteiShinseishaInfoDiv.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/KaigoNinteiShinseishaInfo/KaigoNinteiShinseishaInfo/KaigoNinteiShinseishaInfoDiv.java
@@ -6,15 +6,18 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.KaigoNint
  */
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jp.co.ndensan.reams.db.dbz.divcontroller.entity.parentdiv.KaigoNinteiAtenaInfo.IKaigoNinteiAtenaInfoDiv;
-import jp.co.ndensan.reams.db.dbz.divcontroller.entity.parentdiv.KaigoNinteiAtenaInfo.KaigoNinteiAtenaInfoDiv;
-import jp.co.ndensan.reams.db.dbz.divcontroller.entity.parentdiv.kaigoshikakukihon.IKaigoShikakuKihonDiv;
-import jp.co.ndensan.reams.db.dbz.divcontroller.entity.parentdiv.kaigoshikakukihon.KaigoShikakuKihonDiv;
-import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
 
 import jp.co.ndensan.reams.uz.uza.ui.binding.Mode;
 import java.util.HashSet;
+import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
+import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShinseishoKanriNo;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.KaigoNinteiAtenaInfo.IKaigoNinteiAtenaInfoDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.KaigoNinteiAtenaInfo.KaigoNinteiAtenaInfoDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.kaigoshikakukihon.KaigoShikakuKihon.IKaigoShikakuKihonDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.kaigoshikakukihon.KaigoShikakuKihon.KaigoShikakuKihonDiv;
+import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.ui.servlets.ICommonChildDivMode;
 import jp.co.ndensan.reams.uz.uza.ui.servlets._CommonChildDivModeUtil;
 
@@ -126,4 +129,47 @@ public class KaigoNinteiShinseishaInfoDiv extends Panel implements IKaigoNinteiS
 
     // </editor-fold>
     //--------------- この行より下にコードを追加してください -------------------
+    private static final RString 連絡先ボタンを = new RString("RenrakusakiAriMode");
+    private static final RString 認定履歴ボタンを = new RString("HihokenrirekiNashiMode");
+    private static final RString 被保履歴ボタンを = new RString("NinteirirekiNashiMode");
+
+    @Override
+    public void load(RString 介護導入形態, RString 処理タイプ, ShikibetsuCode 識別コード, ShinseishoKanriNo 申請書管理番号, HihokenshaNo 被保険者番号, RString 表示モード) {
+       kaigoNinteiAtenaInfo_onload(介護導入形態, 処理タイプ, 識別コード, 申請書管理番号);
+       kaigoShikakuKihon_onload(識別コード, 被保険者番号, 表示モード);
+    }
+    
+    private void kaigoShikakuKihon_onload(ShikibetsuCode 識別コード, HihokenshaNo 被保険者番号, RString 表示モード){
+         if (!RString.isNullOrEmpty(表示モード)) {
+            if (連絡先ボタンを.equals(表示モード)) {
+                getCcdKaigoShikakuKihon().set連絡先ボタンDisable(false);
+                getCcdKaigoShikakuKihon().set認定履歴ボタンDisable(true);
+                getCcdKaigoShikakuKihon().set被保履歴ボタンDisable(true);
+            } else if (認定履歴ボタンを.equals(表示モード)) {
+                getCcdKaigoShikakuKihon().set連絡先ボタンDisable(true);
+                getCcdKaigoShikakuKihon().set認定履歴ボタンDisable(false);
+                getCcdKaigoShikakuKihon().set被保履歴ボタンDisable(true);
+            } else if (被保履歴ボタンを.equals(表示モード)) {
+                getCcdKaigoShikakuKihon().set連絡先ボタンDisable(true);
+                getCcdKaigoShikakuKihon().set認定履歴ボタンDisable(true);
+                getCcdKaigoShikakuKihon().set被保履歴ボタンDisable(false);
+            }
+        }
+
+        // 楊さんより、以下実行です。
+        if (!RString.isNullOrEmpty(識別コード.getColumnValue())) {
+            getCcdKaigoShikakuKihon().onLoad(識別コード);
+        }
+        if (!RString.isNullOrEmpty(被保険者番号.getColumnValue())) {
+            getCcdKaigoShikakuKihon().onLoad(識別コード);
+        }
+    }
+    
+    private void kaigoNinteiAtenaInfo_onload(RString 介護導入形態, RString 処理タイプ, ShikibetsuCode 識別コード, ShinseishoKanriNo 申請書管理番号){
+        getCcdKaigoNinteiAtenaInfo().setKaigoDonyuKeitai(介護導入形態);
+        getCcdKaigoNinteiAtenaInfo().setShoriType(処理タイプ);
+        getCcdKaigoNinteiAtenaInfo().initialize();
+        getCcdKaigoNinteiAtenaInfo().setShinseishaJohoByShikibetsuCode(申請書管理番号, 識別コード);
+    }
+    
 }
