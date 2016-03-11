6e4a8d1ea2d34491a2d74c92f15491ba4ea100e4
unknown <ds_kobayashi@LDNS-DT-1590.sunjapan.com.cn>
2016-03-11 21:17:14 +0800

    介護認定（画面修正）

---
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/KaigoNinteiShinseiKihonJohoInput/KaigoNinteiShinseiKihonJohoInput/NinteiShinseiRiyuDiv.java
@@ -0,0 +1,72 @@
+package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.KaigoNinteiShinseiKihonJohoInput.KaigoNinteiShinseiKihonJohoInput;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+
+/**
+ * NinteiShinseiRiyu のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class NinteiShinseiRiyuDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-02-19_14-33-39">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("btnNinteiShinseiRiyuTeikeibun")
+    private ButtonDialog btnNinteiShinseiRiyuTeikeibun;
+    @JsonProperty("txtNinteiShinseRiyu")
+    private TextBoxMultiLine txtNinteiShinseRiyu;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * getbtnNinteiShinseiRiyuTeikeibun
+     * @return btnNinteiShinseiRiyuTeikeibun
+     */
+    @JsonProperty("btnNinteiShinseiRiyuTeikeibun")
+    public ButtonDialog getBtnNinteiShinseiRiyuTeikeibun() {
+        return btnNinteiShinseiRiyuTeikeibun;
+    }
+
+    /*
+     * setbtnNinteiShinseiRiyuTeikeibun
+     * @param btnNinteiShinseiRiyuTeikeibun btnNinteiShinseiRiyuTeikeibun
+     */
+    @JsonProperty("btnNinteiShinseiRiyuTeikeibun")
+    public void setBtnNinteiShinseiRiyuTeikeibun(ButtonDialog btnNinteiShinseiRiyuTeikeibun) {
+        this.btnNinteiShinseiRiyuTeikeibun = btnNinteiShinseiRiyuTeikeibun;
+    }
+
+    /*
+     * gettxtNinteiShinseRiyu
+     * @return txtNinteiShinseRiyu
+     */
+    @JsonProperty("txtNinteiShinseRiyu")
+    public TextBoxMultiLine getTxtNinteiShinseRiyu() {
+        return txtNinteiShinseRiyu;
+    }
+
+    /*
+     * settxtNinteiShinseRiyu
+     * @param txtNinteiShinseRiyu txtNinteiShinseRiyu
+     */
+    @JsonProperty("txtNinteiShinseRiyu")
+    public void setTxtNinteiShinseRiyu(TextBoxMultiLine txtNinteiShinseRiyu) {
+        this.txtNinteiShinseRiyu = txtNinteiShinseRiyu;
+    }
+
+    // </editor-fold>
+}
