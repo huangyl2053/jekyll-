904abdc567e35c742bfba5164d6c2399b4c87cd1
王康磊 <wangkanglei@ustcsoft.com>
2016-03-14 11:43:00 +0800

    下記一つ機能修正：画面設計_PaymentDate_支払日設定

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/PaymentDate/IPaymentDateDiv.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/PaymentDate/IPaymentDateDiv.java
@@ -11,19 +11,19 @@ import jp.co.ndensan.reams.uz.uza.ui.servlets.ValidationMessageControlPairs;
  */
 public interface IPaymentDateDiv extends ICommonChildDivBaseProperties {
 
-    void onLoad(RString 支払方法機能区分, RString 支払予定日印字有無);
+    public void onLoad(RString 支払方法機能区分, RString 支払予定日印字有無);
 
-    RDate get振込予定日();
+    public RDate get振込予定日();
 
-    RString get支払場所();
+    public RString get支払場所();
 
-    RDate get支払期間FROM();
+    public RDate get支払期間FROM();
 
-    RDate get支払期間TO();
+    public RDate get支払期間TO();
 
-    RTime get開始時間();
+    public RTime get開始時間();
 
-    RTime get終了時間();
+    public RTime get終了時間();
 
-    ValidationMessageControlPairs getCheckMessage();
+    public ValidationMessageControlPairs getCheckMessage();
 }
