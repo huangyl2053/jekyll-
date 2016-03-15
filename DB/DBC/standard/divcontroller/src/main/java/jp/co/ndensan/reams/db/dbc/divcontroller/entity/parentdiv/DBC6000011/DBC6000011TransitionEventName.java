acc1ee02f4c58aea4c1787dc8e3645b8c9f5427e
n8300姜井図 <n8300@ndensan.co.jp>
2016-03-15 15:01:00 +0900

    QA#77910 画面設計_DBCMNF2002_国保連情報データ取込 ・「スケジュール設定」ボタンの処理の追加に伴う修正

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC6000011/DBC6000011TransitionEventName.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC6000011/DBC6000011TransitionEventName.java
@@ -1,4 +1,4 @@
-package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0410011;
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC6000011;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
@@ -7,20 +7,18 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBC0410011のイベント名定義クラスです</br>
+ * DBC6000011のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBC0410011TransitionEventName implements IContainerEvents {
+public enum DBC6000011TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    バッチ起動("バッチ起動"),
-    戻る("戻る"),
-    スケジュール設定へ("スケジュール設定へ");
+    国保連情報データ取込へ("国保連情報データ取込へ");
 
     private final RString name;
 
-    private DBC0410011TransitionEventName(String name) {
+    private DBC6000011TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
