358b3c0457592ec5e93989657bb87d5fde992677
楊謹 <n8345@ndensan.com>
2016-03-15 10:14:10 +0900

    要介護認定通知書発行 要介護認定取消通知書発行

--- DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD5530001/DBD5530001TransitionEventName.java
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD5530001/DBD5530001TransitionEventName.java
@@ -1,4 +1,4 @@
-package jp.co.ndensan.reams.db.dbz.divcontroller.entity.parentdiv.DBZ0200001;
+package jp.co.ndensan.reams.db.dbd.divcontroller.entity.parentdiv.DBD5530001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
@@ -7,19 +7,18 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBZ0200001のイベント名定義クラスです</br>
+ * DBD5530001のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBZ0200001TransitionEventName implements IContainerEvents {
+public enum DBD5530001TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    対象者特定("対象者特定"),
-    終了("終了");
+    対象者検索に戻る("対象者検索に戻る");
 
     private final RString name;
 
-    private DBZ0200001TransitionEventName(String name) {
+    private DBD5530001TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
