465674b50e5fc739cea92c2eda1f5fc35c3f24a7
lipengpeng <lipengpeng@liandisys.com.cn>
2016-03-18 09:41:28 +0800

    DBC-0980-480 帳票設計_DBC200021_償還払支給決定者一覧表 新規作成

---
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/shokanbaraishikyuketteishaichiran/IShokanbaraiShikyuKetteishaIchiranBuilder.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.business.report.shokanbaraishikyuketteishaichiran;
+
+import jp.co.ndensan.reams.db.dbc.entity.report.source.shokanbaraishikyuketteishaichiran.ShokanbaraiShikyuKetteishaIchiranSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportBuilder;
+
+/**
+ * 償還払支給決定者一覧表Builderのインターフェースクラスです。
+ */
+public interface IShokanbaraiShikyuKetteishaIchiranBuilder
+        extends IReportBuilder<ShokanbaraiShikyuKetteishaIchiranSource> {
+
+}
