e85ea7847e880be85bf9ef10d04788028af13117
張鋭 <rzhang@liandisys.com.cn>
2016-03-18 09:14:30 +0800

    バッチ設計_DBBBT21001_調定簿作成　ソースパス修正及び仕様変更（374,375）対応

---
+++ DB/DBB/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbb/entity/db/relate/choteibo/SaishutsuKampuEntity.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbb.entity.db.relate.choteibo;
+
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleYear;
+import jp.co.ndensan.reams.uz.uza.math.Decimal;
+
+/**
+ *
+ * 歳出還付Entityクラスです。
+ */
+@lombok.Getter
+@lombok.Setter
+public class SaishutsuKampuEntity {
+
+    /**
+     * 調定年度
+     */
+    private FlexibleYear choteiNendo;
+    /**
+     * 賦課年度
+     */
+    private FlexibleYear fukaNendo;
+    /**
+     * 特徴歳出還付の件数
+     */
+    private Decimal tkSaishutsuKampuCount;
+    /**
+     * 特徴歳出還付の調定額
+     */
+    private Decimal tkSaishutsuKampuChoteiGaku;
+    /**
+     * 普徴歳出還付の件数
+     */
+    private Decimal fuSaishutsuKampuCount;
+    /**
+     * 普徴歳出還付の調定額
+     */
+    private Decimal fuSaishutsuKampuChoteiGaku;
+}
