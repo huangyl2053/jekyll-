55b20506de82f841e26935894aefe8fade7d93f8
quxiaodong <quxiaodong@liandisys.com.cn>
2016-03-19 15:42:07 +0800

    画面設計_DBCMNF2002_国保連情報受取データ取込_[152]総合事業費請求額通知書情報（CheckList対応）

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/dbc0410013/TsuchishoJoho152.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/dbc0410013/TsuchishoJoho152.java
@@ -6,12 +6,7 @@
 package jp.co.ndensan.reams.db.dbc.divcontroller.controller.parentdiv.dbc0410013;
 
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0410013.TsuchishoJoho152Div;
-import jp.co.ndensan.reams.db.dbc.divcontroller.viewbox.kaigokyufukokuhorenjohotorikomi.KokuhorenDataTorikomiViewStateClass;
-import jp.co.ndensan.reams.db.dbz.definition.core.enumeratedtype.ViewStateHolderName;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
-import jp.co.ndensan.reams.uz.uza.lang.RDate;
-import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.ui.servlets.ViewStateHolder;
 
 /**
  *
@@ -26,12 +21,8 @@ public class TsuchishoJoho152 {
      * @return ResponseData
      */
     public ResponseData<TsuchishoJoho152Div> onLoad(TsuchishoJoho152Div div) {
-        KokuhorenDataTorikomiViewStateClass parmater = new KokuhorenDataTorikomiViewStateClass(
-                RDate.getNowDate().getYearMonth(), new RString("1"));
-        ViewStateHolder.put(ViewStateHolderName.国保連取込情報, parmater);
         div.getCcdKokurenJohoTorikomi().onLoadMode1();
         return ResponseData.of(div).respond();
-
     }
 
     /**
@@ -41,13 +32,10 @@ public class TsuchishoJoho152 {
      * @return ResponseData
      */
     public ResponseData<TsuchishoJoho152Div> onImplement(TsuchishoJoho152Div div) {
-        //TODO
-//        KokuhorenDataTorikomiBatchParameter  parameter =new KokuhorenDataTorikomiBatchParameter();
-
+        // TODO QA471 パラメータ作成ビジネスを呼び出して、バッチを起動する
         div.getCcdKokurenJohoTorikomi().get再処理区分();
         div.getCcdKokurenJohoTorikomi().get処理対象情報();
         div.getCcdKokurenJohoTorikomi().get処理年月();
-
         return ResponseData.of(div).respond();
 
     }
