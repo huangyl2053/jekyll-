006e729750f32f1558bbef8eb09679334248c628
suguangjun <suguangjun@liandisys.com.cn>
2016-03-21 14:15:45 +0800

    画面設計_DBZ.NinteiShinseiRenrakuJiko_市町村連絡事項 仕様変更対応　QA850

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/NinteiShinseiShichosonRenrakuJiko/NinteiShinseiShichosonRenrakuJiko/INinteiShinseiShichosonRenrakuJikoDiv.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/NinteiShinseiShichosonRenrakuJiko/NinteiShinseiShichosonRenrakuJiko/INinteiShinseiShichosonRenrakuJikoDiv.java
@@ -1,10 +1,10 @@
 package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.NinteiShinseiShichosonRenrakuJiko.NinteiShinseiShichosonRenrakuJiko;
 
 import jp.co.ndensan.reams.uz.uza.ui.binding.ICommonChildDivBaseProperties;
+import jp.co.ndensan.reams.uz.uza.ui.binding.IDialogDiv;
 
 /*
  * このコードはツールによって生成されました。
  */
-public interface INinteiShinseiShichosonRenrakuJikoDiv extends ICommonChildDivBaseProperties {
-    
+public interface INinteiShinseiShichosonRenrakuJikoDiv extends ICommonChildDivBaseProperties, IDialogDiv {
 }
