873e7c7e6072d85afc12814eff85a205e991382a
unknown <ds_kobayashi@LDNS-DT-1590.sunjapan.com.cn>
2016-03-21 19:03:25 +0800

    介護認定（画面修正）

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE5180001/DBE5180001TransitionEventName.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE5180001/DBE5180001TransitionEventName.java
@@ -1,26 +1,25 @@
-package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE5610001;
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE5180001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 
 /**
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBE5610001の状態名定義クラスです
+ * DBE5180001のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBE5610001StateName implements IStateEnumerations {
+public enum DBE5180001TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    NoChange(""),
-    初期表示("初期表示"),
-    検索結果("検索結果");
+    一覧に戻る("一覧に戻る"),
+    処理完了("処理完了");
 
     private final RString name;
 
-    private DBE5610001StateName(String name) {
+    private DBE5180001TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
