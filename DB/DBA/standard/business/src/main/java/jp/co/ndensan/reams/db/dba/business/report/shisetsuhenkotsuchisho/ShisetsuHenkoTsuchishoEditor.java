015eec4d6a0bf6e726ead8596efa6916ef2b4ee1
duanzhanli <duanzhanli@liandisys.com.cn>
2016-03-21 15:46:42 +0800

    帳票「介護保険住所地特例施設退所通知書」と「介護保険住所地特例施設変更通知書」の作成

--- DB/DBA/standard/business/src/main/java/jp/co/ndensan/reams/db/dba/business/report/shisetsuhenkotsuchisho/ShisetsuHenkoTsuchishoEditor.java
+++ DB/DBA/standard/business/src/main/java/jp/co/ndensan/reams/db/dba/business/report/shisetsuhenkotsuchisho/ShisetsuHenkoTsuchishoEditor.java
@@ -25,7 +25,44 @@ class ShisetsuHenkoTsuchishoEditor implements IShisetsuHenkoTsuchishoEditor {
 
     @Override
     public ShisetsuHenkoTsuchishoReportSource edit(ShisetsuHenkoTsuchishoReportSource source) {
-
+        source.hokenshaYubinNo = item.getHokenshaYubinNo();
+        source.bunshoNo = item.getBunshoNo();
+        source.hokenshaJusho = item.getHokenshaJusho();
+        source.hakkoYMD = item.getHakkoYMD();
+        source.hokenshaName = item.getHokenshaName();
+        source.tantoBushoName = item.getTantoBushoName();
+        source.barcode = item.getBarcode();
+        source.midashi = item.getMidashi();
+        source.hihokenshaNo1 = item.getHihokenshaNo1();
+        source.hihokenshaNo2 = item.getHihokenshaNo2();
+        source.hihokenshaNo3 = item.getHihokenshaNo3();
+        source.hihokenshaNo4 = item.getHihokenshaNo4();
+        source.hihokenshaNo5 = item.getHihokenshaNo5();
+        source.hihokenshaNo6 = item.getHihokenshaNo6();
+        source.hihokenshaNo7 = item.getHihokenshaNo7();
+        source.hihokenshaNo8 = item.getHihokenshaNo8();
+        source.hihokenshaNo9 = item.getHihokenshaNo9();
+        source.hihokenshaNo10 = item.getHihokenshaNo10();
+        source.taishoshaNameKana = item.getTaishoshaNameKana();
+        source.taishoshaName = item.getTaishoshaName();
+        source.birthYMD = item.getBirthYMD();
+        source.seibetsu = item.getSeibetsu();
+        source.henkoYMD = item.getHenkoYMD();
+        source.henkomaeShisetsuName = item.getHenkomaeShisetsuName();
+        source.henkomaeShisetsuTelNo = item.getHenkomaeShisetsuTelNo();
+        source.henkomaeShisetsuFaxNo = item.getHenkomaeShisetsuFaxNo();
+        source.henkomaeShisetsuYubinNo = item.getHenkomaeShisetsuYubinNo();
+        source.henkomaeShisetsuJusho = item.getHenkomaeShisetsuJusho();
+        source.henkogoShisetsuName = item.getHenkogoShisetsuName();
+        source.henkogoShisetsuTelNo = item.getHenkogoShisetsuTelNo();
+        source.henkogoShisetsuFaxNo = item.getHenkogoShisetsuFaxNo();
+        source.henkogoShisetsuYubinNo = item.getHenkogoShisetsuYubinNo();
+        source.henkogoShisetsuJusho = item.getHenkogoShisetsuJusho();
+        source.denshiKoin = item.getDenshiKoin();
+        source.shomeiHakkoYMD = item.getShomeiHakkoYMD();
+        source.shuchoMei = item.getShuchoMei();
+        source.shichosonMei = item.getShichosonMei();
+        source.koinShoryaku = item.getKoinShoryaku();
         return source;
     }
 }
--- DB/DBA/standard/business/src/main/java/jp/co/ndensan/reams/db/dba/business/report/shisetsutaishotsuchisho/ShisetsuTaishoTsuchishoEditor.java
+++ DB/DBA/standard/business/src/main/java/jp/co/ndensan/reams/db/dba/business/report/shisetsutaishotsuchisho/ShisetsuTaishoTsuchishoEditor.java
@@ -25,7 +25,42 @@ class ShisetsuTaishoTsuchishoEditor implements IShisetsuTaishoTsuchishoEditor {
 
     @Override
     public ShisetsuTaishoTsuchishoReportSource edit(ShisetsuTaishoTsuchishoReportSource source) {
-
+        source.hokenshaYubinNo = item.getHokenshaYubinNo();
+        source.bunshoNo = item.getBunshoNo();
+        source.hokenshaJusho = item.getHokenshaJusho();
+        source.hakkoYMD = item.getHakkoYMD();
+        source.hokenshaName = item.getHokenshaName();
+        source.tantoBushoName = item.getTantoBushoName();
+        source.barcode = item.getBarcode();
+        source.midashi = item.getMidashi();
+        source.hihokenshaNo1 = item.getHihokenshaNo1();
+        source.hihokenshaNo2 = item.getHihokenshaNo2();
+        source.hihokenshaNo3 = item.getHihokenshaNo3();
+        source.hihokenshaNo4 = item.getHihokenshaNo4();
+        source.hihokenshaNo5 = item.getHihokenshaNo5();
+        source.hihokenshaNo6 = item.getHihokenshaNo6();
+        source.hihokenshaNo7 = item.getHihokenshaNo7();
+        source.hihokenshaNo8 = item.getHihokenshaNo8();
+        source.hihokenshaNo9 = item.getHihokenshaNo9();
+        source.hihokenshaNo10 = item.getHihokenshaNo10();
+        source.taishoshaNameKana = item.getTaishoshaNameKana();
+        source.taishoshaName = item.getTaishoshaName();
+        source.birthYMD = item.getBirthYMD();
+        source.seibetsu = item.getSeibetsu();
+        source.taishogoYubinNo = item.getTaishogoYubinNo();
+        source.taishogoJusho = item.getTaishogoJusho();
+        source.taishoYMD = item.getTaishoYMD();
+        source.taishoJiyu = item.getTaishoJiyu();
+        source.shisetsuName = item.getShisetsuName();
+        source.shisetsuTelNo = item.getShisetsuTelNo();
+        source.shisetsuFaxNo = item.getShisetsuFaxNo();
+        source.shisetsuYubinNo = item.getShisetsuYubinNo();
+        source.shisetsuJusho = item.getShisetsuJusho();
+        source.denshiKoin = item.getDenshiKoin();
+        source.shomeiHakkoYMD = item.getShomeiHakkoYMD();
+        source.shuchoMei = item.getShuchoMei();
+        source.shichosonMei2 = item.getShichosonMei2();
+        source.koinShoryaku = item.getKoinShoryaku();
         return source;
     }
 }
