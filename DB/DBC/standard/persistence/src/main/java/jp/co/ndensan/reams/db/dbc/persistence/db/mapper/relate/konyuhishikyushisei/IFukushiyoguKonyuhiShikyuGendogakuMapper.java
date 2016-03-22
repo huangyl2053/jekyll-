9efa9bb0548bcde8183d960030c8b224292c4115
張鋭 <rzhang@liandisys.com.cn>
2016-03-22 09:38:27 +0800

    ビジネス設計_DBCMN51001_福祉用具購入費支給申請 パス修正

--- DB/DBC/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbc/persistence/db/mapper/relate/konyuhishikyushisei/IFukushiyoguKonyuhiShikyuGendogakuMapper.java
+++ DB/DBC/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbc/persistence/db/mapper/relate/konyuhishikyushisei/IFukushiyoguKonyuhiShikyuGendogakuMapper.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbc.persistence.db.mapper.relate.fukushiyogukonyuhishikyushisei;
+package jp.co.ndensan.reams.db.dbc.persistence.db.mapper.relate.konyuhishikyushisei;
 
 import java.util.List;
 import jp.co.ndensan.reams.db.dbc.definition.mybatisprm.fukushiyogukonyuhishikyushisei.ServiceShuruiCodeParameter;
