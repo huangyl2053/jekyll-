b5f27180d683a49f511ff6e6dc9c951f700850c3
wangkanglei <wangkanglei@liandisys.com.cn>
2016-03-22 17:20:14 +0800

    画面設計_DBCMN53001_償還払い費支給申請決定_サービス提供証明書(特定入所者費用）(連番の設定横展開)

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/dbc0820025/TokuteiNyushoshaHiyoPanelHandler.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/dbc0820025/TokuteiNyushoshaHiyoPanelHandler.java
@@ -611,7 +611,7 @@ public class TokuteiNyushoshaHiyoPanelHandler {
                         事業者番号,
                         様式番号,
                         明細番号,
-                        new RString(String.valueOf(連番))).createBuilderForEdit().build();
+                        new RString(String.format("%02d", 連番))).createBuilderForEdit().build();
                 entityAdded = buildShokanTokuteiNyushoshaKaigoServiceHiyo(entityAdded, dgdRow);
                 特定入所者費用List.add(entityAdded);
             }
@@ -631,36 +631,24 @@ public class TokuteiNyushoshaHiyoPanelHandler {
         if (dgdRow.getDefaultDataName2().getValue() != null) {
             entity = entity.createBuilderForEdit().set費用単価(dgdRow.getDefaultDataName2()
                     .getValue().intValue()).build();
-        } else {
-            entity = entity.createBuilderForEdit().set費用単価(0).build();
         }
         if (dgdRow.getDefaultDataName3().getValue() != null) {
             entity = entity.createBuilderForEdit().set負担限度額(dgdRow.getDefaultDataName3()
                     .getValue().intValue()).build();
-        } else {
-            entity = entity.createBuilderForEdit().set負担限度額(0).build();
         }
         if (dgdRow.getDefaultDataName4().getValue() != null) {
             entity = entity.createBuilderForEdit().set日数(dgdRow.getDefaultDataName4().getValue().intValue()).build();
-        } else {
-            entity = entity.createBuilderForEdit().set日数(0).build();
         }
         if (dgdRow.getDefaultDataName5().getValue() != null) {
             entity = entity.createBuilderForEdit().set費用額(dgdRow.getDefaultDataName5().getValue().intValue()).build();
-        } else {
-            entity = entity.createBuilderForEdit().set費用額(0).build();
         }
         if (dgdRow.getDefaultDataName6().getValue() != null) {
             entity = entity.createBuilderForEdit().set保険分請求額(dgdRow.getDefaultDataName6()
                     .getValue().intValue()).build();
-        } else {
-            entity = entity.createBuilderForEdit().set保険分請求額(0).build();
         }
         if (dgdRow.getDefaultDataName7().getValue() != null) {
             entity = entity.createBuilderForEdit().set利用者負担額(dgdRow.getDefaultDataName7()
                     .getValue().intValue()).build();
-        } else {
-            entity = entity.createBuilderForEdit().set利用者負担額(0).build();
         }
         if (div.getPanelTokutei().getTxtHiyogakuTotal().getValue() != null) {
             entity = entity.createBuilderForEdit().set費用額合計(div.getPanelTokutei().getTxtHiyogakuTotal().getValue()
