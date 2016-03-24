a7debc5d9457efd7957561d755d2c7feff8f6a14
wanghui <wanghui@liandisys.com.cn>
2016-03-24 19:33:13 +0800

    checkstyle

--- DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/handler/parentdiv/DBB0030001/FukaKakuteiPanelHandler.java
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/handler/parentdiv/DBB0030001/FukaKakuteiPanelHandler.java
@@ -33,6 +33,7 @@ public class FukaKakuteiPanelHandler {
     private static final RString 仮算定異動賦課処理の結果を確定します = new RString("仮算定異動賦課処理の結果を確定します。");
     private static final RString 本算定賦課処理の結果を確定します = new RString("本算定賦課処理の結果を確定します。");
     private static final RString 本算定 = new RString("本算定異動（現年度）賦課処理の結果を確定します。");
+    private static final RString 確定処理 = new RString("確定処理");
 
     /**
      * コンストラクタ。
@@ -60,7 +61,7 @@ public class FukaKakuteiPanelHandler {
      */
     public boolean 処理名_特徴仮算定賦課(YMDHMS 特徴仮算定賦課の基準日時, YMDHMS 特徴仮算定賦課確定の基準日時) {
         if (特徴仮算定賦課の基準日時 == null) {
-            throw new ApplicationException(UrErrorMessages.実行不可.getMessage().replace("確定処理").evaluate());
+            throw new ApplicationException(UrErrorMessages.実行不可.getMessage().replace((確定処理).toString()).evaluate());
         } else {
             if (特徴仮算定賦課確定の基準日時 == null) {
                 div.getFukaKakuteiBatchParameter().getLblFukaKeisanKakuteiMsg().setText(特別徴収仮算定賦課処理の結果を確定します);
@@ -82,7 +83,7 @@ public class FukaKakuteiPanelHandler {
      */
     public boolean 処理名_普徴仮算定賦課(YMDHMS 普徴仮算定賦課の最大基準日時, YMDHMS 普徴仮算定賦課確定の基準日時) {
         if (普徴仮算定賦課の最大基準日時 == null) {
-            throw new ApplicationException(UrErrorMessages.実行不可.getMessage().replace("確定処理").evaluate());
+            throw new ApplicationException(UrErrorMessages.実行不可.getMessage().replace((確定処理).toString()).evaluate());
         } else {
             if (普徴仮算定賦課確定の基準日時 == null) {
                 div.getFukaKakuteiBatchParameter().getLblFukaKeisanKakuteiMsg().setText(普通徴収仮算定賦課処理の結果を確定します);
@@ -104,7 +105,7 @@ public class FukaKakuteiPanelHandler {
      */
     public boolean 処理名_仮算定異動賦課(YMDHMS 仮算定異動賦課の基準日, YMDHMS 仮算定異動賦課確定の最大基準日時) {
         if (仮算定異動賦課の基準日 == null) {
-            throw new ApplicationException(UrErrorMessages.実行不可.getMessage().replace("確定処理").evaluate());
+            throw new ApplicationException(UrErrorMessages.実行不可.getMessage().replace((確定処理).toString()).evaluate());
         } else {
             if (仮算定異動賦課確定の最大基準日時 == null) {
                 div.getFukaKakuteiBatchParameter().getLblFukaKeisanKakuteiMsg().setText(仮算定異動賦課処理の結果を確定します);
@@ -132,7 +133,7 @@ public class FukaKakuteiPanelHandler {
      */
     public boolean 処理名_本算定賦課(YMDHMS 本算定賦課の基準日時, YMDHMS 本算定賦課確定の基準日時) {
         if (本算定賦課の基準日時 == null) {
-            throw new ApplicationException(UrErrorMessages.実行不可.getMessage().replace("確定処理").evaluate());
+            throw new ApplicationException(UrErrorMessages.実行不可.getMessage().replace((確定処理).toString()).evaluate());
         } else {
             if (本算定賦課確定の基準日時 == null) {
                 div.getFukaKakuteiBatchParameter().getLblFukaKeisanKakuteiMsg().setText(本算定賦課処理の結果を確定します);
@@ -154,7 +155,7 @@ public class FukaKakuteiPanelHandler {
      */
     public boolean 処理名_異動賦課(YMDHMS 異動賦課の基準日時, YMDHMS 異動賦課確定の最大基準日時) {
         if (異動賦課の基準日時 == null) {
-            throw new ApplicationException(UrErrorMessages.実行不可.getMessage().replace("確定処理").evaluate());
+            throw new ApplicationException(UrErrorMessages.実行不可.getMessage().replace((確定処理).toString()).evaluate());
         } else {
             if (異動賦課確定の最大基準日時 == null) {
                 div.getFukaKakuteiBatchParameter().getLblFukaKeisanKakuteiMsg().setText(本算定);
@@ -169,7 +170,7 @@ public class FukaKakuteiPanelHandler {
                 div.getFukaKakuteiBatchParameter().set処理名(DataPassingConverter.serialize(異動賦課確定));
                 return true;
             } else if (異動賦課の基準日時.isBefore(異動賦課確定の最大基準日時)) {
-                throw new ApplicationException(UrErrorMessages.実行不可.getMessage().replace("確定処理").evaluate());
+                throw new ApplicationException(UrErrorMessages.実行不可.getMessage().replace((確定処理).toString()).evaluate());
             }
         }
         return false;
--- DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/core/koseishichosonmaster/KoseiShichosonMaster.java
+++ DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/core/koseishichosonmaster/KoseiShichosonMaster.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbz.business.core.koseishichosonmaster.koseishichosonmaster;
+package jp.co.ndensan.reams.db.dbz.business.core.koseishichosonmaster;
 
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShoKisaiHokenshaNo;
 import jp.co.ndensan.reams.db.dbx.entity.db.basic.DbT7051KoseiShichosonMasterEntity;
