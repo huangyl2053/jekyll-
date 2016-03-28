69e1f0d2267e03407672a09f26643f6ac00b2096
n8300姜井図 <n8300@ndensan.co.jp>
2016-03-28 20:25:00 +0900

    QA#78229 画面設計_DBE2310001_主治医意見書登録 ・DBEUC23101.uicontainerにて、画面遷移を修正 ・DBE2310001.ramlにて、完了状態を追加 ・DBE2070001.ramlにて、遷移イベントを修正

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/dbe2070001/DBE2070001TransitionEventName.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/dbe2070001/DBE2070001TransitionEventName.java
@@ -1,26 +1,24 @@
-package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE2310001;
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE2070001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 
 /**
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBE2310001の状態名定義クラスです
+ * DBE2070001のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBE2310001StateName implements IStateEnumerations {
+public enum DBE2070001TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    NoChange(""),
-    初期表示("初期表示"),
-    完了状態("完了状態");
+    主治医意見書登録へ("主治医意見書登録へ");
 
     private final RString name;
 
-    private DBE2310001StateName(String name) {
+    private DBE2070001TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
