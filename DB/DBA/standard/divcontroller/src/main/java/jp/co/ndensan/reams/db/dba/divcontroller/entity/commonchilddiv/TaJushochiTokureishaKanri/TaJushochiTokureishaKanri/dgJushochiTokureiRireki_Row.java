4d9c7b0db16a04ecaf88119a95aec63d156a2fe5
linghuhang <linghuhang@liandisys.com.cn>
2016-03-28 17:33:00 +0800

    仕様変更の対応

--- DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/commonchilddiv/TaJushochiTokureishaKanri/TaJushochiTokureishaKanri/dgJushochiTokureiRireki_Row.java
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/commonchilddiv/TaJushochiTokureishaKanri/TaJushochiTokureishaKanri/dgJushochiTokureiRireki_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dba.divcontroller.entity.commonchilddiv.TaJushoch
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Mon Mar 28 13:36:33 CST 2016 
+ * Mon Mar 28 17:26:51 CST 2016 
  */
 
 
