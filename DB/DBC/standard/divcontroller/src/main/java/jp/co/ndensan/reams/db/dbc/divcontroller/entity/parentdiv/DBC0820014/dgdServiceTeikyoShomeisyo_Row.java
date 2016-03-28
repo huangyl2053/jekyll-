51c49abc0ba67968c8cbbfb13c99d751e8e01f41
gongliang <gongliang@liandisys.com.cn>
2016-03-28 12:10:42 +0800

    画面設計_DBCMN53001_償還払い費支給申請決定_サービス提供証明書　　画面作成、コード初回アップロード

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0820014/dgdServiceTeikyoShomeisyo_Row.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0820014/dgdServiceTeikyoShomeisyo_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0820014;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Sat Jan 30 12:04:08 JST 2016 
+ * Fri Mar 25 18:39:19 CST 2016 
  */
 
 
@@ -19,7 +19,7 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
  */
 
 public class dgdServiceTeikyoShomeisyo_Row extends DataRow {
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：バージョン情報無し">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-01-15_09-59-03">
 
     private RString data1;
     private RString data2;
