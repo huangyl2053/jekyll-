b43ff0a69a02ba9fecf1460bea51301302955668
zhangzhiming <zhangzhiming@liandisys.com.cn>
2016-03-29 13:24:38 +0800

    画面設計_ShujiiIkenshoiraiShokai_主治医意見書作成依頼照会 checkStyle 対応

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/controller/commonchilddiv/shujiiikenshoiraishokai/ShujiiIkenshoIraiShokai.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/controller/commonchilddiv/shujiiikenshoiraishokai/ShujiiIkenshoIraiShokai.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbe.divcontroller.entity.shujiiikenshoiraishokai;
+package jp.co.ndensan.reams.db.dbe.divcontroller.controller.commonchilddiv.shujiiikenshoiraishokai;
 
 import jp.co.ndensan.reams.db.dbe.divcontroller.entity.commonchilddiv.ShujiiIkenshoIraiShokai.ShujiiIkenshoIraiShokai.ShujiiIkenshoIraiHandler;
 import jp.co.ndensan.reams.db.dbe.divcontroller.entity.commonchilddiv.ShujiiIkenshoIraiShokai.ShujiiIkenshoIraiShokai.ShujiiIkenshoIraiShokaiDiv;
