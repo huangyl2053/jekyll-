11437ee53e044e2fd74081913bf3f460db304ee6
unknown <ds_kobayashi@LDNS-DT-1590.sunjapan.com.cn>
2016-03-30 14:36:51 +0800

    介護認定（画面修正　横幅修正、DBD5210001→DBE0210001）

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE0210001/DBE0210001StateName.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE0210001/DBE0210001StateName.java
@@ -1,4 +1,4 @@
-package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE1920001;
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE0210001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
@@ -7,20 +7,19 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBE1920001の状態名定義クラスです
+ * DBE0210001の状態名定義クラスです
  *
  * @author 自動生成
  */
-public enum DBE1920001StateName implements IStateEnumerations {
+public enum DBE0210001StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
     NoChange(""),
-    初期表示("初期表示"),
-    一覧表示("一覧表示");
+    Default("default");
 
     private final RString name;
 
-    private DBE1920001StateName(String name) {
+    private DBE0210001StateName(String name) {
         this.name = new RString(name);
     }
 
