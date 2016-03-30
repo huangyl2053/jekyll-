2f981d40921f4985a230ee854a28898324b08722
橋本　治義 <n8322@ndensan.co.jp>
2016-03-31 08:20:00 +0900

    画面修正　DBB0130001（特徴平準化（特徴8月分））

--- DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB0130001/TokuchoHeijunkaPanelDiv.java
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB0130001/TokuchoHeijunkaPanelDiv.java
@@ -5,15 +5,16 @@ package jp.co.ndensan.reams.db.dbb.divcontroller.entity.parentdiv.DBB0130001;
  * 不正な動作の原因になります。
  */
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.ur.urz.divcontroller.entity.commonchilddiv.OutputChohyoIchiran.IOutputChohyoIchiranDiv;
+import jp.co.ndensan.reams.ur.urz.divcontroller.entity.commonchilddiv.OutputChohyoIchiran.OutputChohyoIchiranDiv;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
 
 /**
- * Heijunka8ShoriNaiyo のクラスファイル 
+ * TokuchoHeijunkaPanel のクラスファイル 
  * 
  * @author 自動生成
  */
-public class Heijunka8ShoriNaiyoDiv extends Panel {
+public class TokuchoHeijunkaPanelDiv extends Panel {
     // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-01-15_09-59-03">
     /*
      * [ private の作成 ]
@@ -21,10 +22,8 @@ public class Heijunka8ShoriNaiyoDiv extends Panel {
      * コントロール名とフィールド名を取得する
      * private + コントロール名 + フィールド名 の文字列を作成
      */
-    @JsonProperty("txtChoteiNendo")
-    private TextBox txtChoteiNendo;
-    @JsonProperty("txtFukaNendo")
-    private TextBox txtFukaNendo;
+    @JsonProperty("ccdOutputChohyoIchiran")
+    private OutputChohyoIchiranDiv ccdOutputChohyoIchiran;
 
     /*
      * [ GetterとSetterの作成 ]
@@ -33,39 +32,12 @@ public class Heijunka8ShoriNaiyoDiv extends Panel {
      * フィールド名のGetterとSetter を作成
      */
     /*
-     * gettxtChoteiNendo
-     * @return txtChoteiNendo
+     * getccdOutputChohyoIchiran
+     * @return ccdOutputChohyoIchiran
      */
-    @JsonProperty("txtChoteiNendo")
-    public TextBox getTxtChoteiNendo() {
-        return txtChoteiNendo;
-    }
-
-    /*
-     * settxtChoteiNendo
-     * @param txtChoteiNendo txtChoteiNendo
-     */
-    @JsonProperty("txtChoteiNendo")
-    public void setTxtChoteiNendo(TextBox txtChoteiNendo) {
-        this.txtChoteiNendo = txtChoteiNendo;
-    }
-
-    /*
-     * gettxtFukaNendo
-     * @return txtFukaNendo
-     */
-    @JsonProperty("txtFukaNendo")
-    public TextBox getTxtFukaNendo() {
-        return txtFukaNendo;
-    }
-
-    /*
-     * settxtFukaNendo
-     * @param txtFukaNendo txtFukaNendo
-     */
-    @JsonProperty("txtFukaNendo")
-    public void setTxtFukaNendo(TextBox txtFukaNendo) {
-        this.txtFukaNendo = txtFukaNendo;
+    @JsonProperty("ccdOutputChohyoIchiran")
+    public IOutputChohyoIchiranDiv getCcdOutputChohyoIchiran() {
+        return ccdOutputChohyoIchiran;
     }
 
     // </editor-fold>
