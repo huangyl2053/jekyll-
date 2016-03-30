b79a0d0212518b09763625f69a455a77dd80b518
橋本　治義 <n8322@ndensan.co.jp>
2016-03-30 18:51:51 +0900

    画面修正　DBB2710001（特徴対象者同定（一括））

---
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB2710001/dgTokuchoTaishoshaDoteiShoriKakuninTanitsu_Row.java
@@ -0,0 +1,76 @@
+package jp.co.ndensan.reams.db.dbb.divcontroller.entity.parentdiv.DBB2710001;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+ * コードが再生成されるときに損失したりします。
+ * Wed Mar 30 18:48:07 JST 2016 
+ */
+
+
+
+import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
+import jp.co.ndensan.reams.uz.uza.ui.binding.TextBox;
+
+
+/**
+ * dgTokuchoTaishoshaDoteiShoriKakuninTanitsu_Row のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+
+public class dgTokuchoTaishoshaDoteiShoriKakuninTanitsu_Row extends DataRow {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-01-15_09-59-03">
+
+    private TextBox txtShoriMei;
+    private TextBox txtJokyo;
+    private TextBox txtShoriNichiji;
+
+    public dgTokuchoTaishoshaDoteiShoriKakuninTanitsu_Row() {
+        super();
+        this.txtShoriMei = new TextBox();
+        this.txtJokyo = new TextBox();
+        this.txtShoriNichiji = new TextBox();
+        this.setOriginalData("txtShoriMei", txtShoriMei);
+        this.setOriginalData("txtJokyo", txtJokyo);
+        this.setOriginalData("txtShoriNichiji", txtShoriNichiji);
+    }
+
+    public dgTokuchoTaishoshaDoteiShoriKakuninTanitsu_Row(TextBox txtShoriMei, TextBox txtJokyo, TextBox txtShoriNichiji) {
+        super();
+        this.setOriginalData("txtShoriMei", txtShoriMei);
+        this.setOriginalData("txtJokyo", txtJokyo);
+        this.setOriginalData("txtShoriNichiji", txtShoriNichiji);
+        this.txtShoriMei = txtShoriMei;
+        this.txtJokyo = txtJokyo;
+        this.txtShoriNichiji = txtShoriNichiji;
+    }
+
+    public TextBox getTxtShoriMei() {
+        return txtShoriMei;
+    }
+
+    public TextBox getTxtJokyo() {
+        return txtJokyo;
+    }
+
+    public TextBox getTxtShoriNichiji() {
+        return txtShoriNichiji;
+    }
+
+    public void setTxtShoriMei(TextBox txtShoriMei) {
+        this.setOriginalData("txtShoriMei", txtShoriMei);
+        this.txtShoriMei = txtShoriMei;
+    }
+
+    public void setTxtJokyo(TextBox txtJokyo) {
+        this.setOriginalData("txtJokyo", txtJokyo);
+        this.txtJokyo = txtJokyo;
+    }
+
+    public void setTxtShoriNichiji(TextBox txtShoriNichiji) {
+        this.setOriginalData("txtShoriNichiji", txtShoriNichiji);
+        this.txtShoriNichiji = txtShoriNichiji;
+    }
+
+    // </editor-fold>
+}
