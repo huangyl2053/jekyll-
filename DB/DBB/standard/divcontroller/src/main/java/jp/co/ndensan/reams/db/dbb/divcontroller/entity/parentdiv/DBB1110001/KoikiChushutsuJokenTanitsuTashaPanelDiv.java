1d6f90586bdc4e8bd2580421703c28280f597f5a
橋本　治義 <n8322@ndensan.co.jp>
2016-03-31 11:00:29 +0900

    画面修正　DBB1110001（所得情報一覧表作成（広域用／単一他社用））

---
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB1110001/KoikiChushutsuJokenTanitsuTashaPanelDiv.java
@@ -0,0 +1,232 @@
+package jp.co.ndensan.reams.db.dbb.divcontroller.entity.parentdiv.DBB1110001;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+
+/**
+ * KoikiChushutsuJokenTanitsuTashaPanel のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class KoikiChushutsuJokenTanitsuTashaPanelDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-01-15_09-59-03">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("lblKakuteiStYMD")
+    private Label lblKakuteiStYMD;
+    @JsonProperty("lblKakuteiStTime")
+    private Label lblKakuteiStTime;
+    @JsonProperty("lblKakuteiEdYMD")
+    private Label lblKakuteiEdYMD;
+    @JsonProperty("lblKakuteiEdTime")
+    private Label lblKakuteiEdTime;
+    @JsonProperty("lblChushutsuKikan")
+    private Label lblChushutsuKikan;
+    @JsonProperty("txtKakuteiStYMD")
+    private TextBoxDate txtKakuteiStYMD;
+    @JsonProperty("txtKakuteiStTime")
+    private TextBoxTime txtKakuteiStTime;
+    @JsonProperty("lblKara")
+    private Label lblKara;
+    @JsonProperty("txtKakuteiEdYMD")
+    private TextBoxDate txtKakuteiEdYMD;
+    @JsonProperty("txtKakuteiEdTime")
+    private TextBoxTime txtKakuteiEdTime;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * getlblKakuteiStYMD
+     * @return lblKakuteiStYMD
+     */
+    @JsonProperty("lblKakuteiStYMD")
+    public Label getLblKakuteiStYMD() {
+        return lblKakuteiStYMD;
+    }
+
+    /*
+     * setlblKakuteiStYMD
+     * @param lblKakuteiStYMD lblKakuteiStYMD
+     */
+    @JsonProperty("lblKakuteiStYMD")
+    public void setLblKakuteiStYMD(Label lblKakuteiStYMD) {
+        this.lblKakuteiStYMD = lblKakuteiStYMD;
+    }
+
+    /*
+     * getlblKakuteiStTime
+     * @return lblKakuteiStTime
+     */
+    @JsonProperty("lblKakuteiStTime")
+    public Label getLblKakuteiStTime() {
+        return lblKakuteiStTime;
+    }
+
+    /*
+     * setlblKakuteiStTime
+     * @param lblKakuteiStTime lblKakuteiStTime
+     */
+    @JsonProperty("lblKakuteiStTime")
+    public void setLblKakuteiStTime(Label lblKakuteiStTime) {
+        this.lblKakuteiStTime = lblKakuteiStTime;
+    }
+
+    /*
+     * getlblKakuteiEdYMD
+     * @return lblKakuteiEdYMD
+     */
+    @JsonProperty("lblKakuteiEdYMD")
+    public Label getLblKakuteiEdYMD() {
+        return lblKakuteiEdYMD;
+    }
+
+    /*
+     * setlblKakuteiEdYMD
+     * @param lblKakuteiEdYMD lblKakuteiEdYMD
+     */
+    @JsonProperty("lblKakuteiEdYMD")
+    public void setLblKakuteiEdYMD(Label lblKakuteiEdYMD) {
+        this.lblKakuteiEdYMD = lblKakuteiEdYMD;
+    }
+
+    /*
+     * getlblKakuteiEdTime
+     * @return lblKakuteiEdTime
+     */
+    @JsonProperty("lblKakuteiEdTime")
+    public Label getLblKakuteiEdTime() {
+        return lblKakuteiEdTime;
+    }
+
+    /*
+     * setlblKakuteiEdTime
+     * @param lblKakuteiEdTime lblKakuteiEdTime
+     */
+    @JsonProperty("lblKakuteiEdTime")
+    public void setLblKakuteiEdTime(Label lblKakuteiEdTime) {
+        this.lblKakuteiEdTime = lblKakuteiEdTime;
+    }
+
+    /*
+     * getlblChushutsuKikan
+     * @return lblChushutsuKikan
+     */
+    @JsonProperty("lblChushutsuKikan")
+    public Label getLblChushutsuKikan() {
+        return lblChushutsuKikan;
+    }
+
+    /*
+     * setlblChushutsuKikan
+     * @param lblChushutsuKikan lblChushutsuKikan
+     */
+    @JsonProperty("lblChushutsuKikan")
+    public void setLblChushutsuKikan(Label lblChushutsuKikan) {
+        this.lblChushutsuKikan = lblChushutsuKikan;
+    }
+
+    /*
+     * gettxtKakuteiStYMD
+     * @return txtKakuteiStYMD
+     */
+    @JsonProperty("txtKakuteiStYMD")
+    public TextBoxDate getTxtKakuteiStYMD() {
+        return txtKakuteiStYMD;
+    }
+
+    /*
+     * settxtKakuteiStYMD
+     * @param txtKakuteiStYMD txtKakuteiStYMD
+     */
+    @JsonProperty("txtKakuteiStYMD")
+    public void setTxtKakuteiStYMD(TextBoxDate txtKakuteiStYMD) {
+        this.txtKakuteiStYMD = txtKakuteiStYMD;
+    }
+
+    /*
+     * gettxtKakuteiStTime
+     * @return txtKakuteiStTime
+     */
+    @JsonProperty("txtKakuteiStTime")
+    public TextBoxTime getTxtKakuteiStTime() {
+        return txtKakuteiStTime;
+    }
+
+    /*
+     * settxtKakuteiStTime
+     * @param txtKakuteiStTime txtKakuteiStTime
+     */
+    @JsonProperty("txtKakuteiStTime")
+    public void setTxtKakuteiStTime(TextBoxTime txtKakuteiStTime) {
+        this.txtKakuteiStTime = txtKakuteiStTime;
+    }
+
+    /*
+     * getlblKara
+     * @return lblKara
+     */
+    @JsonProperty("lblKara")
+    public Label getLblKara() {
+        return lblKara;
+    }
+
+    /*
+     * setlblKara
+     * @param lblKara lblKara
+     */
+    @JsonProperty("lblKara")
+    public void setLblKara(Label lblKara) {
+        this.lblKara = lblKara;
+    }
+
+    /*
+     * gettxtKakuteiEdYMD
+     * @return txtKakuteiEdYMD
+     */
+    @JsonProperty("txtKakuteiEdYMD")
+    public TextBoxDate getTxtKakuteiEdYMD() {
+        return txtKakuteiEdYMD;
+    }
+
+    /*
+     * settxtKakuteiEdYMD
+     * @param txtKakuteiEdYMD txtKakuteiEdYMD
+     */
+    @JsonProperty("txtKakuteiEdYMD")
+    public void setTxtKakuteiEdYMD(TextBoxDate txtKakuteiEdYMD) {
+        this.txtKakuteiEdYMD = txtKakuteiEdYMD;
+    }
+
+    /*
+     * gettxtKakuteiEdTime
+     * @return txtKakuteiEdTime
+     */
+    @JsonProperty("txtKakuteiEdTime")
+    public TextBoxTime getTxtKakuteiEdTime() {
+        return txtKakuteiEdTime;
+    }
+
+    /*
+     * settxtKakuteiEdTime
+     * @param txtKakuteiEdTime txtKakuteiEdTime
+     */
+    @JsonProperty("txtKakuteiEdTime")
+    public void setTxtKakuteiEdTime(TextBoxTime txtKakuteiEdTime) {
+        this.txtKakuteiEdTime = txtKakuteiEdTime;
+    }
+
+    // </editor-fold>
+}
