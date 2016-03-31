e7727c779d33b701635fe9c03b77f75ec4bbe824
zhengshukai <zhengshukai@liandisys.com.cn>
2016-03-31 19:18:26 +0800

    iBug 14D203D-7484 対応

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/commonchilddiv/ShobyoIken/ShobyoIken/dgGenyin_Row.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/commonchilddiv/ShobyoIken/ShobyoIken/dgGenyin_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbe.divcontroller.entity.commonchilddiv.ShobyoIke
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Tue Mar 29 10:25:43 CST 2016 
+ * Thu Mar 31 15:02:23 CST 2016 
  */
 
 
