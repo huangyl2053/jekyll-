b5776e04b15b21bb18b1a71c52f991ef08f191cd
wangjie2 <wangjie2@liandisys.com.cn>
2016-03-31 19:35:31 +0800

    受給資格証明書の仕様変更

--- DB/DBU/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbu/entity/jukyushikakushomeisho/JukyuShikakuShomeishoDataEntity.java
+++ DB/DBU/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbu/entity/jukyushikakushomeisho/JukyuShikakuShomeishoDataEntity.java
@@ -8,6 +8,7 @@ package jp.co.ndensan.reams.db.dbu.entity.jukyushikakushomeisho;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
 import jp.co.ndensan.reams.uz.uza.biz.AtenaKanaMeisho;
 import jp.co.ndensan.reams.uz.uza.biz.AtenaMeisho;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
@@ -22,18 +23,24 @@ public class JukyuShikakuShomeishoDataEntity {
     private HihokenshaNo 被保険者番号;
     private AtenaKanaMeisho 被保険者フリガナ;
     private AtenaMeisho 被保険者氏名;
+    private RString 年号_明治;
+    private RString 年号_大正;
+    private RString 年号_昭和;
     private RString 被保険者生年月日;
     private RString 被保険者性別;
     private RString 被保険者住所_転出前;
     private RString 被保険者住所_転出先予定;
     private RString 被保険者異動予定日;
     private RString 保険者番号;
-    private RString 日付;
-    private RString 役職名;
-    private RString 認職者氏名;
-    private RString 電子公印_イメージファイル;
+    private RString 電子公印;
+    private RDate 発行日;
+    private RString 認証者役職名;
+    private RString 認証者役職名1;
+    private RString 公印文字列;
+    private RString 認証者氏名掛けない;
+    private RString 認証者氏名掛ける;
+    private RString 認証者役職名2;
     private RString 公印省略;
-    private RString 市町村名;
     private RString 申請状況;
     private RString 申請年月日;
     private RString 要介護状態区分;
