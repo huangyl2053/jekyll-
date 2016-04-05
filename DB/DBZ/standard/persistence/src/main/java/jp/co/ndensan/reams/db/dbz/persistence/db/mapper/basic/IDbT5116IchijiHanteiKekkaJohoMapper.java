8bf3995fe981d0e8a518c1b079cd6f97bdbf4364
梁百川 <pangsw@liandisys.com.cn>
2016-04-05 17:53:07 +0800

    myBatisを移動

--- DB/DBZ/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbz/persistence/db/mapper/basic/IDbT5116IchijiHanteiKekkaJohoMapper.java
+++ DB/DBZ/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbz/persistence/db/mapper/basic/IDbT5116IchijiHanteiKekkaJohoMapper.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbe.persistence.db.mapper.basic;
+package jp.co.ndensan.reams.db.dbz.persistence.db.mapper.basic;
 
 import jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT5116IchijiHanteiKekkaJohoEntity;
 
