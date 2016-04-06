d3ee3f826fa0199b02a3df20878e7535f4f1cbf1
xupeng <xupeng@liandisys.com.cn>
2016-04-06 18:01:06 +0800

    DBC-1030-060 画面設計_DBCMN53001_償還払い費支給申請決定_サービス提供証明書(緊急時施設療養費） 横展開対応:作成したソースに機能IDのコメントを追加する

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0820027/dgdKinkyujiShiseturyoyo_Row.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0820027/dgdKinkyujiShiseturyoyo_Row.java
@@ -3,23 +3,19 @@ package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0820027;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Mon Mar 28 14:30:25 CST 2016 
+ * Mon Mar 28 14:30:25 CST 2016
  */
 
-
-
-import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
 import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxDate;
 import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxNum;
 
-
 /**
- * dgdKinkyujiShiseturyoyo_Row のクラスファイル 
- * 
- * @author 自動生成
+ * dgdKinkyujiShiseturyoyo_Row のクラスファイル
+ *
+ * @reamsid_L DBC-1030-060 xupeng
  */
-
 public class dgdKinkyujiShiseturyoyo_Row extends DataRow {
     // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-01-15_09-59-03">
 
