12a48427eae8d0750629039c3d5b51508232f1aa
xupeng <xupeng@liandisys.com.cn>
2016-04-07 13:33:05 +0800

    DBC-1010-170 画面設計_DBCMN11006_償還払い状況照会_申請情報照会 onLoad修正

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/dbc0810012/ShinseiDetail.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/dbc0810012/ShinseiDetail.java
@@ -10,10 +10,9 @@ import jp.co.ndensan.reams.db.dbc.business.core.basic.ShokanShinsei;
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0810012.ShinseiDetailDiv;
 import jp.co.ndensan.reams.db.dbc.divcontroller.handler.dbc0810012.ShinseiDetailHandler;
 import jp.co.ndensan.reams.db.dbc.divcontroller.viewbox.ViewStateKeys;
-import jp.co.ndensan.reams.db.dbc.divcontroller.viewbox.dbc0810014.ServiceTeiKyoShomeishoParameter;
 import jp.co.ndensan.reams.db.dbc.service.core.shokanbaraijyokyoshokai.ShokanbaraiJyokyoShokai;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
-import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.JigyoshaNo;
+import jp.co.ndensan.reams.db.dbz.service.TaishoshaKey;
 import jp.co.ndensan.reams.ur.urz.definition.message.UrErrorMessages;
 import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
@@ -37,27 +36,19 @@ public class ShinseiDetail {
      * @return response
      */
     public ResponseData<ShinseiDetailDiv> onLoad(ShinseiDetailDiv div) {
-        ServiceTeiKyoShomeishoParameter parmeter = new ServiceTeiKyoShomeishoParameter(
-                new HihokenshaNo("000000003"), new FlexibleYearMonth(new RString("201601")),
-                new RString("0000000003"), new JigyoshaNo("0000000003"), new RString("事業者名"),
-                new RString("0003"), new RString("証明書"));
-        ViewStateHolder.put(ViewStateKeys.基本情報パラメータ, parmeter);
-
-        ServiceTeiKyoShomeishoParameter parameter = ViewStateHolder.get(
-                ViewStateKeys.基本情報パラメータ, ServiceTeiKyoShomeishoParameter.class);
-        FlexibleYearMonth サービス年月 = parameter.getServiceTeikyoYM();
-        HihokenshaNo 被保険者番号 = parameter.getHiHokenshaNo();
-        RString 整理番号 = parameter.getSeiriNp();
-
-        ViewStateHolder.put(ViewStateKeys.決定日, new RDate("20151224"));
-        RDate 決定日 = ViewStateHolder.get(ViewStateKeys.決定日, RDate.class);
+        TaishoshaKey 引継ぎデータ = ViewStateHolder.get(ViewStateKeys.資格対象者, TaishoshaKey.class);
+        FlexibleYearMonth サービス年月 = new FlexibleYearMonth((new RDate(
+                ViewStateHolder.get(ViewStateKeys.償還払申請一覧_サービス年月, RString.class).
+                toString())).getYearMonth().toDateString());
+        HihokenshaNo 被保険者番号 = 引継ぎデータ.get被保険者番号();
+        RString 整理番号 = ViewStateHolder.get(
+                ViewStateKeys.償還払申請一覧_整理番号, RString.class);
+        RString 決定日 = ViewStateHolder.get(ViewStateKeys.償還払申請一覧_決定日, RString.class);
         if (決定日 == null) {
             div.getPanelHead().getBtnShokanBaraiKeteiInfo().setDisabled(true);
         }
 
-        ViewStateHolder.put(ViewStateKeys.識別コード, new ShikibetsuCode(new RString("000000000000010")));
-        ShikibetsuCode 識別コード = ViewStateHolder.get(
-                ViewStateKeys.識別コード, ShikibetsuCode.class);
+        ShikibetsuCode 識別コード = 引継ぎデータ.get識別コード();
         div.getPanelUp().getCcdKaigoAtenaInfo().onLoad(識別コード);
         if (被保険者番号 != null && !被保険者番号.isEmpty()) {
             div.getPanelUp().getCcdKaigoShikakuKihon().onLoad(被保険者番号);
