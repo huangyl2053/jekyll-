efa762268b091ed329c2085e09d208da255dcebe
楊謹 <n8345@ndensan.com>
2016-04-07 14:32:13 +0900

    DBEの画面サイズを修正します。

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/commonchilddiv/IchijiHanteiKekkaJoho/IchijiHanteiKekkaJoho/IchijiHanteiKekkaJohoDiv.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/commonchilddiv/IchijiHanteiKekkaJoho/IchijiHanteiKekkaJoho/IchijiHanteiKekkaJohoDiv.java
@@ -26,7 +26,7 @@ import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxNum;
  * @author 自動生成
  */
 public class IchijiHanteiKekkaJohoDiv extends Panel implements IIchijiHanteiKekkaJohoDiv {
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-01-15_09-59-03">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-02-19_14-33-39">
     /*
      * [ private の作成 ]
      * クライアント側から取得した情報を元にを検索を行い
