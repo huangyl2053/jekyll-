483508c38bd32da8eecd7bdfd932e73952987ff5
姜岐 <n8344@ndensan.co.jp>
2016-04-07 20:53:22 +0900

    DBZ.YokaigoNinteiShinsakaiIchiranListDiv   横幅のサイズ変更

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/YokaigoNinteiShinsakaiIchiranList/YokaigoNinteiShinsakaiIchiranList/dgShinsakaiIchiran_Row.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/YokaigoNinteiShinsakaiIchiranList/YokaigoNinteiShinsakaiIchiranList/dgShinsakaiIchiran_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.YokaigoNi
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Thu Mar 03 13:19:31 CST 2016 
+ * Thu Apr 07 20:07:53 JST 2016 
  */
 
 
@@ -22,7 +22,7 @@ import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxNum;
  */
 
 public class dgShinsakaiIchiran_Row extends DataRow {
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-01-15_09-59-03">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-02-19_14-33-39">
 
     private TextBoxFlexibleDate kaisaiYoteiDate;
     private TextBoxTime yoteiKaishiTime;
