335492b0cc77ce3e6e03f57824d31013a85edbb2
sunhadi <sunhaidi@liandisys.com.cn>
2016-04-09 10:45:36 +0800

    パッケージ名修正(kaigoninteishinsakaiiinshozokukikanjoho⇒kaigoninteishinsakaiiinshozokukikan)

--- DB/DBE/standard/service/src/main/java/jp/co/ndensan/reams/db/dbe/service/core/shinsakaiiinjoho/kaigoninteishinsakaiiinshozokukikan/KaigoNinteiShinsakaiIinShozokuKikanJohoManager.java
+++ DB/DBE/standard/service/src/main/java/jp/co/ndensan/reams/db/dbe/service/core/shinsakaiiinjoho/kaigoninteishinsakaiiinshozokukikan/KaigoNinteiShinsakaiIinShozokuKikanJohoManager.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbe.service.core.shinsakaiiinjoho.kaigoninteishinsakaiiinshozokukikanjoho;
+package jp.co.ndensan.reams.db.dbe.service.core.shinsakaiiinjoho.kaigoninteishinsakaiiinshozokukikan;
 
 import static java.util.Objects.requireNonNull;
 import jp.co.ndensan.reams.db.dbe.business.core.shinsakaiiinjoho.ninteishinsakaiiinshozokukikanjoho.KaigoNinteiShinsakaiIinShozokuKikanJoho;
