c1430718bedbc3d04d7bd0570342ec47ca6c89d3
wangkanglei <wangkanglei@liandisys.com.cn>
2016-04-11 10:42:19 +0800

    画面設計_DBCMN11006_償還払い状況照会_特定診療費(ソースのコメントに機能IDを追加する)

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0810023/TokuteiShinryohiDiv.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0810023/TokuteiShinryohiDiv.java
@@ -4,15 +4,17 @@ package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0810023;
  * このファイルへの変更は、再生成時には損失するため
  * 不正な動作の原因になります。
  */
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
 
 /**
- * TokuteiShinryohi のクラスファイル 
- * 
- * @author 自動生成
+ * TokuteiShinryohi のクラスファイル
+ *
+ * @reamsid_L DBC-1010-150 wangkanglei
  */
 public class TokuteiShinryohiDiv extends Panel {
+
     // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2015-11-16_12-38-19">
     /*
      * [ private の作成 ]
