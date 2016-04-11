38e31d0898486b6a446a39032d6c7888010316b6
橋本　治義 <n8322@ndensan.co.jp>
2016-04-11 22:04:57 +0900

    DBB 画面修正　／共有子DIV追加（gemmenshurui、gemmentorikeshishurui）

---
+++ DB/DBB/standard/divcontroller/src/main/webapp/WEB-INF/standard/CommonChildDiv/gemmenshurui/GemmenShurui_Design.ts
@@ -0,0 +1,49 @@
+/// <reference path="../../d.ts/jquery.d.ts" />
+/// <reference path="../../d.ts/UzViewControls.d.ts" />
+/// <reference path="../../d.ts/UzaConverter.d.ts" />
+
+module DBB {
+
+     export module GemmenShurui {
+
+        export class Events {
+
+        }
+
+        export class Controls {
+            private _myName: string;
+
+            public static myType(): string {
+                return "GemmenShurui";
+            }
+
+            constructor(fieldName: string) {
+                this._myName = fieldName;
+            }
+
+            public convFiledNameSelf(): string {
+                return this._myName + "_" + DBB.GemmenShurui.Controls.myType();
+            }
+
+            public convFiledName(fieldName: string): string {
+                return this._myName + "_" + DBB.GemmenShurui.Controls.myType() + "_" + fieldName;
+            }
+
+            public GemmenShurui(): UZA.Panel {
+                return new UZA.Panel(this.convFiledNameSelf());
+            }
+
+            public dgGemmenShurui(): UZA.DataGrid {
+                return new UZA.DataGrid(this.convFiledName("dgGemmenShurui"));
+            }
+
+            public btnTorikeshi(): UZA.Button {
+                return new UZA.Button(this.convFiledName("btnTorikeshi"));
+            }
+
+        }
+
+     }
+
+}
+
---
+++ DB/DBB/standard/divcontroller/src/main/webapp/WEB-INF/standard/CommonChildDiv/gemmentorikeshishurui/GemmenTorikeshiShurui_Design.js
@@ -0,0 +1,43 @@
+var DBB;
+(function (DBB) {
+    (function (GemmenTorikeshiShurui) {
+        var Events = (function () {
+            function Events() {
+            }
+            return Events;
+        })();
+        GemmenTorikeshiShurui.Events = Events;
+
+        var Controls = (function () {
+            function Controls(fieldName) {
+                this._myName = fieldName;
+            }
+            Controls.myType = function () {
+                return "GemmenTorikeshiShurui";
+            };
+
+            Controls.prototype.convFiledNameSelf = function () {
+                return this._myName + "_" + DBB.GemmenTorikeshiShurui.Controls.myType();
+            };
+
+            Controls.prototype.convFiledName = function (fieldName) {
+                return this._myName + "_" + DBB.GemmenTorikeshiShurui.Controls.myType() + "_" + fieldName;
+            };
+
+            Controls.prototype.GemmenTorikeshiShurui = function () {
+                return new UZA.Panel(this.convFiledNameSelf());
+            };
+
+            Controls.prototype.dgGemmenTorikeshiShurui = function () {
+                return new UZA.DataGrid(this.convFiledName("dgGemmenTorikeshiShurui"));
+            };
+
+            Controls.prototype.btnTorikeshi = function () {
+                return new UZA.Button(this.convFiledName("btnTorikeshi"));
+            };
+            return Controls;
+        })();
+        GemmenTorikeshiShurui.Controls = Controls;
+    })(DBB.GemmenTorikeshiShurui || (DBB.GemmenTorikeshiShurui = {}));
+    var GemmenTorikeshiShurui = DBB.GemmenTorikeshiShurui;
+})(DBB || (DBB = {}));
