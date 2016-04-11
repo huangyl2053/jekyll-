2492ceb832c841e36bd23c62ac35507a92e4a1b8
wangkun <wangkun@liandisys.com.cn>
2016-04-11 11:45:10 +0800

    ソースを修正しました。

--- DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/parentdiv/DBA1010011/ShikakuShutokuJohoDiv.java
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/parentdiv/DBA1010011/ShikakuShutokuJohoDiv.java
@@ -7,6 +7,8 @@ package jp.co.ndensan.reams.db.dba.divcontroller.entity.parentdiv.DBA1010011;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.IryohokenRirekiCommonChildDiv.IIryohokenRirekiCommonChildDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.RoreiFukushiNenkinShokai.IRoreiFukushiNenkinShokaiDiv;
 import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.ShisetsuNyutaishoRirekiKanri.IShisetsuNyutaishoRirekiKanriDiv;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
@@ -18,7 +20,7 @@ import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
  */
 public class ShikakuShutokuJohoDiv extends Panel {
 
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-01-15_09-59-03">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-03-22_14-06-37">
     /*
      * [ private の作成 ]
      * クライアント側から取得した情報を元にを検索を行い
@@ -196,6 +198,11 @@ public class ShikakuShutokuJohoDiv extends Panel {
     }
 
     @JsonIgnore
+    public IIryohokenRirekiCommonChildDiv getCcdIryoHokenRireki() {
+        return this.getTabInputs().getTplIryoHoken().getIryoHokenRirekiMain().getCcdIryoHokenRireki();
+    }
+
+    @JsonIgnore
     public tplRofukuNenkinDiv getTplRofukuNenkin() {
         return this.getTabInputs().getTplRofukuNenkin();
     }
@@ -213,6 +220,11 @@ public class ShikakuShutokuJohoDiv extends Panel {
     @JsonIgnore
     public void setRohukuNenkin(RohukuNenkinDiv RohukuNenkin) {
         this.getTabInputs().getTplRofukuNenkin().setRohukuNenkin(RohukuNenkin);
+    }
+
+    @JsonIgnore
+    public IRoreiFukushiNenkinShokaiDiv getCcdRohukuNenkin() {
+        return this.getTabInputs().getTplRofukuNenkin().getRohukuNenkin().getCcdRohukuNenkin();
     }
 
     @JsonIgnore
