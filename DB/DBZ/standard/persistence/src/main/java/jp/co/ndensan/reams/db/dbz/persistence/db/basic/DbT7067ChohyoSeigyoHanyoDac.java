0121cab19c225b37ac98b72a6be5158ae65b7f9c
wangchao <wangchao@liandisys.com.cn>
2016-04-12 21:12:08 +0800

    画面開発でramlファイルを再修正

--- DB/DBZ/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbz/persistence/db/basic/DbT7067ChohyoSeigyoHanyoDac.java
+++ DB/DBZ/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbz/persistence/db/basic/DbT7067ChohyoSeigyoHanyoDac.java
@@ -153,4 +153,29 @@ public class DbT7067ChohyoSeigyoHanyoDac implements ISaveable<DbT7067ChohyoSeigy
                                 eq(komokuName, 項目名))).
                 toList(DbT7067ChohyoSeigyoHanyoEntity.class);
     }
+
+    /**
+     * 帳票制御汎用をキーから取得します。
+     *
+     * @param サブ業務コード SubGyomuCode
+     * @param 帳票分類ID ChohyoBunruiID
+     * @param 管理年度 FlexibleYear
+     * @return List<DbT7067ChohyoSeigyoHanyoEntity>
+     */
+    @Transaction
+    public List<DbT7067ChohyoSeigyoHanyoEntity> get帳票制御汎用(
+            SubGyomuCode サブ業務コード,
+            ReportId 帳票分類ID,
+            FlexibleYear 管理年度) {
+
+        DbAccessorNormalType accessor = new DbAccessorNormalType(session);
+
+        return accessor.select().
+                table(DbT7067ChohyoSeigyoHanyo.class).
+                where(and(
+                                eq(subGyomuCode, サブ業務コード),
+                                eq(chohyoBunruiID, 帳票分類ID),
+                                eq(kanriNendo, 管理年度))).
+                toList(DbT7067ChohyoSeigyoHanyoEntity.class);
+    }
 }
