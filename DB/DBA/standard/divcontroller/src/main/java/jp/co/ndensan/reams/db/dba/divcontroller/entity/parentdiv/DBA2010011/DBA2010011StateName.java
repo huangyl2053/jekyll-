e8a30ed595a00e541af220d329c9fac66756a6dc
wanghui <wanghui@liandisys.com.cn>
2016-04-13 10:42:25 +0800

    DBA-0340-030 画面設計_DBAMN41002_介護事業者・施設管理 Review修正

--- DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/parentdiv/DBA2010011/DBA2010011StateName.java
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/parentdiv/DBA2010011/DBA2010011StateName.java
@@ -1,7 +1,7 @@
 package jp.co.ndensan.reams.db.dba.divcontroller.entity.parentdiv.DBA2010011;
 
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
 
 /**
  * このコードはツールによって生成されました。</br>
@@ -9,7 +9,7 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
  * 不正な動作の原因になります。</br>
  * DBA2010011の状態名定義クラスです
  *
- * @reamsid_L DBA-0340-030 wanghui
+ * @author 自動生成
  */
 public enum DBA2010011StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
