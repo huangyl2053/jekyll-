bee8d3c22889643347af2b99ff9a244d62d28b72
hezhenzhen <hezhenzhen@liandisys.com.cn>
2016-04-15 16:57:53 +0800

    医療保険履歴の修正しました。

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/IryohokenRirekiCommonChildDiv/IryohokenRirekiHandler.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/IryohokenRirekiCommonChildDiv/IryohokenRirekiHandler.java
@@ -97,7 +97,7 @@ public class IryohokenRirekiHandler {
             div.getPnlIryohokenJoho().getTxtKigoBango().setReadOnly(true);
             div.getDgIryohokenIchiran().getGridSetting().setIsShowDeleteButtonColumn(false);
             div.getDgIryohokenIchiran().getGridSetting().setIsShowModifyButtonColumn(false);
-            div.getDgIryohokenIchiran().getGridSetting().setIsShowRowState(true);
+            div.getDgIryohokenIchiran().getGridSetting().getColumn(new RString("defaultDataName2")).setVisible(false);
             div.getPlIryohokenRireki().getBtnCancel().setVisible(false);
             div.getPlIryohokenRireki().getBtnIryohokenKakute().setVisible(false);
             div.getPlIryohokenRireki().getBtnIryohokenTsuika().setVisible(false);
