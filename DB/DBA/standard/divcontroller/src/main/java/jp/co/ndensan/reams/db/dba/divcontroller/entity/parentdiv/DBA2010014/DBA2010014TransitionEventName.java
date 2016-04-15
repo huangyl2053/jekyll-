edd60d115d362c28eea38216f5cc2a63f79a7726
dongyabin <dongyabin@liandisys.com.cn>
2016-04-15 16:54:41 +0800

    画面設計_DBA2010014_サービス登録　修正

--- DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/parentdiv/DBA2010014/DBA2010014TransitionEventName.java
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/parentdiv/DBA2010014/DBA2010014TransitionEventName.java
@@ -1,7 +1,7 @@
 package jp.co.ndensan.reams.db.dba.divcontroller.entity.parentdiv.DBA2010014;
 
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 
 /**
  * このコードはツールによって生成されました。</br>
@@ -9,7 +9,7 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
  * 不正な動作の原因になります。</br>
  * DBA2010014のイベント名定義クラスです</br>
  *
- * @reamsid_L DBA-0340-060 dongyabin
+ * @author 自動生成
  */
 public enum DBA2010014TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
