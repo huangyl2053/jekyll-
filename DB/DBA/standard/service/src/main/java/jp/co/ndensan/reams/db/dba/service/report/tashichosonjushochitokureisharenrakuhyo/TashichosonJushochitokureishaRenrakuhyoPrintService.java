dbe3809e92e2aa4b414017e065f42ab396dc0d56
duanzhanli <duanzhanli@liandisys.com.cn>
2016-04-19 13:35:50 +0800

    下記の機能仕様変更の対応 帳票設計_DBA100005_介護保険住所地特例施設退所通知書 帳票設計_DBA100006_介護保険住所地特例施設変更通知書 帳票設計_DBA100007_介護保険他市町村住所地特例者連絡票

--- DB/DBA/standard/service/src/main/java/jp/co/ndensan/reams/db/dba/service/report/tashichosonjushochitokureisharenrakuhyo/TashichosonJushochitokureishaRenrakuhyoPrintService.java
+++ DB/DBA/standard/service/src/main/java/jp/co/ndensan/reams/db/dba/service/report/tashichosonjushochitokureisharenrakuhyo/TashichosonJushochitokureishaRenrakuhyoPrintService.java
@@ -11,8 +11,19 @@ import jp.co.ndensan.reams.db.dba.business.report.tashichosonjushochitokureishar
 import jp.co.ndensan.reams.db.dba.business.report.tashichosonjushochitokureisharenrakuhyo.TashichosonJushochitokureishaRenrakuhyoProperty;
 import jp.co.ndensan.reams.db.dba.business.report.tashichosonjushochitokureisharenrakuhyo.TashichosonJushochitokureishaRenrakuhyoReport;
 import jp.co.ndensan.reams.db.dba.entity.report.source.tashichosonjushochitokureisharenrakuhyo.TashichosonJushochitokureishaRenrakuhyoReportSource;
+import jp.co.ndensan.reams.db.dbz.service.util.report.ReportUtil;
+import jp.co.ndensan.reams.ur.urz.entity.report.parts.ninshosha.NinshoshaSource;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
+import jp.co.ndensan.reams.uz.uza.report.IReportProperty;
+import jp.co.ndensan.reams.uz.uza.report.IReportSource;
 import jp.co.ndensan.reams.uz.uza.report.Printer;
+import jp.co.ndensan.reams.uz.uza.report.Report;
+import jp.co.ndensan.reams.uz.uza.report.ReportAssembler;
+import jp.co.ndensan.reams.uz.uza.report.ReportAssemblerBuilder;
+import jp.co.ndensan.reams.uz.uza.report.ReportManager;
+import jp.co.ndensan.reams.uz.uza.report.ReportSourceWriter;
 import jp.co.ndensan.reams.uz.uza.report.SourceDataCollection;
+import jp.co.ndensan.reams.uz.uza.report.source.breaks.BreakAggregator;
 
 /**
  * 介護保険他市町村住所地特例者連絡票Printerです。
@@ -30,6 +41,24 @@ public class TashichosonJushochitokureishaRenrakuhyoPrintService {
      */
     public SourceDataCollection print(TashichosonJushochitokureishaRenrakuhyoItem item) {
         TashichosonJushochitokureishaRenrakuhyoProperty property = new TashichosonJushochitokureishaRenrakuhyoProperty();
+        try (ReportManager reportManager = new ReportManager()) {
+            try (ReportAssembler<TashichosonJushochitokureishaRenrakuhyoReportSource> assembler = createAssembler(property, reportManager)) {
+                ReportSourceWriter<TashichosonJushochitokureishaRenrakuhyoReportSource> reportSourceWriter = new ReportSourceWriter(assembler);
+                NinshoshaSource ninshoshaSource = ReportUtil.get認証者情報(property.subGyomuCode(),
+                        property.reportId(),
+                        FlexibleDate.getNowDate(),
+                        reportSourceWriter);
+                item.setDenshiKoin(ninshoshaSource.denshiKoin);
+                item.setHakkoYMD1(ninshoshaSource.hakkoYMD);
+                item.setNinshoshaYakushokuMei(ninshoshaSource.ninshoshaYakushokuMei);
+                item.setNinshoshaYakushokuMei1(ninshoshaSource.ninshoshaYakushokuMei1);
+                item.setKoinMojiretsu(ninshoshaSource.koinMojiretsu);
+                item.setNinshoshaShimeiKakenai(ninshoshaSource.ninshoshaShimeiKakenai);
+                item.setNinshoshaShimeiKakeru(ninshoshaSource.ninshoshaShimeiKakeru);
+                item.setNinshoshaYakushokuMei2(ninshoshaSource.ninshoshaYakushokuMei2);
+                item.setKoinShoryaku(ninshoshaSource.koinShoryaku);
+            }
+        }
         return new Printer<TashichosonJushochitokureishaRenrakuhyoReportSource>().spool(property, toReports(item));
     }
 
@@ -37,5 +66,16 @@ public class TashichosonJushochitokureishaRenrakuhyoPrintService {
         List<TashichosonJushochitokureishaRenrakuhyoReport> list = new ArrayList<>();
         list.add(TashichosonJushochitokureishaRenrakuhyoReport.createFrom(item));
         return list;
+    }
+
+    private static <T extends IReportSource, R extends Report<T>> ReportAssembler<T> createAssembler(
+            IReportProperty<T> property, ReportManager manager) {
+        ReportAssemblerBuilder builder = manager.reportAssembler(property.reportId().value(), property.subGyomuCode());
+        for (BreakAggregator<? super T, ?> breaker : property.breakers()) {
+            builder.addBreak(breaker);
+        }
+        builder.isHojinNo(property.containsHojinNo());
+        builder.isKojinNo(property.containsKojinNo());
+        return builder.<T>create();
     }
 }
