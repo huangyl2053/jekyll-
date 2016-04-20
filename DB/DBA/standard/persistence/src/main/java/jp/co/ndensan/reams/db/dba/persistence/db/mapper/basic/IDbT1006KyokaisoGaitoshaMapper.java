37b7d82af822d9db4f4d55293f6c8ec5f59724a1
huangh <huangh@liandisys.com>
2016-04-20 09:48:14 +0800

    実装者_機能_境界層業務概念の新規

---
+++ DB/DBA/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dba/persistence/db/mapper/basic/IDbT1006KyokaisoGaitoshaMapper.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dba.persistence.db.mapper.basic;
+
+/**
+ * {@link DbT1006KyokaisoGaitoshaEntity}のマッパーIFです。
+ *
+ * @reamsid_L DBA-9999-013 huangh
+ */
+public interface IDbT1006KyokaisoGaitoshaMapper {
+
+}
