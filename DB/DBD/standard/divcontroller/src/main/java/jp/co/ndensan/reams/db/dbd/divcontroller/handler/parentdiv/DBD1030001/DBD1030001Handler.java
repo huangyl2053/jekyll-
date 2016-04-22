af29dc5a1f3de8450a9af2f62b43d55fe7854526
wangjie2 <wangjie2@liandisys.com.cn>
2016-04-22 09:47:55 +0800

    社会福祉法人等利用者負担軽減申請　「ViewState」を利用 することの訂正

--- DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/handler/parentdiv/DBD1030001/DBD1030001Handler.java
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/handler/parentdiv/DBD1030001/DBD1030001Handler.java
@@ -38,6 +38,7 @@ import jp.co.ndensan.reams.db.dbz.business.core.HihokenshaDaicho;
 import jp.co.ndensan.reams.db.dbz.definition.core.enumeratedtype.JigyoshaKubun;
 import jp.co.ndensan.reams.db.dbz.definition.core.enumeratedtype.ShinseiTodokedeDaikoKubunCode;
 import jp.co.ndensan.reams.db.dbz.definition.message.DbzInformationMessages;
+import jp.co.ndensan.reams.db.dbz.divcontroller.viewbox.ViewStateKeys;
 import jp.co.ndensan.reams.db.dbz.service.core.basic.HihokenshaDaichoManager;
 import jp.co.ndensan.reams.ur.urd.service.core.seikatsuhogo.SeikatsuhogoManagerFactory;
 import jp.co.ndensan.reams.ur.urz.definition.message.UrErrorMessages;
@@ -226,7 +227,7 @@ public class DBD1030001Handler {
     }
 
     private ShikibetsuCode get識別コードFromViewState() {
-        ShikibetsuCode 識別コード = ViewStateHolder.get(DBD1030001ViewStateKey.識別コード, ShikibetsuCode.class);
+        ShikibetsuCode 識別コード = ViewStateHolder.get(ViewStateKeys.識別コード, ShikibetsuCode.class);
         if (null == 識別コード) {
             識別コード = ShikibetsuCode.EMPTY;
         }
@@ -234,7 +235,7 @@ public class DBD1030001Handler {
     }
 
     private HihokenshaNo get被保険者番号FromViewState() {
-        HihokenshaNo 被保険者番号 = ViewStateHolder.get(DBD1030001ViewStateKey.被保険者番号, HihokenshaNo.class);
+        HihokenshaNo 被保険者番号 = ViewStateHolder.get(ViewStateKeys.被保険者番号, HihokenshaNo.class);
         if (null == 被保険者番号) {
             被保険者番号 = HihokenshaNo.EMPTY;
         }
@@ -1573,14 +1574,6 @@ public class DBD1030001Handler {
      */
     public enum DBD1030001ViewStateKey {
 
-        /**
-         * 識別コードです。
-         */
-        識別コード,
-        /**
-         * 被保険者番号です。
-         */
-        被保険者番号,
         /**
          * 申請一覧情報です。
          */
