c3760291166a8db040f89e5210137c0fdf185546
姜岐 <n8344@ndensan.co.jp>
2016-04-22 13:42:41 +0900

    DBE6010001、DBE6020001、DBE6030001、DBE6040001 のUIコンテナを追加する

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE6010001/DBE6010001TransitionEventName.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE6010001/DBE6010001TransitionEventName.java
@@ -1,26 +1,24 @@
-package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE6040001;
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE6010001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 
 /**
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBE6040001の状態名定義クラスです
+ * DBE6010001のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBE6040001StateName implements IStateEnumerations {
+public enum DBE6010001TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    NoChange(""),
-    初期表示("初期表示"),
-    検索結果表示("検索結果表示");
+    完了("完了");
 
     private final RString name;
 
-    private DBE6040001StateName(String name) {
+    private DBE6010001TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
