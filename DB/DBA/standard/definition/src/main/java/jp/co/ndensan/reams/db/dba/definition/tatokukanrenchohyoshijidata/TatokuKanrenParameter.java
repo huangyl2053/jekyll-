59eda616f96b61d6dee6d34d9e157f7ad23edcba
houtianpeng <houtianpeng@liandisys.com.cn>
2016-04-23 17:08:16 +0800

    画面設計_DBAMN12008_他市町村住所地特例者関連帳票発行

--- DB/DBA/standard/definition/src/main/java/jp/co/ndensan/reams/db/dba/definition/tatokukanrenchohyoshijidata/TatokuKanrenParameter.java
+++ DB/DBA/standard/definition/src/main/java/jp/co/ndensan/reams/db/dba/definition/tatokukanrenchohyoshijidata/TatokuKanrenParameter.java
@@ -13,28 +13,29 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * 被保険者証一括発行のバッチ処理クラスのパラメータ
+ * 他市町村住所地特例者関連帳票クラスのパラメータ
  *
  * @reamsid_L DBA-0380-020 houtianpeng
  */
 @SuppressWarnings("PMD.UnusedPrivateField")
 @Getter
 @Setter
-public final class TatokuKanrenParameter {
+public class TatokuKanrenParameter {
 
-    private final YubinNo 郵便番号;
-    private final RString 文書番号;
-    private final RString 住所;
-    private final FlexibleDate 発行日;
-    private final RString 役所名;
-    private final RString 役所名敬称;
-    private final RString 担当課名;
-    private final RString 担当部署名敬称;
-    private final ShikibetsuCode 識別コード;
-    private final RString 被保険者番号;
-    private final FlexibleDate 他市町村住所地特例異動日;
-    private final RString 他市町村住所地特例枝番;
-    private final FlexibleDate 入所日;
+    private YubinNo 郵便番号;
+    private RString 文書番号;
+    private RString 住所;
+    private FlexibleDate 発行日;
+    private RString 役所名;
+    private RString 役所名敬称;
+    private RString 担当課名;
+    private RString 担当課名敬称;
+    private ShikibetsuCode 識別コード;
+    private RString 措置被保険者番号;
+    private FlexibleDate 異動日;
+    private RString 枝番;
+    private FlexibleDate 入所年月日;
+    private boolean 住所出力不要フラグ;
 
     /**
      * コンストラクタ
@@ -46,14 +47,15 @@ public final class TatokuKanrenParameter {
      * @param 役所名 役所名
      * @param 役所名敬称 役所名敬称
      * @param 担当課名 担当課名
-     * @param 担当部署名敬称 担当部署名敬称
+     * @param 担当課名敬称 担当課名敬称
      * @param 識別コード 識別コード
-     * @param 被保険者番号 被保険者番号
-     * @param 他市町村住所地特例異動日 他市町村住所地特例異動日
-     * @param 他市町村住所地特例枝番 他市町村住所地特例枝番
-     * @param 入所日 入所日
+     * @param 措置被保険者番号 措置被保険者番号
+     * @param 異動日 異動日
+     * @param 枝番 枝番
+     * @param 入所年月日 入所年月日
+     * @param 住所出力不要フラグ 住所出力不要フラグ
      */
-    private TatokuKanrenParameter(
+    public TatokuKanrenParameter(
             YubinNo 郵便番号,
             RString 文書番号,
             RString 住所,
@@ -61,12 +63,13 @@ public final class TatokuKanrenParameter {
             RString 役所名,
             RString 役所名敬称,
             RString 担当課名,
-            RString 担当部署名敬称,
+            RString 担当課名敬称,
             ShikibetsuCode 識別コード,
-            RString 被保険者番号,
-            FlexibleDate 他市町村住所地特例異動日,
-            RString 他市町村住所地特例枝番,
-            FlexibleDate 入所日
+            RString 措置被保険者番号,
+            FlexibleDate 異動日,
+            RString 枝番,
+            FlexibleDate 入所年月日,
+            boolean 住所出力不要フラグ
     ) {
         this.郵便番号 = 郵便番号;
         this.文書番号 = 文書番号;
@@ -75,12 +78,13 @@ public final class TatokuKanrenParameter {
         this.役所名 = 役所名;
         this.役所名敬称 = 役所名敬称;
         this.担当課名 = 担当課名;
-        this.担当部署名敬称 = 担当部署名敬称;
+        this.担当課名敬称 = 担当課名敬称;
         this.識別コード = 識別コード;
-        this.被保険者番号 = 被保険者番号;
-        this.他市町村住所地特例異動日 = 他市町村住所地特例異動日;
-        this.他市町村住所地特例枝番 = 他市町村住所地特例枝番;
-        this.入所日 = 入所日;
+        this.措置被保険者番号 = 措置被保険者番号;
+        this.異動日 = 異動日;
+        this.枝番 = 枝番;
+        this.入所年月日 = 入所年月日;
+        this.住所出力不要フラグ = 住所出力不要フラグ;
     }
 
     /**
@@ -92,12 +96,13 @@ public final class TatokuKanrenParameter {
      * @param 役所名 役所名
      * @param 役所名敬称 役所名敬称
      * @param 担当課名 担当課名
-     * @param 担当部署名敬称 担当部署名敬称
+     * @param 担当課名敬称 担当課名敬称
      * @param 識別コード 識別コード
-     * @param 被保険者番号 被保険者番号
-     * @param 他市町村住所地特例異動日 他市町村住所地特例異動日
-     * @param 他市町村住所地特例枝番 他市町村住所地特例枝番
-     * @param 入所日 入所日
+     * @param 措置被保険者番号 措置被保険者番号
+     * @param 異動日 異動日
+     * @param 枝番 枝番
+     * @param 入所年月日 入所年月日
+     * @param 住所出力不要フラグ 住所出力不要フラグ
      * @return createParam_TatokuKanren
      */
     public static TatokuKanrenParameter createParam_TatokuKanren(
@@ -108,12 +113,14 @@ public final class TatokuKanrenParameter {
             RString 役所名,
             RString 役所名敬称,
             RString 担当課名,
-            RString 担当部署名敬称,
+            RString 担当課名敬称,
             ShikibetsuCode 識別コード,
-            RString 被保険者番号,
-            FlexibleDate 他市町村住所地特例異動日,
-            RString 他市町村住所地特例枝番,
-            FlexibleDate 入所日) {
+            RString 措置被保険者番号,
+            FlexibleDate 異動日,
+            RString 枝番,
+            FlexibleDate 入所年月日,
+            boolean 住所出力不要フラグ
+    ) {
         return new TatokuKanrenParameter(
                 郵便番号,
                 文書番号,
@@ -122,11 +129,13 @@ public final class TatokuKanrenParameter {
                 役所名,
                 役所名敬称,
                 担当課名,
-                担当部署名敬称,
+                担当課名敬称,
                 識別コード,
-                被保険者番号,
-                他市町村住所地特例異動日,
-                他市町村住所地特例枝番,
-                入所日);
+                措置被保険者番号,
+                異動日,
+                枝番,
+                入所年月日,
+                住所出力不要フラグ
+        );
     }
 }
