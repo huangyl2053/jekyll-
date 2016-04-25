8cf1e676d2b7e4c5bf20f86089b1fa779850fb0c
zuotao <zuotao@liandisys.com.cn>
2016-04-25 10:00:22 +0800

    画面設計_DBAMN11002_被保険者照会  仕様変更対応 V1.0.6「関連ほかのソースも修正しました」

--- DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/ikenshosakuseiiraiichiranhyo/IkenshoSakuseiIraiIchiranhyoBodyEditor.java
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/ikenshosakuseiiraiichiranhyo/IkenshoSakuseiIraiIchiranhyoBodyEditor.java
@@ -15,7 +15,7 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
  */
 public class IkenshoSakuseiIraiIchiranhyoBodyEditor implements IkenshoSakuseiIraiIchiranhyoEditor {
 
-    private final IkenshoSakuseiIraiIchiranhyoBodyItem item;
+    private final IkenshoSakuseiIraiIchiranhyoItem item;
     private final int index;
 
     /**
@@ -24,7 +24,7 @@ public class IkenshoSakuseiIraiIchiranhyoBodyEditor implements IkenshoSakuseiIra
      * @param item {@link IkenshoSakuseiIraiIchiranhyoHeadEditor}
      * @param index int
      */
-    protected IkenshoSakuseiIraiIchiranhyoBodyEditor(IkenshoSakuseiIraiIchiranhyoBodyItem item, int index) {
+    protected IkenshoSakuseiIraiIchiranhyoBodyEditor(IkenshoSakuseiIraiIchiranhyoItem item, int index) {
         this.item = item;
         this.index = index;
     }
@@ -35,6 +35,22 @@ public class IkenshoSakuseiIraiIchiranhyoBodyEditor implements IkenshoSakuseiIra
     }
 
     private IkenshoSakuseiIraiIchiranhyoReportSource editSource(IkenshoSakuseiIraiIchiranhyoReportSource source) {
+        source.hakkoYMD = item.getHakkoYMD();
+        source.denshiKoin = item.getDenshiKoin();
+        source.ninshoshaYakushokuMei1 = item.getNinshoshaYakushokuMei1();
+        source.ninshoshaYakushokuMei = item.getNinshoshaYakushokuMei();
+        source.ninshoshaYakushokuMei2 = item.getNinshoshaYakushokuMei2();
+        source.ninshoshaShimeiKakeru = item.getNinshoshaShimeiKakeru();
+        source.ninshoshaShimeiKakenai = item.getNinshoshaShimeiKakenai();
+        source.koinMojiretsu = item.getKoinMojiretsu();
+        source.koinShoryaku = item.getKoinShoryaku();
+        source.yubinNo1 = item.getYubinNo1();
+        source.jushoText = item.getJushoText();
+        source.kikanNameText = item.getKikanNameText();
+        source.shimeiText = item.getShimeiText();
+        source.meishoFuyo = item.getMeishoFuyo();
+        source.printTimeStamp = item.getPrintTimeStamp();
+        source.tsuchibun1 = item.getTsuchibun1();
         source.listIchiranhyo_1 = new RString(String.valueOf(index));
         source.listIchiranhyo_2 = item.getListIchiranhyo_2();
         source.listIchiranhyo_3 = item.getListIchiranhyo_3();
