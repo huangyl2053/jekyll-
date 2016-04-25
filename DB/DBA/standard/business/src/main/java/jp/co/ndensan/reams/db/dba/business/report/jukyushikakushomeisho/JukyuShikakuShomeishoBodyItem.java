f4a812f27e2270b79bec92678b88d5b9d5a8a052
suguangjun <suguangjun@liandisys.com.cn>
2016-04-25 16:02:45 +0800

    帳票設計_DBA100004_受給資格証明書 仕様変更対応（1.04バージョン） 画面設計_DBAMN81001_被保険者番号付番方法 レビュー対応

--- DB/DBA/standard/business/src/main/java/jp/co/ndensan/reams/db/dba/business/report/jukyushikakushomeisho/JukyuShikakuShomeishoBodyItem.java
+++ DB/DBA/standard/business/src/main/java/jp/co/ndensan/reams/db/dba/business/report/jukyushikakushomeisho/JukyuShikakuShomeishoBodyItem.java
@@ -10,8 +10,8 @@ import lombok.Getter;
 
 /**
  * 受給資格証明書情報です。
- * 
- * @reamsid_L DBU-0490-090  suguangjun 
+ *
+ * @reamsid_L DBU-0490-090 suguangjun
  */
 @Getter
 @SuppressWarnings("PMD.UnusedPrivateField")
@@ -29,8 +29,8 @@ public class JukyuShikakuShomeishoBodyItem {
     private final RString tenshutsusakiYoteiJusho;
     private final RString idoYoteiYMD;
     private final RString hokenshaNo;
-    private final RString shomeiHakkoYMD;
-    private final RString shuchoMei;
+    private final RString hakkoYMD;
+    private final RString ninshoshaYakushokuMei;
     private final RString recognizedName;
     private final RString denshiKoin;
     private final RString koinShoryaku;
@@ -44,6 +44,11 @@ public class JukyuShikakuShomeishoBodyItem {
     private final RString shinsakaiIken;
     private final RString biko;
     private final RString remban;
+    private final RString koinMojiretsu;
+    private final RString ninshoshaShimeiKakenai;
+    private final RString ninshoshaShimeiKakeru;
+    private final RString ninshoshaYakushokuMei1;
+    private final RString ninshoshaYakushokuMei2;
 
     /**
      * インスタンスを生成します。
@@ -60,8 +65,8 @@ public class JukyuShikakuShomeishoBodyItem {
      * @param tenshutsusakiYoteiJusho 被保険者住所（転出先予定）
      * @param idoYoteiYMD 被保険者異動予定日
      * @param hokenshaNo 保険者番号
-     * @param shomeiHakkoYMD 発行日
-     * @param shuchoMei 役職名
+     * @param hakkoYMD 発行日
+     * @param ninshoshaYakushokuMei 役職名
      * @param recognizedName 認職者氏名
      * @param denshiKoin 電子公印
      * @param koinShoryaku 公印省略
@@ -75,6 +80,11 @@ public class JukyuShikakuShomeishoBodyItem {
      * @param shinsakaiIken 認定審査会の意見等
      * @param biko 備考
      * @param remban 連番
+     * @param koinMojiretsu 公印文字列
+     * @param ninshoshaShimeiKakenai 認証者氏名(公印に掛けない)
+     * @param ninshoshaShimeiKakeru 認証者氏名(公印に掛ける)
+     * @param ninshoshaYakushokuMei1 認証者役職名1
+     * @param ninshoshaYakushokuMei2 認証者役職名2
      */
     public JukyuShikakuShomeishoBodyItem(RString hihokenshaNo,
             RString hihokenshaNameKana,
@@ -88,8 +98,8 @@ public class JukyuShikakuShomeishoBodyItem {
             RString tenshutsusakiYoteiJusho,
             RString idoYoteiYMD,
             RString hokenshaNo,
-            RString shomeiHakkoYMD,
-            RString shuchoMei,
+            RString hakkoYMD,
+            RString ninshoshaYakushokuMei,
             RString recognizedName,
             RString denshiKoin,
             RString koinShoryaku,
@@ -102,7 +112,12 @@ public class JukyuShikakuShomeishoBodyItem {
             RString yukoShuryoYMD,
             RString shinsakaiIken,
             RString biko,
-            RString remban
+            RString remban,
+            RString koinMojiretsu,
+            RString ninshoshaShimeiKakenai,
+            RString ninshoshaShimeiKakeru,
+            RString ninshoshaYakushokuMei1,
+            RString ninshoshaYakushokuMei2
     ) {
         this.hihokenshaNo = hihokenshaNo;
         this.hihokenshaNameKana = hihokenshaNameKana;
@@ -116,8 +131,8 @@ public class JukyuShikakuShomeishoBodyItem {
         this.tenshutsusakiYoteiJusho = tenshutsusakiYoteiJusho;
         this.idoYoteiYMD = idoYoteiYMD;
         this.hokenshaNo = hokenshaNo;
-        this.shomeiHakkoYMD = shomeiHakkoYMD;
-        this.shuchoMei = shuchoMei;
+        this.hakkoYMD = hakkoYMD;
+        this.ninshoshaYakushokuMei = ninshoshaYakushokuMei;
         this.recognizedName = recognizedName;
         this.denshiKoin = denshiKoin;
         this.koinShoryaku = koinShoryaku;
@@ -131,5 +146,10 @@ public class JukyuShikakuShomeishoBodyItem {
         this.shinsakaiIken = shinsakaiIken;
         this.biko = biko;
         this.remban = remban;
+        this.koinMojiretsu = koinMojiretsu;
+        this.ninshoshaShimeiKakenai = ninshoshaShimeiKakenai;
+        this.ninshoshaShimeiKakeru = ninshoshaShimeiKakeru;
+        this.ninshoshaYakushokuMei1 = ninshoshaYakushokuMei1;
+        this.ninshoshaYakushokuMei2 = ninshoshaYakushokuMei2;
     }
 }
