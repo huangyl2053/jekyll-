286bf1877bfe3c8cb5757b72556ed194fb5a9547
wangkun <wangkun@liandisys.com.cn>
2016-04-25 20:05:46 +0800

    「画面設計_DBAMN22001_資格喪失異動」と「資格取得異動」のソースを修正しました。

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/IryohokenRirekiCommonChildDiv/dgIryohokenIchiran_Row.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/IryohokenRirekiCommonChildDiv/dgIryohokenIchiran_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.Iryohoken
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Wed Apr 13 10:44:03 CST 2016 
+ * Mon Apr 25 17:22:15 CST 2016 
  */
 
 
