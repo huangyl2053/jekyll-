d01aa3f4bc089a7e92eb6e78e1025235c1a3235c
許悦 <xuyue@liandisys.com.cn>
2016-04-26 18:17:17 +0800

    DBD-3600-010　１．関連チェックの修正。２．更新処理の修正

--- DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/handler/parentdiv/DBD1020001/RiyoshaFutangakuGengakuValidationHandler.java
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/handler/parentdiv/DBD1020001/RiyoshaFutangakuGengakuValidationHandler.java
@@ -5,6 +5,7 @@
  */
 package jp.co.ndensan.reams.db.dbd.divcontroller.handler.parentdiv.DBD1020001;
 
+import jp.co.ndensan.reams.db.dbd.definition.message.DbdErrorMessages;
 import jp.co.ndensan.reams.db.dbd.divcontroller.entity.parentdiv.DBD1020001.RiyoshaFutangakuGengakuPanelDiv;
 import jp.co.ndensan.reams.db.dbd.divcontroller.entity.parentdiv.DBD1020001.RiyoshaFutangakuGengakuPanelDivSpec;
 import jp.co.ndensan.reams.ur.urz.definition.message.UrErrorMessages;
@@ -28,7 +29,7 @@ public class RiyoshaFutangakuGengakuValidationHandler {
      * 申請日の必須入力チェックを行います。
      *
      * @param pairs バリデーションコントロール
-     * @param div NinnteiChousaKekkaTouroku1Div
+     * @param div RiyoshaFutangakuGengakuPanelDiv
      * @return バリデーション結果
      */
     public ValidationMessageControlPairs validateFor申請日の必須入力(ValidationMessageControlPairs pairs, RiyoshaFutangakuGengakuPanelDiv div) {
@@ -45,7 +46,7 @@ public class RiyoshaFutangakuGengakuValidationHandler {
      * 決定区分の必須入力チェックを行います。
      *
      * @param pairs バリデーションコントロール
-     * @param div NinnteiChousaKekkaTouroku1Div
+     * @param div RiyoshaFutangakuGengakuPanelDiv
      * @return バリデーション結果
      */
     public ValidationMessageControlPairs validateFor決定区分の必須入力(ValidationMessageControlPairs pairs, RiyoshaFutangakuGengakuPanelDiv div) {
@@ -62,7 +63,7 @@ public class RiyoshaFutangakuGengakuValidationHandler {
      * 決定日の必須入力チェックを行います。
      *
      * @param pairs バリデーションコントロール
-     * @param div NinnteiChousaKekkaTouroku1Div
+     * @param div RiyoshaFutangakuGengakuPanelDiv
      * @return バリデーション結果
      */
     public ValidationMessageControlPairs validateFor決定日の必須入力(ValidationMessageControlPairs pairs, RiyoshaFutangakuGengakuPanelDiv div) {
@@ -79,7 +80,7 @@ public class RiyoshaFutangakuGengakuValidationHandler {
      * 適用日の必須入力チェックを行います。
      *
      * @param pairs バリデーションコントロール
-     * @param div NinnteiChousaKekkaTouroku1Div
+     * @param div RiyoshaFutangakuGengakuPanelDiv
      * @return バリデーション結果
      */
     public ValidationMessageControlPairs validateFor適用日の必須入力(ValidationMessageControlPairs pairs, RiyoshaFutangakuGengakuPanelDiv div) {
@@ -96,7 +97,7 @@ public class RiyoshaFutangakuGengakuValidationHandler {
      * 有効期限の必須入力チェックを行います。
      *
      * @param pairs バリデーションコントロール
-     * @param div NinnteiChousaKekkaTouroku1Div
+     * @param div RiyoshaFutangakuGengakuPanelDiv
      * @return バリデーション結果
      */
     public ValidationMessageControlPairs validateFor有効期限の必須入力(ValidationMessageControlPairs pairs, RiyoshaFutangakuGengakuPanelDiv div) {
@@ -113,7 +114,7 @@ public class RiyoshaFutangakuGengakuValidationHandler {
      * 給付率の必須入力チェックを行います。
      *
      * @param pairs バリデーションコントロール
-     * @param div NinnteiChousaKekkaTouroku1Div
+     * @param div RiyoshaFutangakuGengakuPanelDiv
      * @return バリデーション結果
      */
     public ValidationMessageControlPairs validateFor給付率の必須入力(ValidationMessageControlPairs pairs, RiyoshaFutangakuGengakuPanelDiv div) {
@@ -126,6 +127,113 @@ public class RiyoshaFutangakuGengakuValidationHandler {
         return pairs;
     }
 
+    /**
+     * 利用者負担額減額_給付率範囲外チェックを行います。
+     *
+     * @param pairs バリデーションコントロール
+     * @param div RiyoshaFutangakuGengakuPanelDiv
+     * @return バリデーション結果
+     */
+    public ValidationMessageControlPairs validateFor利用者負担額減額_給付率範囲外(ValidationMessageControlPairs pairs, RiyoshaFutangakuGengakuPanelDiv div) {
+
+        IValidationMessages messages = ValidationMessagesFactory.createInstance();
+        messages.add(ValidateChain.validateStart(div).ifNot(RiyoshaFutangakuGengakuPanelDivSpec.利用者負担額減額_給付率範囲外のチェック)
+                .thenAdd(NoInputMessages.利用者負担額減額_給付率範囲外).messages());
+        pairs.add(new ValidationMessageControlDictionaryBuilder().add(
+                NoInputMessages.利用者負担額減額_給付率範囲外, div.getTxtKyufuRitsu()).build().check(messages));
+        return pairs;
+    }
+
+    /**
+     * 利用者負担額減額_適用開始日が法施行以前のチェックを行います。
+     *
+     * @param pairs バリデーションコントロール
+     * @param div RiyoshaFutangakuGengakuPanelDiv
+     * @return バリデーション結果
+     */
+    public ValidationMessageControlPairs validateFor利用者負担額減額_適用開始日が法施行以前(ValidationMessageControlPairs pairs,
+            RiyoshaFutangakuGengakuPanelDiv div) {
+
+        IValidationMessages messages = ValidationMessagesFactory.createInstance();
+        messages.add(ValidateChain.validateStart(div).ifNot(RiyoshaFutangakuGengakuPanelDivSpec.利用者負担額減額_適用開始日が法施行以前のチェック)
+                .thenAdd(NoInputMessages.減免減額_適用日が法施行前).messages());
+        pairs.add(new ValidationMessageControlDictionaryBuilder().add(
+                NoInputMessages.減免減額_適用日が法施行前, div.getTxtTekiyoYmd()).build().check(messages));
+        return pairs;
+    }
+
+    /**
+     * 利用者負担額減額_適用終了日が年度外のチェックを行います。
+     *
+     * @param pairs バリデーションコントロール
+     * @param div RiyoshaFutangakuGengakuPanelDiv
+     * @return バリデーション結果
+     */
+    public ValidationMessageControlPairs validateFor利用者負担額減額_適用終了日が年度外(ValidationMessageControlPairs pairs,
+            RiyoshaFutangakuGengakuPanelDiv div) {
+
+        IValidationMessages messages = ValidationMessagesFactory.createInstance();
+        messages.add(ValidateChain.validateStart(div).ifNot(RiyoshaFutangakuGengakuPanelDivSpec.利用者負担額減額_適用終了日が年度外のチェック)
+                .thenAdd(NoInputMessages.減免減額_有効期限が年度外).messages());
+        pairs.add(new ValidationMessageControlDictionaryBuilder().add(
+                NoInputMessages.減免減額_有効期限が年度外, div.getTxtYukoKigenYmd()).build().check(messages));
+        return pairs;
+    }
+
+    /**
+     * 利用者負担額減額_適用終了日が開始日以前のチェックを行います。
+     *
+     * @param pairs バリデーションコントロール
+     * @param div RiyoshaFutangakuGengakuPanelDiv
+     * @return バリデーション結果
+     */
+    public ValidationMessageControlPairs validateFor利用者負担額減額_適用終了日が開始日以前(ValidationMessageControlPairs pairs,
+            RiyoshaFutangakuGengakuPanelDiv div) {
+
+        IValidationMessages messages = ValidationMessagesFactory.createInstance();
+        messages.add(ValidateChain.validateStart(div).ifNot(RiyoshaFutangakuGengakuPanelDivSpec.利用者負担額減額_適用終了日が開始日以前のチェック)
+                .thenAdd(NoInputMessages.減免減額_有効期限が適用日以前).messages());
+        pairs.add(new ValidationMessageControlDictionaryBuilder().add(
+                NoInputMessages.減免減額_有効期限が適用日以前, div.getTxtYukoKigenYmd()).build().check(messages));
+        return pairs;
+    }
+
+    /**
+     * 受給共通_受給者登録なしのチェックを行います。
+     *
+     * @param pairs バリデーションコントロール
+     * @param div RiyoshaFutangakuGengakuPanelDiv
+     * @return バリデーション結果
+     */
+    public ValidationMessageControlPairs validateFor受給共通_受給者登録なし(ValidationMessageControlPairs pairs,
+            RiyoshaFutangakuGengakuPanelDiv div) {
+
+        IValidationMessages messages = ValidationMessagesFactory.createInstance();
+        messages.add(ValidateChain.validateStart(div).ifNot(RiyoshaFutangakuGengakuPanelDivSpec.受給共通_受給者登録なしのチェック)
+                .thenAdd(NoInputMessages.受給共通_受給者_事業対象者登録なし).messages());
+        pairs.add(new ValidationMessageControlDictionaryBuilder().add(
+                NoInputMessages.受給共通_受給者_事業対象者登録なし, div.getTxtTekiyoYmd()).build().check(messages));
+        return pairs;
+    }
+
+    /**
+     * 適用期間重複なしのチェックを行います。
+     *
+     * @param pairs バリデーションコントロール
+     * @param div RiyoshaFutangakuGengakuPanelDiv
+     * @return バリデーション結果
+     */
+    public ValidationMessageControlPairs validateFor適用期間重複なし(ValidationMessageControlPairs pairs,
+            RiyoshaFutangakuGengakuPanelDiv div) {
+
+        IValidationMessages messages = ValidationMessagesFactory.createInstance();
+        messages.add(ValidateChain.validateStart(div).ifNot(RiyoshaFutangakuGengakuPanelDivSpec.適用期間重複なしのチェック)
+                .thenAdd(NoInputMessages.減免減額_適用期間重複).messages());
+        pairs.add(new ValidationMessageControlDictionaryBuilder().add(
+                NoInputMessages.減免減額_適用期間重複, div.getRiyoshaFutangakuGengakuShinseiList().getDdlShinseiIchiran()).build().check(messages));
+        return pairs;
+    }
+
     private static enum NoInputMessages implements IValidationMessage {
 
         申請日の必須入力(UrErrorMessages.必須項目_追加メッセージあり, "申請日"),
@@ -133,7 +241,14 @@ public class RiyoshaFutangakuGengakuValidationHandler {
         決定日の必須入力(UrErrorMessages.必須項目_追加メッセージあり, "決定日"),
         適用日の必須入力(UrErrorMessages.必須項目_追加メッセージあり, "適用日"),
         有効期限の必須入力(UrErrorMessages.必須項目_追加メッセージあり, "有効期限"),
-        給付率の必須入力(UrErrorMessages.必須項目_追加メッセージあり, "給付率");
+        給付率の必須入力(UrErrorMessages.必須項目_追加メッセージあり, "給付率"),
+        利用者負担額減額_給付率範囲外(DbdErrorMessages.利用者負担額減額_給付率範囲外),
+        減免減額_適用日が法施行前(DbdErrorMessages.減免減額_適用日が法施行前),
+        減免減額_有効期限が年度外(DbdErrorMessages.減免減額_有効期限が年度外),
+        減免減額_有効期限が適用日以前(DbdErrorMessages.減免減額_有効期限が適用日以前),
+        受給共通_受給者_事業対象者登録なし(DbdErrorMessages.受給共通_受給者_事業対象者登録なし),
+        減免減額_適用期間重複(DbdErrorMessages.減免減額_適用期間重複);
+
         private final Message message;
 
         private NoInputMessages(IMessageGettable message, String... replacements) {
