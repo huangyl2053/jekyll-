157c80450f36a649c9c6f66aec805e1db77435bd
wangkun <wangkun@liandisys.com.cn>
2016-04-26 20:56:10 +0800

    ソースを修正しました。

--- DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/commonchilddiv/ShoKaishuKirokuKanri/ShoKaishuKirokuKanriHandler.java
+++ DB/DBA/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dba/divcontroller/entity/commonchilddiv/ShoKaishuKirokuKanri/ShoKaishuKirokuKanriHandler.java
@@ -155,10 +155,10 @@ public class ShoKaishuKirokuKanriHandler {
             if (追加.equals(dgKoufuKaishu.getStatus())) {
                 builder.set交付年月日(new FlexibleDate(new RDate(dgKoufuKaishu.getKoufuDate().toString()).toDateString()));
                 builder.set有効期限(new FlexibleDate(new RDate(dgKoufuKaishu.getYukoKigen().toString()).toDateString()));
-                builder.set交付事由(dgKoufuKaishu.getKoufuJiyu());
+                builder.set交付事由(dgKoufuKaishu.getKoufuJiyuNo());
                 builder.set交付理由(dgKoufuKaishu.getKofuRiyu());
                 builder.set回収年月日(new FlexibleDate(new RDate(dgKoufuKaishu.getKaishuDate().toString()).toDateString()));
-                builder.set回収事由(dgKoufuKaishu.getKaishuJiyu());
+                builder.set回収事由(dgKoufuKaishu.getKaishuJiyuNo());
                 builder.set回収理由(dgKoufuKaishu.getKaishuRiyu());
                 builder.set発行処理日時(YMDHMS.now());
                 ShoKofuKaishuJohoManager.createInstance().証交付回収情報の追加(builder.build());
@@ -166,10 +166,10 @@ public class ShoKaishuKirokuKanriHandler {
             if (更新.equals(dgKoufuKaishu.getStatus())) {
                 builder.set交付年月日(new FlexibleDate(new RDate(dgKoufuKaishu.getKoufuDate().toString()).toDateString()));
                 builder.set有効期限(new FlexibleDate(new RDate(dgKoufuKaishu.getYukoKigen().toString()).toDateString()));
-                builder.set交付事由(dgKoufuKaishu.getKoufuJiyu());
+                builder.set交付事由(dgKoufuKaishu.getKoufuJiyuNo());
                 builder.set交付理由(dgKoufuKaishu.getKofuRiyu());
                 builder.set回収年月日(new FlexibleDate(new RDate(dgKoufuKaishu.getKaishuDate().toString()).toDateString()));
-                builder.set回収事由(dgKoufuKaishu.getKaishuJiyu());
+                builder.set回収事由(dgKoufuKaishu.getKaishuJiyuNo());
                 builder.set回収理由(dgKoufuKaishu.getKaishuRiyu());
                 builder.set発行処理日時(YMDHMS.now());
                 ShoKofuKaishuJohoManager.createInstance().証交付回収情報の更新(builder.build());
