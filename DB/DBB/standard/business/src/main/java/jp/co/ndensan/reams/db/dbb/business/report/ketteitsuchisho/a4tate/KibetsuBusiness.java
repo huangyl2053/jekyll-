7d15c552dc741e6bee76fa1fc963f9d8cc0abc9f
sunhadi <sunhaidi@liandisys.com.cn>
2016-04-27 10:51:19 +0800

    帳票設計_DBBRP00005_2_介護保険料額決定通知書　新規作成

---
+++ DB/DBB/standard/business/src/main/java/jp/co/ndensan/reams/db/dbb/business/report/ketteitsuchisho/a4tate/KibetsuBusiness.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbb.business.report.ketteitsuchisho.a4tate;
+
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+/**
+ * 介護保険料額決定通知書のA4縦タイプのItemです。
+ *
+ * @reamsid_L DBB-9080-010 sunhaidi
+ */
+@lombok.Getter
+@lombok.Setter
+@SuppressWarnings("PMD.UnusedPrivateField")
+public class KibetsuBusiness {
+
+    private RString listKibetsu_1;
+    private RString listKibetsu_2;
+    private RString listKibetsu_3;
+    private RString listKibetsu_4;
+    private RString listKibetsu_5;
+    private RString listKibetsu_6;
+    private RString listKibetsu_7;
+    private RString listKibetsu_8;
+    private RString listKibetsu_9;
+    private RString listZuiji_1;
+}
