ce6d1f4166a4994827d2d713172b6e21f28e9d12
zhulx <zhulx@liandisys.com.cn>
2016-04-27 09:04:47 +0800

    DBD_3630_DBDMN32002_支払方法変更管理リスト作成

---
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD2020002/TorokushaJokenDiv.java
@@ -0,0 +1,152 @@
+package jp.co.ndensan.reams.db.dbd.divcontroller.entity.parentdiv.DBD2020002;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+
+/**
+ * TorokushaJoken のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class TorokushaJokenDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-01-15_09-59-03">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("ddl2GoSashitomeYokoku")
+    private DropDownList ddl2GoSashitomeYokoku;
+    @JsonProperty("ddlGoSashitomeToroku")
+    private DropDownList ddlGoSashitomeToroku;
+    @JsonProperty("ddl2GoShokanYokoku")
+    private DropDownList ddl2GoShokanYokoku;
+    @JsonProperty("ddl1GoShokanKettei")
+    private DropDownList ddl1GoShokanKettei;
+    @JsonProperty("ddl1GoShokanKetteiSashitomeAriOnly")
+    private DropDownList ddl1GoShokanKetteiSashitomeAriOnly;
+    @JsonProperty("ddl1GoShokanKetteiKojoAriOnly")
+    private DropDownList ddl1GoShokanKetteiKojoAriOnly;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * getddl2GoSashitomeYokoku
+     * @return ddl2GoSashitomeYokoku
+     */
+    @JsonProperty("ddl2GoSashitomeYokoku")
+    public DropDownList getDdl2GoSashitomeYokoku() {
+        return ddl2GoSashitomeYokoku;
+    }
+
+    /*
+     * setddl2GoSashitomeYokoku
+     * @param ddl2GoSashitomeYokoku ddl2GoSashitomeYokoku
+     */
+    @JsonProperty("ddl2GoSashitomeYokoku")
+    public void setDdl2GoSashitomeYokoku(DropDownList ddl2GoSashitomeYokoku) {
+        this.ddl2GoSashitomeYokoku = ddl2GoSashitomeYokoku;
+    }
+
+    /*
+     * getddlGoSashitomeToroku
+     * @return ddlGoSashitomeToroku
+     */
+    @JsonProperty("ddlGoSashitomeToroku")
+    public DropDownList getDdlGoSashitomeToroku() {
+        return ddlGoSashitomeToroku;
+    }
+
+    /*
+     * setddlGoSashitomeToroku
+     * @param ddlGoSashitomeToroku ddlGoSashitomeToroku
+     */
+    @JsonProperty("ddlGoSashitomeToroku")
+    public void setDdlGoSashitomeToroku(DropDownList ddlGoSashitomeToroku) {
+        this.ddlGoSashitomeToroku = ddlGoSashitomeToroku;
+    }
+
+    /*
+     * getddl2GoShokanYokoku
+     * @return ddl2GoShokanYokoku
+     */
+    @JsonProperty("ddl2GoShokanYokoku")
+    public DropDownList getDdl2GoShokanYokoku() {
+        return ddl2GoShokanYokoku;
+    }
+
+    /*
+     * setddl2GoShokanYokoku
+     * @param ddl2GoShokanYokoku ddl2GoShokanYokoku
+     */
+    @JsonProperty("ddl2GoShokanYokoku")
+    public void setDdl2GoShokanYokoku(DropDownList ddl2GoShokanYokoku) {
+        this.ddl2GoShokanYokoku = ddl2GoShokanYokoku;
+    }
+
+    /*
+     * getddl1GoShokanKettei
+     * @return ddl1GoShokanKettei
+     */
+    @JsonProperty("ddl1GoShokanKettei")
+    public DropDownList getDdl1GoShokanKettei() {
+        return ddl1GoShokanKettei;
+    }
+
+    /*
+     * setddl1GoShokanKettei
+     * @param ddl1GoShokanKettei ddl1GoShokanKettei
+     */
+    @JsonProperty("ddl1GoShokanKettei")
+    public void setDdl1GoShokanKettei(DropDownList ddl1GoShokanKettei) {
+        this.ddl1GoShokanKettei = ddl1GoShokanKettei;
+    }
+
+    /*
+     * getddl1GoShokanKetteiSashitomeAriOnly
+     * @return ddl1GoShokanKetteiSashitomeAriOnly
+     */
+    @JsonProperty("ddl1GoShokanKetteiSashitomeAriOnly")
+    public DropDownList getDdl1GoShokanKetteiSashitomeAriOnly() {
+        return ddl1GoShokanKetteiSashitomeAriOnly;
+    }
+
+    /*
+     * setddl1GoShokanKetteiSashitomeAriOnly
+     * @param ddl1GoShokanKetteiSashitomeAriOnly ddl1GoShokanKetteiSashitomeAriOnly
+     */
+    @JsonProperty("ddl1GoShokanKetteiSashitomeAriOnly")
+    public void setDdl1GoShokanKetteiSashitomeAriOnly(DropDownList ddl1GoShokanKetteiSashitomeAriOnly) {
+        this.ddl1GoShokanKetteiSashitomeAriOnly = ddl1GoShokanKetteiSashitomeAriOnly;
+    }
+
+    /*
+     * getddl1GoShokanKetteiKojoAriOnly
+     * @return ddl1GoShokanKetteiKojoAriOnly
+     */
+    @JsonProperty("ddl1GoShokanKetteiKojoAriOnly")
+    public DropDownList getDdl1GoShokanKetteiKojoAriOnly() {
+        return ddl1GoShokanKetteiKojoAriOnly;
+    }
+
+    /*
+     * setddl1GoShokanKetteiKojoAriOnly
+     * @param ddl1GoShokanKetteiKojoAriOnly ddl1GoShokanKetteiKojoAriOnly
+     */
+    @JsonProperty("ddl1GoShokanKetteiKojoAriOnly")
+    public void setDdl1GoShokanKetteiKojoAriOnly(DropDownList ddl1GoShokanKetteiKojoAriOnly) {
+        this.ddl1GoShokanKetteiKojoAriOnly = ddl1GoShokanKetteiKojoAriOnly;
+    }
+
+    // </editor-fold>
+}
