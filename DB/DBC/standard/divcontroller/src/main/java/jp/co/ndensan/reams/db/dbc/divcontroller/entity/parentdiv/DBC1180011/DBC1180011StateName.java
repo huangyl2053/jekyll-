e36ad8a151569835150d332649cdfadfc79e26f7
橋本　治義 <n8322@ndensan.co.jp>
2016-04-27 09:47:25 +0900

    画面設計時のRaml、UIコンテナ作成 DBCMN62003_高額合算支給額計算結果登録

---
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC1180011/DBC1180011StateName.java
@@ -0,0 +1,38 @@
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC1180011;
+
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+
+/**
+ * このコードはツールによって生成されました。</br>
+ * このファイルへの変更は、再生成時には損失するため</br>
+ * 不正な動作の原因になります。</br>
+ * DBC1180011の状態名定義クラスです
+ *
+ * @author 自動生成
+ */
+public enum DBC1180011StateName implements IStateEnumerations {
+// <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
+
+    NoChange(""),
+    計算結果一覧("計算結果一覧"),
+    計算結果入力("計算結果入力"),
+    切替用_計算結果入力内訳情報("切替用_計算結果入力内訳情報");
+
+    private final RString name;
+
+    private DBC1180011StateName(String name) {
+        this.name = new RString(name);
+    }
+
+    /**
+     * getName
+     *
+     * @return 名前
+     */
+    @Override
+    public RString getName() {
+        return name;
+    }
+// </editor-fold>
+}
