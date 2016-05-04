d1a8e8b899ac85faddd465d9e3a4611f176754b2
sunhui <sunhui@liandisys.com.cn>
2016-05-04 11:21:14 +0800

    画面設計_DBCGM23001_汎用リスト出力(高額介護サービス費状況)--Exception情報追加

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC7020001/DvKogakuServiceJoho.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC7020001/DvKogakuServiceJoho.java
@@ -13,10 +13,12 @@ import jp.co.ndensan.reams.db.dbc.divcontroller.viewbox.ViewStateKeys;
 import jp.co.ndensan.reams.db.dbx.business.core.shichosonsecurity.ShichosonSecurityJoho;
 import jp.co.ndensan.reams.db.dbx.definition.core.shichosonsecurity.GyomuBunrui;
 import jp.co.ndensan.reams.db.dbx.service.core.shichosonsecurity.ShichosonSecurityJohoFinder;
+import jp.co.ndensan.reams.ur.urz.definition.message.UrErrorMessages;
 import jp.co.ndensan.reams.uz.uza.biz.ReportId;
 import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.lang.SystemException;
 import jp.co.ndensan.reams.uz.uza.ui.servlets.ValidationMessageControlPairs;
 import jp.co.ndensan.reams.uz.uza.ui.servlets.ViewStateHolder;
 
@@ -44,7 +46,10 @@ public class DvKogakuServiceJoho {
                 .getShichosonSecurityJoho(GyomuBunrui.介護事務);
         DvKogakuChushutsuJokenDiv panel = div.getDvKogakuServiceParam().getDvKogakuChushutsuJoken();
         panel.getCcdHokenshaList().loadHokenshaList();
-        if (市町村セキュリティ情報 != null && 市町村セキュリティ情報.get導入形態コード() != null
+        if (市町村セキュリティ情報 == null) {
+            throw new SystemException(UrErrorMessages.対象データなし.getMessage().evaluate());
+        }
+        if (市町村セキュリティ情報.get導入形態コード() != null
                 && 市町村セキュリティ情報.get導入形態コード().is広域()) {
             ViewStateHolder.put(ViewStateKeys.市町村判定, 事務広域);
             panel.getCcdHokenshaList().setDisabled(false);
@@ -53,6 +58,7 @@ public class DvKogakuServiceJoho {
             ViewStateHolder.put(ViewStateKeys.市町村判定, 事務単一);
             panel.getCcdHokenshaList().setDisabled(true);
             panel.getCcdHokenshaList().setVisible(false);
+            throw new SystemException(UrErrorMessages.対象データなし.getMessage().evaluate());
         }
         div.getDvKogakuServiceParam().getCcdKogakuShutsuryokujun().load(SubGyomuCode.DBC介護給付, 帳票ID);
         div.getCcdKogakuShutsuryokuKomoku().setVisible(false);
