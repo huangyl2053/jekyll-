eaac89363aee3e322e29b5fb2db692fdeb786ba9
楊謹 <n8345@ndensan.com>
2016-05-06 17:10:18 +0900

    画面DBE6910001_報酬マスタメンテナンス作成

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE6910001/DBE6910001TransitionEventName.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE6910001/DBE6910001TransitionEventName.java
@@ -1,29 +1,24 @@
 package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE6910001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 
 /**
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBE6910001の状態名定義クラスです
+ * DBE6910001のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBE6910001StateName implements IStateEnumerations {
+public enum DBE6910001TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    NoChange(""),
-    照会("照会"),
-    審査明細("審査明細"),
-    意見明細("意見明細"),
-    訪問明細("訪問明細"),
-    完了状態("完了状態");
+    終了状態("終了状態");
 
     private final RString name;
 
-    private DBE6910001StateName(String name) {
+    private DBE6910001TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
