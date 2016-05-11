03427d854779d9200058e062132a0159d3fe259d
wangkanglei <wangkanglei@liandisys.com.cn>
2016-05-11 15:55:43 +0800

    画面設計_DBUMN91003_事業報告（月報）補正発行_検索画面(修正)

--- DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/core/jigyohokokugeppohoseihako/ShichosonCodeResult.java
+++ DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/core/jigyohokokugeppohoseihako/ShichosonCodeResult.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbu.business.jigyohokokugeppohoseihako;
+package jp.co.ndensan.reams.db.dbu.business.core.jigyohokokugeppohoseihako;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 
