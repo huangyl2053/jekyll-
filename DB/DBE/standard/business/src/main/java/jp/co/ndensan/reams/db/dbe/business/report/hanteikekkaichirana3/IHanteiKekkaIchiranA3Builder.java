aff4db7a95a826cfc8ca6215bd3d99e9eba3ad52
xuyannan <xuyannan@liandisys.com.cn>
2016-05-12 18:20:13 +0800

    帳票設計_DBE525002_要介護認定判定結果一覧表Ａ３版 新規作成

---
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/hanteikekkaichirana3/IHanteiKekkaIchiranA3Builder.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.business.report.hanteikekkaichirana3;
+
+import jp.co.ndensan.reams.db.dbe.entity.report.hanteikekkaichirana3.HanteiKekkaIchiranA3ReportSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportBuilder;
+
+/**
+ * 要介護認定判定結果一覧表Ａ３版のインターフェースクラスです。
+ *
+ * @reamsid_L DBE-0180-050 xuyannan
+ */
+public interface IHanteiKekkaIchiranA3Builder extends IReportBuilder<HanteiKekkaIchiranA3ReportSource> {
+
+}
