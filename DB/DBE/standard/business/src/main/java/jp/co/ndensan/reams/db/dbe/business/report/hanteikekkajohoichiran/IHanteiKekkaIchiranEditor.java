9997d3f5374a22e0081c3a7d41ffff4528a4253a
wangkun <wangkun@liandisys.com.cn>
2016-05-12 14:47:34 +0800

    「帳票設計_DBE525001_要介護認定判定結果一覧表」のソースを新規作成しました。

---
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/hanteikekkajohoichiran/IHanteiKekkaIchiranEditor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.business.report.hanteikekkajohoichiran;
+
+import jp.co.ndensan.reams.db.dbe.entity.report.source.hanteikekkajohoichiran.HanteiKekkaIchiranA4ReportSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportEditor;
+
+/**
+ * 要介護認定判定結果一覧ヘッダエディターのインターフェースクラスです。
+ *
+ * @reamsid_L DBE-0170-020 wangkun
+ */
+interface IHanteiKekkaIchiranEditor extends IReportEditor<HanteiKekkaIchiranA4ReportSource> {
+}
