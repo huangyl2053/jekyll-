6012ace639139784db9db15054e668200afbd041
lizhuoxuan <lizhuoxuan@liandisys.com.cn>
2016-05-16 13:52:27 +0800

    画面設計_DBE5250002_判定結果情報出力（保険者） 仕様変更対応

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE5250002/dgTaishoshaIchiran_Row.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE5250002/dgTaishoshaIchiran_Row.java
@@ -3,20 +3,25 @@ package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE5250002;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Thu Mar 17 15:33:00 CST 2016
+ * Mon May 16 10:17:31 CST 2016 
  */
 
-import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+
 import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
 import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxCode;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxFlexibleDate;
 
+
 /**
- * dgTaishoshaIchiran_Row のクラスファイル
- *
+ * dgTaishoshaIchiran_Row のクラスファイル 
+ * 
+ * @author 自動生成
  */
+
 public class dgTaishoshaIchiran_Row extends DataRow {
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-01-15_09-59-03">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-03-22_14-06-37">
 
     private TextBoxCode hihokenshaNo;
     private RString hihokenshaShimei;
