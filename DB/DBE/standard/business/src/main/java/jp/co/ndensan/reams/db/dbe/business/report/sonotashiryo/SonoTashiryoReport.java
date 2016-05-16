8a7a0945aece13c486b8af67eec201fff318e068
zhangzhiming <zhangzhiming@liandisys.com.cn>
2016-05-16 15:04:45 +0800

    DBE-0230-050 帳票設計_DBE517XXX_その他資料 新規

---
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/sonotashiryo/SonoTashiryoReport.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.business.report.sonotashiryo;
+
+import jp.co.ndensan.reams.db.dbe.entity.report.source.sonotashiryo.SonoTashiryo;
+import jp.co.ndensan.reams.db.dbe.entity.report.source.sonotashiryo.SonoTashiryoReportSource;
+import jp.co.ndensan.reams.uz.uza.report.Report;
+import jp.co.ndensan.reams.uz.uza.report.ReportSourceWriter;
+
+/**
+ * 要介護認定情報提供その他資料情報のReportです。
+ *
+ * @reamsid_L DBE-0230-050 zhangzhiming
+ */
+public class SonoTashiryoReport extends Report<SonoTashiryoReportSource> {
+
+    private final SonoTashiryo data;
+
+    /**
+     * インスタンスを生成します。
+     *
+     * @param data 要介護認定情報提供その他資料情報のdataList
+     */
+    public SonoTashiryoReport(SonoTashiryo data) {
+        this.data = data;
+    }
+
+    @Override
+    public void writeBy(ReportSourceWriter<SonoTashiryoReportSource> reportSourceWriter) {
+        ISonoTashiryoBodyEditor editor = new SonoTashiryoBodyEditor(data);
+        ISonoTashiryoBuilder builder = new SonoTashiryoBuilder(editor);
+        reportSourceWriter.writeLine(builder);
+    }
+}
