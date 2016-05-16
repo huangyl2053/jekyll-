164184ad90b6923e2cdcc2452fd2f0f730b2e106
zuotao <zuotao@liandisys.com.cn>
2016-05-16 21:19:03 +0800

    画面設計_DBE2300001_主治医意見書作成依頼　仕様変更対応

--- DB/DBE/standard/divcontroller/src/main/webapp/WEB-INF/standard/View/DBE2300001_HiddenBatch.js
+++ DB/DBE/standard/divcontroller/src/main/webapp/WEB-INF/standard/View/DBE2300001_HiddenBatch.js
@@ -5,6 +5,11 @@ var callBackHandler = Uz._LoadInOrder.prototype.requestSequenceCallbackHandler;
 Uz._LoadInOrder.prototype.requestSequenceCallbackHandler = function(eventName) {
     callBackHandler.call(this, eventName);
     if (eventName === "btnHakkouValidationCheck_onClick_btnHakkouValidationCheck" && $("#UzErrorAndWarningDiv").css('display') == 'none') {
-        $("#btnHakkou").click();
+        $(document).on("click", ".ui-dialog-buttonset button", function(event) {
+            var $span = $(event.target);
+            if ($span.text() === "はい") {
+                $("#btnHakkou").click();
+            }
+        });
     }
 }
