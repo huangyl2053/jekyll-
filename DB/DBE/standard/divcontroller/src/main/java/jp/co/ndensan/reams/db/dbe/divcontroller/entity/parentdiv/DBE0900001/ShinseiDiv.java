15b3198d7682c7e8ceaf7609ed0bfcb61e3387ae
zhangzhiming <zhangzhiming@liandisys.com.cn>
2016-05-16 15:45:17 +0800

    DBE-0230-010　画面設計_DBE0900001_要介護認定情報提供　新規

---
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE0900001/ShinseiDiv.java
@@ -0,0 +1,92 @@
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE0900001;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+
+/**
+ * Shinsei のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class ShinseiDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-03-22_14-06-37">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("txtShinseibi")
+    private TextBoxDate txtShinseibi;
+    @JsonProperty("txtShinseiKubunShin")
+    private TextBox txtShinseiKubunShin;
+    @JsonProperty("txtShinseiKubun")
+    private TextBox txtShinseiKubun;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * gettxtShinseibi
+     * @return txtShinseibi
+     */
+    @JsonProperty("txtShinseibi")
+    public TextBoxDate getTxtShinseibi() {
+        return txtShinseibi;
+    }
+
+    /*
+     * settxtShinseibi
+     * @param txtShinseibi txtShinseibi
+     */
+    @JsonProperty("txtShinseibi")
+    public void setTxtShinseibi(TextBoxDate txtShinseibi) {
+        this.txtShinseibi = txtShinseibi;
+    }
+
+    /*
+     * gettxtShinseiKubunShin
+     * @return txtShinseiKubunShin
+     */
+    @JsonProperty("txtShinseiKubunShin")
+    public TextBox getTxtShinseiKubunShin() {
+        return txtShinseiKubunShin;
+    }
+
+    /*
+     * settxtShinseiKubunShin
+     * @param txtShinseiKubunShin txtShinseiKubunShin
+     */
+    @JsonProperty("txtShinseiKubunShin")
+    public void setTxtShinseiKubunShin(TextBox txtShinseiKubunShin) {
+        this.txtShinseiKubunShin = txtShinseiKubunShin;
+    }
+
+    /*
+     * gettxtShinseiKubun
+     * @return txtShinseiKubun
+     */
+    @JsonProperty("txtShinseiKubun")
+    public TextBox getTxtShinseiKubun() {
+        return txtShinseiKubun;
+    }
+
+    /*
+     * settxtShinseiKubun
+     * @param txtShinseiKubun txtShinseiKubun
+     */
+    @JsonProperty("txtShinseiKubun")
+    public void setTxtShinseiKubun(TextBox txtShinseiKubun) {
+        this.txtShinseiKubun = txtShinseiKubun;
+    }
+
+    // </editor-fold>
+}
---
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/handler/parentdiv/DBE0900001/YokaigoNinteiJohoTeikyoValidationHandler.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.divcontroller.handler.parentdiv.DBE0900001;
+
+import java.util.List;
+import jp.co.ndensan.reams.db.dbe.definition.message.DbeErrorMessages;
+import jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE0900001.YokaigoNinteiJohoTeikyoDiv;
+import jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE0900001.dgNinteiKekkaIchiran_Row;
+import jp.co.ndensan.reams.ur.urz.definition.message.UrErrorMessages;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.message.IMessageGettable;
+import jp.co.ndensan.reams.uz.uza.message.IValidationMessage;
+import jp.co.ndensan.reams.uz.uza.message.Message;
+import jp.co.ndensan.reams.uz.uza.ui.servlets.ValidationMessageControlPair;
+import jp.co.ndensan.reams.uz.uza.ui.servlets.ValidationMessageControlPairs;
+
+/**
+ * 要介護認定情報提供のバリデーションハンドラークラスです。
+ *
+ * @reamsid_L DBE-0230-010 zhangzhiming
+ */
+public class YokaigoNinteiJohoTeikyoValidationHandler {
+
+    private final YokaigoNinteiJohoTeikyoDiv div;
+
+    /**
+     * コンストラクタです。
+     *
+     * @param div 要介護認定情報提供Div
+     */
+    public YokaigoNinteiJohoTeikyoValidationHandler(YokaigoNinteiJohoTeikyoDiv div) {
+        this.div = div;
+    }
+
+    /**
+     * データチェック実行します。
+     *
+     * @return ValidationMessageControlPairs
+     */
+    public ValidationMessageControlPairs validateCheck() {
+        ValidationMessageControlPairs validationMessages = new ValidationMessageControlPairs();
+        List<dgNinteiKekkaIchiran_Row> rowList = div.getNInteiRirekiInfo().getDgNinteiKekkaIchiran().getDataSource();
+        if (rowList == null || rowList.isEmpty()) {
+            validationMessages.add(new ValidationMessageControlPair(new YokaigoNinteiJohoTeikyoValidationHandler.RRVMessages(
+                    UrErrorMessages.該当データなし)));
+        }
+        if (div.getNInteiRirekiInfo().getDgNinteiKekkaIchiran().getSelectedItems() == null
+                || div.getNInteiRirekiInfo().getDgNinteiKekkaIchiran().getSelectedItems().isEmpty()) {
+            validationMessages.add(new ValidationMessageControlPair(new YokaigoNinteiJohoTeikyoValidationHandler.RRVMessages(
+                    UrErrorMessages.対象行を選択)));
+        }
+        getMessage(validationMessages);
+        return validationMessages;
+    }
+
+    private ValidationMessageControlPairs getMessage(ValidationMessageControlPairs validationMessages) {
+        if (!div.getHakkoChohyo().getChkNinteiChosahyo().isAllSelected() && !div.getHakkoChohyo().getChkTokkiJiko().isAllSelected()
+                && !div.getHakkoChohyo().getChkShujiiIkensho().isAllSelected() && !div.getHakkoChohyo().getChkSonotaShiryo().isAllSelected()
+                && !div.getHakkoChohyo().getChkIchijiHanteiKekka().isAllSelected()) {
+            validationMessages.add(new ValidationMessageControlPair(new YokaigoNinteiJohoTeikyoValidationHandler.RRVMessages(
+                    UrErrorMessages.選択されていない, "帳票")));
+        }
+        if ((div.getHakkoChohyo().getChkNinteiChosahyo().isAllSelected() || div.getHakkoChohyo().getChkTokkiJiko().isAllSelected()
+                || div.getHakkoChohyo().getChkShujiiIkensho().isAllSelected() || div.getHakkoChohyo().getChkSonotaShiryo().isAllSelected()
+                || div.getHakkoChohyo().getChkIchijiHanteiKekka().isAllSelected())
+                && div.getNinteiKekkaShosai().getRadHihokenshaJohoTeikyoDoi().getSelectedKey().equals(new RString("key1"))) {
+            validationMessages.add(new ValidationMessageControlPair(new YokaigoNinteiJohoTeikyoValidationHandler.RRVMessages(
+                    DbeErrorMessages.印刷不可, "被保険者")));
+        }
+        if ((div.getHakkoChohyo().getChkNinteiChosahyo().isAllSelected() || div.getHakkoChohyo().getChkTokkiJiko().isAllSelected()
+                || div.getHakkoChohyo().getChkShujiiIkensho().isAllSelected() || div.getHakkoChohyo().getChkSonotaShiryo().isAllSelected()
+                || div.getHakkoChohyo().getChkIchijiHanteiKekka().isAllSelected())
+                && div.getNinteiKekkaShosai().getRadShijiiJohoTeikyoDoi().getSelectedKey().equals(new RString("key1"))) {
+            validationMessages.add(new ValidationMessageControlPair(new YokaigoNinteiJohoTeikyoValidationHandler.RRVMessages(
+                    DbeErrorMessages.印刷不可, "主治医")));
+        }
+        return validationMessages;
+    }
+
+    private static final class RRVMessages implements IValidationMessage {
+
+        private final Message message;
+
+        private RRVMessages(IMessageGettable message, String... replacements) {
+            this.message = message.getMessage().replace(replacements);
+        }
+
+        @Override
+        public Message getMessage() {
+            return message;
+        }
+    }
+}
