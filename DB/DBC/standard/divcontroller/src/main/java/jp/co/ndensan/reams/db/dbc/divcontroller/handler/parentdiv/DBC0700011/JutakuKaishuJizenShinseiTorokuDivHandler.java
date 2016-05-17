f3797ed44e4afcf47c47cae7736131fa5b708873
panhe <panhe@liandisys.com.cn>
2016-05-17 15:07:25 +0800

    画面設計_DBCMN52001_住宅改修費事前申請(セルフ業務決定のNo.111を対応する)

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/parentdiv/DBC0700011/JutakuKaishuJizenShinseiTorokuDivHandler.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/parentdiv/DBC0700011/JutakuKaishuJizenShinseiTorokuDivHandler.java
@@ -260,6 +260,9 @@ public final class JutakuKaishuJizenShinseiTorokuDivHandler {
             口座情報選択時(画面モード, data);
             div.setHidDataChangeFlg(RString.EMPTY);
         } else if (住宅改修情報.equals(selectedTab) && !初期化済み.equals(div.getHidJutakuKaisyuJyohoFlg())) {
+            if (div.getKaigoShikakuKihonShaPanel().getTxtServiceYM().getValue() == null) {
+                throw new ApplicationException(UrErrorMessages.必須.getMessage().replace(サービス年月.toString()));
+            }
             住宅改修情報選択時(画面モード, 被保険者番号);
             div.setHidDataChangeFlg(RString.EMPTY);
         } else if (審査結果.equals(selectedTab) && !初期化済み.equals(div.getHidSeikyuSummaryFlg())) {
