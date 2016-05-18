79e11fad7c1cf51edcaeaec36e81a5a1c5ae73bc
wanghui <wanghui@liandisys.com.cn>
2016-05-18 15:59:45 +0800

    DBU-1050-010　　画面設計_DBUMN52001_境界層管理マスタリスト　修正

--- DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/entity/parentdiv/DBU0520011/KyokaisouKanriMasterListPanelDiv.java
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/entity/parentdiv/DBU0520011/KyokaisouKanriMasterListPanelDiv.java
@@ -4,25 +4,20 @@ package jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0520011;
  * このファイルへの変更は、再生成時には損失するため
  * 不正な動作の原因になります。
  */
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.ur.urz.divcontroller.entity.commonchilddiv.chohyoshutsuryokujun.ChohyoShutsuryokujun.ChohyoShutsuryokujunDiv;
 import jp.co.ndensan.reams.ur.urz.divcontroller.entity.commonchilddiv.chohyoshutsuryokujun.ChohyoShutsuryokujun.IChohyoShutsuryokujunDiv;
-import jp.co.ndensan.reams.uz.uza.ui.binding.CheckBoxList;
-import jp.co.ndensan.reams.uz.uza.ui.binding.Label;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
 import jp.co.ndensan.reams.uz.uza.ui.binding.PanelBatchParameter;
-import jp.co.ndensan.reams.uz.uza.ui.binding.RadioButton;
-import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxDate;
-import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxDateRange;
 
 /**
- * KyokaisouKanriMasterListPanel のクラスファイル
- *
- * @reamsid_L DBU-1050-010 wanghui
+ * KyokaisouKanriMasterListPanel のクラスファイル 
+ * 
+ * @author 自動生成
  */
 public class KyokaisouKanriMasterListPanelDiv extends PanelBatchParameter {
-
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2015-11-30_08-54-50">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-03-22_14-06-37">
     /*
      * [ private の作成 ]
      * クライアント側から取得した情報を元にを検索を行い
@@ -80,82 +75,12 @@ public class KyokaisouKanriMasterListPanelDiv extends PanelBatchParameter {
      * [ ショートカットの作成 ]
      */
     @JsonIgnore
-    public RadioButton getRadKijunbi() {
-        return this.getKyokaisoKariParam().getRadKijunbi();
-    }
-
-    @JsonIgnore
-    public void setRadKijunbi(RadioButton radKijunbi) {
-        this.getKyokaisoKariParam().setRadKijunbi(radKijunbi);
-    }
-
-    @JsonIgnore
-    public TextBoxDate getTxtKijumbi() {
-        return this.getKyokaisoKariParam().getTxtKijumbi();
-    }
-
-    @JsonIgnore
-    public void setTxtKijumbi(TextBoxDate txtKijumbi) {
-        this.getKyokaisoKariParam().setTxtKijumbi(txtKijumbi);
-    }
-
-    @JsonIgnore
-    public Label getLblKyokaisoTourokusha() {
-        return this.getKyokaisoKariParam().getLblKyokaisoTourokusha();
-    }
-
-    @JsonIgnore
-    public void setLblKyokaisoTourokusha(Label lblKyokaisoTourokusha) {
-        this.getKyokaisoKariParam().setLblKyokaisoTourokusha(lblKyokaisoTourokusha);
-    }
-
-    @JsonIgnore
-    public RadioButton getRadHani() {
-        return this.getKyokaisoKariParam().getRadHani();
-    }
-
-    @JsonIgnore
-    public void setRadHani(RadioButton radHani) {
-        this.getKyokaisoKariParam().setRadHani(radHani);
-    }
-
-    @JsonIgnore
-    public RadioButton getRadHaniRadio() {
-        return this.getKyokaisoKariParam().getRadHaniRadio();
-    }
-
-    @JsonIgnore
-    public void setRadHaniRadio(RadioButton radHaniRadio) {
-        this.getKyokaisoKariParam().setRadHaniRadio(radHaniRadio);
-    }
-
-    @JsonIgnore
-    public TextBoxDateRange getTxtHaniChushutsu() {
-        return this.getKyokaisoKariParam().getTxtHaniChushutsu();
-    }
-
-    @JsonIgnore
-    public void setTxtHaniChushutsu(TextBoxDateRange txtHaniChushutsu) {
-        this.getKyokaisoKariParam().setTxtHaniChushutsu(txtHaniChushutsu);
-    }
-
-    @JsonIgnore
-    public RadioButton getRadShiteiNashi() {
-        return this.getKyokaisoKariParam().getRadShiteiNashi();
-    }
-
-    @JsonIgnore
-    public void setRadShiteiNashi(RadioButton radShiteiNashi) {
-        this.getKyokaisoKariParam().setRadShiteiNashi(radShiteiNashi);
-    }
-
-    @JsonIgnore
     public Label getLblShiteiNaiyoKubun() {
         return this.getShiteiNaiyoKubun().getLblShiteiNaiyoKubun();
     }
 
     @JsonIgnore
-    public void setLblShiteiNaiyoKubun(Label lblShiteiNaiyoKubun) {
+    public void  setLblShiteiNaiyoKubun(Label lblShiteiNaiyoKubun) {
         this.getShiteiNaiyoKubun().setLblShiteiNaiyoKubun(lblShiteiNaiyoKubun);
     }
 
@@ -165,7 +90,7 @@ public class KyokaisouKanriMasterListPanelDiv extends PanelBatchParameter {
     }
 
     @JsonIgnore
-    public void setChkShiteiNaiyoKubun(CheckBoxList chkShiteiNaiyoKubun) {
+    public void  setChkShiteiNaiyoKubun(CheckBoxList chkShiteiNaiyoKubun) {
         this.getShiteiNaiyoKubun().setChkShiteiNaiyoKubun(chkShiteiNaiyoKubun);
     }
 
