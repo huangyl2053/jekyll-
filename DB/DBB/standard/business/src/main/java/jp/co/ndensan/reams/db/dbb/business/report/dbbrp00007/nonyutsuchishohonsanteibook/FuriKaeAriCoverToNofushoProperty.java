13b93f7e46371077a3ea1d679e71084212040c9d
許悦 <xuyue@liandisys.com.cn>
2016-05-19 14:04:35 +0800

    DBB-9110-100 コメントを修正。　帳票ID：DBB100055_NonyuTsuchishoBookFuriKaeAri

--- DB/DBB/standard/business/src/main/java/jp/co/ndensan/reams/db/dbb/business/report/dbbrp00007/nonyutsuchishohonsanteibook/FuriKaeAriCoverToNofushoProperty.java
+++ DB/DBB/standard/business/src/main/java/jp/co/ndensan/reams/db/dbb/business/report/dbbrp00007/nonyutsuchishohonsanteibook/FuriKaeAriCoverToNofushoProperty.java
@@ -3,8 +3,8 @@ package jp.co.ndensan.reams.db.dbb.business.report.dbbrp00007.nonyutsuchishohons
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import jp.co.ndensan.reams.db.dbb.definition.reportid.ReportIdDBB;
 import jp.co.ndensan.reams.db.dbb.entity.report.dbbrp00007.nonyutsuchishohonsanteibook.FuriKaeAriCoverToNofushoReportSource;
-import jp.co.ndensan.reams.uz.uza.biz.ReportId;
 import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.report.BreakerCatalog;
@@ -14,25 +14,25 @@ import jp.co.ndensan.reams.uz.uza.report.ReportPropertyBase;
 import jp.co.ndensan.reams.uz.uza.report.data.chart.ReportDynamicChart;
 
 /**
- * 帳票「DBBRP00007_10_保険料納入通知書（本算定）【ブックタイプ】」のプロパティです。
+ * 保険料納入通知書（本算定）【ブック（口振依頼あり通知書＋納付書）タイプ】のプロパティです。
  *
  * @reamsid_L DBB-9110-100 xuyue
  */
 public class FuriKaeAriCoverToNofushoProperty extends ReportPropertyBase<FuriKaeAriCoverToNofushoReportSource> {
 
-    private static final ReportId REPORTID = new ReportId("DBB100055_DBB100020");
-
     private static final List<RString> LAYOUT_BREAK_KEY = Collections.unmodifiableList(Arrays.
             asList(new RString(FuriKaeAriCoverToNofushoReportSource.LAYOUTBREAKITEM.toString())));
 
     public FuriKaeAriCoverToNofushoProperty() {
-        super(SubGyomuCode.DBB介護賦課, REPORTID);
+        super(SubGyomuCode.DBB介護賦課, ReportIdDBB.DBB100055.getReportId());
     }
 
     @Override
     public Breakers<FuriKaeAriCoverToNofushoReportSource> defineBreakers(Breakers<FuriKaeAriCoverToNofushoReportSource> breakers,
             BreakerCatalog<FuriKaeAriCoverToNofushoReportSource> catalog) {
         return breakers.add(catalog.new SimpleLayoutBreaker(
+
+
             LAYOUT_BREAK_KEY) {
                 @Override
             public ReportLineRecord<FuriKaeAriCoverToNofushoReportSource> occuredBreak(ReportLineRecord<FuriKaeAriCoverToNofushoReportSource> currentRecord,
