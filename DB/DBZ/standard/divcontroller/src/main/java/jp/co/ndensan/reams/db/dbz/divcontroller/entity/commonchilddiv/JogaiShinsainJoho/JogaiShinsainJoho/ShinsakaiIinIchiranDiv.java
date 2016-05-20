2bd656b50879451ec2584344339aaad7facff418
dongyabin <dongyabin@liandisys.com.cn>
2016-05-20 15:43:06 +0800

    画面設計_DBZ.JogaiShinsainJoho_除外審査員情報　新規

---
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/JogaiShinsainJoho/JogaiShinsainJoho/ShinsakaiIinIchiranDiv.java
@@ -0,0 +1,72 @@
+package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.JogaiShinsainJoho.JogaiShinsainJoho;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+
+/**
+ * ShinsakaiIinIchiran のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class ShinsakaiIinIchiranDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-03-22_14-06-37">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("btnShinkiTsuika")
+    private Button btnShinkiTsuika;
+    @JsonProperty("dgShinsakaiIinIchiran")
+    private DataGrid<dgShinsakaiIinIchiran_Row> dgShinsakaiIinIchiran;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * getbtnShinkiTsuika
+     * @return btnShinkiTsuika
+     */
+    @JsonProperty("btnShinkiTsuika")
+    public Button getBtnShinkiTsuika() {
+        return btnShinkiTsuika;
+    }
+
+    /*
+     * setbtnShinkiTsuika
+     * @param btnShinkiTsuika btnShinkiTsuika
+     */
+    @JsonProperty("btnShinkiTsuika")
+    public void setBtnShinkiTsuika(Button btnShinkiTsuika) {
+        this.btnShinkiTsuika = btnShinkiTsuika;
+    }
+
+    /*
+     * getdgShinsakaiIinIchiran
+     * @return dgShinsakaiIinIchiran
+     */
+    @JsonProperty("dgShinsakaiIinIchiran")
+    public DataGrid<dgShinsakaiIinIchiran_Row> getDgShinsakaiIinIchiran() {
+        return dgShinsakaiIinIchiran;
+    }
+
+    /*
+     * setdgShinsakaiIinIchiran
+     * @param dgShinsakaiIinIchiran dgShinsakaiIinIchiran
+     */
+    @JsonProperty("dgShinsakaiIinIchiran")
+    public void setDgShinsakaiIinIchiran(DataGrid<dgShinsakaiIinIchiran_Row> dgShinsakaiIinIchiran) {
+        this.dgShinsakaiIinIchiran = dgShinsakaiIinIchiran;
+    }
+
+    // </editor-fold>
+}
