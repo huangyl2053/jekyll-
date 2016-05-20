3d31fd9eeae8a6d3c428e885527fa97d68ec6c28
橋本　治義 <n8322@ndensan.co.jp>
2016-05-20 10:56:11 +0900

    画面設計　DBCMN62006_自己負担額計算（一括）

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC1230011/DBC1230011StateName.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC1230011/DBC1230011StateName.java
@@ -1,25 +1,27 @@
-package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0220011;
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC1230011;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
 
 /**
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBC0220011のイベント名定義クラスです</br>
+ * DBC1230011の状態名定義クラスです
  *
  * @author 自動生成
  */
-public enum DBC0220011TransitionEventName implements IContainerEvents {
+public enum DBC1230011StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    連絡票変更("連絡票変更"),
-    完了("完了");
+    NoChange(""),
+    Default("default"),
+    支給決定情報一覧("支給決定情報一覧"),
+    支給決定情報補正("支給決定情報補正");
 
     private final RString name;
 
-    private DBC0220011TransitionEventName(String name) {
+    private DBC1230011StateName(String name) {
         this.name = new RString(name);
     }
 
