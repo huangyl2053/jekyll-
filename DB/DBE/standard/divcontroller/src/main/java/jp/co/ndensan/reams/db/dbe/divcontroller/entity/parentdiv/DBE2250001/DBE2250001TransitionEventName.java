b45906cc3344dcc520ee46cc4c916f34043a547d
王　昱翰 <n8372@ndensan.co.jp>
2016-05-23 20:40:22 +0900

    DBE_1870_DBEMN31003_認定調査データ取込み（モバイル）作成

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE2250001/DBE2250001TransitionEventName.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE2250001/DBE2250001TransitionEventName.java
@@ -1,26 +1,24 @@
 package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE2250001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 
 /**
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBE2250001の状態名定義クラスです
+ * DBE2250001のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBE2250001StateName implements IStateEnumerations {
+public enum DBE2250001TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    NoChange(""),
-    初期表示("初期表示"),
-    一覧表示("一覧表示");
+    完了("完了");
 
     private final RString name;
 
-    private DBE2250001StateName(String name) {
+    private DBE2250001TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
