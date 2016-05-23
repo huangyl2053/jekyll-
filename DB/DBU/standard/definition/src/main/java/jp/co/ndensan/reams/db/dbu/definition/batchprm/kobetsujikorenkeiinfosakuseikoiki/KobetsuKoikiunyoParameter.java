69507dd585046a480d2e72954ca36fd93ab9e3bb
wanghui <wanghui@liandisys.com.cn>
2016-05-23 09:51:04 +0800

    DBU-0550-020バッチ設計_DBUMN82003_介護住民票個別事項連携情報作成【広域運用】_CSVデータ作成の修正

--- DB/DBU/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbu/definition/batchprm/kobetsujikorenkeiinfosakuseikoiki/KobetsuKoikiunyoParameter.java
+++ DB/DBU/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbu/definition/batchprm/kobetsujikorenkeiinfosakuseikoiki/KobetsuKoikiunyoParameter.java
@@ -19,6 +19,6 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
 public class KobetsuKoikiunyoParameter {
 
     private RString shichosonCode;
-    private RDateTime dateFrom;
+    private RString dateFrom;
     private RDateTime dateTo;
 }
