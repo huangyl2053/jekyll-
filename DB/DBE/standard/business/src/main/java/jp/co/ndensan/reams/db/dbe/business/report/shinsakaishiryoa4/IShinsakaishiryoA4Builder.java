563187c568a3395e0ddc2cdb317763c738f3e03c
lishengli <lishengli@liandisys.com.cn>
2016-05-24 21:31:45 +0800

    帳票設計_DBE516001_事務局用介護認定審査対象者一覧表　新規作成

---
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/shinsakaishiryoa4/IShinsakaishiryoA4Builder.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.business.report.shinsakaishiryoa4;
+
+import jp.co.ndensan.reams.db.dbe.entity.report.source.shinsakaishiryoa4.ShinsakaishiryoA4ReportSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportBuilder;
+
+/**
+ * 事務局用介護認定審査対象者一覧表Builderのインターフェースクラスです。
+ *
+ * @reamsid_L DBE-0150-020 lishengli
+ */
+interface IShinsakaishiryoA4Builder extends IReportBuilder<ShinsakaishiryoA4ReportSource> {
+
+}
