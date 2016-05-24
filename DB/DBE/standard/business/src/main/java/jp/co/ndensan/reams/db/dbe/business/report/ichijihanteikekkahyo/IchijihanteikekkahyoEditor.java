52ef475fa92c12ed6cc93855729289eab03ad0e3
lizhuoxuan <lizhuoxuan@liandisys.com.cn>
2016-05-24 11:56:39 +0800

    帳票設計_DBE517XXX_一次判定結果を修正

--- DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/ichijihanteikekkahyo/IchijihanteikekkahyoEditor.java
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/ichijihanteikekkahyo/IchijihanteikekkahyoEditor.java
@@ -7,6 +7,13 @@ package jp.co.ndensan.reams.db.dbe.business.report.ichijihanteikekkahyo;
 
 import jp.co.ndensan.reams.db.dbe.entity.db.ichijihanteikekkahyoa4.IchijihanteikekkahyoEntity;
 import jp.co.ndensan.reams.db.dbe.entity.report.source.ichijihanteikekkahyoa4.IchijihanteikekkahyoReportSource;
+import jp.co.ndensan.reams.uz.uza.lang.EraType;
+import jp.co.ndensan.reams.uz.uza.lang.FillType;
+import jp.co.ndensan.reams.uz.uza.lang.FirstYear;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.lang.RStringBuilder;
+import jp.co.ndensan.reams.uz.uza.lang.Separator;
 
 /**
  * 一次判定結果のEditorです。
@@ -17,13 +24,6 @@ public class IchijihanteikekkahyoEditor implements IIchijihanteikekkahyoEditor {
 
     private final IchijihanteikekkahyoEntity item;
     private final int index;
-    private static final int 桁数_2 = 2;
-    private static final int 桁数_5 = 5;
-    private static final int 桁数_6 = 6;
-    private static final int 桁数_9 = 9;
-    private static final int 桁数_12 = 12;
-    private static final int 桁数_15 = 15;
-    private static final int 桁数_20 = 20;
 
     /**
      * インスタンスを生成します。
@@ -43,71 +43,115 @@ public class IchijihanteikekkahyoEditor implements IIchijihanteikekkahyoEditor {
 
     private IchijihanteikekkahyoReportSource editSource(IchijihanteikekkahyoReportSource source) {
 
-        if (index < 桁数_6) {
+        if (index < item.get社会生活への適応1リスト().size()) {
             source.listGogun_1 = item.get社会生活への適応1リスト().get(index);
+        }
+        if (index < item.get社会生活への適応2リスト().size()) {
             source.listGogun_2 = item.get社会生活への適応2リスト().get(index);
+        }
+        if (index < item.get社会生活への適応3リスト().size()) {
             source.listGogun_3 = item.get社会生活への適応3リスト().get(index);
+        }
+        if (index < item.get社会生活への適応4リスト().size()) {
             source.listGogun_4 = item.get社会生活への適応4リスト().get(index);
         }
-        if (index < 桁数_20) {
+        if (index < item.get身体機能_起居動作1リスト().size()) {
             source.listIchigun_1 = item.get身体機能_起居動作1リスト().get(index);
+        }
+        if (index < item.get身体機能_起居動作2リスト().size()) {
             source.listIchigun_2 = item.get身体機能_起居動作2リスト().get(index);
+        }
+        if (index < item.get身体機能_起居動作3リスト().size()) {
             source.listIchigun_3 = item.get身体機能_起居動作3リスト().get(index);
+        }
+        if (index < item.get身体機能_起居動作4リスト().size()) {
             source.listIchigun_4 = item.get身体機能_起居動作4リスト().get(index);
         }
-        if (index < 桁数_2) {
+        if (index < item.get高齢者自立度リスト().size()) {
             source.ｌistNichijoseikatsujiritsudo_1 = item.get高齢者自立度リスト().get(index);
         }
-        if (index < 桁数_12) {
+        if (index < item.get生活機能1リスト().size()) {
             source.listNigun_1 = item.get生活機能1リスト().get(index);
+        }
+        if (index < item.get生活機能2リスト().size()) {
             source.listNigun_2 = item.get生活機能2リスト().get(index);
+        }
+        if (index < item.get生活機能3リスト().size()) {
             source.listNigun_3 = item.get生活機能3リスト().get(index);
+        }
+        if (index < item.get生活機能4リスト().size()) {
             source.listNigun_4 = item.get生活機能4リスト().get(index);
         }
-        if (index < 桁数_9) {
+        if (index < item.get認知機能1リスト().size()) {
             source.listSangun_1 = item.get認知機能1リスト().get(index);
+        }
+        if (index < item.get認知機能2リスト().size()) {
             source.listSangun_2 = item.get認知機能2リスト().get(index);
+        }
+        if (index < item.get認知機能3リスト().size()) {
             source.listSangun_3 = item.get認知機能3リスト().get(index);
+        }
+        if (index < item.get認知機能4リスト().size()) {
             source.listSangun_4 = item.get認知機能4リスト().get(index);
         }
-        if (index < 桁数_5) {
+        if (index < item.get主治医意見書項目1リスト().size()) {
             source.listshujiiikensho_1 = item.get主治医意見書項目1リスト().get(index);
+        }
+        if (index < item.get主治医意見書項目2リスト().size()) {
             source.listshujiiikensho_2 = item.get主治医意見書項目2リスト().get(index);
+        }
+        if (index < item.get主治医意見書項目3リスト().size()) {
             source.listshujiiikensho_3 = item.get主治医意見書項目3リスト().get(index);
+        }
+        if (index < item.get主治医意見書項目4リスト().size()) {
             source.listshujiiikensho_4 = item.get主治医意見書項目4リスト().get(index);
         }
-        if (index < 桁数_20) {
+        if (index < item.get身体機能_起居動作リスト().size()) {
             source.listtokki1_1 = item.get身体機能_起居動作リスト().get(index);
         }
-        if (index < 桁数_12) {
+        if (index < item.get生活機能リスト().size()) {
             source.listtokki2_1 = item.get生活機能リスト().get(index);
         }
-        if (index < 桁数_9) {
+        if (index < item.get認知機能リスト().size()) {
             source.listtokki3_1 = item.get認知機能リスト().get(index);
         }
-        if (index < 桁数_15) {
+        if (index < item.get精神_行動障害リスト().size()) {
             source.listtokki4_1 = item.get精神_行動障害リスト().get(index);
         }
-        if (index < 桁数_6) {
+        if (index < item.get社会生活への適応リスト().size()) {
             source.listtokki5_1 = item.get社会生活への適応リスト().get(index);
+        }
+        if (index < item.get特別な医療1リスト().size()) {
             source.listtokki6_1 = item.get特別な医療1リスト().get(index);
+        }
+        if (index < item.get特別な医療2リスト().size()) {
             source.listtokki7_1 = item.get特別な医療2リスト().get(index);
         }
-        if (index < 桁数_2) {
+        if (index < item.get日常生活自立度リスト().size()) {
             source.listtokki8_1 = item.get日常生活自立度リスト().get(index);
         }
-        if (index < 桁数_6) {
+        if (index < item.get特別な医療3_1リスト().size()) {
             source.listtokubetsunaIryo1_1 = item.get特別な医療3_1リスト().get(index);
+        }
+        if (index < item.get特別な医療3_2リスト().size()) {
             source.listtokubetsunaIryo1_2 = item.get特別な医療3_2リスト().get(index);
         }
-        if (index < 桁数_6) {
+        if (index < item.get特別な医療4_1リスト().size()) {
             source.listtokubetsunaIryo2_1 = item.get特別な医療4_1リスト().get(index);
+        }
+        if (index < item.get特別な医療4_2リスト().size()) {
             source.listtokubetsunaIryo2_2 = item.get特別な医療4_2リスト().get(index);
         }
-        if (index < 桁数_15) {
+        if (index < item.get精神_行動障害1リスト().size()) {
             source.listYongun_1 = item.get精神_行動障害1リスト().get(index);
+        }
+        if (index < item.get精神_行動障害2リスト().size()) {
             source.listYongun_2 = item.get精神_行動障害2リスト().get(index);
+        }
+        if (index < item.get精神_行動障害3リスト().size()) {
             source.listYongun_3 = item.get精神_行動障害3リスト().get(index);
+        }
+        if (index < item.get精神_行動障害4リスト().size()) {
             source.listYongun_4 = item.get精神_行動障害4リスト().get(index);
         }
         source.title = item.getタイトル();
@@ -136,13 +180,13 @@ public class IchijihanteikekkahyoEditor implements IIchijihanteikekkahyoEditor {
         source.shisetsuriyo = item.get現在の状況();
         source.zenZenkaiNijihanteikekka = item.get前々回要介護度();
         source.zenzenkaiYukokikan = item.get前々回認定有効期間();
-        source.zZenkaiNijiKaishiYMD = item.get前々回認定有効期間開始年月日();
-        source.zZenkaiNijishuryoYMD = item.get前々回認定有効期間終了年月日();
+        source.zZenkaiNijiKaishiYMD = dateformat(item.get前々回認定有効期間開始年月日());
+        source.zZenkaiNijishuryoYMD = dateformat(item.get前々回認定有効期間終了年月日());
         source.zenkaiNijihanteikekka = item.get前回要介護度();
         source.zenkaiYukokikan = item.get前回認定有効期間();
-        source.zenkaiNijiKaishiYMD = item.get前回認定有効期間開始年月日();
-        source.zenkaiNijishuryoYMD = item.get前回認定有効期間終了年月日();
-        source.zenkaiNijihanteiDATE = item.get前回認定日();
+        source.zenkaiNijiKaishiYMD = dateformat(item.get前回認定有効期間開始年月日());
+        source.zenkaiNijishuryoYMD = dateformat(item.get前回認定有効期間終了年月日());
+        source.zenkaiNijihanteiDATE = dateformat(item.get前回認定日());
         source.zenkaiJotaizo = item.get前回状態像();
         source.kanriNo = item.get管理番号();
         source.hihokenshaName = item.get氏名();
@@ -162,8 +206,8 @@ public class IchijihanteikekkahyoEditor implements IIchijihanteikekkahyoEditor {
         source.ichijiHanteiKekka = item.get一次判定結果();
         source.nijiHanteiKekka = item.get二次判定結果();
         source.ｙukokikan = item.get認定有効期間();
-        source.nijihanteiKaishiYMD = item.get認定有効期間開始年月日();
-        source.nijihanteishuryoYMD = item.get認定有効期間終了年月日();
+        source.nijihanteiKaishiYMD = dateformat(item.get認定有効期間開始年月日());
+        source.nijihanteishuryoYMD = dateformat(item.get認定有効期間終了年月日());
         source.tokuteishippeiName = item.get特定疾病名();
         source.jotaizo = item.get状態像名称();
         source.kijunGokeiTime = item.get要介護認定等基準時間();
@@ -176,7 +220,17 @@ public class IchijihanteikekkahyoEditor implements IIchijihanteikekkahyoEditor {
         source.listkijunTime_7 = item.get要介護認定等基準時間_機能訓練();
         source.listkijunTime_8 = item.get要介護認定等基準時間_医療関連();
         source.listkijunTime_9 = item.get要介護認定等基準時間_認知症加算();
-        source.keikokuNo = item.get警告コード();
+        RString 警告コード = item.get警告コード();
+        StringBuilder stringBuilder = new StringBuilder();
+        int len = 0;
+        while (警告コード.indexOf("1", len) > -1) {
+            int index1 = 警告コード.indexOf("1", len);
+            stringBuilder.append(index1 + 1 < 10 ? "0" + String.valueOf(index1 + 1) : String.valueOf(index1 + 1));
+            stringBuilder.append("、");
+            len = index1 + 1;
+        }
+        String result = stringBuilder.toString().substring(0, stringBuilder.toString().length() - 1);
+        source.keikokuNo = new RString(result);
         source.listChukanhyoka_1 = item.get中間評価項目得点第1群();
         source.listChukanhyoka_2 = item.get中間評価項目得点第2群();
         source.listChukanhyoka_3 = item.get中間評価項目得点第3群();
@@ -184,11 +238,23 @@ public class IchijihanteikekkahyoEditor implements IIchijihanteikekkahyoEditor {
         source.listChukanhyoka_5 = item.get中間評価項目得点第5群();
         source.chosaNinchishoJiritsudo = item.get認定調査結果認知症高齢者自立度();
         source.ikenNinchishoJiritsudo = item.get意見書認知症高齢者自立度();
-        source.manzensei = item.get認知症自立度Ⅱ以上の蓋然性();
+        RStringBuilder 蓋然性 = new RStringBuilder();
+        蓋然性.append(item.get認知症自立度Ⅱ以上の蓋然性());
+        蓋然性.append(new RString("%"));
+        source.manzensei = 蓋然性.toRString();
         source.jotainoanteisei = item.get状態の安定性();
         source.kyufukubun = item.get給付区分();
         source.sabisuName = item.get現在のサービス利用状況名();
         source.sabisuText = item.get現在のサービス状況();
         return source;
+    }
+
+    private RString dateformat(RString date) {
+        if (!RString.isNullOrEmpty(date)) {
+            return new FlexibleDate(date).wareki().eraType(EraType.ALPHABET)
+                    .firstYear(FirstYear.GAN_NEN).separator(Separator.SLASH).fillType(FillType.ZERO).toDateString();
+        } else {
+            return RString.EMPTY;
+        }
     }
 }
