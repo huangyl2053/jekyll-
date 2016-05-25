fefbeb35351044ca1e296be004e27baf63c03265
yaodongsheng <yaodongsheng@liandisys.com.cn>
2016-05-25 16:50:43 +0800

    画面設計_DBZ.NinteiShinseiTodokedesha_介護認定申請届出者 QA1203対応

---
+++ DB/DBZ/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbz/definition/mybatisprm/ninteishinseitodokedesha/NinteiShinseiTodokedeshaMybatisParameter.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbz.definition.mybatisprm.ninteishinseitodokedesha;
+
+import jp.co.ndensan.reams.ua.uax.definition.mybatisprm.shikibetsutaisho.IShikibetsuTaishoPSMSearchKey;
+import jp.co.ndensan.reams.ua.uax.definition.mybatisprm.shikibetsutaisho.UaFt200FindShikibetsuTaishoParam;
+
+/**
+ * 介護認定申請届出者の宛名検索条件。
+ *
+ * @reamsid_L DBA-0360-050 yaodongsheng
+ */
+@SuppressWarnings("PMD.UnusedPrivateField")
+@lombok.Getter
+@lombok.Setter
+public class NinteiShinseiTodokedeshaMybatisParameter extends UaFt200FindShikibetsuTaishoParam {
+
+    /**
+     * コンストラクタです
+     *
+     * @param searchKey IShikibetsuTaishoPSMSearchKey
+     */
+    public NinteiShinseiTodokedeshaMybatisParameter(IShikibetsuTaishoPSMSearchKey searchKey) {
+        super(searchKey);
+    }
+
+}
