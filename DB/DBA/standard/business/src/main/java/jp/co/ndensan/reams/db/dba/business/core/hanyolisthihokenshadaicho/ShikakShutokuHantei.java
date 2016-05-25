87333b3cd8e1d1e7cf73f939dda1b473f39250b7
久保田　英男 <n8187@ndensan.co.jp>
2016-05-25 20:51:34 +0900

    汎用リスト 被保険者台帳 の対応（資格取得の事由・取得日・届出日の出力内容修正）

---
+++ DB/DBA/standard/business/src/main/java/jp/co/ndensan/reams/db/dba/business/core/hanyolisthihokenshadaicho/ShikakShutokuHantei.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dba.business.core.hanyolisthihokenshadaicho;
+
+import jp.co.ndensan.reams.uz.uza.biz.Code;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
+
+/**
+ * {@link HanyoListHihokenshadaichoProcess}の資格取得判定のためのクラスです。
+ */
+@lombok.Getter
+@lombok.Setter
+public class ShikakShutokuHantei {
+
+    private String codeShubetsu;
+    private Code jiyuCode;
+    private FlexibleDate tekiyoYMD;
+    private FlexibleDate todokedeYMD;
+}
