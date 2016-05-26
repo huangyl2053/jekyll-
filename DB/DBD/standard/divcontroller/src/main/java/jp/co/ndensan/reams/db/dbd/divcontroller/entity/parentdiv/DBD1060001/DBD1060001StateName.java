73193b28e70bba118c6de4e77512f5bfb0cc97f0
王　萌 <n8371@ndensan.co.jp>
2016-05-26 11:01:10 +0900

    2016年5月設計書チェック [画面設計_DBDGM12009_1_減免減額帳票一括]を修正する

--- DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD1060001/DBD1060001StateName.java
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD1060001/DBD1060001StateName.java
@@ -15,7 +15,8 @@ public enum DBD1060001StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
     NoChange(""),
-    Default("default");
+    負担限度額認定("負担限度額認定"),
+    社福軽減("社福軽減");
 
     private final RString name;
 
