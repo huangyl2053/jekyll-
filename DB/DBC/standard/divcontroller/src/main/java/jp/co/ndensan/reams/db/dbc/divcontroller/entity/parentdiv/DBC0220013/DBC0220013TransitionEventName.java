bf2e24671352d69f4656418034b4795b017b21b9
北爪敏之 <n8365@ndensan.co.jp>
2016-05-30 10:11:45 +0900

    2016年5月設計書チェック 共通ボタン修正 完了メッセージ追加

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0220013/DBC0220013TransitionEventName.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0220013/DBC0220013TransitionEventName.java
@@ -1,27 +1,24 @@
-package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0220012;
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0220013;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 
 /**
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBC0220012の状態名定義クラスです
+ * DBC0220013のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBC0220012StateName implements IStateEnumerations {
+public enum DBC0220013TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    NoChange(""),
-    連絡票変更("連絡票変更"),
-    連絡票削除("連絡票削除"),
-    完了メッセージ("完了メッセージ");
+    対象者検索へ("対象者検索へ");
 
     private final RString name;
 
-    private DBC0220012StateName(String name) {
+    private DBC0220013TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
