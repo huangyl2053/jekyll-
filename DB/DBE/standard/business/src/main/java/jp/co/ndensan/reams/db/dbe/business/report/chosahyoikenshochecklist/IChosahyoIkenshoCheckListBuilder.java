ec1b9684629b333f81cfa2a04c2259e5c11cd640
dongyabin <dongyabin@liandisys.com.cn>
2016-05-31 14:32:56 +0800

    帳票設計_DBE012003_認定調査結果と主治医意見書のチェックリスト 新規

---
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/chosahyoikenshochecklist/IChosahyoIkenshoCheckListBuilder.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.business.report.chosahyoikenshochecklist;
+
+import jp.co.ndensan.reams.db.dbe.entity.report.source.chosahyoikenshochecklist.ChosahyoIkenshoCheckListReportSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportBuilder;
+
+/**
+ * 認定調査結果と主治医意見書のチェックリストBuilderのインターフェースクラスです。
+ *
+ * @reamsid_L DBE-1390-190 dongyabin
+ */
+interface IChosahyoIkenshoCheckListBuilder extends IReportBuilder<ChosahyoIkenshoCheckListReportSource> {
+}
---
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/chosahyoikenshochecklist/IChosahyoIkenshoCheckListEditor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.business.report.chosahyoikenshochecklist;
+
+import jp.co.ndensan.reams.db.dbe.entity.report.source.chosahyoikenshochecklist.ChosahyoIkenshoCheckListReportSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportEditor;
+
+/**
+ * 認定調査結果と主治医意見書のチェックリストのインターフェースクラスです。
+ *
+ * @reamsid_L DBE-1390-190 dongyabin
+ */
+public interface IChosahyoIkenshoCheckListEditor extends IReportEditor<ChosahyoIkenshoCheckListReportSource> {
+
+}
