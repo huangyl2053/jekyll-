56c3eb1e9a842a82294fe7bd536a372a5e6a778a
wangkanglei <wangkanglei@liandisys.com.cn>
2016-05-31 13:47:35 +0800

    画面設計_DBCMNF2002_国保連情報受取データ取込_共有子DIV(checkStyleの表示モード 修正)

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/KaigoKyufuKokuhorenJohoTorikomi/KaigoKyufuKokuhorenJohoTorikomiDiv.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/KaigoKyufuKokuhorenJohoTorikomi/KaigoKyufuKokuhorenJohoTorikomiDiv.java
@@ -30,7 +30,7 @@ import jp.co.ndensan.reams.uz.uza.ui.servlets._CommonChildDivModeUtil;
  */
 public class KaigoKyufuKokuhorenJohoTorikomiDiv extends Panel implements IKaigoKyufuKokuhorenJohoTorikomiDiv {
 
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-01-15_09-59-03">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-03-22_14-06-37">
     /*
      * [ private の作成 ]
      * クライアント側から取得した情報を元にを検索を行い
@@ -170,7 +170,7 @@ public class KaigoKyufuKokuhorenJohoTorikomiDiv extends Panel implements IKaigoK
     @JsonProperty("modes")
     private HashSet<Mode> modes;
 
-    public static enum 表示モード implements ICommonChildDivMode {
+    public static enum HyoujiModo implements ICommonChildDivMode {
 
         Mode1("Mode1"),
         ModeShutsuryokujunJoken2("ModeShutsuryokujunJoken2"),
@@ -178,14 +178,14 @@ public class KaigoKyufuKokuhorenJohoTorikomiDiv extends Panel implements IKaigoK
 
         private final String name;
 
-        private 表示モード(final String name) {
+        private HyoujiModo(final String name) {
             this.name = name;
         }
 
-        public static 表示モード getEnum(String str) {
-            表示モード[] enumArray = 表示モード.values();
+        public static HyoujiModo getEnum(String str) {
+            HyoujiModo[] enumArray = HyoujiModo.values();
 
-            for (表示モード enumStr : enumArray) {
+            for (HyoujiModo enumStr : enumArray) {
                 if (str.equals(enumStr.name.toString())) {
                     return enumStr;
                 }
@@ -200,14 +200,12 @@ public class KaigoKyufuKokuhorenJohoTorikomiDiv extends Panel implements IKaigoK
 
     }
 
-    @JsonIgnore
-    public 表示モード getMode_表示モード() {
-        return (表示モード) _CommonChildDivModeUtil.getMode(this.modes, 表示モード.class);
+    public HyoujiModo getMode_HyoujiModo() {
+        return (HyoujiModo) _CommonChildDivModeUtil.getMode(this.modes, HyoujiModo.class);
     }
 
-    @JsonIgnore
-    public void setMode_表示モード(表示モード value) {
-        _CommonChildDivModeUtil.setMode(this.modes, 表示モード.class, value);
+    public void setMode_HyoujiModo(HyoujiModo value) {
+        _CommonChildDivModeUtil.setMode(this.modes, HyoujiModo.class, value);
     }
 
     /*
