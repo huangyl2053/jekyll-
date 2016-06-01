48881a1b66fa5887a030f4e430c1858a68296544
xupeng <xupeng@liandisys.com.cn>
2016-06-01 11:03:06 +0800

    DBC-0991-030 画面設計_DBCMN52003_住宅改修費支給申請一括審査・決定 ErrorMessagesソースの修正

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC0720011/ShinsaButtonDivValidator.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC0720011/ShinsaButtonDivValidator.java
@@ -5,13 +5,18 @@
  */
 package jp.co.ndensan.reams.db.dbc.divcontroller.controller.parentdiv.DBC0720011;
 
-import jp.co.ndensan.reams.db.dbc.definition.message.jutakukaishuhishikyushinsei.JutakuKaishuhiShikyuShinseiErrorMessages;
+import jp.co.ndensan.reams.db.dbc.definition.message.DbcErrorMessages;
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0720011.JutakuKaishuhiShikyuShinseiPanelDiv;
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0720011.JutakuKaishuhiShikyuShinseiPanelSpec;
+import jp.co.ndensan.reams.ur.urz.definition.message.UrErrorMessages;
 import jp.co.ndensan.reams.uz.uza.core.validation.IValidatable;
 import jp.co.ndensan.reams.uz.uza.core.validation.ValidateChain;
 import jp.co.ndensan.reams.uz.uza.core.validation.ValidationMessagesFactory;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.message.IMessageGettable;
+import jp.co.ndensan.reams.uz.uza.message.IValidationMessage;
 import jp.co.ndensan.reams.uz.uza.message.IValidationMessages;
+import jp.co.ndensan.reams.uz.uza.message.Message;
 
 /**
  * ShinsaButtonDivValidator
@@ -21,6 +26,7 @@ import jp.co.ndensan.reams.uz.uza.message.IValidationMessages;
 public class ShinsaButtonDivValidator implements IValidatable {
 
     private final JutakuKaishuhiShikyuShinseiPanelDiv div;
+    private static final RString メッセージ_決定日 = new RString("決定日");
 
     /**
      * コンストラクタです
@@ -43,5 +49,24 @@ public class ShinsaButtonDivValidator implements IValidatable {
                 .thenAdd(JutakuKaishuhiShikyuShinseiErrorMessages.決定日のチェック)
                 .messages());
         return messages;
+    }
+
+    private static enum JutakuKaishuhiShikyuShinseiErrorMessages implements IValidationMessage {
+
+        データ選択のチェック(UrErrorMessages.対象行を選択),
+        未審査のチェック(DbcErrorMessages.未審査有りで保存不可),
+        決定日のチェック(UrErrorMessages.必須項目_追加メッセージあり, メッセージ_決定日.toString());
+
+        private final Message message;
+
+        private JutakuKaishuhiShikyuShinseiErrorMessages(IMessageGettable message, String... replacements) {
+            this.message = message.getMessage().replace(replacements);
+        }
+
+        @Override
+        public Message getMessage() {
+            return message;
+        }
+
     }
 }
