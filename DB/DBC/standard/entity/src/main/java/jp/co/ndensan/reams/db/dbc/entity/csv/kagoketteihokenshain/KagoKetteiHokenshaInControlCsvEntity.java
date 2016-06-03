56b0cdadc4d72968434ff942fdf71a35d42a0806
surun <surun@liandisys.com.cn>
2016-06-03 11:22:27 +0800

    バッチ設計_DBCMNF2002-17x_過誤決定通知書情報取込共通処理（保険者分・ファイル読込）ソース修正

--- DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/csv/kagoketteihokenshain/KagoKetteiHokenshaInControlCsvEntity.java
+++ DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/csv/kagoketteihokenshain/KagoKetteiHokenshaInControlCsvEntity.java
@@ -20,31 +20,30 @@ import lombok.Setter;
 @SuppressWarnings("PMD.UnusedPrivateField")
 public class KagoKetteiHokenshaInControlCsvEntity {
 
-    //TODO keyN 目前名称没找
     @CsvField(order = 10, name = "レコード種別")
     private RString codeShubetsu;
     @CsvField(order = 20, name = "レコード番号")
     private RString codeNo;
-    @CsvField(order = 30, name = "keyOne")
-    private RString keyOne;
+    @CsvField(order = 30, name = "ボリューム通番")
+    private RString volumeRenban;
     @CsvField(order = 40, name = "レコード件数")
     private RString codeNum;
-    @CsvField(order = 50, name = "keyTwo")
-    private RString keyTwo;
-    @CsvField(order = 60, name = "keyThree")
-    private RString keyThree;
+    @CsvField(order = 50, name = "データ種別")
+    private RString dataShubetsu;
+    @CsvField(order = 60, name = "福祉事務所特定番号")
+    private RString fukushijimushoTokuteiNo;
     @CsvField(order = 70, name = "保険者番号")
     private RString hokenshaNo;
     @CsvField(order = 80, name = "事業所番号")
     private RString jigyoshoNo;
-    @CsvField(order = 90, name = "keyFour")
-    private RString keyFour;
-    @CsvField(order = 100, name = "keyFive")
-    private RString keyFive;
+    @CsvField(order = 90, name = "都道府県番号")
+    private RString todofukenNo;
+    @CsvField(order = 100, name = "媒体区分")
+    private RString baitaiKubun;
     @CsvField(order = 110, name = "処理対象年月")
     private RString shoriYM;
-    @CsvField(order = 120, name = "keySix")
-    private RString keySix;
+    @CsvField(order = 120, name = "ファイル管理番号")
+    private RString fileKanriNo;
     @CsvField(order = 130, name = "ブランク")
     private RString flag;
 
