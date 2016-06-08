fa0dd1ae7d800e257f3c30c8287acd298dcf0eff
zuotao <zuotao@liandisys.com.cn>
2016-06-08 11:15:14 +0800

    ViewStateHolder対応

--- DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/handler/parentdiv/DBU0130011/DBU0130011MainHandler.java
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/handler/parentdiv/DBU0130011/DBU0130011MainHandler.java
@@ -10,11 +10,9 @@ import jp.co.ndensan.reams.db.dbu.service.core.roujinhokenjukyushadaichokanri.Ro
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
 import jp.co.ndensan.reams.db.dbz.business.core.basic.RojinHokenJukyushaJoho;
 import jp.co.ndensan.reams.db.dbz.business.core.basic.RojinHokenJukyushaJohoBuilder;
-import jp.co.ndensan.reams.db.dbz.definition.core.ViewStateKeys;
 import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
 import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.ui.servlets.ViewStateHolder;
 import jp.co.ndensan.reams.uz.uza.util.db.EntityDataState;
 
 /**
@@ -39,37 +37,16 @@ public class DBU0130011MainHandler {
     }
 
     /**
-     * 画面初期化処理します。
-     *
-     * @param 老健受給情報 老健受給情報
-     */
-    public void initialize(RojinHokenJukyushaJoho 老健受給情報) {
-        if (老健受給情報 == null) {
-            ViewStateHolder.put(ViewStateKeys.老人保健受給者台帳管理_モード, モード_データなし);
-            div.getMeisaiPanel().getTxtRokenJukyushaNo().setValue(RString.EMPTY);
-            div.getMeisaiPanel().getTxtRoukenShichosonNo().setValue(RString.EMPTY);
-        } else {
-            ViewStateHolder.put(ViewStateKeys.老人保健受給者台帳管理_モード, モード_データあり);
-            ViewStateHolder.put(ViewStateKeys.老健受給情報, 老健受給情報);
-            div.getMeisaiPanel().getTxtRokenJukyushaNo().setValue(老健受給情報.get老人保健受給者番号());
-            if (老健受給情報.get老人保健市町村コード() == null) {
-                div.getMeisaiPanel().getTxtRoukenShichosonNo().setValue(RString.EMPTY);
-            } else {
-                div.getMeisaiPanel().getTxtRoukenShichosonNo().setValue(老健受給情報.get老人保健市町村コード().value());
-            }
-        }
-    }
-
-    /**
      * 老健受給情報を設定します。
      *
      * @param 識別コード 識別コード
      * @param 被保険者番号 被保険者番号
+     * @param モード モード
+     * @param 老健受給情報 老健受給情報
      */
-    public void update老健受給情報(ShikibetsuCode 識別コード, HihokenshaNo 被保険者番号) {
-        RojinHokenJukyushaJoho hokenJukyushaJoho;
-        if (モード_データなし.equals(ViewStateHolder.get(ViewStateKeys.老人保健受給者台帳管理_モード, RString.class))) {
-            hokenJukyushaJoho = new RojinHokenJukyushaJoho(識別コード);
+    public void update老健受給情報(ShikibetsuCode 識別コード, HihokenshaNo 被保険者番号, RString モード, RojinHokenJukyushaJoho 老健受給情報) {
+        if (モード_データなし.equals(モード)) {
+            RojinHokenJukyushaJoho hokenJukyushaJoho = new RojinHokenJukyushaJoho(識別コード);
             RojinHokenJukyushaJohoBuilder builder = hokenJukyushaJoho.createBuilderForEdit();
             builder.set市町村コード(new LasdecCode(RoujinHokenJukyushaDaichoKanriManager.createInstance().get宛名情報(識別コード)));
             if (div.getTxtRoukenShichosonNo().getValue() == null) {
@@ -81,8 +58,7 @@ public class DBU0130011MainHandler {
             builder.set老人保健受給者番号(div.getTxtRokenJukyushaNo().getValue());
             RoujinHokenJukyushaDaichoKanriManager.createInstance().updateRoukenJukyuJoho(builder.build(), EntityDataState.Added);
         } else {
-            hokenJukyushaJoho = ViewStateHolder.get(ViewStateKeys.老健受給情報, RojinHokenJukyushaJoho.class);
-            RojinHokenJukyushaJohoBuilder builder = hokenJukyushaJoho.createBuilderForEdit();
+            RojinHokenJukyushaJohoBuilder builder = 老健受給情報.createBuilderForEdit();
             if (div.getTxtRoukenShichosonNo().getValue() == null) {
                 builder.set老人保健市町村コード(RString.EMPTY);
             } else {
@@ -91,6 +67,5 @@ public class DBU0130011MainHandler {
             builder.set老人保健受給者番号(div.getTxtRokenJukyushaNo().getValue());
             RoujinHokenJukyushaDaichoKanriManager.createInstance().updateRoukenJukyuJoho(builder.build(), EntityDataState.Modified);
         }
-        initialize(RoujinHokenJukyushaDaichoKanriManager.createInstance().getRoukenJukyuJoho(識別コード));
     }
 }
