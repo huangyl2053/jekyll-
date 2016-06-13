571a21fcf1eead2dfa97abbaf3b58d4456700879
梁百川 <pangsw@liandisys.com.cn>
2016-06-13 18:00:38 +0800

    画面設計_DBDGM11002_7_【共有子Div】所得状況  新規作成

--- DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/commonchilddiv/ShotokuJokyo/ShotokuJokyo/IShotokuJokyoDiv.java
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/commonchilddiv/ShotokuJokyo/ShotokuJokyo/IShotokuJokyoDiv.java
@@ -1,10 +1,15 @@
 package jp.co.ndensan.reams.db.dbd.divcontroller.entity.commonchilddiv.ShotokuJokyo.ShotokuJokyo;
 
+import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
 import jp.co.ndensan.reams.uz.uza.ui.binding.ICommonChildDivBaseProperties;
+import jp.co.ndensan.reams.uz.uza.ui.binding.IDialogDiv;
 
-/*
+/**
  * このコードはツールによって生成されました。
+ *
+ * @reamsid_L DBD-3650-110 liangbc
  */
-public interface IShotokuJokyoDiv extends ICommonChildDivBaseProperties {
+public interface IShotokuJokyoDiv extends ICommonChildDivBaseProperties, IDialogDiv {
 
+    void load(ShikibetsuCode 識別コード);
 }
