8a3071569b4745b69361bff88bb2f3a2ca78635a
wangjie2 <wangjie2@liandisys.com.cn>
2016-06-15 15:57:33 +0800

    【共有子Div】 特別地域加算減免 新規作成

--- DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/commonchilddiv/TokubetsuChiikiKasanGemmen/TokubetsuChiikiKasanGemmen/TokubetsuChiikiKasanGemmenDiv.java
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/commonchilddiv/TokubetsuChiikiKasanGemmen/TokubetsuChiikiKasanGemmen/TokubetsuChiikiKasanGemmenDiv.java
@@ -5,16 +5,22 @@ package jp.co.ndensan.reams.db.dbd.divcontroller.entity.commonchilddiv.Tokubetsu
  * 不正な動作の原因になります。
  */
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.ui.binding.*;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+import java.util.List;
+import jp.co.ndensan.reams.db.dbd.business.core.gemmengengaku.tokubetsuchikikasangemmen.TokubetsuchiikiKasanGemmen;
+import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Button;
+import jp.co.ndensan.reams.uz.uza.ui.binding.DataGrid;
 
 /**
- * TokubetsuChiikiKasanGemmen のクラスファイル 
- * 
- * @author 自動生成
+ * TokubetsuChiikiKasanGemmen のクラスファイル
+ *
+ * @reamsid_L DBD-3560-070 wangjie2
  */
 public class TokubetsuChiikiKasanGemmenDiv extends Panel implements ITokubetsuChiikiKasanGemmenDiv {
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-03-22_14-06-37">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-05-30_13-18-33">
     /*
      * [ private の作成 ]
      * クライアント側から取得した情報を元にを検索を行い
@@ -27,6 +33,8 @@ public class TokubetsuChiikiKasanGemmenDiv extends Panel implements ITokubetsuCh
     private TokubetsuChiikiKasanGemmenDetailDiv TokubetsuChiikiKasanGemmenDetail;
     @JsonProperty("btnClose")
     private Button btnClose;
+    @JsonProperty("hihokenshaNo")
+    private RString hihokenshaNo;
 
     /*
      * [ GetterとSetterの作成 ]
@@ -88,7 +96,33 @@ public class TokubetsuChiikiKasanGemmenDiv extends Panel implements ITokubetsuCh
         this.btnClose = btnClose;
     }
 
+    /*
+     * gethihokenshaNo
+     * @return hihokenshaNo
+     */
+    @JsonProperty("hihokenshaNo")
+    public RString getHihokenshaNo() {
+        return hihokenshaNo;
+    }
+
+    /*
+     * sethihokenshaNo
+     * @param hihokenshaNo hihokenshaNo
+     */
+    @JsonProperty("hihokenshaNo")
+    public void setHihokenshaNo(RString hihokenshaNo) {
+        this.hihokenshaNo = hihokenshaNo;
+    }
+
     // </editor-fold>
     //--------------- この行より下にコードを追加してください -------------------
+    @Override
+    public List<TokubetsuchiikiKasanGemmen> initializa(HihokenshaNo 被保険者番号) {
+        return getHandler(this).onLoad(被保険者番号);
+    }
+
+    private TokubetsuChiikiKasanGemmenHandler getHandler(TokubetsuChiikiKasanGemmenDiv div) {
+        return new TokubetsuChiikiKasanGemmenHandler(div);
+    }
 
 }
