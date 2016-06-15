b4209c3e7119f6b06fbbad0946c596ead0e7fd22
yaodongsheng <yaodongsheng@liandisys.com.cn>
2016-06-15 15:44:53 +0800

    画面設計_DBZ0400002_帳票制御メンテナンス 新規作成

--- DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/core/basic/ChohyoSeigyoHanyo.java
+++ DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/core/basic/ChohyoSeigyoHanyo.java
@@ -232,7 +232,7 @@ public class ChohyoSeigyoHanyo extends
 
     @Override
     public boolean hasChanged() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return hasChangedEntity();
     }
 
     private static final class _SerializationProxy implements Serializable {
