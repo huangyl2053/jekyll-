299a1cf3b794ac03e1c5414c51fa6fb69abac6ee
lizhuoxuan <lizhuoxuan@liandisys.com.cn>
2016-06-15 18:10:09 +0800

    帳票設計_DBE517XXX_特記事項 新規を作成

---
+++ DB/DBE/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbe/entity/db/ninteichosatokkiimage/NinteiChosaTokkiImageEntity.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.entity.db.ninteichosatokkiimage;
+
+import java.util.List;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+/**
+ * 特記事項Entity。
+ *
+ * @reamsid_L DBE-0230-080 lizhuoxuan
+ */
+@lombok.Getter
+@lombok.Setter
+@SuppressWarnings("PMD.UnusedPrivateField")
+public class NinteiChosaTokkiImageEntity {
+
+    private RString 保険者番号;
+    private RString 被保険者番号;
+    private RString 被保険者氏名;
+    private RString 申請日_元号;
+    private RString 申請日_年;
+    private RString 申請日_月;
+    private RString 申請日_日;
+    private RString 作成日_元号;
+    private RString 作成日_年;
+    private RString 作成日_月;
+    private RString 作成日_日;
+    private RString 調査日_元号;
+    private RString 調査日_年;
+    private RString 調査日_月;
+    private RString 調査日_日;
+    private RString 審査日_元号;
+    private RString 審査日_年;
+    private RString 審査日_月;
+    private RString 審査日_日;
+    private RString 現ページ;
+    private RString 総ページ数;
+    private List<RString> 特記事項リスト1;
+    private List<RString> 特記事項リスト2;
+    private List<RString> 特記事項リスト3;
+    private List<RString> 特記事項リスト4;
+    private List<RString> 特記事項リスト5;
+    private List<RString> 特記事項リスト6;
+    private RString 特記事項イメージ1;
+    private RString 特記事項イメージ2;
+    private RString 特記事項イメージ3;
+    private RString 特記事項イメージ4;
+    private RString 特記事項イメージ5;
+    private RString 特記事項イメージ6;
+    private RString 特記事項イメージ7;
+    private RString 特記事項イメージ8;
+    private RString 特記事項イメージ9;
+    private RString 特記事項イメージ10;
+    private RString 特記事項イメージ11;
+    private RString 特記事項イメージ12;
+    private RString 特記事項イメージ13;
+    private RString 特記事項イメージ14;
+    private RString 特記事項イメージ15;
+    private RString 特記事項イメージ16;
+    private RString 特記事項イメージ17;
+    private RString 特記事項イメージ18;
+    private RString 特記事項イメージ19;
+    private RString 特記事項イメージ20;
+    private RString 特記事項イメージ21;
+    private RString 特記事項イメージ22;
+    private RString 特記事項イメージ23;
+    private RString 特記事項イメージ24;
+}
