c7f610e235ad72fbdb2dfa17cb6734d7150ae874
yebangqiang <yebangqiang@liandisys.com.cn>
2016-06-16 11:09:51 +0800

    画面設計_DBCMN52002_住宅改修費支給申請_一覧（横展開対応：No.11、ViewStateHolder）

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/parentdiv/DBC0710011/JutakuKaishuShinseiHandler.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/parentdiv/DBC0710011/JutakuKaishuShinseiHandler.java
@@ -14,13 +14,10 @@ import jp.co.ndensan.reams.db.dbc.definition.core.shikyufushikyukubun.ShikyuFush
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0710011.DBC0710011StateName;
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0710011.JutakuKaishuShinseiDiv;
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0710011.dgJutakuKaishuShinseiList_Row;
-import jp.co.ndensan.reams.db.dbc.divcontroller.viewbox.ViewStateKeys;
 import jp.co.ndensan.reams.db.dbc.service.core.jutakukaishusikyushinsei.JutakukaishuSikyuShinseiManager;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
-import jp.co.ndensan.reams.uz.uza.lang.FlexibleYearMonth;
 import jp.co.ndensan.reams.uz.uza.lang.RDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.ui.servlets.ViewStateHolder;
 
 /**
  * 住宅改修費支給申請ハンドラークラスです
@@ -33,14 +30,6 @@ public final class JutakuKaishuShinseiHandler {
     private final RString 審査済み = new RString("審査済み");
     private final RString 未審査 = new RString("未審査");
     private final RString 事前申請有る = new RString("○");
-    private final RString 事前申請登録モード = new RString("事前申請登録モード");
-    private final RString 修正モード = new RString("修正モード");
-    private final RString 取消モード = new RString("取消モード");
-    private final RString 削除モード = new RString("削除モード");
-    private final RString 登録モード = new RString("登録モード");
-    private final RString 処理モード_修正 = new RString("修正");
-    private final RString 処理モード_取消 = new RString("取消");
-    private final RString 処理モード_削除 = new RString("削除");
 
     private JutakuKaishuShinseiHandler(JutakuKaishuShinseiDiv div) {
         this.div = div;
@@ -72,74 +61,6 @@ public final class JutakuKaishuShinseiHandler {
             set事前申請一覧(manager, 被保険者番号, rows);
         }
         div.getJutakuKaishuShinseiList().getDgJutakuKaishuShinseiList().setDataSource(rows);
-    }
-
-    /**
-     * 「申請を追加ボタン」押した時、表示モードの設定
-     */
-    public void setAddMode() {
-        ViewStateHolder.put(ViewStateKeys.表示モード, 登録モード);
-    }
-
-    /**
-     * 「｢修正｣ボタン」押した時、表示モードの設定
-     *
-     * @param 画面モード 画面モード
-     */
-    public void setModifyMode(RString 画面モード) {
-        RString 申請区分 = div.getJutakuKaishuShinseiList().getDgJutakuKaishuShinseiList().getClickedItem()
-                .getTxtShinseiKubun();
-        if (画面モード.equals(DBC0710011StateName.支給申請モード.getName())) {
-            if (JutakukaishuShinseiKubun.事前申請.get名称().equals(申請区分)) {
-                ViewStateHolder.put(ViewStateKeys.表示モード, 事前申請登録モード);
-            } else if (JutakukaishuShinseiKubun.支給申請.get名称().equals(申請区分)) {
-                ViewStateHolder.put(ViewStateKeys.表示モード, 修正モード);
-            } else if (JutakukaishuShinseiKubun.取消.get名称().equals(申請区分)) {
-                ViewStateHolder.put(ViewStateKeys.表示モード, 取消モード);
-            }
-        } else if (画面モード.equals(DBC0710011StateName.事前申請モード.getName())) {
-            if (JutakukaishuShinseiKubun.事前申請.get名称().equals(申請区分)) {
-                ViewStateHolder.put(ViewStateKeys.表示モード, 修正モード);
-                ViewStateHolder.put(ViewStateKeys.処理モード, 処理モード_修正);
-            } else if (JutakukaishuShinseiKubun.取消.get名称().equals(申請区分)) {
-                ViewStateHolder.put(ViewStateKeys.表示モード, 取消モード);
-                ViewStateHolder.put(ViewStateKeys.処理モード, 処理モード_取消);
-            }
-        }
-        ViewStateHolder.put(ViewStateKeys.サービス提供年月, new FlexibleYearMonth(div.getJutakuKaishuShinseiList()
-                .getDgJutakuKaishuShinseiList().getClickedItem().getTxtTeikyoYM().getValue().getYearMonth()
-                .toDateString()));
-        ViewStateHolder.put(
-                ViewStateKeys.整理番号, div.getJutakuKaishuShinseiList().getDgJutakuKaishuShinseiList()
-                .getClickedItem().getTxtSeiriNo().getValue());
-    }
-
-    /**
-     * 「｢削除｣ボタン」押した時、表示モードの設定
-     */
-    public void setDeleteMode() {
-        ViewStateHolder.put(ViewStateKeys.サービス提供年月, new FlexibleYearMonth(div.getJutakuKaishuShinseiList()
-                .getDgJutakuKaishuShinseiList().getClickedItem().getTxtTeikyoYM().getValue().getYearMonth()
-                .toDateString()));
-        ViewStateHolder.put(
-                ViewStateKeys.整理番号, div.getJutakuKaishuShinseiList().getDgJutakuKaishuShinseiList()
-                .getClickedItem().getTxtSeiriNo().getValue());
-        ViewStateHolder.put(ViewStateKeys.表示モード, 削除モード);
-        ViewStateHolder.put(ViewStateKeys.処理モード, 処理モード_削除);
-    }
-
-    /**
-     * 「｢取消｣ボタン」押した時、表示モードの設定
-     */
-    public void setCancelMode() {
-        ViewStateHolder.put(ViewStateKeys.サービス提供年月, new FlexibleYearMonth(div.getJutakuKaishuShinseiList()
-                .getDgJutakuKaishuShinseiList().getClickedItem().getTxtTeikyoYM().getValue().getYearMonth()
-                .toDateString()));
-        ViewStateHolder.put(
-                ViewStateKeys.整理番号, div.getJutakuKaishuShinseiList().getDgJutakuKaishuShinseiList()
-                .getClickedItem().getTxtSeiriNo().getValue());
-        ViewStateHolder.put(ViewStateKeys.表示モード, 取消モード);
-        ViewStateHolder.put(ViewStateKeys.処理モード, 処理モード_取消);
     }
 
     private void set事前申請一覧(JutakukaishuSikyuShinseiManager manager, HihokenshaNo 被保険者番号,
