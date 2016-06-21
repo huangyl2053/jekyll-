1f4c522788770e77d45f77b6beb898ebe50d3e82
橋本　治義 <n8322@ndensan.co.jp>
2016-06-21 20:26:30 +0900

    画面設計時　高額合算関連UP

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC8120012/DBC8120012StateName.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC8120012/DBC8120012StateName.java
@@ -1,27 +1,26 @@
-package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC1230011;
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC8120012;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
 
 /**
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBC1230011のイベント名定義クラスです</br>
+ * DBC8120012の状態名定義クラスです
  *
  * @author 自動生成
  */
-public enum DBC1230011TransitionEventName implements IContainerEvents {
+public enum DBC8120012StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    初期表示("初期表示"),
-    完了("完了"),
-    対象者検索に戻る("対象者検索に戻る"),
-    検索結果一覧に戻る("検索結果一覧に戻る");
+    NoChange(""),
+    Default("default"),
+    計算結果連絡票作成("計算結果連絡票作成");
 
     private final RString name;
 
-    private DBC1230011TransitionEventName(String name) {
+    private DBC8120012StateName(String name) {
         this.name = new RString(name);
     }
 
--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC8120012/DBC8120012TransitionEventName.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC8120012/DBC8120012TransitionEventName.java
@@ -1,4 +1,4 @@
-package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC1230011;
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC8120012;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
@@ -7,21 +7,21 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBC1230011のイベント名定義クラスです</br>
+ * DBC8120012のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBC1230011TransitionEventName implements IContainerEvents {
+public enum DBC8120012TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    初期表示("初期表示"),
+    事業分支給額計算結果連絡票作成("事業分支給額計算結果連絡票作成"),
     完了("完了"),
     対象者検索に戻る("対象者検索に戻る"),
     検索結果一覧に戻る("検索結果一覧に戻る");
 
     private final RString name;
 
-    private DBC1230011TransitionEventName(String name) {
+    private DBC8120012TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
