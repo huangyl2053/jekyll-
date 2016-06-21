9d91af43114372e199812ad1a01aa994eb00b7a8
wanghui <wanghui@liandisys.com.cn>
2016-06-21 17:01:44 +0800

    DBU-3990-030 画面設計_DBZccd_KaigoAtesakiJushoSettei_介護宛先住所設定の修正

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/kaigoatesakijushosettei/KaigoAtesakiJushoSettei/KaigoAtesakiJushoSetteiDiv.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/kaigoatesakijushosettei/KaigoAtesakiJushoSettei/KaigoAtesakiJushoSetteiDiv.java
@@ -4,18 +4,19 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.kaigoates
  * このファイルへの変更は、再生成時には損失するため
  * 不正な動作の原因になります。
  */
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.HashSet;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.ui.binding.ButtonDialog;
-import jp.co.ndensan.reams.uz.uza.ui.binding.Mode;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
-import jp.co.ndensan.reams.uz.uza.ui.binding.RadioButton;
-import jp.co.ndensan.reams.uz.uza.ui.binding.TextBox;
+
+import java.util.HashSet;
 import jp.co.ndensan.reams.uz.uza.ui.servlets.ICommonChildDivMode;
 import jp.co.ndensan.reams.uz.uza.ui.servlets._CommonChildDivModeUtil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jp.co.ndensan.reams.uz.uza.ui.binding.ButtonDialog;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Mode;
+import jp.co.ndensan.reams.uz.uza.ui.binding.RadioButton;
+import jp.co.ndensan.reams.uz.uza.ui.binding.TextBox;
 
 /**
  * KaigoAtesakiJushoSettei のクラスファイル
@@ -23,7 +24,6 @@ import jp.co.ndensan.reams.uz.uza.ui.servlets._CommonChildDivModeUtil;
  * @author 自動生成
  */
 public class KaigoAtesakiJushoSetteiDiv extends Panel implements IKaigoAtesakiJushoSetteiDiv {
-
     // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-05-30_13-18-33">
     /*
      * [ private の作成 ]
@@ -37,16 +37,6 @@ public class KaigoAtesakiJushoSetteiDiv extends Panel implements IKaigoAtesakiJu
     private ButtonDialog btnAtesakiJushoSettei;
     @JsonProperty("txtAtesakiJushoSettei")
     private TextBox txtAtesakiJushoSettei;
-    @JsonProperty("hdnShichosonKyotsuTodofukenMei")
-    private RString hdnShichosonKyotsuTodofukenMei;
-    @JsonProperty("hdnShichosonKyotsuGunMei")
-    private RString hdnShichosonKyotsuGunMei;
-    @JsonProperty("hdnShichosonKyotsuShichosonMei")
-    private RString hdnShichosonKyotsuShichosonMei;
-    @JsonProperty("hdnShichosonKyotsuHensyuHoho")
-    private RString hdnShichosonKyotsuHensyuHoho;
-    @JsonProperty("hdnShichosonKyotsuKatagaki")
-    private RString hdnShichosonKyotsuKatagaki;
     @JsonProperty("hdnChohyoDokujiTodofukenMei")
     private RString hdnChohyoDokujiTodofukenMei;
     @JsonProperty("hdnChohyoDokujiGunMei")
@@ -116,96 +106,6 @@ public class KaigoAtesakiJushoSetteiDiv extends Panel implements IKaigoAtesakiJu
     @JsonProperty("txtAtesakiJushoSettei")
     public void setTxtAtesakiJushoSettei(TextBox txtAtesakiJushoSettei) {
         this.txtAtesakiJushoSettei = txtAtesakiJushoSettei;
-    }
-
-    /*
-     * gethdnShichosonKyotsuTodofukenMei
-     * @return hdnShichosonKyotsuTodofukenMei
-     */
-    @JsonProperty("hdnShichosonKyotsuTodofukenMei")
-    public RString getHdnShichosonKyotsuTodofukenMei() {
-        return hdnShichosonKyotsuTodofukenMei;
-    }
-
-    /*
-     * sethdnShichosonKyotsuTodofukenMei
-     * @param hdnShichosonKyotsuTodofukenMei hdnShichosonKyotsuTodofukenMei
-     */
-    @JsonProperty("hdnShichosonKyotsuTodofukenMei")
-    public void setHdnShichosonKyotsuTodofukenMei(RString hdnShichosonKyotsuTodofukenMei) {
-        this.hdnShichosonKyotsuTodofukenMei = hdnShichosonKyotsuTodofukenMei;
-    }
-
-    /*
-     * gethdnShichosonKyotsuGunMei
-     * @return hdnShichosonKyotsuGunMei
-     */
-    @JsonProperty("hdnShichosonKyotsuGunMei")
-    public RString getHdnShichosonKyotsuGunMei() {
-        return hdnShichosonKyotsuGunMei;
-    }
-
-    /*
-     * sethdnShichosonKyotsuGunMei
-     * @param hdnShichosonKyotsuGunMei hdnShichosonKyotsuGunMei
-     */
-    @JsonProperty("hdnShichosonKyotsuGunMei")
-    public void setHdnShichosonKyotsuGunMei(RString hdnShichosonKyotsuGunMei) {
-        this.hdnShichosonKyotsuGunMei = hdnShichosonKyotsuGunMei;
-    }
-
-    /*
-     * gethdnShichosonKyotsuShichosonMei
-     * @return hdnShichosonKyotsuShichosonMei
-     */
-    @JsonProperty("hdnShichosonKyotsuShichosonMei")
-    public RString getHdnShichosonKyotsuShichosonMei() {
-        return hdnShichosonKyotsuShichosonMei;
-    }
-
-    /*
-     * sethdnShichosonKyotsuShichosonMei
-     * @param hdnShichosonKyotsuShichosonMei hdnShichosonKyotsuShichosonMei
-     */
-    @JsonProperty("hdnShichosonKyotsuShichosonMei")
-    public void setHdnShichosonKyotsuShichosonMei(RString hdnShichosonKyotsuShichosonMei) {
-        this.hdnShichosonKyotsuShichosonMei = hdnShichosonKyotsuShichosonMei;
-    }
-
-    /*
-     * gethdnShichosonKyotsuHensyuHoho
-     * @return hdnShichosonKyotsuHensyuHoho
-     */
-    @JsonProperty("hdnShichosonKyotsuHensyuHoho")
-    public RString getHdnShichosonKyotsuHensyuHoho() {
-        return hdnShichosonKyotsuHensyuHoho;
-    }
-
-    /*
-     * sethdnShichosonKyotsuHensyuHoho
-     * @param hdnShichosonKyotsuHensyuHoho hdnShichosonKyotsuHensyuHoho
-     */
-    @JsonProperty("hdnShichosonKyotsuHensyuHoho")
-    public void setHdnShichosonKyotsuHensyuHoho(RString hdnShichosonKyotsuHensyuHoho) {
-        this.hdnShichosonKyotsuHensyuHoho = hdnShichosonKyotsuHensyuHoho;
-    }
-
-    /*
-     * gethdnShichosonKyotsuKatagaki
-     * @return hdnShichosonKyotsuKatagaki
-     */
-    @JsonProperty("hdnShichosonKyotsuKatagaki")
-    public RString getHdnShichosonKyotsuKatagaki() {
-        return hdnShichosonKyotsuKatagaki;
-    }
-
-    /*
-     * sethdnShichosonKyotsuKatagaki
-     * @param hdnShichosonKyotsuKatagaki hdnShichosonKyotsuKatagaki
-     */
-    @JsonProperty("hdnShichosonKyotsuKatagaki")
-    public void setHdnShichosonKyotsuKatagaki(RString hdnShichosonKyotsuKatagaki) {
-        this.hdnShichosonKyotsuKatagaki = hdnShichosonKyotsuKatagaki;
     }
 
     /*
@@ -319,7 +219,7 @@ public class KaigoAtesakiJushoSetteiDiv extends Panel implements IKaigoAtesakiJu
             DisplayJushoSetteiRad[] enumArray = DisplayJushoSetteiRad.values();
 
             for (DisplayJushoSetteiRad enumStr : enumArray) {
-                if (str.equals(enumStr.name.toString())) {
+                if (str.equals(enumStr.name.toString())) { 
                     return enumStr;
                 }
             }
@@ -334,11 +234,11 @@ public class KaigoAtesakiJushoSetteiDiv extends Panel implements IKaigoAtesakiJu
     }
 
     public DisplayJushoSetteiRad getMode_DisplayJushoSetteiRad() {
-        return (DisplayJushoSetteiRad) _CommonChildDivModeUtil.getMode(this.modes, DisplayJushoSetteiRad.class);
+        return (DisplayJushoSetteiRad) _CommonChildDivModeUtil.getMode( this.modes, DisplayJushoSetteiRad.class );
     }
 
-    public void setMode_DisplayJushoSetteiRad(DisplayJushoSetteiRad value) {
-        _CommonChildDivModeUtil.setMode(this.modes, DisplayJushoSetteiRad.class, value);
+    public void setMode_DisplayJushoSetteiRad( DisplayJushoSetteiRad value ) {
+        _CommonChildDivModeUtil.setMode( this.modes, DisplayJushoSetteiRad.class , value );
     }
 
     // </editor-fold>
