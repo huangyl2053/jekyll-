45515128f57bdc0023a4641856771982e83f5f92
wanghui <wanghui@liandisys.com.cn>
2016-06-22 11:46:49 +0800

    DBU-3990-030 画面設計_DBZccd_KaigoAtesakiJushoSettei_介護宛先住所設定のreview修正

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/kaigoatesakijushosettei/KaigoAtesakiJushoSettei/KaigoAtesakiJushoSetteiDiv.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/kaigoatesakijushosettei/KaigoAtesakiJushoSettei/KaigoAtesakiJushoSetteiDiv.java
@@ -4,9 +4,9 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.kaigoates
  * このファイルへの変更は、再生成時には損失するため
  * 不正な動作の原因になります。
  */
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.ui.binding.*;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
 
 import java.util.HashSet;
@@ -21,9 +21,10 @@ import jp.co.ndensan.reams.uz.uza.ui.binding.TextBox;
 /**
  * KaigoAtesakiJushoSettei のクラスファイル
  *
- * @author 自動生成
+ * @reamsid_L　DBU-3990-030　wanghui
  */
 public class KaigoAtesakiJushoSetteiDiv extends Panel implements IKaigoAtesakiJushoSetteiDiv {
+
     // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-05-30_13-18-33">
     /*
      * [ private の作成 ]
@@ -259,7 +260,7 @@ public class KaigoAtesakiJushoSetteiDiv extends Panel implements IKaigoAtesakiJu
             DisplayJushoSetteiRad[] enumArray = DisplayJushoSetteiRad.values();
 
             for (DisplayJushoSetteiRad enumStr : enumArray) {
-                if (str.equals(enumStr.name.toString())) { 
+                if (str.equals(enumStr.name.toString())) {
                     return enumStr;
                 }
             }
@@ -274,11 +275,11 @@ public class KaigoAtesakiJushoSetteiDiv extends Panel implements IKaigoAtesakiJu
     }
 
     public DisplayJushoSetteiRad getMode_DisplayJushoSetteiRad() {
-        return (DisplayJushoSetteiRad) _CommonChildDivModeUtil.getMode( this.modes, DisplayJushoSetteiRad.class );
+        return (DisplayJushoSetteiRad) _CommonChildDivModeUtil.getMode(this.modes, DisplayJushoSetteiRad.class);
     }
 
-    public void setMode_DisplayJushoSetteiRad( DisplayJushoSetteiRad value ) {
-        _CommonChildDivModeUtil.setMode( this.modes, DisplayJushoSetteiRad.class , value );
+    public void setMode_DisplayJushoSetteiRad(DisplayJushoSetteiRad value) {
+        _CommonChildDivModeUtil.setMode(this.modes, DisplayJushoSetteiRad.class, value);
     }
 
     // </editor-fold>
