8328d27cb9460a8a5af2b2c532106826c7f7845b
huangh <huangh@liandisys.com>
2016-06-22 09:27:11 +0800

    画面設計_DBE2210003_認定調査結果登録3の横展開対応

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/handler/parentdiv/DBE2210003/GaikyoTokkiYichiranNyurokuHandler.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/handler/parentdiv/DBE2210003/GaikyoTokkiYichiranNyurokuHandler.java
@@ -13,21 +13,19 @@ import java.util.Set;
 import jp.co.ndensan.reams.db.dbe.business.core.gaikyotokkiyichirannyuroku.GaikyoTokkiYichiranNyurokuBusiness;
 import jp.co.ndensan.reams.db.dbe.business.core.ninteichosahyo.ninteichosahyotokkijiko.NinteichosahyoTokkijiko;
 import jp.co.ndensan.reams.db.dbe.business.core.ninteichosahyo.ninteichosahyotokkijiko.NinteichosahyoTokkijikoBuilder;
-import jp.co.ndensan.reams.db.dbe.definition.core.viewstate.ViewStateKeys;
 import jp.co.ndensan.reams.db.dbe.definition.message.DbeErrorMessages;
 import jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE2210003.GaikyoTokkiYichiranNyurokuDiv;
 import jp.co.ndensan.reams.db.dbe.service.core.ninteichosahyo.ninteichosahyotokkijiko.NinteichosahyoTokkijikoManager;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShinseishoKanriNo;
+import jp.co.ndensan.reams.db.dbz.definition.core.chosahyokomoku.NinteichosaKomoku09B;
 import jp.co.ndensan.reams.db.dbz.definition.core.chosajisshishajoho.ChosaJisshishaJohoModel;
 import jp.co.ndensan.reams.db.dbz.definition.core.yokaigonintei.GenponMaskKubun;
 import jp.co.ndensan.reams.db.dbz.definition.core.yokaigonintei.chosain.TokkijikoTextImageKubun;
-import jp.co.ndensan.reams.db.dbz.definition.core.chosahyokomoku.NinteichosaKomoku09B;
 import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.ChosaJisshishaJoho.ChosaJisshishaJoho.ChosaJisshishaJohoDiv;
 import jp.co.ndensan.reams.uz.uza.biz.Code;
 import jp.co.ndensan.reams.uz.uza.lang.ApplicationException;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.math.Decimal;
-import jp.co.ndensan.reams.uz.uza.ui.servlets.ViewStateHolder;
 import jp.co.ndensan.reams.uz.uza.util.di.InstanceProvider;
 import jp.co.ndensan.reams.uz.uza.util.serialization.DataPassingConverter;
 
@@ -116,32 +114,18 @@ public class GaikyoTokkiYichiranNyurokuHandler {
 
     }
 
-    private enum DBE2210003Keys {
-
-        入力内容を取り消す用データ
-    }
-
     /**
      * 認定調査結果登録3初期化の設定します。
      *
+     * @param model ChosaJisshishaJohoModel
+     * @param temp_申請書管理番号 ShinseishoKanriNo
+     * @param temp_認定調査履歴番号 int
+     * @return HashMap<RString, GaikyoTokkiYichiranNyurokuBusiness>
      */
-    public void onLoad() {
-
-        ChosaJisshishaJohoModel model = new ChosaJisshishaJohoModel();
-        ShinseishoKanriNo temp_申請書管理番号 = ViewStateHolder.get(ViewStateKeys.申請書管理番号, ShinseishoKanriNo.class);
-        RString 調査実施日 = ViewStateHolder.get(ViewStateKeys.調査実施日, RString.class);
-        RString 調査実施場所 = ViewStateHolder.get(ViewStateKeys.調査実施場所, RString.class);
-        RString 実施場所名称 = ViewStateHolder.get(ViewStateKeys.実施場所名称, RString.class);
-        RString 所属機関 = ViewStateHolder.get(ViewStateKeys.所属機関, RString.class);
-        RString 記入者 = ViewStateHolder.get(ViewStateKeys.記入者, RString.class);
-        RString 調査区分 = ViewStateHolder.get(ViewStateKeys.調査区分, RString.class);
-        model.set調査実施日(調査実施日);
-        model.set調査実施場所(調査実施場所);
-        model.set実施場所名称(実施場所名称);
-        model.set所属機関(所属機関);
-        model.set記入者(記入者);
-        model.set調査区分(調査区分);
-        model.set申請書管理番号(temp_申請書管理番号.getColumnValue());
+    public HashMap<RString, GaikyoTokkiYichiranNyurokuBusiness> onLoad(
+            ChosaJisshishaJohoModel model,
+            ShinseishoKanriNo temp_申請書管理番号,
+            int temp_認定調査履歴番号) {
 
         div.getChosaTaisho().getCcdNinteiShinseishaKihonInfo().initialize(temp_申請書管理番号);
         div.getChosaTaisho().getCcdNinteiShinseiRenrakusakiKihon().initialize(temp_申請書管理番号);
@@ -149,7 +133,7 @@ public class GaikyoTokkiYichiranNyurokuHandler {
         div.getCcdChosaJisshishaJoho().intialize(model);
 
         当前ページ数 = 1;
-        this.set各特記事項基本情報();
+        this.set各特記事項基本情報(temp_申請書管理番号, temp_認定調査履歴番号);
 
         this.set初期化活性制御();
 
@@ -166,7 +150,7 @@ public class GaikyoTokkiYichiranNyurokuHandler {
         div.getTokkiNyuryoku()
                 .setHiddenGaikyoTokkiNyurokuMap(DataPassingConverter.serialize(gaikyoTokkiNyurokuMap));
 
-        ViewStateHolder.put(DBE2210003Keys.入力内容を取り消す用データ, gaikyoTokkiNyurokuMap);
+        return gaikyoTokkiNyurokuMap;
     }
 
     /**
@@ -561,14 +545,11 @@ public class GaikyoTokkiYichiranNyurokuHandler {
      * 「特記事項を保存する」ボタンの操作処理を行う。
      *
      */
-    public void onClick_Save() {
+    public void onClick_Save(ShinseishoKanriNo temp_申請書管理番号, int temp_認定調査履歴番号) {
 
         NinteichosahyoTokkijikoManager manager = InstanceProvider.create(NinteichosahyoTokkijikoManager.class);
 
         gaikyoTokkiNyurokuMap = DataPassingConverter.deserialize(div.getTokkiNyuryoku().getHiddenGaikyoTokkiNyurokuMap(), HashMap.class);
-
-        ShinseishoKanriNo temp_申請書管理番号 = ViewStateHolder.get(ViewStateKeys.申請書管理番号, ShinseishoKanriNo.class);
-        int temp_認定調査履歴番号 = ViewStateHolder.get(ViewStateKeys.認定調査履歴番号, Integer.class);
 
         Set<Entry<RString, GaikyoTokkiYichiranNyurokuBusiness>> set = gaikyoTokkiNyurokuMap.entrySet();
         Iterator<Entry<RString, GaikyoTokkiYichiranNyurokuBusiness>> it = set.iterator();
@@ -622,10 +603,9 @@ public class GaikyoTokkiYichiranNyurokuHandler {
     /**
      * 「入力内容を取り消し」ボタンの操作処理を行う。
      *
+     * @param gaikyoTokkiNyurokuMap HashMap<RString, GaikyoTokkiYichiranNyurokuBusiness>
      */
-    public void onClick_btnCancel() {
-
-        gaikyoTokkiNyurokuMap = ViewStateHolder.get(DBE2210003Keys.入力内容を取り消す用データ, HashMap.class);
+    public void onClick_btnCancel(HashMap<RString, GaikyoTokkiYichiranNyurokuBusiness> gaikyoTokkiNyurokuMap) {
 
         当前ページ数 = 1;
 
@@ -648,12 +628,9 @@ public class GaikyoTokkiYichiranNyurokuHandler {
      * 各特記事項基本情報を設定します。
      *
      */
-    private void set各特記事項基本情報() {
+    private void set各特記事項基本情報(ShinseishoKanriNo temp_申請書管理番号, int temp_認定調査履歴番号) {
 
         NinteichosahyoTokkijikoManager manager = InstanceProvider.create(NinteichosahyoTokkijikoManager.class);
-
-        ShinseishoKanriNo temp_申請書管理番号 = ViewStateHolder.get(ViewStateKeys.申請書管理番号, ShinseishoKanriNo.class);
-        int temp_認定調査履歴番号 = ViewStateHolder.get(ViewStateKeys.認定調査履歴番号, Integer.class);
 
         List<NinteichosahyoTokkijiko> returnList = manager.get認定調査票_特記情報(temp_申請書管理番号, temp_認定調査履歴番号);
 
