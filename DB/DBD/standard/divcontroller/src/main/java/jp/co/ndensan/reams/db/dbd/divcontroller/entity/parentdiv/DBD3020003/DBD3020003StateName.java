59d0d2984d8d90a1619bbec652137309b1a37000
城間 篤人 <n8178@ndensan.co.jp>
2016-06-24 10:59:49 +0900

    時効起算日特殊登録のレイアウト追加

---
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD3020003/DBD3020003StateName.java
@@ -0,0 +1,37 @@
+package jp.co.ndensan.reams.db.dbd.divcontroller.entity.parentdiv.DBD3020003;
+
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+
+/**
+ * このコードはツールによって生成されました。</br>
+ * このファイルへの変更は、再生成時には損失するため</br>
+ * 不正な動作の原因になります。</br>
+ * DBD3020003の状態名定義クラスです
+ *
+ * @author 自動生成
+ */
+public enum DBD3020003StateName implements IStateEnumerations {
+// <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
+
+    NoChange(""),
+    登録("登録"),
+    完了("完了");
+
+    private final RString name;
+
+    private DBD3020003StateName(String name) {
+        this.name = new RString(name);
+    }
+
+    /**
+     * getName
+     *
+     * @return 名前
+     */
+    @Override
+    public RString getName() {
+        return name;
+    }
+// </editor-fold>
+}
