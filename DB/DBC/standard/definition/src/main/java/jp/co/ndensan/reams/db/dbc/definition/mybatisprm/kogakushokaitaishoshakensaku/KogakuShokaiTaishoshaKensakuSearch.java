8a6898f39df61656697dac8b84bbb3837a18ac3f
gongliang <gongliang@liandisys.com.cn>
2016-06-27 10:48:02 +0800

    変更管理_927　を対応。

--- DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/mybatisprm/kogakushokaitaishoshakensaku/KogakuShokaiTaishoshaKensakuSearch.java
+++ DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/mybatisprm/kogakushokaitaishoshakensaku/KogakuShokaiTaishoshaKensakuSearch.java
@@ -6,8 +6,10 @@
 package jp.co.ndensan.reams.db.dbc.definition.mybatisprm.kogakushokaitaishoshakensaku;
 
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
+import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleYearMonth;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,7 +23,7 @@ import lombok.Setter;
 @SuppressWarnings("PMD.UnusedPrivateField")
 public class KogakuShokaiTaishoshaKensakuSearch {
 
-    private boolean 指定して場合;
+    private RString メニューID;
     private HihokenshaNo 被保険者番号;
     private FlexibleYearMonth 提供年月From;
     private FlexibleYearMonth 提供年月To;
@@ -29,9 +31,6 @@ public class KogakuShokaiTaishoshaKensakuSearch {
     private FlexibleDate 申請年月To;
     private FlexibleDate 決定年月From;
     private FlexibleDate 決定年月To;
-    private FlexibleYearMonth 提供年月;
-    private FlexibleDate 申請年月初;
-    private FlexibleDate 申請年月末;
-    private FlexibleDate 決定年月初;
-    private FlexibleDate 決定年月末;
+    private LasdecCode 市町村コード;
+
 }
