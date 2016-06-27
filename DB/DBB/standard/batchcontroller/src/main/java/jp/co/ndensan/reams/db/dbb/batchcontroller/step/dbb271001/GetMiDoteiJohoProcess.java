3d6ceba468b7eb556d68f1c1c524598f475adc51
zhangrui <rzhang@sinoi.com.cn>
2016-06-27 09:54:23 +0800

    バッチ設計_DBBBT81002_特徴対象者同定（一括） DBB-1850-040 DBB271001_TokuchoTaishoshaDotei QA 894がありますので、TODOソースがあります

---
+++ DB/DBB/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/batchcontroller/step/dbb271001/GetMiDoteiJohoProcess.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbb.batchcontroller.step.dbb271001;
+
+import jp.co.ndensan.reams.db.dbb.definition.processprm.tokuchotaishoshadotei.GetMiDoteiJohoProcessParameter;
+import jp.co.ndensan.reams.db.dbb.service.core.tokuchotaishosha.TokuchoTaishoshaDoteiIkatsu;
+import jp.co.ndensan.reams.uz.uza.batch.process.SimpleBatchProcessBase;
+
+/**
+ *
+ * 特徴対象者同定（一括）のバッチフロークラス<br>
+ * 未同定情報を取得する
+ *
+ * @reamsid_L DBB-1850-040 zhangrui
+ */
+public class GetMiDoteiJohoProcess extends SimpleBatchProcessBase {
+
+    private TokuchoTaishoshaDoteiIkatsu manager;
+    private GetMiDoteiJohoProcessParameter parameter;
+
+    @Override
+    protected void beforeExecute() {
+        super.beforeExecute();
+        manager = TokuchoTaishoshaDoteiIkatsu.createInstance();
+    }
+
+    @Override
+    protected void process() {
+        manager.createTkMidoteiJoho(parameter.get開始月(), parameter.get処理日時());
+    }
+
+}
