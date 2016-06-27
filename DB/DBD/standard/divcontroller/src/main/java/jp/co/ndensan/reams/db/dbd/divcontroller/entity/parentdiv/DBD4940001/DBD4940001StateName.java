a4a764b450ccc4f09c17ab6a9512715e9cabff91
姜岐 <n8344@ndensan.co.jp>
2016-06-27 14:46:43 +0900

    介護認定申請取消　画面作成

--- DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD4940001/DBD4940001StateName.java
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD4940001/DBD4940001StateName.java
@@ -15,7 +15,9 @@ public enum DBD4940001StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
     NoChange(""),
-    初期表示("初期表示");
+    初期状態("初期状態"),
+    一覧状態("一覧状態"),
+    詳細状態("詳細状態");
 
     private final RString name;
 
