c1510896f9e1bb05774af89c1331d73f2436169e
北爪敏之 <n8365@ndensan.co.jp>
2016-06-27 17:47:50 +0900

    レイアウト修正（DBC1140012.raml、DBC1141011.raml）

---
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC1141011/自己負担額情報補正.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC1141011;
+
+/**
+ *
+ * @author n8365
+ */
+class 自己負担額情報補正 {
+    
+}
