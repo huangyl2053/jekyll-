a335976e9120f2f820a21f8ed5e7e63244157c84
xupeng <xupeng@liandisys.com.cn>
2016-06-29 11:39:36 +0800

    DBC-4471-010 画面設計_TaishoshaIchiran_対象者一覧 新規作成

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/TaishoshaIchiran/TaishoshaIchiran/dgJukyushaIdoRenrakuhyoHenkoSearchResultList_Row.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/TaishoshaIchiran/TaishoshaIchiran/dgJukyushaIdoRenrakuhyoHenkoSearchResultList_Row.java
@@ -1,31 +1,27 @@
-package jp.co.ndensan.reams.db.dbc.divcontroller.entity.commonchilddiv.TaishoshaIchiran;
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.commonchilddiv.TaishoshaIchiran.TaishoshaIchiran;
 /*
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Thu May 26 13:33:08 JST 2016 
+ * Tue Jun 28 16:35:11 CST 2016
  */
 
-
-
-import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
-import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxCode;
+import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
 import jp.co.ndensan.reams.uz.uza.ui.binding.TextBox;
-
+import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxCode;
+import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxDate;
 
 /**
- * dgJukyushaIdoRenrakuhyoHenkoSearchResultList_Row のクラスファイル 
- * 
- * @author 自動生成
+ * dgJukyushaIdoRenrakuhyoHenkoSearchResultList_Row のクラスファイル
+ *
+ * @reamsid_L DBC-4471-010 xupeng
  */
-
 public class dgJukyushaIdoRenrakuhyoHenkoSearchResultList_Row extends DataRow {
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-01-15_09-59-03">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-05-30_13-18-33">
 
     private TextBoxDate txtResultIdoDate;
-    private RString txtTaishoNengetsu;
+    private TextBoxDate txtTaishoNengetsu;
     private TextBoxCode txtResultHihoNo;
     private TextBox txtResultHihoName;
     private RString txtRirekiNo;
@@ -34,7 +30,7 @@ public class dgJukyushaIdoRenrakuhyoHenkoSearchResultList_Row extends DataRow {
     public dgJukyushaIdoRenrakuhyoHenkoSearchResultList_Row() {
         super();
         this.txtResultIdoDate = new TextBoxDate();
-        this.txtTaishoNengetsu = RString.EMPTY;
+        this.txtTaishoNengetsu = new TextBoxDate();
         this.txtResultHihoNo = new TextBoxCode();
         this.txtResultHihoName = new TextBox();
         this.txtRirekiNo = RString.EMPTY;
@@ -47,7 +43,7 @@ public class dgJukyushaIdoRenrakuhyoHenkoSearchResultList_Row extends DataRow {
         this.setOriginalData("txtDeleteFlag", txtDeleteFlag);
     }
 
-    public dgJukyushaIdoRenrakuhyoHenkoSearchResultList_Row(TextBoxDate txtResultIdoDate, RString txtTaishoNengetsu, TextBoxCode txtResultHihoNo, TextBox txtResultHihoName, RString txtRirekiNo, RString txtDeleteFlag) {
+    public dgJukyushaIdoRenrakuhyoHenkoSearchResultList_Row(TextBoxDate txtResultIdoDate, TextBoxDate txtTaishoNengetsu, TextBoxCode txtResultHihoNo, TextBox txtResultHihoName, RString txtRirekiNo, RString txtDeleteFlag) {
         super();
         this.setOriginalData("txtResultIdoDate", txtResultIdoDate);
         this.setOriginalData("txtTaishoNengetsu", txtTaishoNengetsu);
@@ -67,7 +63,7 @@ public class dgJukyushaIdoRenrakuhyoHenkoSearchResultList_Row extends DataRow {
         return txtResultIdoDate;
     }
 
-    public RString getTxtTaishoNengetsu() {
+    public TextBoxDate getTxtTaishoNengetsu() {
         return txtTaishoNengetsu;
     }
 
@@ -92,7 +88,7 @@ public class dgJukyushaIdoRenrakuhyoHenkoSearchResultList_Row extends DataRow {
         this.txtResultIdoDate = txtResultIdoDate;
     }
 
-    public void setTxtTaishoNengetsu(RString txtTaishoNengetsu) {
+    public void setTxtTaishoNengetsu(TextBoxDate txtTaishoNengetsu) {
         this.setOriginalData("txtTaishoNengetsu", txtTaishoNengetsu);
         this.txtTaishoNengetsu = txtTaishoNengetsu;
     }
