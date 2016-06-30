41dd850de11551cc7ae0150e61fcfdcd3fdbbd1f
lishengli <lishengli@liandisys.com.cn>
2016-06-30 21:46:59 +0800

    帳票設計_JImuIchijihanteiKekkahyoSakusei_事務局用一次判定結果票　新規作成

---
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/jimukyokuyouichijihanteikekkahyoa3/IchijihanteikekkahyoA3Builder.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.business.report.jimukyokuyouichijihanteikekkahyoa3;
+
+import jp.co.ndensan.reams.db.dbe.entity.report.source.jimukyokuyouichijihanteikekkahyo.IchijihanteikekkahyoA3ReportSource;
+import jp.co.ndensan.reams.uz.uza.report.ReportEditorJoiner;
+
+/**
+ * 事務局用一次判定結果票A3Builderクラスです。
+ *
+ * @reamsid_L DBE-0150-040 lishengli
+ */
+class IchijihanteikekkahyoA3Builder implements IIchijihanteikekkahyoA3Builder {
+
+    private final IIchijihanteikekkahyoA3Editor headerEditor;
+    private final IIchijihanteikekkahyoA3Editor bodyEditor;
+
+    /**
+     * インスタンスを生成します。
+     *
+     * @param headerEditor {@link IIchijihanteikekkahyoA3Editor}
+     * @param bodyEditor {@link IIchijihanteikekkahyoA3Editor}
+     */
+    public IchijihanteikekkahyoA3Builder(IIchijihanteikekkahyoA3Editor headerEditor,
+            IIchijihanteikekkahyoA3Editor bodyEditor) {
+        this.headerEditor = headerEditor;
+        this.bodyEditor = bodyEditor;
+    }
+
+    /**
+     * 帳票ソースをビルドします。
+     *
+     * @return {@link IchijihanteikekkahyoA3ReportSource}
+     */
+    @Override
+    public IchijihanteikekkahyoA3ReportSource build() {
+        return ReportEditorJoiner.from(new IchijihanteikekkahyoA3ReportSource()).join(headerEditor).join(bodyEditor).buildSource();
+    }
+
+}
