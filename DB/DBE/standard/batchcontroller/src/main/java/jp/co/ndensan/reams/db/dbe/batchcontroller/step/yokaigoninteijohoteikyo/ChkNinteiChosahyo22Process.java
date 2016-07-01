853f65091787e35a7c946dc2dd49095c69063163
zhangzhiming <zhangzhiming@liandisys.com.cn>
2016-07-01 09:51:53 +0800

    バッチ設計_DBE090002_要介護認定結果情報提供票　ソース修正

--- DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/step/yokaigoninteijohoteikyo/ChkNinteiChosahyo22Process.java
+++ DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/step/yokaigoninteijohoteikyo/ChkNinteiChosahyo22Process.java
@@ -561,7 +561,11 @@ public class ChkNinteiChosahyo22Process extends BatchProcessBase<YokaigoninteiEn
         ReadOnlySharedFileEntryDescriptor descriptor
                 = new ReadOnlySharedFileEntryDescriptor(new FilesystemName(sharedFileName),
                         sharedFileId);
-        SharedFile.copyToLocal(descriptor, new FilesystemPath(imagePath));
+        try {
+            SharedFile.copyToLocal(descriptor, new FilesystemPath(imagePath));
+        } catch (Exception e) {
+            return RString.EMPTY;
+        }
         return Path.combinePath(new RString("/db/dbe/image/"), sharedFileName);
     }
 
--- DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/step/yokaigoninteijohoteikyo/ChkShujiiIkenshoProcess.java
+++ DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/step/yokaigoninteijohoteikyo/ChkShujiiIkenshoProcess.java
@@ -151,7 +151,7 @@ public class ChkShujiiIkenshoProcess extends BatchProcessBase<YokaigoninteiEntit
             if (フラグ.equals(processPrm.getRadShujii())) {
                 imagePath = getFilePath(イメージID, FILENAME);
             } else {
-                imagePath = getFilePath(イメージID, FILENAME_BAK);
+                imagePath = getFilePathBak(イメージID, FILENAME_BAK);
             }
         }
         return imagePath;
@@ -163,13 +163,13 @@ public class ChkShujiiIkenshoProcess extends BatchProcessBase<YokaigoninteiEntit
             if (フラグ.equals(processPrm.getRadShujii())) {
                 imagePath = getFilePath(イメージID, FILENAME02);
             } else {
-                imagePath = getFilePath(イメージID, FILENAME_BAK02);
+                imagePath = getFilePathBak(イメージID, FILENAME_BAK02);
             }
         }
         return imagePath;
     }
 
-    private RString getFilePath(RDateTime sharedFileId, RString sharedFileName) {
+    private RString getFilePathBak(RDateTime sharedFileId, RString sharedFileName) {
         RString imagePath = Path.combinePath(Path.getUserHomePath(), new RString("app/webapps/db#dbe/WEB-INF/image/"));
         ReadOnlySharedFileEntryDescriptor descriptor
                 = new ReadOnlySharedFileEntryDescriptor(new FilesystemName(sharedFileName),
@@ -177,10 +177,27 @@ public class ChkShujiiIkenshoProcess extends BatchProcessBase<YokaigoninteiEntit
         try {
             SharedFile.copyToLocal(descriptor, new FilesystemPath(imagePath));
         } catch (Exception e) {
-            ReadOnlySharedFileEntryDescriptor descriptor_BAK
-                    = new ReadOnlySharedFileEntryDescriptor(new FilesystemName(sharedFileName.replace(".png", "_BAK.png")), sharedFileId);
-            SharedFile.copyToLocal(descriptor_BAK, new FilesystemPath(imagePath));
-            return Path.combinePath(new RString("/db/dbe/image/"), sharedFileName.replace(".png", "_BAK.png"));
+            return RString.EMPTY;
+        }
+        return Path.combinePath(new RString("/db/dbe/image/"), sharedFileName);
+    }
+
+    private RString getFilePath(RDateTime sharedFileId, RString sharedFileName) {
+        RString imagePath = Path.combinePath(Path.getUserHomePath(), new RString("app/webapps/db#dbe/WEB-INF/image/"));
+        ReadOnlySharedFileEntryDescriptor descriptor
+                = new ReadOnlySharedFileEntryDescriptor(new FilesystemName(sharedFileName),
+                        sharedFileId);
+        ReadOnlySharedFileEntryDescriptor descriptor_BAK
+                = new ReadOnlySharedFileEntryDescriptor(new FilesystemName(sharedFileName.replace(".png", "_BAK.png")), sharedFileId);
+        try {
+            SharedFile.copyToLocal(descriptor, new FilesystemPath(imagePath));
+        } catch (Exception e) {
+            try {
+                SharedFile.copyToLocal(descriptor_BAK, new FilesystemPath(imagePath));
+                return Path.combinePath(new RString("/db/dbe/image/"), sharedFileName.replace(".png", "_BAK.png"));
+            } catch (Exception ex) {
+                return RString.EMPTY;
+            }
         }
         return Path.combinePath(new RString("/db/dbe/image/"), sharedFileName);
     }
