1a606f959f1ad9cf190458deefd9b766b524401a
suguangjun <suguangjun@liandisys.com.cn>
2016-07-01 09:54:35 +0800

    画面設計_DBE9050001_その他機関マスタ 新規作成

---
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE9050001/ChosainSearchDiv.java
@@ -0,0 +1,245 @@
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE9050001;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.hokenshalist.HokenshaList.HokenshaListDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.hokenshalist.HokenshaList.IHokenshaListDiv;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+
+/**
+ * ChosainSearch のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class ChosainSearchDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-05-30_13-18-33">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("radSearchHaisiFlag")
+    private RadioButton radSearchHaisiFlag;
+    @JsonProperty("txtSearchSonotaKikanCodeFrom")
+    private TextBoxCode txtSearchSonotaKikanCodeFrom;
+    @JsonProperty("txtSearchSonotaKikanCodeTo")
+    private TextBoxCode txtSearchSonotaKikanCodeTo;
+    @JsonProperty("txtSearchSonotaKikanMeisho")
+    private TextBox txtSearchSonotaKikanMeisho;
+    @JsonProperty("txtSearchSonotaKikanKanaMeisho")
+    private TextBoxKana txtSearchSonotaKikanKanaMeisho;
+    @JsonProperty("ddlitakukubun")
+    private DropDownList ddlitakukubun;
+    @JsonProperty("ddlkikankubun")
+    private DropDownList ddlkikankubun;
+    @JsonProperty("txtSaidaiHyojiKensu")
+    private TextBoxNum txtSaidaiHyojiKensu;
+    @JsonProperty("btnSearchShujii")
+    private Button btnSearchShujii;
+    @JsonProperty("btnClearKensakuJoken")
+    private Button btnClearKensakuJoken;
+    @JsonProperty("ccdhokensha")
+    private HokenshaListDiv ccdhokensha;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * getradSearchHaisiFlag
+     * @return radSearchHaisiFlag
+     */
+    @JsonProperty("radSearchHaisiFlag")
+    public RadioButton getRadSearchHaisiFlag() {
+        return radSearchHaisiFlag;
+    }
+
+    /*
+     * setradSearchHaisiFlag
+     * @param radSearchHaisiFlag radSearchHaisiFlag
+     */
+    @JsonProperty("radSearchHaisiFlag")
+    public void setRadSearchHaisiFlag(RadioButton radSearchHaisiFlag) {
+        this.radSearchHaisiFlag = radSearchHaisiFlag;
+    }
+
+    /*
+     * gettxtSearchSonotaKikanCodeFrom
+     * @return txtSearchSonotaKikanCodeFrom
+     */
+    @JsonProperty("txtSearchSonotaKikanCodeFrom")
+    public TextBoxCode getTxtSearchSonotaKikanCodeFrom() {
+        return txtSearchSonotaKikanCodeFrom;
+    }
+
+    /*
+     * settxtSearchSonotaKikanCodeFrom
+     * @param txtSearchSonotaKikanCodeFrom txtSearchSonotaKikanCodeFrom
+     */
+    @JsonProperty("txtSearchSonotaKikanCodeFrom")
+    public void setTxtSearchSonotaKikanCodeFrom(TextBoxCode txtSearchSonotaKikanCodeFrom) {
+        this.txtSearchSonotaKikanCodeFrom = txtSearchSonotaKikanCodeFrom;
+    }
+
+    /*
+     * gettxtSearchSonotaKikanCodeTo
+     * @return txtSearchSonotaKikanCodeTo
+     */
+    @JsonProperty("txtSearchSonotaKikanCodeTo")
+    public TextBoxCode getTxtSearchSonotaKikanCodeTo() {
+        return txtSearchSonotaKikanCodeTo;
+    }
+
+    /*
+     * settxtSearchSonotaKikanCodeTo
+     * @param txtSearchSonotaKikanCodeTo txtSearchSonotaKikanCodeTo
+     */
+    @JsonProperty("txtSearchSonotaKikanCodeTo")
+    public void setTxtSearchSonotaKikanCodeTo(TextBoxCode txtSearchSonotaKikanCodeTo) {
+        this.txtSearchSonotaKikanCodeTo = txtSearchSonotaKikanCodeTo;
+    }
+
+    /*
+     * gettxtSearchSonotaKikanMeisho
+     * @return txtSearchSonotaKikanMeisho
+     */
+    @JsonProperty("txtSearchSonotaKikanMeisho")
+    public TextBox getTxtSearchSonotaKikanMeisho() {
+        return txtSearchSonotaKikanMeisho;
+    }
+
+    /*
+     * settxtSearchSonotaKikanMeisho
+     * @param txtSearchSonotaKikanMeisho txtSearchSonotaKikanMeisho
+     */
+    @JsonProperty("txtSearchSonotaKikanMeisho")
+    public void setTxtSearchSonotaKikanMeisho(TextBox txtSearchSonotaKikanMeisho) {
+        this.txtSearchSonotaKikanMeisho = txtSearchSonotaKikanMeisho;
+    }
+
+    /*
+     * gettxtSearchSonotaKikanKanaMeisho
+     * @return txtSearchSonotaKikanKanaMeisho
+     */
+    @JsonProperty("txtSearchSonotaKikanKanaMeisho")
+    public TextBoxKana getTxtSearchSonotaKikanKanaMeisho() {
+        return txtSearchSonotaKikanKanaMeisho;
+    }
+
+    /*
+     * settxtSearchSonotaKikanKanaMeisho
+     * @param txtSearchSonotaKikanKanaMeisho txtSearchSonotaKikanKanaMeisho
+     */
+    @JsonProperty("txtSearchSonotaKikanKanaMeisho")
+    public void setTxtSearchSonotaKikanKanaMeisho(TextBoxKana txtSearchSonotaKikanKanaMeisho) {
+        this.txtSearchSonotaKikanKanaMeisho = txtSearchSonotaKikanKanaMeisho;
+    }
+
+    /*
+     * getddlitakukubun
+     * @return ddlitakukubun
+     */
+    @JsonProperty("ddlitakukubun")
+    public DropDownList getDdlitakukubun() {
+        return ddlitakukubun;
+    }
+
+    /*
+     * setddlitakukubun
+     * @param ddlitakukubun ddlitakukubun
+     */
+    @JsonProperty("ddlitakukubun")
+    public void setDdlitakukubun(DropDownList ddlitakukubun) {
+        this.ddlitakukubun = ddlitakukubun;
+    }
+
+    /*
+     * getddlkikankubun
+     * @return ddlkikankubun
+     */
+    @JsonProperty("ddlkikankubun")
+    public DropDownList getDdlkikankubun() {
+        return ddlkikankubun;
+    }
+
+    /*
+     * setddlkikankubun
+     * @param ddlkikankubun ddlkikankubun
+     */
+    @JsonProperty("ddlkikankubun")
+    public void setDdlkikankubun(DropDownList ddlkikankubun) {
+        this.ddlkikankubun = ddlkikankubun;
+    }
+
+    /*
+     * gettxtSaidaiHyojiKensu
+     * @return txtSaidaiHyojiKensu
+     */
+    @JsonProperty("txtSaidaiHyojiKensu")
+    public TextBoxNum getTxtSaidaiHyojiKensu() {
+        return txtSaidaiHyojiKensu;
+    }
+
+    /*
+     * settxtSaidaiHyojiKensu
+     * @param txtSaidaiHyojiKensu txtSaidaiHyojiKensu
+     */
+    @JsonProperty("txtSaidaiHyojiKensu")
+    public void setTxtSaidaiHyojiKensu(TextBoxNum txtSaidaiHyojiKensu) {
+        this.txtSaidaiHyojiKensu = txtSaidaiHyojiKensu;
+    }
+
+    /*
+     * getbtnSearchShujii
+     * @return btnSearchShujii
+     */
+    @JsonProperty("btnSearchShujii")
+    public Button getBtnSearchShujii() {
+        return btnSearchShujii;
+    }
+
+    /*
+     * setbtnSearchShujii
+     * @param btnSearchShujii btnSearchShujii
+     */
+    @JsonProperty("btnSearchShujii")
+    public void setBtnSearchShujii(Button btnSearchShujii) {
+        this.btnSearchShujii = btnSearchShujii;
+    }
+
+    /*
+     * getbtnClearKensakuJoken
+     * @return btnClearKensakuJoken
+     */
+    @JsonProperty("btnClearKensakuJoken")
+    public Button getBtnClearKensakuJoken() {
+        return btnClearKensakuJoken;
+    }
+
+    /*
+     * setbtnClearKensakuJoken
+     * @param btnClearKensakuJoken btnClearKensakuJoken
+     */
+    @JsonProperty("btnClearKensakuJoken")
+    public void setBtnClearKensakuJoken(Button btnClearKensakuJoken) {
+        this.btnClearKensakuJoken = btnClearKensakuJoken;
+    }
+
+    /*
+     * getccdhokensha
+     * @return ccdhokensha
+     */
+    @JsonProperty("ccdhokensha")
+    public IHokenshaListDiv getCcdhokensha() {
+        return ccdhokensha;
+    }
+
+    // </editor-fold>
+}
---
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE9050001/NinteichosaItakusakiMainDiv.java
@@ -0,0 +1,257 @@
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE9050001;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.KaigoKanryoMessage.KaigoKanryoMessage.IKaigoKanryoMessageDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.KaigoKanryoMessage.KaigoKanryoMessage.KaigoKanryoMessageDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.hokenshalist.HokenshaList.HokenshaListDiv;
+import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.hokenshalist.HokenshaList.IHokenshaListDiv;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+
+/**
+ * NinteichosaItakusakiMain のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class NinteichosaItakusakiMainDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-05-30_13-18-33">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("ChosainSearch")
+    private ChosainSearchDiv ChosainSearch;
+    @JsonProperty("SonotaKikanichiran")
+    private SonotaKikanichiranDiv SonotaKikanichiran;
+    @JsonProperty("ChosaitakusakiJohoInput")
+    private ChosaitakusakiJohoInputDiv ChosaitakusakiJohoInput;
+    @JsonProperty("ccdKanryoMessage")
+    private KaigoKanryoMessageDiv ccdKanryoMessage;
+    @JsonProperty("hdnTxtSubGyomuCode")
+    private RString hdnTxtSubGyomuCode;
+    @JsonProperty("hdnTxtShikibetsuCode")
+    private RString hdnTxtShikibetsuCode;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * getChosainSearch
+     * @return ChosainSearch
+     */
+    @JsonProperty("ChosainSearch")
+    public ChosainSearchDiv getChosainSearch() {
+        return ChosainSearch;
+    }
+
+    /*
+     * setChosainSearch
+     * @param ChosainSearch ChosainSearch
+     */
+    @JsonProperty("ChosainSearch")
+    public void setChosainSearch(ChosainSearchDiv ChosainSearch) {
+        this.ChosainSearch = ChosainSearch;
+    }
+
+    /*
+     * getSonotaKikanichiran
+     * @return SonotaKikanichiran
+     */
+    @JsonProperty("SonotaKikanichiran")
+    public SonotaKikanichiranDiv getSonotaKikanichiran() {
+        return SonotaKikanichiran;
+    }
+
+    /*
+     * setSonotaKikanichiran
+     * @param SonotaKikanichiran SonotaKikanichiran
+     */
+    @JsonProperty("SonotaKikanichiran")
+    public void setSonotaKikanichiran(SonotaKikanichiranDiv SonotaKikanichiran) {
+        this.SonotaKikanichiran = SonotaKikanichiran;
+    }
+
+    /*
+     * getChosaitakusakiJohoInput
+     * @return ChosaitakusakiJohoInput
+     */
+    @JsonProperty("ChosaitakusakiJohoInput")
+    public ChosaitakusakiJohoInputDiv getChosaitakusakiJohoInput() {
+        return ChosaitakusakiJohoInput;
+    }
+
+    /*
+     * setChosaitakusakiJohoInput
+     * @param ChosaitakusakiJohoInput ChosaitakusakiJohoInput
+     */
+    @JsonProperty("ChosaitakusakiJohoInput")
+    public void setChosaitakusakiJohoInput(ChosaitakusakiJohoInputDiv ChosaitakusakiJohoInput) {
+        this.ChosaitakusakiJohoInput = ChosaitakusakiJohoInput;
+    }
+
+    /*
+     * getccdKanryoMessage
+     * @return ccdKanryoMessage
+     */
+    @JsonProperty("ccdKanryoMessage")
+    public IKaigoKanryoMessageDiv getCcdKanryoMessage() {
+        return ccdKanryoMessage;
+    }
+
+    /*
+     * gethdnTxtSubGyomuCode
+     * @return hdnTxtSubGyomuCode
+     */
+    @JsonProperty("hdnTxtSubGyomuCode")
+    public RString getHdnTxtSubGyomuCode() {
+        return hdnTxtSubGyomuCode;
+    }
+
+    /*
+     * sethdnTxtSubGyomuCode
+     * @param hdnTxtSubGyomuCode hdnTxtSubGyomuCode
+     */
+    @JsonProperty("hdnTxtSubGyomuCode")
+    public void setHdnTxtSubGyomuCode(RString hdnTxtSubGyomuCode) {
+        this.hdnTxtSubGyomuCode = hdnTxtSubGyomuCode;
+    }
+
+    /*
+     * gethdnTxtShikibetsuCode
+     * @return hdnTxtShikibetsuCode
+     */
+    @JsonProperty("hdnTxtShikibetsuCode")
+    public RString getHdnTxtShikibetsuCode() {
+        return hdnTxtShikibetsuCode;
+    }
+
+    /*
+     * sethdnTxtShikibetsuCode
+     * @param hdnTxtShikibetsuCode hdnTxtShikibetsuCode
+     */
+    @JsonProperty("hdnTxtShikibetsuCode")
+    public void setHdnTxtShikibetsuCode(RString hdnTxtShikibetsuCode) {
+        this.hdnTxtShikibetsuCode = hdnTxtShikibetsuCode;
+    }
+
+    /*
+     * [ ショートカットの作成 ]
+     */
+    @JsonIgnore
+    public RadioButton getRadSearchHaisiFlag() {
+        return this.getChosainSearch().getRadSearchHaisiFlag();
+    }
+
+    @JsonIgnore
+    public void  setRadSearchHaisiFlag(RadioButton radSearchHaisiFlag) {
+        this.getChosainSearch().setRadSearchHaisiFlag(radSearchHaisiFlag);
+    }
+
+    @JsonIgnore
+    public TextBoxCode getTxtSearchSonotaKikanCodeFrom() {
+        return this.getChosainSearch().getTxtSearchSonotaKikanCodeFrom();
+    }
+
+    @JsonIgnore
+    public void  setTxtSearchSonotaKikanCodeFrom(TextBoxCode txtSearchSonotaKikanCodeFrom) {
+        this.getChosainSearch().setTxtSearchSonotaKikanCodeFrom(txtSearchSonotaKikanCodeFrom);
+    }
+
+    @JsonIgnore
+    public TextBoxCode getTxtSearchSonotaKikanCodeTo() {
+        return this.getChosainSearch().getTxtSearchSonotaKikanCodeTo();
+    }
+
+    @JsonIgnore
+    public void  setTxtSearchSonotaKikanCodeTo(TextBoxCode txtSearchSonotaKikanCodeTo) {
+        this.getChosainSearch().setTxtSearchSonotaKikanCodeTo(txtSearchSonotaKikanCodeTo);
+    }
+
+    @JsonIgnore
+    public TextBox getTxtSearchSonotaKikanMeisho() {
+        return this.getChosainSearch().getTxtSearchSonotaKikanMeisho();
+    }
+
+    @JsonIgnore
+    public void  setTxtSearchSonotaKikanMeisho(TextBox txtSearchSonotaKikanMeisho) {
+        this.getChosainSearch().setTxtSearchSonotaKikanMeisho(txtSearchSonotaKikanMeisho);
+    }
+
+    @JsonIgnore
+    public TextBoxKana getTxtSearchSonotaKikanKanaMeisho() {
+        return this.getChosainSearch().getTxtSearchSonotaKikanKanaMeisho();
+    }
+
+    @JsonIgnore
+    public void  setTxtSearchSonotaKikanKanaMeisho(TextBoxKana txtSearchSonotaKikanKanaMeisho) {
+        this.getChosainSearch().setTxtSearchSonotaKikanKanaMeisho(txtSearchSonotaKikanKanaMeisho);
+    }
+
+    @JsonIgnore
+    public DropDownList getDdlitakukubun() {
+        return this.getChosainSearch().getDdlitakukubun();
+    }
+
+    @JsonIgnore
+    public void  setDdlitakukubun(DropDownList ddlitakukubun) {
+        this.getChosainSearch().setDdlitakukubun(ddlitakukubun);
+    }
+
+    @JsonIgnore
+    public DropDownList getDdlkikankubun() {
+        return this.getChosainSearch().getDdlkikankubun();
+    }
+
+    @JsonIgnore
+    public void  setDdlkikankubun(DropDownList ddlkikankubun) {
+        this.getChosainSearch().setDdlkikankubun(ddlkikankubun);
+    }
+
+    @JsonIgnore
+    public TextBoxNum getTxtSaidaiHyojiKensu() {
+        return this.getChosainSearch().getTxtSaidaiHyojiKensu();
+    }
+
+    @JsonIgnore
+    public void  setTxtSaidaiHyojiKensu(TextBoxNum txtSaidaiHyojiKensu) {
+        this.getChosainSearch().setTxtSaidaiHyojiKensu(txtSaidaiHyojiKensu);
+    }
+
+    @JsonIgnore
+    public Button getBtnSearchShujii() {
+        return this.getChosainSearch().getBtnSearchShujii();
+    }
+
+    @JsonIgnore
+    public void  setBtnSearchShujii(Button btnSearchShujii) {
+        this.getChosainSearch().setBtnSearchShujii(btnSearchShujii);
+    }
+
+    @JsonIgnore
+    public Button getBtnClearKensakuJoken() {
+        return this.getChosainSearch().getBtnClearKensakuJoken();
+    }
+
+    @JsonIgnore
+    public void  setBtnClearKensakuJoken(Button btnClearKensakuJoken) {
+        this.getChosainSearch().setBtnClearKensakuJoken(btnClearKensakuJoken);
+    }
+
+    @JsonIgnore
+    public IHokenshaListDiv getCcdhokensha() {
+        return this.getChosainSearch().getCcdhokensha();
+    }
+
+    // </editor-fold>
+}
