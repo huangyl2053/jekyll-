f7c6e663d3fd9aecf623e5482b433b8ccfd15471
duanzhanli <duanzhanli@liandisys.com.cn>
2016-07-02 12:34:04 +0800

    画面設計_DBE2270001_認定調査データ出力（モバイル）  新規作成

---
+++ DB/DBE/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbe/definition/batchprm/ninteichosadataoutput/NinteiChosaDataOutputBatchParamter.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.definition.batchprm.ninteichosadataoutput;
+
+import java.util.List;
+import jp.co.ndensan.reams.uz.uza.batch.BatchParameter;
+import jp.co.ndensan.reams.uz.uza.batch.flow.BatchParameterBase;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * 認定調査データ出力（モバイル）ですためのバッチ用パラメータクラスです。
+ *
+ * @reamsid_L DBE-1860-011 duanzhanli
+ */
+@Getter
+@Setter
+public class NinteiChosaDataOutputBatchParamter extends BatchParameterBase {
+
+    private static final String NINTEICHOSAITAKUSAKICODE = "ninteichosaItakusakiCode";
+    private static final String NINTEICHOSAINCODE = "ninteiChosainCode";
+    private static final String SHINSEISHOKANRINOLIST = "shinseishoKanriNoList";
+
+    @BatchParameter(key = NINTEICHOSAITAKUSAKICODE, name = "認定調査委託先コード")
+    private RString ninteichosaItakusakiCode;
+    @BatchParameter(key = NINTEICHOSAINCODE, name = "認定調査員コード")
+    private RString ninteiChosainCode;
+    @BatchParameter(key = SHINSEISHOKANRINOLIST, name = "申請書管理番号リスト")
+    private List<RString> shinseishoKanriNoList;
+
+    /**
+     * コンストラクタです。
+     */
+    public NinteiChosaDataOutputBatchParamter() {
+    }
+
+    /**
+     * コンストラクタです。
+     *
+     * @param 認定調査委託先コード 認定調査委託先コード
+     * @param 認定調査員コード 認定調査員コード
+     * @param 申請書管理番号リスト 申請書管理番号リスト
+     * @throws NullPointerException 引数のいずれかが{@code null}の場合
+     */
+    public NinteiChosaDataOutputBatchParamter(
+            RString 認定調査委託先コード,
+            RString 認定調査員コード,
+            List<RString> 申請書管理番号リスト) {
+        this.ninteichosaItakusakiCode = 認定調査委託先コード;
+        this.ninteiChosainCode = 認定調査員コード;
+        this.shinseishoKanriNoList = 申請書管理番号リスト;
+    }
+}
