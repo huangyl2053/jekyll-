550fc51b344ba69cb940ccd2ed377ea5874503d1
zhangzhiming <zhangzhiming@liandisys.com.cn>
2016-07-05 17:33:32 +0800

    画面設計_DBE2410001_個人依頼内容更新(認定調査票・主治医意見書編集)  新規

---
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE2410001/ZenkaiHomonChosaIraiJohoDiv.java
@@ -0,0 +1,573 @@
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE2410001;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+import jp.co.ndensan.reams.uz.uza.ui.binding.domain.*;
+
+/**
+ * ZenkaiHomonChosaIraiJoho のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class ZenkaiHomonChosaIraiJohoDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-05-30_13-18-33">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("lblShinkiChosaZen")
+    private Label lblShinkiChosaZen;
+    @JsonProperty("txtChosaBashoKubunZen")
+    private TextBox txtChosaBashoKubunZen;
+    @JsonProperty("txtChosaJisshibiZen")
+    private TextBoxDate txtChosaJisshibiZen;
+    @JsonProperty("txtChosaJIsshiChikuCodeZen")
+    private TextBoxCode txtChosaJIsshiChikuCodeZen;
+    @JsonProperty("txtChosaJIsshiChikuZen")
+    private TextBox txtChosaJIsshiChikuZen;
+    @JsonProperty("txtChosaKikanCodeZen")
+    private TextBoxCode txtChosaKikanCodeZen;
+    @JsonProperty("txtChosaKikanZen")
+    private TextBox txtChosaKikanZen;
+    @JsonProperty("txtChosaItakuKubunZen")
+    private TextBox txtChosaItakuKubunZen;
+    @JsonProperty("txtChosainCodeZen")
+    private TextBoxCode txtChosainCodeZen;
+    @JsonProperty("txtChosainZen")
+    private TextBox txtChosainZen;
+    @JsonProperty("txtChosainShikakuZen")
+    private TextBox txtChosainShikakuZen;
+    @JsonProperty("txtDenwaBangoZen")
+    private TextBoxTelNo txtDenwaBangoZen;
+    @JsonProperty("txtFaxZen")
+    private TextBoxTelNo txtFaxZen;
+    @JsonProperty("lblSaiChosaZen")
+    private Label lblSaiChosaZen;
+    @JsonProperty("txtChosaBashoKubunSaiZen")
+    private TextBox txtChosaBashoKubunSaiZen;
+    @JsonProperty("txtChosaJisshibiSaiZen")
+    private TextBoxDate txtChosaJisshibiSaiZen;
+    @JsonProperty("txtChosaJIsshiChikuCodeSaiZen")
+    private TextBoxCode txtChosaJIsshiChikuCodeSaiZen;
+    @JsonProperty("txtChosaJIsshiChikuSaiZen")
+    private TextBox txtChosaJIsshiChikuSaiZen;
+    @JsonProperty("txtChosaKikanCodeSaiZen")
+    private TextBoxCode txtChosaKikanCodeSaiZen;
+    @JsonProperty("txtChosaKikanSaiZen")
+    private TextBox txtChosaKikanSaiZen;
+    @JsonProperty("txtChosaItakuKubunSaiZen")
+    private TextBox txtChosaItakuKubunSaiZen;
+    @JsonProperty("txtChosainCodeSaiZen")
+    private TextBoxCode txtChosainCodeSaiZen;
+    @JsonProperty("txtChosainSaiZen")
+    private TextBox txtChosainSaiZen;
+    @JsonProperty("txtChosainShikakuSaiZen")
+    private TextBox txtChosainShikakuSaiZen;
+    @JsonProperty("txtDenwaBangoSaiZen")
+    private TextBoxTelNo txtDenwaBangoSaiZen;
+    @JsonProperty("txtFaxSaiZen")
+    private TextBoxTelNo txtFaxSaiZen;
+    @JsonProperty("txtHomonUmuZen")
+    private TextBox txtHomonUmuZen;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * getlblShinkiChosaZen
+     * @return lblShinkiChosaZen
+     */
+    @JsonProperty("lblShinkiChosaZen")
+    public Label getLblShinkiChosaZen() {
+        return lblShinkiChosaZen;
+    }
+
+    /*
+     * setlblShinkiChosaZen
+     * @param lblShinkiChosaZen lblShinkiChosaZen
+     */
+    @JsonProperty("lblShinkiChosaZen")
+    public void setLblShinkiChosaZen(Label lblShinkiChosaZen) {
+        this.lblShinkiChosaZen = lblShinkiChosaZen;
+    }
+
+    /*
+     * gettxtChosaBashoKubunZen
+     * @return txtChosaBashoKubunZen
+     */
+    @JsonProperty("txtChosaBashoKubunZen")
+    public TextBox getTxtChosaBashoKubunZen() {
+        return txtChosaBashoKubunZen;
+    }
+
+    /*
+     * settxtChosaBashoKubunZen
+     * @param txtChosaBashoKubunZen txtChosaBashoKubunZen
+     */
+    @JsonProperty("txtChosaBashoKubunZen")
+    public void setTxtChosaBashoKubunZen(TextBox txtChosaBashoKubunZen) {
+        this.txtChosaBashoKubunZen = txtChosaBashoKubunZen;
+    }
+
+    /*
+     * gettxtChosaJisshibiZen
+     * @return txtChosaJisshibiZen
+     */
+    @JsonProperty("txtChosaJisshibiZen")
+    public TextBoxDate getTxtChosaJisshibiZen() {
+        return txtChosaJisshibiZen;
+    }
+
+    /*
+     * settxtChosaJisshibiZen
+     * @param txtChosaJisshibiZen txtChosaJisshibiZen
+     */
+    @JsonProperty("txtChosaJisshibiZen")
+    public void setTxtChosaJisshibiZen(TextBoxDate txtChosaJisshibiZen) {
+        this.txtChosaJisshibiZen = txtChosaJisshibiZen;
+    }
+
+    /*
+     * gettxtChosaJIsshiChikuCodeZen
+     * @return txtChosaJIsshiChikuCodeZen
+     */
+    @JsonProperty("txtChosaJIsshiChikuCodeZen")
+    public TextBoxCode getTxtChosaJIsshiChikuCodeZen() {
+        return txtChosaJIsshiChikuCodeZen;
+    }
+
+    /*
+     * settxtChosaJIsshiChikuCodeZen
+     * @param txtChosaJIsshiChikuCodeZen txtChosaJIsshiChikuCodeZen
+     */
+    @JsonProperty("txtChosaJIsshiChikuCodeZen")
+    public void setTxtChosaJIsshiChikuCodeZen(TextBoxCode txtChosaJIsshiChikuCodeZen) {
+        this.txtChosaJIsshiChikuCodeZen = txtChosaJIsshiChikuCodeZen;
+    }
+
+    /*
+     * gettxtChosaJIsshiChikuZen
+     * @return txtChosaJIsshiChikuZen
+     */
+    @JsonProperty("txtChosaJIsshiChikuZen")
+    public TextBox getTxtChosaJIsshiChikuZen() {
+        return txtChosaJIsshiChikuZen;
+    }
+
+    /*
+     * settxtChosaJIsshiChikuZen
+     * @param txtChosaJIsshiChikuZen txtChosaJIsshiChikuZen
+     */
+    @JsonProperty("txtChosaJIsshiChikuZen")
+    public void setTxtChosaJIsshiChikuZen(TextBox txtChosaJIsshiChikuZen) {
+        this.txtChosaJIsshiChikuZen = txtChosaJIsshiChikuZen;
+    }
+
+    /*
+     * gettxtChosaKikanCodeZen
+     * @return txtChosaKikanCodeZen
+     */
+    @JsonProperty("txtChosaKikanCodeZen")
+    public TextBoxCode getTxtChosaKikanCodeZen() {
+        return txtChosaKikanCodeZen;
+    }
+
+    /*
+     * settxtChosaKikanCodeZen
+     * @param txtChosaKikanCodeZen txtChosaKikanCodeZen
+     */
+    @JsonProperty("txtChosaKikanCodeZen")
+    public void setTxtChosaKikanCodeZen(TextBoxCode txtChosaKikanCodeZen) {
+        this.txtChosaKikanCodeZen = txtChosaKikanCodeZen;
+    }
+
+    /*
+     * gettxtChosaKikanZen
+     * @return txtChosaKikanZen
+     */
+    @JsonProperty("txtChosaKikanZen")
+    public TextBox getTxtChosaKikanZen() {
+        return txtChosaKikanZen;
+    }
+
+    /*
+     * settxtChosaKikanZen
+     * @param txtChosaKikanZen txtChosaKikanZen
+     */
+    @JsonProperty("txtChosaKikanZen")
+    public void setTxtChosaKikanZen(TextBox txtChosaKikanZen) {
+        this.txtChosaKikanZen = txtChosaKikanZen;
+    }
+
+    /*
+     * gettxtChosaItakuKubunZen
+     * @return txtChosaItakuKubunZen
+     */
+    @JsonProperty("txtChosaItakuKubunZen")
+    public TextBox getTxtChosaItakuKubunZen() {
+        return txtChosaItakuKubunZen;
+    }
+
+    /*
+     * settxtChosaItakuKubunZen
+     * @param txtChosaItakuKubunZen txtChosaItakuKubunZen
+     */
+    @JsonProperty("txtChosaItakuKubunZen")
+    public void setTxtChosaItakuKubunZen(TextBox txtChosaItakuKubunZen) {
+        this.txtChosaItakuKubunZen = txtChosaItakuKubunZen;
+    }
+
+    /*
+     * gettxtChosainCodeZen
+     * @return txtChosainCodeZen
+     */
+    @JsonProperty("txtChosainCodeZen")
+    public TextBoxCode getTxtChosainCodeZen() {
+        return txtChosainCodeZen;
+    }
+
+    /*
+     * settxtChosainCodeZen
+     * @param txtChosainCodeZen txtChosainCodeZen
+     */
+    @JsonProperty("txtChosainCodeZen")
+    public void setTxtChosainCodeZen(TextBoxCode txtChosainCodeZen) {
+        this.txtChosainCodeZen = txtChosainCodeZen;
+    }
+
+    /*
+     * gettxtChosainZen
+     * @return txtChosainZen
+     */
+    @JsonProperty("txtChosainZen")
+    public TextBox getTxtChosainZen() {
+        return txtChosainZen;
+    }
+
+    /*
+     * settxtChosainZen
+     * @param txtChosainZen txtChosainZen
+     */
+    @JsonProperty("txtChosainZen")
+    public void setTxtChosainZen(TextBox txtChosainZen) {
+        this.txtChosainZen = txtChosainZen;
+    }
+
+    /*
+     * gettxtChosainShikakuZen
+     * @return txtChosainShikakuZen
+     */
+    @JsonProperty("txtChosainShikakuZen")
+    public TextBox getTxtChosainShikakuZen() {
+        return txtChosainShikakuZen;
+    }
+
+    /*
+     * settxtChosainShikakuZen
+     * @param txtChosainShikakuZen txtChosainShikakuZen
+     */
+    @JsonProperty("txtChosainShikakuZen")
+    public void setTxtChosainShikakuZen(TextBox txtChosainShikakuZen) {
+        this.txtChosainShikakuZen = txtChosainShikakuZen;
+    }
+
+    /*
+     * gettxtDenwaBangoZen
+     * @return txtDenwaBangoZen
+     */
+    @JsonProperty("txtDenwaBangoZen")
+    public TextBoxTelNo getTxtDenwaBangoZen() {
+        return txtDenwaBangoZen;
+    }
+
+    /*
+     * settxtDenwaBangoZen
+     * @param txtDenwaBangoZen txtDenwaBangoZen
+     */
+    @JsonProperty("txtDenwaBangoZen")
+    public void setTxtDenwaBangoZen(TextBoxTelNo txtDenwaBangoZen) {
+        this.txtDenwaBangoZen = txtDenwaBangoZen;
+    }
+
+    /*
+     * gettxtFaxZen
+     * @return txtFaxZen
+     */
+    @JsonProperty("txtFaxZen")
+    public TextBoxTelNo getTxtFaxZen() {
+        return txtFaxZen;
+    }
+
+    /*
+     * settxtFaxZen
+     * @param txtFaxZen txtFaxZen
+     */
+    @JsonProperty("txtFaxZen")
+    public void setTxtFaxZen(TextBoxTelNo txtFaxZen) {
+        this.txtFaxZen = txtFaxZen;
+    }
+
+    /*
+     * getlblSaiChosaZen
+     * @return lblSaiChosaZen
+     */
+    @JsonProperty("lblSaiChosaZen")
+    public Label getLblSaiChosaZen() {
+        return lblSaiChosaZen;
+    }
+
+    /*
+     * setlblSaiChosaZen
+     * @param lblSaiChosaZen lblSaiChosaZen
+     */
+    @JsonProperty("lblSaiChosaZen")
+    public void setLblSaiChosaZen(Label lblSaiChosaZen) {
+        this.lblSaiChosaZen = lblSaiChosaZen;
+    }
+
+    /*
+     * gettxtChosaBashoKubunSaiZen
+     * @return txtChosaBashoKubunSaiZen
+     */
+    @JsonProperty("txtChosaBashoKubunSaiZen")
+    public TextBox getTxtChosaBashoKubunSaiZen() {
+        return txtChosaBashoKubunSaiZen;
+    }
+
+    /*
+     * settxtChosaBashoKubunSaiZen
+     * @param txtChosaBashoKubunSaiZen txtChosaBashoKubunSaiZen
+     */
+    @JsonProperty("txtChosaBashoKubunSaiZen")
+    public void setTxtChosaBashoKubunSaiZen(TextBox txtChosaBashoKubunSaiZen) {
+        this.txtChosaBashoKubunSaiZen = txtChosaBashoKubunSaiZen;
+    }
+
+    /*
+     * gettxtChosaJisshibiSaiZen
+     * @return txtChosaJisshibiSaiZen
+     */
+    @JsonProperty("txtChosaJisshibiSaiZen")
+    public TextBoxDate getTxtChosaJisshibiSaiZen() {
+        return txtChosaJisshibiSaiZen;
+    }
+
+    /*
+     * settxtChosaJisshibiSaiZen
+     * @param txtChosaJisshibiSaiZen txtChosaJisshibiSaiZen
+     */
+    @JsonProperty("txtChosaJisshibiSaiZen")
+    public void setTxtChosaJisshibiSaiZen(TextBoxDate txtChosaJisshibiSaiZen) {
+        this.txtChosaJisshibiSaiZen = txtChosaJisshibiSaiZen;
+    }
+
+    /*
+     * gettxtChosaJIsshiChikuCodeSaiZen
+     * @return txtChosaJIsshiChikuCodeSaiZen
+     */
+    @JsonProperty("txtChosaJIsshiChikuCodeSaiZen")
+    public TextBoxCode getTxtChosaJIsshiChikuCodeSaiZen() {
+        return txtChosaJIsshiChikuCodeSaiZen;
+    }
+
+    /*
+     * settxtChosaJIsshiChikuCodeSaiZen
+     * @param txtChosaJIsshiChikuCodeSaiZen txtChosaJIsshiChikuCodeSaiZen
+     */
+    @JsonProperty("txtChosaJIsshiChikuCodeSaiZen")
+    public void setTxtChosaJIsshiChikuCodeSaiZen(TextBoxCode txtChosaJIsshiChikuCodeSaiZen) {
+        this.txtChosaJIsshiChikuCodeSaiZen = txtChosaJIsshiChikuCodeSaiZen;
+    }
+
+    /*
+     * gettxtChosaJIsshiChikuSaiZen
+     * @return txtChosaJIsshiChikuSaiZen
+     */
+    @JsonProperty("txtChosaJIsshiChikuSaiZen")
+    public TextBox getTxtChosaJIsshiChikuSaiZen() {
+        return txtChosaJIsshiChikuSaiZen;
+    }
+
+    /*
+     * settxtChosaJIsshiChikuSaiZen
+     * @param txtChosaJIsshiChikuSaiZen txtChosaJIsshiChikuSaiZen
+     */
+    @JsonProperty("txtChosaJIsshiChikuSaiZen")
+    public void setTxtChosaJIsshiChikuSaiZen(TextBox txtChosaJIsshiChikuSaiZen) {
+        this.txtChosaJIsshiChikuSaiZen = txtChosaJIsshiChikuSaiZen;
+    }
+
+    /*
+     * gettxtChosaKikanCodeSaiZen
+     * @return txtChosaKikanCodeSaiZen
+     */
+    @JsonProperty("txtChosaKikanCodeSaiZen")
+    public TextBoxCode getTxtChosaKikanCodeSaiZen() {
+        return txtChosaKikanCodeSaiZen;
+    }
+
+    /*
+     * settxtChosaKikanCodeSaiZen
+     * @param txtChosaKikanCodeSaiZen txtChosaKikanCodeSaiZen
+     */
+    @JsonProperty("txtChosaKikanCodeSaiZen")
+    public void setTxtChosaKikanCodeSaiZen(TextBoxCode txtChosaKikanCodeSaiZen) {
+        this.txtChosaKikanCodeSaiZen = txtChosaKikanCodeSaiZen;
+    }
+
+    /*
+     * gettxtChosaKikanSaiZen
+     * @return txtChosaKikanSaiZen
+     */
+    @JsonProperty("txtChosaKikanSaiZen")
+    public TextBox getTxtChosaKikanSaiZen() {
+        return txtChosaKikanSaiZen;
+    }
+
+    /*
+     * settxtChosaKikanSaiZen
+     * @param txtChosaKikanSaiZen txtChosaKikanSaiZen
+     */
+    @JsonProperty("txtChosaKikanSaiZen")
+    public void setTxtChosaKikanSaiZen(TextBox txtChosaKikanSaiZen) {
+        this.txtChosaKikanSaiZen = txtChosaKikanSaiZen;
+    }
+
+    /*
+     * gettxtChosaItakuKubunSaiZen
+     * @return txtChosaItakuKubunSaiZen
+     */
+    @JsonProperty("txtChosaItakuKubunSaiZen")
+    public TextBox getTxtChosaItakuKubunSaiZen() {
+        return txtChosaItakuKubunSaiZen;
+    }
+
+    /*
+     * settxtChosaItakuKubunSaiZen
+     * @param txtChosaItakuKubunSaiZen txtChosaItakuKubunSaiZen
+     */
+    @JsonProperty("txtChosaItakuKubunSaiZen")
+    public void setTxtChosaItakuKubunSaiZen(TextBox txtChosaItakuKubunSaiZen) {
+        this.txtChosaItakuKubunSaiZen = txtChosaItakuKubunSaiZen;
+    }
+
+    /*
+     * gettxtChosainCodeSaiZen
+     * @return txtChosainCodeSaiZen
+     */
+    @JsonProperty("txtChosainCodeSaiZen")
+    public TextBoxCode getTxtChosainCodeSaiZen() {
+        return txtChosainCodeSaiZen;
+    }
+
+    /*
+     * settxtChosainCodeSaiZen
+     * @param txtChosainCodeSaiZen txtChosainCodeSaiZen
+     */
+    @JsonProperty("txtChosainCodeSaiZen")
+    public void setTxtChosainCodeSaiZen(TextBoxCode txtChosainCodeSaiZen) {
+        this.txtChosainCodeSaiZen = txtChosainCodeSaiZen;
+    }
+
+    /*
+     * gettxtChosainSaiZen
+     * @return txtChosainSaiZen
+     */
+    @JsonProperty("txtChosainSaiZen")
+    public TextBox getTxtChosainSaiZen() {
+        return txtChosainSaiZen;
+    }
+
+    /*
+     * settxtChosainSaiZen
+     * @param txtChosainSaiZen txtChosainSaiZen
+     */
+    @JsonProperty("txtChosainSaiZen")
+    public void setTxtChosainSaiZen(TextBox txtChosainSaiZen) {
+        this.txtChosainSaiZen = txtChosainSaiZen;
+    }
+
+    /*
+     * gettxtChosainShikakuSaiZen
+     * @return txtChosainShikakuSaiZen
+     */
+    @JsonProperty("txtChosainShikakuSaiZen")
+    public TextBox getTxtChosainShikakuSaiZen() {
+        return txtChosainShikakuSaiZen;
+    }
+
+    /*
+     * settxtChosainShikakuSaiZen
+     * @param txtChosainShikakuSaiZen txtChosainShikakuSaiZen
+     */
+    @JsonProperty("txtChosainShikakuSaiZen")
+    public void setTxtChosainShikakuSaiZen(TextBox txtChosainShikakuSaiZen) {
+        this.txtChosainShikakuSaiZen = txtChosainShikakuSaiZen;
+    }
+
+    /*
+     * gettxtDenwaBangoSaiZen
+     * @return txtDenwaBangoSaiZen
+     */
+    @JsonProperty("txtDenwaBangoSaiZen")
+    public TextBoxTelNo getTxtDenwaBangoSaiZen() {
+        return txtDenwaBangoSaiZen;
+    }
+
+    /*
+     * settxtDenwaBangoSaiZen
+     * @param txtDenwaBangoSaiZen txtDenwaBangoSaiZen
+     */
+    @JsonProperty("txtDenwaBangoSaiZen")
+    public void setTxtDenwaBangoSaiZen(TextBoxTelNo txtDenwaBangoSaiZen) {
+        this.txtDenwaBangoSaiZen = txtDenwaBangoSaiZen;
+    }
+
+    /*
+     * gettxtFaxSaiZen
+     * @return txtFaxSaiZen
+     */
+    @JsonProperty("txtFaxSaiZen")
+    public TextBoxTelNo getTxtFaxSaiZen() {
+        return txtFaxSaiZen;
+    }
+
+    /*
+     * settxtFaxSaiZen
+     * @param txtFaxSaiZen txtFaxSaiZen
+     */
+    @JsonProperty("txtFaxSaiZen")
+    public void setTxtFaxSaiZen(TextBoxTelNo txtFaxSaiZen) {
+        this.txtFaxSaiZen = txtFaxSaiZen;
+    }
+
+    /*
+     * gettxtHomonUmuZen
+     * @return txtHomonUmuZen
+     */
+    @JsonProperty("txtHomonUmuZen")
+    public TextBox getTxtHomonUmuZen() {
+        return txtHomonUmuZen;
+    }
+
+    /*
+     * settxtHomonUmuZen
+     * @param txtHomonUmuZen txtHomonUmuZen
+     */
+    @JsonProperty("txtHomonUmuZen")
+    public void setTxtHomonUmuZen(TextBox txtHomonUmuZen) {
+        this.txtHomonUmuZen = txtHomonUmuZen;
+    }
+
+    // </editor-fold>
+}
