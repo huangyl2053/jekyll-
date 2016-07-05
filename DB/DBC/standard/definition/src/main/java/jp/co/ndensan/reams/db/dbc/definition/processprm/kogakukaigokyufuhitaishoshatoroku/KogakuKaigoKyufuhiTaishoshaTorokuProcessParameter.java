d338e2f86ad90c76c707233a049bc78c9c721b09
huzongcheng <huzongcheng@liandisys.com.cn>
2016-07-05 15:51:45 +0800

    バッチ設計_DBCMN41002_高額介護サービス費給付対象者登録

---
+++ DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/processprm/kogakukaigokyufuhitaishoshatoroku/KogakuKaigoKyufuhiTaishoshaTorokuProcessParameter.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.definition.processprm.kogakukaigokyufuhitaishoshatoroku;
+
+import jp.co.ndensan.reams.uz.uza.batch.parameter.IBatchProcessParameter;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleYearMonth;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+/**
+ *
+ * 高額介護サービス費給付対象者登録のProcess用パラメータクラスです。
+ *
+ * @reamsid_L DBC-2010-040 huzongcheng
+ */
+@lombok.Getter
+@lombok.Setter
+@SuppressWarnings("PMD.UnusedPrivateField")
+public class KogakuKaigoKyufuhiTaishoshaTorokuProcessParameter implements IBatchProcessParameter {
+
+    private RString menuId;
+    private RString shuturyokuJunn;
+    private FlexibleYearMonth ShinsaYMFrom;
+    private FlexibleYearMonth ShinsaYMTo;
+    private boolean shuturyokuFlg;
+
+    /**
+     * コンストラクタです。
+     *
+     *
+     * @param menuId RString
+     * @param shuturyokuJunn RString
+     * @param ShinsaYMFrom FlexibleYearMonth
+     * @param ShinsaYMTo FlexibleYearMonth
+     * @param shuturyokuFlg boolean
+     */
+    public KogakuKaigoKyufuhiTaishoshaTorokuProcessParameter(
+            RString menuId,
+            RString shuturyokuJunn,
+            FlexibleYearMonth ShinsaYMFrom,
+            FlexibleYearMonth ShinsaYMTo,
+            boolean shuturyokuFlg) {
+        this.menuId = menuId;
+        this.shuturyokuJunn = shuturyokuJunn;
+        this.ShinsaYMFrom = ShinsaYMFrom;
+        this.ShinsaYMTo = ShinsaYMTo;
+        this.shuturyokuFlg = shuturyokuFlg;
+    }
+
+    /**
+     * コンストラクタです。
+     *
+     *
+     */
+    public KogakuKaigoKyufuhiTaishoshaTorokuProcessParameter() {
+    }
+
+}
