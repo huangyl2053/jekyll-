957762687aff0cb9f1ddd44e9eb237a13f7f2b48
姜岐 <n8344@ndensan.co.jp>
2016-07-07 20:06:29 +0900

    特殊修正UIコンテナ修正

--- DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD5520001/DBD5520001StateName.java
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD5520001/DBD5520001StateName.java
@@ -15,7 +15,7 @@ public enum DBD5520001StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
     NoChange(""),
-    Default("default");
+    修正("修正");
 
     private final RString name;
 
