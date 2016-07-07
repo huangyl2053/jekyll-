4e4a1a827c2d0e978334d3899cd4e5df8aa19a41
wangkanglei <wangkanglei@liandisys.com.cn>
2016-07-07 09:14:04 +0800

    画面設計_TaishoshaIchiran_対象者一覧  QA No.905修正

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/TaishoshaIchiran/TaishoshaIchiran/TaishoshaIchiranDivHandler.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/TaishoshaIchiran/TaishoshaIchiran/TaishoshaIchiranDivHandler.java
@@ -56,10 +56,12 @@ public final class TaishoshaIchiranDivHandler {
      * @param 異動日From FlexibleDate
      * @param 異動日To FlexibleDate
      * @param 表示モード RString
+     * @param 削除データ検索 RString
      * @param 対象者一覧情報 List<KyodoJukyushaTaishoshaEntity>
      */
     public void initialize(RString メニューID, HihokenshaNo 被保険者番号, FlexibleDate 異動日From,
-            FlexibleDate 異動日To, RString 表示モード, List<KyodoJukyushaTaishoshaEntity> 対象者一覧情報) {
+            FlexibleDate 異動日To, RString 表示モード, RString 削除データ検索,
+            List<KyodoJukyushaTaishoshaEntity> 対象者一覧情報) {
         if (メニューID != null && !メニューID.isEmpty()) {
             div.setMenuID(メニューID);
         }
@@ -88,6 +90,7 @@ public final class TaishoshaIchiranDivHandler {
         if (被保険者番号 != null && !被保険者番号.isEmpty()) {
             div.setHihoNo(DataPassingConverter.serialize(被保険者番号));
         }
+        div.setDeleteDateFlag(削除データ検索);
     }
 
     private void setGrid(List<KyodoJukyushaTaishoshaEntity> 対象者一覧情報) {
@@ -166,10 +169,9 @@ public final class TaishoshaIchiranDivHandler {
         FlexibleDate 異動日From = DataPassingConverter.deserialize(div.getIdoFromYMD(), FlexibleDate.class);
         FlexibleDate 異動日To = DataPassingConverter.deserialize(div.getIdoToYMD(), FlexibleDate.class);
         HihokenshaNo 被保険者番号 = DataPassingConverter.deserialize(div.getHihoNo(), HihokenshaNo.class);
-
-        //TODO QA内部番号905
+        RString 削除データ検索 = div.getDeleteDateFlag();
         TaishoshaIchiranParameter parameter = new TaishoshaIchiranParameter(異動日From, 異動日To,
-                被保険者番号, null);
+                被保険者番号, 削除データ検索);
         return parameter;
     }
 
--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/TaishoshaIchiran/TaishoshaIchiran/dgJukyushaIdoRenrakuhyoHenkoSearchResultList_Row.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/TaishoshaIchiran/TaishoshaIchiran/dgJukyushaIdoRenrakuhyoHenkoSearchResultList_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbc.divcontroller.entity.commonchilddiv.Taishosha
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Thu Jun 30 16:18:23 CST 2016
+ * Wed Jul 06 20:56:28 CST 2016
  */
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
