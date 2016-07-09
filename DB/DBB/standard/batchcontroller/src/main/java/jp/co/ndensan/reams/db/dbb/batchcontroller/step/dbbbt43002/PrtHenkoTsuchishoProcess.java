c08ccfe84aaed4842eba2eb158a0e07b2691ea49
xicongwang <xicongwang@liandisys.com.cn>
2016-07-09 12:41:19 +0800

    DBB-0780-050_本算定通知書一括発行(ソースを修正)

--- DB/DBB/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/batchcontroller/step/dbbbt43002/PrtHenkoTsuchishoProcess.java
+++ DB/DBB/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/batchcontroller/step/dbbbt43002/PrtHenkoTsuchishoProcess.java
@@ -28,13 +28,13 @@ public class PrtHenkoTsuchishoProcess extends SimpleBatchProcessBase {
 
     @Override
     protected void process() {
-        //TODO 「変更通知書出力対象区分」 バッチパラメータ．「変更通知書」　≪出力内容≫　対象者 :引数が存在しない
         HonsanteiTsuchishoIkkatsuHakko manager = HonsanteiTsuchishoIkkatsuHakko.createInstance();
         HonsanteifukaBatchTyouhyou 出力帳票一覧 = processParameter.get出力帳票一覧();
         try {
-            manager.prtHenkoTsuchisho(processParameter.get調定年度(), processParameter.get決定変更_発行日(), processParameter.get決定変更_文書番号(),
+            manager.prtHenkoTsuchisho(processParameter.get調定年度(), processParameter.get決定変更_発行日(),
+                    processParameter.get決定変更_文書番号(),
                     processParameter.get帳票作成日時().getRDateTime(), 出力帳票一覧.get出力順ID(), 出力帳票一覧.get帳票ID(),
-                    processParameter.is一括発行起動フラグ(), processParameter.get打分け条件情報(), null);
+                    processParameter.is一括発行起動フラグ(), processParameter.get打分け条件情報());
         } catch (InvocationTargetException ex) {
             Logger.getLogger(PrtHenkoTsuchishoProcess.class.getName()).log(Level.SEVERE, null, ex);
         }
