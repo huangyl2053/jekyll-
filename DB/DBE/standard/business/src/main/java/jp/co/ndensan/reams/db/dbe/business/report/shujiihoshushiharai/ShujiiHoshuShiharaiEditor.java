670b27911ca2dcf99ef3985292efca5725abccb3
xuyongchao <xuyongchao@liandisys.com.cn>
2016-07-09 14:42:25 +0800

    pageCount2をpageCount#2に変更した

--- DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/shujiihoshushiharai/ShujiiHoshuShiharaiEditor.java
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/shujiihoshushiharai/ShujiiHoshuShiharaiEditor.java
@@ -51,7 +51,7 @@ public class ShujiiHoshuShiharaiEditor implements IShujiiHoshuShiharaiEditor {
         source.meishoFuyo = item.get名称付与();
         source.customerBarCode = item.getバーコード();
         source.sonota = item.getその他();
-        source.pageCount2 = item.getページ番号();
+        source.pageCount = item.getページ番号();
         source.title = DbBusinessConfig.get(ConfigNameDBE.主治医意見書作成報酬支払通知書, RDate.getNowDate(), SubGyomuCode.DBE認定支援,
                 LASDEC_CODE, ConfigNameDBE.主治医意見書作成報酬支払通知書.get名称());
         source.tsuchibun1 = item.get通知文1();
