d9e2ae5e3e7a06ac41b2c79e44507ac557814764
三浦 凌 <n3327@ndensan.co.jp>
2016-07-09 20:45:31 +0900

    賦課比較　Optionalの使い方が間違っていたので修正。

--- DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/controller/parentdiv/DBB0320005/input/FukaHikakuTargets.java
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/controller/parentdiv/DBB0320005/input/FukaHikakuTargets.java
@@ -35,10 +35,10 @@ public class FukaHikakuTargets {
     }
 
     private static Optional<Fuka> findFukaBy(FukaManager manager, FukaShokaiKey key) {
-        return Optional.of(manager.get介護賦課(key.get調定年度(), key.get賦課年度(), key.get通知書番号(), key.get履歴番号()));
+        return Optional.ofNullable(manager.get介護賦課(key.get調定年度(), key.get賦課年度(), key.get通知書番号(), key.get履歴番号()));
     }
 
- //   private static Optional<Fuka> findFukaBy任意比較(FukaManager manager, FukaShokaiKey key) {
+    //   private static Optional<Fuka> findFukaBy任意比較(FukaManager manager, FukaShokaiKey key) {
 //        return manager.get介護賦課For任意対象比較(key.get調定年度(), key.get賦課年度(), key.get被保険者番号(), key.get更正日時().getRDateTime());
 //        return manager.get介護賦課For任意対象比較(key.get調定年度(), key.get賦課年度(), key.get通知書番号(), key.get履歴番号());
 //    }
