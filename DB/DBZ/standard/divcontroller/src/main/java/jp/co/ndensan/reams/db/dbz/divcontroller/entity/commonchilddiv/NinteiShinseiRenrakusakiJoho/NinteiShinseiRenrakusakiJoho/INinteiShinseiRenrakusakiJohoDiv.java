9225de03887812af501c2072f4341f6686bc449c
三浦 凌 <n3327@ndensan.co.jp>
2016-07-09 09:20:36 +0900

    自動生成でIDialogDivが消えてしまったので修正

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/NinteiShinseiRenrakusakiJoho/NinteiShinseiRenrakusakiJoho/INinteiShinseiRenrakusakiJohoDiv.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/NinteiShinseiRenrakusakiJoho/NinteiShinseiRenrakusakiJoho/INinteiShinseiRenrakusakiJohoDiv.java
@@ -1,11 +1,12 @@
 package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.NinteiShinseiRenrakusakiJoho.NinteiShinseiRenrakusakiJoho;
 
 import jp.co.ndensan.reams.uz.uza.ui.binding.ICommonChildDivBaseProperties;
+import jp.co.ndensan.reams.uz.uza.ui.binding.IDialogDiv;
 
 /*
  * このコードはツールによって生成されました。
  * @reamsid_L DBZ-1300-100 dongyabin
  */
-public interface INinteiShinseiRenrakusakiJohoDiv extends ICommonChildDivBaseProperties {
+public interface INinteiShinseiRenrakusakiJohoDiv extends ICommonChildDivBaseProperties, IDialogDiv {
 
 }
