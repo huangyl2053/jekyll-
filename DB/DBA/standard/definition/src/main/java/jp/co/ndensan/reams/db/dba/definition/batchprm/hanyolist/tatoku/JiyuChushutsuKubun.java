c4880aad702409d7f290d4c66a8be42517307cca
wangchao <wangchao@liandisys.com.cn>
2016-07-11 21:00:45 +0800

    DBA介護資格_Enum設定表.xlsにより、ソースを反映

---
+++ DB/DBA/standard/definition/src/main/java/jp/co/ndensan/reams/db/dba/definition/batchprm/hanyolist/tatoku/JiyuChushutsuKubun.java
@@ -0,0 +1,66 @@
+package jp.co.ndensan.reams.db.dba.definition.batchprm.hanyolist.tatoku;
+
+import jp.co.ndensan.reams.ur.urz.definition.message.UrSystemErrorMessages;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+/**
+ * 事由抽出区分を表す列挙型です。
+ *
+ * @reamsid_L DBA-9999-023 wangchao
+ */
+public enum JiyuChushutsuKubun {
+
+    /**
+     * コード:1 名称:他特例適用者のみ 略称:定義なし
+     */
+    他特例適用者のみ("1", "他特例適用者のみ"),
+    /**
+     * コード:2 名称:他特例解除者のみ 略称:定義なし
+     */
+    他特例解除者のみ("2", "他特例解除者のみ"),
+    /**
+     * コード:0 名称:両方 略称:定義なし
+     */
+    両方("0", "両方");
+
+    private final RString code;
+    private final RString fullName;
+
+    private JiyuChushutsuKubun(String code, String fullname) {
+        this.code = new RString(code);
+        this.fullName = new RString(fullname);
+    }
+
+    /**
+     * 事由抽出区分のコードを返します。
+     *
+     * @return 事由抽出区分のコード
+     */
+    public RString getコード() {
+        return code;
+    }
+
+    /**
+     * 事由抽出区分の名称を返します。
+     *
+     * @return 事由抽出区分の名称
+     */
+    public RString get名称() {
+        return fullName;
+    }
+
+    /**
+     * 事由抽出区分のコードと一致する内容を探します。
+     *
+     * @param code 事由抽出区分のコード
+     * @return {@code code} に対応する事由抽出区分
+     */
+    public static JiyuChushutsuKubun toValue(RString code) {
+        for (JiyuChushutsuKubun jiyuChushutsuKubun : JiyuChushutsuKubun.values()) {
+            if (jiyuChushutsuKubun.code.equals(code)) {
+                return jiyuChushutsuKubun;
+            }
+        }
+        throw new IllegalArgumentException(UrSystemErrorMessages.変換不可.getReplacedMessage("事由抽出区分"));
+    }
+}
