946aa2ffacb9ec50b2926db3db170a4fbb0c6b81
lit <lit@liandisys.com.cn>
2016-07-13 13:27:42 +0800

    画面設計_DBD5020001_要介護認定処理 DBD-1530-010の実装

--- DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD5020001/DBD5020001TransitionEventName.java
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD5020001/DBD5020001TransitionEventName.java
@@ -1,27 +1,26 @@
 package jp.co.ndensan.reams.db.dbd.divcontroller.entity.parentdiv.DBD5020001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 
 /**
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBD5020001の状態名定義クラスです
+ * DBD5020001のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBD5020001StateName implements IStateEnumerations {
+public enum DBD5020001TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    NoChange(""),
-    要介護認定("要介護認定"),
-    却下完了("却下完了"),
-    認定完了("認定完了");
+    区分変更通知書を発行("区分変更通知書を発行"),
+    サービス変更通知書を発行("サービス変更通知書を発行"),
+    一覧へ戻る("一覧へ戻る");
 
     private final RString name;
 
-    private DBD5020001StateName(String name) {
+    private DBD5020001TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
