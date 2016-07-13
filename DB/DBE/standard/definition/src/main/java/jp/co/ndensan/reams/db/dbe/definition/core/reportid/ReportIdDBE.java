d857c94d3dc17c7e02ac58b586245b962b2837c9
lishengli <lishengli@liandisys.com.cn>
2016-07-13 08:59:59 +0800

    帳票設計_DBE231003_主治医意見書（オルカ）修正

--- DB/DBE/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbe/definition/core/reportid/ReportIdDBE.java
+++ DB/DBE/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbe/definition/core/reportid/ReportIdDBE.java
@@ -16,6 +16,14 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
 public enum ReportIdDBE {
 
     /**
+     * 主治医意見書（オルカ）の帳票ID。
+     */
+    DBE231102(new ReportId("DBE231102_ikenshokinyuyoshi"), new RString("主治医意見書（オルカ）")),
+    /**
+     * 主治医意見書（オルカ）の帳票ID。
+     */
+    DBE231101(new ReportId("DBE231101_ikenshokinyuyoshi"), new RString("主治医意見書（オルカ）")),
+    /**
      * 認定調査報酬照会の帳票ID。
      */
     DBE601006(new ReportId("DBE601006_chosahoshuichiran"), new RString("認定調査報酬照会")),
