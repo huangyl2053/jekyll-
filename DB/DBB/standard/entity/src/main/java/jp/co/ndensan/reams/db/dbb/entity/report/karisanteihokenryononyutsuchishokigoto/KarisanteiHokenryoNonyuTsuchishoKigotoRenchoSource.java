b7efda133252d53e226b96ed480a46a003cb9054
wangjie2 <wangjie2@liandisys.com.cn>
2016-07-15 09:18:07 +0800

    帳票設計_DBBRP00007_2_保険料納入通知書（仮算定）【期毎タイプ】　訂正

--- DB/DBB/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbb/entity/report/karisanteihokenryononyutsuchishokigoto/KarisanteiHokenryoNonyuTsuchishoKigotoRenchoSource.java
+++ DB/DBB/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbb/entity/report/karisanteihokenryononyutsuchishokigoto/KarisanteiHokenryoNonyuTsuchishoKigotoRenchoSource.java
@@ -17,21 +17,21 @@ public class KarisanteiHokenryoNonyuTsuchishoKigotoRenchoSource implements IRepo
     public RString titleNendo;
     @ReportItem(name = "hyojiKomoku11", length = 8, order = 2)
     public RString hyojiKomoku11;
-    @ReportItem(name = "hyojicodeName1", length = 6, order = 3)
+    @ReportItem(name = "HyojicodeName1", length = 6, order = 3)
     public RString hyojicodeName1;
     @ReportItem(name = "hyojiKomoku21", length = 8, order = 4)
     public RString hyojiKomoku21;
-    @ReportItem(name = "hyojicodeName2", length = 6, order = 5)
+    @ReportItem(name = "HyojicodeName2", length = 6, order = 5)
     public RString hyojicodeName2;
     @ReportItem(name = "hyojiKomoku31", length = 8, order = 6)
     public RString hyojiKomoku31;
-    @ReportItem(name = "hyojicodeName3", length = 6, order = 7)
+    @ReportItem(name = "HyojicodeName3", length = 6, order = 7)
     public RString hyojicodeName3;
-    @ReportItem(name = "hyojicode1", length = 15, order = 8)
+    @ReportItem(name = "Hyojicode1", length = 15, order = 8)
     public RString hyojicode1;
-    @ReportItem(name = "hyojicode2", length = 15, order = 9)
+    @ReportItem(name = "Hyojicode2", length = 15, order = 9)
     public RString hyojicode2;
-    @ReportItem(name = "hyojicode3", length = 15, order = 10)
+    @ReportItem(name = "Hyojicode3", length = 15, order = 10)
     public RString hyojicode3;
     @ReportItem(name = "tsuchishoNo", order = 11)
     public RString tsuchishoNo;
