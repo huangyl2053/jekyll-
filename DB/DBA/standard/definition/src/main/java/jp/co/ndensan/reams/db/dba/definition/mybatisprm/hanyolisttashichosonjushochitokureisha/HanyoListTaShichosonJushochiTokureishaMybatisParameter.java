e890b4666fe0cee8bff1f735abcb47a51ed6010b
yaodongsheng <yaodongsheng@liandisys.com.cn>
2016-07-18 11:43:57 +0800

    バッチ設計_DBABT13003_汎用リスト 他市町村住所地特例者 新規作成 画面設計_DBAGM13002_汎用リスト（資格） 指摘対応

---
+++ DB/DBA/standard/definition/src/main/java/jp/co/ndensan/reams/db/dba/definition/mybatisprm/hanyolisttashichosonjushochitokureisha/HanyoListTaShichosonJushochiTokureishaMybatisParameter.java
@@ -0,0 +1,321 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dba.definition.mybatisprm.hanyolisttashichosonjushochitokureisha;
+
+import java.util.List;
+import jp.co.ndensan.reams.db.dba.definition.batchprm.hanyolist.hihokenshadaicho.HizukeChushutsuKubun;
+import jp.co.ndensan.reams.db.dba.definition.batchprm.hanyolist.tatoku.HaniChushutsubiKubun;
+import jp.co.ndensan.reams.db.dba.definition.batchprm.hanyolist.tatoku.JiyuChushutsuKubun;
+import jp.co.ndensan.reams.db.dba.definition.batchprm.hanyolist.tatoku.KijunbiKubun;
+import jp.co.ndensan.reams.db.dbz.definition.batchprm.hanyolist.atena.NenreiSoChushutsuHoho;
+import jp.co.ndensan.reams.uz.uza.batch.parameter.IMyBatisParameter;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.math.Decimal;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 汎用リスト_他市町村住所地特例者のMyBatisパラメータ。
+ *
+ * @reamsid_L DBA-1590-030 yaodongsheng
+ */
+@SuppressWarnings("PMD.UnusedPrivateField")
+@Getter
+@Setter
+public class HanyoListTaShichosonJushochiTokureishaMybatisParameter implements IMyBatisParameter {
+
+    private final RString kijunYMD;
+    private final RString haniChushutsunichiFrom;
+    private final RString haniChushutsunichiTo;
+    private final boolean is直近;
+    private final boolean is基準日;
+    private final boolean is適用開始日;
+    private final boolean is適用開始届出日;
+    private final boolean is範囲;
+    private final boolean is適用日;
+    private final boolean is適用届出日;
+    private final boolean is解除日;
+    private final boolean is解除届出日;
+    private final boolean has範囲抽出日From;
+    private final boolean has範囲抽出日To;
+    private final boolean is他特例適用者のみ;
+    private final boolean is他特例解除者のみ;
+    private final boolean has適用事由;
+    private final boolean has解除事由;
+    private final boolean is施設変更あり;
+    private final boolean is施設変更なし;
+    private final boolean is年齢;
+    private final boolean has年齢範囲開始;
+    private final boolean has年齢範囲終了;
+    private final RDate 生年月日範囲終了;
+    private final RDate 生年月日範囲開始;
+    private final boolean is生年月日;
+    private final boolean has生年月日範囲終了;
+    private final boolean has生年月日範囲開始;
+    private final List<RString> tekiyoJiyu;
+    private final List<RString> kaijoJiyu;
+    private final RString psmShikibetsuTaisho;
+    private final RString psmAtesaki;
+
+    /**
+     * コンストラクタです。
+     *
+     * @param kijunYMD 基準日
+     * @param haniChushutsunichiFrom 範囲抽出日From
+     * @param haniChushutsunichiTo 範囲抽出日To
+     * @param is直近 is直近
+     * @param is基準日 is基準日
+     * @param is適用開始日 is適用開始日
+     * @param is適用開始届出日 is適用開始届出日
+     * @param is範囲 is範囲
+     * @param is適用日 is適用日
+     * @param has範囲抽出日From has範囲抽出日From
+     * @param has範囲抽出日To has範囲抽出日To
+     * @param is適用届出日 is適用届出日
+     * @param is解除日 is解除日
+     * @param is解除届出日 is解除届出日
+     * @param is他特例適用者のみ is他特例適用者のみ
+     * @param is他特例解除者のみ is他特例解除者のみ
+     * @param has適用事由 has適用事由
+     * @param has解除事由 has解除事由
+     * @param tekiyoJiyu 適用事由
+     * @param kaijoJiyu 解除事由
+     * @param is施設変更あり is施設変更あり
+     * @param is施設変更なし is施設変更なし
+     * @param is年齢 is年齢
+     * @param has年齢範囲開始 has年齢範囲開始
+     * @param has年齢範囲終了 has年齢範囲終了
+     * @param 生年月日範囲終了 生年月日範囲終了
+     * @param 生年月日範囲開始 生年月日範囲開始
+     * @param is生年月日 is生年月日
+     * @param has生年月日範囲終了 has生年月日範囲終了
+     * @param has生年月日範囲開始 has生年月日範囲開始
+     * @param psmShikibetsuTaisho 宛名識別対象抽出PSM
+     * @param psmAtesaki 宛先抽出PSM
+     */
+    protected HanyoListTaShichosonJushochiTokureishaMybatisParameter(
+            RString kijunYMD,
+            RString haniChushutsunichiFrom,
+            RString haniChushutsunichiTo,
+            boolean is直近,
+            boolean is基準日,
+            boolean is適用開始日,
+            boolean is適用開始届出日,
+            boolean is範囲,
+            boolean is適用日,
+            boolean has範囲抽出日From,
+            boolean has範囲抽出日To,
+            boolean is適用届出日,
+            boolean is解除日,
+            boolean is解除届出日,
+            boolean is他特例適用者のみ,
+            boolean is他特例解除者のみ,
+            boolean has適用事由,
+            boolean has解除事由,
+            List<RString> tekiyoJiyu,
+            List<RString> kaijoJiyu,
+            boolean is施設変更あり,
+            boolean is施設変更なし,
+            boolean is年齢,
+            boolean has年齢範囲開始,
+            boolean has年齢範囲終了,
+            RDate 生年月日範囲終了,
+            RDate 生年月日範囲開始,
+            boolean is生年月日,
+            boolean has生年月日範囲終了,
+            boolean has生年月日範囲開始,
+            RString psmShikibetsuTaisho,
+            RString psmAtesaki
+    ) {
+        this.kijunYMD = kijunYMD;
+        this.haniChushutsunichiFrom = haniChushutsunichiFrom;
+        this.haniChushutsunichiTo = haniChushutsunichiTo;
+        this.is直近 = is直近;
+        this.is基準日 = is基準日;
+        this.is適用開始日 = is適用開始日;
+        this.is適用開始届出日 = is適用開始届出日;
+        this.is範囲 = is範囲;
+        this.is適用日 = is適用日;
+        this.has範囲抽出日From = has範囲抽出日From;
+        this.has範囲抽出日To = has範囲抽出日To;
+        this.is適用届出日 = is適用届出日;
+        this.is解除日 = is解除日;
+        this.is解除届出日 = is解除届出日;
+        this.is他特例適用者のみ = is他特例適用者のみ;
+        this.is他特例解除者のみ = is他特例解除者のみ;
+        this.has適用事由 = has適用事由;
+        this.has解除事由 = has解除事由;
+        this.tekiyoJiyu = tekiyoJiyu;
+        this.kaijoJiyu = kaijoJiyu;
+        this.is施設変更あり = is施設変更あり;
+        this.is施設変更なし = is施設変更なし;
+        this.is年齢 = is年齢;
+        this.has年齢範囲開始 = has年齢範囲開始;
+        this.has年齢範囲終了 = has年齢範囲終了;
+        this.生年月日範囲終了 = 生年月日範囲終了;
+        this.生年月日範囲開始 = 生年月日範囲開始;
+        this.is生年月日 = is生年月日;
+        this.has生年月日範囲終了 = has生年月日範囲終了;
+        this.has生年月日範囲開始 = has生年月日範囲開始;
+        this.psmShikibetsuTaisho = psmShikibetsuTaisho;
+        this.psmAtesaki = psmAtesaki;
+    }
+
+    /**
+     * パラメータを作成します。
+     *
+     * @param 宛名抽出年齢開始 宛名抽出年齢開始
+     * @param 宛名抽出年齢終了 宛名抽出年齢終了
+     * @param 宛名抽出生年月日開始 宛名抽出生年月日開始
+     * @param 宛名抽出生年月日終了 宛名抽出生年月日終了
+     * @param 年齢基準日 年齢基準日
+     * @param 年齢層抽出方法 年齢層抽出方法
+     * @param 日付抽出区分 日付抽出区分
+     * @param 基準日区分 基準日区分
+     * @param 基準日 基準日
+     * @param 範囲抽出日区分 範囲抽出日区分
+     * @param 範囲抽出日From 範囲抽出日From
+     * @param 範囲抽出日To 範囲抽出日To
+     * @param 事由抽出区分 事由抽出区分
+     * @param tekiyoJiyu 適用事由
+     * @param kaijoJiyu 解除事由
+     * @param is施設変更あり is施設変更あり
+     * @param is施設変更なし is施設変更なし
+     * @param psmShikibetsuTaisho 宛名識別対象抽出PSM
+     * @param psmAtesaki 宛先抽出PSM
+     * @return HanyoListTaShichosonJushochiTokureishaMybatisParameter
+     */
+    public static HanyoListTaShichosonJushochiTokureishaMybatisParameter creatParameter(
+            Decimal 宛名抽出年齢開始,
+            Decimal 宛名抽出年齢終了,
+            RDate 宛名抽出生年月日開始,
+            RDate 宛名抽出生年月日終了,
+            RDate 年齢基準日,
+            RString 年齢層抽出方法,
+            RString 日付抽出区分,
+            RString 基準日区分,
+            RString 基準日,
+            RString 範囲抽出日区分,
+            RString 範囲抽出日From,
+            RString 範囲抽出日To,
+            RString 事由抽出区分,
+            List<RString> tekiyoJiyu,
+            List<RString> kaijoJiyu,
+            boolean is施設変更あり,
+            boolean is施設変更なし,
+            RString psmShikibetsuTaisho,
+            RString psmAtesaki) {
+        boolean is年齢 = false;
+        boolean is生年月日 = false;
+        boolean is直近 = false;
+        boolean is基準日 = false;
+        boolean is適用開始日 = false;
+        boolean is適用開始届出日 = false;
+        boolean is範囲 = false;
+        boolean is適用日 = false;
+        boolean is適用届出日 = false;
+        boolean is解除日 = false;
+        boolean is解除届出日 = false;
+        boolean is他特例適用者のみ = false;
+        boolean is他特例解除者のみ = false;
+        if (HizukeChushutsuKubun.直近.getコード().equals(日付抽出区分)) {
+            is直近 = true;
+        } else if (HizukeChushutsuKubun.基準日.getコード().equals(日付抽出区分)) {
+            is基準日 = true;
+        } else if (HizukeChushutsuKubun.範囲.getコード().equals(日付抽出区分)) {
+            is範囲 = true;
+        }
+        if (NenreiSoChushutsuHoho.年齢範囲.getコード().equals(年齢層抽出方法)) {
+            is年齢 = true;
+        } else if (NenreiSoChushutsuHoho.生年月日範囲.getコード().equals(年齢層抽出方法)) {
+            is生年月日 = true;
+        }
+        boolean has年齢範囲開始 = nullHandan(宛名抽出年齢開始);
+        boolean has年齢範囲終了 = nullHandan(宛名抽出年齢終了);
+        if (NenreiSoChushutsuHoho.年齢範囲.getコード().equals(年齢層抽出方法) && 宛名抽出年齢開始 != null) {
+            宛名抽出生年月日終了 = 年齢基準日.minusYear(宛名抽出年齢開始.add(1).intValue()).plusDay(2);
+        }
+        if (NenreiSoChushutsuHoho.年齢範囲.getコード().equals(年齢層抽出方法) && 宛名抽出年齢終了 != null) {
+            宛名抽出生年月日開始 = 年齢基準日.minusYear(宛名抽出年齢終了.intValue()).plusDay(1);
+        }
+        boolean has生年月日範囲開始 = nullHandan(宛名抽出生年月日開始);
+        boolean has生年月日範囲終了 = nullHandan(宛名抽出生年月日終了);
+        if (KijunbiKubun.適用開始日.getコード().equals(基準日区分)) {
+            is適用開始日 = true;
+        } else if (KijunbiKubun.適用開始届出日.getコード().equals(基準日区分)) {
+            is適用開始届出日 = true;
+        }
+        if (HaniChushutsubiKubun.適用日.getコード().equals(範囲抽出日区分)) {
+            is適用日 = true;
+        } else if (HaniChushutsubiKubun.適用届出日.getコード().equals(範囲抽出日区分)) {
+            is適用届出日 = true;
+        } else if (HaniChushutsubiKubun.解除日.getコード().equals(範囲抽出日区分)) {
+            is解除日 = true;
+        } else if (HaniChushutsubiKubun.解除届出日.getコード().equals(範囲抽出日区分)) {
+            is解除届出日 = true;
+        }
+        boolean has範囲抽出日From = nullHandan(範囲抽出日From);
+        boolean has範囲抽出日To = nullHandan(範囲抽出日To);
+        if (JiyuChushutsuKubun.他特例適用者のみ.getコード().equals(事由抽出区分)) {
+            is他特例適用者のみ = true;
+        } else if (JiyuChushutsuKubun.他特例解除者のみ.getコード().equals(事由抽出区分)) {
+            is他特例解除者のみ = true;
+        }
+        boolean has適用事由 = nullHandan(tekiyoJiyu);
+        boolean has解除事由 = nullHandan(kaijoJiyu);
+        return new HanyoListTaShichosonJushochiTokureishaMybatisParameter(
+                基準日,
+                範囲抽出日From,
+                範囲抽出日To,
+                is直近,
+                is基準日,
+                is適用開始日,
+                is適用開始届出日,
+                is範囲,
+                is適用日,
+                has範囲抽出日From,
+                has範囲抽出日To,
+                is適用届出日,
+                is解除日,
+                is解除届出日,
+                is他特例適用者のみ,
+                is他特例解除者のみ,
+                has適用事由,
+                has解除事由,
+                tekiyoJiyu,
+                kaijoJiyu,
+                is施設変更あり,
+                is施設変更なし,
+                is年齢,
+                has年齢範囲開始,
+                has年齢範囲終了,
+                宛名抽出生年月日終了,
+                宛名抽出生年月日開始,
+                is生年月日,
+                has生年月日範囲終了,
+                has生年月日範囲開始,
+                psmShikibetsuTaisho,
+                psmAtesaki);
+    }
+
+    private static boolean nullHandan(Decimal 宛名抽出年齢) {
+        return 宛名抽出年齢 != null;
+    }
+
+    private static boolean nullHandan(RDate 年月日) {
+        return 年月日 != null;
+    }
+
+    private static boolean nullHandan(RString 抽出日) {
+        return !RString.isNullOrEmpty(抽出日);
+    }
+
+    private static boolean nullHandan(List<RString> jiyu) {
+        return jiyu != null && !jiyu.isEmpty();
+    }
+
+}
---
+++ DB/DBA/standard/definition/src/main/java/jp/co/ndensan/reams/db/dba/definition/processprm/hanyolisttashichosonjushochitokureisha/HanyoListTaShichosonJushochiTokureishaProcessParameter.java
@@ -0,0 +1,255 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dba.definition.processprm.hanyolisttashichosonjushochitokureisha;
+
+import java.util.List;
+import jp.co.ndensan.reams.db.dba.definition.mybatisprm.hanyolisttashichosonjushochitokureisha.HanyoListTaShichosonJushochiTokureishaMybatisParameter;
+import jp.co.ndensan.reams.uz.uza.batch.parameter.IBatchProcessParameter;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.math.Decimal;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 汎用リスト_他市町村住所地特例者のProcessクラスのパラメータ。
+ *
+ * @reamsid_L DBA-1590-030 yaodongsheng
+ */
+@SuppressWarnings("PMD.UnusedPrivateField")
+@Getter
+@Setter
+public class HanyoListTaShichosonJushochiTokureishaProcessParameter implements IBatchProcessParameter {
+
+    private RString chohyoId;
+    private Long shutsuryokujunId;
+    private RString shutsuryokuKomokuId;
+    private boolean komokumeiFuka;
+    private boolean renbanFuka;
+    private boolean hitsukeHenshu;
+    private RString hitsukeChushutsuKubun;
+    private RString kijunYMD;
+    private RString chushutsunichiKubun;
+    private RString chushutsunichiFrom;
+    private RString chushutsunichiTo;
+    private boolean jukyushaJoho_Nihonjin;
+    private boolean jukyushaJoho_Gaikokujin;
+    private RString kijunYMDkubun;
+    private RString jyuChusyutsuKubun;
+    private List<RString> dekyujyu;
+    private List<RString> kaijyojyu;
+    private boolean shisetsuHenkoari;
+    private boolean shisetsuHenkonashi;
+    private RString psmChushutsu_Kubun;
+    private Decimal psmChushutsuAge_Start;
+    private Decimal psmChushutsuAge_End;
+    private RDate psmSeinengappiYMD_Start;
+    private RDate psmSeinengappiYMD_End;
+    private RDate psmAgeKijunni;
+    private RString shichoson_Code;
+    private RString shichoson_Name;
+    private RString psmChiku_Kubun;
+    private RString psmJusho_From;
+    private RString psmJusho_From_Name;
+    private RString psmJusho_To;
+    private RString psmJusho_To_Name;
+    private RString psmGyoseiku_From;
+    private RString psmGyoseiku_From_Name;
+    private RString psmGyoseiku_To;
+    private RString psmGyoseiku_To_Name;
+    private RString psmChiku1_From;
+    private RString psmChiku1_To;
+    private RString psmChiku2_From;
+    private RString psmChiku2_To;
+    private RString psmChiku3_From;
+    private RString psmChiku3_To;
+    private RString psmChiku1_From_Name;
+    private RString psmChiku1_To_Name;
+    private RString psmChiku2_From_Name;
+    private RString psmChiku2_To_Name;
+    private RString psmChiku3_From_Name;
+    private RString psmChiku3_To_Name;
+
+    /**
+     * @param chohyoId 帳票ID
+     * @param shutsuryokujunId 出力順ID
+     * @param shutsuryokuKomokuId 出力項目ID
+     * @param komokumeiFuka 項目名付加
+     * @param renbanFuka 連番付加
+     * @param hitsukeHenshu 日付編集
+     * @param hitsukeChushutsuKubun 日付抽出区分
+     * @param kijunYMD 基準日
+     * @param chushutsunichiKubun 範囲抽出日区分
+     * @param chushutsunichiFrom 範囲抽出日From
+     * @param chushutsunichiTo 範囲抽出日To
+     * @param jukyushaJoho_Nihonjin 他特例者情報・日本人
+     * @param jukyushaJoho_Gaikokujin 他特例者情報・外国人
+     * @param kijunYMDkubun 基準日区分
+     * @param jyuChusyutsuKubun 事由抽出区分
+     * @param dekyujyu 適用事由
+     * @param kaijyojyu 解除事由
+     * @param shisetsuHenkoari 施設変更あり
+     * @param shisetsuHenkonashi 施設変更なし
+     * @param psmChushutsu_Kubun 年齢層抽出方法
+     * @param psmChushutsuAge_Start 宛名抽出年齢開始
+     * @param psmChushutsuAge_End 宛名抽出年齢終了
+     * @param psmSeinengappiYMD_Start 宛名抽出生年月日開始
+     * @param psmSeinengappiYMD_End 宛名抽出生年月日終了
+     * @param psmAgeKijunni 年齢基準日
+     * @param shichoson_Code 市町村コード
+     * @param shichoson_Name 市町村名称
+     * @param psmChiku_Kubun 地区区分
+     * @param psmJusho_From 町域From
+     * @param psmJusho_From_Name 町域From名称
+     * @param psmJusho_To 町域To
+     * @param psmJusho_To_Name 町域To名称
+     * @param psmGyoseiku_From 行政区From
+     * @param psmGyoseiku_From_Name 行政区From名称
+     * @param psmGyoseiku_To 行政区To
+     * @param psmGyoseiku_To_Name 行政区To名称
+     * @param psmChiku1_From 地区１From
+     * @param psmChiku1_To 地区１To
+     * @param psmChiku2_From 地区２From
+     * @param psmChiku2_To 地区２To
+     * @param psmChiku3_From 地区３From
+     * @param psmChiku3_To 地区３To
+     * @param psmChiku1_From_Name 地区１From名称
+     * @param psmChiku1_To_Name 地区１To名称
+     * @param psmChiku2_From_Name 地区２From名称
+     * @param psmChiku2_To_Name 地区２To名称
+     * @param psmChiku3_From_Name 地区３From名称
+     * @param psmChiku3_To_Name 地区３To名称
+     */
+    public HanyoListTaShichosonJushochiTokureishaProcessParameter(
+            RString chohyoId,
+            Long shutsuryokujunId,
+            RString shutsuryokuKomokuId,
+            boolean komokumeiFuka,
+            boolean renbanFuka,
+            boolean hitsukeHenshu,
+            RString hitsukeChushutsuKubun,
+            RString kijunYMD,
+            RString chushutsunichiKubun,
+            RString chushutsunichiFrom,
+            RString chushutsunichiTo,
+            boolean jukyushaJoho_Nihonjin,
+            boolean jukyushaJoho_Gaikokujin,
+            RString kijunYMDkubun,
+            RString jyuChusyutsuKubun,
+            List<RString> dekyujyu,
+            List<RString> kaijyojyu,
+            boolean shisetsuHenkoari,
+            boolean shisetsuHenkonashi,
+            RString psmChushutsu_Kubun,
+            Decimal psmChushutsuAge_Start,
+            Decimal psmChushutsuAge_End,
+            RDate psmSeinengappiYMD_Start,
+            RDate psmSeinengappiYMD_End,
+            RDate psmAgeKijunni,
+            RString shichoson_Code,
+            RString shichoson_Name,
+            RString psmChiku_Kubun,
+            RString psmJusho_From,
+            RString psmJusho_From_Name,
+            RString psmJusho_To,
+            RString psmJusho_To_Name,
+            RString psmGyoseiku_From,
+            RString psmGyoseiku_From_Name,
+            RString psmGyoseiku_To,
+            RString psmGyoseiku_To_Name,
+            RString psmChiku1_From,
+            RString psmChiku1_To,
+            RString psmChiku2_From,
+            RString psmChiku2_To,
+            RString psmChiku3_From,
+            RString psmChiku3_To,
+            RString psmChiku1_From_Name,
+            RString psmChiku1_To_Name,
+            RString psmChiku2_From_Name,
+            RString psmChiku2_To_Name,
+            RString psmChiku3_From_Name,
+            RString psmChiku3_To_Name) {
+        this.chohyoId = chohyoId;
+        this.shutsuryokujunId = shutsuryokujunId;
+        this.shutsuryokuKomokuId = shutsuryokuKomokuId;
+        this.komokumeiFuka = komokumeiFuka;
+        this.renbanFuka = renbanFuka;
+        this.hitsukeHenshu = hitsukeHenshu;
+        this.hitsukeChushutsuKubun = hitsukeChushutsuKubun;
+        this.kijunYMD = kijunYMD;
+        this.chushutsunichiKubun = chushutsunichiKubun;
+        this.chushutsunichiFrom = chushutsunichiFrom;
+        this.chushutsunichiTo = chushutsunichiTo;
+        this.jukyushaJoho_Nihonjin = jukyushaJoho_Nihonjin;
+        this.jukyushaJoho_Gaikokujin = jukyushaJoho_Gaikokujin;
+        this.kijunYMDkubun = kijunYMDkubun;
+        this.jyuChusyutsuKubun = jyuChusyutsuKubun;
+        this.dekyujyu = dekyujyu;
+        this.kaijyojyu = kaijyojyu;
+        this.shisetsuHenkoari = shisetsuHenkoari;
+        this.shisetsuHenkonashi = shisetsuHenkonashi;
+        this.psmChushutsu_Kubun = psmChushutsu_Kubun;
+        this.psmChushutsuAge_Start = psmChushutsuAge_Start;
+        this.psmChushutsuAge_End = psmChushutsuAge_End;
+        this.psmSeinengappiYMD_Start = psmSeinengappiYMD_Start;
+        this.psmSeinengappiYMD_End = psmSeinengappiYMD_End;
+        this.psmAgeKijunni = psmAgeKijunni;
+        this.shichoson_Code = shichoson_Code;
+        this.shichoson_Name = shichoson_Name;
+        this.psmChiku_Kubun = psmChiku_Kubun;
+        this.psmJusho_From = psmJusho_From;
+        this.psmJusho_From_Name = psmJusho_From_Name;
+        this.psmJusho_To = psmJusho_To;
+        this.psmJusho_To_Name = psmJusho_To_Name;
+        this.psmGyoseiku_From = psmGyoseiku_From;
+        this.psmGyoseiku_From_Name = psmGyoseiku_From_Name;
+        this.psmGyoseiku_To = psmGyoseiku_To;
+        this.psmGyoseiku_To_Name = psmGyoseiku_To_Name;
+        this.psmChiku1_From = psmChiku1_From;
+        this.psmChiku1_To = psmChiku1_To;
+        this.psmChiku2_From = psmChiku2_From;
+        this.psmChiku2_To = psmChiku2_To;
+        this.psmChiku3_From = psmChiku3_From;
+        this.psmChiku3_To = psmChiku3_To;
+        this.psmChiku1_From_Name = psmChiku1_From_Name;
+        this.psmChiku1_To_Name = psmChiku1_To_Name;
+        this.psmChiku2_From_Name = psmChiku2_From_Name;
+        this.psmChiku2_To_Name = psmChiku2_To_Name;
+        this.psmChiku3_From_Name = psmChiku3_From_Name;
+        this.psmChiku3_To_Name = psmChiku3_To_Name;
+    }
+
+    /**
+     * Mybatisのパラメータを作成します。
+     *
+     * @param psmShikibetsuTaisho 宛名識別対象抽出PSM
+     * @param psmAtesaki 宛先抽出PSM
+     * @return HanyoListTaShichosonJushochiTokureishaMybatisParameter
+     */
+    public HanyoListTaShichosonJushochiTokureishaMybatisParameter
+            toHanyoListTaShichosonJushochiTokureishaMybatisParameter(RString psmShikibetsuTaisho, RString psmAtesaki) {
+        return HanyoListTaShichosonJushochiTokureishaMybatisParameter.creatParameter(
+                psmChushutsuAge_Start,
+                psmChushutsuAge_End,
+                psmSeinengappiYMD_Start,
+                psmSeinengappiYMD_End,
+                psmAgeKijunni,
+                psmChushutsu_Kubun,
+                chushutsunichiKubun,
+                kijunYMDkubun,
+                kijunYMD,
+                chushutsunichiKubun,
+                chushutsunichiFrom,
+                chushutsunichiTo,
+                jyuChusyutsuKubun,
+                dekyujyu,
+                kaijyojyu,
+                shisetsuHenkoari,
+                shisetsuHenkonashi,
+                psmShikibetsuTaisho,
+                psmAtesaki);
+    }
+}
