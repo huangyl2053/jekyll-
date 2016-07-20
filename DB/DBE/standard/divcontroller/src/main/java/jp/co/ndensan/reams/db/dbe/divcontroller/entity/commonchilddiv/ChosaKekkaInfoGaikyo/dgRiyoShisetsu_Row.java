e7d18f2f6321a9da6ac66a9f203c0b262f6336ef
lizhuoxuan <lizhuoxuan@liandisys.com.cn>
2016-07-20 21:37:35 +0800

    画面設計_ChosaKekkaInfoGaikyo_認定調査結果情報照会（概況調査） [バグ対応]14D203D-20750

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/commonchilddiv/ChosaKekkaInfoGaikyo/dgRiyoShisetsu_Row.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/commonchilddiv/ChosaKekkaInfoGaikyo/dgRiyoShisetsu_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbe.divcontroller.entity.commonchilddiv.ChosaKekk
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Tue Jul 12 14:36:46 CST 2016 
+ * Thu Jul 14 10:22:48 CST 2016 
  */
 
 
