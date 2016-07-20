5d68ce988aa0f8c979d2a51514a278bdca61c8c0
huangh <huangh@liandisys.com>
2016-07-20 09:40:44 +0800

    14D203D-20504を対応した

--- DB/DBB/standard/business/src/main/java/jp/co/ndensan/reams/db/dbb/business/report/kanendononyutsuchishocvskigoto/KanendoNonyuTsuchishoCVSKigotoEditor.java
+++ DB/DBB/standard/business/src/main/java/jp/co/ndensan/reams/db/dbb/business/report/kanendononyutsuchishocvskigoto/KanendoNonyuTsuchishoCVSKigotoEditor.java
@@ -250,19 +250,6 @@ public class KanendoNonyuTsuchishoCVSKigotoEditor implements IKanendoNonyuTsuchi
                     && item.get納付書共通().get通知書番号() != null) {
                 source.detail_tsuchishoNo = item.get納付書共通().get通知書番号().value();
             }
-        } else {
-            source.ryoshushoNendo = HOSHI_4;
-            source.nendoNenbun = HOSHI_4;
-            source.kibetsu = HOSHI_2;
-            source.ryoshushoNenbun = HOSHI_4;
-            source.gokeigaku = HOSHI_13;
-            source.detail_tsuchishoNo = HOSHI_16;
-            source.nokigenYmd = HOSHI_11;
-            source.hakkoYmd = HOSHI_11;
-            source.honzei = HOSHI_13;
-            source.ocr1 = HOSHI_22;
-            source.ocr2 = HOSHI_28;
-            source.cvsToriatsukaikigen = HOSHI_16;
         }
 
         source.nokigenTitle = NOKIGEN;
