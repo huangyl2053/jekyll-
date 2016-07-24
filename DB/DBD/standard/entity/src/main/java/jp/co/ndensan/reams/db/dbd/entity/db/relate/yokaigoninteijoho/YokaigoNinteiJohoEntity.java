f29136e98377339dbcd07357590a0c86e0dae5c6
lit <lit@liandisys.com.cn>
2016-07-24 17:45:54 +0800

    画面設計_DBD5020001_要介護認定処理 DBD-1530-010の実装

--- DB/DBD/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbd/entity/db/relate/yokaigoninteijoho/YokaigoNinteiJohoEntity.java
+++ DB/DBD/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbd/entity/db/relate/yokaigoninteijoho/YokaigoNinteiJohoEntity.java
@@ -75,4 +75,22 @@ public class YokaigoNinteiJohoEntity implements Cloneable, Serializable {
         this.要介護認定結果情報Entity.initializeMd5();
         this.要介護認定インターフェース情報Entity.initializeMd5();
     }
+
+    /**
+     * MyBatisで取得された場合に使用して下さい。<br/>
+     * MyBatisで当クラス取得時は、新規追加(Added)となるため、変更無し(Unchanged)に設定します。<br/>
+     * のMD5値を計算し、設定します。
+     */
+    public void initializeMd5ToEntitiesWithoutJukyusha() {
+        this.要介護認定申請情報認定Entity.initializeMd5();
+        this.主治医医療機関情報認定Entity.initializeMd5();
+        this.主治医情報認定Entity.initializeMd5();
+        this.要介護認定申請情報受給Entity.initializeMd5();
+        this.認定調査委託先情報Entity.initializeMd5();
+        this.主治医医療機関情報受給Entity.initializeMd5();
+        this.主治医情報受給Entity.initializeMd5();
+        this.調査員情報Entity.initializeMd5();
+        this.要介護認定結果情報Entity.initializeMd5();
+        this.要介護認定インターフェース情報Entity.initializeMd5();
+    }
 }
