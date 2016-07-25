78817f988e49c0cf659bef0f455b6488af12c216
sunhadi <sunhaidi@liandisys.com.cn>
2016-07-25 21:20:25 +0800

    認定調査依頼履歴一覧表　アクセスログを出力する　追加

--- DB/DBE/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbe/entity/report/chosairairirekiichiran/ChosairairirekiIchiranReportSource.java
+++ DB/DBE/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbe/entity/report/chosairairirekiichiran/ChosairairirekiIchiranReportSource.java
@@ -1,8 +1,13 @@
 package jp.co.ndensan.reams.db.dbe.entity.report.chosairairirekiichiran;
 
+import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.log.accesslog.core.ExpandedInformation;
 import jp.co.ndensan.reams.uz.uza.report.IReportSource;
+import jp.co.ndensan.reams.uz.uza.report.source.ReportExpandedInfo;
 import jp.co.ndensan.reams.uz.uza.report.source.ReportItem;
+import jp.co.ndensan.reams.uz.uza.report.source.ReportPerson;
+
 /**
  * 認定調査依頼履歴一覧表Sourceクラスです。
  *
@@ -37,4 +42,9 @@ public class ChosairairirekiIchiranReportSource implements IReportSource {
     //帳票ソースデータクラスを再作成する場合は、「User Customize Area」内のソースコードは記述されません。
     //再作成した後、当箇所に記述したコードを再作成された帳票ソースデータクラスの「User Customize Area」内にコピー＆ペーストする必要があります。
 // </editor-fold>
+
+    @ReportPerson(id = "X")
+    public ShikibetsuCode shikibetuCode;
+    @ReportExpandedInfo(id = "X")
+    public ExpandedInformation hishokenshaNo;
 }
