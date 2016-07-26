eb4454a8a6a3a8197a46ea8f737b619f475c058c
xuyongchao <xuyongchao@liandisys.com.cn>
2016-07-26 21:06:17 +0800

    修正した

--- DB/DBU/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/batchcontroller/step/jigyohokokurenkei/JigyoHokokuRenkei11_14ShokanProcess.java
+++ DB/DBU/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/batchcontroller/step/jigyohokokurenkei/JigyoHokokuRenkei11_14ShokanProcess.java
@@ -84,7 +84,7 @@ public class JigyoHokokuRenkei11_14ShokanProcess extends BatchProcessBase<DbT702
                     + "get事業報告統計償還分決定年月情報の取得");
         }
         csvFileName = new RString("DUJRENF07_" + processParameter.get過去集計年月()
-                + "_" + DbBusinessConfig.get(ConfigNameDBU.保険者情報_保険者番号, 基準日, SubGyomuCode.DBE認定支援));
+                + "_" + DbBusinessConfig.get(ConfigNameDBU.保険者情報_保険者番号, 基準日, SubGyomuCode.DBE認定支援) + ".csv");
         record1Entity = new JigyoHokokuRenkei11or14Entity();
         record2Entity = new JigyoHokokuRenkei11or14Entity();
         record3Entity = new JigyoHokokuRenkei11or14Entity();
@@ -118,7 +118,7 @@ public class JigyoHokokuRenkei11_14ShokanProcess extends BatchProcessBase<DbT702
         eucCsvWriter.writeLine(
                 new JigyoHokokuRenkeiHeadEucCsvEntity(
                         RString.EMPTY,
-                        new RString("Ti"),
+                        new RString("H1"),
                         new RString(processParameter.get過去集計年月() + "01"),
                         RDate.getNowDate().toDateString(),
                         new RString("国民健康保険団体連合会"),
