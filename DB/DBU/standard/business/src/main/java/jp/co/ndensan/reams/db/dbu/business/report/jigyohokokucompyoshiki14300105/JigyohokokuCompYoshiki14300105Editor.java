d3e4dba6007d7f14863fe2ea0b6952b75b19b42a
lishengli <lishengli@liandisys.com.cn>
2016-07-27 17:50:12 +0800

    介護事業状況報告年報　新規作成

--- DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/report/jigyohokokucompyoshiki14300105/JigyohokokuCompYoshiki14300105Editor.java
+++ DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/report/jigyohokokucompyoshiki14300105/JigyohokokuCompYoshiki14300105Editor.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbu.business.report.jigyohokokucompyoshiki300103;
+package jp.co.ndensan.reams.db.dbu.business.report.jigyohokokucompyoshiki14300105;
 
-import jp.co.ndensan.reams.db.dbu.entity.db.relate.jigyohokokucompyoshiki300103.JigyohokokuCompYoshiki300103Data;
-import jp.co.ndensan.reams.db.dbu.entity.report.jigyohokokucompyoshiki300103.JigyohokokuCompYoshiki300103ReportSource;
+import jp.co.ndensan.reams.db.dbu.entity.db.relate.jigyohokokucompyoshiki14300105.JigyohokokuCompYoshiki14300105Data;
+import jp.co.ndensan.reams.db.dbu.entity.report.jigyohokokucompyoshiki14300105.JigyohokokuCompYoshiki14300105ReportSource;
 import jp.co.ndensan.reams.uz.uza.lang.EraType;
 import jp.co.ndensan.reams.uz.uza.lang.FillType;
 import jp.co.ndensan.reams.uz.uza.lang.FirstYear;
@@ -18,36 +18,39 @@ import jp.co.ndensan.reams.uz.uza.lang.RStringBuilder;
 import jp.co.ndensan.reams.uz.uza.lang.Separator;
 
 /**
- * 介護事業状況報告年報（様式1-2）のEditorです。
+ * 介護事業状況報告年報（様式1-4）のEditorクラスです。
  *
- * @reamsid_L DBU-5600-120 lishengli
+ * @reamsid_L DBU-5600-140 lishengli
  */
-public class JigyohokokuCompYoshiki300103Editor implements IJigyohokokuCompYoshiki300103Editor {
+public class JigyohokokuCompYoshiki14300105Editor implements IJigyohokokuCompYoshiki14300105Editor {
 
     private static final RString DATE_時 = new RString("時");
     private static final RString DATE_分 = new RString("分");
     private static final RString DATE_秒 = new RString("秒");
     private static final RString 月報 = new RString("1");
     private static final RString 年報 = new RString("2");
-    private final JigyohokokuCompYoshiki300103Data item;
+    private final JigyohokokuCompYoshiki14300105Data data;
+    private final JigyohokokuCompYoshiki14300105Change change;
 
     /**
      * インスタンスを生成します。
      *
-     * @param item JigyohokokuCompYoshiki300103Data
+     * @param data JigyohokokuCompYoshiki14300105Data
+     * @param change JigyohokokuCompYoshiki14300105Change
      */
-    protected JigyohokokuCompYoshiki300103Editor(JigyohokokuCompYoshiki300103Data item) {
-        this.item = item;
+    protected JigyohokokuCompYoshiki14300105Editor(JigyohokokuCompYoshiki14300105Data data, JigyohokokuCompYoshiki14300105Change change) {
+        this.data = data;
+        this.change = change;
     }
 
     @Override
-    public JigyohokokuCompYoshiki300103ReportSource edit(JigyohokokuCompYoshiki300103ReportSource source) {
+    public JigyohokokuCompYoshiki14300105ReportSource edit(JigyohokokuCompYoshiki14300105ReportSource source) {
         return editSource(source);
     }
 
-    private JigyohokokuCompYoshiki300103ReportSource editSource(JigyohokokuCompYoshiki300103ReportSource source) {
+    private JigyohokokuCompYoshiki14300105ReportSource editSource(JigyohokokuCompYoshiki14300105ReportSource source) {
         RStringBuilder printTimeStampSb = new RStringBuilder();
-        RDateTime printdate = item.get処理日時();
+        RDateTime printdate = data.get処理日時();
         printTimeStampSb.append(printdate.getDate().wareki().eraType(EraType.KANJI).firstYear(FirstYear.GAN_NEN).
                 separator(Separator.JAPANESE).
                 fillType(FillType.BLANK).toDateString());
@@ -59,37 +62,43 @@ public class JigyohokokuCompYoshiki300103Editor implements IJigyohokokuCompYoshi
         printTimeStampSb.append(String.format("%02d", printdate.getSecond()));
         printTimeStampSb.append(DATE_秒);
         source.printTimeStamp = printTimeStampSb.toRString();
-        source.yoshiki = item.get様式();
-        if (月報.equals(item.get年報月報区分())) {
+        if (月報.equals(data.get年報月報区分())) {
             source.shukeiKubun = new RString("月報");
-        } else if (年報.equals(item.get年報月報区分())) {
+        } else if (年報.equals(data.get年報月報区分())) {
             source.shukeiKubun = new RString("年報");
         }
         source.shuukeiHani = get集計範囲();
-        source.hokenshaNo = item.get保険者番号();
-        source.hokenshaName = item.get保険者名();
-        source.hyo1Title = item.get表1のタイトル();
-        source.hyo2Title1 = item.get表2のタイトル1();
-        source.hyo3Title = item.get表3のタイトル();
-        source.hyo2Title2 = item.get表2のタイトル2();
-        source.hyo1ShinseishaKensu1 = item.get介護老人福祉施設の申請件数();
-        source.hyo1ShinseishaKensu2 = item.get介護老人保健施設の申請件数();
-        source.hyo1ShinseishaKensu3 = item.get介護療養型医療施設の申請件数();
-        source.hyo1ShinseishaKensu4 = item.get地域密着型介護老人福祉施設入居者生活介護の申請件数();
-        source.hyo1ShinseishaKensu5 = item.getその他の申請件数();
-        source.hyo1ShinseishaKensu6 = item.get合計の申請件数();
-        source.hyo3ShinseishaKensu = item.get特定負担限度額の申請件数();
+        source.hokenshaNo = data.get保険者番号();
+        source.hokenshaName = data.get保険者名();
+        source.hyo1ShinseishaKensu1 = data.get介護老人福祉施設の申請件数();
+        source.hyo1ShinseishaKensu2 = data.get介護老人保健施設の申請件数();
+        source.hyo1ShinseishaKensu3 = data.get介護療養型医療施設の申請件数();
+        source.hyo1ShinseishaKensu4 = data.get地域密着型介護老人福祉施設入居者生活介護の申請件数();
+        source.hyo1ShinseishaKensu5 = data.getその他の申請件数();
+        source.hyo1ShinseishaKensu6 = data.get合計の申請件数();
+        source.list_1 = change.getList_1();
+        source.list_2 = change.getList_2();
+        source.list_3 = change.getList_3();
+        source.list_4 = change.getList_4();
+        source.list_5 = change.getList_5();
+        source.list_6 = change.getList_6();
+        source.list_7 = change.getList_7();
+        source.list_8 = change.getList_8();
+        source.list_9 = change.getList_9();
+        source.list_10 = change.getList_10();
+        source.list_11 = change.getList_11();
+        source.list_12 = change.getList_12();
         return source;
     }
 
     private RString get集計範囲() {
         RStringBuilder 集計範囲_SB = new RStringBuilder();
         集計範囲_SB.append(new RString("（"));
-        集計範囲_SB.append(getパターン107(item.get集計年度()));
+        集計範囲_SB.append(getパターン107(data.get集計年度()));
         集計範囲_SB.append(new RString("年度分）"));
-        集計範囲_SB.append(getパターン62(item.get集計期間FROM()));
+        集計範囲_SB.append(getパターン62(data.get集計期間FROM()));
         集計範囲_SB.append(new RString("～"));
-        集計範囲_SB.append(getパターン62(item.get集計期間TO()));
+        集計範囲_SB.append(getパターン62(data.get集計期間TO()));
         return 集計範囲_SB.toRString();
     }
 
--- DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/report/jigyohokokucompyoshiki151300106/JigyohokokuCompYoshiki151300106Editor.java
+++ DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/report/jigyohokokucompyoshiki151300106/JigyohokokuCompYoshiki151300106Editor.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbu.business.report.jigyohokokucompyoshiki300103;
+package jp.co.ndensan.reams.db.dbu.business.report.jigyohokokucompyoshiki151300106;
 
-import jp.co.ndensan.reams.db.dbu.entity.db.relate.jigyohokokucompyoshiki300103.JigyohokokuCompYoshiki300103Data;
-import jp.co.ndensan.reams.db.dbu.entity.report.jigyohokokucompyoshiki300103.JigyohokokuCompYoshiki300103ReportSource;
+import jp.co.ndensan.reams.db.dbu.entity.db.relate.jigyohokokucompyoshiki151300106.JigyohokokuCompYoshiki151300106Data;
+import jp.co.ndensan.reams.db.dbu.entity.report.jigyohokokucompyoshiki151300106.JigyohokokuCompYoshiki151300106ReportSource;
 import jp.co.ndensan.reams.uz.uza.lang.EraType;
 import jp.co.ndensan.reams.uz.uza.lang.FillType;
 import jp.co.ndensan.reams.uz.uza.lang.FirstYear;
@@ -18,36 +18,36 @@ import jp.co.ndensan.reams.uz.uza.lang.RStringBuilder;
 import jp.co.ndensan.reams.uz.uza.lang.Separator;
 
 /**
- * 介護事業状況報告年報（様式1-2）のEditorです。
+ * 介護事業状況報告年報（様式1-5）のEditorクラスです。
  *
- * @reamsid_L DBU-5600-120 lishengli
+ * @reamsid_L DBU-5600-150 lishengli
  */
-public class JigyohokokuCompYoshiki300103Editor implements IJigyohokokuCompYoshiki300103Editor {
+public class JigyohokokuCompYoshiki151300106Editor implements IJigyohokokuCompYoshiki151300106Editor {
 
     private static final RString DATE_時 = new RString("時");
     private static final RString DATE_分 = new RString("分");
     private static final RString DATE_秒 = new RString("秒");
     private static final RString 月報 = new RString("1");
     private static final RString 年報 = new RString("2");
-    private final JigyohokokuCompYoshiki300103Data item;
+    private final JigyohokokuCompYoshiki151300106Data data;
 
     /**
      * インスタンスを生成します。
      *
-     * @param item JigyohokokuCompYoshiki300103Data
+     * @param data JigyohokokuCompYoshiki151300106Data
      */
-    protected JigyohokokuCompYoshiki300103Editor(JigyohokokuCompYoshiki300103Data item) {
-        this.item = item;
+    protected JigyohokokuCompYoshiki151300106Editor(JigyohokokuCompYoshiki151300106Data data) {
+        this.data = data;
     }
 
     @Override
-    public JigyohokokuCompYoshiki300103ReportSource edit(JigyohokokuCompYoshiki300103ReportSource source) {
+    public JigyohokokuCompYoshiki151300106ReportSource edit(JigyohokokuCompYoshiki151300106ReportSource source) {
         return editSource(source);
     }
 
-    private JigyohokokuCompYoshiki300103ReportSource editSource(JigyohokokuCompYoshiki300103ReportSource source) {
+    private JigyohokokuCompYoshiki151300106ReportSource editSource(JigyohokokuCompYoshiki151300106ReportSource source) {
         RStringBuilder printTimeStampSb = new RStringBuilder();
-        RDateTime printdate = item.get処理日時();
+        RDateTime printdate = data.get処理日時();
         printTimeStampSb.append(printdate.getDate().wareki().eraType(EraType.KANJI).firstYear(FirstYear.GAN_NEN).
                 separator(Separator.JAPANESE).
                 fillType(FillType.BLANK).toDateString());
@@ -59,37 +59,25 @@ public class JigyohokokuCompYoshiki300103Editor implements IJigyohokokuCompYoshi
         printTimeStampSb.append(String.format("%02d", printdate.getSecond()));
         printTimeStampSb.append(DATE_秒);
         source.printTimeStamp = printTimeStampSb.toRString();
-        source.yoshiki = item.get様式();
-        if (月報.equals(item.get年報月報区分())) {
+        if (月報.equals(data.get年報月報区分())) {
             source.shukeiKubun = new RString("月報");
-        } else if (年報.equals(item.get年報月報区分())) {
+        } else if (年報.equals(data.get年報月報区分())) {
             source.shukeiKubun = new RString("年報");
         }
         source.shuukeiHani = get集計範囲();
-        source.hokenshaNo = item.get保険者番号();
-        source.hokenshaName = item.get保険者名();
-        source.hyo1Title = item.get表1のタイトル();
-        source.hyo2Title1 = item.get表2のタイトル1();
-        source.hyo3Title = item.get表3のタイトル();
-        source.hyo2Title2 = item.get表2のタイトル2();
-        source.hyo1ShinseishaKensu1 = item.get介護老人福祉施設の申請件数();
-        source.hyo1ShinseishaKensu2 = item.get介護老人保健施設の申請件数();
-        source.hyo1ShinseishaKensu3 = item.get介護療養型医療施設の申請件数();
-        source.hyo1ShinseishaKensu4 = item.get地域密着型介護老人福祉施設入居者生活介護の申請件数();
-        source.hyo1ShinseishaKensu5 = item.getその他の申請件数();
-        source.hyo1ShinseishaKensu6 = item.get合計の申請件数();
-        source.hyo3ShinseishaKensu = item.get特定負担限度額の申請件数();
+        source.hokenshaNo = data.get保険者番号();
+        source.hokenshaName = data.get保険者名();
         return source;
     }
 
     private RString get集計範囲() {
         RStringBuilder 集計範囲_SB = new RStringBuilder();
         集計範囲_SB.append(new RString("（"));
-        集計範囲_SB.append(getパターン107(item.get集計年度()));
+        集計範囲_SB.append(getパターン107(data.get集計年度()));
         集計範囲_SB.append(new RString("年度分）"));
-        集計範囲_SB.append(getパターン62(item.get集計期間FROM()));
+        集計範囲_SB.append(getパターン62(data.get集計期間FROM()));
         集計範囲_SB.append(new RString("～"));
-        集計範囲_SB.append(getパターン62(item.get集計期間TO()));
+        集計範囲_SB.append(getパターン62(data.get集計期間TO()));
         return 集計範囲_SB.toRString();
     }
 
