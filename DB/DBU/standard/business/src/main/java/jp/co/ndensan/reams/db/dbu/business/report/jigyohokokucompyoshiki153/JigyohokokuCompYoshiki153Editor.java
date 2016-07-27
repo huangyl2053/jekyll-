6c59f7316c63a3f2e054732e6b796e7b929efd45
dangjingjing <dangjingjing@liandisys.com.cn>
2016-07-27 11:46:18 +0800

    帳票設計_DBU300007_介護事業状況報告月報・一般状況（様式1-5）_新様式　　修正

--- DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/report/jigyohokokucompyoshiki153/JigyohokokuCompYoshiki153Editor.java
+++ DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/report/jigyohokokucompyoshiki153/JigyohokokuCompYoshiki153Editor.java
@@ -12,7 +12,7 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.lang.RStringBuilder;
 
 /**
- * 介護事業状況報告月報・一般状況（様式1-5）_新様式 のReportSourceクラスです。
+ * 介護事業状況報告月報・一般状況（様式1-5）_新様式 のEditorです。
  *
  * @reamsid_L DBU-5540-060 dangjingjing
  */
