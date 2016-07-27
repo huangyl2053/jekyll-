a775d5f41c765e31d1226394364a6ece71bc16df
斉 振 <n8362@ndensan.co.jp>
2016-07-27 16:40:48 +0900

    過誤決定情報照会画面と再審査決定情報照会画面を修正する。

---
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0081011/DBC0081011StateName.java
@@ -0,0 +1,36 @@
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0081011;
+
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+
+/**
+ * このコードはツールによって生成されました。</br>
+ * このファイルへの変更は、再生成時には損失するため</br>
+ * 不正な動作の原因になります。</br>
+ * DBC0081011の状態名定義クラスです
+ *
+ * @author 自動生成
+ */
+public enum DBC0081011StateName implements IStateEnumerations {
+// <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
+
+    NoChange(""),
+    初期状態("初期状態");
+
+    private final RString name;
+
+    private DBC0081011StateName(String name) {
+        this.name = new RString(name);
+    }
+
+    /**
+     * getName
+     *
+     * @return 名前
+     */
+    @Override
+    public RString getName() {
+        return name;
+    }
+// </editor-fold>
+}
