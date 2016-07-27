e17e83670c0b4fd7118ee852548abe4b3e9b2be9
姜岐 <n8344@ndensan.co.jp>
2016-07-27 15:11:50 +0900

    後期資格情報登録新規作成

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0530011/DBC0530011StateName.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0530011/DBC0530011StateName.java
@@ -1,25 +1,25 @@
-package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0520011;
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0530011;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
 
 /**
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBC0520011のイベント名定義クラスです</br>
+ * DBC0530011の状態名定義クラスです
  *
  * @author 自動生成
  */
-public enum DBC0520011TransitionEventName implements IContainerEvents {
+public enum DBC0530011StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    対象者検索へ戻る("対象者検索へ戻る"),
-    完了("完了");
+    NoChange(""),
+    初期状態("初期状態");
 
     private final RString name;
 
-    private DBC0520011TransitionEventName(String name) {
+    private DBC0530011StateName(String name) {
         this.name = new RString(name);
     }
 
