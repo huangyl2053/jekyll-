87e292e4e2ef0c5bc45aef4a8914147369f4d567
lizhi <lizhi@liandisys.com.cn>
2016-07-27 23:54:15 +0800

    checkStyle対応

--- DB/DBB/standard/business/src/main/java/jp/co/ndensan/reams/db/dbb/business/core/shichosonkado/ShichosonJoho.java
+++ DB/DBB/standard/business/src/main/java/jp/co/ndensan/reams/db/dbb/business/core/shichosonkado/ShichosonJoho.java
@@ -3,21 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbb.entity.db.relate.shichoson;
+package jp.co.ndensan.reams.db.dbb.business.core.shichosonkado;
 
 import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
 import jp.co.ndensan.reams.uz.uza.biz.YMDHMS;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
- * 市町村情報entity ShichosonJohoEntity
+ * 市町村情報entity ShichosonJoho
  *
  * @reamsid_L DBB-1690-040 sunhui
  */
 @lombok.Getter
 @lombok.Setter
 @SuppressWarnings("PMD.UnusedPrivateField")
-public class ShichosonJohoEntity {
+public class ShichosonJoho {
 
     private LasdecCode 市町村コード;
     private RString 市町村名;
