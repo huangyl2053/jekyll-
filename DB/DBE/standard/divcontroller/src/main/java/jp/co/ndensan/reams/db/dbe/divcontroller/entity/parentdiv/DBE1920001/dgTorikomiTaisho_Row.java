08014a5d4fb6b2b36dd30dcf4767752f58e1914d
xuyannan <xuyannan@liandisys.com.cn>
2016-08-01 20:24:14 +0800

    画面設計_DBE1920001_要介護認定申請連携データ取込 仕様変更の対応

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE1920001/dgTorikomiTaisho_Row.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE1920001/dgTorikomiTaisho_Row.java
@@ -3,14 +3,13 @@ package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE1920001;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Wed Jul 13 18:13:25 CST 2016 
+ * Thu Jul 28 11:26:46 CST 2016 
  */
 
 
 
 import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.ui.binding.Button;
 
 
 /**
@@ -26,7 +25,6 @@ public class dgTorikomiTaisho_Row extends DataRow {
     private RString meisho;
     private RString total;
     private RString fileName;
-    private Button sansho;
 
     public dgTorikomiTaisho_Row() {
         super();
@@ -34,26 +32,22 @@ public class dgTorikomiTaisho_Row extends DataRow {
         this.meisho = RString.EMPTY;
         this.total = RString.EMPTY;
         this.fileName = RString.EMPTY;
-        this.sansho = new Button();
         this.setOriginalData("fileFormat", fileFormat);
         this.setOriginalData("meisho", meisho);
         this.setOriginalData("total", total);
         this.setOriginalData("fileName", fileName);
-        this.setOriginalData("sansho", sansho);
     }
 
-    public dgTorikomiTaisho_Row(RString fileFormat, RString meisho, RString total, RString fileName, Button sansho) {
+    public dgTorikomiTaisho_Row(RString fileFormat, RString meisho, RString total, RString fileName) {
         super();
         this.setOriginalData("fileFormat", fileFormat);
         this.setOriginalData("meisho", meisho);
         this.setOriginalData("total", total);
         this.setOriginalData("fileName", fileName);
-        this.setOriginalData("sansho", sansho);
         this.fileFormat = fileFormat;
         this.meisho = meisho;
         this.total = total;
         this.fileName = fileName;
-        this.sansho = sansho;
     }
 
     public RString getFileFormat() {
@@ -70,10 +64,6 @@ public class dgTorikomiTaisho_Row extends DataRow {
 
     public RString getFileName() {
         return fileName;
-    }
-
-    public Button getSansho() {
-        return sansho;
     }
 
     public void setFileFormat(RString fileFormat) {
@@ -94,11 +84,6 @@ public class dgTorikomiTaisho_Row extends DataRow {
     public void setFileName(RString fileName) {
         this.setOriginalData("fileName", fileName);
         this.fileName = fileName;
-    }
-
-    public void setSansho(Button sansho) {
-        this.setOriginalData("sansho", sansho);
-        this.sansho = sansho;
     }
 
     // </editor-fold>
