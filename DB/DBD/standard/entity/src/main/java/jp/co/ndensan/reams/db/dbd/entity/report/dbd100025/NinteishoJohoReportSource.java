a47019a8e7436d44976e7717c4a64c1e59a44fdc
wangchao <wangchao@liandisys.com.cn>
2016-08-01 19:56:45 +0800

    ソースを更新

---
+++ DB/DBD/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbd/entity/report/dbd100025/NinteishoJohoReportSource.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbd.entity.report.dbd100025;
+
+/**
+ *
+ * @author wangchao
+ */
+public class NinteishoJohoReportSource {
+
+}
