4ff6c1bbc330c4901f926b5163eb8d2cce3ee3bf
wangchao <wangchao@liandisys.com.cn>
2016-08-01 20:01:59 +0800

    ビルドエラーを修正

--- DB/DBD/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbd/entity/report/dbd100025/NinteishoJohoReportSource.java
+++ DB/DBD/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbd/entity/report/dbd100025/NinteishoJohoReportSource.java
@@ -5,10 +5,12 @@
  */
 package jp.co.ndensan.reams.db.dbd.entity.report.dbd100025;
 
+import jp.co.ndensan.reams.uz.uza.report.IReportSource;
+
 /**
  *
  * @author wangchao
  */
-public class NinteishoJohoReportSource {
+public class NinteishoJohoReportSource implements IReportSource {
 
 }
