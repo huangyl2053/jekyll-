20ca01a9db2b7946ee23443fe835b2102a0ed0a6
lishengli <lishengli@liandisys.com.cn>
2016-08-01 15:09:33 +0800

    一次判定結果票　修正

--- DB/DBE/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbe/entity/db/relate/ichijihanteikekkahyo/TiyosaKekka.java
+++ DB/DBE/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbe/entity/db/relate/ichijihanteikekkahyo/TiyosaKekka.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbe.entity.report.source.ichijihanteikekkahyoa3;
+package jp.co.ndensan.reams.db.dbe.entity.db.relate.ichijihanteikekkahyo;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 
@@ -15,9 +15,12 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
 @lombok.Getter
 @lombok.Setter
 @SuppressWarnings("PMD.UnusedPrivateField")
-public class TiyosaKekkaHuseiko {
+public class TiyosaKekka {
 
+    private RString 調査結果;
+    private RString 段階改善フラグ;
+    private RString 段階改善値;
+    private RString 前回結果;
     private RString 認定調査と主治医意見書の結果比較;
     private RString 特記事項有無;
-
 }
