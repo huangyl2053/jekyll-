80301fe231d754b532906e5652a3355baec96378
wangxiaodong <wangxiaodong@liandisys.com.cn>
2016-08-02 14:33:05 +0800

    バッチ設計_DBE517001_審査会資料一括作成（委員）共通情報修正

--- DB/DBE/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbe/entity/db/relate/shiryoshinsakai/ShinsakaiSiryoKyotsuEntity.java
+++ DB/DBE/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbe/entity/db/relate/shiryoshinsakai/ShinsakaiSiryoKyotsuEntity.java
@@ -7,6 +7,7 @@ package jp.co.ndensan.reams.db.dbe.entity.db.relate.shiryoshinsakai;
 
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShinseishoKanriNo;
 import jp.co.ndensan.reams.uz.uza.biz.AtenaMeisho;
+import jp.co.ndensan.reams.uz.uza.biz.Code;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
 import jp.co.ndensan.reams.uz.uza.lang.RDateTime;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
@@ -24,6 +25,7 @@ public class ShinsakaiSiryoKyotsuEntity {
     private ShinseishoKanriNo shinseishoKanriNo;
     private int shinsakaiOrder;
     private int ninteichosaRirekiNo;
+    private Code koroshoIfShikibetsuCode;
     private RString shoKisaiHokenshaNo;
     private RString hihokenshaNo;
     private AtenaMeisho hihokenshaName;
