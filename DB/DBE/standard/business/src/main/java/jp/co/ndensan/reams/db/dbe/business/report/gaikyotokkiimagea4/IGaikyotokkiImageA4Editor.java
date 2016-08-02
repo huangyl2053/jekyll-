f8d14966c273f43081834e309f4761f31f9842e9
wangrenze <wangrenze@liandisys.com.cn>
2016-08-02 19:21:40 +0800

    帳票設計_DBE517143_事務局概況特記 新規作成

---
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/gaikyotokkiimagea4/IGaikyotokkiImageA4Editor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.business.report.gaikyotokkiimagea4;
+
+import jp.co.ndensan.reams.db.dbe.entity.report.gaikyotokkiimagea4.GaikyotokkiImageA4ReportSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportEditor;
+
+/**
+ * 事務局概況特記Editorのインターフェースクラスです。
+ *
+ * @reamsid_L DBE-0150-370 wangrenze
+ */
+public interface IGaikyotokkiImageA4Editor extends IReportEditor<GaikyotokkiImageA4ReportSource> {
+
+}
