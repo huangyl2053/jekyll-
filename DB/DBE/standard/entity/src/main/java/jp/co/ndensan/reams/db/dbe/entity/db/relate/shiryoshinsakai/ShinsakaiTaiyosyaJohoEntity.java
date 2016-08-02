ab025f2fc632e4203a04006d2e313365a91be091
wangxiaodong <wangxiaodong@liandisys.com.cn>
2016-08-02 15:42:32 +0800

    バッチ設計_DBE517001_審査会資料一括作成（委員）共通情報修正

--- DB/DBE/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbe/entity/db/relate/shiryoshinsakai/ShinsakaiTaiyosyaJohoEntity.java
+++ DB/DBE/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbe/entity/db/relate/shiryoshinsakai/ShinsakaiTaiyosyaJohoEntity.java
@@ -9,34 +9,32 @@ import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShinseishoK
 import jp.co.ndensan.reams.uz.uza.biz.AtenaMeisho;
 import jp.co.ndensan.reams.uz.uza.biz.Code;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
-import jp.co.ndensan.reams.uz.uza.lang.RDateTime;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
  * 介護認定審査対象者一覧表の情報です。
  *
- * @reamsid_L DBE-0150-200 linghuhang
+ * @reamsid_L DBE-0150-200 wangxiaodong
  */
 @lombok.Getter
 @lombok.Setter
 @SuppressWarnings("PMD.UnusedPrivateField")
-public class ShinsakaiSiryoKyotsuEntity {
+public class ShinsakaiTaiyosyaJohoEntity {
 
     private ShinseishoKanriNo shinseishoKanriNo;
     private int shinsakaiOrder;
-    private int ninteichosaRirekiNo;
-    private Code koroshoIfShikibetsuCode;
     private RString shoKisaiHokenshaNo;
     private RString hihokenshaNo;
     private AtenaMeisho hihokenshaName;
-    private FlexibleDate ninteiShinseiYMD;
-    private FlexibleDate ninteichosaJisshiYMD;
-    private FlexibleDate shinsakaiKaisaiYMD;
-    private RString gaikyoChosaTextImageKubun;
-    private RString tokki;
-    private RString riyoShisetsuShimei;
-    private RString riyoShisetsuJusho;
-    private RString riyoShisetsuTelNo;
-    private RDateTime imageSharedFileId;
-    private boolean isJimukyoku;
+    private Code seibetsu;
+    private int age;
+    private Code koroshoIfShikibetsuCode;
+    private Code nijiHanteiYokaigoJotaiKubunCode;
+    private int nijiHanteiNinteiYukoKikan;
+    private FlexibleDate nijiHanteiNinteiYukoKaishiYMD;
+    private FlexibleDate nijiHanteiNinteiYukoShuryoYMD;
+    private Code ichijiHanteiKekkaCode;
+    private Code ichijiHanteiKekkaNinchishoKasanCode;
+    private RString ichijiHnateiKeikokuCode;
+    private int kijunJikan;
 }
