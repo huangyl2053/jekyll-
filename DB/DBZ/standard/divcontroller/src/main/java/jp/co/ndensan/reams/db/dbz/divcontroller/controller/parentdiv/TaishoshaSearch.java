f1a319d81133adfe13e0f1ca2851655f39285e9e
城間 篤人 <n8178@ndensan.co.jp>
2016-08-05 23:29:02 +0900

    以下のチケットで指摘された不具合に対応（途中） 94718 94719 94720 94722

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/controller/parentdiv/TaishoshaSearch.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/controller/parentdiv/TaishoshaSearch.java
@@ -150,9 +150,9 @@ public class TaishoshaSearch {
         // 検索条件未指定チェック
         HihokenshaFinderDiv 検索条件Div = div.getSearchCondition().getCcdSearchCondition();
         boolean 検索条件Flag = 検索条件Div.getKaigoFinder().getTxtHihokenshaNo().getValue().isEmpty()
-                           && 検索条件Div.getKaigoFinder().getKaigoFinderDetail().getChkHihokenshaDaicho().getSelectedItems().isEmpty()
-                           && 検索条件Div.getKaigoFinder().getKaigoFinderDetail().getChkJukyushaDaicho().getSelectedItems().isEmpty()
-                           && 検索条件Div.getKaigoFinder().getKaigoFinderDetail().getChkJushochiTokureisha().getSelectedItems().isEmpty(); //&& !検索条件Div.getCcdAtenaFinder().hasChanged()
+                && 検索条件Div.getKaigoFinder().getKaigoFinderDetail().getChkHihokenshaDaicho().getSelectedItems().isEmpty()
+                && 検索条件Div.getKaigoFinder().getKaigoFinderDetail().getChkJukyushaDaicho().getSelectedItems().isEmpty()
+                && 検索条件Div.getKaigoFinder().getKaigoFinderDetail().getChkJushochiTokureisha().getSelectedItems().isEmpty(); //&& !検索条件Div.getCcdAtenaFinder().hasChanged()
 
         boolean 宛名条件修正Flag = 検索条件Div.getCcdAtenaFinder().hasChanged();
 
@@ -392,7 +392,7 @@ public class TaishoshaSearch {
         } else if (資格検索.getJukyushaDaichoHihokenshaNo() != null && !資格検索.getJukyushaDaichoHihokenshaNo().isEmpty()) {
             被保険者区分 = HihoKubun.受給;
         } else if ((資格検索.getHihokenshaNo() != null && !資格検索.getHihokenshaNo().isEmpty())
-                   && (資格検索.getShikakuSoshitsuYMD() == null || 資格検索.getShikakuSoshitsuYMD().isEmpty())) {
+                && (資格検索.getShikakuSoshitsuYMD() == null || 資格検索.getShikakuSoshitsuYMD().isEmpty())) {
             被保険者区分 = HihoKubun.資格;
         }
         return 被保険者区分;
@@ -410,7 +410,7 @@ public class TaishoshaSearch {
 
         return new dgGaitoshaList_Row(
                 資格検索結果.getHihokenshaNo() != null ? 資格検索結果.getHihokenshaNo().value() : RString.EMPTY,
-                資格検索結果.getShikibetsuCode() != null ? 資格検索結果.getShikibetsuCode().value() : RString.EMPTY,
+                識別対象.get識別コード() != null ? 識別対象.get識別コード().getColumnValue() : RString.EMPTY,
                 被保険者区分 != null ? 被保険者区分.getName() : RString.EMPTY,
                 名称,
                 名称カナ,
