119f621727e4131e36573a247eb567c78aeb33b4
chenxiangyu <chenxiangyu@liandisys.com.cn>
2016-08-05 18:59:32 +0800

    「画面設計_DBUMNA1002_全国保険者マスタ更新」の新規です。

---
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/entity/parentdiv/DBU0800011/dgHokenshas_Row.java
@@ -0,0 +1,118 @@
+package jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0800011;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+ * コードが再生成されるときに損失したりします。
+ * Tue Aug 02 16:49:15 CST 2016 
+ */
+
+
+
+import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+
+/**
+ * dgHokenshas_Row のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+
+public class dgHokenshas_Row extends DataRow {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-05-30_13-18-33">
+
+    private RString jotai;
+    private RString hokenshaNo;
+    private RString hokenshaName;
+    private RString yubinNo;
+    private RString jusho;
+    private RString telNo;
+
+    public dgHokenshas_Row() {
+        super();
+        this.jotai = RString.EMPTY;
+        this.hokenshaNo = RString.EMPTY;
+        this.hokenshaName = RString.EMPTY;
+        this.yubinNo = RString.EMPTY;
+        this.jusho = RString.EMPTY;
+        this.telNo = RString.EMPTY;
+        this.setOriginalData("jotai", jotai);
+        this.setOriginalData("hokenshaNo", hokenshaNo);
+        this.setOriginalData("hokenshaName", hokenshaName);
+        this.setOriginalData("yubinNo", yubinNo);
+        this.setOriginalData("jusho", jusho);
+        this.setOriginalData("telNo", telNo);
+    }
+
+    public dgHokenshas_Row(RString jotai, RString hokenshaNo, RString hokenshaName, RString yubinNo, RString jusho, RString telNo) {
+        super();
+        this.setOriginalData("jotai", jotai);
+        this.setOriginalData("hokenshaNo", hokenshaNo);
+        this.setOriginalData("hokenshaName", hokenshaName);
+        this.setOriginalData("yubinNo", yubinNo);
+        this.setOriginalData("jusho", jusho);
+        this.setOriginalData("telNo", telNo);
+        this.jotai = jotai;
+        this.hokenshaNo = hokenshaNo;
+        this.hokenshaName = hokenshaName;
+        this.yubinNo = yubinNo;
+        this.jusho = jusho;
+        this.telNo = telNo;
+    }
+
+    public RString getJotai() {
+        return jotai;
+    }
+
+    public RString getHokenshaNo() {
+        return hokenshaNo;
+    }
+
+    public RString getHokenshaName() {
+        return hokenshaName;
+    }
+
+    public RString getYubinNo() {
+        return yubinNo;
+    }
+
+    public RString getJusho() {
+        return jusho;
+    }
+
+    public RString getTelNo() {
+        return telNo;
+    }
+
+    public void setJotai(RString jotai) {
+        this.setOriginalData("jotai", jotai);
+        this.jotai = jotai;
+    }
+
+    public void setHokenshaNo(RString hokenshaNo) {
+        this.setOriginalData("hokenshaNo", hokenshaNo);
+        this.hokenshaNo = hokenshaNo;
+    }
+
+    public void setHokenshaName(RString hokenshaName) {
+        this.setOriginalData("hokenshaName", hokenshaName);
+        this.hokenshaName = hokenshaName;
+    }
+
+    public void setYubinNo(RString yubinNo) {
+        this.setOriginalData("yubinNo", yubinNo);
+        this.yubinNo = yubinNo;
+    }
+
+    public void setJusho(RString jusho) {
+        this.setOriginalData("jusho", jusho);
+        this.jusho = jusho;
+    }
+
+    public void setTelNo(RString telNo) {
+        this.setOriginalData("telNo", telNo);
+        this.telNo = telNo;
+    }
+
+    // </editor-fold>
+}
