c1fc31abad2095df92f1aa3d884243bb57a65f5b
橋本　治義 <n8322@ndensan.co.jp>
2016-08-05 20:25:46 +0900

    画面設計_DBDMN24006_事業所向け社会福祉法人軽減対象者一覧発行

---
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD1050021/DBD1050021TransitionEventName.java
@@ -0,0 +1,36 @@
+package jp.co.ndensan.reams.db.dbd.divcontroller.entity.parentdiv.DBD1050021;
+
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
+
+/**
+ * このコードはツールによって生成されました。</br>
+ * このファイルへの変更は、再生成時には損失するため</br>
+ * 不正な動作の原因になります。</br>
+ * DBD1050021のイベント名定義クラスです</br>
+ *
+ * @author 自動生成
+ */
+public enum DBD1050021TransitionEventName implements IContainerEvents {
+// <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
+
+    対象者一覧表("対象者一覧表"),
+    完了("完了");
+
+    private final RString name;
+
+    private DBD1050021TransitionEventName(String name) {
+        this.name = new RString(name);
+    }
+
+    /**
+     * getName
+     *
+     * @return 名前
+     */
+    @Override
+    public RString getName() {
+        return name;
+    }
+// </editor-fold>
+}
