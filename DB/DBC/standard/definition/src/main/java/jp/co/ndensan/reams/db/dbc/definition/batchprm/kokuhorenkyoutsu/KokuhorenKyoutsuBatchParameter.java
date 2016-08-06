cf0d3d3cae3d0f859f8df6c943abd979c6ac70a7
zhangrui <rzhang@sinoi.com.cn>
2016-08-06 10:06:32 +0800

    国保連共通のバッチパラメータです

---
+++ DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/batchprm/kokuhorenkyoutsu/KokuhorenKyoutsuBatchParameter.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.definition.batchprm.kokuhorenkyoutsu;
+
+import jp.co.ndensan.reams.db.dbc.definition.core.saishori.SaiShoriKubun;
+import jp.co.ndensan.reams.uz.uza.batch.BatchParameter;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleYearMonth;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+/**
+ * 国保連共通のバッチパラメータです。
+ *
+ * @reamsid_L DBC-2450-030 zhangrui
+ */
+@lombok.Getter
+@lombok.Setter
+@SuppressWarnings("PMD.UnusedPrivateField")
+public class KokuhorenKyoutsuBatchParameter {
+
+    private static final String KEY_SHORIYM = "shoriYM";
+    private static final String NAME_SHORIYM = "処理年月";
+    private static final String KEY_SAISHORIKUBUN = "saishoriKubun";
+    private static final String NAME_SAISHORIKUBUN = "再処理区分";
+    private static final String KEY_SHUTSURYOKUJUNID = "shutsuryokujunId";
+    private static final String NAME_SHUTSURYOKUJUNID = "出力順ID";
+
+    @BatchParameter(key = KEY_SHORIYM, name = NAME_SHORIYM)
+    private FlexibleYearMonth shoriYM;
+    @BatchParameter(key = KEY_SAISHORIKUBUN, name = NAME_SAISHORIKUBUN)
+    private SaiShoriKubun saishoriKubun;
+    @BatchParameter(key = KEY_SHUTSURYOKUJUNID, name = NAME_SHUTSURYOKUJUNID)
+    private RString shutsuryokujunId;
+
+}
