03ff7e621f8f18b1dafab3bf5486347d468b1624
surun <surun@liandisys.com.cn>
2016-08-06 19:20:01 +0800

    帳票設計_DBC200054_給付実績更新結果情報一覧表ソースの実装

--- DB/DBC/standard/service/src/main/java/jp/co/ndensan/reams/db/dbc/service/report/kyufujissekikoshinkekkaichiran/KyufujissekiKoshinkekkaIchiranPrintService.java
+++ DB/DBC/standard/service/src/main/java/jp/co/ndensan/reams/db/dbc/service/report/kyufujissekikoshinkekkaichiran/KyufujissekiKoshinkekkaIchiranPrintService.java
@@ -6,19 +6,19 @@
 package jp.co.ndensan.reams.db.dbc.service.report.kyufujissekikoshinkekkaichiran;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import jp.co.ndensan.reams.db.dbc.business.report.kyufujissekikoshinkekkaichiran.KyufujissekiKoshinkekkaIchiranProperty;
 import jp.co.ndensan.reams.db.dbc.business.report.kyufujissekikoshinkekkaichiran.KyufujissekiKoshinkekkaIchiranReport;
 import jp.co.ndensan.reams.db.dbc.definition.reportid.ReportIdDBC;
 import jp.co.ndensan.reams.db.dbc.entity.db.relate.chohyoshutsuryokutaishodate.ChohyoShutsuryokuTaishoDateEntity;
 import jp.co.ndensan.reams.db.dbc.entity.db.relate.kyufujissekikoshinkekkaichiran.KyufujissekiKoshinkekkaIchiranSource;
-import jp.co.ndensan.reams.ur.urz.business.core.association.Association;
 import jp.co.ndensan.reams.ur.urz.business.core.reportoutputorder.IOutputOrder;
 import jp.co.ndensan.reams.ur.urz.business.core.reportoutputorder.ISetSortItem;
-import jp.co.ndensan.reams.ur.urz.service.core.association.AssociationFinderFactory;
-import jp.co.ndensan.reams.ur.urz.service.core.association.IAssociationFinder;
 import jp.co.ndensan.reams.ur.urz.service.core.reportoutputorder.ChohyoShutsuryokujunFinderFactory;
 import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
+import jp.co.ndensan.reams.uz.uza.biz.YMDHMS;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleYearMonth;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.report.IReportProperty;
@@ -34,7 +34,7 @@ import jp.co.ndensan.reams.uz.uza.report.source.breaks.BreakAggregator;
 /**
  * 帳票設計_DBC200054_給付実績更新結果情報一覧表 PrintService
  *
- * @reamsid_L DBC-2470-030 sunhui
+ * @reamsid_L DBC-2470-030 surun
  */
 public class KyufujissekiKoshinkekkaIchiranPrintService {
 
@@ -50,27 +50,34 @@ public class KyufujissekiKoshinkekkaIchiranPrintService {
      * @param 処理年月 FlexibleYearMonth
      * @param shutsuryokujunID RString
      * @param entityList List<ChohyoShutsuryokuTaishoDateEntity>
+     * @param システム日時 YMDHMS
      * @return SourceDataCollection
      */
     public SourceDataCollection print(FlexibleYearMonth 処理年月, Long shutsuryokujunID,
-            List<ChohyoShutsuryokuTaishoDateEntity> entityList) {
+            List<ChohyoShutsuryokuTaishoDateEntity> entityList, YMDHMS システム日時) {
         SourceDataCollection collection;
         try (ReportManager reportManager = new ReportManager()) {
-            printFukusu(処理年月, shutsuryokujunID, entityList, reportManager);
+            printFukusu(処理年月, shutsuryokujunID, entityList, システム日時, reportManager);
             collection = reportManager.publish();
         }
         return collection;
     }
 
-    private void printFukusu(FlexibleYearMonth 処理年月, Long shutsuryokujunID,
-            List<ChohyoShutsuryokuTaishoDateEntity> entityList, ReportManager reportManager) {
+    /**
+     * 給付実績更新結果情報一覧表を印刷します。
+     *
+     * @param 処理年月 FlexibleYearMonth
+     * @param shutsuryokujunID Long
+     * @param entityList List<ChohyoShutsuryokuTaishoDateEntity>
+     * @param システム日時 YMDHMS
+     * @param reportManager ReportManager
+     */
+    public void printFukusu(FlexibleYearMonth 処理年月, Long shutsuryokujunID,
+            List<ChohyoShutsuryokuTaishoDateEntity> entityList, YMDHMS システム日時, ReportManager reportManager) {
         KyufujissekiKoshinkekkaIchiranProperty property = new KyufujissekiKoshinkekkaIchiranProperty();
-        IAssociationFinder finder = AssociationFinderFactory.createInstance();
-        Association association = finder.getAssociation();
         try (ReportAssembler<KyufujissekiKoshinkekkaIchiranSource> assembler = createAssembler(property, reportManager)) {
             ReportSourceWriter<KyufujissekiKoshinkekkaIchiranSource> reportSourceWriter
                     = new ReportSourceWriter(assembler);
-
             IOutputOrder 並び順 = ChohyoShutsuryokujunFinderFactory.createInstance()
                     .get出力順(SubGyomuCode.DBC介護給付, ReportIdDBC.DBC200054.getReportId(), shutsuryokujunID);
             int i = 0;
@@ -99,8 +106,22 @@ public class KyufujissekiKoshinkekkaIchiranPrintService {
                     i = i + 1;
                 }
             }
-            new KyufujissekiKoshinkekkaIchiranReport(処理年月, entityList, association, 並び順の１件目, 並び順の２件目,
-                    並び順の３件目, 並び順の４件目, 並び順の５件目, 改頁項目List).writeBy(reportSourceWriter);
+            Collections.sort(entityList, new Comparator<ChohyoShutsuryokuTaishoDateEntity>() {
+                @Override
+                public int compare(ChohyoShutsuryokuTaishoDateEntity e1, ChohyoShutsuryokuTaishoDateEntity e2) {
+                    return e1.get給付実績_コントロールレコード保険者番号().compareTo(e2.get給付実績_コントロールレコード保険者番号());
+                }
+            });
+            List<RString> indexList = new ArrayList<>();
+            for (int j = INDEX_0; j < entityList.size() - INDEX_1; j++) {
+                if (!entityList.get(j).get給付実績_コントロールレコード保険者番号().equals(entityList.get(++j).
+                        get給付実績_コントロールレコード保険者番号())) {
+                    indexList.add(new RString(j + INDEX_1));
+                }
+            }
+            indexList.add(new RString(entityList.size()));
+            new KyufujissekiKoshinkekkaIchiranReport(処理年月, entityList, 並び順の１件目, 並び順の２件目,
+                    並び順の３件目, 並び順の４件目, 並び順の５件目, 改頁項目List, システム日時, indexList).writeBy(reportSourceWriter);
         }
     }
 
