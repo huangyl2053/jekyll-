d7a2f687cc0a9eabfeb42c396c90b7146401bed3
橋本　治義 <n8322@ndensan.co.jp>
2016-08-08 15:35:14 +0900

    発注依頼分の不備に関する緊急対応依頼 1930-010 居宅サービス自己作成サービス利用票登録 画面設計_DBC0020011_居宅サービス計画照会 DBC0020011.raml

---
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0020011/KyotakuServiceRirekiIchiranDiv.java
@@ -0,0 +1,52 @@
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0020011;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+
+/**
+ * KyotakuServiceRirekiIchiran のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class KyotakuServiceRirekiIchiranDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-07-28_11-34-20">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("dgKyotakuServiceRirekiIchiran")
+    private DataGrid<dgKyotakuServiceRirekiIchiran_Row> dgKyotakuServiceRirekiIchiran;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * getdgKyotakuServiceRirekiIchiran
+     * @return dgKyotakuServiceRirekiIchiran
+     */
+    @JsonProperty("dgKyotakuServiceRirekiIchiran")
+    public DataGrid<dgKyotakuServiceRirekiIchiran_Row> getDgKyotakuServiceRirekiIchiran() {
+        return dgKyotakuServiceRirekiIchiran;
+    }
+
+    /*
+     * setdgKyotakuServiceRirekiIchiran
+     * @param dgKyotakuServiceRirekiIchiran dgKyotakuServiceRirekiIchiran
+     */
+    @JsonProperty("dgKyotakuServiceRirekiIchiran")
+    public void setDgKyotakuServiceRirekiIchiran(DataGrid<dgKyotakuServiceRirekiIchiran_Row> dgKyotakuServiceRirekiIchiran) {
+        this.dgKyotakuServiceRirekiIchiran = dgKyotakuServiceRirekiIchiran;
+    }
+
+    // </editor-fold>
+}
---
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0020011/TodokedeshaDiv.java
@@ -0,0 +1,153 @@
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0020011;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+import jp.co.ndensan.reams.uz.uza.ui.binding.domain.*;
+
+/**
+ * Todokedesha のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class TodokedeshaDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-07-28_11-34-20">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("txtTodokedeshaShimei")
+    private TextBoxAtenaMeisho txtTodokedeshaShimei;
+    @JsonProperty("txtTodokedeshaKankeiKubun")
+    private TextBox txtTodokedeshaKankeiKubun;
+    @JsonProperty("txtTodokedeshaShimeiKana")
+    private TextBoxAtenaKanaMeisho txtTodokedeshaShimeiKana;
+    @JsonProperty("txtTodokedeshaTelNo")
+    private TextBoxTelNo txtTodokedeshaTelNo;
+    @JsonProperty("txtTodokedeshaYubinNo")
+    private TextBoxYubinNo txtTodokedeshaYubinNo;
+    @JsonProperty("txtTodokedeshaJusho")
+    private TextBox txtTodokedeshaJusho;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * gettxtTodokedeshaShimei
+     * @return txtTodokedeshaShimei
+     */
+    @JsonProperty("txtTodokedeshaShimei")
+    public TextBoxAtenaMeisho getTxtTodokedeshaShimei() {
+        return txtTodokedeshaShimei;
+    }
+
+    /*
+     * settxtTodokedeshaShimei
+     * @param txtTodokedeshaShimei txtTodokedeshaShimei
+     */
+    @JsonProperty("txtTodokedeshaShimei")
+    public void setTxtTodokedeshaShimei(TextBoxAtenaMeisho txtTodokedeshaShimei) {
+        this.txtTodokedeshaShimei = txtTodokedeshaShimei;
+    }
+
+    /*
+     * gettxtTodokedeshaKankeiKubun
+     * @return txtTodokedeshaKankeiKubun
+     */
+    @JsonProperty("txtTodokedeshaKankeiKubun")
+    public TextBox getTxtTodokedeshaKankeiKubun() {
+        return txtTodokedeshaKankeiKubun;
+    }
+
+    /*
+     * settxtTodokedeshaKankeiKubun
+     * @param txtTodokedeshaKankeiKubun txtTodokedeshaKankeiKubun
+     */
+    @JsonProperty("txtTodokedeshaKankeiKubun")
+    public void setTxtTodokedeshaKankeiKubun(TextBox txtTodokedeshaKankeiKubun) {
+        this.txtTodokedeshaKankeiKubun = txtTodokedeshaKankeiKubun;
+    }
+
+    /*
+     * gettxtTodokedeshaShimeiKana
+     * @return txtTodokedeshaShimeiKana
+     */
+    @JsonProperty("txtTodokedeshaShimeiKana")
+    public TextBoxAtenaKanaMeisho getTxtTodokedeshaShimeiKana() {
+        return txtTodokedeshaShimeiKana;
+    }
+
+    /*
+     * settxtTodokedeshaShimeiKana
+     * @param txtTodokedeshaShimeiKana txtTodokedeshaShimeiKana
+     */
+    @JsonProperty("txtTodokedeshaShimeiKana")
+    public void setTxtTodokedeshaShimeiKana(TextBoxAtenaKanaMeisho txtTodokedeshaShimeiKana) {
+        this.txtTodokedeshaShimeiKana = txtTodokedeshaShimeiKana;
+    }
+
+    /*
+     * gettxtTodokedeshaTelNo
+     * @return txtTodokedeshaTelNo
+     */
+    @JsonProperty("txtTodokedeshaTelNo")
+    public TextBoxTelNo getTxtTodokedeshaTelNo() {
+        return txtTodokedeshaTelNo;
+    }
+
+    /*
+     * settxtTodokedeshaTelNo
+     * @param txtTodokedeshaTelNo txtTodokedeshaTelNo
+     */
+    @JsonProperty("txtTodokedeshaTelNo")
+    public void setTxtTodokedeshaTelNo(TextBoxTelNo txtTodokedeshaTelNo) {
+        this.txtTodokedeshaTelNo = txtTodokedeshaTelNo;
+    }
+
+    /*
+     * gettxtTodokedeshaYubinNo
+     * @return txtTodokedeshaYubinNo
+     */
+    @JsonProperty("txtTodokedeshaYubinNo")
+    public TextBoxYubinNo getTxtTodokedeshaYubinNo() {
+        return txtTodokedeshaYubinNo;
+    }
+
+    /*
+     * settxtTodokedeshaYubinNo
+     * @param txtTodokedeshaYubinNo txtTodokedeshaYubinNo
+     */
+    @JsonProperty("txtTodokedeshaYubinNo")
+    public void setTxtTodokedeshaYubinNo(TextBoxYubinNo txtTodokedeshaYubinNo) {
+        this.txtTodokedeshaYubinNo = txtTodokedeshaYubinNo;
+    }
+
+    /*
+     * gettxtTodokedeshaJusho
+     * @return txtTodokedeshaJusho
+     */
+    @JsonProperty("txtTodokedeshaJusho")
+    public TextBox getTxtTodokedeshaJusho() {
+        return txtTodokedeshaJusho;
+    }
+
+    /*
+     * settxtTodokedeshaJusho
+     * @param txtTodokedeshaJusho txtTodokedeshaJusho
+     */
+    @JsonProperty("txtTodokedeshaJusho")
+    public void setTxtTodokedeshaJusho(TextBox txtTodokedeshaJusho) {
+        this.txtTodokedeshaJusho = txtTodokedeshaJusho;
+    }
+
+    // </editor-fold>
+}
