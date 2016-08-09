4c37cc94f634dd1f3b74f17108189dcce1acc2b9
suguangjun <suguangjun@liandisys.com.cn>
2016-08-09 20:45:54 +0800

    バッチ設計_DBE621001_報酬支払い通知書請求書・確認書 仕様変更対応

--- DB/DBE/standard/service/src/main/java/jp/co/ndensan/reams/db/dbe/service/core/ninteishinseijoho/ninteikekkajoho/NinteiKekkaJohoManager.java
+++ DB/DBE/standard/service/src/main/java/jp/co/ndensan/reams/db/dbe/service/core/ninteishinseijoho/ninteikekkajoho/NinteiKekkaJohoManager.java
@@ -6,6 +6,8 @@ package jp.co.ndensan.reams.db.dbe.service.core.ninteishinseijoho.ninteikekkajoh
 
 import static java.util.Objects.requireNonNull;
 import jp.co.ndensan.reams.db.dbe.business.core.ninteishinseijoho.ninteikekkajoho.NinteiKekkaJoho;
+import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShinseishoKanriNo;
+import jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT5102NinteiKekkaJohoEntity;
 import jp.co.ndensan.reams.db.dbz.persistence.db.basic.DbT5102NinteiKekkaJohoDac;
 import jp.co.ndensan.reams.ur.urz.definition.message.UrSystemErrorMessages;
 import jp.co.ndensan.reams.uz.uza.util.di.InstanceProvider;
