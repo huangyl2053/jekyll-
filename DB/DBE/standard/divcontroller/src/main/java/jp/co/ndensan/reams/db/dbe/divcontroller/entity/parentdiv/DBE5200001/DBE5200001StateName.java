93e0772ac2de0d467240b1d5ad622643388d474c
姜岐 <n8344@ndensan.co.jp>
2016-08-09 09:00:04 +0900

    状態設定追加

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE5200001/DBE5200001StateName.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE5200001/DBE5200001StateName.java
@@ -1,4 +1,4 @@
-package jp.co.ndensan.reams.db.dbd.divcontroller.entity.parentdiv.DBD5190002;
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE5200001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
@@ -7,19 +7,19 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBD5190002の状態名定義クラスです
+ * DBE5200001の状態名定義クラスです
  *
  * @author 自動生成
  */
-public enum DBD5190002StateName implements IStateEnumerations {
+public enum DBE5200001StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
     NoChange(""),
-    All("All");
+    事前審査結果登録("事前審査結果登録");
 
     private final RString name;
 
-    private DBD5190002StateName(String name) {
+    private DBE5200001StateName(String name) {
         this.name = new RString(name);
     }
 
--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE5200001/DBE5200001TransitionEventName.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE5200001/DBE5200001TransitionEventName.java
@@ -1,25 +1,25 @@
-package jp.co.ndensan.reams.db.dbd.divcontroller.entity.parentdiv.DBD5190002;
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE5200001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 
 /**
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBD5190002の状態名定義クラスです
+ * DBE5200001のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBD5190002StateName implements IStateEnumerations {
+public enum DBE5200001TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    NoChange(""),
-    All("All");
+    一覧に戻る("一覧に戻る"),
+    処理完了("処理完了");
 
     private final RString name;
 
-    private DBD5190002StateName(String name) {
+    private DBE5200001TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
