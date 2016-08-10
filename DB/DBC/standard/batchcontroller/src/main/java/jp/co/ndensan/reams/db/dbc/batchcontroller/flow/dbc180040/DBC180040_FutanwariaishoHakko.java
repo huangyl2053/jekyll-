b41a3dacef5500381892521c0f9ccee5404a9f05
pengxingyi <pengxingyi@liandisys.com.cn>
2016-08-10 14:10:25 +0800

    バッチ設計_DBCMNK3001_負担割合証発行（一括）(ソース修正)

--- DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/flow/dbc180040/DBC180040_FutanwariaishoHakko.java
+++ DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/flow/dbc180040/DBC180040_FutanwariaishoHakko.java
@@ -10,11 +10,14 @@ import jp.co.ndensan.reams.db.dbc.batchcontroller.step.dbc180040.FutanWariaiShoH
 import jp.co.ndensan.reams.db.dbc.batchcontroller.step.dbc180040.FutanWariaiShoOutputProcess;
 import jp.co.ndensan.reams.db.dbc.batchcontroller.step.dbc180040.FutanWariaiShoRenchoTateOutputProcess;
 import jp.co.ndensan.reams.db.dbc.batchcontroller.step.dbc180040.FutanWariaiShoRenchoYokoOutputProcess;
+import jp.co.ndensan.reams.db.dbc.batchcontroller.step.dbc180040.RiyoshaFutanWariaiShoInsertProcess;
 import jp.co.ndensan.reams.db.dbc.batchcontroller.step.dbc180040.ShoriDateKanriUpdateProcess;
 import jp.co.ndensan.reams.db.dbc.definition.batchprm.futanwariaishohakko.FutanwariaishoHakkoBatchParameter;
+import jp.co.ndensan.reams.db.dbc.definition.mybatisprm.futanwariaishohakko.FutanwariaishoHakkoMybatisParameter;
 import jp.co.ndensan.reams.db.dbc.definition.processprm.futanwariaishohakko.FutanwariaishoHakkoProcessParameter;
 import jp.co.ndensan.reams.db.dbc.definition.reportid.ReportIdDBC;
-import jp.co.ndensan.reams.db.dbz.business.core.basic.ChohyoSeigyoHanyo;
+import jp.co.ndensan.reams.db.dbc.persistence.db.mapper.relate.futanwariaishohakko.IFutanwariaishoHakkoMapper;
+import jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT7067ChohyoSeigyoHanyoEntity;
 import jp.co.ndensan.reams.ua.uax.business.core.shikibetsutaisho.search.AtesakiGyomuHanteiKeyFactory;
 import jp.co.ndensan.reams.ua.uax.business.core.shikibetsutaisho.search.AtesakiPSMSearchKeyBuilder;
 import jp.co.ndensan.reams.ua.uax.business.core.shikibetsutaisho.search.ShikibetsuTaishoPSMSearchKeyBuilder;
@@ -41,6 +44,7 @@ public class DBC180040_FutanwariaishoHakko extends BatchFlowBase<FutanwariaishoH
     private static final RString THREE = new RString("3");
     private static final RString 全件用紙タイプ = new RString("全件用紙タイプ");
     private static final RString 異動認定分用紙タイプ = new RString("異動、認定分用紙タイプ");
+    private IFutanwariaishoHakkoMapper mapper;
 
     private static final String 利用者負担割合期間 = "FutanWariaiInsertProcess";
     private static final String 負担割合証 = "FutanWariaiShoOutputProcess";
@@ -48,11 +52,13 @@ public class DBC180040_FutanwariaishoHakko extends BatchFlowBase<FutanwariaishoH
     private static final String 負担割合証連帳横 = "FutanWariaiShoRenchoYokoOutputProcess";
     private static final String 負担割合証発行一覧表 = "FutanWariaiShoHakkoIchiranOutputProcess";
     private static final String 処理日付管理 = "ShoriDateKanriUpdateProcess";
+    private static final String 利用者負担割合証Temp = "RiyoshaFutanWariaiShoInsertProcess";
 
     @Override
     protected void defineFlow() {
         RString type = getType();
         executeStep(利用者負担割合期間);
+        executeStep(利用者負担割合証Temp);
         if (ONE.equals(type)) {
             executeStep(負担割合証);
         } else if (TWO.equals(type)) {
@@ -136,12 +142,32 @@ public class DBC180040_FutanwariaishoHakko extends BatchFlowBase<FutanwariaishoH
                 .define();
     }
 
+    /**
+     * 処理日付管理です。
+     *
+     * @return IBatchFlowCommand
+     */
+    @Step(利用者負担割合証Temp)
+    protected IBatchFlowCommand createReport7() {
+        return loopBatch(RiyoshaFutanWariaiShoInsertProcess.class)
+                .arguments(getProcessParameter())
+                .define();
+    }
+
     private RString getType() {
+        mapper = getMapper(IFutanwariaishoHakkoMapper.class);
+        FutanwariaishoHakkoProcessParameter processParam = getProcessParameter();
+        FutanwariaishoHakkoMybatisParameter param = processParam.toMybatisParameter();
+        param.setサブ業務コード(SubGyomuCode.DBC介護給付.getColumnValue());
+        param.set帳票分類ID(ReportIdDBC.DBC100065.getReportId().getColumnValue());
+        param.set管理年度(FlexibleYear.MIN);
         if (ZERO.equals(getParameter().get出力対象())) {
-            return new ChohyoSeigyoHanyo(SubGyomuCode.DBC介護給付, ReportIdDBC.DBC100065.getReportId(), 全件用紙タイプ, FlexibleYear.MIN).get設定値();
+            param.set項目名(全件用紙タイプ);
         } else {
-            return new ChohyoSeigyoHanyo(SubGyomuCode.DBC介護給付, ReportIdDBC.DBC100065.getReportId(), 異動認定分用紙タイプ, FlexibleYear.MIN).get設定値();
+            param.set項目名(異動認定分用紙タイプ);
         }
+        DbT7067ChohyoSeigyoHanyoEntity 帳票制御汎用キー = mapper.select帳票制御汎用キー(param);
+        return 帳票制御汎用キー.getKomokuValue();
     }
 
     private FutanwariaishoHakkoProcessParameter getProcessParameter() {
