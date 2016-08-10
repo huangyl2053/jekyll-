03118b9ab32728d52425d340391e0be5e4fe5842
wanghuafeng <wanghuafeng@liandisys.com.cn>
2016-08-10 17:14:56 +0800

    帳票設計_DBU300021_事業報告月報様式_03  　新規作成

---
+++ DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/report/jigyohokokugeppoyoshiki2120161/IJigyohokokuGeppoYoshiki2120161Editor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbu.business.report.jigyohokokugeppoyoshiki2120161;
+
+import jp.co.ndensan.reams.db.dbu.entity.report.jigyohokokugeppoyoshiki2120161.JigyohokokuGeppoYoshiki2120161ReportSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportEditor;
+
+/**
+ * 事業報告月報様式_03のEditorのインタ—フェ—スです。
+ *
+ * @reamsid_L DBU-5560-070 wanghuafeng
+ */
+public interface IJigyohokokuGeppoYoshiki2120161Editor extends IReportEditor<JigyohokokuGeppoYoshiki2120161ReportSource> {
+
+}
