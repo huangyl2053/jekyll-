cc7bf0e54f83941c8de9d9015daffada95cb2651
李利歓 <x_lilh@subcon.liandisys.com.cn>
2016-08-10 15:34:23 +0800

    バッチ設計_DBDBT81002_2_非課税年金対象者同定 ソース一時テーブルEntity項目作成

--- DB/DBD/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbd/entity/db/relate/dbd8100202/temptable/JissekiDataIchijiSakuseiTempTableEntity.java
+++ DB/DBD/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbd/entity/db/relate/dbd8100202/temptable/JissekiDataIchijiSakuseiTempTableEntity.java
@@ -5,7 +5,13 @@
  */
 package jp.co.ndensan.reams.db.dbd.entity.db.relate.dbd8100202.temptable;
 
+import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
+import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
+import jp.co.ndensan.reams.uz.uza.biz.YubinNo;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleYear;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.math.Decimal;
 import jp.co.ndensan.reams.uz.uza.util.db.DbTableEntityBase;
 import jp.co.ndensan.reams.uz.uza.util.db.IDbAccessable;
 import lombok.Getter;
@@ -22,53 +28,53 @@ import lombok.Setter;
 public class JissekiDataIchijiSakuseiTempTableEntity extends DbTableEntityBase<JissekiDataIchijiSakuseiTempTableEntity> implements IDbAccessable {
 
     public static final RString TABLE_NAME = new RString("JissekiDataIchijiSakuseiTempTable");
-    //TODO 
-//        被保険者番号
-//    宛名カナ氏名
-//    宛名生年月日
-//    宛名性別
-//    識別コード
-//    世帯コード
-//    宛名漢字氏名
-//    宛名郵便番号
-//    宛名漢字住所
-//    年度
-//    処理区分
-//    対象月
-//    基礎年金番号
-//    現基礎年金番号
-//    年金コード
-//    登録区分
-//    DTレコード区分
-//    DT市町村コード
-//    DT年金保険者コード
-//    DT通知内容コード
-//    DT予備1
-//    DT制度コード
-//    DT作成年月日
-//    DT基礎年金番号
-//    DT年金コード
-//    DT予備2
-//    DT生年月日
-//    DT性別
-//    DTカナ氏名
-//    DTシフトコード1
-//    DT漢字氏名
-//    DTシフトコード2
-//    DT郵便番号
-//    DTカナ住所
-//    DTシフトコード3
-//    DT漢字住所
-//    DTシフトコード4
-//    DT対象年
-//    DT訂正表示
-//    DT各種区分
-//    DT処理結果
-//    DT予備3
-//    DT予備4
-//    DT金額1
-//    DT金額予備1
-//    DT金額予備2
-//    DT予備5
-//    DT共済年金証書記号番号
+
+    private HihokenshaNo hihokenshaNo;
+    private RString atenaKanaShimei;
+    private FlexibleDate atenaSeinenngappi;
+    private RString atenaSeibetsu;
+    private ShikibetsuCode shikibetsuCode;
+    private RString shotaiCode;
+    private RString atenaKanjiShimei;
+    private RString atenaYubinNo;
+    private RString atenakanaJusyo;
+    private RString nendo;
+    private RString shoriKubunn;
+    private RString TaishoMonth;
+    private RString kisoNennkinnNo;
+    private RString genKisoNennkinnNo;
+    private RString nennkinnCode;
+    private RString tourokuKubunn;
+    private RString dtRekoDoKubunn;
+    private RString dtShichosonCode;
+    private RString dtNennkinnHokenshaCode;
+    private RString dtTsuuchiContentCode;
+    private RString dtYobi1;
+    private RString dtSeidoCode;
+    private FlexibleYear dtCreateYMD;
+    private RString dtKisoNennkinnNo;
+    private RString dtNennkinnCode;
+    private RString dtYobi2;
+    private FlexibleDate dtSeinenngappi;
+    private RString dtSeibetsu;
+    private RString dtKanaShimei;
+    private RString dtShifutoCode1;
+    private RString dtKanjiShimei;
+    private RString dtShifutoCode2;
+    private YubinNo dtYubinNo;
+    private RString dtKanajusyo;
+    private RString dtShifutoCode3;
+    private RString dtKanjijusyo;
+    private RString dtShifutoCode4;
+    private RString dtTaisyoYear;
+    private RString dtTeiseiHyouji;
+    private RString dtKakushuKubun;
+    private RString dtShoriResult;
+    private RString dtYobi3;
+    private RString dtYobi4;
+    private Decimal dtkinngaku1;
+    private Decimal dtKinngakuYobi1;
+    private Decimal dtKinngakuYobi2;
+    private RString dtYobi5;
+    private RString dtKyousaiNennkinnShoushoKigouNo;
 }
