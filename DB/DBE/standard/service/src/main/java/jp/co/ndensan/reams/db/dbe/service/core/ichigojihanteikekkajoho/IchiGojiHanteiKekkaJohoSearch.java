1e33eb3a5b05b1615438ee7cfbc7244824ace0ff
chenxiangyu <chenxiangyu@liandisys.com.cn>
2016-08-11 15:18:15 +0800

    TODO を追加します。

--- DB/DBE/standard/service/src/main/java/jp/co/ndensan/reams/db/dbe/service/core/ichigojihanteikekkajoho/IchiGojiHanteiKekkaJohoSearch.java
+++ DB/DBE/standard/service/src/main/java/jp/co/ndensan/reams/db/dbe/service/core/ichigojihanteikekkajoho/IchiGojiHanteiKekkaJohoSearch.java
@@ -72,15 +72,7 @@ public class IchiGojiHanteiKekkaJohoSearch {
             throw new ApplicationException(DbeErrorMessages._1_5次判定実行不可.getMessage());
         }
 
-//        IDLLExecutor executor = DLLExecutorFactory.createInstance(DLLTypes.NCC10_09);
-//        IIchijiHanteiConditions conditions = ;
-//        IIchijiHanteiResults results = executor.exec();
-        // TODO QA#93089 基盤で行う予定の組み込み関数は未完了です、呼び出しすることができません。
-        // List<IchiGojiHanteiKekkaJohoEntity> 基本調査項目 = mapper.get基本調査項目(parameter);
-        // List<IchiGojiHanteiKekkaJohoEntity> 主治医意見書項目 = mapper.get主治医意見書項目(parameter);
-        // RString 認定調査_障害自立度コード = mapper.get認定調査高齢者自立度(parameter).get認定調査_障害自立度コード();
-        // RString 認定調査_認知症自立度コード = mapper.get認定調査高齢者自立度(parameter).get認定調査_認知症自立度コード();
-        // RString 主治医意見書_認知症自立度 = mapper.get主治医意見書高齢者自立度(parameter).get主治医意見書_認知症自立度();
+        // TODO QA#93089 JavaでDLLを呼び出し方のご提供がありません。
         IchiGojiHanteiKekkaJohoEntity entity = new IchiGojiHanteiKekkaJohoEntity();
         entity.set厚労省IF識別コード(厚労省IF識別コード);
         entity.set主治医意見書_件数(mapper.get主治医意見書件数(parameter));
