8b516e1913bfd82572d6f1cfcfc2919354ca60b4
yuqingzhang <yuqingzhang@liandisys.com.cn>
2016-08-11 19:22:38 +0800

    bug対応

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC1171011/DBC1171011TransitionEventName.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC1171011/DBC1171011TransitionEventName.java
@@ -1,7 +1,7 @@
 package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC1171011;
 
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 
 /**
  * このコードはツールによって生成されました。</br>
@@ -9,7 +9,7 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
  * 不正な動作の原因になります。</br>
  * DBC1171011のイベント名定義クラスです</br>
  *
- * @reamsid_L DBC-2380-010 yuqingzhang
+ * @author 自動生成
  */
 public enum DBC1171011TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
