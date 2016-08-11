e74a0f0a325fdc3ec34d95f1feeb3337d9067802
wanghui <wanghui@liandisys.com.cn>
2016-08-11 11:04:03 +0800

    DBA-1410-012 ビジネス設計_DBAMN00000_住民異動連動資格登録_転入の新規作成する。

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/controller/commonchilddiv/kaigiatesakijushosettei/KaigoAtesakiJushoSettei.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/controller/commonchilddiv/kaigiatesakijushosettei/KaigoAtesakiJushoSettei.java
@@ -12,7 +12,7 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
 /**
  * 介護宛先住所設定クラスです。
  *
- * @reamsid_L　DBU-3990-030　wanghui
+ * @reamsid_L DBU-3990-030 wanghui
  */
 public class KaigoAtesakiJushoSettei {
 
