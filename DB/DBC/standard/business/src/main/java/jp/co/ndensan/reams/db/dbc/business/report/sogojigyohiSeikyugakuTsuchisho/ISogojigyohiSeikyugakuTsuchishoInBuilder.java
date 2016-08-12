53fa36cb5d8d4397e651bb317ee8280b368589b4
sunqingzhu <sunqingzhu@liandisys.com.cn>
2016-08-12 15:58:21 +0800

    バッチ設計_DBCMNF2002-153_総合事業費等請求額通知書情報取込 、帳票設計_DBC200087_総合事業費等請求額通知書source  commit

---
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/sogojigyohiSeikyugakuTsuchisho/ISogojigyohiSeikyugakuTsuchishoInBuilder.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.business.report.sogojigyohiSeikyugakuTsuchisho;
+
+import jp.co.ndensan.reams.db.dbc.entity.report.source.dbc120890.SogojigyohiSeikyugakuTsuchishoInSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportBuilder;
+
+/**
+ * 総合事業費等請求額通知書帳票IBuilder
+ *
+ * @reamsid_L DBC-2480-032 sunqingzhu
+ */
+public interface ISogojigyohiSeikyugakuTsuchishoInBuilder extends IReportBuilder<SogojigyohiSeikyugakuTsuchishoInSource> {
+
+}
---
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/sogojigyohiSeikyugakuTsuchisho/SogojigyohiSeikyugakuTsuchishoInHeaderEditor.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.business.report.sogojigyohiSeikyugakuTsuchisho;
+
+import jp.co.ndensan.reams.db.dbc.entity.csv.dbc120230.DbWT1511SeikyugakuTsuchishoTempEntity;
+import jp.co.ndensan.reams.db.dbc.entity.report.source.dbc120890.SogojigyohiSeikyugakuTsuchishoInSource;
+import jp.co.ndensan.reams.uz.uza.lang.EraType;
+import jp.co.ndensan.reams.uz.uza.lang.FillType;
+import jp.co.ndensan.reams.uz.uza.lang.FirstYear;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleYearMonth;
+import jp.co.ndensan.reams.uz.uza.lang.RDateTime;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.lang.Separator;
+import jp.co.ndensan.reams.uz.uza.ui.binding.propertyenum.DisplayTimeFormat;
+
+/**
+ * 総合事業費等請求額通知書帳票HeaderEditor
+ *
+ * @reamsid_L DBC-2480-032 sunqingzhu
+ */
+public class SogojigyohiSeikyugakuTsuchishoInHeaderEditor implements ISogojigyohiSeikyugakuTsuchishoInEditor {
+
+    private final DbWT1511SeikyugakuTsuchishoTempEntity 帳票出力対象データ;
+    private final RDateTime 作成日時;
+    private static final RString SAKUSEI = new RString("作成");
+    private static final RString NUM = new RString("99");
+    private final RString 総合計 = new RString("＊＊　総合計　＊＊");
+
+    /**
+     * コンストラクタです
+     *
+     * @param 帳票出力対象データ SogojigyohiSeikyugakuTsuchishoInEntity
+     * @param 作成日時 RDateTime
+     */
+    public SogojigyohiSeikyugakuTsuchishoInHeaderEditor(
+            DbWT1511SeikyugakuTsuchishoTempEntity 帳票出力対象データ, RDateTime 作成日時) {
+        this.帳票出力対象データ = 帳票出力対象データ;
+        this.作成日時 = 作成日時;
+    }
+
+    @Override
+    public SogojigyohiSeikyugakuTsuchishoInSource edit(
+            SogojigyohiSeikyugakuTsuchishoInSource source) {
+        RString 作成日 = 作成日時.getDate().wareki().eraType(EraType.KANJI)
+                .firstYear(FirstYear.GAN_NEN).separator(Separator.JAPANESE).fillType(FillType.BLANK).toDateString();
+        RString 作成時 = 作成日時.getTime()
+                .toFormattedTimeString(DisplayTimeFormat.HH時mm分ss秒).concat(RString.HALF_SPACE).concat(SAKUSEI);
+        source.printTimeStamp = 作成日.concat(RString.HALF_SPACE).concat(作成時);
+        source.shinsaYM = doパターン56(帳票出力対象データ.get審査年月());
+        source.hokenshaNo = 帳票出力対象データ.get保険者番号().value();
+        source.hokenshaName = 帳票出力対象データ.get保険者名();
+        if (帳票出力対象データ.get款コード() != NUM) {
+            source.kanName = 帳票出力対象データ.get款名();
+        } else {
+            source.kanName = 総合計;
+        }
+        if (帳票出力対象データ.get項コード() != NUM) {
+            source.kouName = 帳票出力対象データ.get項名();
+        } else {
+            source.kouName = RString.EMPTY;
+        }
+        source.kokuhorenName = 帳票出力対象データ.get国保連合会名();
+        source.shoKisaiHokenshaNo = 帳票出力対象データ.get証記載保険者番号().value();
+
+        return source;
+    }
+
+    private RString doパターン56(FlexibleYearMonth 年月) {
+        if (null == 年月) {
+            return RString.EMPTY;
+        }
+        return 年月.wareki().eraType(EraType.KANJI_RYAKU).firstYear(FirstYear.GAN_NEN)
+                .separator(Separator.JAPANESE).fillType(FillType.BLANK).toDateString();
+    }
+}
