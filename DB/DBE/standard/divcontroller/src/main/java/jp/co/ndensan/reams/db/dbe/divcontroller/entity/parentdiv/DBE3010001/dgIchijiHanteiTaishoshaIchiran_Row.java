6b17849d8ad59aa2f2bf04f362f94ebcb89f1df9
houtianpeng <houtianpeng@liandisys.com.cn>
2016-08-12 12:48:16 +0800

    画面設計_DBE3010001_一次判定処理

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE3010001/dgIchijiHanteiTaishoshaIchiran_Row.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE3010001/dgIchijiHanteiTaishoshaIchiran_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE3010001;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Wed Aug 10 16:04:12 CST 2016 
+ * Fri Aug 12 12:47:03 CST 2016 
  */
 
 
@@ -21,7 +21,7 @@ import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxFlexibleDate;
  */
 
 public class dgIchijiHanteiTaishoshaIchiran_Row extends DataRow {
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-07-28_11-34-20">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-08-09_21-40-56">
 
     private ButtonDialog btnSyokai;
     private ButtonDialog btnSentaku;
