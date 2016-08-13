85eb75c29e11a72df237a4344e1d2d9086c9f01d
b_liuyang2 <b_liuyang2@liandisys.com.cn>
2016-08-13 11:06:09 +0800

    一時の改正

--- DB/DBD/standard/business/src/main/java/jp/co/ndensan/reams/db/dbd/business/report/dbd100004/TainoHokenryoKojoTsuchishoEditor.java
+++ DB/DBD/standard/business/src/main/java/jp/co/ndensan/reams/db/dbd/business/report/dbd100004/TainoHokenryoKojoTsuchishoEditor.java
@@ -188,6 +188,7 @@ public class TainoHokenryoKojoTsuchishoEditor implements ITainoHokenryoKojoTsuch
             source.listKyufuhiNaiyo_3 = DecimalFormatter.toコンマ区切りRString(new Decimal(償還払集計情報.get支払金額()), 0);
             source.kyufugakuGokei = DecimalFormatter.toコンマ区切りRString(get給付額合計(), 0);
             source.shikyuGaku = DecimalFormatter.toコンマ区切りRString(get給付額合計().subtract(get保険料額合計()), 0);
+            source.hokenryoGokei = DecimalFormatter.toコンマ区切りRString(get保険料額合計(), 0);
         }
         if (null != this.帳票情報.getShiharaiHohoHenkoTainoList() && this.帳票情報.getShiharaiHohoHenkoTainoList().size() > index) {
             ShiharaiHohoHenkoTaino 支払方法変更滞納 = this.帳票情報.getShiharaiHohoHenkoTainoList().get(index);
