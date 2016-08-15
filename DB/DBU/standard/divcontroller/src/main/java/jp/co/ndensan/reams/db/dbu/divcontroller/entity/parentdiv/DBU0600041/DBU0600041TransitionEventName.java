868da7e2dcad6117cc969cd148865ffde7ba446d
劉振涛 <n8399@ndensan.co.jp>
2016-08-15 14:39:24 +0900

    新規作成

---
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/entity/parentdiv/DBU0600041/DBU0600041TransitionEventName.java
@@ -0,0 +1,35 @@
+package jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0600041;
+
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
+
+/**
+ * このコードはツールによって生成されました。</br>
+ * このファイルへの変更は、再生成時には損失するため</br>
+ * 不正な動作の原因になります。</br>
+ * DBU0600041のイベント名定義クラスです</br>
+ *
+ * @author 自動生成
+ */
+public enum DBU0600041TransitionEventName implements IContainerEvents {
+// <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
+
+    再検索("再検索");
+
+    private final RString name;
+
+    private DBU0600041TransitionEventName(String name) {
+        this.name = new RString(name);
+    }
+
+    /**
+     * getName
+     *
+     * @return 名前
+     */
+    @Override
+    public RString getName() {
+        return name;
+    }
+// </editor-fold>
+}
