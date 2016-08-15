d731383ff53bf6d549caedc8d651030733989f67
jiangzongyue <jiangzongyue@liandisys.com.cn>
2016-08-15 13:31:33 +0800

    画面設計_DBC4510011_サービス種類登録 修正

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC4510011/ServiceShuruiShousaiDiv.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC4510011/ServiceShuruiShousaiDiv.java
@@ -4,16 +4,20 @@ package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC4510011;
  * このファイルへの変更は、再生成時には損失するため
  * 不正な動作の原因になります。
  */
+
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.DropDownList;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+import jp.co.ndensan.reams.uz.uza.ui.binding.TextBox;
+import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxDate;
 
 /**
- * ServiceShuruiShousai のクラスファイル 
- * 
- * @author 自動生成
+ * ServiceShuruiShousai のクラスファイル
+ *
+ * @reamsid_L DBC-3350-010 jiangzongyue
  */
 public class ServiceShuruiShousaiDiv extends Panel {
+
     // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-07-28_11-34-20">
     /*
      * [ private の作成 ]
