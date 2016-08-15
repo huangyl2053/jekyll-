09f6eb5783fc13b40bbe94c6be010ac1356b26d5
wangkanglei <wangkanglei@liandisys.com.cn>
2016-08-15 21:06:51 +0800

    画面設計_DBCMNF2002_国保連情報受取データ取込　修正

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC0410015/TsuchishoJoho741.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC0410015/TsuchishoJoho741.java
@@ -5,10 +5,15 @@
  */
 package jp.co.ndensan.reams.db.dbc.divcontroller.controller.parentdiv.DBC0410015;
 
+import jp.co.ndensan.reams.db.dbc.definition.batchprm.kokuhorenkyoutsu.KokuhorenKyoutsuBatchParameter;
+import jp.co.ndensan.reams.db.dbc.definition.core.saishori.SaiShoriKubun;
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0410015.TsuchishoJoho741Div;
 import jp.co.ndensan.reams.db.dbc.divcontroller.viewbox.kaigokyufukokuhorenjohotorikomi.KokuhorenDataTorikomiViewStateClass;
 import jp.co.ndensan.reams.db.dbz.definition.core.viewstatename.ViewStateHolderName;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleYearMonth;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.ui.servlets.ViewStateHolder;
 
 /**
@@ -37,8 +42,18 @@ public class TsuchishoJoho741 {
      * @param div TsuchishoJoho741Div
      * @return ResponseData
      */
-    public ResponseData<TsuchishoJoho741Div> onClick_btnExcute(TsuchishoJoho741Div div) {
-        // TODO QA471 パラメータ作成ビジネスを呼び出して、バッチを起動する
-        return ResponseData.of(div).respond();
+    public ResponseData<KokuhorenKyoutsuBatchParameter> onClick_btnExcute(TsuchishoJoho741Div div) {
+        KokuhorenKyoutsuBatchParameter parameter = new KokuhorenKyoutsuBatchParameter();
+        RDate 処理年月 = div.getCcdKokurenJohoTorikomi().get処理年月();
+        Long 出力順ID = div.getCcdKokurenJohoTorikomi().get出力順ID();
+        RString 再処理区分 = div.getCcdKokurenJohoTorikomi().get再処理区分();
+        parameter.setShoriYM(new FlexibleYearMonth(処理年月.getYearMonth().toDateString()));
+        parameter.setShutsuryokujunId(new RString(出力順ID));
+        if (SaiShoriKubun.再処理.get名称().equals(再処理区分)) {
+            parameter.setSaishoriKubun(SaiShoriKubun.再処理);
+        } else if (SaiShoriKubun.空白.get名称().equals(再処理区分)) {
+            parameter.setSaishoriKubun(SaiShoriKubun.空白);
+        }
+        return ResponseData.of(parameter).respond();
     }
 }
