2851010b120004cbbb60c0792c384f7db9cbd300
xuxin <xuxin@liandisys.com.cn>
2016-08-15 11:49:33 +0800

    画面設計_DBC0150011_サービス提供票別表初版作成です

--- DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/core/jigosakuseimeisaitouroku/TeikyohyoBeppyoEntityResult.java
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/core/jigosakuseimeisaitouroku/TeikyohyoBeppyoEntityResult.java
@@ -7,10 +7,11 @@ package jp.co.ndensan.reams.db.dbc.business.core.jigosakuseimeisaitouroku;
 
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
 import jp.co.ndensan.reams.ua.uax.entity.db.basic.UaFt200FindShikibetsuTaishoEntity;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleYearMonth;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
- * 画面設計_DBC0150011_サービス提供票別表 Result
+ * 画面設計_DBC0150011_サービス提供票別表のResultです。
  *
  * @reamsid_L DBC-5100-010 xuxin
  */
@@ -19,7 +20,10 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
 @SuppressWarnings("PMD.UnusedPrivateField")
 public class TeikyohyoBeppyoEntityResult {
 
-    private HihokenshaNo 被保険者番号;
+    private FlexibleYearMonth 対象年月;
+    private int 履歴番号;
     private RString 総合事業区分;
     private UaFt200FindShikibetsuTaishoEntity 宛名;
+
+    private HihokenshaNo 被保険者番号;
 }
