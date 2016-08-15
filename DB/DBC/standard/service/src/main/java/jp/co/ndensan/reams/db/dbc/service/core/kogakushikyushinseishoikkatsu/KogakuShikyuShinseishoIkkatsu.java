e10fc6c3431ea35da9f535cc974ab9c01539b327
zhengshenlei <zhengshenlei@liandisys.com.cn>
2016-08-15 14:27:50 +0800

    DBC_4770_DBCMN43001_新総合事業・事業高額支給申請書一括発行画面　变更

--- DB/DBC/standard/service/src/main/java/jp/co/ndensan/reams/db/dbc/service/core/kogakushikyushinseishoikkatsu/KogakuShikyuShinseishoIkkatsu.java
+++ DB/DBC/standard/service/src/main/java/jp/co/ndensan/reams/db/dbc/service/core/kogakushikyushinseishoikkatsu/KogakuShikyuShinseishoIkkatsu.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbc.service.core.kogakuShikyuShinseishoIkkatsu;
+package jp.co.ndensan.reams.db.dbc.service.core.kogakushikyushinseishoikkatsu;
 
 import java.util.ArrayList;
 import java.util.Collections;
