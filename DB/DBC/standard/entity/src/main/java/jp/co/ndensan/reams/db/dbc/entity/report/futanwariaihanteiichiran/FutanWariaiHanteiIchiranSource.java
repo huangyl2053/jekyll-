7666610ac1c11a98ed80cf5797fa706ee64504cf
yuanzhenxia <yuanzhenxia@liandisys.com.cn>
2016-08-16 14:56:29 +0800

    年次利用者負担割合判定 初版作成

---
+++ DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/report/futanwariaihanteiichiran/FutanWariaiHanteiIchiranSource.java
@@ -0,0 +1,109 @@
+package jp.co.ndensan.reams.db.dbc.entity.report.futanwariaihanteiichiran;
+
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.report.IReportSource;
+import jp.co.ndensan.reams.uz.uza.report.source.ReportItem;
+
+/**
+ * 負担割合判定一覧表のソースです。
+ *
+ * @reamsid_L DBC-4980-040 yuanzhenxia
+ */
+public class FutanWariaiHanteiIchiranSource implements IReportSource {
+// <editor-fold defaultstate="collapsed" desc="Generated Code">
+
+    @ReportItem(name = "shori", length = 9, order = 1)
+    public RString shori;
+    @ReportItem(name = "nendo", length = 6, order = 2)
+    public RString nendo;
+    @ReportItem(name = "kijunYmd", length = 11, order = 3)
+    public RString kijunYmd;
+    @ReportItem(name = "cityCode", length = 6, order = 4)
+    public RString cityCode;
+    @ReportItem(name = "cityMei", length = 12, order = 5)
+    public RString cityMei;
+    @ReportItem(name = "sort2", length = 10, order = 6)
+    public RString sort2;
+    @ReportItem(name = "sort3", length = 10, order = 7)
+    public RString sort3;
+    @ReportItem(name = "sort4", length = 10, order = 8)
+    public RString sort4;
+    @ReportItem(name = "sort5", length = 20, order = 9)
+    public RString sort5;
+    @ReportItem(name = "sort1", length = 10, order = 10)
+    public RString sort1;
+    @ReportItem(name = "shoriYmd", length = 23, order = 11)
+    public RString shoriYmd;
+    @ReportItem(name = "page1", length = 20, order = 12)
+    public RString page1;
+    @ReportItem(name = "page4", length = 20, order = 13)
+    public RString page4;
+    @ReportItem(name = "page2", length = 20, order = 14)
+    public RString page2;
+    @ReportItem(name = "page3", length = 20, order = 15)
+    public RString page3;
+    @ReportItem(name = "page5", length = 20, order = 16)
+    public RString page5;
+    @ReportItem(name = "listList4_1", length = 6, order = 17)
+    public RString listList4_1;
+    @ReportItem(name = "listList1_1", length = 10, order = 18)
+    public RString listList1_1;
+    @ReportItem(name = "listList1_2", length = 6, order = 19)
+    public RString listList1_2;
+    @ReportItem(name = "listList1_3", length = 5, order = 20)
+    public RString listList1_3;
+    @ReportItem(name = "listList1_4", length = 4, order = 21)
+    public RString listList1_4;
+    @ReportItem(name = "listList1_5", length = 17, order = 22)
+    public RString listList1_5;
+    @ReportItem(name = "listList1_6", length = 2, order = 23)
+    public RString listList1_6;
+    @ReportItem(name = "listList1_7", length = 4, order = 24)
+    public RString listList1_7;
+    @ReportItem(name = "listList1_8", length = 17, order = 25)
+    public RString listList1_8;
+    @ReportItem(name = "listList1_9", length = 2, order = 26)
+    public RString listList1_9;
+    @ReportItem(name = "listList1_10", length = 7, order = 27)
+    public RString listList1_10;
+    @ReportItem(name = "listList2_1", length = 9, order = 28)
+    public RString listList2_1;
+    @ReportItem(name = "listList2_2", length = 9, order = 29)
+    public RString listList2_2;
+    @ReportItem(name = "listList2_3", length = 9, order = 30)
+    public RString listList2_3;
+    @ReportItem(name = "listList2_4", length = 17, order = 31)
+    public RString listList2_4;
+    @ReportItem(name = "listList2_5", length = 17, order = 32)
+    public RString listList2_5;
+    @ReportItem(name = "listList2_6", length = 9, order = 33)
+    public RString listList2_6;
+    @ReportItem(name = "listList2_7", length = 17, order = 34)
+    public RString listList2_7;
+    @ReportItem(name = "listList2_8", length = 17, order = 35)
+    public RString listList2_8;
+    @ReportItem(name = "listList2_9", length = 7, order = 36)
+    public RString listList2_9;
+    @ReportItem(name = "listList5_1", length = 20, order = 37)
+    public RString listList5_1;
+    @ReportItem(name = "listList3_1", length = 9, order = 38)
+    public RString listList3_1;
+    @ReportItem(name = "listList3_2", length = 9, order = 39)
+    public RString listList3_2;
+    @ReportItem(name = "listList3_3", length = 9, order = 40)
+    public RString listList3_3;
+    @ReportItem(name = "listList3_4", length = 15, order = 41)
+    public RString listList3_4;
+    @ReportItem(name = "listList3_5", length = 9, order = 42)
+    public RString listList3_5;
+    @ReportItem(name = "listList3_6", length = 20, order = 43)
+    public RString listList3_6;
+    @ReportItem(name = "listList3_7", length = 7, order = 44)
+    public RString listList3_7;
+// </editor-fold>
+// <editor-fold defaultstate="collapsed" desc="User Customize Area">
+    //追加コードは以下（「User Customize Area」内）に記述してください。
+    //帳票ソースデータクラスを再作成する場合は、「User Customize Area」内のソースコードは記述されません。
+    //再作成した後、当箇所に記述したコードを再作成された帳票ソースデータクラスの「User Customize Area」内にコピー＆ペーストする必要があります。
+// </editor-fold>
+}
