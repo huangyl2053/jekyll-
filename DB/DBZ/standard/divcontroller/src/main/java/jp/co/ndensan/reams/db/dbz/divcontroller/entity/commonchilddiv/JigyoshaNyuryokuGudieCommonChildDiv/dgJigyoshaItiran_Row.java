d27e67d67beeeb6d791656f0abd38e7625713d39
邢 政 <n8373@ndensan.co.jp>
2016-08-16 16:50:18 +0900

    実装課題 #94567　対応

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/JigyoshaNyuryokuGudieCommonChildDiv/dgJigyoshaItiran_Row.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/JigyoshaNyuryokuGudieCommonChildDiv/dgJigyoshaItiran_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.JigyoshaN
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Thu Aug 11 13:24:30 JST 2016 
+ * Tue Aug 16 16:45:00 JST 2016 
  */
 
 
