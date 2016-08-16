407302def2746302ae445cc75fca4e2121cd69a2
lijunjun <lijunjun@liandisys.com.cn>
2016-08-16 17:42:13 +0800

    画面設計_DBBGM51001_所得照会票一括発行(BUG対応)

--- DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB1140001/DBB1140001StateName.java
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB1140001/DBB1140001StateName.java
@@ -1,7 +1,7 @@
 package jp.co.ndensan.reams.db.dbb.divcontroller.entity.parentdiv.DBB1140001;
 
-import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
  * このコードはツールによって生成されました。</br>
@@ -9,7 +9,7 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
  * 不正な動作の原因になります。</br>
  * DBB1140001の状態名定義クラスです
  *
- * @author 自動生成
+ * @reamsid_L DBB-1720-010 lijunjun
  */
 public enum DBB1140001StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
