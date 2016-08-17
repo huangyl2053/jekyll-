e5dc8ef15aafe76fde3d1012623e78c0cb7bc0b4
huzongcheng <huzongcheng@liandisys.com.cn>
2016-08-17 13:56:49 +0800

    高額介護サービス費給付対象者登録

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0400011/KogakuKaigoKyufuhiTaishoshaTorokuBatchParameterDiv.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0400011/KogakuKaigoKyufuhiTaishoshaTorokuBatchParameterDiv.java
@@ -19,7 +19,7 @@ import jp.co.ndensan.reams.uz.uza.ui.binding.Space;
  */
 public class KogakuKaigoKyufuhiTaishoshaTorokuBatchParameterDiv extends PanelBatchParameter {
 
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-05-30_13-18-33">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-08-09_21-40-56">
     /*
      * [ private の作成 ]
      * クライアント側から取得した情報を元にを検索を行い
