63387c3568fcd10362c9bc553ddf21cc755f22d1
dingminghao <dingminghao@liandisys.com.cn>
2016-08-17 12:56:55 +0800

    画面設計_DBCMNF2002_国保連情報受取データ取込_[123]総合事業費資格照合表情報 修正

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0410044/DBC0410044StateName.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0410044/DBC0410044StateName.java
@@ -9,7 +9,7 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
  * 不正な動作の原因になります。</br>
  * DBC0410044の状態名定義クラスです
  *
- * @author 自動生成
+ * @reamsid_L DBC-2890-040 dingminghao
  */
 public enum DBC0410044StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
