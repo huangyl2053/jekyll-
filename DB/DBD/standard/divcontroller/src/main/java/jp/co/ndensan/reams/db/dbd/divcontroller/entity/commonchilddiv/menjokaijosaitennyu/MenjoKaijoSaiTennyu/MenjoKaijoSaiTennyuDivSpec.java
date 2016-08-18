cb2679b2b09e0d0d7e4442c72feed950f832b533
潘 暁波 <pangxb@liandisys.com.cn>
2016-08-18 15:48:11 +0800

    BUGID:14D203D-24266 対応

--- DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/commonchilddiv/menjokaijosaitennyu/MenjoKaijoSaiTennyu/MenjoKaijoSaiTennyuDivSpec.java
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/commonchilddiv/menjokaijosaitennyu/MenjoKaijoSaiTennyu/MenjoKaijoSaiTennyuDivSpec.java
@@ -16,20 +16,19 @@ import jp.co.ndensan.reams.uz.uza.core.validation.IPredicate;
 public enum MenjoKaijoSaiTennyuDivSpec implements IPredicate<MenjoKaijoSaiTennyuDiv> {
 
     適用期間チェック {
-        /**
-         * 適用期間のチェックです。
-         *
-         * @param div ShokanBaraiKa1GoDiv
-         * @return true:期間が正確、false:期間が不正
-         */
-        @Override
-        public boolean apply(MenjoKaijoSaiTennyuDiv div) {
-            if (!div.getTxtGengakuTekiyoKikanKaishiYMD().getValue().isEmpty() && !div.getTxtGengakuTekiyoKikanShuryoYMD().getValue().isEmpty()) {
-                return div.getTxtGengakuTekiyoKikanKaishiYMD().getValue().isBeforeOrEquals(div.getTxtGengakuTekiyoKikanShuryoYMD().getValue());
-            } else {
-                return !div.getTxtGengakuTekiyoKikanKaishiYMD().getValue().isEmpty() || !div.getTxtGengakuTekiyoKikanShuryoYMD().getValue().isEmpty();
+                /**
+                 * 適用期間のチェックです。
+                 *
+                 * @param div MenjoKaijoSaiTennyuDiv
+                 * @return true:期間が正確、false:期間が不正
+                 */
+                @Override
+                public boolean apply(MenjoKaijoSaiTennyuDiv div) {
+                    if (!div.getTxtKonkaiKikanKaishiYMD().getValue().isEmpty() && !div.getTxtKonkaiKikanShuryoYMD().getValue().isEmpty()) {
+                        return div.getTxtKonkaiKikanKaishiYMD().getValue().isBeforeOrEquals(div.getTxtKonkaiKikanShuryoYMD().getValue());
+                    } else {
+                        return !div.getTxtKonkaiKikanKaishiYMD().getValue().isEmpty() || !div.getTxtKonkaiKikanShuryoYMD().getValue().isEmpty();
+                    }
+                }
             }
-        }
-    }
-
 }
