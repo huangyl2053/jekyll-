b05c9c8ad5d58dc103482cba5efd75598a98d96d
sunhadi <sunhaidi@liandisys.com.cn>
2016-08-19 16:40:35 +0800

    主治医コード、主治医医療機関コードのTypeHandlerを作成します

--- DB/DBZ/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbz/entity/typehandlers/ShujiiIryokikanCodeTypeHandler.java
+++ DB/DBZ/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbz/entity/typehandlers/ShujiiIryokikanCodeTypeHandler.java
@@ -4,32 +4,37 @@ import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import jp.co.ndensan.reams.db.dbz.definition.core.valueobject.ninteishinsei.ChosainCode;
+import jp.co.ndensan.reams.db.dbz.definition.core.valueobject.ninteishinsei.ShujiiIryokikanCode;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 
-public class ChosainCodeTypeHandler extends BaseTypeHandler<ChosainCode> {
+/**
+ * 主治医医療機関コードのTypeHandlerです。
+ *
+ * @reamsid_L DBZ-9999-010 sunhaidi
+ */
+public class ShujiiIryokikanCodeTypeHandler extends BaseTypeHandler<ShujiiIryokikanCode> {
 // <editor-fold defaultstate="collapsed" desc="Created By POJO Tool ver 1.2">
 
     @Override
-    public void setNonNullParameter(PreparedStatement ps, int i, ChosainCode parameter, JdbcType jdbcType) throws SQLException {
+    public void setNonNullParameter(PreparedStatement ps, int i, ShujiiIryokikanCode parameter, JdbcType jdbcType) throws SQLException {
         ps.setString(i, parameter == null ? null : parameter.getColumnValue().toString());
     }
 
     @Override
-    public ChosainCode getNullableResult(ResultSet rs, String columnName) throws SQLException {
-        return new ChosainCode(getResultRString(rs.getString(columnName)));
+    public ShujiiIryokikanCode getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return new ShujiiIryokikanCode(getResultRString(rs.getString(columnName)));
     }
 
     @Override
-    public ChosainCode getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-        return new ChosainCode(getResultRString(rs.getString(columnIndex)));
+    public ShujiiIryokikanCode getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return new ShujiiIryokikanCode(getResultRString(rs.getString(columnIndex)));
     }
 
     @Override
-    public ChosainCode getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-        return new ChosainCode(getResultRString(cs.getString(columnIndex)));
+    public ShujiiIryokikanCode getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return new ShujiiIryokikanCode(getResultRString(cs.getString(columnIndex)));
     }
 
     private RString getResultRString(String result) {
