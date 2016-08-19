62df0633157f79158b31b7f8dd003a4063968a5c
lizhuoxuan <lizhuoxuan@liandisys.com.cn>
2016-08-19 17:43:55 +0800

    画面設計_DBC0060013_訪問通所サービスの給付管理照会 新規を作成

---
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0060013/ServiceDiv.java
@@ -0,0 +1,332 @@
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0060013;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+
+/**
+ * Service のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class ServiceDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-08-09_21-40-56">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("txt1")
+    private TextBox txt1;
+    @JsonProperty("txt2")
+    private TextBox txt2;
+    @JsonProperty("txt7")
+    private TextBox txt7;
+    @JsonProperty("txt4")
+    private TextBox txt4;
+    @JsonProperty("txt5")
+    private TextBox txt5;
+    @JsonProperty("txt8")
+    private TextBox txt8;
+    @JsonProperty("txt9")
+    private TextBoxDateRange txt9;
+    @JsonProperty("line1")
+    private HorizontalLine line1;
+    @JsonProperty("dgServive")
+    private DataGrid<dgServive_Row> dgServive;
+    @JsonProperty("txt11")
+    private TextBox txt11;
+    @JsonProperty("txt12")
+    private TextBox txt12;
+    @JsonProperty("txt13")
+    private TextBox txt13;
+    @JsonProperty("txt15")
+    private TextBox txt15;
+    @JsonProperty("txt17")
+    private TextBox txt17;
+    @JsonProperty("txt18")
+    private TextBox txt18;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * gettxt1
+     * @return txt1
+     */
+    @JsonProperty("txt1")
+    public TextBox getTxt1() {
+        return txt1;
+    }
+
+    /*
+     * settxt1
+     * @param txt1 txt1
+     */
+    @JsonProperty("txt1")
+    public void setTxt1(TextBox txt1) {
+        this.txt1 = txt1;
+    }
+
+    /*
+     * gettxt2
+     * @return txt2
+     */
+    @JsonProperty("txt2")
+    public TextBox getTxt2() {
+        return txt2;
+    }
+
+    /*
+     * settxt2
+     * @param txt2 txt2
+     */
+    @JsonProperty("txt2")
+    public void setTxt2(TextBox txt2) {
+        this.txt2 = txt2;
+    }
+
+    /*
+     * gettxt7
+     * @return txt7
+     */
+    @JsonProperty("txt7")
+    public TextBox getTxt7() {
+        return txt7;
+    }
+
+    /*
+     * settxt7
+     * @param txt7 txt7
+     */
+    @JsonProperty("txt7")
+    public void setTxt7(TextBox txt7) {
+        this.txt7 = txt7;
+    }
+
+    /*
+     * gettxt4
+     * @return txt4
+     */
+    @JsonProperty("txt4")
+    public TextBox getTxt4() {
+        return txt4;
+    }
+
+    /*
+     * settxt4
+     * @param txt4 txt4
+     */
+    @JsonProperty("txt4")
+    public void setTxt4(TextBox txt4) {
+        this.txt4 = txt4;
+    }
+
+    /*
+     * gettxt5
+     * @return txt5
+     */
+    @JsonProperty("txt5")
+    public TextBox getTxt5() {
+        return txt5;
+    }
+
+    /*
+     * settxt5
+     * @param txt5 txt5
+     */
+    @JsonProperty("txt5")
+    public void setTxt5(TextBox txt5) {
+        this.txt5 = txt5;
+    }
+
+    /*
+     * gettxt8
+     * @return txt8
+     */
+    @JsonProperty("txt8")
+    public TextBox getTxt8() {
+        return txt8;
+    }
+
+    /*
+     * settxt8
+     * @param txt8 txt8
+     */
+    @JsonProperty("txt8")
+    public void setTxt8(TextBox txt8) {
+        this.txt8 = txt8;
+    }
+
+    /*
+     * gettxt9
+     * @return txt9
+     */
+    @JsonProperty("txt9")
+    public TextBoxDateRange getTxt9() {
+        return txt9;
+    }
+
+    /*
+     * settxt9
+     * @param txt9 txt9
+     */
+    @JsonProperty("txt9")
+    public void setTxt9(TextBoxDateRange txt9) {
+        this.txt9 = txt9;
+    }
+
+    /*
+     * getline1
+     * @return line1
+     */
+    @JsonProperty("line1")
+    public HorizontalLine getLine1() {
+        return line1;
+    }
+
+    /*
+     * setline1
+     * @param line1 line1
+     */
+    @JsonProperty("line1")
+    public void setLine1(HorizontalLine line1) {
+        this.line1 = line1;
+    }
+
+    /*
+     * getdgServive
+     * @return dgServive
+     */
+    @JsonProperty("dgServive")
+    public DataGrid<dgServive_Row> getDgServive() {
+        return dgServive;
+    }
+
+    /*
+     * setdgServive
+     * @param dgServive dgServive
+     */
+    @JsonProperty("dgServive")
+    public void setDgServive(DataGrid<dgServive_Row> dgServive) {
+        this.dgServive = dgServive;
+    }
+
+    /*
+     * gettxt11
+     * @return txt11
+     */
+    @JsonProperty("txt11")
+    public TextBox getTxt11() {
+        return txt11;
+    }
+
+    /*
+     * settxt11
+     * @param txt11 txt11
+     */
+    @JsonProperty("txt11")
+    public void setTxt11(TextBox txt11) {
+        this.txt11 = txt11;
+    }
+
+    /*
+     * gettxt12
+     * @return txt12
+     */
+    @JsonProperty("txt12")
+    public TextBox getTxt12() {
+        return txt12;
+    }
+
+    /*
+     * settxt12
+     * @param txt12 txt12
+     */
+    @JsonProperty("txt12")
+    public void setTxt12(TextBox txt12) {
+        this.txt12 = txt12;
+    }
+
+    /*
+     * gettxt13
+     * @return txt13
+     */
+    @JsonProperty("txt13")
+    public TextBox getTxt13() {
+        return txt13;
+    }
+
+    /*
+     * settxt13
+     * @param txt13 txt13
+     */
+    @JsonProperty("txt13")
+    public void setTxt13(TextBox txt13) {
+        this.txt13 = txt13;
+    }
+
+    /*
+     * gettxt15
+     * @return txt15
+     */
+    @JsonProperty("txt15")
+    public TextBox getTxt15() {
+        return txt15;
+    }
+
+    /*
+     * settxt15
+     * @param txt15 txt15
+     */
+    @JsonProperty("txt15")
+    public void setTxt15(TextBox txt15) {
+        this.txt15 = txt15;
+    }
+
+    /*
+     * gettxt17
+     * @return txt17
+     */
+    @JsonProperty("txt17")
+    public TextBox getTxt17() {
+        return txt17;
+    }
+
+    /*
+     * settxt17
+     * @param txt17 txt17
+     */
+    @JsonProperty("txt17")
+    public void setTxt17(TextBox txt17) {
+        this.txt17 = txt17;
+    }
+
+    /*
+     * gettxt18
+     * @return txt18
+     */
+    @JsonProperty("txt18")
+    public TextBox getTxt18() {
+        return txt18;
+    }
+
+    /*
+     * settxt18
+     * @param txt18 txt18
+     */
+    @JsonProperty("txt18")
+    public void setTxt18(TextBox txt18) {
+        this.txt18 = txt18;
+    }
+
+    // </editor-fold>
+}
