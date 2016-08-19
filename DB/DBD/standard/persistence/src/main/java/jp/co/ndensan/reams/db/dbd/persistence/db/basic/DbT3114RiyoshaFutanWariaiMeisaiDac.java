3433b93071efee8f34009b2edb629eaa8abcc35a
lizhi <lizhi@liandisys.com.cn>
2016-08-19 16:39:47 +0800

    バッチ設計_DBBBT36001_仮算定異動賦課 bug対応  画面設計_DBCMNK2001_利用者負担割合即時更正_修正 修正

--- DB/DBD/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbd/persistence/db/basic/DbT3114RiyoshaFutanWariaiMeisaiDac.java
+++ DB/DBD/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbd/persistence/db/basic/DbT3114RiyoshaFutanWariaiMeisaiDac.java
@@ -122,4 +122,25 @@ public class DbT3114RiyoshaFutanWariaiMeisaiDac implements ISaveable<DbT3114Riyo
                                 eq(logicalDeletedFlag, false))).
                 toList(DbT3114RiyoshaFutanWariaiMeisaiEntity.class);
     }
+
+    /**
+     * 利用者負担割合明細を取得します。
+     *
+     * @param 年度 FlexibleYear
+     * @param 被保険者番号 HihokenshaNo
+     * @return List<DbT3114RiyoshaFutanWariaiMeisaiEntity>
+     */
+    @Transaction
+    public List<DbT3114RiyoshaFutanWariaiMeisaiEntity> select履歴番号BY年度と被保険者番号(
+            FlexibleYear 年度,
+            HihokenshaNo 被保険者番号) {
+        DbAccessorNormalType accessor = new DbAccessorNormalType(session);
+
+        return accessor.select().
+                table(DbT3114RiyoshaFutanWariaiMeisai.class).
+                where(and(
+                                eq(nendo, 年度),
+                                eq(hihokenshaNo, 被保険者番号))).
+                toList(DbT3114RiyoshaFutanWariaiMeisaiEntity.class);
+    }
 }
