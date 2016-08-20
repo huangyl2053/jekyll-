de8eff8bb7d094b5b28985ca4836aeea0386a432
wangchao <wangchao@liandisys.com.cn>
2016-08-20 09:28:12 +0800

    ビルドエラーについて臨時対応

---
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/core/sofujoho/SofujohoBusiness.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.business.core.sofujoho;
+
+import jp.co.ndensan.reams.db.dbc.entity.db.relate.sofujoho.SofujohoRelateEntity;
+
+/**
+ *
+ * @author wangchao
+ */
+public class SofujohoBusiness extends SofujohoRelateEntity {
+
+}
