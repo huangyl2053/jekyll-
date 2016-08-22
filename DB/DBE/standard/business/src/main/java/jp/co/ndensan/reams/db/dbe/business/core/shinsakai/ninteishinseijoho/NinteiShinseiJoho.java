648465613e71c36741d49910cab6aa7381f61458
sunhadi <sunhaidi@liandisys.com.cn>
2016-08-22 14:58:31 +0800

    checkStyle対応

--- DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/core/shinsakai/ninteishinseijoho/NinteiShinseiJoho.java
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/core/shinsakai/ninteishinseijoho/NinteiShinseiJoho.java
@@ -13,6 +13,8 @@ import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.JigyoshaNo;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShinseishoKanriNo;
 import jp.co.ndensan.reams.db.dbz.definition.core.valueobject.ninteishinsei.ChosaItakusakiCode;
 import jp.co.ndensan.reams.db.dbz.definition.core.valueobject.ninteishinsei.ChosainCode;
+import jp.co.ndensan.reams.db.dbz.definition.core.valueobject.ninteishinsei.ShujiiCode;
+import jp.co.ndensan.reams.db.dbz.definition.core.valueobject.ninteishinsei.ShujiiIryokikanCode;
 import jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT5101NinteiShinseiJohoEntity;
 import jp.co.ndensan.reams.ur.urz.definition.message.UrErrorMessages;
 import jp.co.ndensan.reams.ur.urz.definition.message.UrSystemErrorMessages;
@@ -468,7 +470,12 @@ public class NinteiShinseiJoho
      * @return 主治医医療機関コード
      */
     public RString get主治医医療機関コード() {
-        return entity.getShujiiIryokikanCode().value();
+        ShujiiIryokikanCode code = entity.getShujiiIryokikanCode();
+        if (code != null) {
+            return code.value();
+        } else {
+            return RString.EMPTY;
+        }
     }
 
     /**
@@ -477,7 +484,13 @@ public class NinteiShinseiJoho
      * @return 主治医コード
      */
     public RString get主治医コード() {
-        return entity.getShujiiCode().value();
+
+        ShujiiCode code = entity.getShujiiCode();
+        if (code != null) {
+            return code.value();
+        } else {
+            return RString.EMPTY;
+        }
     }
 
     /**
