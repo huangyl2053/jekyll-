747507e4d2a8e8d7189a7f4ff21553e9f3a14a89
jinjue <jinjue@liandisys.com.cn>
2016-08-22 16:08:13 +0900

    [DBC-2950-010]画面設計_DBC0070011_過誤申立送付情報照会 review対応 [DBC-2950-030]ビジネス設計_DBC0070011_過誤申立送付情報照会 review対応

--- DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/mybatisprm/sofujoho/SofujohoMybatisParameter.java
+++ DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/mybatisprm/sofujoho/SofujohoMybatisParameter.java
@@ -10,7 +10,7 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
 /**
  * 過誤申立送付情報照会のMybatisパラメータクラスです。
  *
- * @reamsid_L DBC-2950-010 jinjue
+ * @reamsid_L DBC-2950-030 jinjue
  */
 @SuppressWarnings("PMD.UnusedPrivateField")
 @lombok.Getter
@@ -21,6 +21,7 @@ public final class SofujohoMybatisParameter {
     private final RString 送付年月;
     private final RString 申立書区分コード;
     private final RString 市町村コード;
+    private final RString 証記載保険者番号;
 
     /**
      * コンストラクタです。
@@ -30,14 +31,16 @@ public final class SofujohoMybatisParameter {
      * @param 市町村コード 市町村コード
      * @param 送付年月フラグ 送付年月フラグ
      * @param 保険者番号フラグ 保険者番号フラグ
+     * @param 証記載保険者番号 証記載保険者番号
      */
     protected SofujohoMybatisParameter(RString 送付年月, RString 申立書区分コード, RString 市町村コード,
-            boolean 送付年月フラグ, boolean 保険者番号フラグ) {
+            boolean 送付年月フラグ, boolean 保険者番号フラグ, RString 証記載保険者番号) {
         this.送付年月 = 送付年月;
         this.申立書区分コード = 申立書区分コード;
         this.市町村コード = 市町村コード;
         this.送付年月フラグ = 送付年月フラグ;
         this.保険者番号フラグ = 保険者番号フラグ;
+        this.証記載保険者番号 = 証記載保険者番号;
     }
 
     /**
@@ -48,10 +51,11 @@ public final class SofujohoMybatisParameter {
      * @param 市町村コード 市町村コード
      * @param 送付年月フラグ 送付年月フラグ
      * @param 保険者番号フラグ 保険者番号フラグ
+     * @param 証記載保険者番号 証記載保険者番号
      * @return パラメータ
      */
     public static SofujohoMybatisParameter creatParameter(RString 送付年月, RString 申立書区分コード, RString 市町村コード,
-            boolean 送付年月フラグ, boolean 保険者番号フラグ) {
-        return new SofujohoMybatisParameter(送付年月, 申立書区分コード, 市町村コード, 送付年月フラグ, 保険者番号フラグ);
+            boolean 送付年月フラグ, boolean 保険者番号フラグ, RString 証記載保険者番号) {
+        return new SofujohoMybatisParameter(送付年月, 申立書区分コード, 市町村コード, 送付年月フラグ, 保険者番号フラグ, 証記載保険者番号);
     }
 }
