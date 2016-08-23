f7bec5bba68dd57b3e75390a52a5c60a34daa529
zuotao <zuotao@liandisys.com.cn>
2016-08-23 21:55:40 +0800

    画面設計_DBD5510001_職権修正／職権取消(一部)／認定結果入力(サ変・区変)  申請履歴情報関連修正

---
+++ DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/core/basic/DbT4121ShinseiRirekiJohoIdentifier.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbz.business.core.basic;
+
+import java.io.Serializable;
+import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShinseishoKanriNo;
+
+/**
+ * 申請履歴情報の識別子です。
+ *
+ * @reamsid_L DBZ-9999-011 zuotao
+ */
+@SuppressWarnings("PMD.UnusedPrivateField")
+@lombok.Value
+public class DbT4121ShinseiRirekiJohoIdentifier implements Serializable {
+
+    private final ShinseishoKanriNo 申請管理番号;
+
+    /**
+     * コンストラクタです。
+     *
+     * @param 申請管理番号 申請管理番号
+     */
+    public DbT4121ShinseiRirekiJohoIdentifier(ShinseishoKanriNo 申請管理番号) {
+        this.申請管理番号 = 申請管理番号;
+    }
+}
