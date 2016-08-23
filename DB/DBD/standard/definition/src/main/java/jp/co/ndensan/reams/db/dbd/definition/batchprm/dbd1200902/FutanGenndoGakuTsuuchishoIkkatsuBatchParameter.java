233bbf5b433231b92e11e74664d92a21f27ee24b
李利歓 <x_lilh@subcon.liandisys.com.cn>
2016-08-23 10:28:12 +0800

    社会福祉法人等利用者負担額軽減確認証・通知書一括発行 修正

--- DB/DBD/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbd/definition/batchprm/dbd1200902/FutanGenndoGakuTsuuchishoIkkatsuBatchParameter.java
+++ DB/DBD/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbd/definition/batchprm/dbd1200902/FutanGenndoGakuTsuuchishoIkkatsuBatchParameter.java
@@ -11,6 +11,7 @@ import jp.co.ndensan.reams.db.dbd.definition.batchprm.gemmen.chohyoikkatsu.Tanpy
 import jp.co.ndensan.reams.db.dbd.definition.processprm.dbd1200902.FutanGenndoGakuNinnteiListProcessParameter;
 import jp.co.ndensan.reams.db.dbd.definition.processprm.dbd1200902.FutanGenndoGakuNinnteiShouProcessParameter;
 import jp.co.ndensan.reams.db.dbd.definition.processprm.dbd1200902.FutanGenndoGakuNinnteiTsuuchishoProcessParameter;
+import jp.co.ndensan.reams.uz.uza.batch.BatchParameter;
 import jp.co.ndensan.reams.uz.uza.batch.flow.BatchParameterBase;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
@@ -27,18 +28,45 @@ import lombok.Setter;
 @SuppressWarnings("PMD.UnusedPrivateField")
 public class FutanGenndoGakuTsuuchishoIkkatsuBatchParameter extends BatchParameterBase {
 
+    private static final String TANPYOHAKKOKUBUN = "tanpyoHakkoKubun";
+    private static final String KYUSOCHISHAKUBUN = "kyusochishaKubun";
+    private static final String STARTYMD = "startYMD";
+    private static final String ENDYMD = "endYMD";
+    private static final String TAISHOKUBUN = "taishoKubun";
+    private static final String TAISHOFROM = "taishoFrom";
+    private static final String TAISHOTO = "taishoTo";
+    private static final String NINNTEIPUBLISHFLG = "ninnteiPublishFlg";
+    private static final String NINNTEICOMYMD = "ninnteiComYMD";
+    private static final String TSUUCHISHOPUBLISHFLG = "tsuuchishoPublishFlg";
+    private static final String TSUUCHISHOCOMYMD = "tsuuchishoComYMD";
+    private static final String TSUCHISHONO = "tsuchishoNo";
+    private static final String CHANGEPAGEID = "changePageID";
+
+    @BatchParameter(key = TANPYOHAKKOKUBUN, name = "単票発行区分")
     private TanpyoHakkoKubun 単票発行区分;
+    @BatchParameter(key = KYUSOCHISHAKUBUN, name = "旧措置者区分")
     private KyusochishaKubun 旧措置者区分;
+    @BatchParameter(key = STARTYMD, name = "年度開始日")
     private FlexibleDate 年度開始日;
+    @BatchParameter(key = ENDYMD, name = "年度終了日")
     private FlexibleDate 年度終了日;
+    @BatchParameter(key = TAISHOKUBUN, name = "対象区分")
     private TaishoKubun 対象区分;
+    @BatchParameter(key = TAISHOFROM, name = "対象日FROM")
     private FlexibleDate 対象日FROM;
+    @BatchParameter(key = TAISHOTO, name = "対象日TO")
     private FlexibleDate 対象日TO;
+    @BatchParameter(key = NINNTEIPUBLISHFLG, name = "認定証発行フラグ")
     private boolean 認定証発行フラグ;
+    @BatchParameter(key = NINNTEICOMYMD, name = "認定証の交付日")
     private FlexibleDate 認定証の交付日;
+    @BatchParameter(key = TSUUCHISHOPUBLISHFLG, name = "通知書発行フラグ")
     private boolean 通知書発行フラグ;
+    @BatchParameter(key = TSUUCHISHOCOMYMD, name = "通知書の交付日")
     private FlexibleDate 通知書の交付日;
+    @BatchParameter(key = TSUCHISHONO, name = "通知書の文書番号")
     private RString 通知書の文書番号;
+    @BatchParameter(key = CHANGEPAGEID, name = "改頁出力順ID")
     private Long 改頁出力順ID;
 
     /**
