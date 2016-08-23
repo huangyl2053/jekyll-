b36a93b61240668559abeb2a4b1014728063ad59
zhouchuanlin <zhouchuanlin@liandisys.com.cn>
2016-08-23 13:28:41 +0800

    画面設計_DBC_ShotokuJokyo_所得状況 とビジネス設計_DBC1000062_基準収入額適用申請登録_基準収入額データを新規作成する。

---
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/ShotokuJokyo/IShotokuJokyoDiv.java
@@ -0,0 +1,12 @@
+package jp.co.ndensan.reams.db.dbc.divcontroller.entity.commonchilddiv.ShotokuJokyo;
+
+import jp.co.ndensan.reams.uz.uza.ui.binding.ICommonChildDivBaseProperties;
+
+/**
+ * このコードはツールによって生成されました。
+ *
+ * @reamsid_L DBC-4620-030 zhouchuanlin
+ */
+public interface IShotokuJokyoDiv extends ICommonChildDivBaseProperties {
+
+}
