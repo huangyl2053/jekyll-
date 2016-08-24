5c104ae99f6d903d36ef7aa4ffb0836ab47b0491
lihang <lihang@liandisys.com.cn>
2016-08-24 13:38:40 +0800

    画面設計_DBC4210011_種類支給限度額登録

---
+++ DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/core/shikyugendogaku/KubunShikyuGendogakuYokaigoJotaiKubun.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.definition.core.shikyuGendogaku;
+
+import jp.co.ndensan.reams.ur.urz.definition.message.UrSystemErrorMessages;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+/**
+ * 区分支給限度額要介護状態区分を表す列挙型です。
+ *
+ * @reamsid_L DBC-3440-010 lihang
+ */
+public enum KubunShikyuGendogakuYokaigoJotaiKubun {
+
+    /**
+     * コード:1 名称:経過介護 略称:定義なし
+     */
+    経過介護("11", "経過介護"),
+    /**
+     * コード:1 名称:要支援1 略称:定義なし
+     */
+    要支援1("12", "要支援1"),
+    /**
+     * コード:1 名称:要支援2 略称:定義なし
+     */
+    要支援2("13", "要支援2"),
+    /**
+     * コード:1 名称:要介護1 略称:定義なし
+     */
+    要介護1("21", "要介護1"),
+    /**
+     * コード:1 名称:要介護2 略称:定義なし
+     */
+    要介護2("22", "要介護2"),
+    /**
+     * コード:1 名称:要介護3 略称:定義なし
+     */
+    要介護3("23", "要介護3"),
+    /**
+     * コード:1 名称:要介護4 略称:定義なし
+     */
+    要介護4("24", "要介護4"),
+    /**
+     * コード:2 名称:要介護5 略称:定義なし
+     */
+    要介護5("25", "要介護5");
+
+    private final RString code;
+    private final RString fullName;
+
+    private KubunShikyuGendogakuYokaigoJotaiKubun(String code, String fullname) {
+        this.code = new RString(code);
+        this.fullName = new RString(fullname);
+    }
+
+    /**
+     * 区分支給限度額要介護状態区分のコードを返します。
+     *
+     * @return 区分支給限度額要介護状態区分のコード
+     */
+    public RString getコード() {
+        return code;
+    }
+
+    /**
+     * 区分支給限度額要介護状態区分の名称を返します。
+     *
+     * @return 区分支給限度額要介護状態区分の名称
+     */
+    public RString get名称() {
+        return fullName;
+    }
+
+    /**
+     * 区分支給限度額要介護状態区分のコードと一致する内容を探します。
+     *
+     * @param code 区分支給限度額要介護状態区分のコード
+     * @return {@code code} に対応する区分支給限度額要介護状態区分
+     */
+    public static KubunShikyuGendogakuYokaigoJotaiKubun toValue(RString code) {
+
+        for (KubunShikyuGendogakuYokaigoJotaiKubun kaigoJotaiKubun : KubunShikyuGendogakuYokaigoJotaiKubun.values()) {
+            if (kaigoJotaiKubun.code.equals(code)) {
+                return kaigoJotaiKubun;
+            }
+        }
+        throw new IllegalArgumentException(UrSystemErrorMessages.変換不可.getReplacedMessage("区分支給限度額要介護状態区分のコード"));
+    }
+}
