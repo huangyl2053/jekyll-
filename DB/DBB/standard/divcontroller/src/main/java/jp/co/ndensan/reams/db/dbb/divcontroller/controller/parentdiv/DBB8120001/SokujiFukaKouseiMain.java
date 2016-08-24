b0f1b62b840012b9e8fcd0104f11e1a976e39e62
tianshuai <tianshuai@liandisys.com.cn>
2016-08-24 10:57:08 +0800

    画面設計_DBC0110011_居宅サービス計画作成依頼届出情報登録 BUG対応

--- DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/controller/parentdiv/DBB8120001/SokujiFukaKouseiMain.java
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/controller/parentdiv/DBB8120001/SokujiFukaKouseiMain.java
@@ -356,6 +356,9 @@ public class SokujiFukaKouseiMain {
     public ResponseData<SokujiFukaKouseiMainDiv> onClick_btnApplyGemmenGaku(SokujiFukaKouseiMainDiv div) {
         List<KoseiZengoFuka> 更正前後賦課のリスト = ViewStateHolder.get(ViewStateKeys.更正前後賦課のリスト, List.class);
         FlexibleYear 賦課年度 = ViewStateHolder.get(ViewStateKeys.賦課年度, FlexibleYear.class);
+        if (前年度の情報を表示する.equals(div.getBtnYokunendoHyoji().getText())) {
+            賦課年度 = 賦課年度.plusYear(INT_1);
+        }
         TsuchishoNo 通知書番号 = ViewStateHolder.get(ViewStateKeys.通知書番号, TsuchishoNo.class);
         HihokenshaNo 被保険者番号 = ViewStateHolder.get(ViewStateKeys.被保険者番号, HihokenshaNo.class);
         KoseiZengoChoshuHoho 更正前後徴収方法 = ViewStateHolder.get(ViewStateKeys.更正前後徴収方法, KoseiZengoChoshuHoho.class);
