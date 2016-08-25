264b982984bb01d728c9aa7ac2d95cc837d3cda5
zhangrui <rzhang@sinoi.com.cn>
2016-08-25 14:11:11 +0800

    バッチ設計_DBCMNF2002-112_給付管理票情報取込　source修正

--- DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/csv/kyufukanrihyoin/KyufukanrihyoIchiranTanitsuCSVEntity.java
+++ DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/csv/kyufukanrihyoin/KyufukanrihyoIchiranTanitsuCSVEntity.java
@@ -11,14 +11,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * 帳票のCSVファイル作成のヘッダエンティティです。
+ * 帳票のCSVファイル作成のヘッダエンティティです(単一)。
  *
  * @reamsid_L DBC-2450-030 zhangrui
  */
 @Getter
 @Setter
 @SuppressWarnings("PMD.UnusedPrivateField")
-public class KyufukanrihyoIchiranCSVEntity {
+public class KyufukanrihyoIchiranTanitsuCSVEntity {
 
     @CsvField(order = 10, name = "取込年月")
     private RString 取込年月;
@@ -68,8 +68,6 @@ public class KyufukanrihyoIchiranCSVEntity {
     private RString 合計単位;
     @CsvField(order = 240, name = "備考")
     private RString 備考;
-    @CsvField(order = 250, name = "証記載保険者番号")
-    private RString 証記載保険者番号;
     @CsvField(order = 260, name = "訪問通所件数")
     private RString 訪問通所件数;
     @CsvField(order = 270, name = "短期入所件数")
@@ -79,42 +77,4 @@ public class KyufukanrihyoIchiranCSVEntity {
     @CsvField(order = 290, name = "合計件数")
     private RString 合計件数;
 
-    /**
-     *
-     * 証記載保険者番号列を出力しない
-     *
-     * @return KyufukanrihyoIchiranTanitsuCSVEntity
-     */
-    public KyufukanrihyoIchiranTanitsuCSVEntity to単一() {
-        KyufukanrihyoIchiranTanitsuCSVEntity 単一 = new KyufukanrihyoIchiranTanitsuCSVEntity();
-        単一.set取込年月(this.取込年月);
-        単一.set作成日時(this.作成日時);
-        単一.set保険者番号(this.保険者番号);
-        単一.set保険者名(this.保険者名);
-        単一.setNo(this.no);
-        単一.set対象年月(this.対象年月);
-        単一.set支援事業者番号(this.支援事業者番号);
-        単一.set支援事業者名(this.支援事業者名);
-        単一.set計画作成区分(this.計画作成区分);
-        単一.set作成区分(this.作成区分);
-        単一.set作成区分名(this.作成区分名);
-        単一.set被保険者番号(this.被保険者番号);
-        単一.set被保険者氏名(this.被保険者氏名);
-        単一.set作成種別区分(this.作成種別区分);
-        単一.set作成種別区分名(this.作成種別区分名);
-        単一.set要介護状態区分(this.要介護状態区分);
-        単一.set要介護度(this.要介護度);
-        単一.set限度額管理期間_開始(this.限度額管理期間_開始);
-        単一.set限度額管理期間_終了(this.限度額管理期間_終了);
-        単一.set区分支給限度基準額(this.区分支給限度基準額);
-        単一.set指定サービス単位(this.指定サービス単位);
-        単一.set基準該当サービス単位(this.基準該当サービス単位);
-        単一.set合計単位(this.合計単位);
-        単一.set備考(this.備考);
-        単一.set訪問通所件数(this.訪問通所件数);
-        単一.set短期入所件数(this.短期入所件数);
-        単一.set居宅件数(this.居宅件数);
-        単一.set合計件数(this.合計件数);
-        return 単一;
-    }
 }
