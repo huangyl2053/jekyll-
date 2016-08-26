11603bcac34ca788c7bb3748cc941446e49cc0af
王　萌 <n8371@ndensan.co.jp>
2016-08-26 15:33:26 +0900

    実装課題 #97662対応

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/IryohokenRirekiCommonChildDiv/dgIryohokenIchiran_Row.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/IryohokenRirekiCommonChildDiv/dgIryohokenIchiran_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.Iryohoken
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Tue Aug 09 09:44:03 JST 2016 
+ * Fri Aug 26 14:42:11 JST 2016 
  */
 
 
