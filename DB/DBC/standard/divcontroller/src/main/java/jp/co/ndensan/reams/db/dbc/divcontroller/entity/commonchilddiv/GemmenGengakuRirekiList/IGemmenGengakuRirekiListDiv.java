8a73ac90d430d2a0b5b86ebc77fc9f7544ad5fa3
liuxiaoyu <liuxiaoyu@liandisys.com.cn>
2016-08-29 11:46:09 +0800

    画面設計_DBC-4350-030_減免・減額履歴情報(新規 )

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/GemmenGengakuRirekiList/IGemmenGengakuRirekiListDiv.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/GemmenGengakuRirekiList/IGemmenGengakuRirekiListDiv.java
@@ -4,6 +4,8 @@ import jp.co.ndensan.reams.uz.uza.ui.binding.ICommonChildDivBaseProperties;
 
 /*
  * このコードはツールによって生成されました。
+ *
+ * @reamsid_L DBC-4350-030 liuxiaoyu
  */
 public interface IGemmenGengakuRirekiListDiv extends ICommonChildDivBaseProperties {
 
