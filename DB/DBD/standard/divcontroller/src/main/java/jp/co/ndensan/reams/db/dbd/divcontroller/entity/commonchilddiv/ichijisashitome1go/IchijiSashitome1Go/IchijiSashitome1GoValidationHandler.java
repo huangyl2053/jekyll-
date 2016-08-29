992db38cbe9fdbb94a8cca740600cc421ddbf9f8
潘 暁波 <pangxb@liandisys.com.cn>
2016-08-29 13:23:22 +0800

    14D203D-25781 BUG対応

--- DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/commonchilddiv/ichijisashitome1go/IchijiSashitome1Go/IchijiSashitome1GoValidationHandler.java
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/commonchilddiv/ichijisashitome1go/IchijiSashitome1Go/IchijiSashitome1GoValidationHandler.java
@@ -94,7 +94,8 @@ public class IchijiSashitome1GoValidationHandler {
         if (div.getKey_Button().equals(ShoriKubun.給付一時差止登録.getコード())) {
             for (ShiharaiHohoHenkoSashitome shiharaiHohoHenkoSashitome : shiharaiHohoHenko.getShiharaiHohoHenkoSashitomeList()) {
                 if (shiharaiHohoHenkoSashitome.get情報分類区分().equals(ShiharaiHenkoJohoBunruiKubun.差止情報.getコード())
-                        && shiharaiHohoHenkoSashitome.get差止解除年月日() == null
+                        && (shiharaiHohoHenkoSashitome.get差止解除年月日() == null
+                        || shiharaiHohoHenkoSashitome.get差止解除年月日().isEmpty())
                         && !shiharaiHohoHenkoSashitome.is論理削除フラグ()) {
                     pairs.add(new ValidationMessageControlPair(IchijiSashitome1GoMessages.給付一時差止未解除データチェック));
                 }
