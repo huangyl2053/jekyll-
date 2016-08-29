c93f1db068f0473138dd7964ea9676036b44e21c
dangjingjing <dangjingjing@liandisys.com.cn>
2016-08-29 13:50:30 +0800

    帳票設計_DBU300001_介護事業状況報告月報・一般状況（様式1） 修正

--- DB/DBU/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbu/entity/db/relate/jigyohokokugeppoyoshiki1/JigyohokokuGeppoYoshiki1Data.java
+++ DB/DBU/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbu/entity/db/relate/jigyohokokugeppoyoshiki1/JigyohokokuGeppoYoshiki1Data.java
@@ -22,6 +22,11 @@ public class JigyohokokuGeppoYoshiki1Data {
     private RString 集計区分;
     private RString 作成日時;
     private RString 集計範囲;
+    private RString 年報月報区分;
+    private RString 集計期間FROM;
+    private RString 集計期間TO;
+    private RString 集計年月;
+    private RString 集計年度;
     private RString 保険者番号;
     private RString 保険者名;
     private RString 第1号被保険者数_項目標題列1;
@@ -110,5 +115,4 @@ public class JigyohokokuGeppoYoshiki1Data {
     private RString 左記以外_3;
     private RString 左記以外_4;
     private RString 左記以外_5;
-
 }
