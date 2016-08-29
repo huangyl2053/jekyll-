c2d17cc72c0efc84cb6b2b76c97a1aab1b4e4902
chenyadong <chenyadong@liandisys.com.cn>
2016-08-29 14:00:10 +0800

    画面設計_DBCMN62002_高額合算自己負担額情報補正（一括）

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC1141011/JikoFutangakuJohoHosei.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC1141011/JikoFutangakuJohoHosei.java
@@ -5,13 +5,17 @@
  */
 package jp.co.ndensan.reams.db.dbc.divcontroller.controller.parentdiv.DBC1141011;
 
+import jp.co.ndensan.reams.db.dbc.business.core.basic.KokuhorenInterfaceKanri;
 import jp.co.ndensan.reams.db.dbc.definition.batchprm.dbc040030.DBC040030_KogakugassanJikofutangakuInfoHoseiParameter;
+import jp.co.ndensan.reams.db.dbc.definition.message.DbcErrorMessages;
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC1141011.DBC1141011StateName;
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC1141011.JikoFutangakuJohoHoseiDiv;
 import jp.co.ndensan.reams.db.dbc.divcontroller.handler.parentdiv.DBC1141011.JikoFutangakuJohoHoseiHandler;
 import jp.co.ndensan.reams.db.dbc.divcontroller.handler.parentdiv.DBC1141011.JikoFutangakuJohoHoseiValidationHandler;
+import jp.co.ndensan.reams.db.dbc.service.core.basic.KokuhorenInterfaceKanriManager;
 import jp.co.ndensan.reams.db.dbx.definition.core.configkeys.ConfigNameDBC;
 import jp.co.ndensan.reams.db.dbx.definition.core.dbbusinessconfig.DbBusinessConfig;
+import jp.co.ndensan.reams.db.dbx.definition.core.viewstate.ViewStateKeys;
 import jp.co.ndensan.reams.ur.urz.business.core.association.Association;
 import jp.co.ndensan.reams.ur.urz.service.core.association.AssociationFinderFactory;
 import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
@@ -19,10 +23,13 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
 import jp.co.ndensan.reams.uz.uza.exclusion.LockingKey;
 import jp.co.ndensan.reams.uz.uza.exclusion.PessimisticLockingException;
 import jp.co.ndensan.reams.uz.uza.exclusion.RealInitialLocker;
+import jp.co.ndensan.reams.uz.uza.lang.ApplicationException;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleYearMonth;
 import jp.co.ndensan.reams.uz.uza.lang.RDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.ui.servlets.ResponseHolder;
 import jp.co.ndensan.reams.uz.uza.ui.servlets.ValidationMessageControlPairs;
+import jp.co.ndensan.reams.uz.uza.ui.servlets.ViewStateHolder;
 
 /**
  * 画面設計_DBCMN62002_高額合算自己負担額情報補正（一括）のクラスです。
@@ -30,6 +37,12 @@ import jp.co.ndensan.reams.uz.uza.ui.servlets.ValidationMessageControlPairs;
  * @reamsid_L DBC-2320-010 chenyadong
  */
 public class JikoFutangakuJohoHosei {
+
+    private static final RString 送付取込区分 = new RString("2");
+    private static final RString 処理状態区分 = new RString("3");
+    private static final RString 交換情報識別番号 = new RString("37J");
+    private static final RString MAX = new RString("MAX");
+    private static final RString MIN = new RString("MIN");
 
     /**
      * 画面初期化のメソッドます。
@@ -43,6 +56,21 @@ public class JikoFutangakuJohoHosei {
         LockingKey 前排他キー = new LockingKey(メニューID);
         if (!RealInitialLocker.tryGetLock(前排他キー)) {
             throw new PessimisticLockingException();
+        }
+        KokuhorenInterfaceKanri 国保連インターフェース管理Max;
+        KokuhorenInterfaceKanri 国保連インターフェース管理Min;
+        KokuhorenInterfaceKanriManager 国保連インターフェース管理Manager = new KokuhorenInterfaceKanriManager();
+        国保連インターフェース管理Max = 国保連インターフェース管理Manager.get新国保連インターフェース管理(
+                MAX, 送付取込区分, 処理状態区分, 交換情報識別番号);
+        国保連インターフェース管理Min = 国保連インターフェース管理Manager.get新国保連インターフェース管理(
+                MIN, 送付取込区分, 処理状態区分, 交換情報識別番号);
+        if (国保連インターフェース管理Max != null) {
+            FlexibleYearMonth 最新の処理年月 = 国保連インターフェース管理Max.get処理年月();
+            FlexibleYearMonth 最古の処理年月 = 国保連インターフェース管理Min.get処理年月();
+            ViewStateHolder.put(ViewStateKeys.最新の処理年月, 最新の処理年月);
+            ViewStateHolder.put(ViewStateKeys.最古の処理年月, 最古の処理年月);
+        } else {
+            throw new ApplicationException(DbcErrorMessages.自己負担額確認情報取込前.getMessage());
         }
         getHandler(div).initialize();
         return ResponseData.of(div).setState(DBC1141011StateName.自己負担額情報補正一括);
--- DB/DBX/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbx/definition/core/viewstate/ViewStateKeys.java
+++ DB/DBX/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbx/definition/core/viewstate/ViewStateKeys.java
@@ -13,6 +13,14 @@ package jp.co.ndensan.reams.db.dbx.definition.core.viewstate;
 public enum ViewStateKeys {
 
     /**
+     * 最新の処理年月
+     */
+    最新の処理年月,
+    /**
+     * 最古の処理年月
+     */
+    最古の処理年月,
+    /**
      * イメージ情報_存在
      */
     イメージ情報_存在,
