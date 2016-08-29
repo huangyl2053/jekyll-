b1123578264b9c800f3641d2227220f783134d06
chenhui <chenhui@liandisys.com.cn>
2016-08-29 20:57:42 +0800

    画面設計_DBBGM91003_納付額データ作成 新規作成

---
+++ DB/DBB/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbb/persistence/db/mapper/relate/noufugakudatasakuseishoridate/INoufuGakuDataSakuseiShoriDateMapper.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbb.persistence.db.mapper.relate.noufugakudatasakuseishoridate;
+
+import java.util.List;
+import jp.co.ndensan.reams.db.dbb.definition.mybatisprm.noufugakudatasakuseishoridate.NoufuGakuDataSakuseiShoriDateParameter;
+import jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT7022ShoriDateKanriEntity;
+
+/**
+ * 前回の処理日付取得クラスです。
+ *
+ * @reamsid_L DBB-1890-010 chenhui
+ */
+public interface INoufuGakuDataSakuseiShoriDateMapper {
+
+    /**
+     * 前回の処理日付取得を取得する。
+     *
+     * @param parameter parameter
+     * @return DealKyufujissekiEntity
+     */
+    List<DbT7022ShoriDateKanriEntity> get前回の処理日付取得(NoufuGakuDataSakuseiShoriDateParameter parameter);
+}
