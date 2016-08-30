8a060e73d6af6582cb8158dec38f7b585dc9b58f
wangxingpeng <wangxingpeng@liandisys.com.cn>
2016-08-30 15:36:27 +0800

    画面設計_DBCMNF1001_保険者情報送付データ作成_[37K]高額合算補正済自己負担額情報(ビルドエラーの修正)

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0200013/DBC0200013StateName.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0200013/DBC0200013StateName.java
@@ -1,7 +1,7 @@
 package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0200013;
 
-import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
  * このコードはツールによって生成されました。</br>
@@ -9,7 +9,7 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
  * 不正な動作の原因になります。</br>
  * DBC0200013の状態名定義クラスです
  *
- * @author 自動生成
+ * DBC-3300-050 wangxingpeng
  */
 public enum DBC0200013StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
