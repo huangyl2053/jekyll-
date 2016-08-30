e7951eb11a9dc52a2678bb44f0c146cedd77b254
wangxingpeng <wangxingpeng@liandisys.com.cn>
2016-08-30 15:04:17 +0800

    画面設計_DBCMNF1001_保険者情報送付データ作成_[211]償還連絡票情報の新規作成

---
+++ DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/batchprm/DBC110050/DBC110050_ShokanRenrakuhyoOutParameter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.definition.batchprm.DBC110050;
+
+import jp.co.ndensan.reams.uz.uza.batch.BatchParameter;
+import jp.co.ndensan.reams.uz.uza.batch.flow.BatchParameterBase;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.lang.RYearMonth;
+
+/**
+ * 償還連絡票情報作成のParameter
+ *
+ * @reamsid_L DBC-3300-090 wangxingpeng
+ */
+@lombok.Getter
+@lombok.Setter
+@SuppressWarnings("PMD.UnusedPrivateField")
+public class DBC110050_ShokanRenrakuhyoOutParameter extends BatchParameterBase {
+
+    private static final String KEY_処理年月 = "処理年月";
+    private static final String KEY_再処理区分 = "再処理区分";
+    private static final String KEY_出力順ID = "出力順ID";
+    @BatchParameter(key = KEY_処理年月, name = "処理年月")
+    private RYearMonth 処理年月;
+    @BatchParameter(key = KEY_再処理区分, name = "再処理区分")
+    private RString 再処理区分;
+    @BatchParameter(key = KEY_出力順ID, name = "出力順ID")
+    private RString 出力順ID;
+}
