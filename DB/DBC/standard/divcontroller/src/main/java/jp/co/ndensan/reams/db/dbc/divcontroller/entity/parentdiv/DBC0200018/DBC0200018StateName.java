db09a04e1661a3889334caa07a70e8a9e93a4554
wangxingpeng <wangxingpeng@liandisys.com.cn>
2016-08-30 15:40:06 +0800

    画面設計_DBCMNF1001_保険者情報送付データ作成_[341]高額介護サービス費給付判定結果情報(ビルドエラーの修正)

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0200018/DBC0200018StateName.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0200018/DBC0200018StateName.java
@@ -1,7 +1,7 @@
 package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0200018;
 
-import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
  * このコードはツールによって生成されました。</br>
@@ -9,7 +9,7 @@ import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
  * 不正な動作の原因になります。</br>
  * DBC0200018の状態名定義クラスです
  *
- * @author 自動生成
+ * DBC-3300-100 wangxingpeng
  */
 public enum DBC0200018StateName implements IStateEnumerations {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
