f5930557034a5fe25ea0946cb273fe275d51835e
wangxingpeng <wangxingpeng@liandisys.com.cn>
2016-08-30 19:45:22 +0800

    画面設計_DBCMNF1001_保険者情報送付データ作成_共有子DIV(ビルドエラーの修正)

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/KokuhorenJohoSofu/KokuhorenJohoSofu/IKokuhorenJohoSofuDiv.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/KokuhorenJohoSofu/KokuhorenJohoSofu/IKokuhorenJohoSofuDiv.java
@@ -1,5 +1,6 @@
 package jp.co.ndensan.reams.db.dbc.divcontroller.entity.commonchilddiv.KokuhorenJohoSofu.KokuhorenJohoSofu;
 
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.lang.RYearMonth;
 import jp.co.ndensan.reams.uz.uza.ui.binding.ICommonChildDivBaseProperties;
@@ -33,9 +34,30 @@ public interface IKokuhorenJohoSofuDiv extends ICommonChildDivBaseProperties {
     public void initialize(RYearMonth 処理年月, RString 再処理区分, RString 外部ＣＳＶファイル名, RString サイクル区分, RString 状態パターン);
 
     /**
-     * get送付対象情報のメソッドます。
+     * get送付対象情報のkeyのメソッドます。
      *
      * @return RString
      */
-    public RString get送付対象情報();
+    public RString get送付対象情報のkey();
+
+    /**
+     * get送付対象情報のValueメソッドます。
+     *
+     * @return RString
+     */
+    public RString get送付対象情報のValue();
+
+    /**
+     * get処理年月のValueのメソッドます。
+     *
+     * @return RDate
+     */
+    public RDate get処理年月のValue();
+
+    /**
+     * get再処理区分のValueのメソッドます。
+     *
+     * @return RString
+     */
+    public RString get再処理区分のValue();
 }
