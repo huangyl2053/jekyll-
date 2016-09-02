5adc4d49d06b93953cd4e4a313964cfd02f73bde
yebangqiang <yebangqiang@liandisys.com.cn>
2016-09-02 14:15:16 +0800

    帳票設計_DBC100062_新総合事業・事業高額決定通知書（単）(支払予定日あり) 帳票設計_DBC100061_新総合事業・事業高額決定通知書（単）(新規作成)

---
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/jigyokogakuketteitsuchishoyoteibiyijiari/IJigyoKogakuKetteiTsuchishoYoteiBiYijiAriBuilder.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.business.report.jigyokogakuketteitsuchishoyoteibiyijiari;
+
+import jp.co.ndensan.reams.db.dbc.entity.report.jigyokogakuketteitsuchishoyoteibiyijiari.JigyoKogakuKetteiTsuchishoYoteiBiYijiAriSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportBuilder;
+
+/**
+ * 新総合事業・事業高額決定通知書（単）(支払予定日あり)帳票Builder
+ *
+ * @reamsid_L DBC-4760-040 yebangqiang
+ */
+public interface IJigyoKogakuKetteiTsuchishoYoteiBiYijiAriBuilder
+        extends IReportBuilder<JigyoKogakuKetteiTsuchishoYoteiBiYijiAriSource> {
+}
