bfc558cb6449517598a68c4252fb71c1fcd58e07
qinzhen <qinzhen@liandisys.com.cn>
2016-09-03 15:31:55 +0800

    DBC200039

--- DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/reportid/ReportIdDBC.java
+++ DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/reportid/ReportIdDBC.java
@@ -516,7 +516,11 @@ public enum ReportIdDBC {
     /**
      * 帳票設計_DBC200044_給付費通知発行一覧表ＩＤです。
      */
-    DBC200044(new ReportId("DBC200044_KyufuhiTuchiHakkoIchiran"), new RString("給付費通知発行一覧表"));
+    DBC200044(new ReportId("DBC200044_KyufuhiTuchiHakkoIchiran"), new RString("給付費通知発行一覧表")),
+    /**
+     * 帳票設計_DBC200039_高額合算支給不支給決定通知書情報取込一覧表 ＩＤです。
+     */
+    DBC200039(new ReportId("DBC200039_GassanShikyuFushikyuKetteiTsuchishoTorikomiIchiran"), new RString("高額合算支給不支給決定通知書情報取込一覧表 "));
 
     private final ReportId reportId;
 
