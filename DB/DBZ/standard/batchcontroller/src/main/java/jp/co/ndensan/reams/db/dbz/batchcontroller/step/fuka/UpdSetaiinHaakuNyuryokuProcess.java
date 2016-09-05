ecb60b2e99384aa6f7f0e3436dd883fd9304a5c8
chenaoqi <chenaoqi>
2016-09-05 10:34:37 +0800

    LOOPBATCH改造 DBB-0640-010バッチ設計_DBBBT00001_世帯員把握（バッチ）

--- DB/DBZ/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/batchcontroller/step/fuka/UpdSetaiinHaakuNyuryokuProcess.java
+++ DB/DBZ/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/batchcontroller/step/fuka/UpdSetaiinHaakuNyuryokuProcess.java
@@ -8,44 +8,39 @@ package jp.co.ndensan.reams.db.dbz.batchcontroller.step.fuka;
 import java.util.List;
 import jp.co.ndensan.reams.db.dbx.definition.core.configkeys.ConfigNameDBB;
 import jp.co.ndensan.reams.db.dbx.definition.core.dbbusinessconfig.DbBusinessConfig;
-import jp.co.ndensan.reams.db.dbz.definition.processprm.fuka.SetaiShotokuKazeiHanteiProcessParameter;
 import jp.co.ndensan.reams.db.dbz.entity.db.relate.fuka.SetaiHakuEntity;
 import jp.co.ndensan.reams.db.dbz.persistence.db.mapper.relate.fuka.ISetaiShotokuKazeiHanteiMapper;
-import jp.co.ndensan.reams.db.dbz.service.core.fuka.SetaiShotokuKazeiHantei;
 import jp.co.ndensan.reams.uz.uza.batch.process.SimpleBatchProcessBase;
 import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleYear;
 import jp.co.ndensan.reams.uz.uza.lang.RDate;
 
 /**
- * 世帯員把握（バッチ）クラスです。
+ * update世帯員把握入力です。
  *
  * @reamsid_L DBB-0640-010 chenaoqi
  */
-public class SetaiShotokuKazeiHanteiUpdateProcess extends SimpleBatchProcessBase {
+public class UpdSetaiinHaakuNyuryokuProcess extends SimpleBatchProcessBase {
 
-    private SetaiShotokuKazeiHanteiProcessParameter processParameter;
     private ISetaiShotokuKazeiHanteiMapper mapper;
+    private FlexibleYear 所得年度;
 
     @Override
     protected void beforeExecute() {
         super.beforeExecute();
         mapper = getMapper(ISetaiShotokuKazeiHanteiMapper.class);
-        mapper.createTmpSetaiShotoku();
-        List<SetaiHakuEntity> list = mapper.selectNotNull();
-        if (list == null || list.isEmpty()) {
-            SetaiHakuEntity entity = new SetaiHakuEntity();
-            FlexibleYear 所得年度 = new FlexibleYear(DbBusinessConfig.get(ConfigNameDBB.日付関連_調定年度,
-                    RDate.getNowDate(), SubGyomuCode.DBB介護賦課));
-            entity.setShotokuNendo(所得年度);
-            mapper.update世帯員把握入力Temp(entity);
-        }
+        所得年度 = new FlexibleYear(DbBusinessConfig.get(ConfigNameDBB.日付関連_調定年度,
+                RDate.getNowDate(), SubGyomuCode.DBB介護賦課));
+
     }
 
     @Override
     protected void process() {
-        SetaiShotokuKazeiHantei sut = SetaiShotokuKazeiHantei.createInstance();
-        sut.getSetaiinHaaku(processParameter.get管理識別区分());
-        sut.getJuminShotokuJoho();
+        List<SetaiHakuEntity> list = mapper.selectNotNull();
+        if (list == null || list.isEmpty()) {
+            SetaiHakuEntity entity = new SetaiHakuEntity();
+            entity.setShotokuNendo(所得年度);
+            mapper.update世帯員把握入力Temp(entity);
+        }
     }
 }
