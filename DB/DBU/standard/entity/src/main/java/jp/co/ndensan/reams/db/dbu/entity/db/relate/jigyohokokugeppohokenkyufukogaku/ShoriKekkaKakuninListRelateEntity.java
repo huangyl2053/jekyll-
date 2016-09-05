dcc3e90a6b0b46589621d947685de5745fc106d7
zhangzhiming <zhangzhiming@liandisys.com.cn>
2016-09-05 10:12:16 +0800

    バッチ設計_DBU010090_事業状況報告資料（月報）作成 保険給付決定情報（高額介護サービス費）新規

---
+++ DB/DBU/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbu/entity/db/relate/jigyohokokugeppohokenkyufukogaku/ShoriKekkaKakuninListRelateEntity.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbu.entity.db.relate.jigyohokokugeppohokenkyufukogaku;
+
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+/**
+ * 処理結果確認リスト情報のRelateEntityクラスです。
+ *
+ * @reamsid_L DBU-5580-030 zhangzhiming
+ */
+@SuppressWarnings("PMd.UnusedPrivateField")
+@lombok.Setter
+@lombok.Getter
+public class ShoriKekkaKakuninListRelateEntity {
+
+    private RString komokuNo1;
+    private RString komokuNo2;
+    private RString komokuNo3;
+    private RString komokuNo4;
+    private RString programId;
+    private RString syoriMeisyou;
+    private RString komoku1;
+    private RString komoku2;
+    private RString komoku3;
+    private RString komoku4;
+    private RString naiYou1;
+    private RString naiYou2;
+}
