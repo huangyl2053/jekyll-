4a0cc924d797820e8997d64ddaa3c26ae6063535
chengsanyuan <chengsanyuan@liandisys.com.cn>
2016-09-06 20:40:14 +0800

    審査依頼受付／みなし２号審査受付 14D203D-27675  対応

--- DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/core/kaigoninteiatenainfo/KaigoNinteiAtenaInfoBusiness.java
+++ DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/core/kaigoninteiatenainfo/KaigoNinteiAtenaInfoBusiness.java
@@ -56,7 +56,10 @@ public class KaigoNinteiAtenaInfoBusiness {
      * @return 住民種別コード
      */
     public RString get住民種別コード() {
-        return kaigoEntity.getFt200Entity().getJuminShubetsuCode();
+        if (kaigoEntity.getFt200Entity() != null) {
+            return kaigoEntity.getFt200Entity().getJuminShubetsuCode();
+        }
+        return RString.EMPTY;
     }
 
     /**
@@ -65,7 +68,10 @@ public class KaigoNinteiAtenaInfoBusiness {
      * @return 識別コード
      */
     public ShikibetsuCode get宛名PSM識別コード() {
-        return kaigoEntity.getFt200Entity().getShikibetsuCode();
+        if (kaigoEntity.getFt200Entity() != null) {
+            return kaigoEntity.getFt200Entity().getShikibetsuCode();
+        }
+        return ShikibetsuCode.EMPTY;
     }
 
     /**
@@ -74,7 +80,10 @@ public class KaigoNinteiAtenaInfoBusiness {
      * @return 個人番号
      */
     public KojinNo get個人番号() {
-        return kaigoEntity.getFt200Entity().getKojinNo();
+        if (kaigoEntity.getFt200Entity() != null) {
+            return kaigoEntity.getFt200Entity().getKojinNo();
+        }
+        return KojinNo.EMPTY;
     }
 
     /**
@@ -83,7 +92,10 @@ public class KaigoNinteiAtenaInfoBusiness {
      * @return 郵便番号
      */
     public YubinNo get郵便番号() {
-        return kaigoEntity.getFt200Entity().getYubinNo();
+        if (kaigoEntity.getFt200Entity() != null) {
+            return kaigoEntity.getFt200Entity().getYubinNo();
+        }
+        return YubinNo.EMPTY;
     }
 
     /**
@@ -92,7 +104,10 @@ public class KaigoNinteiAtenaInfoBusiness {
      * @return 住所
      */
     public AtenaJusho get住所() {
-        return kaigoEntity.getFt200Entity().getJusho();
+        if (kaigoEntity.getFt200Entity() != null) {
+            return kaigoEntity.getFt200Entity().getJusho();
+        }
+        return AtenaJusho.EMPTY;
     }
 
     /**
@@ -101,7 +116,10 @@ public class KaigoNinteiAtenaInfoBusiness {
      * @return 連絡先
      */
     public TelNo get連絡先() {
-        return kaigoEntity.getFt200Entity().getRenrakusaki1();
+        if (kaigoEntity.getFt200Entity() != null) {
+            return kaigoEntity.getFt200Entity().getRenrakusaki1();
+        }
+        return TelNo.EMPTY;
     }
 
     /**
@@ -110,7 +128,10 @@ public class KaigoNinteiAtenaInfoBusiness {
      * @return 行政区名
      */
     public RString get行政区名() {
-        return kaigoEntity.getFt200Entity().getGyoseikuName();
+        if (kaigoEntity.getFt200Entity() != null) {
+            return kaigoEntity.getFt200Entity().getGyoseikuName();
+        }
+        return RString.EMPTY;
     }
 
     /**
@@ -119,7 +140,10 @@ public class KaigoNinteiAtenaInfoBusiness {
      * @return 世帯コード
      */
     public SetaiCode get世帯コード() {
-        return kaigoEntity.getFt200Entity().getSetaiCode();
+        if (kaigoEntity.getFt200Entity() != null) {
+            return kaigoEntity.getFt200Entity().getSetaiCode();
+        }
+        return SetaiCode.EMPTY;
     }
 
     /**
