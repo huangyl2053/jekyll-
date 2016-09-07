a621af5c32419a100cdee9867e5107c8ccc19124
lijian <lijian@liandisys.com.cn>
2016-09-07 10:16:18 +0800

    帳票設計_DBC200003_事業合算・事業分振込明細書作成（一括） 追加

---
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/furikomimeisaiichiran/IFurikomiMeisaiIchiranBuilder.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.business.report.furikomimeisaiichiran;
+
+import jp.co.ndensan.reams.db.dbc.entity.report.furikomimeisaiichiran.FurikomiMeisaiIchiranSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportBuilder;
+
+/**
+ * 帳票設計_DBC100060_事業合算・事業分振込明細書作成（一括） Builderクラスです。
+ *
+ * @reamsid_L DBC-4870-050 lijian
+ */
+public interface IFurikomiMeisaiIchiranBuilder extends IReportBuilder<FurikomiMeisaiIchiranSource> {
+
+}
