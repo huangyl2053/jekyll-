0595702e48dd3b850e7e71a9b87e5a8337e5ed19
tz_chengpeng <tz_chengpeng@liandisys.com.cn>
2016-09-07 13:24:37 +0800

    ビジネス設計_DBDMN91001_医療費控除確認申請　宛名取得メソッドのMapper作成

--- DB/DBD/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbd/persistence/db/mapper/relate/iryohikojokakuninsinsei/IIryoHiKojoKakuninSinseiMapper.java
+++ DB/DBD/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbd/persistence/db/mapper/relate/iryohikojokakuninsinsei/IIryoHiKojoKakuninSinseiMapper.java
@@ -5,17 +5,33 @@
  */
 package jp.co.ndensan.reams.db.dbd.persistence.db.mapper.relate.iryohikojokakuninsinsei;
 
+import jp.co.ndensan.reams.db.dbd.definition.mybatisprm.iryohikojokakuninsinsei.IryoHiKojoKakuninSinseiParameter;
+import jp.co.ndensan.reams.ua.uax.entity.db.basic.UaFt200FindShikibetsuTaishoEntity;
+import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import org.apache.ibatis.annotations.Param;
 
 /**
- * 
+ *
  * @reamsid_L DBD-5770-030 x_zhaowen
  */
 public interface IIryoHiKojoKakuninSinseiMapper {
+
     /**
      * 受給者判定
+     *
      * @param 被保険者番号 RString
      * @return Integer
      */
     Integer 受給者台帳抽出(RString 被保険者番号);
+
+    /**
+     * おむつ使用証明書、主治医意見書確認書の編集に用いる宛名情報を取得する。
+     *
+     * @param param パラメータ
+     * @param 識別コード 識別コード
+     * @return 宛名情報
+     */
+    UaFt200FindShikibetsuTaishoEntity select宛名情報(IryoHiKojoKakuninSinseiParameter param, @Param("識別コード") ShikibetsuCode 識別コード);
+
 }
