e108de4f45699747e99b1fa328b6a668bc286227
b_liuyang2 <b_liuyang2@liandisys.com.cn>
2016-09-07 17:03:41 +0800

    帳票設計_DBDPR32003_1_給付額減額把握リスト  新規作成

---
+++ DB/DBD/standard/business/src/main/java/jp/co/ndensan/reams/db/dbd/business/report/dbd200008/IKyufuGengakuHaakuIchiranEditor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbd.business.report.dbd200008;
+
+import jp.co.ndensan.reams.db.dbd.entity.report.dbd200008.KyufuGengakuHaakuIchiranReportSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportEditor;
+
+/**
+ * 給付額減額把握リストEditorのインターフェースクラスです。
+ *
+ * @reamsid_L DBD-3610-060 b_liuyang2
+ */
+interface IKyufuGengakuHaakuIchiranEditor extends IReportEditor<KyufuGengakuHaakuIchiranReportSource> {
+
+}
