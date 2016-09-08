806f89ae8f80230129f9160af8c16f593c809a8b
quxiaodong <quxiaodong@liandisys.com.cn>
2016-09-08 19:25:55 +0800

    ビジネス設計_DBCMN62004_DBCMNN1005_支給決定情報補正（単）(事業分兼)（初版作成）

---
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/core/kogakugassanshikyuketteihosei/KogakuGassanShikyuKetteiHoseiResult.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.business.core.kogakugassanshikyuketteihosei;
+
+import jp.co.ndensan.reams.db.dbc.business.core.basic.JigyoKogakuGassanShikyuFushikyuKettei;
+import jp.co.ndensan.reams.db.dbc.business.core.basic.KogakuGassanShikyuFushikyuKettei;
+
+/**
+ * ビジネス設計_DBCMN62004_DBCMNN1005_支給決定情報補正（単）(事業分兼)
+ *
+ * @reamsid_L DBC-2290-030 quxiaodong
+ */
+@lombok.Getter
+@lombok.Setter
+@SuppressWarnings("PMD.UnusedPrivateField")
+public class KogakuGassanShikyuKetteiHoseiResult {
+
+    private KogakuGassanShikyuFushikyuKettei 高額合算決定entity;
+    private JigyoKogakuGassanShikyuFushikyuKettei 事業高額合算決定entity;
+
+}
