1e50d81e92b5389b50f81dbe57c02d41fc843281
姜岐 <n8344@ndensan.co.jp>
2016-09-09 13:23:52 +0900

    QA #98624対応

--- DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB3140001/DBB3140001TransitionEventName.java
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB3140001/DBB3140001TransitionEventName.java
@@ -1,25 +1,24 @@
 package jp.co.ndensan.reams.db.dbb.divcontroller.entity.parentdiv.DBB3140001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IStateEnumerations;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 
 /**
  * このコードはツールによって生成されました。</br>
  * このファイルへの変更は、再生成時には損失するため</br>
  * 不正な動作の原因になります。</br>
- * DBB3140001の状態名定義クラスです
+ * DBB3140001のイベント名定義クラスです</br>
  *
  * @author 自動生成
  */
-public enum DBB3140001StateName implements IStateEnumerations {
+public enum DBB3140001TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
 
-    NoChange(""),
-    Syuryou("Syuryou");
+    完了("完了");
 
     private final RString name;
 
-    private DBB3140001StateName(String name) {
+    private DBB3140001TransitionEventName(String name) {
         this.name = new RString(name);
     }
 
