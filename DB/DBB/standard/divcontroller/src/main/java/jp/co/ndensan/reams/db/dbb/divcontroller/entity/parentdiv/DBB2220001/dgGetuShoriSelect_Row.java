b98e8e0fe4c030f0aa6b1dc5b38800527f101555
chenhui <chenhui@liandisys.com.cn>
2016-09-09 10:29:45 +0800

    画面設計_DBB2220001_特別徴収情報アップロード new

---
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB2220001/dgGetuShoriSelect_Row.java
@@ -0,0 +1,86 @@
+package jp.co.ndensan.reams.db.dbb.divcontroller.entity.parentdiv.DBB2220001;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+ * コードが再生成されるときに損失したりします。
+ * Thu Sep 08 18:36:48 CST 2016
+ */
+
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
+
+/**
+ * dgGetuShoriSelect_Row のクラスファイル
+ *
+ * @reamsid_L DBB-5670-010 chenhui
+ */
+public class dgGetuShoriSelect_Row extends DataRow {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-08-09_21-40-56">
+
+    private RString data1;
+    private RString data2;
+    private RString data3;
+    private RString data4;
+
+    public dgGetuShoriSelect_Row() {
+        super();
+        this.data1 = RString.EMPTY;
+        this.data2 = RString.EMPTY;
+        this.data3 = RString.EMPTY;
+        this.data4 = RString.EMPTY;
+        this.setOriginalData("data1", data1);
+        this.setOriginalData("data2", data2);
+        this.setOriginalData("data3", data3);
+        this.setOriginalData("data4", data4);
+    }
+
+    public dgGetuShoriSelect_Row(RString data1, RString data2, RString data3, RString data4) {
+        super();
+        this.setOriginalData("data1", data1);
+        this.setOriginalData("data2", data2);
+        this.setOriginalData("data3", data3);
+        this.setOriginalData("data4", data4);
+        this.data1 = data1;
+        this.data2 = data2;
+        this.data3 = data3;
+        this.data4 = data4;
+    }
+
+    public RString getData1() {
+        return data1;
+    }
+
+    public RString getData2() {
+        return data2;
+    }
+
+    public RString getData3() {
+        return data3;
+    }
+
+    public RString getData4() {
+        return data4;
+    }
+
+    public void setData1(RString data1) {
+        this.setOriginalData("data1", data1);
+        this.data1 = data1;
+    }
+
+    public void setData2(RString data2) {
+        this.setOriginalData("data2", data2);
+        this.data2 = data2;
+    }
+
+    public void setData3(RString data3) {
+        this.setOriginalData("data3", data3);
+        this.data3 = data3;
+    }
+
+    public void setData4(RString data4) {
+        this.setOriginalData("data4", data4);
+        this.data4 = data4;
+    }
+
+    // </editor-fold>
+}
