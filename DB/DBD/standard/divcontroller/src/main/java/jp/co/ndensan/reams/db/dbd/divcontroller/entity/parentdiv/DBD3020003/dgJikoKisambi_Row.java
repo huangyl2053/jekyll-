877a7ff9af05ae23ee80981872122bf0aa82a98b
huangh <huangh@liandisys.com>
2016-09-09 11:59:22 +0800

    画面設計_DBD3020003_時効起算日特殊登改修

--- DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD3020003/dgJikoKisambi_Row.java
+++ DB/DBD/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/divcontroller/entity/parentdiv/DBD3020003/dgJikoKisambi_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbd.divcontroller.entity.parentdiv.DBD3020003;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Tue Sep 06 14:33:53 CST 2016 
+ * Fri Sep 09 11:39:51 CST 2016 
  */
 
 
