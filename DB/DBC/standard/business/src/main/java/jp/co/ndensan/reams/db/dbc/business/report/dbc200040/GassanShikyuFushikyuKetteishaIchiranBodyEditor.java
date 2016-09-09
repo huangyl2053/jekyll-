e10844a9f84f8477e8f2b9cf7b2286bcc219b216
jiangxiaolong <jiangxiaolong@liandisys.com.cn>
2016-09-09 14:38:54 +0800

    ソースを修正する

--- DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/dbc200040/GassanShikyuFushikyuKetteishaIchiranBodyEditor.java
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/dbc200040/GassanShikyuFushikyuKetteishaIchiranBodyEditor.java
@@ -32,7 +32,12 @@ public class GassanShikyuFushikyuKetteishaIchiranBodyEditor
     private static final RString 前括弧 = new RString("（");
     private static final RString 接続符 = new RString("～");
     private static final RString 支給計 = new RString("支給計");
-    private static final RString 不支給計 = new RString("不支給計");
+    private static final RString 不支給計 = new RString("不支給");
+    private static final RString 支給 = new RString("支給");
+    private static final RString 不支給 = new RString("不支給");
+    private static final RString 窓口払 = new RString("窓口払");
+    private static final RString 口座払 = new RString("口座払");
+    private static final RString 件 = new RString("件");
     private static final RString アステリスク_15 = new RString("***************");
     private static final RString 対象データは存在しません = new RString("**　対象データは存在しません　**");
     private static final int INT_15 = 15;
@@ -63,7 +68,6 @@ public class GassanShikyuFushikyuKetteishaIchiranBodyEditor
             }
             return source;
         }
-        //edit明細(source);
         if (集計Flag) {
             edit集計(source);
         } else {
@@ -74,20 +78,32 @@ public class GassanShikyuFushikyuKetteishaIchiranBodyEditor
 
     private void edit明細(GassanShikyuFushikyuKetteishaIchiranSource source) {
 
-        source.listCerter_1 = new RString(entity.get履歴番号());
+        source.listCerter_1 = new RString(entity.get帳票通番());
         source.listCerter_2 = getColumnValue(entity.get被保険者番号());
-        source.listCerter_3 = entity.get支給区分コード();
-        source.listCerter_4 = entity.get支払方法区分();
-        source.listLower_1 = entity.get住所();
+        if (区分_1.equals(entity.get支給区分コード())) {
+            source.listCerter_3 = 支給;
+        } else if (区分_2.equals(entity.get支給区分コード())) {
+            source.listCerter_3 = 不支給;
+        }
+        if (区分_1.equals(entity.get支払方法区分())) {
+            source.listCerter_4 = 窓口払;
+        } else if (区分_2.equals(entity.get支払方法区分())) {
+            source.listCerter_4 = 口座払;
+        }
+        if (区分_1.equals(内部帳票文字切れ制御)) {
+            source.listLower_1 = entity.get住所().substringReturnAsPossible(0, INT_15);
+        } else {
+            source.listLower_1 = アステリスク_15;
+        }
         source.listLower_2 = entity.get対象年度().toDateString();
         source.listLower_3 = new RString(entity.get履歴番号());
         source.listLower_4 = new RString(entity.get決定年月日().toString());
         source.listLower_5 = doカンマ編集(entity.get支給額());
         if (区分_1.equals(entity.get支払方法区分())) {
             source.listLower_6 = toRS(entity.get支払期間開始年月日().toString()).concat(前括弧)
-                    .concat(entity.get支払期間開始年月日().getDayOfWeek().toString()).concat(後括弧).concat(
+                    .concat(entity.get支払期間開始年月日().getDayOfWeek().getInFullParentheses()).concat(後括弧).concat(
                             entity.get支払期間開始時間().toString()).concat(接続符).concat(entity.get支払期間終了年月日().toString()).concat(前括弧)
-                    .concat(entity.get支払期間終了年月日().getDayOfWeek().toString()).concat(後括弧)
+                    .concat(entity.get支払期間終了年月日().getDayOfWeek().getInFullParentheses()).concat(後括弧)
                     .concat(entity.get支払期間終了時間().toString()
                     );
             source.listUpper_5 = entity.get支払場所();
@@ -102,7 +118,7 @@ public class GassanShikyuFushikyuKetteishaIchiranBodyEditor
         }
         source.listUpper_2 = entity.get支給申請整理番号();
         source.listUpper_3 = toRS(entity.get申請年月日().toString());
-        source.listUpper_4 = entity.get被保険者氏名();
+        source.listUpper_4 = doカンマ編集(entity.get自己負担総額());
     }
 
     private void edit集計(GassanShikyuFushikyuKetteishaIchiranSource source) {
@@ -111,9 +127,9 @@ public class GassanShikyuFushikyuKetteishaIchiranBodyEditor
         } else {
             source.listUpper_2 = 不支給計;
         }
-        source.listUpper_3 = new RString(paramter.get支給の件数());
-        source.listUpper_4 = paramter.get自己負担総額の合計金額();
-        source.listLower_5 = paramter.get支給額の合計金額();
+        source.listUpper_3 = new RString(paramter.get支給の件数()).concat(件);
+        source.listUpper_4 = doカンマ編集(paramter.get自己負担総額の合計金額());
+        source.listLower_5 = doカンマ編集(paramter.get支給額の合計金額());
     }
 
     private void editEmpty(GassanShikyuFushikyuKetteishaIchiranSource source) {
@@ -128,6 +144,13 @@ public class GassanShikyuFushikyuKetteishaIchiranBodyEditor
     }
 
     private RString getColumnValue(IDbColumnMappable entity) {
+        if (null != entity) {
+            return entity.getColumnValue();
+        }
+        return RString.EMPTY;
+    }
+
+    private RString get曜日(IDbColumnMappable entity) {
         if (null != entity) {
             return entity.getColumnValue();
         }
