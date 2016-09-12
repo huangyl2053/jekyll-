964da5292a4ab381a4546f2804fcdfb68d3fba43
donghj <donghj@liandisys.com.cn>
2016-09-12 16:54:17 +0800

    BUG対応

--- DB/DBD/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/batchcontroller/step/dbd5720001/JyukyushaDaichoIdoCheckListProcess.java
+++ DB/DBD/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/batchcontroller/step/dbd5720001/JyukyushaDaichoIdoCheckListProcess.java
@@ -5,12 +5,11 @@
  */
 package jp.co.ndensan.reams.db.dbd.batchcontroller.step.dbd5720001;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import jp.co.ndensan.reams.db.dbd.business.core.dbd5720011.JyukyushaDaichoIdoCheckListOutputResult;
-import jp.co.ndensan.reams.db.dbd.business.report.dbd200037.JukyushaIdoCheckListItem;
 import jp.co.ndensan.reams.db.dbd.business.report.dbd200037.JukyushaIdoCheckListProperty.DBD200037_JukyushaIdoCheckListEnum;
 import jp.co.ndensan.reams.db.dbd.business.report.dbd200037.JukyushaIdoCheckListReport;
 import jp.co.ndensan.reams.db.dbd.definition.processprm.dbd5720001.JyukyushaDaichoIdoCheckListProcessParameter;
@@ -20,7 +19,7 @@ import jp.co.ndensan.reams.db.dbd.entity.db.relate.dbd5720001.LowerEntity;
 import jp.co.ndensan.reams.db.dbd.entity.db.relate.dbd5720001.UpperEntity;
 import jp.co.ndensan.reams.db.dbd.entity.report.dbd200037.JukyushaIdoCheckListReportSource;
 import jp.co.ndensan.reams.db.dbd.service.report.dbd5720001.OutputJokenhyoFactory;
-import jp.co.ndensan.reams.db.dbz.service.core.util.report.ReportUtil;
+import jp.co.ndensan.reams.db.dbz.business.core.util.report.ChohyoUtil;
 import jp.co.ndensan.reams.ua.uax.business.core.shikibetsutaisho.search.ShikibetsuTaishoPSMSearchKeyBuilder;
 import jp.co.ndensan.reams.ua.uax.definition.core.enumeratedtype.shikibetsutaisho.KensakuYusenKubun;
 import jp.co.ndensan.reams.ua.uax.definition.mybatisprm.shikibetsutaisho.IShikibetsuTaishoPSMSearchKey;
@@ -65,21 +64,16 @@ public class JyukyushaDaichoIdoCheckListProcess extends BatchKeyBreakBase<Jyukyu
     private static final RString MYBATIS_SELECT_ID = new RString(
             "jp.co.ndensan.reams.db.dbd.persistence.db.mapper.relate.jukyushaidochecklist."
             + "IJukyushaIdoCheckListMapper.get帳票出力対象データ");
-    private static final int INT0 = 0;
-    private static final int INT1 = 1;
-    private static final int INT2 = 2;
-    private static final int INT3 = 3;
-    private static final int INT4 = 4;
-    private RString 出力順1 = RString.EMPTY;
-    private RString 出力順2 = RString.EMPTY;
-    private RString 出力順3 = RString.EMPTY;
-    private RString 出力順4 = RString.EMPTY;
-    private RString 出力順5 = RString.EMPTY;
-    private RString 改頁1 = RString.EMPTY;
-    private RString 改頁2 = RString.EMPTY;
-    private RString 改頁3 = RString.EMPTY;
-    private RString 改頁4 = RString.EMPTY;
-    private RString 改頁5 = RString.EMPTY;
+    private IOutputOrder order = null;
+    private IOutputOrder breakoutputOrder = null;
+    private RString 出力順 = RString.EMPTY;
+    private static final int NUM5 = 5;
+    private static final RString 申請書管理番号 = new RString("申請書管理番号");
+    private static final int NO_0 = 0;
+    private static final int NO_1 = 1;
+    private static final int NO_2 = 2;
+    private static final int NO_3 = 3;
+    private static final int NO_4 = 4;
 
     @BatchWriter
     private BatchReportWriter<JukyushaIdoCheckListReportSource> batchReportWriter;
@@ -87,7 +81,14 @@ public class JyukyushaDaichoIdoCheckListProcess extends BatchKeyBreakBase<Jyukyu
 
     @Override
     protected void initialize() {
-        get出力順項目();
+        IChohyoShutsuryokujunFinder finder = ChohyoShutsuryokujunFinderFactory.createInstance();
+        if (parameter.get出力順ID() != null) {
+            order = finder.get出力順(SubGyomuCode.DBD介護受給, REPORT_DBD200037, parameter.get出力順ID());
+            出力順 = get出力順(order);
+        }
+        if (parameter.get改頁出力順ID() != null) {
+            breakoutputOrder = finder.get出力順(SubGyomuCode.DBD介護受給, REPORT_DBD200037, parameter.get改頁出力順ID());
+        }
     }
 
     @Override
@@ -98,14 +99,17 @@ public class JyukyushaDaichoIdoCheckListProcess extends BatchKeyBreakBase<Jyukyu
         YMDHMS 行挿入日時開始 = new YMDHMS(parameter.get今回抽出開始年月日(), parameter.get今回抽出開始時分秒());
         YMDHMS 行挿入日時終了 = new YMDHMS(parameter.get今回抽出終了年月日(), parameter.get今回抽出終了時分秒());
         return new BatchDbReader(MYBATIS_SELECT_ID, parameter.toJyukyushaDaichoIdoCheckListMybatisParameter(
-                shikibetsuTaishoPSMSearchKey, 行挿入日時開始.getRDateTime(), 行挿入日時終了.getRDateTime(), get出力順()));
+                shikibetsuTaishoPSMSearchKey, 行挿入日時開始.getRDateTime(), 行挿入日時終了.getRDateTime(), 出力順));
     }
 
     @Override
     protected void createWriter() {
+        List pageBreakKeys = new ArrayList<>();
+        set改頁Key(breakoutputOrder, pageBreakKeys);
         batchReportWriter = BatchReportFactory.createBatchReportWriter(REPORT_DBD200037.value()).addBreak(
-                new BreakerCatalog<JukyushaIdoCheckListReportSource>().simplePageBreaker(PAGE_BREAK_KEYS)).create();
+                new BreakerCatalog<JukyushaIdoCheckListReportSource>().simplePageBreaker(pageBreakKeys)).create();
         reportSourceWriter = new ReportSourceWriter(batchReportWriter);
+
     }
 
     @Override
@@ -118,7 +122,7 @@ public class JyukyushaDaichoIdoCheckListProcess extends BatchKeyBreakBase<Jyukyu
         JyukyushaDaichoIdoCheckListOutputResult business = new JyukyushaDaichoIdoCheckListOutputResult();
         UpperEntity upperEntity = business.getUpperEntity(entity);
         LowerEntity lowerEntity = business.getLowerEntity(entity);
-        JukyushaIdoCheckListReport report = new JukyushaIdoCheckListReport(setItem(), upperEntity, lowerEntity);
+        JukyushaIdoCheckListReport report = new JukyushaIdoCheckListReport(upperEntity, lowerEntity, order, breakoutputOrder);
         report.writeBy(reportSourceWriter);
     }
 
@@ -127,80 +131,64 @@ public class JyukyushaDaichoIdoCheckListProcess extends BatchKeyBreakBase<Jyukyu
         OutputJokenhyoFactory outputJokenhyo = new OutputJokenhyoFactory();
         outputJokenhyo.outputJokenhyoFactory(reportSourceWriter, parameter);
         PersonalData personalData = PersonalData.of(ShikibetsuCode.EMPTY,
-                new ExpandedInformation(new Code("0001"), new RString("申請書管理番号"), new RString("申請書管理番号")));
+                new ExpandedInformation(new Code("0001"), 申請書管理番号, 申請書管理番号));
         AccessLogger.log(AccessLogType.照会, personalData);
     }
 
-    private RString get出力順() {
-        IChohyoShutsuryokujunFinder finder = ChohyoShutsuryokujunFinderFactory.createInstance();
-        IOutputOrder order = finder.get出力順(SubGyomuCode.DBD介護受給, REPORT_DBD200037, parameter.get出力順ID());
-        RString 出力順 = RString.EMPTY;
+    private RString get出力順(IOutputOrder order) {
         if (null == order) {
             throw new BatchInterruptedException(UrErrorMessages.実行不可.getMessage()
                     .replace(new RString("帳票出力順の取得").toString()).toString());
         } else {
-            出力順 = MyBatisOrderByClauseCreator.create(DBD200037_JukyushaIdoCheckListEnum.class, order);
+            出力順 = ChohyoUtil.get出力順OrderBy(MyBatisOrderByClauseCreator.
+                    create(DBD200037_JukyushaIdoCheckListEnum.class, order), NUM5);
         }
         return 出力順;
     }
 
-    private JukyushaIdoCheckListItem setItem() {
-        return new JukyushaIdoCheckListItem(
-                出力順1,
-                出力順2,
-                出力順3,
-                出力順4,
-                出力順5,
-                改頁1,
-                改頁2,
-                改頁3,
-                改頁4,
-                改頁5);
-    }
-
-    private void get出力順項目() {
-        Map<Integer, RString> 改頁Map = ReportUtil.get改頁項目(SubGyomuCode.DBD介護受給,
-                parameter.get出力順ID(),
-                REPORT_DBD200037);
-        Map<Integer, ISetSortItem> 出力順Map = ReportUtil.get出力順項目(SubGyomuCode.DBD介護受給,
-                parameter.get出力順ID(),
-                REPORT_DBD200037);
-        if (出力順Map.get(INT0) != null) {
-            出力順1 = 出力順Map.get(INT0).get項目名();
+    private void set改頁Key(IOutputOrder outputOrder, List pageBreakKeys) {
+        RString 改頁１ = RString.EMPTY;
+        RString 改頁２ = RString.EMPTY;
+        RString 改頁３ = RString.EMPTY;
+        RString 改頁４ = RString.EMPTY;
+        RString 改頁５ = RString.EMPTY;
+        if (outputOrder != null) {
+            List<ISetSortItem> list = outputOrder.get設定項目リスト();
+            if (list == null) {
+                list = new ArrayList<>();
+            }
+            if (list.size() > NO_0 && list.get(NO_0).is改頁項目()) {
+                改頁１ = list.get(0).get項目名();
+            }
+            if (list.size() > NO_1 && list.get(NO_1).is改頁項目()) {
+                改頁２ = list.get(NO_1).get項目名();
+            }
+            if (list.size() > NO_2 && list.get(NO_2).is改頁項目()) {
+                改頁３ = list.get(NO_2).get項目名();
+            }
+            if (list.size() > NO_3 && list.get(NO_3).is改頁項目()) {
+                改頁４ = list.get(NO_3).get項目名();
+            }
+            if (list.size() > NO_4 && list.get(NO_4).is改頁項目()) {
+                改頁５ = list.get(NO_4).get項目名();
+            }
+            pageBreakKeys.add(PAGE_BREAK_KEYS);
+            if (!改頁１.isEmpty()) {
+                pageBreakKeys.add(改頁１);
+            }
+            if (!改頁２.isEmpty()) {
+                pageBreakKeys.add(改頁２);
+            }
+            if (!改頁３.isEmpty()) {
+                pageBreakKeys.add(改頁３);
+            }
+            if (!改頁４.isEmpty()) {
+                pageBreakKeys.add(改頁４);
+            }
+            if (!改頁５.isEmpty()) {
+                pageBreakKeys.add(改頁５);
+            }
         }
-        if (出力順Map.get(INT1) != null) {
-            出力順2 = 出力順Map.get(INT1).get項目名();
-        }
-        if (出力順Map.get(INT2) != null) {
-            出力順3 = 出力順Map.get(INT2).get項目名();
-        }
-        if (出力順Map.get(INT3) != null) {
-            出力順4 = 出力順Map.get(INT3).get項目名();
-        }
-        if (出力順Map.get(INT4) != null) {
-            出力順5 = 出力順Map.get(INT4).get項目名();
-        }
-        get改頁(改頁Map);
-
-    }
-
-    private void get改頁(Map<Integer, RString> 改頁Map) {
-        if (改頁Map.get(INT0) != null) {
-            改頁1 = 改頁Map.get(INT0);
-        }
-        if (改頁Map.get(INT1) != null) {
-            改頁2 = 改頁Map.get(INT1);
-        }
-        if (改頁Map.get(INT2) != null) {
-            改頁3 = 改頁Map.get(INT2);
-        }
-        if (改頁Map.get(INT3) != null) {
-            改頁4 = 改頁Map.get(INT3);
-        }
-        if (改頁Map.get(INT4) != null) {
-            改頁5 = 改頁Map.get(INT4);
-        }
-
     }
 
 }
