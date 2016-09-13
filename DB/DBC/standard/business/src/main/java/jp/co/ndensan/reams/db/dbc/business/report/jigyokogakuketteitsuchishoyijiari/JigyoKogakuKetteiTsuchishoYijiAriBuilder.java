50112b61a41ccd24cd12eccea91b1d82f9696370
lijian <lijian@liandisys.com.cn>
2016-09-13 17:26:03 +0800

    帳票設計_DBCMN43002_事業高額決定通知書(支払予定日あり） 追加

---
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/jigyokogakuketteitsuchishoyijiari/JigyoKogakuKetteiTsuchishoYijiAriBuilder.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.business.report.jigyokogakuketteitsuchishoyijiari;
+
+import jp.co.ndensan.reams.db.dbc.entity.jigyokogakuketteitsuchishoyijiari.JigyoKogakuKetteiTsuchishoYijiAriSource;
+import jp.co.ndensan.reams.uz.uza.report.ReportEditorJoiner;
+
+/**
+ * 帳票設計_DBCMN43002_事業高額決定通知書(支払予定日あり） Builderするクラスです。
+ *
+ * @reamsid_L DBC-2000-090 lijian
+ */
+public class JigyoKogakuKetteiTsuchishoYijiAriBuilder implements IJigyoKogakuKetteiTsuchishoYijiAriBuilder {
+
+    private final IJigyoKogakuKetteiTsuchishoYijiAriEditor editor;
+
+    /**
+     * コンストラクタです。
+     *
+     * @param editor IJigyoKogakuKetteiTsuchishoYijiAriEditor
+     */
+    public JigyoKogakuKetteiTsuchishoYijiAriBuilder(IJigyoKogakuKetteiTsuchishoYijiAriEditor editor) {
+        this.editor = editor;
+    }
+
+    @Override
+    public JigyoKogakuKetteiTsuchishoYijiAriSource build() {
+        return ReportEditorJoiner.from(new JigyoKogakuKetteiTsuchishoYijiAriSource()).join(editor).buildSource();
+    }
+
+}
