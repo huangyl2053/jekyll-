195dd19bfe0d9ab0f08986181f2adc011e1e8066
qinzhen <qinzhen@liandisys.com.cn>
2016-09-13 20:45:32 +0800

    帳票設計_DBC200007_給付実績情報取込結果一覧表(新規)

---
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/kyufujissekitorikomiichiran/IKyufujissekiTorikomiIchiranBuilder.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.business.report.kyufujissekitorikomiichiran;
+
+import jp.co.ndensan.reams.db.dbc.entity.report.source.kyufujissekitorikomiichiran.KyufujissekiTorikomiIchiranSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportBuilder;
+
+/**
+ * 帳票設計_DBC200007_給付実績情報取込結果一覧表 IBuilder
+ *
+ * @reamsid_L DBC-2440-030 qinzhen
+ */
+public interface IKyufujissekiTorikomiIchiranBuilder extends
+        IReportBuilder<KyufujissekiTorikomiIchiranSource> {
+
+}
