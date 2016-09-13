fd4c38e8208aa9f07d7ef0ebd704c4611e398732
jiangxiaolong <jiangxiaolong@liandisys.com.cn>
2016-09-13 09:13:38 +0800

    ソースを修正する

--- DB/DBB/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbb/entity/report/atenaseal/DBZ100001AtenaSealSource.java
+++ DB/DBB/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbb/entity/report/atenaseal/DBZ100001AtenaSealSource.java
@@ -772,5 +772,6 @@ public class DBZ100001AtenaSealSource implements IReportSource {
     //追加コードは以下（「User Customize Area」内）に記述してください。
     //帳票ソースデータクラスを再作成する場合は、「User Customize Area」内のソースコードは記述されません。
     //再作成した後、当箇所に記述したコードを再作成された帳票ソースデータクラスの「User Customize Area」内にコピー＆ペーストする必要があります。
+    public int index;
 // </editor-fold>
 }
