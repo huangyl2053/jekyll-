d88bfb724db3d869a40622ca28d8027aea87fc98
田宇鸿 <tianyh@liandisys.com.cn>
2016-09-13 09:21:52 +0800

    batch横展開修正

--- DB/DBD/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/batchcontroller/step/DBD203010/NinteishaListSakuseiProcess.java
+++ DB/DBD/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/batchcontroller/step/DBD203010/NinteishaListSakuseiProcess.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbd.batchcontroller.step.dbdbt00004;
+package jp.co.ndensan.reams.db.dbd.batchcontroller.step.DBD203010;
 
 import java.util.ArrayList;
 import java.util.List;
--- DB/DBD/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/batchcontroller/step/DBD203010/SystemTimeKarisanteiProcess.java
+++ DB/DBD/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/batchcontroller/step/DBD203010/SystemTimeKarisanteiProcess.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbd.batchcontroller.step.dbdbt00004;
+package jp.co.ndensan.reams.db.dbd.batchcontroller.step.DBD203010;
 
 import jp.co.ndensan.reams.uz.uza.batch.process.OutputParameter;
 import jp.co.ndensan.reams.uz.uza.batch.process.SimpleBatchProcessBase;
