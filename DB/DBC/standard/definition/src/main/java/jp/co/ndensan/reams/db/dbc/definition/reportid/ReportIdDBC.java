dcf7bdc6fabf3e26f33e8540cee06fa26022aeaf
wangxingpeng <xpwang@sinoi.com.cn>
2016-09-13 20:32:17 +0800

    ReportID

--- DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/reportid/ReportIdDBC.java
+++ DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/reportid/ReportIdDBC.java
@@ -679,6 +679,14 @@ public enum ReportIdDBC {
      */
     DBC200040(new ReportId("DBC200040_GassanShikyuFushikyuKetteishaIchiran"), new RString("高額合算支給不支給決定通知書情報取込一覧表 ")),
     /**
+     * 帳票設計_DBCMN43002_高額介護サービス費支給（不支給）決定通知一覧表 ＩＤです。
+     */
+    DBC200020(new ReportId("DBC200020_KogakuShikyuFushikyuKetteiTsuchiHakkoIchiran"), new RString("高額介護サービス費支給（不支給）決定通知一覧表")),
+    /**
+     * 帳票設計_DBCMN43002_事業高額支給不支給決定通知一覧表 ＩＤです。
+     */
+    DBC200078(new ReportId("DBC200078_JigyoKogakuShikyuFushikyuKetteTsuchiIchiran"), new RString("事業高額支給不支給決定通知一覧表")),
+    /**
      * 自己負担証明書_2009年以降 帳票ＩＤです。
      */
     DBC100051(new ReportId("DBC100051_JikoFutangakushomeishoFrom2009"), new RString("自己負担額証明書（介護）(平成２１年度以降用）"));
