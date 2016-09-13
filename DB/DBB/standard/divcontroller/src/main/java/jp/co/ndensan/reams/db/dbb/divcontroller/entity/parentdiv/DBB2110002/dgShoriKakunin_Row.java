1eb7f069bda27d8ea79bb0867ebc3935afd94a70
chenhui <chenhui@liandisys.com.cn>
2016-09-13 11:26:51 +0800

    特徴送付情報ダウンロード 変更

--- DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB2110002/dgShoriKakunin_Row.java
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB2110002/dgShoriKakunin_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbb.divcontroller.entity.parentdiv.DBB2110002;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Fri Sep 02 14:46:38 CST 2016
+ * Mon Sep 12 16:15:01 CST 2016
  */
 
 import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
