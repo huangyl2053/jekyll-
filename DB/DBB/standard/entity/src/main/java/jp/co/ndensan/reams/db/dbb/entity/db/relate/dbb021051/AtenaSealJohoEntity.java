7c9a5460c32566fe3387eeda63cf9eb236172c5c
liuyang <liuyang@liandisys.com.cn>
2016-09-14 14:35:03 +0800

    コード更新

--- DB/DBB/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbb/entity/db/relate/dbb021051/AtenaSealJohoEntity.java
+++ DB/DBB/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbb/entity/db/relate/dbb021051/AtenaSealJohoEntity.java
@@ -6,25 +6,24 @@
 package jp.co.ndensan.reams.db.dbb.entity.db.relate.dbb021051;
 
 import java.util.List;
-import jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT1001HihokenshaDaichoEntity;
+import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
+import jp.co.ndensan.reams.ua.uax.entity.db.basic.UaFt200FindShikibetsuTaishoEntity;
 import jp.co.ndensan.reams.ua.uax.entity.db.basic.UaFt250FindAtesakiEntity;
 import jp.co.ndensan.reams.uz.uza.util.db.DbTableEntityBase;
 import jp.co.ndensan.reams.uz.uza.util.db.IDbAccessable;
 
 /**
- *
- * バッチ設計_DBB021051_宛名シール作成（介護賦課）の 被保険者台帳宛名情報のエンティティクラスです。
+ * バッチ設計_DBB021051_宛名シール作成（介護賦課）の介護徴収方法情報のエンティティクラスです。
  *
  * @reamsid_L DBB-5660-030 liuyang
  */
 @SuppressWarnings("PMD.UnusedPrivateField")
 @lombok.Getter
 @lombok.Setter
+public class AtenaSealJohoEntity
+        extends DbTableEntityBase<AtenaSealJohoEntity> implements IDbAccessable {
 
-public class HihokenshaAteanAtesakiJohoEntity
-        extends DbTableEntityBase<HihokenshaAteanAtesakiJohoEntity> implements IDbAccessable {
-
-    private DBB021051TableJohoTempEntity 介護情報一時テーブル;
-    private DbT1001HihokenshaDaichoEntity 被保険者台帳;
+    private HihokenshaNo 被保険者番号;
+    private List<UaFt200FindShikibetsuTaishoEntity> 宛名識別対象PSM;
     private List<UaFt250FindAtesakiEntity> 宛先PSM;
 }
