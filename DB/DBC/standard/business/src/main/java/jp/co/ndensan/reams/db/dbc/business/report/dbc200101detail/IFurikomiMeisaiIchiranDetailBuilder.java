d260699c8fd0400b9a18c9723dc4f3fb75bbaacd
b_liuyang2 <b_liuyang2@liandisys.com.cn>
2016-09-14 16:50:32 +0800

    帳票設計_DBC200101_振込明細一覧表（明細）新規作成 帳票設計_DBD300001_受給者減免月別申請・認定状況表　新規作成

---
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/dbc200101detail/IFurikomiMeisaiIchiranDetailBuilder.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.business.report.dbc200101detail;
+
+import jp.co.ndensan.reams.db.dbc.entity.report.dbc200101detail.FurikomiMeisaiIchiranDetailReportSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportBuilder;
+
+/**
+ * 振込明細一覧表（明細）Builderのinterfaceです。
+ *
+ * @reamsid_L DBC-2180-050 b_liuyang2
+ */
+interface IFurikomiMeisaiIchiranDetailBuilder extends IReportBuilder<FurikomiMeisaiIchiranDetailReportSource> {
+
+}
