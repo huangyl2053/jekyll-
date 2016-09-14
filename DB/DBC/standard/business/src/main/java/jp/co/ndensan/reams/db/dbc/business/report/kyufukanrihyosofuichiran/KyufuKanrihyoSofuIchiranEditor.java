985f8dc6cd204620c9c8be5af1aa4df86c0cc760
wangchao <wangchao@liandisys.com.cn>
2016-09-14 20:57:58 +0800

    ビルドエラーを臨時修正

--- DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/kyufukanrihyosofuichiran/KyufuKanrihyoSofuIchiranEditor.java
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/kyufukanrihyosofuichiran/KyufuKanrihyoSofuIchiranEditor.java
@@ -8,7 +8,7 @@ package jp.co.ndensan.reams.db.dbc.business.report.kyufukanrihyosofuichiran;
 import java.util.List;
 import java.util.Map;
 import jp.co.ndensan.reams.db.dbc.definition.core.kyotakuservice.KyotakuServiceKubun;
-import jp.co.ndensan.reams.db.dbx.service.core.shichosonsecurity.ShichosonSecurityJohoFinder;
+//import jp.co.ndensan.reams.db.dbx.service.core.shichosonsecurity.ShichosonSecurityJohoFinder;
 import jp.co.ndensan.reams.db.dbc.definition.core.kyotakuservice.KyufukanrihyoSakuseiKubun;
 import jp.co.ndensan.reams.db.dbc.entity.db.relate.kyufukanrihyosofuichiran.KyufuKanrihyoSofuIchiranEntity;
 import jp.co.ndensan.reams.db.dbc.entity.report.source.kyufukanrihyosofuichiran.KyufuKanrihyoSofuIchiranReportSource;
@@ -59,7 +59,7 @@ public class KyufuKanrihyoSofuIchiranEditor implements IKyufuKanrihyoSofuIchiran
         this.entity = entity;
         this.出力順Map = 出力順Map;
         this.改頁リスト = 改頁リスト;
-        
+
     }
 
     @Override
@@ -95,7 +95,7 @@ public class KyufuKanrihyoSofuIchiranEditor implements IKyufuKanrihyoSofuIchiran
         source.listUpper_6 = KyotakuServiceKubun.toValue(entity.get居宅サービス区分()).get名称();
         source.listUpper_7 = entity.get要介護状態区分();//TODO QA確認中
         source.listUpper_8 = toRString(entity.get区分支給限度基準額());
-        source.listUpper_9 =  パターン54(entity.get限度額管理開始年月日());
+        source.listUpper_9 = パターン54(entity.get限度額管理開始年月日());
         source.listUpper_10 = パターン54(entity.get限度額管理終了年月日());
         source.listUpper_11 = new RString("");
         source.listUpper_12 = new RString("");
@@ -103,19 +103,18 @@ public class KyufuKanrihyoSofuIchiranEditor implements IKyufuKanrihyoSofuIchiran
         if (entity.get区分支給限度基準額().compareTo(entity.get合計単位()) < 0) {
             source.listUpper_14 = 備考;
         }
-        ShichosonSecurityJoho 介護導入形態 = ShichosonSecurityJohoFinder
-                                         .createInstance().getShichosonSecurityJoho(GyomuBunrui.介護事務);
-        if (null != 介護導入形態) {
-            KaigoDonyuKubun 介護導入区分 = 介護導入形態.get介護導入区分();
-            if (KaigoDonyuKubun.未導入.code().equals(介護導入区分.code())) {
-                source.bikoTitle = new RString("");
-                source.listLower_1 = new RString("");
-            } else if (KaigoDonyuKubun.導入済.code().equals(介護導入区分.code())) {
-                source.bikoTitle = entity.get備考_証記載保険者番号();
-                source.listLower_1 = entity.get備考_証記載保険者番号();
-            }
-        }
-        
+//        ShichosonSecurityJoho 介護導入形態 = ShichosonSecurityJohoFinder.createInstance().getShichosonSecurityJoho(GyomuBunrui.介護事務);
+//        if (null != 介護導入形態) {
+//            KaigoDonyuKubun 介護導入区分 = 介護導入形態.get介護導入区分();
+//            if (KaigoDonyuKubun.未導入.code().equals(介護導入区分.code())) {
+//                source.bikoTitle = new RString("");
+//                source.listLower_1 = new RString("");
+//            } else if (KaigoDonyuKubun.導入済.code().equals(介護導入区分.code())) {
+//                source.bikoTitle = entity.get備考_証記載保険者番号();
+//                source.listLower_1 = entity.get備考_証記載保険者番号();
+//            }
+//        }
+
         source.shinkiGokeiKensu = toRString(entity.get合計件数_新規());
         source.shuseiGokeiKensu = toRString(entity.get合計件数_修正());
         source.torikeshiGokeiKensu = toRString(entity.get合計件数_取消());
@@ -129,7 +128,7 @@ public class KyufuKanrihyoSofuIchiranEditor implements IKyufuKanrihyoSofuIchiran
     private RString get改頁(int index) {
         return index < 改頁リスト.size() ? 改頁リスト.get(index) : RString.EMPTY;
     }
-    
+
     private RString パターン56(FlexibleYearMonth 年月) {
         if (null == 年月) {
             return RString.EMPTY;
@@ -137,14 +136,14 @@ public class KyufuKanrihyoSofuIchiranEditor implements IKyufuKanrihyoSofuIchiran
         return 年月.wareki().eraType(EraType.KANJI_RYAKU)
                 .firstYear(FirstYear.GAN_NEN).separator(Separator.JAPANESE).fillType(FillType.BLANK).toDateString();
     }
-    
+
     private RString toRString(Decimal 金額) {
         if (金額 != null) {
             return DecimalFormatter.toRString(金額, 0);
         }
         return RString.EMPTY;
     }
-    
+
     private RString パターン54(FlexibleYearMonth 年月) {
         if (null == 年月) {
             return RString.EMPTY;
