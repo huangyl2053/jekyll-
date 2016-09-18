c48db9f5b5954dd355d73d40b93bad26735a1b5d
jiangxiaolong <jiangxiaolong@liandisys.com.cn>
2016-09-18 10:30:51 +0800

    ソースを修正する

--- DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/dbc200025/ShokanRenrakuhyoSofuIchiranParameter.java
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/dbc200025/ShokanRenrakuhyoSofuIchiranParameter.java
@@ -3,35 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbc.business.report.dbc200031;
+package jp.co.ndensan.reams.db.dbc.business.report.dbc200025;
 
-import jp.co.ndensan.reams.db.dbc.entity.db.relate.dbc200031.GassanJikofutangakuHoseiIchiranEntity;
+import java.util.Map;
+import jp.co.ndensan.reams.db.dbc.entity.db.relate.dbc200025.ShokanRenrakuhyoSofuIchiranEntity;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleYearMonth;
 import jp.co.ndensan.reams.uz.uza.lang.RDateTime;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
- * 帳票設計_DBCMN62002_高額合算自己負担額補正結果一覧表帳票のパラメータです。
+ * 帳票設計_DBC200025_償還連絡票送付一覧表帳票のパラメータです。
  *
- * @reamsid_L DBC-2320-040 jiangxiaolong
+ * @reamsid_L DBC-2570-040 jiangxiaolong
  */
 @lombok.Getter
 @lombok.Setter
 @SuppressWarnings("PMD.UnusedPrivateField")
-public class GassanJikofutangakuHoseiIchiranParameter {
+public class ShokanRenrakuhyoSofuIchiranParameter {
 
-    private GassanJikofutangakuHoseiIchiranEntity 高額合算自己負担額補正一覧表;
-    private RString 市町村コード;
-    private RString 市町村名称;
+    private ShokanRenrakuhyoSofuIchiranEntity 償還連絡票一覧表データ;
     private RDateTime 作成日時;
-    private RString 出力順1;
-    private RString 出力順2;
-    private RString 出力順3;
-    private RString 出力順4;
-    private RString 出力順5;
-    private RString 改頁1;
-    private RString 改頁2;
-    private RString 改頁3;
-    private RString 改頁4;
-    private RString 改頁5;
+    private RString 帳票タイトル;
+    private FlexibleYearMonth 処理年月;
+    private Map<RString, RString> 出力順Map;
 
 }
