85b5a56ffac3983078b07502e5d6b6cad87b41b2
wangxingpeng <wangxingpeng@liandisys.com.cn>
2016-09-19 16:36:27 +0800

    画面設計_DBCMNF1001_保険者情報送付データ作成_[38Q]高額合算給付実績情報(ビルドエラーの修正)

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC0200014/KogakuGassanKyufuJissekiJoho.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC0200014/KogakuGassanKyufuJissekiJoho.java
@@ -59,8 +59,10 @@ public class KogakuGassanKyufuJissekiJoho {
      * @return ResponseData DBC110110_KogakugassanKyufujissekiOutParameter
      */
     public ResponseData<DBC110110_KogakugassanKyufujissekiOutParameter> onClick_Execute(KogakuGassanKyufuJissekiJohoDiv div) {
-        再処理区分 = div.getCcdKokuhorenJohoSofu().get再処理区分のValue();
-        処理年月 = div.getCcdKokuhorenJohoSofu().get処理年月のValue().getYearMonth();
+        KokuhorenDataSofuViewState parmater = ViewStateHolder.get(ViewStateHolderName.国保連取込情報,
+                KokuhorenDataSofuViewState.class);
+        再処理区分 = parmater.get再処理区分();
+        処理年月 = parmater.get処理年月();
         if (getHandler(div).setBatchParameter(再処理区分, 処理年月) != null) {
             return ResponseData.of(getHandler(div).setBatchParameter(再処理区分, 処理年月)).respond();
         }
