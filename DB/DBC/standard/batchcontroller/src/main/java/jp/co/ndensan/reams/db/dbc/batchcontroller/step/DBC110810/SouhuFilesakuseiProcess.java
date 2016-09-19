8b0a90f5b17f9785ccd3a36bfdbc2ae7264e2860
xuyongchao <xuyongchao@liandisys.com.cn>
2016-09-19 12:11:07 +0800

    バッチ設計_DBCMNF1003_536_受給者突合依頼情報作成　trimを修正する

--- DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/step/DBC110810/SouhuFilesakuseiProcess.java
+++ DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/step/DBC110810/SouhuFilesakuseiProcess.java
@@ -199,65 +199,65 @@ public class SouhuFilesakuseiProcess extends BatchProcessBase<DbT3001JukyushaIdo
         record2Entity.set認定有効年月(有効年月);
         record2Entity.set突合開始年月(有効期間開始年月日);
         record2Entity.set突合終了年月(有効期間終了年月日);
-        record2Entity.set異動年月日(new RString(entity.getIdoYMD().toString()));
-        record2Entity.set異動区分コード(entity.getIdoKubunCode());
-        record2Entity.set異動事由(entity.getJukyushaIdoJiyu());
-        record2Entity.set証記載保険者番号(entity.getShoKisaiHokenshaNo().value());
-        record2Entity.set被保険者番号(entity.getHiHokenshaNo().value());
-        record2Entity.set被保険者氏名カナ(entity.getHiHokenshaNameKana());
-        record2Entity.set生年月日(new RString(entity.getUmareYMD().toString()));
-        record2Entity.set性別コード(entity.getSeibetsuCode());
-        record2Entity.set資格取得年月日(new RString(entity.getShikakuShutokuYMD().toString()));
+        record2Entity.set異動年月日(new RString(entity.getIdoYMD().toString().trim()));
+        record2Entity.set異動区分コード(entity.getIdoKubunCode().trim());
+        record2Entity.set異動事由(entity.getJukyushaIdoJiyu().trim());
+        record2Entity.set証記載保険者番号(entity.getShoKisaiHokenshaNo().value().trim());
+        record2Entity.set被保険者番号(entity.getHiHokenshaNo().value().trim());
+        record2Entity.set被保険者氏名カナ(entity.getHiHokenshaNameKana().trim());
+        record2Entity.set生年月日(new RString(entity.getUmareYMD().toString().trim()));
+        record2Entity.set性別コード(entity.getSeibetsuCode().trim());
+        record2Entity.set資格取得年月日(new RString(entity.getShikakuShutokuYMD().toString().trim()));
         record2Entity.set資格喪失年月日(change(entity.getShikakuSoshitsuYMD()));
-        record2Entity.set老人保健市町村番号(entity.getRojinHokenShichosonNo());
-        record2Entity.set老人保健受給者番号(entity.getRojinHokenJukyushaNo());
-        record2Entity.set公費負担者番号(entity.getKohiFutanshaNo());
+        record2Entity.set老人保健市町村番号(trim(entity.getRojinHokenShichosonNo()));
+        record2Entity.set老人保健受給者番号(trim(entity.getRojinHokenJukyushaNo()));
+        record2Entity.set公費負担者番号(trim(entity.getKohiFutanshaNo()));
         record2Entity.set広域連合保険者番号(changeValue(entity.getKoikiRengoHokenshaNo()));
-        record2Entity.set申請種別コード(entity.getShinseiShubetsuCode());
-        record2Entity.set変更申請中区分コード(entity.getHenkoShinseichuKubunCode());
+        record2Entity.set申請種別コード(trim(entity.getShinseiShubetsuCode()));
+        record2Entity.set変更申請中区分コード(trim(entity.getHenkoShinseichuKubunCode()));
         record2Entity.set申請年月日(change(entity.getShinseiYMD()));
-        record2Entity.setみなし要介護区分コード(entity.getMinashiYokaigoJotaiKubunCode());
-        record2Entity.set要介護状態区分コード(entity.getYokaigoJotaiKubunCode());
+        record2Entity.setみなし要介護区分コード(trim(entity.getMinashiYokaigoJotaiKubunCode()));
+        record2Entity.set要介護状態区分コード(trim(entity.getYokaigoJotaiKubunCode()));
         record2Entity.set認定有効期間開始年月日(change(entity.getNinteiYukoKikankaishiYMD()));
-        record2Entity.set認定有効期間終了年月日(entity.getNinteiYukoKikanShuryoYMD());
-        record2Entity.set居宅サービス計画作成区分コード(entity.getKyotakuServiceSakuseiKubunCode());
-        record2Entity.set居宅介護支援事業所番号(entity.getKyotakuKaigoShienJigyoshoNo());
-        record2Entity.set居宅サービス計画適用開始年月日(entity.getKyotakuServiceTekiyoKaishiYMD());
-        record2Entity.set居宅サービス計画適用終了年月日(entity.getKyotakuServiceTekiyoShuryoYMD());
-        record2Entity.set訪問支給限度基準額(new RString(entity.getHomonTsushoServiceShikyuGendoKijungaku()));
+        record2Entity.set認定有効期間終了年月日(trim(entity.getNinteiYukoKikanShuryoYMD()));
+        record2Entity.set居宅サービス計画作成区分コード(trim(entity.getKyotakuServiceSakuseiKubunCode()));
+        record2Entity.set居宅介護支援事業所番号(trim(entity.getKyotakuKaigoShienJigyoshoNo()));
+        record2Entity.set居宅サービス計画適用開始年月日(trim(entity.getKyotakuServiceTekiyoKaishiYMD()));
+        record2Entity.set居宅サービス計画適用終了年月日(trim(entity.getKyotakuServiceTekiyoShuryoYMD()));
+        record2Entity.set訪問支給限度基準額(new RString(entity.getHomonTsushoServiceShikyuGendoKijungaku()).trim());
         record2Entity.set訪問開始年月日(change(entity.getHomonTsushoServiceJogenKanriTekiyoKaishiYMD()));
-        record2Entity.set訪問終了年月日(entity.getHomonTsushoServiceJogenKanriTekiyoShuryoYMD());
-        record2Entity.set短期支給限度基準額(new RString(entity.getTankiNyushoServiceShikyuGendoKijungaku()));
+        record2Entity.set訪問終了年月日(trim(entity.getHomonTsushoServiceJogenKanriTekiyoShuryoYMD()));
+        record2Entity.set短期支給限度基準額(new RString(entity.getTankiNyushoServiceShikyuGendoKijungaku()).trim());
         record2Entity.set短期開始年月日(change(entity.getTankinyushoServiceJogenKanriTekiyoKaishiYMD()));
         record2Entity.set短期終了年月日(change(entity.getTankinyushoServiceJogenKanriTekiyoShuryoYMD()));
         record2Entity.set公費負担上限額減額の有無(entity.getKohiFutanJogenGengakuAriFlag());
-        record2Entity.set償還払化開始年月日(entity.getShokanbaraikaKaishiYMD());
-        record2Entity.set償還払化終了年月日(entity.getShokanbaraikaShuryoYMD());
-        record2Entity.set給付率引下げ開始年月日(entity.getKyufuritsuHikisageKaishiYMD());
-        record2Entity.set給付率引下げ終了年月日(entity.getKyufuritsuHikisageShuryoYMD());
-        record2Entity.set減免申請中区分コード(entity.getGemmenShinseichuKubunCode());
-        record2Entity.set利用者負担区分コード(entity.getRiyoshaFutanKubunCode());
+        record2Entity.set償還払化開始年月日(trim(entity.getShokanbaraikaKaishiYMD()));
+        record2Entity.set償還払化終了年月日(trim(entity.getShokanbaraikaShuryoYMD()));
+        record2Entity.set給付率引下げ開始年月日(trim(entity.getKyufuritsuHikisageKaishiYMD()));
+        record2Entity.set給付率引下げ終了年月日(trim(entity.getKyufuritsuHikisageShuryoYMD()));
+        record2Entity.set減免申請中区分コード(trim(entity.getGemmenShinseichuKubunCode()));
+        record2Entity.set利用者負担区分コード(trim(entity.getRiyoshaFutanKubunCode()));
         record2Entity.set給付率(entity.getKyufuritsu());
-        record2Entity.set適用開始年月日(entity.getTekiyoKaishiYMD());
-        record2Entity.set適用終了年月日(entity.getTekiyoShuryoYMD());
-        record2Entity.set標準負担区分コード(entity.getHyojunFutanKubunCode());
+        record2Entity.set適用開始年月日(trim(entity.getTekiyoKaishiYMD()));
+        record2Entity.set適用終了年月日(trim(entity.getTekiyoShuryoYMD()));
+        record2Entity.set標準負担区分コード(trim(entity.getHyojunFutanKubunCode()));
         record2Entity.set負担額(entity.getFutangaku());
-        record2Entity.set負担額適用開始年月日(entity.getFutangakuTekiyoKaishiYMD());
-        record2Entity.set負担額適用終了年月日(entity.getFutangakuTekiyoShuryoYMD());
-        record2Entity.set特定入所者認定申請中区分コード(entity.getTokuteiNyushoshaNinteiShinseichuKubunCode());
-        record2Entity.set特定入所者介護サービス区分コード(entity.getTokuteiNyushoshaKaigoServiceKubunCode());
+        record2Entity.set負担額適用開始年月日(trim(entity.getFutangakuTekiyoKaishiYMD()));
+        record2Entity.set負担額適用終了年月日(trim(entity.getFutangakuTekiyoShuryoYMD()));
+        record2Entity.set特定入所者認定申請中区分コード(trim(entity.getTokuteiNyushoshaNinteiShinseichuKubunCode()));
+        record2Entity.set特定入所者介護サービス区分コード(trim(entity.getTokuteiNyushoshaKaigoServiceKubunCode()));
         record2Entity.set課税層の特例減額措置対象(entity.getKaizeisoTokureiGengakuSochiTaishoFlag());
-        record2Entity.set食費負担限度額(entity.getShokuhiFutanGendogaku());
-        record2Entity.set居住費個室負担限度額(entity.getKyojuhiUnitGataKoshitsuFutanGendogaku());
-        record2Entity.set居住費準個室負担限度額(entity.getKyojuhiUnitGataJunKoshitsuFutanGendogaku());
-        record2Entity.set居住費特養負担限度額(entity.getKyojuhiJuraiGataKoshitsuTokuyoFutanGendogaku());
-        record2Entity.set居住費従来型個室負担限度額(entity.getKyojuhiJuraiGataKoshitsuRokenRyoyoFutanGendogaku());
-        record2Entity.set居住費多床室負担限度額(entity.getKyujuhiTashoshitsuFutanGendogaku());
-        record2Entity.set負担限度額適用開始年月日(entity.getFutanGendogakuTekiyoKaishiYMD());
-        record2Entity.set負担限度額適用終了年月日(entity.getFutanGendogakuTekiyoShuryoYMD());
-        record2Entity.set軽減率(entity.getKeigenritsu());
-        record2Entity.set軽減率適用開始年月日(entity.getKeigenritsuTekiyoKaishiYMD());
-        record2Entity.set軽減率適用終了年月日(entity.getKeigenritsuTekiyoShuryoYMD());
+        record2Entity.set食費負担限度額(trim(entity.getShokuhiFutanGendogaku()));
+        record2Entity.set居住費個室負担限度額(trim(entity.getKyojuhiUnitGataKoshitsuFutanGendogaku()));
+        record2Entity.set居住費準個室負担限度額(trim(entity.getKyojuhiUnitGataJunKoshitsuFutanGendogaku()));
+        record2Entity.set居住費特養負担限度額(trim(entity.getKyojuhiJuraiGataKoshitsuTokuyoFutanGendogaku()));
+        record2Entity.set居住費従来型個室負担限度額(trim(entity.getKyojuhiJuraiGataKoshitsuRokenRyoyoFutanGendogaku()));
+        record2Entity.set居住費多床室負担限度額(trim(entity.getKyujuhiTashoshitsuFutanGendogaku()));
+        record2Entity.set負担限度額適用開始年月日(trim(entity.getFutanGendogakuTekiyoKaishiYMD()));
+        record2Entity.set負担限度額適用終了年月日(trim(entity.getFutanGendogakuTekiyoShuryoYMD()));
+        record2Entity.set軽減率(trim(entity.getKeigenritsu()));
+        record2Entity.set軽減率適用開始年月日(trim(entity.getKeigenritsuTekiyoKaishiYMD()));
+        record2Entity.set軽減率適用終了年月日(trim(entity.getKeigenritsuTekiyoShuryoYMD()));
         record2Entity.set小規模多機能型居宅介護(entity.getShoTakinoKyotakuKaigoRiyozukiRiyoAriFlag());
         return record2Entity;
 
@@ -267,14 +267,21 @@ public class SouhuFilesakuseiProcess extends BatchProcessBase<DbT3001JukyushaIdo
         if (null == date) {
             return RString.EMPTY;
         }
-        return new RString(date.toString());
+        return new RString(date.toString()).trim();
     }
 
     private RString changeValue(ShoKisaiHokenshaNo shoKisaiHokenshaNo) {
         if (null == shoKisaiHokenshaNo) {
             return RString.EMPTY;
         }
-        return shoKisaiHokenshaNo.value();
+        return shoKisaiHokenshaNo.value().trim();
+    }
+
+    private RString trim(RString rst) {
+        if (null == rst) {
+            return RString.EMPTY;
+        }
+        return rst.trim();
     }
 
     private SouhuFilesakuseiEntity getEnd(SouhuFilesakuseiEntity record3Entity) {
