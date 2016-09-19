8becf4a6aefdef18707fadfe5c0e9fccd04c0d61
lihang <lihang@liandisys.com.cn>
2016-09-19 11:58:29 +0800

    修正します

--- DB/DBB/standard/business/src/main/java/jp/co/ndensan/reams/db/dbb/business/report/tokubetsuchoshuidojohokensuhyo/TokubetsuChoshuIdojohoKensuhyoReport.java
+++ DB/DBB/standard/business/src/main/java/jp/co/ndensan/reams/db/dbb/business/report/tokubetsuchoshuidojohokensuhyo/TokubetsuChoshuIdojohoKensuhyoReport.java
@@ -5,13 +5,10 @@
  */
 package jp.co.ndensan.reams.db.dbb.business.report.tokubetsuchoshuidojohokensuhyo;
 
-import jp.co.ndensan.reams.db.dbb.entity.db.relate.tokubetsuchoshuidojohokensuhyo.TokuChoYidoKensu;
 import jp.co.ndensan.reams.db.dbb.entity.report.tokubetsuchoshuidojohokensuhyo.TokubetsuChoshuIdojohoKensuhyoSource;
-import jp.co.ndensan.reams.uz.uza.biz.YMDHMS;
-import jp.co.ndensan.reams.uz.uza.lang.FlexibleYear;
-import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.report.Report;
 import jp.co.ndensan.reams.uz.uza.report.ReportSourceWriter;
+import lombok.NonNull;
 
 /**
  * 帳票設計_DBBPR82001_4_特別徴収異動情報件数表Report
@@ -20,36 +17,35 @@ import jp.co.ndensan.reams.uz.uza.report.ReportSourceWriter;
  */
 public class TokubetsuChoshuIdojohoKensuhyoReport extends Report<TokubetsuChoshuIdojohoKensuhyoSource> {
 
-    private final TokuChoYidoKensu 特徴異動件数Entity;
-    private final YMDHMS 作成日時;
-    private final FlexibleYear 賦課年度;
-    private final RString 処理対象月;
+    private final TokubetsuChoshuIdojohoKensuhyoParameter parameter;
 
     /**
      * コンストラクタです。
      *
-     * @param 特徴異動件数Entity TokubetsuChoshuIdojohoKensuhyoEntity
-     * @param 作成日時 YMDHMS
-     * @param 賦課年度 FlexibleYear
-     * @param 処理対象月 RString
+     *
+     * @param parameter TokubetsuChoshuIdojohoKensuhyoParameter
      */
-    public TokubetsuChoshuIdojohoKensuhyoReport(
-            TokuChoYidoKensu 特徴異動件数Entity,
-            YMDHMS 作成日時,
-            FlexibleYear 賦課年度,
-            RString 処理対象月) {
+    public TokubetsuChoshuIdojohoKensuhyoReport(TokubetsuChoshuIdojohoKensuhyoParameter parameter) {
 
-        this.特徴異動件数Entity = 特徴異動件数Entity;
-        this.作成日時 = 作成日時;
-        this.賦課年度 = 賦課年度;
-        this.処理対象月 = 処理対象月;
+        this.parameter = parameter;
+    }
+
+    /**
+     * createFormメソッド
+     *
+     * @param parameter TokubetsuChoshuIraiJohoKensuhyoParameter
+     * @return TokubetsuChoshuHeijunkaKeisanJuneKekkaIchiranReport
+     */
+    public static TokubetsuChoshuIdojohoKensuhyoReport createForm(
+            @NonNull TokubetsuChoshuIdojohoKensuhyoParameter parameter) {
+        return new TokubetsuChoshuIdojohoKensuhyoReport(parameter);
     }
 
     @Override
     public void writeBy(ReportSourceWriter<TokubetsuChoshuIdojohoKensuhyoSource> writer) {
 
         ITokubetsuChoshuIdojohoKensuhyoEditor editor
-                = new TokubetsuChoshuIdojohoKensuhyoEditor(特徴異動件数Entity, 作成日時, 賦課年度, 処理対象月);
+                = new TokubetsuChoshuIdojohoKensuhyoEditor(parameter);
         ITokubetsuChoshuIdojohoKensuhyoBuilder builder
                 = new TokubetsuChoshuIdojohoKensuhyoBuilder(editor);
         writer.writeLine(builder);
