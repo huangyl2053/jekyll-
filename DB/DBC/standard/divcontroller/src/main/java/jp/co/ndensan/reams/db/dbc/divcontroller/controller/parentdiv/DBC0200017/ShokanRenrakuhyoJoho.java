9d3ff65def52d6a05814dee57f1c133c1d7d7d79
wangxingpeng <wangxingpeng@liandisys.com.cn>
2016-09-20 18:36:03 +0800

    画面設計_DBCMNF1001_保険者情報送付データ作成_[211]償還連絡票情報(ビルドエラーの修正)

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC0200017/ShokanRenrakuhyoJoho.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC0200017/ShokanRenrakuhyoJoho.java
@@ -41,7 +41,7 @@ public class ShokanRenrakuhyoJoho {
      * @return ResponseData ShokanRenrakuhyoJohoDiv
      */
     public ResponseData<ShokanRenrakuhyoJohoDiv> onLoad(ShokanRenrakuhyoJohoDiv div) {
-        KokuhorenDataSofuViewState parmater = ViewStateHolder.get(ViewStateHolderName.国保連取込情報,
+        KokuhorenDataSofuViewState parmater = ViewStateHolder.get(ViewStateHolderName.国保連送付情報,
                 KokuhorenDataSofuViewState.class);
         再処理区分 = parmater.get再処理区分();
         処理年月 = parmater.get処理年月();
@@ -59,7 +59,7 @@ public class ShokanRenrakuhyoJoho {
      * @return ResponseData DBC110050_ShokanRenrakuhyoOutParameter
      */
     public ResponseData<DBC110050_ShokanRenrakuhyoOutParameter> onClick_Execute(ShokanRenrakuhyoJohoDiv div) {
-        KokuhorenDataSofuViewState parmater = ViewStateHolder.get(ViewStateHolderName.国保連取込情報,
+        KokuhorenDataSofuViewState parmater = ViewStateHolder.get(ViewStateHolderName.国保連送付情報,
                 KokuhorenDataSofuViewState.class);
         再処理区分 = parmater.get再処理区分();
         処理年月 = parmater.get処理年月();
