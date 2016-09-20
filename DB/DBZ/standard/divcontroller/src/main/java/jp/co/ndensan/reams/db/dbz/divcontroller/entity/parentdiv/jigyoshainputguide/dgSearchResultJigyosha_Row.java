6de4991a317eef425b0e3b09930dedffe8bf9877
huangh <huangh@liandisys.com>
2016-09-20 10:56:11 +0800

    6.22前の版本に回復する

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/parentdiv/jigyoshainputguide/dgSearchResultJigyosha_Row.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/parentdiv/jigyoshainputguide/dgSearchResultJigyosha_Row.java
@@ -1,4 +1,4 @@
-package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.jigyoshainputguide;
+package jp.co.ndensan.reams.db.dbz.divcontroller.entity.parentdiv.jigyoshainputguide;
 /**
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
