82f06a96773ee7522bce7a4de8b7d95db335b100
n3423 <n3423@ndensan.co.jp>
2016-09-20 11:46:23 +0900

    実装課題 #100891　【不具合】DBU-1080　裁決結果登録　削除するボタンの名称が不正に切り替わる 実装課題 #100863　【不具合】DBU-1080　弁明登録　発行するボタンが非活性 実装課題 #100878　【不具合】DBU-1080　弁明登録　弁明情報未登録レコードの削除時にエラーが発生し、画面遷移が出来なくなる。 実装課題 #100822　【不具合】DBU-1080　審査請求書登録_一覧　不要な発行ボタンの表示対応 実装課題 #100784　【不具合】DBU-1080　審査請求書登録_登録　処分種類のドロップダウンリストに「居宅介護サービス費等」の項目が２つある

--- DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/controller/parentdiv/DBU0900011/SinsaSeikyusyoPanel.java
+++ DB/DBU/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/divcontroller/controller/parentdiv/DBU0900011/SinsaSeikyusyoPanel.java
@@ -6,19 +6,24 @@
 package jp.co.ndensan.reams.db.dbu.divcontroller.controller.parentdiv.DBU0900011;
 
 import java.util.List;
+import jp.co.ndensan.reams.db.dbu.business.core.benmeitoroku.BenmeiTorokuMeisaiJoho;
 import jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0900011.DBU0900011TransitionEventName;
 import jp.co.ndensan.reams.db.dbu.divcontroller.entity.parentdiv.DBU0900011.SinsaSeikyusyoPanelDiv;
 import jp.co.ndensan.reams.db.dbu.divcontroller.handler.parentdiv.DBU0900011.SinsaSeikyusyoJohoHandler;
+import jp.co.ndensan.reams.db.dbu.service.core.benmeitoroku.BenmeiTorokuManager;
 import jp.co.ndensan.reams.db.dbu.service.core.sinsasei.SinsaSeikyusyoJohoFinder;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
 import jp.co.ndensan.reams.db.dbx.definition.core.viewstate.ViewStateKeys;
 import static jp.co.ndensan.reams.db.dbx.definition.core.viewstate.ViewStateKeys.資格対象者;
 import jp.co.ndensan.reams.db.dbz.business.core.basic.FufukuMoshitate;
 import jp.co.ndensan.reams.db.dbz.service.TaishoshaKey;
+import jp.co.ndensan.reams.ur.urz.definition.message.UrErrorMessages;
 import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
+import jp.co.ndensan.reams.uz.uza.lang.ApplicationException;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.ui.servlets.CommonButtonHolder;
 import jp.co.ndensan.reams.uz.uza.ui.servlets.ViewStateHolder;
 
 /**
@@ -31,6 +36,9 @@ public class SinsaSeikyusyoPanel {
     private static final RString 追加 = new RString("追加");
     private static final RString 修正 = new RString("修正");
     private static final RString 削除 = new RString("削除");
+    private static final RString 発行ボタン = new RString("btnReportPublish");
+    private BenmeiTorokuMeisaiJoho benmeiTorokuMeisaiJoho;
+    private final BenmeiTorokuManager benmeiTorokuManager = BenmeiTorokuManager.createInstance();
 
     /**
      * 審査請求書登録_一覧情報。
@@ -51,7 +59,17 @@ public class SinsaSeikyusyoPanel {
         getHandler(div).onLoad(sinsaSeikyusyoJohoList);
         return createResponse(div);
     }
-
+    
+    /**
+     * 審査請求書登録_一覧情報_再表示。
+     * 
+     * @param div　審査請求書登録_一覧情報Div
+     * @return ResponseData<SinsaSeikyusyoPanelDiv>
+     */
+    public ResponseData<SinsaSeikyusyoPanelDiv> onActive(SinsaSeikyusyoPanelDiv div) {
+        return this.onLoad(div);
+    }
+    
     /**
      * 追加ボタン。<br/>
      *
@@ -80,53 +98,60 @@ public class SinsaSeikyusyoPanel {
             ViewStateHolder.put(ViewStateKeys.審査請求届出日,
                     new FlexibleDate(div.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtShinsaSeikyuTodokeYMD().getValue().toDateString()));
         }
+        CommonButtonHolder.setDisabledByCommonButtonFieldName(発行ボタン, true);
         return ResponseData.of(div).respond();
     }
 
     /**
      * 修正ボタン。<br/>
      *
-     * @param sindiv SinsaSeikyusyoPanelDiv
+     * @param div SinsaSeikyusyoPanelDiv
      * @return ResponseData<SinsaSeikyusyoPanelDiv>
      */
-    public ResponseData<SinsaSeikyusyoPanelDiv> onClick_btnShuuSei(SinsaSeikyusyoPanelDiv sindiv) {
+    public ResponseData<SinsaSeikyusyoPanelDiv> onClick_btnShuuSei(SinsaSeikyusyoPanelDiv div) {
         TaishoshaKey key = ViewStateHolder.get(資格対象者, TaishoshaKey.class);
         ViewStateHolder.put(ViewStateKeys.識別コード, key.get識別コード());
         ViewStateHolder.put(ViewStateKeys.被保険者番号, key.get被保険者番号());
         ViewStateHolder.put(ViewStateKeys.状態, 修正);
-        if (sindiv.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtShinsaSeikyuTodokeYMD().getValue() != null) {
+        if (div.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtShinsaSeikyuTodokeYMD().getValue() != null) {
             ViewStateHolder.put(ViewStateKeys.審査請求届出日,
-                    new FlexibleDate(sindiv.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtShinsaSeikyuTodokeYMD().getValue().toDateString()));
+                    new FlexibleDate(div.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtShinsaSeikyuTodokeYMD().getValue().toDateString()));
         }
-        if (sindiv.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtBenmeishoSakuseiYMD().getValue() != null) {
+        if (div.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtBenmeishoSakuseiYMD().getValue() != null) {
             ViewStateHolder.put(ViewStateKeys.弁明書作成日,
-                    new FlexibleDate(sindiv.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtBenmeishoSakuseiYMD().getValue().toDateString()));
+                    new FlexibleDate(div.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtBenmeishoSakuseiYMD().getValue().toDateString()));
         }
-        return ResponseData.of(sindiv).forwardWithEventName(DBU0900011TransitionEventName.登録画面に遷移).respond();
+        return ResponseData.of(div).forwardWithEventName(DBU0900011TransitionEventName.登録画面に遷移).respond();
     }
 
     /**
      * 削除ボタン。<br/>
      *
-     * @param sindiv SinsaSeikyusyoPanelDiv
+     * @param div SinsaSeikyusyoPanelDiv
      * @return ResponseData<SinsaSeikyusyoPanelDiv>
      */
-    public ResponseData<SinsaSeikyusyoPanelDiv> onClick_btnSakuzyo(SinsaSeikyusyoPanelDiv sindiv) {
+    public ResponseData<SinsaSeikyusyoPanelDiv> onClick_btnSakuzyo(SinsaSeikyusyoPanelDiv div) {
         TaishoshaKey key = ViewStateHolder.get(資格対象者, TaishoshaKey.class);
+        benmeiTorokuMeisaiJoho = benmeiTorokuManager.getBenmeiTorokuMeisaiJoho(key.get識別コード(),
+                key.get被保険者番号(),
+                new FlexibleDate(div.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtShinsaSeikyuTodokeYMD().getValue().toDateString()));
+        if (benmeiTorokuMeisaiJoho == null) {
+            throw new ApplicationException(UrErrorMessages.存在しない.getMessage().replace("削除データ"));
+        }
         ViewStateHolder.put(ViewStateKeys.識別コード, key.get識別コード());
         ViewStateHolder.put(ViewStateKeys.被保険者番号, key.get被保険者番号());
         ViewStateHolder.put(ViewStateKeys.状態, 削除);
-        if (sindiv.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtShinsaSeikyuTodokeYMD().getValue() != null) {
+        if (div.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtShinsaSeikyuTodokeYMD().getValue() != null) {
             ViewStateHolder.put(ViewStateKeys.審査請求届出日,
-                    new FlexibleDate(sindiv.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtShinsaSeikyuTodokeYMD().getValue().toDateString()));
+                    new FlexibleDate(div.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtShinsaSeikyuTodokeYMD().getValue().toDateString()));
         }
-        if (sindiv.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtBenmeishoSakuseiYMD().getValue() != null) {
+        if (div.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtBenmeishoSakuseiYMD().getValue() != null) {
             ViewStateHolder.put(ViewStateKeys.弁明書作成日,
-                    new FlexibleDate(sindiv.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtBenmeishoSakuseiYMD().getValue().toDateString()));
+                    new FlexibleDate(div.getGrdSinsaSeikyusyoJoho().getActiveRow().getTxtBenmeishoSakuseiYMD().getValue().toDateString()));
         }
-        return ResponseData.of(sindiv).forwardWithEventName(DBU0900011TransitionEventName.登録画面に遷移).respond();
+        return ResponseData.of(div).forwardWithEventName(DBU0900011TransitionEventName.登録画面に遷移).respond();
     }
-
+    
     /**
      * 弁明書発行。<br/>
      *
@@ -154,5 +179,5 @@ public class SinsaSeikyusyoPanel {
 
     private ResponseData<SinsaSeikyusyoPanelDiv> createResponse(SinsaSeikyusyoPanelDiv div) {
         return ResponseData.of(div).respond();
-    }
+    }    
 }
