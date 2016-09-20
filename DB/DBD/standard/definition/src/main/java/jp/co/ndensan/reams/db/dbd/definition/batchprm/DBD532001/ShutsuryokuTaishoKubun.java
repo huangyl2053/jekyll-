8104a11d8c177452d83570792101f05761f2c315
huangh <huangh@liandisys.com>
2016-09-20 13:18:09 +0800

    バッチ横展開

--- DB/DBD/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbd/definition/batchprm/DBD532001/ShutsuryokuTaishoKubun.java
+++ DB/DBD/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbd/definition/batchprm/DBD532001/ShutsuryokuTaishoKubun.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbd.definition.batchprm.dbd5320001;
+package jp.co.ndensan.reams.db.dbd.definition.batchprm.DBD532001;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.util.strings.IRStringConvertable;
