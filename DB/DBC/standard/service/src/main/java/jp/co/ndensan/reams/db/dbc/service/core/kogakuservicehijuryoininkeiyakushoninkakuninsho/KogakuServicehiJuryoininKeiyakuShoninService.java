570c936b27ea760e1f630068006fd9dafc8a5029
zhouchuanlin <zhouchuanlin@liandisys.com.cn>
2016-09-21 15:12:31 +0800

    ビジネス設計_DBCMN32003_高額サービス費受領委任契約承認（不承認）確認書作成を変更します。

--- DB/DBC/standard/service/src/main/java/jp/co/ndensan/reams/db/dbc/service/core/kogakuservicehijuryoininkeiyakushoninkakuninsho/KogakuServicehiJuryoininKeiyakuShoninService.java
+++ DB/DBC/standard/service/src/main/java/jp/co/ndensan/reams/db/dbc/service/core/kogakuservicehijuryoininkeiyakushoninkakuninsho/KogakuServicehiJuryoininKeiyakuShoninService.java
@@ -12,6 +12,7 @@ import jp.co.ndensan.reams.db.dbc.entity.db.relate.dbc010020.ServiceEntity;
 import jp.co.ndensan.reams.db.dbc.entity.db.relate.dbc100031.KogakuServiceHiJyuryoItakuKeiyakuKakuninShoEntity;
 import jp.co.ndensan.reams.db.dbc.entity.db.relate.dbc200013.JuryoIninShoninKakuninshoIchiranEntity;
 import jp.co.ndensan.reams.db.dbx.entity.db.basic.DbT7060KaigoJigyoshaEntity;
+import jp.co.ndensan.reams.db.dbx.entity.db.basic.DbT7062KaigoJigyoshaDaihyoshaEntity;
 import jp.co.ndensan.reams.ua.uax.business.core.atesaki.AtesakiFactory;
 import jp.co.ndensan.reams.ua.uax.business.core.atesaki.IAtesaki;
 import jp.co.ndensan.reams.ua.uax.business.report.parts.sofubutsuatesaki.SofubutsuAtesakiSourceBuilder;
@@ -20,6 +21,7 @@ import jp.co.ndensan.reams.ua.uax.business.report.parts.util.atesaki.ReportAtesa
 import jp.co.ndensan.reams.ua.uax.entity.db.basic.UaFt250FindAtesakiEntity;
 import jp.co.ndensan.reams.ur.urz.entity.report.sofubutsuatesaki.SofubutsuAtesakiSource;
 import jp.co.ndensan.reams.uz.uza.biz.AtenaJusho;
+import jp.co.ndensan.reams.uz.uza.biz.AtenaMeisho;
 import jp.co.ndensan.reams.uz.uza.biz.TelNo;
 import jp.co.ndensan.reams.uz.uza.biz.YubinNo;
 import jp.co.ndensan.reams.uz.uza.lang.EraType;
@@ -68,23 +70,32 @@ public class KogakuServicehiJuryoininKeiyakuShoninService {
     private JuryoIninShoninKakuninshoIchiranEntity edit一覧表Entity(KogakuServicehiJuryoininKeiyakuShoninKakuninshoEntity entity, int ページ) {
         JuryoIninShoninKakuninshoIchiranEntity 一覧表Entity = new JuryoIninShoninKakuninshoIchiranEntity();
         DbT7060KaigoJigyoshaEntity 介護事業者 = entity.get介護事業者();
-        AtenaJusho jigyoshaAddress = 介護事業者.getJigyoshaAddress();
+        AtenaJusho jigyoshaAddress = new AtenaJusho(RString.EMPTY);
+        AtenaMeisho 事業者名称 = new AtenaMeisho(RString.EMPTY);
+        if (介護事業者 != null) {
+            jigyoshaAddress = 介護事業者.getJigyoshaAddress();
+            事業者名称 = 介護事業者.getJigyoshaName();
+        }
         一覧表Entity.set連番(new RString(ページ));
-        if (entity.get高額介護事業者().getHihokenshaNo() != null) {
+        if (entity.get高額介護事業者().getHihokenshaNo() != null && !entity.get高額介護事業者().getHihokenshaNo().isEmpty()) {
             一覧表Entity.set被保険者番号(entity.get高額介護事業者().getHihokenshaNo().value());
         }
-        if (entity.get名称() != null) {
+        if (entity.get名称() != null && !entity.get名称().isEmpty()) {
             一覧表Entity.set被保険者氏名1(subString(entity.get名称().value(), ONE, FIFTEEN));
         }
-        if (jigyoshaAddress != null) {
+        if (jigyoshaAddress != null && !jigyoshaAddress.isEmpty()) {
             一覧表Entity.set事業者所在地(subString(jigyoshaAddress.value(), ONE, THIRTY_SIX));
         }
         一覧表Entity.set受付日(do日付編集パターン_4(entity.get高額介護事業者().getUketsukeYMD()));
         一覧表Entity.set承認開始日(do日付編集パターン_4(entity.get高額介護事業者().getJuryoininKaishiYMD()));
         一覧表Entity.set承認終了日(do日付編集パターン_4(entity.get高額介護事業者().getJuryoininShuryoYMD()));
         一覧表Entity.set承認終区分(ShoninKubun.toValue(entity.get高額介護事業者().getShoninKekkaKubun()).get名称());
-        一覧表Entity.set被保険者氏名2(subString(entity.get名称().value(), SIXTEEN, THIRTY));
-        一覧表Entity.set事業者名称(subString(介護事業者.getJigyoshaName().value(), ONE, THIRTY_SIX));
+        if (entity.get名称() != null && !entity.get名称().isEmpty()) {
+            一覧表Entity.set被保険者氏名2(subString(entity.get名称().value(), SIXTEEN, THIRTY));
+        }
+        if (事業者名称 != null && !事業者名称.isEmpty()) {
+            一覧表Entity.set事業者名称(subString(事業者名称.value(), ONE, THIRTY_SIX));
+        }
         一覧表Entity.set承認日(do日付編集パターン_4(entity.get高額介護事業者().getKetteiYMD()));
         一覧表Entity.set利用サービス内容(利用サービス内容);
         return 一覧表Entity;
@@ -95,6 +106,21 @@ public class KogakuServicehiJuryoininKeiyakuShoninService {
         KogakuServiceHiJyuryoItakuKeiyakuKakuninShoEntity 介護保険高額Entity = new KogakuServiceHiJyuryoItakuKeiyakuKakuninShoEntity();
         DbT7060KaigoJigyoshaEntity 介護事業者 = entity.get介護事業者();
         DbT3076KogakuJuryoininKeiyakuJigyoshaEntity 高額介護事業者 = entity.get高額介護事業者();
+        DbT7062KaigoJigyoshaDaihyoshaEntity 介護事業者代表者 = entity.get介護事業者代表者();
+        AtenaMeisho 事業者名称 = new AtenaMeisho(RString.EMPTY);
+        YubinNo yubinNo = new YubinNo(RString.EMPTY);
+        AtenaJusho jigyoshaAddress = new AtenaJusho(RString.EMPTY);
+        TelNo telNo = new TelNo(RString.EMPTY);
+        AtenaMeisho 代表者名 = new AtenaMeisho(RString.EMPTY);
+        if (介護事業者 != null) {
+            事業者名称 = 介護事業者.getJigyoshaName();
+            yubinNo = 介護事業者.getYubinNo();
+            jigyoshaAddress = 介護事業者.getJigyoshaAddress();
+            telNo = 介護事業者.getTelNo();
+        }
+        if (介護事業者代表者 != null) {
+            代表者名 = 介護事業者代表者.getDaihyoshaShimei();
+        }
         介護保険高額Entity.set郵便番号(宛先Source.yubinNo);
         介護保険高額Entity.set行政区(宛先Source.gyoseiku);
         介護保険高額Entity.set住所Text(宛先Source.jushoText);
@@ -125,9 +151,15 @@ public class KogakuServicehiJuryoininKeiyakuShoninService {
         介護保険高額Entity.set括弧Right1(宛先Source.kakkoRight1);
         介護保険高額Entity.set括弧Right2(宛先Source.kakkoRight2);
         介護保険高額Entity.setカスタマバーコード(宛先Source.customerBarCode);
-        介護保険高額Entity.set被保険者氏名カナ(entity.getカナ名称().value());
-        介護保険高額Entity.set被保険者氏名(entity.get名称().value());
-        介護保険高額Entity.set被保険者番号(高額介護事業者.getHihokenshaNo().value());
+        if (entity.getカナ名称() != null && !entity.getカナ名称().isEmpty()) {
+            介護保険高額Entity.set被保険者氏名カナ(entity.getカナ名称().value());
+        }
+        if (entity.get名称() != null && !entity.get名称().isEmpty()) {
+            介護保険高額Entity.set被保険者氏名(entity.get名称().value());
+        }
+        if (高額介護事業者.getHihokenshaNo() != null && !高額介護事業者.getHihokenshaNo().isEmpty()) {
+            介護保険高額Entity.set被保険者番号(高額介護事業者.getHihokenshaNo().value());
+        }
         介護保険高額Entity.set受付日(
                 do日付編集パターン_12(高額介護事業者.getUketsukeYMD()));
         介護保険高額Entity.set承認区分(
@@ -137,18 +169,19 @@ public class KogakuServicehiJuryoininKeiyakuShoninService {
         if (ShoninKubun.承認しない.getコード().equals(ShoninKubun.toValue(高額介護事業者.getShoninKekkaKubun()).getコード())) {
             介護保険高額Entity.set不承認理由(高額介護事業者.getFuShoninRiyu());
         }
-        介護保険高額Entity.set事業所名(介護事業者.getJigyoshaName().value());
-        介護保険高額Entity.set代表者名(entity.get介護事業者代表者().getDaihyoshaShimei().value());
-        YubinNo yubinNo = 介護事業者.getYubinNo();
-        AtenaJusho jigyoshaAddress = 介護事業者.getJigyoshaAddress();
-        TelNo telNo = 介護事業者.getTelNo();
-        if (null != yubinNo) {
+        if (事業者名称 != null && !事業者名称.isEmpty()) {
+            介護保険高額Entity.set事業所名(事業者名称.value());
+        }
+        if (代表者名 != null && !代表者名.isEmpty()) {
+            介護保険高額Entity.set代表者名(代表者名.value());
+        }
+        if (null != yubinNo && !yubinNo.isEmpty()) {
             介護保険高額Entity.set事業所郵便番号(yubinNo.value());
         }
-        if (null != jigyoshaAddress) {
+        if (null != jigyoshaAddress && !jigyoshaAddress.isEmpty()) {
             介護保険高額Entity.set事業所所在地(jigyoshaAddress.value());
         }
-        if (null != telNo) {
+        if (null != telNo && !telNo.isEmpty()) {
             介護保険高額Entity.set事業所電話番号(telNo.value());
         }
         Decimal 利用者負担上限額 = 高額介護事業者.getRiyoshaFutanJogenGaku();
