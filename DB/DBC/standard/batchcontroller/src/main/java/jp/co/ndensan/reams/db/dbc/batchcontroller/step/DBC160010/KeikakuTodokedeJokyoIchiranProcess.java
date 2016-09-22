259fbc4cd83a0e70c08988672214c979cf7a3c77
jianglaisheng <jianglaisheng@liandisys.com.cn>
2016-09-22 16:20:27 +0800

    バッチ関連展開

--- DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/step/DBC160010/KeikakuTodokedeJokyoIchiranProcess.java
+++ DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/step/DBC160010/KeikakuTodokedeJokyoIchiranProcess.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbc.batchcontroller.step.dbc160010;
+package jp.co.ndensan.reams.db.dbc.batchcontroller.step.DBC160010;
 
 import java.util.ArrayList;
 import java.util.List;
