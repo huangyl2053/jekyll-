e9c25ef6d46c714fb26ba01f84e7b3e0adf25422
jiangxiaolong <jiangxiaolong@liandisys.com.cn>
2016-09-22 10:51:41 +0800

    バッチ設計_DBCBT23016_汎用リスト出力(高額合算計算結果連絡票情報) check-style 対応

--- DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/core/hanyolistkogakugassankeisan/HanyoListKogakuGassanOutPutOrder.java
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/core/hanyolistkogakugassankeisan/HanyoListKogakuGassanOutPutOrder.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbc.business.core.hanyolistKogakugassan;
+package jp.co.ndensan.reams.db.dbc.business.core.hanyolistkogakugassankeisan;
 
 import jp.co.ndensan.reams.ur.urz.business.core.reportoutputorder.IReportItems;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
