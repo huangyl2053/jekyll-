925a3112b5b0dc767c44623ded9e355ba9b380eb
lizhuoxuan <lizhuoxuan@liandisys.com.cn>
2016-09-23 17:37:43 +0800

    帳票設計_DBC100041_介護保険給付費通知書(２ページ目以降)のEntityはpackageを修正

--- DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/db/relate/kyufuhitsuchishodetail/KyufuhiTsuchishoDetailEntity.java
+++ DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/db/relate/kyufuhitsuchishodetail/KyufuhiTsuchishoDetailEntity.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbc.entity.db.kyufuhitsuchishodetail;
+package jp.co.ndensan.reams.db.dbc.entity.db.relate.kyufuhitsuchishodetail;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 
