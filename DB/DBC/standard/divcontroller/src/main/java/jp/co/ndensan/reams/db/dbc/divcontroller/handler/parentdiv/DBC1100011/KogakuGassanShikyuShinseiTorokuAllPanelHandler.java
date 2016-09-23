621325a70771417a735d68e56a56b2efe9170db2
huzongcheng <huzongcheng@liandisys.com.cn>
2016-09-23 12:52:03 +0800

    画面設計_DBCMN61001-12_高額合算支給申請書登録 修正

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/parentdiv/DBC1100011/KogakuGassanShikyuShinseiTorokuAllPanelHandler.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/parentdiv/DBC1100011/KogakuGassanShikyuShinseiTorokuAllPanelHandler.java
@@ -628,7 +628,7 @@ public class KogakuGassanShikyuShinseiTorokuAllPanelHandler {
         } else if (削除.equals(高額合算申請書状態)) {
             if (追加.equals(状態)) {
                 高額合算申請書保持.delete高額合算申請書(高額合算申請書);
-            } else if (RString.isNullOrEmpty(状態) && || 修正.equals(状態)) {
+            } else if (RString.isNullOrEmpty(状態) || 修正.equals(状態)) {
                 KogakuGassanShinseisho 高額合算申請書Result = 高額合算申請書.get高額合算申請書();
                 高額合算申請書Result = 高額合算申請書Result.deleted();
                 高額合算申請書.set高額合算申請書(高額合算申請書Result);
