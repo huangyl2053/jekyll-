103b187718acc684cb6e242448f14b16b214780f
wangxingpeng <wangxingpeng@liandisys.com.cn>
2016-09-23 16:39:26 +0800

    画面設計_DBC1000065_基準収入額適用申請書_異動分作成(checkstyle対応)

--- DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/batchprm/DBC190030/DBC190030_KijunsyunygetsujiParameter.java
+++ DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/batchprm/DBC190030/DBC190030_KijunsyunygetsujiParameter.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbc.definition.batchprm.kijunshunyugakutekiyoshinseishohakkoichiran;
+package jp.co.ndensan.reams.db.dbc.definition.batchprm.DBC190030;
 
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
 import jp.co.ndensan.reams.uz.uza.batch.BatchParameter;
