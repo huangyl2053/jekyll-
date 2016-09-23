979b25c4dadb4cf8aaeb7f1851a23ec802739dce
dingyi <dingyi@liandisys.com.cn>
2016-09-23 17:42:45 +0800

    ビジネス設計_DBCMN11002_給付実績照会　新規作成

---
+++ DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/db/relate/kyufujissekishokai/KyufujissekiServiceShuruiRyakushoRelateEntity.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.entity.db.relate.kyufujissekishokai;
+
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+/**
+ * サービス種類略称Entityクラスです。
+ *
+ * @reamsid_L DBC-2970-170 dingyi
+ */
+@lombok.Setter
+@lombok.Getter
+@SuppressWarnings("PMD.UnusedPrivateField")
+public class KyufujissekiServiceShuruiRyakushoRelateEntity {
+
+    private RString serviceShuruiRyakusho;
+}
