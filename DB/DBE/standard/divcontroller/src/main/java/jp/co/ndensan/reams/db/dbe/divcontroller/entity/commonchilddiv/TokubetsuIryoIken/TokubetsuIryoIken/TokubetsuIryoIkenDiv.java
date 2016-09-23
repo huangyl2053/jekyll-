257214a19edde83dc850e520461a2c4c6b83ac42
茅野健太郎 <n3415@ndensan.co.jp>
2016-09-23 19:27:36 +0900

    実装課題 #101762 【不具合】DBE_0330_DBEMNA1005_完了処理・主治医意見書入手 特別な医療ダイアログについて 正しくエラーメッセージが表示されるように修正しました。

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/commonchilddiv/TokubetsuIryoIken/TokubetsuIryoIken/TokubetsuIryoIkenDiv.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/commonchilddiv/TokubetsuIryoIken/TokubetsuIryoIken/TokubetsuIryoIkenDiv.java
@@ -18,7 +18,7 @@ import jp.co.ndensan.reams.uz.uza.ui.binding.IDialogDiv;
  * @author 自動生成
  */
 public class TokubetsuIryoIkenDiv extends Panel implements ITokubetsuIryoIkenDiv {
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-03-22_14-06-37">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-07-28_11-34-20">
     /*
      * [ private の作成 ]
      * クライアント側から取得した情報を元にを検索を行い
