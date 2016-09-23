9ad4e6c8347bbfdb7cb97e45a5fe7822a05ebb90
chengsanyuan <chengsanyuan@liandisys.com.cn>
2016-09-23 11:25:52 +0800

    業務概念テーブル4123、4201、4202、4301、4302を追加

---
+++ DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/core/basic/DbT4301ShujiiIkenshoIraiJohoIdentifier.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbz.business.core.basic;
+
+import java.io.Serializable;
+import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShinseishoKanriNo;
+
+/**
+ * 主治医意見書作成依頼情報の識別子です。
+ *
+ * @reamsid_L DBZ-9999-011 chengsanyuan
+ */
+@SuppressWarnings("PMD.UnusedPrivateField")
+@lombok.Value
+public class DbT4301ShujiiIkenshoIraiJohoIdentifier implements Serializable {
+
+    private final ShinseishoKanriNo 申請書管理番号;
+    private final int 主治医意見書作成依頼履歴番号;
+
+    /**
+     * コンストラクタです。
+     *
+     * @param 申請書管理番号 申請書管理番号
+     * @param 主治医意見書作成依頼履歴番号 主治医意見書作成依頼履歴番号
+     */
+    public DbT4301ShujiiIkenshoIraiJohoIdentifier(ShinseishoKanriNo 申請書管理番号,
+            int 主治医意見書作成依頼履歴番号) {
+        this.申請書管理番号 = 申請書管理番号;
+        this.主治医意見書作成依頼履歴番号 = 主治医意見書作成依頼履歴番号;
+    }
+}
