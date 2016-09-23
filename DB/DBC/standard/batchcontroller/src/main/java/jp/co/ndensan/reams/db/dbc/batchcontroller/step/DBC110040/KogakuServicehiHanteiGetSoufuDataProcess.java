445a5d6b00e8aeb143e1c564eec1a2a83da31b30
liuhui <liuhui@liandisys.com.cn>
2016-09-23 10:14:06 +0800

    バッチ設計_DBCMNF1001_341_高額サービス費給付判定結果情報作成 横展開対応

--- DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/step/DBC110040/KogakuServicehiHanteiGetSoufuDataProcess.java
+++ DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/step/DBC110040/KogakuServicehiHanteiGetSoufuDataProcess.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbc.batchcontroller.step.dbc110040;
+package jp.co.ndensan.reams.db.dbc.batchcontroller.step.DBC110040;
 
 import jp.co.ndensan.reams.db.dbc.definition.processprm.kogakuservicehihanteikekkaout.KogakuServicehiHanteiMybatisParameter;
 import jp.co.ndensan.reams.db.dbc.definition.processprm.kogakuservicehihanteikekkaout.KogakuServicehiHanteiProcessParameter;
