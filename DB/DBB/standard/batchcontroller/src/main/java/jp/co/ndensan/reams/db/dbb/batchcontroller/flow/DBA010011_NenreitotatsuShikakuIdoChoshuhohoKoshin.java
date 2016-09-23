74d4a0570abe7794100639aa3cd913c92d5f9df1
xuhao <xuhao@liandisys.com.cn>
2016-09-23 17:34:38 +0800

    batchProcess类バッグ名の修正

--- DB/DBB/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/batchcontroller/flow/DBA010011_NenreitotatsuShikakuIdoChoshuhohoKoshin.java
+++ DB/DBB/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/batchcontroller/flow/DBA010011_NenreitotatsuShikakuIdoChoshuhohoKoshin.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbb.batchcontroller.flow.dba010011;
+package jp.co.ndensan.reams.db.dbb.batchcontroller.flow;
 
-import jp.co.ndensan.reams.db.dbb.batchcontroller.step.dba010011.ShikakuIdoChoshuhohoKoshinHihokenjaShutokuProcess;
+import jp.co.ndensan.reams.db.dbb.batchcontroller.step.DBA010011.ShikakuIdoChoshuhohoKoshinHihokenjaShutokuProcess;
 import jp.co.ndensan.reams.uz.uza.batch.Step;
 import jp.co.ndensan.reams.uz.uza.batch.flow.BatchFlowBase;
 import jp.co.ndensan.reams.uz.uza.batch.flow.IBatchFlowCommand;
--- DB/DBB/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/batchcontroller/step/DBA010011/ShikakuIdoChoshuhohoKoshinHihokenjaShutokuProcess.java
+++ DB/DBB/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/batchcontroller/step/DBA010011/ShikakuIdoChoshuhohoKoshinHihokenjaShutokuProcess.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbb.batchcontroller.step.dba010011;
+package jp.co.ndensan.reams.db.dbb.batchcontroller.step.DBA010011;
 
 import java.util.List;
 import jp.co.ndensan.reams.db.dbb.service.core.nenreitotatsushikakuidochoshuhohokoshin.NenreitotatsuShikakuIdoChoshuhohoKoshin;
