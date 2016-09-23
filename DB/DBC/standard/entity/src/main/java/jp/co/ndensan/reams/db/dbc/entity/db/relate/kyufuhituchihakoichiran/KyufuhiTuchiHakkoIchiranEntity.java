ba381e512afa17d8c0359b16fb992c0ca5d8f317
lizhuoxuan <lizhuoxuan@liandisys.com.cn>
2016-09-23 18:17:34 +0800

    帳票設計_DBC200044_給付費通知発行一覧表のEntityはpackageを修正

--- DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/db/relate/kyufuhituchihakoichiran/KyufuhiTuchiHakkoIchiranEntity.java
+++ DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/db/relate/kyufuhituchihakoichiran/KyufuhiTuchiHakkoIchiranEntity.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbc.entity.db.kyufuhituchihakkoichiran;
+package jp.co.ndensan.reams.db.dbc.entity.db.relate.kyufuhituchihakoichiran;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 
