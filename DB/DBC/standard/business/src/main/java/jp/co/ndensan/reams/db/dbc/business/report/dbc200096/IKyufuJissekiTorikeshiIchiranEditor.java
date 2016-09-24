4736ccb67ae5143007316ede259e58472b20ba4f
jiangxiaolong <jiangxiaolong@liandisys.com.cn>
2016-09-24 17:54:09 +0800

    ソースを提出する

---
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/dbc200096/IKyufuJissekiTorikeshiIchiranEditor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.business.report.dbc200096;
+
+import jp.co.ndensan.reams.db.dbc.entity.report.dbc200096.KyufuJissekiTorikeshiIchiranSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportEditor;
+
+/**
+ * 帳票設計_DBC200096_給付実績取消一覧表IEditor
+ *
+ * @reamsid_L DBC-4960-050 jiangxiaolong
+ */
+public interface IKyufuJissekiTorikeshiIchiranEditor extends
+        IReportEditor<KyufuJissekiTorikeshiIchiranSource> {
+
+}
