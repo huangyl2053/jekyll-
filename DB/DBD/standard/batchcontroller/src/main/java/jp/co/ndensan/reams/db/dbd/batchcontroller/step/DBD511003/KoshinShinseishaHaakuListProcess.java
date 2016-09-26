9b58b7c506ec0b3b250ebf96d5b36fc1da22093b
wangjie2 <wangjie2@liandisys.com.cn>
2016-09-26 10:03:25 +0800

    更新未申請者把握リスト作成　バッチ関連展開

--- DB/DBD/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/batchcontroller/step/DBD511003/KoshinShinseishaHaakuListProcess.java
+++ DB/DBD/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/batchcontroller/step/DBD511003/KoshinShinseishaHaakuListProcess.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbd.batchcontroller.step.dbd5110001;
+package jp.co.ndensan.reams.db.dbd.batchcontroller.step.DBD511003;
 
 import java.util.ArrayList;
 import java.util.List;
