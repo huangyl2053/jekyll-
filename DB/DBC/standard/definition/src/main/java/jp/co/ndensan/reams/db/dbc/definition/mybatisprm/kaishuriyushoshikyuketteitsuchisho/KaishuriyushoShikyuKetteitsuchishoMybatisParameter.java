57aa2f7ad580bc1aa173e34d474d3cc1492fa37a
chenxiangyu <chenxiangyu@liandisys.com.cn>
2016-09-27 23:50:39 +0800

    「画面設計_DBCMNE2005_住宅改修理由書作成手数料支給（不支給）決定通知書作成」のレビュー 指摘を対応します。

--- DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/mybatisprm/kaishuriyushoshikyuketteitsuchisho/KaishuriyushoShikyuKetteitsuchishoMybatisParameter.java
+++ DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/mybatisprm/kaishuriyushoshikyuketteitsuchisho/KaishuriyushoShikyuKetteitsuchishoMybatisParameter.java
@@ -21,19 +21,4 @@ public class KaishuriyushoShikyuKetteitsuchishoMybatisParameter implements IMyBa
     private RString 市町村コード;
     private final RString サブ業務コード = new RString("DBC");
     private final RString 処理名 = new RString("住宅改修理由書作成手数料支給（不支給）決定通知書作成");
-
-    private KaishuriyushoShikyuKetteitsuchishoMybatisParameter(RString 市町村コード) {
-        this.市町村コード = 市町村コード;
-    }
-
-    /**
-     * 前回の実行情報取得のパラメータを作成します。
-     *
-     * @param 市町村コード 市町村コード
-     * @return KaishuriyushoShikyuKetteitsuchishoMybatisParameter
-     */
-    public static KaishuriyushoShikyuKetteitsuchishoMybatisParameter create前回の実行情報取得パラメータ(RString 市町村コード) {
-        return new KaishuriyushoShikyuKetteitsuchishoMybatisParameter(市町村コード);
-    }
-
 }
