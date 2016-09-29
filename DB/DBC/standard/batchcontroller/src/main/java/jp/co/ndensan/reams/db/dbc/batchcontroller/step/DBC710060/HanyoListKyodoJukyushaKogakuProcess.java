4ecf6f9fc2eb6dae9e0a09ead7c2ef7b4920d061
zhouchuanlin <zhouchuanlin@liandisys.com.cn>
2016-09-29 12:53:38 +0800

    バッチ設計_DBCBT23006_汎用リスト 共同処理用受給者情報（高額）のバッグを対応します。

--- DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/step/DBC710060/HanyoListKyodoJukyushaKogakuProcess.java
+++ DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/step/DBC710060/HanyoListKyodoJukyushaKogakuProcess.java
@@ -165,6 +165,7 @@ public class HanyoListKyodoJukyushaKogakuProcess extends BatchProcessBase<HanyoL
 
     @Override
     protected void afterExecute() {
+        long count;
         if (定数_なし.equals(csv出力Flag) && parameter.is項目名付加()) {
             List<RString> bodyList = new ArrayList<>();
             for (int i = 0; i < edit.setHeaderList(parameter).size(); i++) {
@@ -173,13 +174,18 @@ public class HanyoListKyodoJukyushaKogakuProcess extends BatchProcessBase<HanyoL
             csvListWriter.writeLine(bodyList);
         }
         csvListWriter.close();
+        if (なし.equals(csv出力Flag)) {
+            count = 0;
+        } else {
+            count = csvListWriter.getCount();
+        }
         if (!personalDataList.isEmpty()) {
             AccessLogUUID accessLogUUID = AccessLogger.logEUC(UzUDE0835SpoolOutputType.EucOther, personalDataList);
             spoolManager.spool(eucFilePath, accessLogUUID);
         } else {
             spoolManager.spool(eucFilePath);
         }
-        RString csvCount = new RString(csvListWriter.getCount());
+        RString csvCount = new RString(count);
         ReportOutputJokenhyoItem reportOutputJokenhyoItem = new ReportOutputJokenhyoItem(
                 EUC_ENTITY_ID.toRString(),
                 地方公共団体情報.getLasdecCode_().value(),
