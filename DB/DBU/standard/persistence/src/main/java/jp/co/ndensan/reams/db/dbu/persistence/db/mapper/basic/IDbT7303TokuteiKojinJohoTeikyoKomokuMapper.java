45d3d2dcb8ce9e1e775b0c1e7fcd63d441d23b2e
sunhadi <sunhaidi@liandisys.com.cn>
2016-09-30 14:39:35 +0800

    特定個人情報テーブルのentityとmapperを作成

---
+++ DB/DBU/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbu/persistence/db/mapper/basic/IDbT7303TokuteiKojinJohoTeikyoKomokuMapper.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbu.persistence.db.mapper.basic;
+
+/**
+ * {@link jp.co.ndensan.reams.db.dbu.entity.db.basic.DbT7303TokuteiKojinJohoTeikyoKomokuEntity}のマッパーIFです。
+ *
+ * @author @reamsid_L DBU- 9999-021 sunhaidi
+ */
+public interface IDbT7303TokuteiKojinJohoTeikyoKomokuMapper {
+
+}
