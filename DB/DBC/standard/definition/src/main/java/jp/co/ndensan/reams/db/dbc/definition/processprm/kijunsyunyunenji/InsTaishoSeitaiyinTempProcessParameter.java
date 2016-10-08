18f34c529e16a3744294800d37712245a1d45d91
jiangwenkai <jiangwenkai@liandisys.com.cn>
2016-10-08 15:50:18 +0800

    バッチ設計_DBC110064_基準収入額適用申請書_年次分作成 ビジネス設計_DBC110064_基準収入額適用申請書_年次分作成_マッチング ビジネス設計_DBC110064_基準収入額適用申請書_年次分作成_対象世帯員 source commit

---
+++ DB/DBC/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbc/definition/processprm/kijunsyunyunenji/InsTaishoSeitaiyinTempProcessParameter.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.definition.processprm.kijunsyunyunenji;
+
+import jp.co.ndensan.reams.uz.uza.batch.parameter.IBatchProcessParameter;
+import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleYear;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 基準収入額適用申請書_年次分作成のInsTaishoSeitaiyinTempProcessのParameterです
+ *
+ * @reamsid_L DBC-4630-060 jiangwenkai
+ */
+@SuppressWarnings("PMD.UnusedPrivateField")
+@Getter
+@Setter
+public class InsTaishoSeitaiyinTempProcessParameter implements IBatchProcessParameter {
+
+    private RString 処理区分;
+    private RString 出力順ID;
+    private FlexibleYear 処理年度;
+    private FlexibleDate 世帯員把握基準日;
+    private FlexibleDate 世帯員把握基準日2;
+    private RString 抽出条件;
+    private FlexibleDate 作成日;
+    private RString 文書番号;
+    private LasdecCode 市町村コード;
+    private RString 市町村名;
+    private Boolean お知らせ通知書出力フラグ;
+    private Boolean 一覧表CSV出力フラグ;
+    private FlexibleDate 処理年月日;
+
+}
---
+++ DB/DBC/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbc/persistence/db/mapper/relate/kijunsyunyunenji/IKijunsyunyunenjiMapper.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.persistence.db.mapper.relate.kijunsyunyunenji;
+
+import java.util.List;
+import jp.co.ndensan.reams.db.dbc.definition.mybatisprm.kijunsyunyunenji.CreateTaishoSetaiyinProcessMybatisParameter;
+import jp.co.ndensan.reams.db.dbc.definition.mybatisprm.kijunsyunyunenji.InsTaishoSeitaiyinTempProcessMybatisParameter;
+import jp.co.ndensan.reams.db.dbc.definition.mybatisprm.kijunsyunyunenji.UpdTaishoSeitaiyinTemp5ProcessMybatisParameter;
+import jp.co.ndensan.reams.db.dbc.entity.csv.kijunsyunyunenji.CreateTaishoSetaiyinEntity;
+import jp.co.ndensan.reams.db.dbc.entity.csv.kijunsyunyunenji.HihokenshaDaichoTempSixColumnEntity;
+import jp.co.ndensan.reams.db.dbc.entity.csv.kijunsyunyunenji.InsTaishoSeitaiyinTempEntity;
+import jp.co.ndensan.reams.db.dbc.entity.csv.kijunsyunyunenji.TaishoSetaiinEntity;
+import jp.co.ndensan.reams.db.dbc.entity.csv.kijunsyunyunenji.UpdTaishoSeitaiyinTemp1Entity;
+import jp.co.ndensan.reams.db.dbc.entity.csv.kijunsyunyunenji.UpdTaishoSeitaiyinTemp2Entity;
+import jp.co.ndensan.reams.db.dbc.entity.csv.kijunsyunyunenji.UpdTaishoSeitaiyinTemp3Entity;
+import jp.co.ndensan.reams.db.dbc.entity.csv.kijunsyunyunenji.UpdTaishoSeitaiyinTemp4Entity;
+import jp.co.ndensan.reams.db.dbc.entity.db.relate.riyoshafutanwariaihantei.temptables.SetaiShotokuEntity;
+import jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT7022ShoriDateKanriEntity;
+
+/**
+ * 基準収入額適用申請書_年次分作成のMapperです
+ *
+ * @reamsid_L DBC-4630-030 jiangwenkai
+ */
+public interface IKijunsyunyunenjiMapper {
+
+    /**
+     * 処理年度の対象者を取得する。
+     *
+     * @return 処理年度の対象者LのList
+     */
+    List<HihokenshaDaichoTempSixColumnEntity> select処理年度の対象者();
+
+    /**
+     * 被保険者番号指定の対象者を取得する。
+     *
+     * @return 被保険者番号指定度の対象者LのList
+     */
+    List<HihokenshaDaichoTempSixColumnEntity> select被保険者番号指定の対象者();
+
+    /**
+     * 世帯員所得情報一時テーブルをupdateする。
+     */
+    void update世帯員所得情報一時テーブル();
+
+    /**
+     * 被保険者番号指定の対象者を取得する。
+     *
+     * @return SetaiShotokuEntityLのList
+     */
+    List<SetaiShotokuEntity> delete世帯員所得情報一時テーブル();
+
+    /**
+     * 対象世帯員クラスTempに登録。
+     *
+     * @param para InsTaishoSeitaiyinTempProcessMybatisParameter
+     * @return InsTaishoSeitaiyinTempEntityのList
+     */
+    List<InsTaishoSeitaiyinTempEntity> 対象世帯員クラスTempに登録(InsTaishoSeitaiyinTempProcessMybatisParameter para);
+
+    /**
+     * 対象世帯員クラスTempに更新1。
+     *
+     * @return UpdTaishoSeitaiyinTemp1EntityのList
+     */
+    List<UpdTaishoSeitaiyinTemp1Entity> 対象世帯員クラスTempに更新1();
+
+    /**
+     * 対象世帯員クラスTempに更新2。
+     *
+     * @return UpdTaishoSeitaiyinTemp2EntityのList
+     */
+    List<UpdTaishoSeitaiyinTemp2Entity> 対象世帯員クラスTempに更新2();
+
+    /**
+     * 対象世帯員クラスTempに更新3。
+     *
+     * @return UpdTaishoSeitaiyinTemp3EntityのList
+     */
+    List<UpdTaishoSeitaiyinTemp3Entity> 対象世帯員クラスTempに更新3();
+
+    /**
+     * 世帯員クラスTempをdeleteする。
+     */
+    void delete対象世帯員クラスTemp();
+
+    /**
+     * 世帯員所得情報一時テーブルをdeleteする。
+     */
+    void deleteAll世帯員所得情報一時テーブル();
+
+    /**
+     * 処理年度の対象者2を取得する。
+     *
+     * @return 処理年度の対象者LのList
+     */
+    List<HihokenshaDaichoTempSixColumnEntity> select処理年度の対象者2();
+
+    /**
+     * 被保険者番号指定の対象者2を取得する。
+     *
+     * @return 被保険者番号指定度の対象者LのList
+     */
+    List<HihokenshaDaichoTempSixColumnEntity> select被保険者番号指定の対象者2();
+
+    /**
+     * 対象世帯員クラスTempに更新4。
+     *
+     * @return UpdTaishoSeitaiyinTemp4EntityのList
+     */
+    List<UpdTaishoSeitaiyinTemp4Entity> 対象世帯員クラスTempに更新4();
+
+    /**
+     * 対象世帯員クラスTempに更新5。
+     *
+     * @return TaishoSetaiinEntityのList
+     */
+    List<TaishoSetaiinEntity> 対象世帯員クラスTempに更新5();
+
+    /**
+     * 対象世帯員クラスTempに更新5。
+     *
+     * @param para UpdTaishoSeitaiyinTemp5ProcessMybatisParameter
+     * @return 件数 int
+     */
+    int get該当件数(UpdTaishoSeitaiyinTemp5ProcessMybatisParameter para);
+
+    /**
+     * 宛先宛名に取得する。
+     *
+     * @param para CreateTaishoSetaiyinProcessMybatisParameter
+     * @return CreateTaishoSetaiyinEntityのList
+     */
+    List<CreateTaishoSetaiyinEntity> get宛先宛名(CreateTaishoSetaiyinProcessMybatisParameter para);
+
+    /**
+     * 対象世帯員する。
+     *
+     * @return TaishoSetaiinEntityのList
+     */
+    List<TaishoSetaiinEntity> insert基準収入額適用管理();
+
+    /**
+     * 処理日付管理マスタに取得する。
+     *
+     * @param para UpdTaishoSeitaiyinTemp5ProcessMybatisParameter
+     * @return 件数 int
+     */
+    List<DbT7022ShoriDateKanriEntity> select処理日付管理マスタ(UpdTaishoSeitaiyinTemp5ProcessMybatisParameter para);
+
+    /**
+     * 対象世帯員クラスTempに更新。
+     *
+     * @param para UpdTaishoSeitaiyinTemp5ProcessMybatisParameter
+     */
+    void update処理日付管理マスタ(UpdTaishoSeitaiyinTemp5ProcessMybatisParameter para);
+
+    /**
+     * 対象世帯員クラスTempにinsert。
+     *
+     * @param para UpdTaishoSeitaiyinTemp5ProcessMybatisParameter
+     */
+    void insert処理日付管理マスタ(UpdTaishoSeitaiyinTemp5ProcessMybatisParameter para);
+}
