bf590ca14dda1cb45972db30871ae5764c9a2463
lijunjun <lijunjun@liandisys.com.cn>
2016-10-09 17:20:29 +0800

    バッチ設計_DBBBT51001_所得照会票一括発行(LOOPBATCH改造)

---
+++ DB/DBB/standard/business/src/main/java/jp/co/ndensan/reams/db/dbb/business/report/shotokushokaihyohakkoichiran/ShotokushokaihyoHakkoIchiranOutPutOrder.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbb.business.report.shotokushokaihyohakkoichiran;
+
+import jp.co.ndensan.reams.ur.urz.business.core.reportoutputorder.IReportItems;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+/**
+ * 帳票設計_DBBPR51001_所得照会票発行一覧表の出力順クラスです。
+ *
+ * @reamsid_L DBB-1720-040 lijunjun
+ */
+public enum ShotokushokaihyoHakkoIchiranOutPutOrder implements IReportItems {
+
+    /**
+     * 町域コード
+     */
+    町域コード(new RString("0002"), new RString("町域コード"), new RString("\"choikiCode\"")),
+    /**
+     * 行政区コード
+     */
+    行政区コード(new RString("0004"), new RString("行政区コード"), new RString("\"gyoseikuCode\"")),
+    /**
+     * 世帯コード
+     */
+    世帯コード(new RString("0008"), new RString("世帯コード"), new RString("\"setaiCode\"")),
+    /**
+     * 識別コード
+     */
+    識別コード(new RString("0009"), new RString("識別コード"), new RString("\"shikibetsuCode\"")),
+    /**
+     * 氏名５０音カナ
+     */
+    氏名５０音カナ(new RString("0010"), new RString("氏名５０音カナ"), new RString("\"atenaKanaShimei\"")),
+    /**
+     * 生年月日
+     */
+    生年月日(new RString("0012"), new RString("生年月日"), new RString("\"seinengappiYMD\"")),
+    /**
+     * 市町村コード
+     */
+    市町村コード(new RString("0016"), new RString("市町村コード"), new RString("")),
+    /**
+     * 異動年月日
+     */
+    異動年月日(new RString("0017"), new RString("異動年月日"), new RString("\"idoYMD\"")),
+    /**
+     * 前住所コード
+     */
+    前住所コード(new RString("0018"), new RString("前住所コード"), new RString("\"zenJushoCode\""));
+
+    private final RString 項目ID;
+    private final RString フォームフィールド名;
+    private final RString myBatis項目名;
+
+    private ShotokushokaihyoHakkoIchiranOutPutOrder(RString 項目ID, RString フォームフィールド名, RString myBatis項目名) {
+        this.項目ID = 項目ID;
+        this.フォームフィールド名 = フォームフィールド名;
+        this.myBatis項目名 = myBatis項目名;
+    }
+
+    @Override
+    public RString get項目ID() {
+        return 項目ID;
+    }
+
+    @Override
+    public RString getフォームフィールド名() {
+        return フォームフィールド名;
+    }
+
+    @Override
+    public RString getMyBatis項目名() {
+        return myBatis項目名;
+    }
+
+}
---
+++ DB/DBB/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbb/entity/db/relate/shotokushokaihyo/ShotokuShoukaiDataMapbEntity.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbb.entity.db.relate.shotokushokaihyo;
+
+import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HihokenshaNo;
+import jp.co.ndensan.reams.uz.uza.biz.AtenaKanaMeisho;
+import jp.co.ndensan.reams.uz.uza.biz.AtenaMeisho;
+import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
+import jp.co.ndensan.reams.uz.uza.biz.SetaiCode;
+import jp.co.ndensan.reams.uz.uza.biz.ShikibetsuCode;
+import jp.co.ndensan.reams.uz.uza.biz.YubinNo;
+import jp.co.ndensan.reams.uz.uza.biz.ZenkokuJushoCode;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.util.db.annotation.TempTableColumnOrder;
+
+/**
+ * 所得照会票データTempのクラスです。
+ *
+ * @reamsid_L DBB-1720-050 lijunjun
+ */
+@lombok.Getter
+@lombok.Setter
+@SuppressWarnings("PMD.UnusedPrivateField")
+public class ShotokuShoukaiDataMapbEntity {
+
+    private ShikibetsuCode shikibetsuCode;
+    private LasdecCode genLasdecCode;
+    private ZenkokuJushoCode zenkokuJushoCode;
+    private SetaiCode setaiCode;
+    private HihokenshaNo hihokenshaNo;
+    private RString hihokenshaName;
+    private RString hihokenshajusho;
+    private RString zenjusho;
+    private RString genjusho;
+    private AtenaKanaMeisho atenaKanaShimei;
+    private AtenaMeisho atenaShimei;
+    private FlexibleDate seinengappiYMD;
+    private FlexibleDate idoYMD;
+    private RString kouhoshakubun;
+    private RString honninKubun;
+    @TempTableColumnOrder(16)
+    private RString choikiCode;
+    private RString gyoseikuCode;
+    private LasdecCode shichosonCode;
+    private RString zenjushoCode;
+    private YubinNo yubinNo;
+    private RString chikuCode1;
+    private RString chikuCode2;
+    private RString chikuCode3;
+    private RString juminJotaiCode;
+    private RString kannaiKangaiKubun;
+    private RString hihokennshaKubunCode;
+    private RString seibetsuCode;
+    private RString juminShubetsuCode;
+}
