a9ac27c11b74c1b38c40318d2d14ba09a582acdc
dangjingjing <dangjingjing@liandisys.com.cn>
2016-10-09 21:48:00 +0800

    帳票設計_DBU300006_介護事業状況報告月報・一般状況（様式1-5）_新様式　　修正

--- DB/DBU/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbu/entity/report/jigyohokokucompyoshiki152/JigyohokokuCompYoshiki152ReportSource.java
+++ DB/DBU/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbu/entity/report/jigyohokokucompyoshiki152/JigyohokokuCompYoshiki152ReportSource.java
@@ -1,3 +1,4 @@
+// TODO: 業務に合わせてパスを変更してください
 package jp.co.ndensan.reams.db.dbu.entity.report.jigyohokokucompyoshiki152;
 
 import jp.co.ndensan.reams.uz.uza.lang.RString;
@@ -5,7 +6,7 @@ import jp.co.ndensan.reams.uz.uza.report.IReportSource;
 import jp.co.ndensan.reams.uz.uza.report.source.ReportItem;
 
 /**
- * 介護事業状況報告月報・一般状況（様式1-5）_新様式のReportSourceクラスです。
+ * 介護事業状況報告月報・一般状況（様式1-5）_新様式 のReportSourceです。
  *
  * @reamsid_L DBU-5540-050 dangjingjing
  */
@@ -46,31 +47,29 @@ public class JigyohokokuCompYoshiki152ReportSource implements IReportSource {
     public RString list1_10;
     @ReportItem(name = "list1_11", length = 7, order = 17)
     public RString list1_11;
-    @ReportItem(name = "list1_12", length = 7, order = 18)
-    public RString list1_12;
-    @ReportItem(name = "list2_1", length = 7, order = 19)
+    @ReportItem(name = "list2_1", length = 7, order = 18)
     public RString list2_1;
-    @ReportItem(name = "list2_2", length = 7, order = 20)
+    @ReportItem(name = "list2_2", length = 7, order = 19)
     public RString list2_2;
-    @ReportItem(name = "list2_3", length = 7, order = 21)
+    @ReportItem(name = "list2_3", length = 7, order = 20)
     public RString list2_3;
-    @ReportItem(name = "list2_4", length = 7, order = 22)
+    @ReportItem(name = "list2_4", length = 7, order = 21)
     public RString list2_4;
-    @ReportItem(name = "list2_5", length = 7, order = 23)
+    @ReportItem(name = "list2_5", length = 7, order = 22)
     public RString list2_5;
-    @ReportItem(name = "list2_6", length = 7, order = 24)
+    @ReportItem(name = "list2_6", length = 7, order = 23)
     public RString list2_6;
-    @ReportItem(name = "list2_7", length = 7, order = 25)
+    @ReportItem(name = "list2_7", length = 7, order = 24)
     public RString list2_7;
-    @ReportItem(name = "list2_8", length = 7, order = 26)
+    @ReportItem(name = "list2_8", length = 7, order = 25)
     public RString list2_8;
-    @ReportItem(name = "list2_9", length = 7, order = 27)
+    @ReportItem(name = "list2_9", length = 7, order = 26)
     public RString list2_9;
-    @ReportItem(name = "list2_10", length = 7, order = 28)
+    @ReportItem(name = "list2_10", length = 7, order = 27)
     public RString list2_10;
-    @ReportItem(name = "list2_11", length = 7, order = 29)
+    @ReportItem(name = "list2_11", length = 7, order = 28)
     public RString list2_11;
-    @ReportItem(name = "list2_12", length = 7, order = 30)
+    @ReportItem(name = "list2_12", length = 7, order = 29)
     public RString list2_12;
 // </editor-fold>
 // <editor-fold defaultstate="collapsed" desc="User Customize Area">
