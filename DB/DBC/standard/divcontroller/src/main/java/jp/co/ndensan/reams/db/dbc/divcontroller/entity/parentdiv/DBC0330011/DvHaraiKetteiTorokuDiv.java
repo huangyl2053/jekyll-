e1c179fe7314f1ac002a1a7b7d27edcc958531a4
yuqingzhang <yuqingzhang@liandisys.com.cn>
2016-10-09 10:23:24 +0800

    jinkens対応

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0330011/DvHaraiKetteiTorokuDiv.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0330011/DvHaraiKetteiTorokuDiv.java
@@ -4,16 +4,20 @@ package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0330011;
  * このファイルへの変更は、再生成時には損失するため
  * 不正な動作の原因になります。
  */
+
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Button;
+import jp.co.ndensan.reams.uz.uza.ui.binding.DataGrid;
+import jp.co.ndensan.reams.uz.uza.ui.binding.HorizontalLine;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
 
 /**
- * DvHaraiKetteiToroku のクラスファイル 
- * 
- * @author 自動生成
+ * DvHaraiKetteiToroku のクラスファイル
+ *
+ * @reamsid_L DBC-1980-010 yuqingzhang
  */
 public class DvHaraiKetteiTorokuDiv extends Panel {
+
     // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-08-09_21-40-56">
     /*
      * [ private の作成 ]
