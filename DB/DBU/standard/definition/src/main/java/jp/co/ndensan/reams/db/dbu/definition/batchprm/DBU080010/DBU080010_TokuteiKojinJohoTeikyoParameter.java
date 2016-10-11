772c8187d8711c1e79e9d646ece59d150ee835fd
wangxiaodong <wangxiaodong@liandisys.com.cn>
2016-10-11 13:35:09 +0800

    バッチ設計_DBUMNC2001-1_特定個人情報提供 新規作成

--- DB/DBU/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbu/definition/batchprm/DBU080010/DBU080010_TokuteiKojinJohoTeikyoParameter.java
+++ DB/DBU/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbu/definition/batchprm/DBU080010/DBU080010_TokuteiKojinJohoTeikyoParameter.java
@@ -6,8 +6,11 @@
 package jp.co.ndensan.reams.db.dbu.definition.batchprm.DBU080010;
 
 import java.util.List;
+import jp.co.ndensan.reams.db.dbu.definition.core.bangoseido.RenkeisakiDantainaiTogoAtena;
+import jp.co.ndensan.reams.db.dbu.definition.core.bangoseido.ShinkiIdoKubun;
 import jp.co.ndensan.reams.uz.uza.batch.BatchParameter;
 import jp.co.ndensan.reams.uz.uza.batch.flow.BatchParameterBase;
+import jp.co.ndensan.reams.uz.uza.lang.RDateTime;
 
 /**
  * 特定個人情報提供のバッチフ処理parameterクラスです。
@@ -20,9 +23,21 @@ import jp.co.ndensan.reams.uz.uza.batch.flow.BatchParameterBase;
 public class DBU080010_TokuteiKojinJohoTeikyoParameter extends BatchParameterBase {
 
     private static final String TOKUTEIKOJINJOHO = "TokuteiKojinJoho";
+    private static final String SHINKIIDOKUBUN = "ShinkiIdoKubun";
+    private static final String TAISHOKAISHITIMESTAMP = "TaishoKaishiTimestamp";
+    private static final String TAISHOSHURYOTIMESTAMP = "TaishoShuryoTimestamp";
+    private static final String RENKEISAKIDANTAINAITOGOATENA = "RenkeisakiDantainaiTogoAtena";
 
     @BatchParameter(key = TOKUTEIKOJINJOHO, name = "特定個人情報")
     private List<DBU080010_TokuteiKojinJohoTeikyoParameterHandler> 特定個人情報;
+    @BatchParameter(key = SHINKIIDOKUBUN, name = "新規異動区分")
+    private ShinkiIdoKubun 新規異動区分;
+    @BatchParameter(key = TAISHOKAISHITIMESTAMP, name = "対象開始日時")
+    private RDateTime 対象開始日時;
+    @BatchParameter(key = TAISHOSHURYOTIMESTAMP, name = "対象終了日時")
+    private RDateTime 対象終了日時;
+    @BatchParameter(key = RENKEISAKIDANTAINAITOGOATENA, name = "連携先団体内統合宛名（連携方式）")
+    private RenkeisakiDantainaiTogoAtena 連携先団体内統合宛名_連携方式;
 
     /**
      * コンストラクタです。
