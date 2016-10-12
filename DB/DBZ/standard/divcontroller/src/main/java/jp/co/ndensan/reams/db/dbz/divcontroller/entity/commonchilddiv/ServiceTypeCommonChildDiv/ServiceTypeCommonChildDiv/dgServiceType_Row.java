842944ea6b6efdff86b80b66a48994758efce960
dongyabin <dongyabin@liandisys.com.cn>
2016-10-12 14:48:22 +0800

    画面設計_DBAMN00000_サービス入力ガイド_サービス種類入力 修正

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/ServiceTypeCommonChildDiv/ServiceTypeCommonChildDiv/dgServiceType_Row.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/ServiceTypeCommonChildDiv/ServiceTypeCommonChildDiv/dgServiceType_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.ServiceTy
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Tue Oct 11 17:40:47 CST 2016 
+ * Wed Oct 12 14:24:34 CST 2016 
  */
 
 
