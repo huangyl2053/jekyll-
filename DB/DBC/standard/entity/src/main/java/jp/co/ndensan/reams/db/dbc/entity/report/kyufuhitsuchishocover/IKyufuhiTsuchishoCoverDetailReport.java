376b96ab84faf09640e26f99e48f72d8121be374
lizhuoxuan <lizhuoxuan@liandisys.com.cn>
2016-10-13 18:57:23 +0800

    バッチ設計書_DBCMN72002_給付費通知書作成(一括)  指摘を対応

---
+++ DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/report/kyufuhitsuchishocover/IKyufuhiTsuchishoCoverDetailReport.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbc.entity.report.kyufuhitsuchishocover;
+
+import jp.co.ndensan.reams.uz.uza.report.IReportSource;
+
+/**
+ * 帳票設計_介護保険給付費通知書のReportです。
+ *
+ * @reamsid_L DBC-2280-050 lizhuoxuan
+ */
+public class IKyufuhiTsuchishoCoverDetailReport implements IReportSource {
+
+}
