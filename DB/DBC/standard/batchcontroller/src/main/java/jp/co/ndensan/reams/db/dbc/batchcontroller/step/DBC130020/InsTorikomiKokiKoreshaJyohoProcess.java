012f35b5e0336759b0b5322fa159fd6a3ad733fb
qinzhen <qinzhen@liandisys.com.cn>
2016-10-14 13:46:11 +0800

    バッチ設計_DBC130020_後期高齢資格異動情報取込(修正)

--- DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/step/DBC130020/InsTorikomiKokiKoreshaJyohoProcess.java
+++ DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/step/DBC130020/InsTorikomiKokiKoreshaJyohoProcess.java
@@ -7,6 +7,7 @@ package jp.co.ndensan.reams.db.dbc.batchcontroller.step.DBC130020;
 
 import java.io.File;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -438,27 +439,31 @@ public class InsTorikomiKokiKoreshaJyohoProcess extends BatchProcessBase<RString
     }
 
     private void エラーチェック処理_電算２用部分1() {
+        List<RString> 取得事由List = new ArrayList<>();
+        for (ShikakuShutokuJiyu データ作成 : ShikakuShutokuJiyu.values()) {
+            取得事由List.add(データ作成.getコード());
+        }
         RString 資格取得事由 = 取込後期高齢者情報Entity.get資格取得事由コード().substring(INDEX_0, INDEX_2);
-        for (ShikakuShutokuJiyu cod : ShikakuShutokuJiyu.values()) {
-            if (資格取得事由.isEmpty() && !cod.getコード().equals(資格取得事由)) {
-                取込後期高齢者情報Entity.setエラーコード(エラーコード_56);
-                if (文言設定flag) {
-                    取込後期高齢者情報Entity.setエラー文言(コード文言_資格取得事由);
-                    文言設定flag = false;
-                }
-                取込後期高齢者情報Entity.setエラー区分(エラー区分_1);
+        if (資格取得事由.isEmpty() && !取得事由List.contains(資格取得事由)) {
+            取込後期高齢者情報Entity.setエラーコード(エラーコード_56);
+            if (文言設定flag) {
+                取込後期高齢者情報Entity.setエラー文言(コード文言_資格取得事由);
+                文言設定flag = false;
             }
+            取込後期高齢者情報Entity.setエラー区分(エラー区分_1);
+        }
+        List<RString> 喪失事由List = new ArrayList<>();
+        for (ShikakuSoshitsuJiyu データ作成 : ShikakuSoshitsuJiyu.values()) {
+            喪失事由List.add(データ作成.getコード());
         }
         RString 資格喪失事由 = 取込後期高齢者情報Entity.get資格喪失事由コード().substring(INDEX_0, INDEX_2);
-        for (ShikakuSoshitsuJiyu cod : ShikakuSoshitsuJiyu.values()) {
-            if (資格喪失事由.isEmpty() && !cod.getコード().equals(資格喪失事由)) {
-                取込後期高齢者情報Entity.setエラーコード(エラーコード_58);
-                if (文言設定flag) {
-                    取込後期高齢者情報Entity.setエラー文言(コード文言_資格喪失事由);
-                    文言設定flag = false;
-                }
-                取込後期高齢者情報Entity.setエラー区分(エラー区分_1);
+        if (資格喪失事由.isEmpty() && !喪失事由List.contains(資格喪失事由)) {
+            取込後期高齢者情報Entity.setエラーコード(エラーコード_58);
+            if (文言設定flag) {
+                取込後期高齢者情報Entity.setエラー文言(コード文言_資格喪失事由);
+                文言設定flag = false;
             }
+            取込後期高齢者情報Entity.setエラー区分(エラー区分_1);
         }
         RString 性別コード = 取込後期高齢者情報Entity.get性別コード();
         if (!性別コード_1.equals(性別コード) && !性別コード_2.equals(性別コード) && !性別コード_3.equals(性別コード)) {
