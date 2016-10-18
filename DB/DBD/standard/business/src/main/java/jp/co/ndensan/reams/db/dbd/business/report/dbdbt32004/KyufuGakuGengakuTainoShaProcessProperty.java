877ea0574b1ac8523ea5de9a54e9947df9dbfc41
wangjie2 <wangjie2@liandisys.com.cn>
2016-10-18 09:30:12 +0800

    バッチ設計_DBDBT32004_給付額減額管理リスト　訂正

--- DB/DBD/standard/business/src/main/java/jp/co/ndensan/reams/db/dbd/business/report/dbdbt32004/KyufuGakuGengakuTainoShaProcessProperty.java
+++ DB/DBD/standard/business/src/main/java/jp/co/ndensan/reams/db/dbd/business/report/dbdbt32004/KyufuGakuGengakuTainoShaProcessProperty.java
@@ -26,35 +26,31 @@ public class KyufuGakuGengakuTainoShaProcessProperty {
         /**
          * 郵便番号
          */
-        郵便番号(new RString("0001"), new RString("郵便番号"), new RString("")),
+        郵便番号(new RString("0001"), new RString("郵便番号"), new RString("\"ShikibetsuTaisho_yubinNo\"")),
         /**
          * 町域コード
          */
-        町域コード(new RString("0002"), new RString("町域コード"), new RString("")),
+        町域コード(new RString("0002"), new RString("町域コード"), new RString("\"ShikibetsuTaisho_choikiCode\"")),
         /**
          * 行政区コード
          */
-        行政区コード(new RString("0004"), new RString("行政区コード"), new RString("")),
+        行政区コード(new RString("0004"), new RString("行政区コード"), new RString("\"ShikibetsuTaisho_gyoseikuCode\"")),
         /**
          * 世帯コード
          */
-        世帯コード(new RString("0008"), new RString("世帯コード"), new RString("")),
+        世帯コード(new RString("0008"), new RString("世帯コード"), new RString("\"ShikibetsuTaisho_setaiCode\"")),
         /**
          * 識別コード
          */
-        識別コード(new RString("0009"), new RString("識別コード"), new RString("")),
+        識別コード(new RString("0009"), new RString("識別コード"), new RString("\"ShikibetsuTaisho_shikibetsuCode\"")),
         /**
          * 氏名５０音カナ
          */
-        氏名５０音カナ(new RString("0010"), new RString("氏名５０音カナ"), new RString("")),
+        氏名５０音カナ(new RString("0010"), new RString("氏名５０音カナ"), new RString("\"ShikibetsuTaisho_kanaMeisho\"")),
         /**
          * 市町村コード
          */
-        市町村コード(new RString("0016"), new RString("市町村コード"), new RString("")),
-        /**
-         * 被保険者番号
-         */
-        被保険者番号(new RString("0104"), new RString("被保険者番号"), new RString(""));
+        市町村コード(new RString("0016"), new RString("市町村コード"), new RString("\"dbT1001HihokenshaDaicho_shichosonCode\""));
         private final RString 項目ID;
         private final RString フォームフィールド名;
         private final RString myBatis項目名;
