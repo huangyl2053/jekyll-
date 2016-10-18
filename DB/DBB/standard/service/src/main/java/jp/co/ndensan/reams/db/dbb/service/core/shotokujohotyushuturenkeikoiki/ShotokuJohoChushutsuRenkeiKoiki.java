f5797536a7bd949d47a2d5ed03ae62275911f87c
yuanzhenxia <yuanzhenxia@liandisys.com.cn>
2016-10-18 13:33:08 +0800

    残件対応

--- DB/DBB/standard/service/src/main/java/jp/co/ndensan/reams/db/dbb/service/core/shotokujohotyushuturenkeikoiki/ShotokuJohoChushutsuRenkeiKoiki.java
+++ DB/DBB/standard/service/src/main/java/jp/co/ndensan/reams/db/dbb/service/core/shotokujohotyushuturenkeikoiki/ShotokuJohoChushutsuRenkeiKoiki.java
@@ -167,8 +167,6 @@ public class ShotokuJohoChushutsuRenkeiKoiki {
         result.set市町村情報List(parameter.get市町村情報List());
         result.set出力順ID(parameter.get出力順ID());
         result.set帳票ID(ReportIdDBB.DBB200008.getReportId());
-        result.set共有ファイル名(parameter.get共有ファイル名());
-        result.set共有ファイルID(parameter.get共有ファイルID());
         result.set処理区分(異動_広域);
         return result;
     }
