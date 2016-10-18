fd0fa8b33251562a0a996ae0c6b9c90e9fefa894
潘 暁波 <pangxb@liandisys.com.cn>
2016-10-18 16:31:13 +0800

    ソース修正

--- DB/DBD/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/batchcontroller/step/DBD571001/JukyushaDaichoCyouhyoujouhouProcess.java
+++ DB/DBD/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/batchcontroller/step/DBD571001/JukyushaDaichoCyouhyoujouhouProcess.java
@@ -239,7 +239,7 @@ public class JukyushaDaichoCyouhyoujouhouProcess extends BatchProcessBase<IdoChu
 
     @Override
     protected void afterExecute() {
-        if (明細 == 1) {
+        if (!帳票出力用受給者台帳Entity.get要介護認定情報EntityList().isEmpty()) {
             JukyushaDaichoReport report = new JukyushaDaichoReport(帳票出力用受給者台帳Entity, processParamter.get出力オプション区分());
             report.writeBy(reportSourceWriter);
         }
