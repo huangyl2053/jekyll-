ee2319a7d4700da310f26d6feb1f99f0edaec351
李利歓 <x_lilh@subcon.liandisys.com.cn>
2016-10-21 15:27:54 +0800

    非課税年金対象者情報一覧 ソース修正

--- DB/DBD/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbd/entity/db/relate/dbd8100202/JissekiDataIchijiSakuseiJohoEntity.java
+++ DB/DBD/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbd/entity/db/relate/dbd8100202/JissekiDataIchijiSakuseiJohoEntity.java
@@ -8,7 +8,6 @@ package jp.co.ndensan.reams.db.dbd.entity.db.relate.dbd8100202;
 import jp.co.ndensan.reams.db.dbd.entity.db.basic.DbT4037HikazeNenkinTaishoshaEntity;
 import jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT1001HihokenshaDaichoEntity;
 import jp.co.ndensan.reams.ua.uax.entity.db.basic.UaFt200FindShikibetsuTaishoEntity;
-import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
  * 実績データ一時作成SQL検索結果Entityクラスです．
@@ -22,6 +21,5 @@ public class JissekiDataIchijiSakuseiJohoEntity {
 
     private DbT1001HihokenshaDaichoEntity 被保険者台帳管理NewestEntity;
     private DbT4037HikazeNenkinTaishoshaEntity 非課税年金対象者Entity;
-    private RString 履歴番号;
     private UaFt200FindShikibetsuTaishoEntity psmEntity;
 }
