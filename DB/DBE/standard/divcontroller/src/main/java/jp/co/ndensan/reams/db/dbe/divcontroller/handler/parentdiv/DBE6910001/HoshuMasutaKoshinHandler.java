3be27a331c855d93f09db8ce744a112e3841d28c
孫　清飛 <n8398@ndensan.co.jp>
2016-10-22 14:40:48 +0900

    受入 #106975　【不具合】DBE1710_DBEMN74001_報酬マスタメンテナンス　修正ボタンを押すとエラーする対応

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/handler/parentdiv/DBE6910001/HoshuMasutaKoshinHandler.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/handler/parentdiv/DBE6910001/HoshuMasutaKoshinHandler.java
@@ -805,7 +805,11 @@ public class HoshuMasutaKoshinHandler {
 
     private void set審査会委員別単価マスタ明細内容(dgShinsakaiIinBetuTanka_Row 選択行の審査会委員別単価情報) {
         div.getHoshuMasutaTab().getTxtBetuKaishiYM().setDomain(選択行の審査会委員別単価情報.getKaishiYM().getValue().getYearMonth());
-        div.getHoshuMasutaTab().getTxtBetuShuryoYM().setDomain(選択行の審査会委員別単価情報.getShuryoYM().getValue().getYearMonth());
+        if (FlexibleDate.EMPTY.equals(選択行の審査会委員別単価情報.getShuryoYM().getValue())) {
+            div.getHoshuMasutaTab().getTxtBetuShuryoYM().clearDomain();
+        } else {
+            div.getHoshuMasutaTab().getTxtBetuShuryoYM().setDomain(選択行の審査会委員別単価情報.getShuryoYM().getValue().getYearMonth());
+        }
         div.getHoshuMasutaTab().getTxtShinsaIinKodo().setValue(選択行の審査会委員別単価情報.getShinsakaiIinCode());
         div.getHoshuMasutaTab().getTxtBetuTanka().setValue(選択行の審査会委員別単価情報.getTanka().getValue());
         div.getHoshuMasutaTab().getTxtBetuSonotaTanka().setValue(選択行の審査会委員別単価情報.getSonotaTanka().getValue());
