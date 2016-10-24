4f626163650ef2a4841374bfdae1aad176717343
李利歓 <x_lilh@subcon.liandisys.com.cn>
2016-10-24 15:08:25 +0800

    非課税年金対象者情報一覧 SQLの出力順ソース修正

--- DB/DBD/standard/business/src/main/java/jp/co/ndensan/reams/db/dbd/business/report/dbd8100201/ResultListCSVProperty.java
+++ DB/DBD/standard/business/src/main/java/jp/co/ndensan/reams/db/dbd/business/report/dbd8100201/ResultListCSVProperty.java
@@ -26,63 +26,63 @@ public class ResultListCSVProperty {
         /**
          * DT市町村コード
          */
-        DT市町村コード(new RString("0425"), new RString("DT市町村コード"), new RString("dtShichosonCode")),
+        DT市町村コード(new RString("0425"), new RString("DT市町村コード"), new RString("\"dtShichosonCode\"")),
         /**
          * 世帯コード
          */
-        世帯コード(new RString("0008"), new RString("世帯コード"), new RString("shotaiCode")),
+        世帯コード(new RString("0008"), new RString("世帯コード"), new RString("\"shotaiCode\"")),
         /**
          * 識別コード
          */
-        識別コード(new RString("0009"), new RString("識別コード"), new RString("shikibetsuCode")),
+        識別コード(new RString("0009"), new RString("識別コード"), new RString("\"shikibetsuCode\"")),
         /**
          * 宛名カナ氏名
          */
-        宛名カナ氏名(new RString("0437"), new RString("宛名カナ氏名"), new RString("atenaKanaShimei")),
+        宛名カナ氏名(new RString("0437"), new RString("宛名カナ氏名"), new RString("\"atenaKanaShimei\"")),
         /**
          * 被保険者番号
          */
-        被保険者番号(new RString("0104"), new RString("被保険者番号"), new RString("hihokenshaNo")),
+        被保険者番号(new RString("0104"), new RString("被保険者番号"), new RString("\"hihokenshaNo\"")),
         /**
          * DT郵便番号
          */
-        DT郵便番号(new RString("0426"), new RString("DT郵便番号"), new RString("dtYubinNo")),
+        DT郵便番号(new RString("0426"), new RString("DT郵便番号"), new RString("\"dtYubinNo\"")),
         /**
          * DTカナ氏名
          */
-        DTカナ氏名(new RString("0427"), new RString("DTカナ氏名"), new RString("dtKanaShimei")),
+        DTカナ氏名(new RString("0427"), new RString("DTカナ氏名"), new RString("\"dtKanaShimei\"")),
         /**
          * DT性別
          */
-        DT性別(new RString("0429"), new RString("DT性別"), new RString("dtSeibetsu")),
+        DT性別(new RString("0429"), new RString("DT性別"), new RString("\"dtSeibetsu\"")),
         /**
          * DT年金保険者コード
          */
-        DT年金保険者コード(new RString("0430"), new RString("DT年金保険者コード"), new RString("dtNennkinnHokenshaCode")),
+        DT年金保険者コード(new RString("0430"), new RString("DT年金保険者コード"), new RString("\"dtNennkinnHokenshaCode\"")),
         /**
          * DT基礎年金番号
          */
-        DT基礎年金番号(new RString("0431"), new RString("DT基礎年金番号"), new RString("kisoNennkinnNo")),
+        DT基礎年金番号(new RString("0431"), new RString("DT基礎年金番号"), new RString("\"kisoNennkinnNo\"")),
         /**
          * DT年金コード
          */
-        DT年金コード(new RString("0432"), new RString("DT年金コード"), new RString("nennkinnCode")),
+        DT年金コード(new RString("0432"), new RString("DT年金コード"), new RString("\"nennkinnCode\"")),
         /**
          * DT対象年
          */
-        DT対象年(new RString("0433"), new RString("DT対象年"), new RString("dtTaisyoYear")),
+        DT対象年(new RString("0433"), new RString("DT対象年"), new RString("\"dtTaisyoYear\"")),
         /**
          * DT訂正表示
          */
-        DT訂正表示(new RString("0434"), new RString("DT訂正表示"), new RString("dtTeiseiHyouji")),
+        DT訂正表示(new RString("0434"), new RString("DT訂正表示"), new RString("\"dtTeiseiHyouji\"")),
         /**
          * DT各種区分
          */
-        DT各種区分(new RString("0435"), new RString("DT各種区分"), new RString("dtKakushuKubun")),
+        DT各種区分(new RString("0435"), new RString("DT各種区分"), new RString("\"dtKakushuKubun\"")),
         /**
          * DT処理結果
          */
-        DT処理結果(new RString("0436"), new RString("DT処理結果"), new RString("dtShoriResult"));
+        DT処理結果(new RString("0436"), new RString("DT処理結果"), new RString("\"dtShoriResult\""));
         private final RString 項目ID;
         private final RString フォームフィールド名;
         private final RString myBatis項目名;
