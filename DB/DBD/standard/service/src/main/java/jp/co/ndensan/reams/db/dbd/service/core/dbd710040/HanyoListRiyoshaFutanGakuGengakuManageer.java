c468f2ae43bd4e5fcb2cc4c6e13ee79233bf9cd0
田宇鸿 <tianyh@liandisys.com.cn>
2016-10-24 17:56:57 +0800

    ソース修正

--- DB/DBD/standard/service/src/main/java/jp/co/ndensan/reams/db/dbd/service/core/dbd710040/HanyoListRiyoshaFutanGakuGengakuManageer.java
+++ DB/DBD/standard/service/src/main/java/jp/co/ndensan/reams/db/dbd/service/core/dbd710040/HanyoListRiyoshaFutanGakuGengakuManageer.java
@@ -129,20 +129,22 @@ public class HanyoListRiyoshaFutanGakuGengakuManageer {
      *
      */
     public void get方法(RiyoshaFutanGakuGengakuEntity entity, HanyoListEntity hanyolistentity) {
-        ChoikiCode 町域コード = entity.getPsmEntity().getChoikiCode();
-        YubinNo 郵便番号 = entity.getPsmEntity().getYubinNo();
-        AtenaBanchi 番地コード = entity.getPsmEntity().getBanchi();
-        GyoseikuCode 行政区コード = entity.getPsmEntity().getGyoseikuCode();
-        ChikuCode 地区１ = entity.getPsmEntity().getChikuCode1();
-        ChikuCode 地区２ = entity.getPsmEntity().getChikuCode2();
-        SetaiCode 世帯コード = entity.getPsmEntity().getSetaiCode();
-        hanyolistentity.set郵便番号(郵便番号 != null ? 郵便番号.getYubinNo() : RString.EMPTY);
-        hanyolistentity.set町域コード(町域コード != null ? 町域コード.getColumnValue() : RString.EMPTY);
-        hanyolistentity.set番地コード(番地コード != null ? 番地コード.getColumnValue() : RString.EMPTY);
-        hanyolistentity.set行政区コード(行政区コード != null ? 行政区コード.getColumnValue() : RString.EMPTY);
-        hanyolistentity.set地区１(地区１ != null ? 地区１.getColumnValue() : RString.EMPTY);
-        hanyolistentity.set地区２(地区２ != null ? 地区２.getColumnValue() : RString.EMPTY);
-        hanyolistentity.set世帯コード(世帯コード != null ? 世帯コード.getColumnValue() : RString.EMPTY);
+        if (entity.getPsmEntity() != null) {
+            ChoikiCode 町域コード = entity.getPsmEntity().getChoikiCode();
+            YubinNo 郵便番号 = entity.getPsmEntity().getYubinNo();
+            AtenaBanchi 番地コード = entity.getPsmEntity().getBanchi();
+            GyoseikuCode 行政区コード = entity.getPsmEntity().getGyoseikuCode();
+            ChikuCode 地区１ = entity.getPsmEntity().getChikuCode1();
+            ChikuCode 地区２ = entity.getPsmEntity().getChikuCode2();
+            SetaiCode 世帯コード = entity.getPsmEntity().getSetaiCode();
+            hanyolistentity.set郵便番号(郵便番号 != null ? 郵便番号.getYubinNo() : RString.EMPTY);
+            hanyolistentity.set町域コード(町域コード != null ? 町域コード.getColumnValue() : RString.EMPTY);
+            hanyolistentity.set番地コード(番地コード != null ? 番地コード.getColumnValue() : RString.EMPTY);
+            hanyolistentity.set行政区コード(行政区コード != null ? 行政区コード.getColumnValue() : RString.EMPTY);
+            hanyolistentity.set地区１(地区１ != null ? 地区１.getColumnValue() : RString.EMPTY);
+            hanyolistentity.set地区２(地区２ != null ? 地区２.getColumnValue() : RString.EMPTY);
+            hanyolistentity.set世帯コード(世帯コード != null ? 世帯コード.getColumnValue() : RString.EMPTY);
+        }
         hanyolistentity.set識別コード(entity.get被保険者台帳管理_識別コード());
         hanyolistentity.set氏名５０音カナ(entity.getPsmEntity().getKanaName());
         hanyolistentity.set生年月日(entity.getPsmEntity().getSeinengappiYMD());
