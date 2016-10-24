4ae119a2487b564e5b87b3de5f4850bd57df15cf
wangxiaodong <wangxiaodong@liandisys.com.cn>
2016-10-24 15:12:30 +0800

    バッチ設計_DBUMNC2001-1_特定個人情報提供 新規作成

---
+++ DB/DBU/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbu/definition/mybatisprm/tokuteikojinjohoteikyo/TokuteiKojinJohoTeikyoMybatisParamater.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbu.definition.mybatisprm.tokuteikojinjohoteikyo;
+
+import jp.co.ndensan.reams.uz.uza.batch.parameter.IMyBatisParameter;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+/**
+ * 特定個人情報提供のMyBatis用パラメータクラスです。
+ *
+ * @reamsid_L DBU-4880-090 wangxiaodong
+ */
+@lombok.Setter
+@lombok.Getter
+@SuppressWarnings("PMD.UnusedPrivateField")
+public class TokuteiKojinJohoTeikyoMybatisParamater implements IMyBatisParameter {
+
+    private RString subGyomuCode;
+    private RString shoriName;
+
+}
