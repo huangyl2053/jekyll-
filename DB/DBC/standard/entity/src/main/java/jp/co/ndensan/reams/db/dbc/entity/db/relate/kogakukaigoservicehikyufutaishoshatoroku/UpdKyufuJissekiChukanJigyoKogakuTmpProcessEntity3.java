c4d100da0aa396f59471a62fbc1ac4a244b9384f
tianshuai <tianshuai@liandisys.com.cn>
2016-10-24 15:32:31 +0800

    CheckStyle対応

--- DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/db/relate/kogakukaigoservicehikyufutaishoshatoroku/UpdKyufuJissekiChukanJigyoKogakuTmpProcessEntity3.java
+++ DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/db/relate/kogakukaigoservicehikyufutaishoshatoroku/UpdKyufuJissekiChukanJigyoKogakuTmpProcessEntity3.java
@@ -5,21 +5,19 @@
  */
 package jp.co.ndensan.reams.db.dbc.entity.db.relate.kogakukaigoservicehikyufutaishoshatoroku;
 
-import jp.co.ndensan.reams.db.dbc.entity.db.basic.DbT3057KogakuShikyuHanteiKekkaEntity;
-import jp.co.ndensan.reams.db.dbc.entity.db.basic.DbT3058KogakuShikyuShinsaKetteiEntity;
 import jp.co.ndensan.reams.db.dbc.entity.db.relate.kogakukaigokyufuhitaishoshatoroku.TempKyufujissekiTyukannJigyoEntity;
+import jp.co.ndensan.reams.db.dbz.entity.db.basic.DbT3055KogakuKyufuTaishoshaGokeiEntity;
 
 /**
- * 給付実績中間事業高額一時の更新２用エンティティクラスです。
+ * 給付実績中間事業高額一時の更新３用エンティティクラスです。
  *
  * @reamsid_L DBC-2010-040 zhouchuanlin
  */
 @lombok.Setter
 @lombok.Getter
 @SuppressWarnings("PMD.UnusedPrivateField")
-public class UpdKyufuJissekiChukanJigyoKogakuTmpProcess5_2Entity {
+public class UpdKyufuJissekiChukanJigyoKogakuTmpProcessEntity3 {
 
-    private DbT3057KogakuShikyuHanteiKekkaEntity 高額介護サービス費支給判定結果;
-    private DbT3058KogakuShikyuShinsaKetteiEntity 高額介護サービス費支給審査決定;
+    private DbT3055KogakuKyufuTaishoshaGokeiEntity 高額介護サービス費給付対象者合計;
     private TempKyufujissekiTyukannJigyoEntity 給付実績中間事業高額一時５;
 }
