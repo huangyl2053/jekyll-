78d62a7a98dec0956ff66fe65a2e99cd7990d8b8
wangjie2 <wangjie2@liandisys.com.cn>
2016-10-26 09:19:21 +0800

    帳票設計_DBDPR32003_1_給付額減額把握リスト　訂正

--- DB/DBD/standard/business/src/main/java/jp/co/ndensan/reams/db/dbd/business/report/dbd200008/KyufuGengakuHaakuIchiranReport.java
+++ DB/DBD/standard/business/src/main/java/jp/co/ndensan/reams/db/dbd/business/report/dbd200008/KyufuGengakuHaakuIchiranReport.java
@@ -10,6 +10,7 @@ import java.util.List;
 import jp.co.ndensan.reams.db.dbd.definition.core.common.TokuchoFuchoKubun;
 import jp.co.ndensan.reams.db.dbd.entity.db.relate.kyufugengakuhaakuichiran.KyufuGengakuHaakuIchiranEntity;
 import jp.co.ndensan.reams.db.dbd.entity.db.relate.kyufugengakuhaakuichiran.ShunoJohoEntity;
+import jp.co.ndensan.reams.db.dbd.entity.db.relate.kyufugengakuhaakuichiran.ShunoKibetsuEntity;
 import jp.co.ndensan.reams.db.dbd.entity.report.dbd200008.KyufuGengakuHaakuIchiranReportSource;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.HokenshaNo;
 import jp.co.ndensan.reams.ur.urz.business.core.reportoutputorder.IOutputOrder;
@@ -102,18 +103,46 @@ public final class KyufuGengakuHaakuIchiranReport extends Report<KyufuGengakuHaa
 
     private void add賦課年度_収納情報リスト(List<ShunoJohoEntity> 賦課年度_収納情報リスト, ShunoJohoEntity add収納情報) {
         if (賦課年度_収納情報リスト.isEmpty()) {
-            賦課年度_収納情報リスト.add(add収納情報);
+            賦課年度_収納情報リスト.add(get収納情報By期別(add収納情報));
             return;
         }
         for (int index = 0; index < 賦課年度_収納情報リスト.size(); index++) {
             ShunoJohoEntity 収納情報 = 賦課年度_収納情報リスト.get(index);
             if (add収納情報.get調定年度().equals(収納情報.get調定年度()) && TokuchoFuchoKubun.特別徴収.getコード().equals(add収納情報.get特徴普徴区分())
                     || add収納情報.get調定年度().isBefore(収納情報.get調定年度())) {
-                賦課年度_収納情報リスト.add(index, 収納情報);
+                賦課年度_収納情報リスト.add(index, get収納情報By期別(収納情報));
                 return;
             }
         }
-        賦課年度_収納情報リスト.add(add収納情報);
+        賦課年度_収納情報リスト.add(get収納情報By期別(add収納情報));
+    }
+
+    private ShunoJohoEntity get収納情報By期別(ShunoJohoEntity 収納情報) {
+        List<ShunoKibetsuEntity> 期別情報 = 収納情報.get期別情報();
+        List<ShunoKibetsuEntity> new期別情報 = new ArrayList<>();
+        for (ShunoKibetsuEntity 期別 : 期別情報) {
+            add期別情報(new期別情報, 期別);
+        }
+        収納情報.set期別情報(new期別情報);
+        return 収納情報;
+    }
+
+    private void add期別情報(List<ShunoKibetsuEntity> 期別情報, ShunoKibetsuEntity 期別) {
+        if (期別情報.isEmpty()) {
+            期別情報.add(期別);
+            return;
+        }
+        for (int index = 0; index < 期別情報.size(); index++) {
+            if (rStringToInt(期別.get期別()) < rStringToInt(期別情報.get(index).get期別())) {
+                期別情報.add(index, 期別);
+                return;
+            }
+        }
+        期別情報.add(期別);
+    }
+
+    private int rStringToInt(RString string) {
+        return Integer.parseInt(string.toString());
     }
 
     private void add賦課年度List(List<FlexibleYear> 賦課年度List, FlexibleYear 賦課年度) {
