0e17e98f7293805b48d44bcd41828edf3b98edfc
sunhadi <sunhaidi@liandisys.com.cn>
2016-10-28 14:43:08 +0800

    Process作成

--- DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/step/DBC110900/HihokenshaTempUpdateKouikiProcess2.java
+++ DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/step/DBC110900/HihokenshaTempUpdateKouikiProcess2.java
@@ -5,15 +5,18 @@
  */
 package jp.co.ndensan.reams.db.dbc.batchcontroller.step.DBC110900;
 
-import jp.co.ndensan.reams.db.dbc.entity.db.relate.kakohorenjyohosakuseicommon.HihokenshaDaichoEntity;
 import jp.co.ndensan.reams.db.dbc.entity.db.relate.kakohorenjyohosakuseicommon.KakohorenJyohoSakuseiCommonEntity;
 import jp.co.ndensan.reams.db.dbc.entity.db.relate.kyufukanrihyoout.HihokenshaTempEntity;
-import jp.co.ndensan.reams.db.dbc.entity.db.relate.kyufukanrihyoout.KokuhorenSakuseiErrorTempEntity;
+import jp.co.ndensan.reams.db.dbx.definition.core.configkeys.ConfigNameDBU;
+import jp.co.ndensan.reams.db.dbx.definition.core.dbbusinessconfig.DbBusinessConfig;
+import jp.co.ndensan.reams.db.dbx.entity.db.basic.DbT7051KoseiShichosonMasterEntity;
 import jp.co.ndensan.reams.uz.uza.batch.process.BatchDbReader;
 import jp.co.ndensan.reams.uz.uza.batch.process.BatchEntityCreatedTempTableWriter;
 import jp.co.ndensan.reams.uz.uza.batch.process.BatchProcessBase;
 import jp.co.ndensan.reams.uz.uza.batch.process.BatchWriter;
 import jp.co.ndensan.reams.uz.uza.batch.process.IBatchReader;
+import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 
 /**
@@ -21,39 +24,34 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
  *
  * @reamsid_L DBC-2840-031 sunhaidi
  */
-public class HihokenshaTempUpdateKouikiProcess extends BatchProcessBase<KakohorenJyohoSakuseiCommonEntity> {
+public class HihokenshaTempUpdateKouikiProcess2 extends BatchProcessBase<KakohorenJyohoSakuseiCommonEntity> {
 
     private static final RString MYBATIS_SELECT_ID = new RString(
             "jp.co.ndensan.reams.db.dbc.persistence.db.mapper.relate.kakohorenjyohosakuseicommon.IKakohorenJyohoSakuseiCommonMapper."
-            + "select被保険者台帳");
+            + "select合併あり市町村情報");
     private static final RString 被保険者一時TBL_NAME = new RString("DbWT1001Hihokensha");
-    private static final RString 処理結果リスト一時TBL_NAME = new RString("DbWT1002KokuhorenSakuseiError");
-    private static final RString 広域内住所地特例フラグ_TRUE = new RString("1");
     @BatchWriter
     private BatchEntityCreatedTempTableWriter 被保険者一時TBL;
-    @BatchWriter
-    private BatchEntityCreatedTempTableWriter 処理結果リスト一時TBL;
+    private RString 保険者番号;
 
     @Override
     protected IBatchReader createReader() {
         被保険者一時TBL = new BatchEntityCreatedTempTableWriter(被保険者一時TBL_NAME,
                 HihokenshaTempEntity.class);
-        処理結果リスト一時TBL = new BatchEntityCreatedTempTableWriter(処理結果リスト一時TBL_NAME,
-                KokuhorenSakuseiErrorTempEntity.class);
+        RDate 基準日 = RDate.getNowDate();
+        保険者番号 = DbBusinessConfig.get(ConfigNameDBU.保険者情報_保険者番号, 基準日, SubGyomuCode.DBU介護統計報告);
         return new BatchDbReader(MYBATIS_SELECT_ID);
     }
 
     @Override
     protected void process(KakohorenJyohoSakuseiCommonEntity commonEntity) {
-        HihokenshaTempEntity entity = commonEntity.getHihokenshaTempEntity();
-        HihokenshaDaichoEntity hihokenshaDaichoEntity = commonEntity.getHihokenshaDaichoEntity();
-        entity.setOldShichosonCode(hihokenshaDaichoEntity.getKyuShichosonCode());
-        RString 広域内住所地特例フラグ = hihokenshaDaichoEntity.getKoikinaiJushochiTokureiFlag();
-        if (広域内住所地特例フラグ_TRUE.equals(広域内住所地特例フラグ)) {
-            entity.setShichosonCode(hihokenshaDaichoEntity.getKoikinaiShichosonCd());
-        } else {
-            entity.setShichosonCode(hihokenshaDaichoEntity.getShichosonCode());
-        }
-        被保険者一時TBL.update(entity);
+        HihokenshaTempEntity hihokenshaTempEntity = commonEntity.getHihokenshaTempEntity();
+        DbT7051KoseiShichosonMasterEntity dbT7051Entity = commonEntity.getDbT7051Entity();
+        hihokenshaTempEntity.setExShoHokenshaNo(dbT7051Entity.getShoKisaiHokenshaNo().value());
+        hihokenshaTempEntity.setExHokenshaNo(保険者番号);
+        hihokenshaTempEntity.setShichosonCode(RString.EMPTY);
+        hihokenshaTempEntity.setOldShichosonCode(RString.EMPTY);
+        被保険者一時TBL.update(hihokenshaTempEntity);
     }
+
 }
