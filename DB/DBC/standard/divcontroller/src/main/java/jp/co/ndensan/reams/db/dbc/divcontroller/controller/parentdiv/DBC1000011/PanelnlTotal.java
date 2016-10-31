007ebd0bcefd9a55abaf1edffb0fec1dc004f9f9
zhangzhiming <zhangzhiming@liandisys.com.cn>
2016-10-31 22:59:55 +0800

    下記受入問題対応： 受入 #107995 【不具合】DBC-1000 償還払い支給（不支給）決定通知書一括作成 「前回対象日」が表示されない。

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC1000011/PanelnlTotal.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC1000011/PanelnlTotal.java
@@ -9,11 +9,15 @@ import jp.co.ndensan.reams.db.dbc.definition.batchprm.DBC030010.DBC030010_Shokan
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC1000011.PanelnlTotalDiv;
 import jp.co.ndensan.reams.db.dbc.divcontroller.handler.parentdiv.DBC1000011.PanelnlTotalHandler;
 import jp.co.ndensan.reams.db.dbc.service.core.shokanbaraishikyu.ShokanbaraiShikyuFinder;
+import jp.co.ndensan.reams.db.dbx.definition.core.configkeys.ConfigNameDBC;
+import jp.co.ndensan.reams.db.dbx.definition.core.dbbusinessconfig.DbBusinessConfig;
 import jp.co.ndensan.reams.db.dbz.business.core.basic.ChohyoBunruiKanri;
 import jp.co.ndensan.reams.db.dbz.business.core.basic.ShoriDateKanri;
 import jp.co.ndensan.reams.db.dbz.definition.core.kyotsu.ShoriName;
 import jp.co.ndensan.reams.ur.urz.definition.message.UrQuestionMessages;
+import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
 import jp.co.ndensan.reams.uz.uza.core.ui.response.ResponseData;
+import jp.co.ndensan.reams.uz.uza.lang.RDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.message.MessageDialogSelectedResult;
 import jp.co.ndensan.reams.uz.uza.message.QuestionMessage;
@@ -27,6 +31,10 @@ import jp.co.ndensan.reams.uz.uza.ui.servlets.ValidationMessageControlPairs;
  */
 public class PanelnlTotal {
 
+    private static final RString 抽出条件_1 = new RString("1");
+    private static final RString 抽出条件_2 = new RString("2");
+    private static final RString 抽出条件_3 = new RString("3");
+
     /**
      * 償還払い支給（不支給）決定通知書一括作成画面初期化を処理します。
      *
@@ -34,8 +42,20 @@ public class PanelnlTotal {
      * @return ResponseData<PanelnlTotalDiv>
      */
     public ResponseData<PanelnlTotalDiv> onLoad(PanelnlTotalDiv div) {
-        ShoriDateKanri shori = ShokanbaraiShikyuFinder.createInstance()
-                .select前回対象日(ShoriName.償還払い支給不支給決定通知書一括作成_受付日.get名称());
+        RString 処理名 = RString.EMPTY;
+        if (抽出条件_1.equals(DbBusinessConfig.get(ConfigNameDBC.償還払支給不支給決定通知書一括作成_初期選択抽出条件, RDate.getNowDate(),
+                SubGyomuCode.DBC介護給付))) {
+            処理名 = ShoriName.償還払い支給不支給決定通知書一括作成_受付日.get名称();
+        }
+        if (抽出条件_2.equals(DbBusinessConfig.get(ConfigNameDBC.償還払支給不支給決定通知書一括作成_初期選択抽出条件, RDate.getNowDate(),
+                SubGyomuCode.DBC介護給付))) {
+            処理名 = ShoriName.償還払い支給不支給決定通知書一括作成_決定日.get名称();
+        }
+        if (抽出条件_3.equals(DbBusinessConfig.get(ConfigNameDBC.償還払支給不支給決定通知書一括作成_初期選択抽出条件, RDate.getNowDate(),
+                SubGyomuCode.DBC介護給付))) {
+            処理名 = ShoriName.償還払い支給不支給決定通知書一括作成_決定者受付年月.get名称();
+        }
+        ShoriDateKanri shori = ShokanbaraiShikyuFinder.createInstance().select前回対象日(処理名);
         ChohyoBunruiKanri chohyo = ShokanbaraiShikyuFinder.createInstance().select帳票分類ID();
         createHandler(div).onLoad(shori, chohyo);
         return ResponseData.of(div).respond();
