79ddc15852fa57376f3caabf2e14825d1cb0700b
jianglaisheng <jianglaisheng@liandisys.com.cn>
2016-11-01 14:46:52 +0800

    バグ対応＃108439

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/FukushiyoguShohinGuide/IFukushiyoguShohinGuideDiv.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/commonchilddiv/FukushiyoguShohinGuide/IFukushiyoguShohinGuideDiv.java
@@ -1,12 +1,13 @@
 package jp.co.ndensan.reams.db.dbc.divcontroller.entity.commonchilddiv.FukushiyoguShohinGuide;
 
 import jp.co.ndensan.reams.uz.uza.ui.binding.ICommonChildDivBaseProperties;
+import jp.co.ndensan.reams.uz.uza.ui.binding.IDialogDiv;
 
 /**
  * このコードはツールによって生成されました。
  *
  * @reamsid_L DBC-0970-020 lizhuoxuan
  */
-public interface IFukushiyoguShohinGuideDiv extends ICommonChildDivBaseProperties {
+public interface IFukushiyoguShohinGuideDiv extends ICommonChildDivBaseProperties, IDialogDiv {
 
 }
