18735c709910584328735cce052df0cc2a6b4d1c
lijia <lijia@liandisys.com.cn>
2016-11-02 21:43:39 +0800

    住宅改修理由書作成手数料請求情報修正登録(審査結果登録) バグ対応 14D203D-35236 14D203D-35236

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC2410011/dgSeikyuMeisai_Row.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC2410011/dgSeikyuMeisai_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC2410011;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Mon Oct 31 16:57:54 CST 2016 
+ * Wed Nov 02 20:17:50 CST 2016 
  */
 
 
