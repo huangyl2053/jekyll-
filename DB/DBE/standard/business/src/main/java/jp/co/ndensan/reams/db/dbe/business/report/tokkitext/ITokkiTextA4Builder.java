80f8228a2c66733ebf871b0301a32dbdaceaedc2
lishengli <lishengli@liandisys.com.cn>
2016-11-02 18:26:29 +0800

    特記事項A4版　修正

---
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/tokkitext/ITokkiTextA4Builder.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbe.business.report.tokkitext;
+
+import jp.co.ndensan.reams.db.dbe.entity.report.source.tokkitext.TokkiTextA4ReportSource;
+import jp.co.ndensan.reams.uz.uza.report.IReportBuilder;
+
+/**
+ * 特記事項A4版Builderのインターフェースクラスです。
+ *
+ * @reamsid_L DBE-0150-200 lishengli
+ */
+public interface ITokkiTextA4Builder extends IReportBuilder<TokkiTextA4ReportSource> {
+
+}
