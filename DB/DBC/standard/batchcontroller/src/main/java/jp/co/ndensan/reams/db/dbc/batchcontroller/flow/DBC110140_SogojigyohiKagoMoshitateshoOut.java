2a6a15540a44eff5a3a330e4770b842bcd917312
jiangwenkai <jiangwenkai@liandisys.com.cn>
2016-11-03 14:17:19 +0800

    バッチ設計書_DBCMNF1001-173_介護給付費過誤申立書作成 バッチ設計書_DBCMNF1001-176_総合事業費（経過措置）過誤申立書情報作成 バッチ設計書_DBCMNF1001-179_総合事業費過誤申立書情報作成 残件に対応する

--- DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/flow/DBC110140_SogojigyohiKagoMoshitateshoOut.java
+++ DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/flow/DBC110140_SogojigyohiKagoMoshitateshoOut.java
@@ -121,7 +121,6 @@ public class DBC110140_SogojigyohiKagoMoshitateshoOut extends BatchFlowBase<DBC1
     private static final RString 書区分コード = new RString("3");
     private static final RString コード = new RString("179");
     private static final RString SJIS類似 = new RString("SjisRuiji");
-    private static final RString バックスラッシュ = new RString("\\");
     private static final RString 国保連送付外字_変換区分_1 = new RString("1");
 
     @Override
@@ -536,7 +535,7 @@ public class DBC110140_SogojigyohiKagoMoshitateshoOut extends BatchFlowBase<DBC1
             deleteFile(入力ファイルパス);
         }
         SharedFileDescriptor sfd = new SharedFileDescriptor(GyomuCode.DB介護保険,
-                FilesystemName.fromString(出力ファイルパス.substring(出力ファイルパス.lastIndexOf(バックスラッシュ) + INT_1)));
+                FilesystemName.fromString(出力ファイルパス.substring(出力ファイルパス.lastIndexOf(File.separator) + INT_1)));
         sfd = SharedFile.defineSharedFile(sfd, 1, SharedFile.GROUP_ALL, null, true, null);
         CopyToSharedFileOpts opts = new CopyToSharedFileOpts().dateToDelete(RDate.getNowDate().plusMonth(1));
         SharedFile.copyToSharedFile(sfd, FilesystemPath.fromString(出力ファイルパス), opts);
