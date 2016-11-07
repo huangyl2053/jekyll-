b83f48e03796b5e580bc4781b180fa578bb729d4
許悦 <xuyue@liandisys.com.cn>
2016-11-07 11:13:15 +0800

    #106148 管理バッチ対応

--- DB/DBD/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/batchcontroller/step/DBD207012/ShiharaiHohoHenkoKanrPrintProcess.java
+++ DB/DBD/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbd/batchcontroller/step/DBD207012/ShiharaiHohoHenkoKanrPrintProcess.java
@@ -65,7 +65,6 @@ import jp.co.ndensan.reams.uz.uza.log.accesslog.AccessLogger;
 import jp.co.ndensan.reams.uz.uza.log.accesslog.core.ExpandedInformation;
 import jp.co.ndensan.reams.uz.uza.log.accesslog.core.PersonalData;
 import jp.co.ndensan.reams.uz.uza.log.accesslog.core.uuid.AccessLogUUID;
-import jp.co.ndensan.reams.uz.uza.report.BreakerCatalog;
 import jp.co.ndensan.reams.uz.uza.report.ReportSourceWriter;
 
 /**
@@ -115,13 +114,6 @@ public class ShiharaiHohoHenkoKanrPrintProcess extends BatchProcessBase<Shiharai
 
     private static final RString バッチ出力条件出力順 = new RString("出力順:");
     private static final int 帳票期別リストSIZE = 3;
-
-    private static final int NO_0 = 0;
-    private static final int NO_1 = 1;
-    private static final int NO_2 = 2;
-    private static final int NO_3 = 3;
-    private static final int NO_4 = 4;
-
     private Association association;
     private IOutputOrder outputOrder;
     private RString 出力順;
@@ -151,11 +143,8 @@ public class ShiharaiHohoHenkoKanrPrintProcess extends BatchProcessBase<Shiharai
 
     @Override
     protected void createWriter() {
-        List<RString> pageBreakKeys = new ArrayList<>();
-        set改頁Key(outputOrder, pageBreakKeys);
         batchReportWrite = BatchReportFactory.createBatchReportWriter(REPORT_DBD200007.getReportId().value(),
-                SubGyomuCode.DBD介護受給).addBreak(
-                        new BreakerCatalog<ShiharaiHohoHenkoKanriIchiranReportSource>().simplePageBreaker(pageBreakKeys)).create();
+                SubGyomuCode.DBD介護受給).create();
         reportSourceWriter = new ReportSourceWriter<>(batchReportWrite);
     }
 
@@ -165,23 +154,59 @@ public class ShiharaiHohoHenkoKanrPrintProcess extends BatchProcessBase<Shiharai
 
     @Override
     protected void process(ShiharaiHohoHenkoHaakuFiveEntity t) {
-        count++;
-        if (count == 1) {
+        if (reportData == null && reportData1 == null) {
             reportData = new ShiharaiHohoHenkoEntity();
             createShiharaiHohoHenkoEntity(t, reportData);
-        }
-        if (count == 2) {
+        } else {
             reportData1 = new ShiharaiHohoHenkoEntity();
             createShiharaiHohoHenkoEntity(t, reportData1);
 
-            ShiharaiHohoHenkoKanriIchiranReport finder = new ShiharaiHohoHenkoKanriIchiranReport(RDateTime.now(),
-                    new HokenshaNo(association.get地方公共団体コード().value()), association.get市町村名(),
-                    outputOrder, reportData, reportData1, REPORT_DBD200007);
-
-            finder.writeBy(reportSourceWriter);
-            count = 0;
+            if (is改頁()) {
+                ShiharaiHohoHenkoKanriIchiranReport finder = new ShiharaiHohoHenkoKanriIchiranReport(RDateTime.now(),
+                        new HokenshaNo(association.get地方公共団体コード().value()), association.get市町村名(),
+                        outputOrder, reportData, new ShiharaiHohoHenkoEntity(), REPORT_DBD200007);
+                finder.writeBy(reportSourceWriter);
+                reportData = reportData1;
+                reportData1 = null;
+            } else {
+                ShiharaiHohoHenkoKanriIchiranReport finder = new ShiharaiHohoHenkoKanriIchiranReport(RDateTime.now(),
+                        new HokenshaNo(association.get地方公共団体コード().value()), association.get市町村名(),
+                        outputOrder, reportData, reportData1, REPORT_DBD200007);
+                finder.writeBy(reportSourceWriter);
+                reportData = null;
+                reportData1 = null;
+            }
         }
         personalDataList.add(toPersonalData(t));
+    }
+
+    private boolean is改頁() {
+        List<RString> 改頁IDList = new ArrayList<>();
+        if (outputOrder == null || outputOrder.get設定項目リスト() == null || outputOrder.get設定項目リスト().isEmpty()) {
+            return false;
+        }
+        List<ISetSortItem> list = outputOrder.get設定項目リスト();
+        for (ISetSortItem data : list) {
+            if (data.is改頁項目()) {
+                改頁IDList.add(data.get項目ID());
+            }
+        }
+        return (改頁IDList.contains(ShiharaiHohoHenkoHaakuOrderKey.郵便番号.get項目ID())
+                && !reportData.get郵便番号().equals(reportData1.get郵便番号()))
+                || (改頁IDList.contains(ShiharaiHohoHenkoHaakuOrderKey.町域コード.get項目ID())
+                && !reportData.get町域コード().equals(reportData1.get町域コード()))
+                || (改頁IDList.contains(ShiharaiHohoHenkoHaakuOrderKey.行政区コード.get項目ID())
+                && !reportData.get行政区コード().equals(reportData1.get行政区コード()))
+                || (改頁IDList.contains(ShiharaiHohoHenkoHaakuOrderKey.世帯コード.get項目ID())
+                && !reportData.get世帯番号().equals(reportData1.get世帯番号()))
+                || (改頁IDList.contains(ShiharaiHohoHenkoHaakuOrderKey.識別コード.get項目ID())
+                && !reportData.get識別コード().equals(reportData1.get識別コード()))
+                || (改頁IDList.contains(ShiharaiHohoHenkoHaakuOrderKey.氏名５０音カナ.get項目ID())
+                && !reportData.get被保険者氏名カナ().equals(reportData1.get被保険者氏名カナ()))
+                || (改頁IDList.contains(ShiharaiHohoHenkoHaakuOrderKey.市町村コード.get項目ID())
+                && !reportData.get市町村コード().equals(reportData1.get市町村コード()))
+                || (改頁IDList.contains(ShiharaiHohoHenkoHaakuOrderKey.被保険者番号.get項目ID())
+                && !reportData.get被保険者番号().equals(reportData1.get被保険者番号()));
     }
 
     private RString get出力順(IOutputOrder order) {
@@ -194,73 +219,6 @@ public class ShiharaiHohoHenkoKanrPrintProcess extends BatchProcessBase<Shiharai
         return new RString("order by 対象者情報_被保険者番号,"
                 + "収納状況_調定年度,収納状況_賦課年度"
                 + ",収納状況_通知書番号,収納状況_期");
-    }
-
-    private void set改頁Key(IOutputOrder outputOrder, List<RString> pageBreakKeys) {
-        RString 改頁１ = RString.EMPTY;
-        RString 改頁２ = RString.EMPTY;
-        RString 改頁３ = RString.EMPTY;
-        RString 改頁４ = RString.EMPTY;
-        RString 改頁５ = RString.EMPTY;
-        if (outputOrder != null) {
-            List<ISetSortItem> list = outputOrder.get設定項目リスト();
-            if (list == null) {
-                list = new ArrayList<>();
-            }
-            if (list.size() > NO_0 && list.get(NO_0).is改頁項目()) {
-                改頁１ = to帳票物理名(list.get(NO_0).get項目ID());
-            }
-            if (list.size() > NO_1 && list.get(NO_1).is改頁項目()) {
-                改頁２ = to帳票物理名(list.get(NO_1).get項目ID());
-            }
-            if (list.size() > NO_2 && list.get(NO_2).is改頁項目()) {
-                改頁３ = to帳票物理名(list.get(NO_2).get項目ID());
-            }
-            if (list.size() > NO_3 && list.get(NO_3).is改頁項目()) {
-                改頁４ = to帳票物理名(list.get(NO_3).get項目ID());
-            }
-            if (list.size() > NO_4 && list.get(NO_4).is改頁項目()) {
-                改頁５ = to帳票物理名(list.get(NO_4).get項目ID());
-            }
-
-            if (!改頁１.isEmpty()) {
-                pageBreakKeys.add(改頁１);
-            }
-            if (!改頁２.isEmpty()) {
-                pageBreakKeys.add(改頁２);
-            }
-            if (!改頁３.isEmpty()) {
-                pageBreakKeys.add(改頁３);
-            }
-            if (!改頁４.isEmpty()) {
-                pageBreakKeys.add(改頁４);
-            }
-            if (!改頁５.isEmpty()) {
-                pageBreakKeys.add(改頁５);
-            }
-        }
-    }
-
-    private RString to帳票物理名(RString 項目ID) {
-        RString 帳票物理名 = RString.EMPTY;
-        if (ShiharaiHohoHenkoHaakuOrderKey.郵便番号.get項目ID().equals(項目ID)) {
-            帳票物理名 = new RString("yubinNo");
-        } else if (ShiharaiHohoHenkoHaakuOrderKey.町域コード.get項目ID().equals(項目ID)) {
-            帳票物理名 = new RString("choikiCode");
-        } else if (ShiharaiHohoHenkoHaakuOrderKey.行政区コード.get項目ID().equals(項目ID)) {
-            帳票物理名 = new RString("gyoseikuCode");
-        } else if (ShiharaiHohoHenkoHaakuOrderKey.世帯コード.get項目ID().equals(項目ID)) {
-            帳票物理名 = new RString("setaiCode");
-        } else if (ShiharaiHohoHenkoHaakuOrderKey.識別コード.get項目ID().equals(項目ID)) {
-            帳票物理名 = new RString("shikibetsuCode");
-        } else if (ShiharaiHohoHenkoHaakuOrderKey.氏名５０音カナ.get項目ID().equals(項目ID)) {
-            帳票物理名 = new RString("shimeiKana");
-        } else if (ShiharaiHohoHenkoHaakuOrderKey.市町村コード.get項目ID().equals(項目ID)) {
-            帳票物理名 = new RString("shichosonCode");
-        } else if (ShiharaiHohoHenkoHaakuOrderKey.被保険者番号.get項目ID().equals(項目ID)) {
-            帳票物理名 = new RString("hihokenshaNo");
-        }
-        return 帳票物理名;
     }
 
     private ShiharaiHohoHenkoEntity createShiharaiHohoHenkoEntity(ShiharaiHohoHenkoHaakuFiveEntity t, ShiharaiHohoHenkoEntity reportDataEntity) {
