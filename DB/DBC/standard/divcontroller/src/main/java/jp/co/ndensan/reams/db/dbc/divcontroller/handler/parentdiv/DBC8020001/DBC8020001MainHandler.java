7c38b08456ade66f56fb5b7dd6f7ff3d2c754893
zhouchuanlin <zhouchuanlin@liandisys.com.cn>
2016-11-07 16:21:51 +0800

    画面設計_DBCMNN2004_事業合算・事業分振込明細書作成（一括）：残件review指摘对应

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/parentdiv/DBC8020001/DBC8020001MainHandler.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/parentdiv/DBC8020001/DBC8020001MainHandler.java
@@ -66,7 +66,6 @@ public class DBC8020001MainHandler {
     private static final RString INDEX_2 = new RString("2");
     private static final int INDEXSTART = 0;
     private static final int INDEX_4 = 4;
-    private static final int INDEX_5 = 5;
     private static final int INDEXEND = 6;
 
     /**
@@ -132,7 +131,9 @@ public class DBC8020001MainHandler {
                 get(0).get振込委託者Entity().getItakushamei());
         div.getItakusha().setItakushaId(new RString(entity.getFurikomiGroupItakushaRelateEntity().get振込委託者RelateEntity().
                 get(0).get振込委託者Entity().getItakushaId().toString()));
-        div.getItakusha().getTxtFurikomiGroupCode().setValue(代表金融機関コード.value().concat(振込グループコード));
+        if (null != 代表金融機関コード) {
+            div.getItakusha().getTxtFurikomiGroupCode().setValue(代表金融機関コード.value().concat(振込グループコード));
+        }
         div.getItakusha().getTxtFurikomiGroupMeisho().setValue(entity.getFurikomiGroupItakushaRelateEntity().
                 get振込グループEntity().getFurikomiGroupMeisho());
 
