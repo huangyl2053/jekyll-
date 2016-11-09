f5a9d2488cb301ebe96bfed8122ee4f850460589
xuyannan <xuyannan@liandisys.com.cn>
2016-11-09 15:30:51 +0800

    バッチ設計_DBU010080_JigyoHokokuRenkei_Hokenkyufu_ShokanKettei_保険給付【償還_決定】 BUG:14D203D-32761の対応 帳票設計_DBU300009_事業報告月報様式_01 1.02の対応

--- DB/DBU/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/batchcontroller/step/DBU010080/JigyoHokokuShiryouReportDBU300017Process.java
+++ DB/DBU/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbu/batchcontroller/step/DBU010080/JigyoHokokuShiryouReportDBU300017Process.java
@@ -6,6 +6,8 @@
 package jp.co.ndensan.reams.db.dbu.batchcontroller.step.DBU010080;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import jp.co.ndensan.reams.db.dbu.business.core.jigyohokokucompyoshiki201.JigyohokokuCompYoshiki201Business;
 import jp.co.ndensan.reams.db.dbu.business.report.jigyohokokucompyoshiki251.JigyohokokuCompYoshiki251Report;
@@ -13,7 +15,6 @@ import jp.co.ndensan.reams.db.dbu.definition.core.jigyohokoku.ShukeiNoyoshiki2;
 import jp.co.ndensan.reams.db.dbu.definition.processprm.hokenkyufushokankettei.JigyoHokokuRenkeiHokenkyufuShokanKetteiProcessParameter;
 import jp.co.ndensan.reams.db.dbu.definition.reportid.ReportIdDBU;
 import jp.co.ndensan.reams.db.dbu.entity.db.relate.hokenkyufushokankettei.JigyoHokokuShiryouRelateEntity;
-import jp.co.ndensan.reams.db.dbu.entity.db.relate.jigyohokokucompyoshiki201.JigyohokokuCompYoshiki201Entity;
 import jp.co.ndensan.reams.db.dbu.entity.report.jigyohokokucompyoshiki251.JigyohokokuCompYoshiki251ReportSource;
 import jp.co.ndensan.reams.ur.urz.service.core.association.AssociationFinderFactory;
 import jp.co.ndensan.reams.uz.uza.batch.process.BatchDbReader;
@@ -23,6 +24,7 @@ import jp.co.ndensan.reams.uz.uza.batch.process.BatchReportWriter;
 import jp.co.ndensan.reams.uz.uza.batch.process.BatchWriter;
 import jp.co.ndensan.reams.uz.uza.batch.process.IBatchReader;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.report.BreakerCatalog;
 import jp.co.ndensan.reams.uz.uza.report.ReportSourceWriter;
 
 /**
@@ -44,11 +46,16 @@ public class JigyoHokokuShiryouReportDBU300017Process extends BatchProcessBase<J
     private List<RString> 市町村コードList;
     private List<RString> 表番号List;
     private RString 引数Flag;
+    private JigyohokokuCompYoshiki201Business business;
+    private List<JigyoHokokuShiryouRelateEntity> list;
+    private List<RString> page_break_keys;
 
     @Override
     protected void initialize() {
         市町村コードList = new ArrayList<>();
         表番号List = new ArrayList<>();
+        list = new ArrayList<>();
+        business = new JigyohokokuCompYoshiki201Business();
         引数Flag = RString.EMPTY;
         if (RString.isNullOrEmpty(processParameter.get過去集計分旧市町村区分())) {
             市町村コードList.add(processParameter.get市町村コード());
@@ -63,6 +70,22 @@ public class JigyoHokokuShiryouReportDBU300017Process extends BatchProcessBase<J
             表番号List.add(new RString("16"));
             引数Flag = new RString("3");
         }
+        business.set年報月報区分(new RString("1"));
+        business.set処理日時(processParameter.get処理日時().getRDateTime());
+        if (new RString("1").equals(引数Flag)) {
+            business.set旧市町村区分(new RString("0"));
+            business.set保険者コード(AssociationFinderFactory.createInstance().getAssociation().get地方公共団体コード().value());
+        } else if (new RString("2").equals(引数Flag)) {
+            business.set旧市町村区分(new RString("0"));
+        } else if (new RString("3").equals(引数Flag)) {
+            business.set旧市町村区分(new RString("1"));
+        }
+        business.set保険者名(AssociationFinderFactory.createInstance().getAssociation().get市町村名());
+        business.set集計年月(processParameter.get集計年月());
+        business.set給付実績区分コード(new RString("2"));
+        page_break_keys = Collections.unmodifiableList(
+                Arrays.asList(new RString(ReportSourceFields.hokenshaNo.name()), new RString(ReportSourceFields.hyoTitle.name()),
+                        new RString(ReportSourceFields.hyoSubTitle.name())));
     }
 
     @Override
@@ -72,41 +95,39 @@ public class JigyoHokokuShiryouReportDBU300017Process extends BatchProcessBase<J
 
     @Override
     protected void createWriter() {
-        様式_05batchWrite = BatchReportFactory.createBatchReportWriter(ReportIdDBU.DBU300017.getReportId().value()).create();
+        様式_05batchWrite = BatchReportFactory.createBatchReportWriter(ReportIdDBU.DBU300017.getReportId().value())
+                .addBreak(new BreakerCatalog<JigyohokokuCompYoshiki251ReportSource>().simplePageBreaker(page_break_keys)).create();
         様式_05reportSourceWriter = new ReportSourceWriter(様式_05batchWrite);
     }
 
     @Override
     protected void process(JigyoHokokuShiryouRelateEntity entity) {
-        JigyohokokuCompYoshiki201Business business = new JigyohokokuCompYoshiki201Business();
-        business.set年報月報区分(new RString("1"));
-        business.set処理日時(processParameter.get処理日時().getRDateTime());
-        if (new RString("1").equals(引数Flag)) {
-            business.set旧市町村区分(new RString("0"));
-            business.set保険者コード(AssociationFinderFactory.createInstance().getAssociation().get地方公共団体コード().value());
-        } else if (new RString("2").equals(引数Flag)) {
-            business.set旧市町村区分(new RString("0"));
-            business.set保険者コード(processParameter.get過去集計分市町村コードリスト().get(0));
-        } else if (new RString("3").equals(引数Flag)) {
-            business.set旧市町村区分(new RString("1"));
-            business.set保険者コード(processParameter.get過去集計分市町村コードリスト().get(0));
-        }
-        business.set保険者名(AssociationFinderFactory.createInstance().getAssociation().get市町村名());
-        // TODO 内部QA：1874 Redmine# 「集計区分」は事業報告資料に存在しません。
-        List<JigyohokokuCompYoshiki201Entity> list = new ArrayList<>();
-        JigyohokokuCompYoshiki201Entity 帳票entity = new JigyohokokuCompYoshiki201Entity();
-        帳票entity.set集計番号(entity.getSyukeiNo());
-        帳票entity.set縦番号(entity.getHyoNo());
-        帳票entity.set横番号(entity.getYokoNo());
-        list.add(帳票entity);
+        list.add(entity);
+    }
+
+    @Override
+    protected void afterExecute() {
         business.set事業報告資料(list);
-        business.set集計年月(processParameter.get集計年月());
-        if (ShukeiNoyoshiki2._2_特定入所者介護_介護予防_サービス費1総数_件数.getコード().equals(entity.getSyukeiNo())
-                || ShukeiNoyoshiki2._2_特定入所者介護_介護予防_サービス費1総数_給付費.getコード().equals(entity.getSyukeiNo())
-                || ShukeiNoyoshiki2._2_特定入所者介護_介護予防_サービス費2第２号被保険者分_件数.getコード().equals(entity.getSyukeiNo())
-                || ShukeiNoyoshiki2._2_特定入所者介護_介護予防_サービス費2第２号被保険者分_給付費.getコード().equals(entity.getSyukeiNo())) {
-            JigyohokokuCompYoshiki251Report 様式_05report = new JigyohokokuCompYoshiki251Report(business);
-            様式_05report.writeBy(様式_05reportSourceWriter);
+        if (RString.isNullOrEmpty(processParameter.get過去集計分旧市町村区分())) {
+            print帳票(AssociationFinderFactory.createInstance().getAssociation().get地方公共団体コード().value());
+        } else {
+            for (RString 市町村コード : processParameter.get過去集計分市町村コードリスト()) {
+                print帳票(市町村コード);
+            }
+        }
+    }
+
+    private void print帳票(RString 市町村コード) {
+        for (JigyoHokokuShiryouRelateEntity entity : list) {
+            if (市町村コード.equals(entity.getShichosonCode())
+                    && (ShukeiNoyoshiki2._2_特定入所者介護_介護予防_サービス費1総数_件数.getコード().equals(entity.getSyukeiNo())
+                    || ShukeiNoyoshiki2._2_特定入所者介護_介護予防_サービス費1総数_給付費.getコード().equals(entity.getSyukeiNo())
+                    || ShukeiNoyoshiki2._2_特定入所者介護_介護予防_サービス費2第２号被保険者分_件数.getコード().equals(entity.getSyukeiNo())
+                    || ShukeiNoyoshiki2._2_特定入所者介護_介護予防_サービス費2第２号被保険者分_給付費.getコード().equals(entity.getSyukeiNo()))) {
+                business.set保険者コード(市町村コード);
+                JigyohokokuCompYoshiki251Report 様式_05report = new JigyohokokuCompYoshiki251Report(business);
+                様式_05report.writeBy(様式_05reportSourceWriter);
+            }
         }
     }
 }
--- DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/report/jigyohokokucompyoshiki201/JigyohokokuCompYoshiki201Report.java
+++ DB/DBU/standard/business/src/main/java/jp/co/ndensan/reams/db/dbu/business/report/jigyohokokucompyoshiki201/JigyohokokuCompYoshiki201Report.java
@@ -6,16 +6,13 @@
 package jp.co.ndensan.reams.db.dbu.business.report.jigyohokokucompyoshiki201;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import jp.co.ndensan.reams.db.dbu.business.core.jigyohokokucompyoshiki201.JigyohokokuCompYoshiki201Business;
+import jp.co.ndensan.reams.db.dbu.entity.db.relate.hokenkyufushokankettei.JigyoHokokuShiryouRelateEntity;
 import jp.co.ndensan.reams.db.dbu.entity.db.relate.jigyohokokucompyoshiki201.JigyohokokuCompResultEntity;
-import jp.co.ndensan.reams.db.dbu.entity.db.relate.jigyohokokucompyoshiki201.JigyohokokuCompYoshiki201Entity;
 import jp.co.ndensan.reams.db.dbu.entity.report.jigyohokokucompyoshiki201.JigyohokokuCompYoshiki201ReportSource;
 import jp.co.ndensan.reams.db.dbz.definition.core.yokaigonintei.ichijihantei.IchijiHanteiKekkaCode06;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.lang.RStringBuilder;
 import jp.co.ndensan.reams.uz.uza.report.Report;
 import jp.co.ndensan.reams.uz.uza.report.ReportSourceWriter;
 
@@ -26,21 +23,6 @@ import jp.co.ndensan.reams.uz.uza.report.ReportSourceWriter;
  */
 public class JigyohokokuCompYoshiki201Report extends Report<JigyohokokuCompYoshiki201ReportSource> {
 
-    private static final RString 集計番号_0101 = new RString("0101");
-    private static final RString 集計番号_0201 = new RString("0201");
-    private static final RString 集計番号_0301 = new RString("0301");
-    private static final RString 集計番号_0401 = new RString("0401");
-    private static final RString 集計番号_0103 = new RString("0103");
-    private static final RString 集計番号_0203 = new RString("0203");
-    private static final RString 集計番号_0303 = new RString("0303");
-    private static final RString 集計番号_0403 = new RString("0403");
-    private static final RString 集計番号_0104 = new RString("0104");
-    private static final RString 集計番号_0204 = new RString("0204");
-    private static final RString 集計番号_0304 = new RString("0304");
-    private static final RString 集計番号_0404 = new RString("0404");
-    private static final RString ア内容 = new RString("ア．件数");
-    private static final RString ウ内容 = new RString("ウ．費用額");
-    private static final RString エ内容 = new RString("エ．給付額");
     private static final RString 一行目 = new RString("15");
     private static final RString 二行目 = new RString("01");
     private static final RString 三行目 = new RString("16");
@@ -57,8 +39,8 @@ public class JigyohokokuCompYoshiki201Report extends Report<JigyohokokuCompYoshi
     private static final RString 十四行目 = new RString("25");
     private static final RString 十五行目 = new RString("03");
     private static final RString 十六行目 = new RString("22");
-    private static final RString 十七行目 = new RString("17");
-    private static final RString 十八行目 = new RString("18");
+    private static final RString 十七行目 = new RString("04");
+    private static final RString 十八行目 = new RString("05");
     private static final RString 十九行目 = new RString("28");
     private static final RString 二十行目 = new RString("29");
     private static final RString 二十一行目 = new RString("31");
@@ -76,8 +58,6 @@ public class JigyohokokuCompYoshiki201Report extends Report<JigyohokokuCompYoshi
     private static final RString 三十三行目 = new RString("08");
     private static final RString 三十四行目 = new RString("09");
     private static final RString 三十五行目 = new RString("14");
-    private static final RString 集計区分_加算 = new RString("1");
-    private static final RString 集計区分_減算 = new RString("2");
     private static final int INDEX_1 = 0;
     private static final int INDEX_2 = 1;
     private static final int INDEX_3 = 2;
@@ -114,16 +94,7 @@ public class JigyohokokuCompYoshiki201Report extends Report<JigyohokokuCompYoshi
     private static final int INDEX_34 = 33;
     private static final int INDEX_35 = 34;
     private final JigyohokokuCompYoshiki201Business business;
-    private List<JigyohokokuCompResultEntity> list;
-    private int 合計 = 0;
-    private int 加算件数合計 = 0;
-    private int 減算件数合計 = 0;
-    private int 加算請求額合計 = 0;
-    private int 減算請求額合計 = 0;
-    private int 加算集計支給金額合計 = 0;
-    private int 減算集計支給金額合計 = 0;
-    private RString new縦横番号;
-    private RString old縦横番号;
+    private final List<JigyohokokuCompResultEntity> list;
 
     /**
      * インスタンスを生成します。
@@ -134,39 +105,17 @@ public class JigyohokokuCompYoshiki201Report extends Report<JigyohokokuCompYoshi
         this.business = business;
         list = new ArrayList<>();
         for (int i = 0; i <= INDEX_35; i++) {
-            JigyohokokuCompResultEntity entity = new JigyohokokuCompResultEntity();
-            list.add(entity);
+            list.add(new JigyohokokuCompResultEntity());
         }
-        old縦横番号 = RString.EMPTY;
     }
 
     @Override
     public void writeBy(ReportSourceWriter<JigyohokokuCompYoshiki201ReportSource> reportSourceWriter) {
-        RString 集計番号 = RString.EMPTY;
-        RString 給付実績区分コード = RString.EMPTY;
-        Collections.sort(business.get事業報告資料(), new Comparator<JigyohokokuCompYoshiki201Entity>() {
-            @Override
-            public int compare(JigyohokokuCompYoshiki201Entity entity1, JigyohokokuCompYoshiki201Entity entity2) {
-                int flag = entity1.get縦番号().compareTo(entity2.get縦番号());
-                if (flag == 0) {
-                    return entity1.get横番号().compareTo(entity2.get横番号());
-                } else {
-                    return flag;
-                }
-            }
-        });
-        for (JigyohokokuCompYoshiki201Entity entity : business.get事業報告資料()) {
-            if (isValueChanged(entity.get縦番号(), entity.get横番号())) {
-                cleanValue();
-            }
-            list = get縦番号1(entity.get縦番号(), entity.get横番号(),
-                    getTitleThree(entity.get集計番号()), entity.get集計区分(), entity.get請求額(), entity.get集計支給金額());
-            集計番号 = entity.get集計番号();
-            給付実績区分コード = entity.get給付実績区分コード();
+        for (JigyoHokokuShiryouRelateEntity entity : business.get事業報告資料()) {
+            get縦番号1(entity.getTateNo(), entity);
         }
+        IJigyohokokuCompYoshiki201Editor headEditor = new JigyohokokuCompYoshiki201HeadEditor(business);
         for (JigyohokokuCompResultEntity entity : list) {
-            IJigyohokokuCompYoshiki201Editor headEditor = new JigyohokokuCompYoshiki201HeadEditor(
-                    business, getTitleThree(集計番号), 集計番号, 給付実績区分コード);
             IJigyohokokuCompYoshiki201Editor bodyEditor = new JigyohokokuCompYoshiki201BodyEditor(entity);
             IJigyohokokuCompYoshiki201Builder builder = new JigyohokokuCompYoshiki201Builder(headEditor, bodyEditor);
             reportSourceWriter.writeLine(builder);
@@ -174,187 +123,105 @@ public class JigyohokokuCompYoshiki201Report extends Report<JigyohokokuCompYoshi
 
     }
 
-    private RString getTitleThree(RString 集計番号) {
-        RString title;
-        if (集計番号_0101.equals(集計番号)
-                || 集計番号_0201.equals(集計番号)
-                || 集計番号_0301.equals(集計番号)
-                || 集計番号_0401.equals(集計番号)) {
-            title = ア内容;
-        } else if (集計番号_0103.equals(集計番号)
-                || 集計番号_0203.equals(集計番号)
-                || 集計番号_0303.equals(集計番号)
-                || 集計番号_0403.equals(集計番号)) {
-            title = ウ内容;
-        } else if (集計番号_0104.equals(集計番号)
-                || 集計番号_0204.equals(集計番号)
-                || 集計番号_0304.equals(集計番号)
-                || 集計番号_0404.equals(集計番号)) {
-            title = エ内容;
-        } else {
-            title = RString.EMPTY;
-        }
-        return title;
-    }
-
-    private int get合計(RString title3, RString 集計区分, int 請求額, int 集計支給金額) {
-        if (ア内容.equals(title3)) {
-            if (集計区分_加算.equals(集計区分)) {
-                加算件数合計 = 加算件数合計 + 1;
-            } else if (集計区分_減算.equals(集計区分)) {
-                減算件数合計 = 減算件数合計 + 1;
-            }
-            合計 = 加算件数合計 - 減算件数合計;
-        } else if (ウ内容.equals(title3)) {
-            if (集計区分_加算.equals(集計区分)) {
-                加算請求額合計 = 加算請求額合計 + 請求額;
-            } else if (集計区分_減算.equals(集計区分)) {
-                減算請求額合計 = 減算請求額合計 + 請求額;
-            }
-            合計 = 加算請求額合計 - 減算請求額合計;
-        } else if (エ内容.equals(title3)) {
-            if (集計区分_加算.equals(集計区分)) {
-                加算集計支給金額合計 = 加算集計支給金額合計 + 集計支給金額;
-            } else if (集計区分_減算.equals(集計区分)) {
-                減算集計支給金額合計 = 減算集計支給金額合計 + 集計支給金額;
-            }
-            合計 = 加算集計支給金額合計 - 減算集計支給金額合計;
-        }
-        return 合計;
-    }
-
-    private List<JigyohokokuCompResultEntity> get横番号(RString 横番号, int index,
-            RString title3, RString 集計区分, int 請求額, int 集計支給金額) {
-        JigyohokokuCompResultEntity entity = list.get(index);
+    private void get横番号(RString 横番号, JigyohokokuCompResultEntity resultEntity, JigyoHokokuShiryouRelateEntity entity) {
+        resultEntity.set集計番号(entity.getSyukeiNo());
         if (IchijiHanteiKekkaCode06.要支援1.getコード().equals(横番号)) {
-            entity.set要支援１(get合計(title3, 集計区分, 請求額, 集計支給金額));
+            resultEntity.set要支援１(entity.getSyukeiKekkaAtai());
         } else if (IchijiHanteiKekkaCode06.要支援2.getコード().equals(横番号)) {
-            entity.set要支援２(get合計(title3, 集計区分, 請求額, 集計支給金額));
+            resultEntity.set要支援２(entity.getSyukeiKekkaAtai());
         } else if (IchijiHanteiKekkaCode06.非該当.getコード().equals(横番号)) {
-            entity.set非該当(get合計(title3, 集計区分, 請求額, 集計支給金額));
+            resultEntity.set非該当(entity.getSyukeiKekkaAtai());
         } else if (IchijiHanteiKekkaCode06.経過的要介護.getコード().equals(横番号)) {
-            entity.set経過的要介護(get合計(title3, 集計区分, 請求額, 集計支給金額));
+            resultEntity.set経過的要介護(entity.getSyukeiKekkaAtai());
         } else if (IchijiHanteiKekkaCode06.要介護1.getコード().equals(横番号)) {
-            entity.set要介護１(get合計(title3, 集計区分, 請求額, 集計支給金額));
+            resultEntity.set要介護１(entity.getSyukeiKekkaAtai());
         } else if (IchijiHanteiKekkaCode06.要介護2.getコード().equals(横番号)) {
-            entity.set要介護２(get合計(title3, 集計区分, 請求額, 集計支給金額));
+            resultEntity.set要介護２(entity.getSyukeiKekkaAtai());
         } else if (IchijiHanteiKekkaCode06.要介護3.getコード().equals(横番号)) {
-            entity.set要介護３(get合計(title3, 集計区分, 請求額, 集計支給金額));
+            resultEntity.set要介護３(entity.getSyukeiKekkaAtai());
         } else if (IchijiHanteiKekkaCode06.要介護4.getコード().equals(横番号)) {
-            entity.set要介護４(get合計(title3, 集計区分, 請求額, 集計支給金額));
+            resultEntity.set要介護４(entity.getSyukeiKekkaAtai());
         } else if (IchijiHanteiKekkaCode06.要介護5.getコード().equals(横番号)) {
-            entity.set要介護５(get合計(title3, 集計区分, 請求額, 集計支給金額));
+            resultEntity.set要介護５(entity.getSyukeiKekkaAtai());
         }
-        entity.set計１(entity.get要支援１() + entity.get要支援２());
-        entity.set計２(entity.get非該当() + entity.get経過的要介護() + entity.get要介護１() + entity.get要介護２()
-                + entity.get要介護３() + entity.get要介護４() + entity.get要介護５());
-        entity.set合計(entity.get計１() + entity.get計２());
-        return list;
     }
 
-    private List<JigyohokokuCompResultEntity> get縦番号1(RString 縦番号, RString 横番号,
-            RString title3, RString 集計区分, int 請求額, int 集計支給金額) {
+    private void get縦番号1(RString 縦番号, JigyoHokokuShiryouRelateEntity entity) {
         if (一行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_1, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_1), entity);
         } else if (二行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_2, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_2), entity);
         } else if (三行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_3, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_3), entity);
         } else if (四行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_4, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_4), entity);
         } else if (五行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_5, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_5), entity);
         } else if (六行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_6, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_6), entity);
         } else if (七行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_7, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_7), entity);
         } else if (八行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_8, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_8), entity);
         } else if (九行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_9, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_9), entity);
         } else if (十行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_10, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_10), entity);
         } else if (十一行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_11, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_11), entity);
         } else if (十二行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_12, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_12), entity);
         } else if (十三行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_13, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_13), entity);
         } else if (十四行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_14, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_14), entity);
         } else if (十五行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_15, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_15), entity);
         } else if (十六行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_16, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_16), entity);
         } else if (十七行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_17, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_17), entity);
         } else if (十八行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_18, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_18), entity);
         }
-        list = get縦番号2(縦番号, 横番号, title3, 集計区分, 請求額, 集計支給金額);
-        return list;
+        get縦番号2(縦番号, entity);
     }
 
-    private List<JigyohokokuCompResultEntity> get縦番号2(RString 縦番号, RString 横番号,
-            RString title3, RString 集計区分, int 請求額, int 集計支給金額) {
+    private void get縦番号2(RString 縦番号, JigyoHokokuShiryouRelateEntity entity) {
         if (十九行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_19, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_19), entity);
         } else if (二十行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_20, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_20), entity);
         } else if (二十一行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_21, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_21), entity);
         } else if (二十二行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_22, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_22), entity);
         } else if (二十三行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_23, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_23), entity);
         } else if (二十四行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_24, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_24), entity);
         } else if (二十五行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_25, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_25), entity);
         } else if (二十六行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_26, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_26), entity);
         } else if (二十七行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_27, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_27), entity);
         } else if (二十八行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_28, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_28), entity);
         } else if (二十九行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_29, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_29), entity);
         } else if (三十行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_30, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_30), entity);
         } else if (三十一行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_31, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_31), entity);
         } else if (三十二行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_32, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_32), entity);
         } else if (三十三行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_33, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_33), entity);
         } else if (三十四行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_34, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_34), entity);
         } else if (三十五行目.equals(縦番号)) {
-            list = get横番号(横番号, INDEX_35, title3, 集計区分, 請求額, 集計支給金額);
+            get横番号(entity.getYokoNo(), list.get(INDEX_35), entity);
         }
-        return list;
-    }
-
-    private boolean isValueChanged(RString 縦番号, RString 横番号) {
-        RStringBuilder builder = new RStringBuilder();
-        builder.append(縦番号).append(RString.HALF_SPACE).append(横番号);
-        new縦横番号 = builder.toRString();
-        if (RString.isNullOrEmpty(old縦横番号) || old縦横番号.equals(new縦横番号)) {
-            old縦横番号 = new縦横番号;
-            return false;
-        } else {
-            old縦横番号 = new縦横番号;
-            return true;
-        }
-    }
-
-    private void cleanValue() {
-        合計 = 0;
-        加算件数合計 = 0;
-        減算件数合計 = 0;
-        加算請求額合計 = 0;
-        減算請求額合計 = 0;
-        加算集計支給金額合計 = 0;
-        減算集計支給金額合計 = 0;
     }
 }
