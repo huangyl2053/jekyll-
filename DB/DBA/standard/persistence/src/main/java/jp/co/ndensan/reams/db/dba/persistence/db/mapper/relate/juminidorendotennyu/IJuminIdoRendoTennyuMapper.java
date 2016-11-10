290bbdcef5c5a720a12fb9be7ed8074b37e0696d
houtianpeng <houtianpeng@liandisys.com.cn>
2016-11-10 11:29:27 +0800

    住民異動連動資格登録_転入 1.01

---
+++ DB/DBA/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dba/persistence/db/mapper/relate/juminidorendotennyu/IJuminIdoRendoTennyuMapper.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dba.persistence.db.mapper.relate.juminidorendotennyu;
+
+import java.util.List;
+import jp.co.ndensan.reams.db.dba.definition.mybatisprm.juminidorendotennyumanager.JuminIdoRendoTennyuManagerMybatisParameter;
+import jp.co.ndensan.reams.db.dba.entity.db.relate.juminidorendotennyu.JuminIdoRendoTennyuRelateEntity;
+
+/**
+ * ビジネス設計_DBAMN00000_住民異動連動資格登録_転入
+ *
+ * @reamsid_L DBA-1410-012 wanghui
+ */
+public interface IJuminIdoRendoTennyuMapper {
+
+    /**
+     * 宛名識別対象PSMと被保険者台帳を結合して抽出する。
+     *
+     * @param parameter JuminIdoRendoTennyuManagerMybatisParameter
+     * @return List<JuminIdoRendoTennyuRelateEntity>
+     */
+    List<JuminIdoRendoTennyuRelateEntity> getデータ抽出(JuminIdoRendoTennyuManagerMybatisParameter parameter);
+}
