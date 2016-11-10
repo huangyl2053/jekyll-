27a2e5280db52d0992a2f313a7b01c12d009a6c7
liuxiaoyu <liuxiaoyu@liandisys.com.cn>
2016-11-10 19:51:57 +0800

    ビジネス設計_DBCMNK2001_利用者負担割合判定（service）(納品後対応QA1839)

--- DB/DBC/standard/service/src/main/java/jp/co/ndensan/reams/db/dbc/service/core/riyoshafutanwariaihanteimanager/RiyoshaFutanWariaiHanteiManager.java
+++ DB/DBC/standard/service/src/main/java/jp/co/ndensan/reams/db/dbc/service/core/riyoshafutanwariaihanteimanager/RiyoshaFutanWariaiHanteiManager.java
@@ -8,7 +8,9 @@ package jp.co.ndensan.reams.db.dbc.service.core.riyoshafutanwariaihanteimanager;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import static java.util.Objects.requireNonNull;
 import jp.co.ndensan.reams.db.dbc.business.core.basic.SogoJigyoTaishosha;
 import jp.co.ndensan.reams.db.dbc.business.core.riyoshafutanwariaihantei.FutanWariaiHanteiJoho;
@@ -75,6 +77,8 @@ public class RiyoshaFutanWariaiHanteiManager {
     private static final RString THREE = new RString("3");
     private static final RString FOUR = new RString("4");
     private static final RString FIVE = new RString("5");
+    private static final int INDEX_ZERO = 0;
+    private static final int LIST_SIZE_ONE = 1;
     private static final int NUM_ONE = 2;
     private static final int NUM_TWO = 2;
     private static final int NUM_THREE = 3;
@@ -130,6 +134,7 @@ public class RiyoshaFutanWariaiHanteiManager {
             for (int i = 判定基準日list.size() - 1; 0 <= i; i--) {
                 List<SetaiinShikibetsuCd> 世帯員識別コード情報list = SetaiinFinder.createInstance().
                         get世帯員識別コードBy被保番号(被保険者番号, new FlexibleDate(判定基準日list.get(i)));
+                世帯員識別コード情報list = remove重複(世帯員識別コード情報list);
                 HihokenshaDaicho 被保険者台帳 = get被保険者台帳(
                         世帯員識別コード情報list, new FlexibleDate(判定基準日list.get(i)));
                 HanteiTaishoshaTempEntity 判定対象者Temp = new HanteiTaishoshaTempEntity();
@@ -149,6 +154,7 @@ public class RiyoshaFutanWariaiHanteiManager {
                 RiyoshaFutanWariaiHanteiParameter parameter = new RiyoshaFutanWariaiHanteiParameter(
                         年度, get識別code(世帯員識別コード情報));
                 List<DbV2512KaigoShotokuNewestEntity> 介護所得情報 = mapper.get介護所得情報(parameter);
+                set世帯員介護所得情報(年度, 介護所得情報, 世帯員識別コード情報, mapper);
                 set判定対象者Tempの介護所得情報部分(判定対象者Temp, 介護所得情報);
                 JukyushaDaichoManager jukyumanager = new JukyushaDaichoManager();
                 List<JukyushaDaicho> 受給者台帳情報 = jukyumanager.get受給者台帳被保険者番号(被保険者番号);
@@ -217,10 +223,7 @@ public class RiyoshaFutanWariaiHanteiManager {
             int 利用者負担割合の最大履歴番号 = get利用者負担割合の最大履歴番号(被保険者番号, 年度);
             List<DbT3114RiyoshaFutanWariaiMeisaiEntity> 現在の利用者負担割合明細 = 利用者負担割合明細Dac.selectBySomeKey(
                     年度, 被保険者番号, 利用者負担割合の最大履歴番号);
-            if (利用者負担割合明細list != null && !利用者負担割合明細list.isEmpty()
-                    && 現在の利用者負担割合明細 != null && !現在の利用者負担割合明細.isEmpty()) {
-                判定結果.set判定区分(get判定区分(現在の利用者負担割合明細.get(0), 利用者負担割合明細list.get(0)));
-            }
+            判定結果.set判定区分(get判定区分(現在の利用者負担割合明細, 利用者負担割合明細list));
         }
         return 判定結果;
     }
@@ -231,6 +234,39 @@ public class RiyoshaFutanWariaiHanteiManager {
             識別code = 世帯員識別コード情報.get(0).get世帯員識別コード();
         }
         return 識別code;
+    }
+
+    private void set世帯員介護所得情報(FlexibleYear 年度,
+            List<DbV2512KaigoShotokuNewestEntity> 介護所得情報, List<SetaiinShikibetsuCd> 世帯員識別コード情報,
+            IRiyoshaFutanWariaiHanteiManagerMapper mapper) {
+        if (世帯員識別コード情報 != null && LIST_SIZE_ONE < 世帯員識別コード情報.size()) {
+            List<DbV2512KaigoShotokuNewestEntity> 世帯員介護所得情報;
+            ShikibetsuCode 識別code;
+            RiyoshaFutanWariaiHanteiParameter parameter;
+            for (int j = 1; j < 世帯員識別コード情報.size(); j++) {
+                識別code = get世帯員識別code(世帯員識別コード情報.get(j).get世帯員識別コード());
+                parameter = new RiyoshaFutanWariaiHanteiParameter(年度, 識別code);
+                世帯員介護所得情報 = mapper.get介護所得情報(parameter);
+                add世帯員介護所得情報(介護所得情報, 世帯員介護所得情報);
+            }
+        }
+    }
+
+    private ShikibetsuCode get世帯員識別code(ShikibetsuCode 世帯員識別code) {
+        if (世帯員識別code != null) {
+            return 世帯員識別code;
+        }
+        return ShikibetsuCode.EMPTY;
+    }
+
+    private void add世帯員介護所得情報(List<DbV2512KaigoShotokuNewestEntity> 介護所得情報, List<DbV2512KaigoShotokuNewestEntity> 世帯員介護所得情報) {
+        if (介護所得情報 == null) {
+            return;
+        }
+        if (世帯員介護所得情報 == null || 世帯員介護所得情報.isEmpty()) {
+            return;
+        }
+        介護所得情報.add(世帯員介護所得情報.get(INDEX_ZERO));
     }
 
     private SetaiCode get世帯code(List<SetaiinShikibetsuCd> 世帯員識別コード情報) {
@@ -252,20 +288,43 @@ public class RiyoshaFutanWariaiHanteiManager {
     }
 
     private RString get判定区分(
-            DbT3114RiyoshaFutanWariaiMeisaiEntity 現在の利用者負担割合明細,
+            List<DbT3114RiyoshaFutanWariaiMeisaiEntity> 利用者負担割合明細list,
+            List<DbT3114RiyoshaFutanWariaiMeisaiEntity> 今回_作成した負担割合明細list) {
+        RString 判定区分;
+        if (利用者負担割合明細list == null || 利用者負担割合明細list.isEmpty()) {
+            if (今回_作成した負担割合明細list == null || 今回_作成した負担割合明細list.isEmpty()) {
+                判定区分 = THREE;
+            } else {
+                判定区分 = ONE;
+            }
+        } else {
+            if (今回_作成した負担割合明細list == null || 今回_作成した負担割合明細list.isEmpty()) {
+                判定区分 = ONE;
+            } else {
+                DbT3114RiyoshaFutanWariaiMeisaiEntity 現在の利用者負担割合明細 = 利用者負担割合明細list.get(INDEX_ZERO);
+                DbT3114RiyoshaFutanWariaiMeisaiEntity 今回_作成した負担割合明細 = 今回_作成した負担割合明細list.get(INDEX_ZERO);
+                判定区分 = get判定区分2(現在の利用者負担割合明細, 今回_作成した負担割合明細);
+            }
+        }
+        return 判定区分;
+    }
+
+    private RString get判定区分2(DbT3114RiyoshaFutanWariaiMeisaiEntity 現在の利用者負担割合明細,
             DbT3114RiyoshaFutanWariaiMeisaiEntity 今回_作成した負担割合明細) {
-        RString 判定区分 = RString.EMPTY;
+        RString 判定区分;
         if (現在の利用者負担割合明細 != null && 今回_作成した負担割合明細 != null) {
             if (is負担割合情報の比較(現在の利用者負担割合明細, 今回_作成した負担割合明細)) {
                 判定区分 = THREE;
-            } else if (!is比較変更RString(現在の利用者負担割合明細.getFutanWariaiKubun(),
-                    今回_作成した負担割合明細.getFutanWariaiKubun())
-                    && is排除負担割合区分の情報の比較(現在の利用者負担割合明細, 今回_作成した負担割合明細)) {
-                判定区分 = TWO;
             } else if (is比較変更RString(現在の利用者負担割合明細.getFutanWariaiKubun(),
                     今回_作成した負担割合明細.getFutanWariaiKubun())) {
                 判定区分 = ONE;
+            } else {
+                判定区分 = TWO;
             }
+        } else if (現在の利用者負担割合明細 == null && 今回_作成した負担割合明細 == null) {
+            判定区分 = THREE;
+        } else {
+            判定区分 = ONE;
         }
         return 判定区分;
     }
@@ -280,30 +339,6 @@ public class RiyoshaFutanWariaiHanteiManager {
                         今回_作成した負担割合明細.getShikakuKubun())
                 && is比較変更RString(現在の利用者負担割合明細.getFutanWariaiKubun(),
                         今回_作成した負担割合明細.getFutanWariaiKubun())
-                && is比較変更年月日(現在の利用者負担割合明細.getYukoKaishiYMD(),
-                        今回_作成した負担割合明細.getYukoKaishiYMD())
-                && is比較変更年月日(現在の利用者負担割合明細.getYukoShuryoYMD(),
-                        今回_作成した負担割合明細.getYukoShuryoYMD())
-                && is比較変更数字(現在の利用者負担割合明細.getHonninGoukeiShotokuGaku(),
-                        今回_作成した負担割合明細.getHonninGoukeiShotokuGaku())
-                && 現在の利用者負担割合明細.getSetaiIchigouHihokenshaSu()
-                == 今回_作成した負担割合明細.getSetaiIchigouHihokenshaSu()
-                && is比較変更数字(現在の利用者負担割合明細.getNenkinShunyuGoukei(),
-                        今回_作成した負担割合明細.getNenkinShunyuGoukei())
-                && is比較変更数字(現在の利用者負担割合明細.getSonotanoGoukeiShotokuKingakuGoukei(),
-                        今回_作成した負担割合明細.getSonotanoGoukeiShotokuKingakuGoukei())
-                && is比較変更RString(現在の利用者負担割合明細.getKoseiRiyu(), 今回_作成した負担割合明細.getKoseiRiyu())
-                && is比較変更code(現在の利用者負担割合明細.getSetaiCd(), 今回_作成した負担割合明細.getSetaiCd());
-    }
-
-    private boolean is排除負担割合区分の情報の比較(DbT3114RiyoshaFutanWariaiMeisaiEntity 現在の利用者負担割合明細,
-            DbT3114RiyoshaFutanWariaiMeisaiEntity 今回_作成した負担割合明細) {
-        return 現在の利用者負担割合明細.getNendo().equals(今回_作成した負担割合明細.getNendo())
-                && 現在の利用者負担割合明細.getHihokenshaNo().equals(今回_作成した負担割合明細.getHihokenshaNo())
-                && 現在の利用者負担割合明細.getRirekiNo() == 今回_作成した負担割合明細.getRirekiNo()
-                && 現在の利用者負担割合明細.getEdaNo() == 今回_作成した負担割合明細.getEdaNo()
-                && is比較変更RString(現在の利用者負担割合明細.getShikakuKubun(),
-                        今回_作成した負担割合明細.getShikakuKubun())
                 && is比較変更年月日(現在の利用者負担割合明細.getYukoKaishiYMD(),
                         今回_作成した負担割合明細.getYukoKaishiYMD())
                 && is比較変更年月日(現在の利用者負担割合明細.getYukoShuryoYMD(),
@@ -733,6 +768,26 @@ public class RiyoshaFutanWariaiHanteiManager {
             }
         }
         return result;
+    }
+
+    private List<SetaiinShikibetsuCd> remove重複(List<SetaiinShikibetsuCd> 世帯員識別コード情報list) {
+        if (世帯員識別コード情報list == null) {
+            return new ArrayList<>();
+        } else if (世帯員識別コード情報list.isEmpty()) {
+            return 世帯員識別コード情報list;
+        }
+        Map<RString, SetaiinShikibetsuCd> 識別コードMap = new HashMap();
+        for (SetaiinShikibetsuCd コード : 世帯員識別コード情報list) {
+            RString code = コード.get世帯員識別コード().value();
+            if (!識別コードMap.containsKey(code)) {
+                識別コードMap.put(code, コード);
+            }
+        }
+        List<SetaiinShikibetsuCd> tempList = new ArrayList<>();
+        for (RString code : 識別コードMap.keySet()) {
+            tempList.add(識別コードMap.get(code));
+        }
+        return tempList;
     }
 
     private HihokenshaDaicho get被保険者台帳(
