9e8a80591f9979142861ef28c54228bcd687a479
wangxiaodong <wangxiaodong@liandisys.com.cn>
2016-11-11 15:41:08 +0800

    帳票設計_DBE517005_主治医意見書（A3版） アクセスログ 識別コード　横展開対応

--- DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/core/shiryoshinsakai/JimuShinsakaiWariateJohoBusiness.java
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/core/shiryoshinsakai/JimuShinsakaiWariateJohoBusiness.java
@@ -22,6 +22,7 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
  */
 public class JimuShinsakaiWariateJohoBusiness {
 
+    private static final int INDEX_5 = 5;
     private static final RString ファイルID_E0001 = new RString("E0001.png");
     private static final RString ファイルID_E0002 = new RString("E0002.png");
     private static final RString ファイルID_E0001BAK = new RString("E0001_BAK.png");
@@ -62,6 +63,15 @@ public class JimuShinsakaiWariateJohoBusiness {
      */
     public RString get被保険者番号() {
         return entity.getHihokenshaNo();
+    }
+
+    /**
+     * 識別コードは証記載保険者番号（左から5桁）+被保険者番号を設定してください。
+     *
+     * @return 識別コード
+     */
+    public RString get識別コード() {
+        return entity.getShoKisaiHokenshaNo().substring(0, INDEX_5).concat(entity.getHihokenshaNo());
     }
 
     /**
