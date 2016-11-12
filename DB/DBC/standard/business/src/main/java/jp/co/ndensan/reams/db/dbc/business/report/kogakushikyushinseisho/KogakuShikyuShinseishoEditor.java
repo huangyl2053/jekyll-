93ca382d8dbe1500e3c4d68cb952261c26874614
liuxiaoyu <liuxiaoyu@liandisys.com.cn>
2016-11-12 10:36:24 +0800

    帳票アクセスログ出力被保険者番号が空対応一覧

--- DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/kogakushikyushinseisho/KogakuShikyuShinseishoEditor.java
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/kogakushikyushinseisho/KogakuShikyuShinseishoEditor.java
@@ -75,6 +75,7 @@ public class KogakuShikyuShinseishoEditor implements IKogakuShikyuShinseishoEdit
             source.hihokenshaNo = getColumnValue(帳票出力対象データ.getHihokenshaNoChohyo());
             source.kojinNo = getColumnValue(帳票出力対象データ.getKojinNoChohyo());
             source.識別コード = 帳票出力対象データ.getShikibetsuCodeChohyo();
+            source.拡張情報 = new ExpandedInformation(DATA_3, 被保険者番号, source.hihokenshaNo);
         }
         source.hakkoubi = doパターン12(システム日付);
         source.ninshoshaYakushokuMei = 認証者役職名;
@@ -89,7 +90,7 @@ public class KogakuShikyuShinseishoEditor implements IKogakuShikyuShinseishoEdit
         source.shuccho = 文字_出張所;
         source.shisho = 文字_支所;
         source.remban = 連番;
-        source.拡張情報 = new ExpandedInformation(DATA_3, 被保険者番号, source.hihokenshaNo);
+
         return source;
     }
 
