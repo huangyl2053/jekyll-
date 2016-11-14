8af151fff5f249da58c04a47c3d0333a3c6d29fd
徐 赫 <n8343@ndensan.co.jp>
2016-11-14 15:08:04 +0900

    QA #110062対応済み

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/NinteiChosaJokyo/NinteiChosaJokyoHandler.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/NinteiChosaJokyo/NinteiChosaJokyoHandler.java
@@ -7,7 +7,6 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.NinteiCho
 
 import java.util.ArrayList;
 import java.util.List;
-import jp.co.ndensan.reams.db.dbx.definition.core.viewstate.ViewStateKeys;
 import jp.co.ndensan.reams.db.dbz.business.core.ninteichosajokyo.NinteiChosaJokyoDataPass;
 import jp.co.ndensan.reams.db.dbz.definition.core.yokaigojotaikubun.YokaigoJotaiKubun02;
 import jp.co.ndensan.reams.db.dbz.definition.core.yokaigojotaikubun.YokaigoJotaiKubun06;
@@ -22,7 +21,6 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.lang.RStringBuilder;
 import jp.co.ndensan.reams.uz.uza.math.Decimal;
 import jp.co.ndensan.reams.uz.uza.ui.binding.KeyValueDataSource;
-import jp.co.ndensan.reams.uz.uza.ui.servlets.ViewStateHolder;
 import jp.co.ndensan.reams.uz.uza.util.serialization.DataPassingConverter;
 
 /**
@@ -47,7 +45,6 @@ public class NinteiChosaJokyoHandler {
      * Divを初期化する
      */
     public void setLoad() {
-        RString 厚労省IF識別コード = ViewStateHolder.get(ViewStateKeys.厚労省IF識別コード, RString.class);
         NinteiChosaJokyoDataPass konkaiDataPass = DataPassingConverter.deserialize(div.getHdnSerializedBusiness_Konkai(),
                 NinteiChosaJokyoDataPass.class);
         RString displayModeKey = div.getHdnDisplayModeKey();
@@ -62,41 +59,41 @@ public class NinteiChosaJokyoHandler {
             keyValueList.add(new KeyValueDataSource(chosaItaku.getコード(), chosaItaku.get名称()));
         }
         div.getDdlNinteiChosaItakusakiKubun().setDataSource(keyValueList);
-        List<KeyValueDataSource> yokaigodoList = new ArrayList<>();
-        if (!厚労省IF識別コード.isEmpty() && 厚労省IF識別コード != null) {
-            switch (KoroshoIfShikibetsuCode.toValue(厚労省IF識別コード)) {
-                case 認定ｿﾌﾄ2009:
-                case 認定ｿﾌﾄ2009_SP3:
-                    for (YokaigoJotaiKubun09 yokaigoJotaiKubun09 : YokaigoJotaiKubun09.values()) {
-                        yokaigodoList.add(new KeyValueDataSource(yokaigoJotaiKubun09.getコード(),
-                                yokaigoJotaiKubun09.get名称()));
-                    }
-                    break;
-                case 認定ｿﾌﾄ2002:
-                    for (YokaigoJotaiKubun02 yokaigoJotaiKubun02 : YokaigoJotaiKubun02.values()) {
-                        yokaigodoList.add(new KeyValueDataSource(yokaigoJotaiKubun02.getコード(),
-                                yokaigoJotaiKubun02.get名称()));
-                    }
-                    break;
-                case 認定ｿﾌﾄ2006_新要介護認定適用区分が未適用:
-                    for (YokaigoJotaiKubun06 yokaigoJotaiKubun06 : YokaigoJotaiKubun06.values()) {
-                        yokaigodoList.add(new KeyValueDataSource(yokaigoJotaiKubun06.getコード(),
-                                yokaigoJotaiKubun06.get名称()));
-                    }
-                    break;
-                case 認定ｿﾌﾄ99:
-                    for (YokaigoJotaiKubun99 yokaigoJotaiKubun99 : YokaigoJotaiKubun99.values()) {
-                        yokaigodoList.add(new KeyValueDataSource(yokaigoJotaiKubun99.getコード(),
-                                yokaigoJotaiKubun99.get名称()));
-                    }
-                    break;
-                default:
-            }
-            div.getDdlYokaigodo().setDataSource(yokaigodoList);
-            div.getDdlNinchishoKasangoYokaigodo().setDataSource(yokaigodoList);
-            div.getDdlNijiHanteiKekka().setDataSource(yokaigodoList);
-        }
         if (konkaiDataPass != null) {
+            List<KeyValueDataSource> yokaigodoList = new ArrayList<>();
+            if (konkaiDataPass.get厚労省IF識別コード() != null && !konkaiDataPass.get厚労省IF識別コード().isEmpty()) {
+                switch (KoroshoIfShikibetsuCode.toValue(konkaiDataPass.get厚労省IF識別コード().value())) {
+                    case 認定ｿﾌﾄ2009:
+                    case 認定ｿﾌﾄ2009_SP3:
+                        for (YokaigoJotaiKubun09 yokaigoJotaiKubun09 : YokaigoJotaiKubun09.values()) {
+                            yokaigodoList.add(new KeyValueDataSource(yokaigoJotaiKubun09.getコード(),
+                                    yokaigoJotaiKubun09.get名称()));
+                        }
+                        break;
+                    case 認定ｿﾌﾄ2002:
+                        for (YokaigoJotaiKubun02 yokaigoJotaiKubun02 : YokaigoJotaiKubun02.values()) {
+                            yokaigodoList.add(new KeyValueDataSource(yokaigoJotaiKubun02.getコード(),
+                                    yokaigoJotaiKubun02.get名称()));
+                        }
+                        break;
+                    case 認定ｿﾌﾄ2006_新要介護認定適用区分が未適用:
+                        for (YokaigoJotaiKubun06 yokaigoJotaiKubun06 : YokaigoJotaiKubun06.values()) {
+                            yokaigodoList.add(new KeyValueDataSource(yokaigoJotaiKubun06.getコード(),
+                                    yokaigoJotaiKubun06.get名称()));
+                        }
+                        break;
+                    case 認定ｿﾌﾄ99:
+                        for (YokaigoJotaiKubun99 yokaigoJotaiKubun99 : YokaigoJotaiKubun99.values()) {
+                            yokaigodoList.add(new KeyValueDataSource(yokaigoJotaiKubun99.getコード(),
+                                    yokaigoJotaiKubun99.get名称()));
+                        }
+                        break;
+                    default:
+                }
+                div.getDdlYokaigodo().setDataSource(yokaigodoList);
+                div.getDdlNinchishoKasangoYokaigodo().setDataSource(yokaigodoList);
+                div.getDdlNijiHanteiKekka().setDataSource(yokaigodoList);
+            }
             set画面項目(konkaiDataPass);
             div.setHdnInput(get画面項目());
         }
@@ -178,49 +175,66 @@ public class NinteiChosaJokyoHandler {
      *
      * @return 画面の項目値
      */
+    @SuppressWarnings({"empty-statement", "empty-statement"})
     public RString get画面項目() {
         RStringBuilder rsb = new RStringBuilder();
-        rsb.append(div.getTxtNinteiChosainCode().getValue());
-        rsb.append(div.getTxtNinteiChosainName().getValue());
-        rsb.append(div.getTxtNinteiChosaItakusakiCode().getValue());
-        rsb.append(div.getTxtNinteiChosaItakusakiName().getValue());
-        rsb.append(div.getTxtNinteiChosaYubinNo().getValue());
-        rsb.append(div.getTxtNinteiChosaJusho().getDomain().value());
-        rsb.append(div.getTxtNinteiChosaTelNo().getDomain().value());
-        rsb.append(div.getTxtNinteiChosaIraiDate().getValue());
-        rsb.append(div.getTxtNinteiChosaJisshiYoteiDate().getValue());
-        rsb.append(div.getTxtNinteiChosaJisshiDate().getValue());
+        rsb.append(div.getTxtNinteiChosainCode().getText() != null ? div.getTxtNinteiChosainCode().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtNinteiChosainName().getValue() != null ? div.getTxtNinteiChosainName().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtNinteiChosaItakusakiCode().getText() != null ? div.getTxtNinteiChosaItakusakiCode().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtNinteiChosaItakusakiName().getValue() != null ? div.getTxtNinteiChosaItakusakiName().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtNinteiChosaYubinNo().getValue() != null ? div.getTxtNinteiChosaYubinNo().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtNinteiChosaJusho().getDomain() != null ? div.getTxtNinteiChosaJusho().getDomain().value() : RString.EMPTY);
+        try {
+            rsb.append(div.getTxtNinteiChosaTelNo().getDomain() != null ? div.getTxtNinteiChosaTelNo().getDomain().value() : RString.EMPTY);
+        } catch (NullPointerException ex) {
+            rsb.append(RString.EMPTY);
+        };
+        rsb.append(div.getTxtNinteiChosaIraiDate().getValue() != null ? div.getTxtNinteiChosaIraiDate().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtNinteiChosaJisshiYoteiDate().getValue() != null ? div.getTxtNinteiChosaJisshiYoteiDate().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtNinteiChosaJisshiDate().getValue() != null ? div.getTxtNinteiChosaJisshiDate().getValue() : RString.EMPTY);
         rsb.append(div.getDdlNinteiChosaItakusakiKubun().getSelectedKey());
-        rsb.append(div.getTxtChosasakiName().getValue());
-        rsb.append(div.getTxtChosasakiYubinNo().getValue());
-        rsb.append(div.getTxtChosasakiJusho().getDomain().value());
-        rsb.append(div.getTxtChosasakiTelNo().getDomain().value());
-        rsb.append(div.getTxtShujiiCode().getValue());
-        rsb.append(div.getTxtShujiiName().getValue());
-        rsb.append(div.getTxtShujiiIryoKikanCode().getValue());
-        rsb.append(div.getTxtShujiiIryoKikanName().getValue());
-        rsb.append(div.getTxtShujiiIryoKikanYubinNo().getValue());
-        rsb.append(div.getTxtShujiiIryoKikanJusho().getDomain().value());
-        rsb.append(div.getTxtShujiiIryoKikanTelNo().getDomain().value());
-        rsb.append(div.getTxtShujiiIryoKikanIraiDate().getValue());
-        rsb.append(div.getTxtShujiiIryoKikanNyushuYoteiDate().getValue());
-        rsb.append(div.getTxtShujiiIryoKikanNyushuDate().getValue());
+        rsb.append(div.getTxtChosasakiName().getValue() != null ? div.getTxtChosasakiName().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtChosasakiYubinNo().getValue() != null ? div.getTxtChosasakiYubinNo().getValue() : RString.EMPTY);
+        try {
+            rsb.append(div.getTxtChosasakiJusho().getDomain() != null ? div.getTxtChosasakiJusho().getDomain().value() : RString.EMPTY);
+        } catch (NullPointerException ex) {
+            rsb.append(RString.EMPTY);
+        };
+        try {
+            rsb.append(div.getTxtChosasakiTelNo().getDomain() != null ? div.getTxtChosasakiTelNo().getDomain().value() : RString.EMPTY);
+        } catch (NullPointerException ex) {
+            rsb.append(RString.EMPTY);
+        };
+        rsb.append(div.getTxtShujiiCode().getText() != null ? div.getTxtShujiiCode().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtShujiiName().getValue() != null ? div.getTxtShujiiName().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtShujiiIryoKikanCode().getText() != null ? div.getTxtShujiiIryoKikanCode().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtShujiiIryoKikanName().getValue() != null ? div.getTxtShujiiIryoKikanName().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtShujiiIryoKikanYubinNo().getValue() != null ? div.getTxtShujiiIryoKikanYubinNo().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtShujiiIryoKikanJusho().getDomain() != null ? div.getTxtShujiiIryoKikanJusho().getDomain().value() : RString.EMPTY);
+        try {
+            rsb.append(div.getTxtShujiiIryoKikanTelNo().getDomain() != null ? div.getTxtShujiiIryoKikanTelNo().getDomain().value() : RString.EMPTY);
+        } catch (NullPointerException ex) {
+            rsb.append(RString.EMPTY);
+        };
+        rsb.append(div.getTxtShujiiIryoKikanIraiDate().getValue() != null ? div.getTxtShujiiIryoKikanIraiDate().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtShujiiIryoKikanNyushuYoteiDate().getValue() != null ? div.getTxtShujiiIryoKikanNyushuYoteiDate().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtShujiiIryoKikanNyushuDate().getValue() != null ? div.getTxtShujiiIryoKikanNyushuDate().getValue() : RString.EMPTY);
         if (!div.getChkShujiiKubun().getSelectedKeys().isEmpty()) {
             rsb.append(div.getChkShujiiKubun().getSelectedKeys().get(0));
         } else {
             rsb.append(RString.EMPTY);
         }
         rsb.append(div.getDdlYokaigodo().getSelectedKey());
-        rsb.append(div.getTxtIchijiHanteiDate().getValue());
+        rsb.append(div.getTxtIchijiHanteiDate().getValue() != null ? div.getTxtIchijiHanteiDate().getValue() : RString.EMPTY);
         rsb.append(div.getDdlNinchishoKasangoYokaigodo().getSelectedKey());
-        rsb.append(div.getTxtShinsakaiShiryoSakuseiDate().getValue());
-        rsb.append(div.getTxtShinsakaiKaisaiYoteiDate().getValue());
-        rsb.append(div.getTxtNijiHanteiDate().getValue());
+        rsb.append(div.getTxtShinsakaiShiryoSakuseiDate().getValue() != null ? div.getTxtShinsakaiShiryoSakuseiDate().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtShinsakaiKaisaiYoteiDate().getValue() != null ? div.getTxtShinsakaiKaisaiYoteiDate().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtNijiHanteiDate().getValue() != null ? div.getTxtNijiHanteiDate().getValue() : RString.EMPTY);
         rsb.append(div.getDdlNijiHanteiKekka().getSelectedKey());
-        rsb.append(div.getTxtNijiHanteiYukoKikan().getValue());
-        rsb.append(div.getTxtNinteiYukoKikanFrom().getValue());
-        rsb.append(div.getTxtNinteiYukoKikanTo().getValue());
-        rsb.append(div.getTxtEnkiTsuchiHakkoDay().getValue());
+        rsb.append(div.getTxtNijiHanteiYukoKikan().getValue() != null ? div.getTxtNijiHanteiYukoKikan().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtNinteiYukoKikanFrom().getValue() != null ? div.getTxtNinteiYukoKikanFrom().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtNinteiYukoKikanTo().getValue() != null ? div.getTxtNinteiYukoKikanTo().getValue() : RString.EMPTY);
+        rsb.append(div.getTxtEnkiTsuchiHakkoDay().getValue() != null ? div.getTxtEnkiTsuchiHakkoDay().getValue() : RString.EMPTY);
         if (div.getTxtEnkiTsuchiHakkoCount().getValue() != null) {
             rsb.append(div.getTxtEnkiTsuchiHakkoCount().getValue());
         }
@@ -321,13 +335,20 @@ public class NinteiChosaJokyoHandler {
         div.getTxtNinteiChosaItakusakiCode().setValue(konkaiDataPass.get認定調査委託先コード());
         div.getTxtNinteiChosaItakusakiName().setValue(konkaiDataPass.get事業者名称());
         div.getTxtNinteiChosaYubinNo().setValue(konkaiDataPass.get認定郵便番号());
-        div.getTxtNinteiChosaJusho().setDomain(new AtenaJusho(konkaiDataPass.get認定住所()));
+        if (konkaiDataPass.get認定住所() != null && !konkaiDataPass.get認定住所().isEmpty()) {
+            div.getTxtNinteiChosaJusho().setDomain(new AtenaJusho(konkaiDataPass.get認定住所()));
+        }
         div.getTxtNinteiChosaTelNo().setDomain(konkaiDataPass.get認定電話番号());
         div.getTxtNinteiChosaIraiDate().setValue(konkaiDataPass.get認定調査依頼年月日());
         div.getTxtNinteiChosaJisshiYoteiDate().setValue(konkaiDataPass.get認定調査予定年月日());
         div.getTxtNinteiChosaJisshiDate().setValue(konkaiDataPass.get認定調査実施年月日());
-        div.getDdlNinteiChosaItakusakiKubun().setSelectedKey(konkaiDataPass.get調査委託区分());
-        div.getTxtChosasakiName().setValue(konkaiDataPass.get訪問調査先名称().value());
+        div.getDdlNinteiChosaItakusakiKubun().setIsBlankLine(true);
+        if (konkaiDataPass.get調査委託区分() != null && !konkaiDataPass.get調査委託区分().isEmpty()) {
+            div.getDdlNinteiChosaItakusakiKubun().setSelectedKey(konkaiDataPass.get調査委託区分());
+        }
+        if (konkaiDataPass.get訪問調査先名称() != null && !konkaiDataPass.get訪問調査先名称().isEmpty()) {
+            div.getTxtChosasakiName().setValue(konkaiDataPass.get訪問調査先名称().value());
+        }
         div.getTxtChosasakiYubinNo().setValue(konkaiDataPass.get訪問調査先郵便番号());
         div.getTxtChosasakiJusho().setDomain(konkaiDataPass.get訪問調査先住所());
         div.getTxtChosasakiTelNo().setDomain(konkaiDataPass.get訪問調査先電話番号());
@@ -336,7 +357,9 @@ public class NinteiChosaJokyoHandler {
         div.getTxtShujiiIryoKikanCode().setValue(konkaiDataPass.get主治医医療機関コード());
         div.getTxtShujiiIryoKikanName().setValue(konkaiDataPass.get医療機関名称());
         div.getTxtShujiiIryoKikanYubinNo().setValue(konkaiDataPass.get主治医郵便番号());
-        div.getTxtShujiiIryoKikanJusho().setDomain(new AtenaJusho(konkaiDataPass.get主治医住所()));
+        if (konkaiDataPass.get主治医住所() != null && !konkaiDataPass.get主治医住所().isEmpty()) {
+            div.getTxtShujiiIryoKikanJusho().setDomain(new AtenaJusho(konkaiDataPass.get主治医住所()));
+        }
         div.getTxtShujiiIryoKikanTelNo().setDomain(konkaiDataPass.get主治医電話番号());
         div.getTxtShujiiIryoKikanIraiDate().setValue(konkaiDataPass.get主治医意見書作成依頼年月日());
         div.getTxtShujiiIryoKikanNyushuYoteiDate().setValue(konkaiDataPass.get主治医意見書登録予定年月日());
