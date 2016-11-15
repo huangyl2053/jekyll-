db4ac65b9a2bad188e67cf231c00246575580157
張　浩星 <zhanghx@liandisys.com.cn>
2016-11-15 21:33:51 +0800

    コンパイルエラー一旦対応

--- DB/DBB/standard/workflowcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/workflowcontroller/dbbwf82002/DBBWF82002.java
+++ DB/DBB/standard/workflowcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/workflowcontroller/dbbwf82002/DBBWF82002.java
@@ -7,6 +7,7 @@ package jp.co.ndensan.reams.db.dbb.workflowcontroller.dbbwf82002;
 
 import jp.co.ndensan.reams.db.dbb.definition.batchprm.DBB213001.DBB213001_TokuchoSofuJohoRenkeiParameter;
 import jp.co.ndensan.reams.uz.uza.workflow.parameter.FlowParameters;
+import jp.co.ndensan.reams.uz.uza.lang.RString
 
 /**
  * バッチ設計特徴送付情報連携のフローするクラスです。
@@ -23,7 +24,7 @@ public class DBBWF82002 {
      */
     public DBB213001_TokuchoSofuJohoRenkeiParameter toBatchParameter(FlowParameters flowParameter) {
         DBB213001_TokuchoSofuJohoRenkeiParameter parameter
-                = new DBB213001_TokuchoSofuJohoRenkeiParameter();
+                = new DBB213001_TokuchoSofuJohoRenkeiParameter(RString.Empty);
         return parameter;
     }
 
