3e701322684014895ded709023479c88a9c0971e
wangkanglei <wangkanglei@liandisys.com.cn>
2016-11-15 21:21:54 +0900

    本算定異動（過年度）賦課確定（削除込） 改版1.03 対応

--- DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB0550002/dgShoriNichiji_Row.java
+++ DB/DBB/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbb/divcontroller/entity/parentdiv/DBB0550002/dgShoriNichiji_Row.java
@@ -1,9 +1,10 @@
 package jp.co.ndensan.reams.db.dbb.divcontroller.entity.parentdiv.DBB0550002;
+
 /*
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Thu Apr 14 10:26:49 CST 2016
+ * Tue Nov 15 18:14:40 JST 2016
  */
 
 import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
@@ -17,7 +18,7 @@ import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxTime;
  * @reamsid_L DBB-0680-010 wangkanglei
  */
 public class dgShoriNichiji_Row extends DataRow {
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-03-22_14-06-37">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-11-04_20-51-13">
 
     private TextBox txtShoriNichiji;
     private TextBoxDate txtShoriYMD;
