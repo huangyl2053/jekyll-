48042853d818e5c4f3988461832d787dbfc523e3
jinjue <jinjue@liandisys.com.cn>
2016-11-15 16:32:18 +0900

    帳票設計_DBC200004_利用状況一覧表は指摘を対応

--- DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/report/riyojokyotokeihyo/RiyoJokyoIchiranEntity.java
+++ DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/report/riyojokyotokeihyo/RiyoJokyoIchiranEntity.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jp.co.ndensan.reams.db.dbc.entity.db.riyojokyoichiran;
+package jp.co.ndensan.reams.db.dbc.entity.report.riyojokyotokeihyo;
 
 import java.util.List;
 import jp.co.ndensan.reams.db.dbc.entity.db.relate.riyojokyotokeihyomeisailistsakusei.DbWT1513RiyoJokyoTokeihyoEntity;
