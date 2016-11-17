46338c71c33a2ebe1e8929b2b67c96455bb8d1aa
huangh <huangh@liandisys.com>
2016-11-17 22:16:01 +0800

    #109669対応

--- DB/DBC/standard/divcontroller/src/main/webapp/WEB-INF/standard/View/DBC0220012_HiddenTyouhyou.js
+++ DB/DBC/standard/divcontroller/src/main/webapp/WEB-INF/standard/View/DBC0220012_HiddenTyouhyou.js
@@ -1,13 +1,16 @@
 var callBackHandler = Uz._LoadInOrder.prototype.requestSequenceCallbackHandler;
-Uz._LoadInOrder.prototype.requestSequenceCallbackHandler = function(eventName) {
+Uz._LoadInOrder.prototype.requestSequenceCallbackHandler = function (eventName) {
     callBackHandler.call(this, eventName);
-    $(document).on("click", ".ui-dialog-buttonset button", function(event) {
+    $(document).on("click", ".ui-dialog-buttonset button", function (event) {
         if (eventName === "btnUpdate_onClick_btnUpdate" && $("#UzErrorAndWarningDiv").css("display") === 'none') {
             var $span = $(event.target);
             if ($span.text() === "はい" && $("#hdnFlag_core").val() === "1") {
                 if ($("#chkJukyushaTeiseiRearakuhyoHakkou_1").is(':checked')) {
                     $("#btnReportPublish").click();
                 }
+            }
+            if ($span.text() === "はい" && $("#hdnFlag_core").val() === "0") {
+                $("#btnSave").click();
             }
         }
     });
