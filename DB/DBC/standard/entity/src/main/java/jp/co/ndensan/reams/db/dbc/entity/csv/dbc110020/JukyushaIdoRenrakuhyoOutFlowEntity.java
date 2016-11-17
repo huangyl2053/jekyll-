0d897e8fcfdf85271e112824217a3254be2f0d94
chenhui <chenhui@liandisys.com.cn>
2016-11-17 13:13:59 +0800

    ビジネス設計_DBCMNF1001_1_受給者異動連絡票情報編集（バッチ） bug

--- DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/csv/dbc110020/JukyushaIdoRenrakuhyoOutFlowEntity.java
+++ DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/csv/dbc110020/JukyushaIdoRenrakuhyoOutFlowEntity.java
@@ -5,6 +5,8 @@
  */
 package jp.co.ndensan.reams.db.dbc.entity.csv.dbc110020;
 
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
 /**
  * 受給者異動連絡票情報送付対象者リストCSVする。
  *
@@ -17,4 +19,8 @@ public class JukyushaIdoRenrakuhyoOutFlowEntity {
 
     private int 異動連絡票件数;
     private int 訂正連絡票件数;
+    private int レコード件数合計;
+    private RString 入力ファイルパス;
+    private RString 出力ファイルパス;
+
 }
