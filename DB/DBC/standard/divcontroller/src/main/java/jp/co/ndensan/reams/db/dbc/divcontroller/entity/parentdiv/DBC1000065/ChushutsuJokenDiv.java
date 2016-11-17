29031d54d0ef32096fff6c309b3948afebeded15
三浦 凌 <n3327@ndensan.co.jp>
2016-11-18 01:01:15 +0800

    DBC-4640：レイアウト修正、提出期限の位置を変更

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC1000065/ChushutsuJokenDiv.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC1000065/ChushutsuJokenDiv.java
@@ -4,20 +4,17 @@ package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC1000065;
  * このファイルへの変更は、再生成時には損失するため
  * 不正な動作の原因になります。
  */
-
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jp.co.ndensan.reams.uz.uza.ui.binding.Label;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
-import jp.co.ndensan.reams.uz.uza.ui.binding.RadioButton;
 
 /**
- * ChushutsuJoken のクラスファイル
- *
- * @reamsid_L DBC-4640-010 qinzhen
+ * ChushutsuJoken のクラスファイル 
+ * 
+ * @author 自動生成
  */
 public class ChushutsuJokenDiv extends Panel {
-
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-05-30_13-18-33">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-06-27_21-36-36">
     /*
      * [ private の作成 ]
      * クライアント側から取得した情報を元にを検索を行い
--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC1000065/HaakuKijunbiAndTeishutsuKigenDiv.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC1000065/HaakuKijunbiAndTeishutsuKigenDiv.java
@@ -4,22 +4,17 @@ package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC1000065;
  * このファイルへの変更は、再生成時には損失するため
  * 不正な動作の原因になります。
  */
-
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
-import jp.co.ndensan.reams.uz.uza.ui.binding.RadioButton;
-import jp.co.ndensan.reams.uz.uza.ui.binding.Space;
-import jp.co.ndensan.reams.uz.uza.ui.binding.TextBox;
-import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxFlexibleDate;
 
 /**
- * HaakuKijunbiAndTeishutsuKigen のクラスファイル
- *
- * @reamsid_L DBC-4640-010 qinzhen
+ * HaakuKijunbiAndTeishutsuKigen のクラスファイル 
+ * 
+ * @author 自動生成
  */
 public class HaakuKijunbiAndTeishutsuKigenDiv extends Panel {
-
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-05-30_13-18-33">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-06-27_21-36-36">
     /*
      * [ private の作成 ]
      * クライアント側から取得した情報を元にを検索を行い
@@ -30,8 +25,6 @@ public class HaakuKijunbiAndTeishutsuKigenDiv extends Panel {
     private Space blank3;
     @JsonProperty("txtSetaiinHaakuKijunYMD")
     private TextBoxFlexibleDate txtSetaiinHaakuKijunYMD;
-    @JsonProperty("txtTeishutsuKigen")
-    private TextBox txtTeishutsuKigen;
     @JsonProperty("radChushutsuTaisho")
     private RadioButton radChushutsuTaisho;
 
@@ -75,24 +68,6 @@ public class HaakuKijunbiAndTeishutsuKigenDiv extends Panel {
     @JsonProperty("txtSetaiinHaakuKijunYMD")
     public void setTxtSetaiinHaakuKijunYMD(TextBoxFlexibleDate txtSetaiinHaakuKijunYMD) {
         this.txtSetaiinHaakuKijunYMD = txtSetaiinHaakuKijunYMD;
-    }
-
-    /*
-     * gettxtTeishutsuKigen
-     * @return txtTeishutsuKigen
-     */
-    @JsonProperty("txtTeishutsuKigen")
-    public TextBox getTxtTeishutsuKigen() {
-        return txtTeishutsuKigen;
-    }
-
-    /*
-     * settxtTeishutsuKigen
-     * @param txtTeishutsuKigen txtTeishutsuKigen
-     */
-    @JsonProperty("txtTeishutsuKigen")
-    public void setTxtTeishutsuKigen(TextBox txtTeishutsuKigen) {
-        this.txtTeishutsuKigen = txtTeishutsuKigen;
     }
 
     /*
