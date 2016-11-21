fd0dd43ae639160e4765dfaf05f904372a87858f
陳鉢心 <chenbx@liandisys.com.cn>
2016-11-21 14:12:25 +0800

    DBC_3030_DBCMNF2002_国保連情報取込 不具合修正

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC3500011/DBC3500011TransitionEventName.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC3500011/DBC3500011TransitionEventName.java
@@ -1,7 +1,7 @@
 package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC3500011;
 
-import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
+import jp.co.ndensan.reams.uz.uza.core.ui.response.IContainerEvents;
 
 /**
  * このコードはツールによって生成されました。</br>
@@ -9,7 +9,7 @@ import jp.co.ndensan.reams.uz.uza.lang.RString;
  * 不正な動作の原因になります。</br>
  * DBC3500011のイベント名定義クラスです</br>
  *
- * @reamsid_L DBC-3030-010 lihang
+ * @author 自動生成
  */
 public enum DBC3500011TransitionEventName implements IContainerEvents {
 // <editor-fold defaultstate="collapsed" desc="Created By UI Designer ver 1.0.0">
