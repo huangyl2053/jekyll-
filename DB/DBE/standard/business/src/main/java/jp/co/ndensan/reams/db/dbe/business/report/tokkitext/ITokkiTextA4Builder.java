000a84c9271f6ae5c430c3f7af6a559be0932b36
lishengli <lishengli@liandisys.com.cn>
2016-11-21 14:23:46 +0800

    介護認定審査会資料作成　機能ＩＤ修正

--- DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/tokkitext/ITokkiTextA4Builder.java
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/tokkitext/ITokkiTextA4Builder.java
@@ -11,7 +11,7 @@ import jp.co.ndensan.reams.uz.uza.report.IReportBuilder;
 /**
  * 特記事項A4版Builderのインターフェースクラスです。
  *
- * @reamsid_L DBE-0150-200 lishengli
+ * @reamsid_L DBE-0150-340 lishengli
  */
 public interface ITokkiTextA4Builder extends IReportBuilder<TokkiTextA4ReportSource> {
 
