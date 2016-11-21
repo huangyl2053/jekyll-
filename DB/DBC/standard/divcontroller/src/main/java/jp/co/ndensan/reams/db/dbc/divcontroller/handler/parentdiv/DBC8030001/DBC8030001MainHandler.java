23419561cd52ad4c5ccf96d75d9425883c087262
曹緩緩 <caohh@liandisys.com.cn>
2016-11-21 17:15:44 +0800

    受入 #111071

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/parentdiv/DBC8030001/DBC8030001MainHandler.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/parentdiv/DBC8030001/DBC8030001MainHandler.java
@@ -85,12 +85,12 @@ public class DBC8030001MainHandler {
                 }
             }
         }
-
-        div.getCcdFurikomiBaitaiSakusei().initialize(SubGyomuCode.DBC介護給付, list, IFurikomiBaitaiSakuseiDiv.UnyoHohoShokiHyoji.媒体,
-                GyomuCode.DB介護保険, NinshoshaDenshikoinshubetsuCode.保険者印.getコード(), RString.EMPTY);
         div.getCcdFurikomiBaitaiSakusei().setTestBaitaiSakuseiMode(FurikomiBaitaiSakuseiDiv.TestBaitaiSakuseiMode.通常);
         div.getCcdFurikomiBaitaiSakusei().setCommonChohyoSakuseiMode(FurikomiBaitaiSakuseiDiv.CommonChohyoSakuseiMode.送付書＿明細書);
         div.getCcdFurikomiBaitaiSakusei().setSelectButtonDisplayMode(FurikomiBaitaiSakuseiDiv.SelectButtonDisplayMode.表示しない);
         div.getTxtShoriTaisho().setReadOnly(true);
+        div.getCcdFurikomiBaitaiSakusei().initialize(SubGyomuCode.DBC介護給付, list, IFurikomiBaitaiSakuseiDiv.UnyoHohoShokiHyoji.媒体,
+                GyomuCode.DB介護保険, NinshoshaDenshikoinshubetsuCode.保険者印.getコード(), RString.EMPTY);
+
     }
 }
