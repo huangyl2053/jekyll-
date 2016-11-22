2df953349113b4fcf1f883fdddace7a4ee2b01fd
陳鉢心 <chenbx@liandisys.com.cn>
2016-11-22 21:27:06 +0800

    不具合対応【662】総合事業費再審査決定通知書情報(公費負担者分）

--- DB/DBC/standard/service/src/main/java/jp/co/ndensan/reams/db/dbc/service/core/saishinsaketteihokenshainkouhifutann/SaishinsaKetteiHokenshaInKouhiFutann.java
+++ DB/DBC/standard/service/src/main/java/jp/co/ndensan/reams/db/dbc/service/core/saishinsaketteihokenshainkouhifutann/SaishinsaKetteiHokenshaInKouhiFutann.java
@@ -439,7 +439,7 @@ public class SaishinsaKetteiHokenshaInKouhiFutann {
                     List<RString> data = csvReader.readLine();
                     if (data != null && !data.isEmpty()) {
                         if (エンドレコード種別.equals(data.get(INDEX_0))) {
-                            continue;
+                            break;
                         }
                         if (レコード種別.equals(data.get(INDEX_0))) {
                             controlCsvEntity = ListToObjectMappingHelper.toObject(KagoKetteiHokenshaInControlCsvEntity.class, data);
