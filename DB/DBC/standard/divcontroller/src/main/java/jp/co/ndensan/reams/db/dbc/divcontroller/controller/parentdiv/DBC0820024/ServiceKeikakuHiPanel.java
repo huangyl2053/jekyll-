097de77795100dc441b7f4d82158ef44eb7ac56e
liuwei2 <liuwei2@liandisys.com.cn>
2016-11-22 16:44:17 +0800

    仕様変更を対応

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC0820024/ServiceKeikakuHiPanel.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/controller/parentdiv/DBC0820024/ServiceKeikakuHiPanel.java
@@ -65,6 +65,8 @@ public class ServiceKeikakuHiPanel {
     private static final RString 登録 = new RString("登録");
     private static final RString 修正 = new RString("修正");
     private static final RString 確定するボタン = new RString("Element1");
+    private static final RString NUM1 = new RString("1");
+    private static final RString NUM0 = new RString("0");
 
     /**
      * 画面初期化onLoad
@@ -93,6 +95,7 @@ public class ServiceKeikakuHiPanel {
             開始時点の償還払サービス計画費 = new DbJohoViewState();
             ViewStateHolder.put(ViewStateKeys.開始時点の償還払サービス計画費, 開始時点の償還払サービス計画費);
         }
+        ViewStateHolder.put(ViewStateKeys.判定結果, false);
         ShikibetsuCode 識別コード = ViewStateHolder.get(ViewStateKeys.識別コード, ShikibetsuCode.class);
         RString 画面モード = ViewStateHolder.get(ViewStateKeys.処理モード, RString.class);
         div.getPanelCcd().getCcdKaigoAtenaInfo().initialize(識別コード);
@@ -347,6 +350,7 @@ public class ServiceKeikakuHiPanel {
             DbJohoViewState 償還払ViewStateDBBAK = ViewStateHolder.get(ViewStateKeys.償還払ViewStateDBBAK, DbJohoViewState.class);
             ViewStateHolder.put(ViewStateKeys.償還払ViewStateDB, 償還払ViewStateDBBAK);
             resetKensakuData();
+            ViewStateHolder.put(ViewStateKeys.証明書戻り, NUM0);
             return ResponseData.of(div).forwardWithEventName(DBC0820024TransitionEventName.一覧に戻る).respond();
         }
         return createResponse(div);
@@ -364,6 +368,7 @@ public class ServiceKeikakuHiPanel {
             return saveOut(div);
         } else {
             resetKensakuData();
+            ViewStateHolder.put(ViewStateKeys.証明書戻り, NUM1);
             return 保存処理(div, DBC0820024TransitionEventName.一覧に戻る);
         }
     }
@@ -458,6 +463,15 @@ public class ServiceKeikakuHiPanel {
             証明書変更済フラグ.setサービス計画費_証明書変更済フラグ(is変更 ? ShomeishoHenkoKubunType.変更あり : ShomeishoHenkoKubunType.変更なし);
             証明書変更済フラグMap.put(明細キー, 証明書変更済フラグ);
             償還払ViewStateDB.set証明書変更済フラグMap(証明書変更済フラグMap);
+            Map<ShoukanharaihishinseimeisaikensakuParameter, ShomeishoNyuryokuFlag> 証明書入力済フラグMap = 償還払ViewStateDB.get証明書入力済フラグMap();
+            Map<ShoukanharaihishinseimeisaikensakuParameter, ShomeishoNyuryokuKanryoKubunType> 証明書入力完了フラグMap = 償還払ViewStateDB.get証明書入力完了フラグMap();
+            ShomeishoNyuryokuFlag 証明書入力済フラグ = 証明書入力済フラグMap.get(明細キー);
+            証明書入力済フラグ.setサービス計画費_証明書入力済フラグ(is変更 ? ShomeishoNyuryokuKubunType.入力あり : ShomeishoNyuryokuKubunType.入力なし);
+            証明書入力済フラグMap.put(明細キー, 証明書入力済フラグ);
+            償還払ViewStateDB.set証明書入力済フラグMap(証明書入力済フラグMap);
+            if (DBC0820024TransitionEventName.一覧に戻る == eventName) {
+                set証明書完了フラグ(証明書入力済フラグ, 明細キー, 証明書入力完了フラグMap, 償還払ViewStateDB);
+            }
         }
         return 償還払ViewStateDB;
     }
