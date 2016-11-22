f64e9938f8052cbc80a44340ca8a1fab55f1148d
dongyabin <dongyabin@liandisys.com.cn>
2016-11-22 10:39:26 +0800

    1.03ビジネス設計_DBCMNJ1001_受給者給付実績台帳 レビユー指摘の対応

--- DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/db/relate/jukyushakyufujissekidaicho/TokuteiShinryouHiEntity.java
+++ DB/DBC/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbc/entity/db/relate/jukyushakyufujissekidaicho/TokuteiShinryouHiEntity.java
@@ -112,5 +112,7 @@ public class TokuteiShinryouHiEntity extends DbTableEntityBase<TokuteiShinryouHi
     private int 後公費３合計点数;
     @TempTableColumnOrder(39)
     private RString 略称;
+    @TempTableColumnOrder(40)
+    private RString サービス名称;
 
 }
