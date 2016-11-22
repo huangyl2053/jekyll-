b805b8d6af914f8f6eee79d99130a197315bc99b
城間 篤人 <n8178@ndensan.co.jp>
2016-11-22 13:55:13 +0900

    受入 #111140対応 【不具合】DBE_1841_DBEMN52002_介護認定審査会審査対象データ取込み（モバイル）　画面デザイン不良

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/YokaigoNinteiShinsakaiIchiranList/YokaigoNinteiShinsakaiIchiranList/HyojiJokenDiv.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/YokaigoNinteiShinsakaiIchiranList/YokaigoNinteiShinsakaiIchiranList/HyojiJokenDiv.java
@@ -14,7 +14,7 @@ import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
  * @reamsid_L DBE-0120-010 lishengli
  */
 public class HyojiJokenDiv extends Panel {
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-07-28_11-34-20">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-11-04_20-51-13">
     /*
      * [ private の作成 ]
      * クライアント側から取得した情報を元にを検索を行い
