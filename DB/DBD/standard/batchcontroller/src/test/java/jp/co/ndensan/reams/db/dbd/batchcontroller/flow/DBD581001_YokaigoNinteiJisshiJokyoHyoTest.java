b6fe0ee0195b7d271581b654f107cfcf422edbdd
徐 赫 <n8343@ndensan.co.jp>
2016-11-22 22:39:56 +0900

    受入 #106997 ⑥対応

--- DB/DBD/standard/batchcontroller/src/test/java/jp/co/ndensan/reams/db/dbd/batchcontroller/flow/DBD581001_YokaigoNinteiJisshiJokyoHyoTest.java
+++ DB/DBD/standard/batchcontroller/src/test/java/jp/co/ndensan/reams/db/dbd/batchcontroller/flow/DBD581001_YokaigoNinteiJisshiJokyoHyoTest.java
@@ -6,17 +6,15 @@
 package jp.co.ndensan.reams.db.dbd.batchcontroller.flow;
 
 import jp.co.ndensan.reams.uz.uza.auth.manage.mappers._AuthAuthorityDeputyManageMapperTest.テスト.*;
-import jp.co.ndensan.reams.db.dbd.definition.batchprm.DBD532001.DBD532001_NinteiKekkaTsuchiParameter;
+import jp.co.ndensan.reams.db.dbd.definition.batchprm.DBD581001.DBD581001_YokaigoNinteiJisshiJokyoHyoParameter;
 import jp.co.ndensan.reams.db.dbz.testhelper.DbdTestDacBase;
-import jp.co.ndensan.reams.ua.uax.business.core.shikibetsutaisho.search.ShikibetsuTaishoPSMSearchKeyBuilder;
-import jp.co.ndensan.reams.ua.uax.definition.core.enumeratedtype.shikibetsutaisho.KensakuYusenKubun;
 import jp.co.ndensan.reams.ua.uax.definition.mybatisprm.shikibetsutaisho.IShikibetsuTaishoPSMSearchKey;
 import jp.co.ndensan.reams.uz.uza.batch.BatchExitStatus;
 import jp.co.ndensan.reams.uz.uza.batch.config.BatchSettings;
-import jp.co.ndensan.reams.uz.uza.biz.GyomuCode;
+import jp.co.ndensan.reams.uz.uza.biz.Code;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
+import jp.co.ndensan.reams.uz.uza.lang.FlexibleYear;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.lang.RTime;
 import jp.co.ndensan.reams.uz.uza.testhelper.BatchFlowTestHelper;
 import jp.co.ndensan.reams.uz.uza.testhelper.DbTestHelper;
 import org.junit.AfterClass;
@@ -30,14 +28,18 @@ import org.junit.runner.RunWith;
  * @author n8343
  */
 @RunWith(Enclosed.class)
-public class DBD532001_NinteiTsuchishoHakkoTest extends DbdTestDacBase {
+public class DBD581001_YokaigoNinteiJisshiJokyoHyoTest extends DbdTestDacBase {
 
     private static DbTestHelper dbHelper;
-    private static DBD532001_NinteiKekkaTsuchiParameter parameter;
+    //private static DBD581001_YokaigoNinteiJisshiJokyoHyoParameter parameter;
     private static BatchExitStatus result;
-    private static final RString batchID = new RString("DBD532001_NinteiKekkaTsuchiParameter");
-    private static final RString batchName = new RString("一括発行Test");
+    private static FlexibleYear 対象年度;
+    private static final RString batchID = new RString("DBD581001_YokaigoNinteiJisshiJokyoHyoParameter");
+    private static final RString batchName = new RString("要介護認定実施状況表Test");
+    private static final RString 月日FROM = new RString("0401");
+    private static final RString 月日TO = new RString("0331");
     private static IShikibetsuTaishoPSMSearchKey searchKey;
+    private static final RString 要介護認定実施状況表 = new RString("要介護認定実施状況表（その月に申請・認定を受けた受給者の統計）");
 
     @BeforeClass
     public static void setUpClass() {
@@ -45,31 +47,16 @@ public class DBD532001_NinteiTsuchishoHakkoTest extends DbdTestDacBase {
 
     }
 
-    public static class 一括発行Test extends DbdTestDacBase {
+    public static class 要介護認定実施状況表Test extends DbdTestDacBase {
 
         @Test
-        public void 認定結果通知書testes() {
+        public void 要介護認定実施状況表test() {
             BatchFlowTestHelper testhelper = new BatchFlowTestHelper();
             BatchSettings bs = new BatchSettings();
-            result = testhelper.executeFlow(batchID, batchName, DBD532001_NinteiKekkaTsuchi.class, 認定結果通知書発行(), bs);
+            result = testhelper.executeFlow(batchID, batchName, DBD581001_YokaigoNinteiJisshiJokyoHyo.class, 要介護認定実施状況表(), bs);
 
         }
 
-        @Test
-        public void サービス通知書発行testes() {
-            BatchFlowTestHelper testhelper = new BatchFlowTestHelper();
-            BatchSettings bs = new BatchSettings();
-            result = testhelper.executeFlow(batchID, batchName, DBD532001_NinteiKekkaTsuchi.class, サービス通知書発行(), bs);
-            //assertThat(result);
-        }
-
-        @Test
-        public void 要介護区分通知書発行testes() {
-            BatchFlowTestHelper testhelper = new BatchFlowTestHelper();
-            BatchSettings bs = new BatchSettings();
-            result = testhelper.executeFlow(batchID, batchName, DBD532001_NinteiKekkaTsuchi.class, 要介護区分通知書発行(), bs);
-            //assertThat(result);
-        }
     }
 
     @AfterClass
@@ -78,49 +65,22 @@ public class DBD532001_NinteiTsuchishoHakkoTest extends DbdTestDacBase {
         DbdTestDacBase.tearDownClass();
     }
 
-    private static DBD532001_NinteiKekkaTsuchiParameter 認定結果通知書発行() {
-        DBD532001_NinteiKekkaTsuchiParameter pa = new DBD532001_NinteiKekkaTsuchiParameter();
-        searchKey = new ShikibetsuTaishoPSMSearchKeyBuilder(GyomuCode.DB介護保険, KensakuYusenKubun.未定義).build();
-        pa.set発行日(new FlexibleDate("20161121"));
-        pa.set単票発行済み(false);
-        pa.set出力順(new RString("order by \"DbT4001JukyushaDaicho\".\"hihokenshaNo\" asc"));
-        pa.set認定結果通知書文書番号(new RString("第号"));
-        pa.set認定結果通知書今回開始日付(new FlexibleDate("20161028"));
-        pa.set認定結果通知書今回終了日付(new FlexibleDate("20161121"));
-        pa.set認定結果通知書今回開始日時(new RTime(new RString("12:51:01.000")));
-        pa.set認定結果通知書今回終了日時(RTime.now());
-        pa.toNinteiKekkaTsutishoProcessParameter(searchKey);
-        return pa;
-    }
-
-    private static DBD532001_NinteiKekkaTsuchiParameter サービス通知書発行() {
-        DBD532001_NinteiKekkaTsuchiParameter pa = new DBD532001_NinteiKekkaTsuchiParameter();
-        searchKey = new ShikibetsuTaishoPSMSearchKeyBuilder(GyomuCode.DB介護保険, KensakuYusenKubun.未定義).build();
-        pa.set発行日(new FlexibleDate("20161121"));
-        pa.set単票発行済み(false);
-        pa.set出力順(new RString("order by \"DbT4001JukyushaDaicho\".\"hihokenshaNo\" asc"));
-        pa.setサービス変更通知書文書番号(new RString("第号"));
-        pa.setサービス変更通知書今回開始日付(new FlexibleDate("20161028"));
-        pa.setサービス変更通知書今回終了日付(new FlexibleDate("20161121"));
-        pa.setサービス変更通知書今回開始日時(new RTime(new RString("12:51:01.000")));
-        pa.setサービス変更通知書今回終了日時(RTime.now());
-        pa.toServiceHenkoTsutishoProcessParameter(searchKey);
-        return pa;
-    }
-
-    private static DBD532001_NinteiKekkaTsuchiParameter 要介護区分通知書発行() {
-        DBD532001_NinteiKekkaTsuchiParameter pa = new DBD532001_NinteiKekkaTsuchiParameter();
-        searchKey = new ShikibetsuTaishoPSMSearchKeyBuilder(GyomuCode.DB介護保険, KensakuYusenKubun.未定義).build();
-        pa.set発行日(new FlexibleDate("20161121"));
-        pa.set単票発行済み(false);
-        pa.set出力順(new RString("order by \"DbT4001JukyushaDaicho\".\"hihokenshaNo\" asc"));
-        pa.set要介護区分変更通知書文書番号(new RString("第号"));
-        pa.set要介護区分変更通知書今回開始日付(new FlexibleDate("20161028"));
-        pa.set要介護区分変更通知書今回終了日付(new FlexibleDate("20161121"));
-        pa.set要介護区分変更通知書今回開始日時(new RTime(new RString("12:51:01.000")));
-        pa.set要介護区分変更通知書今回終了日時(RTime.now());
-        pa.toYokaigoHenkoTsutishoProcessParameter(searchKey);
-        return pa;
+    private static DBD581001_YokaigoNinteiJisshiJokyoHyoParameter 要介護認定実施状況表() {
+        DBD581001_YokaigoNinteiJisshiJokyoHyoParameter parameter = new DBD581001_YokaigoNinteiJisshiJokyoHyoParameter();
+        対象年度 = new FlexibleYear("2016");
+        parameter.set出力帳票(要介護認定実施状況表);
+        parameter.set年齢基準日(new FlexibleDate("20161122"));
+        parameter.set年齢From(12);
+        parameter.set年齢To(69);
+        parameter.set生年月日From(new FlexibleDate(""));
+        parameter.set生年月日To(new FlexibleDate(""));
+        parameter.set地区区分(new RString("ALL"));
+        parameter.set開始地区コード(Code.EMPTY);
+        parameter.set終了地区コード(Code.EMPTY);
+        parameter.set集計単位(new RString("ZIYU"));
+        parameter.set基準日(RString.EMPTY);
+        parameter.set対象年度(対象年度);
+        return parameter;
     }
 
 }
