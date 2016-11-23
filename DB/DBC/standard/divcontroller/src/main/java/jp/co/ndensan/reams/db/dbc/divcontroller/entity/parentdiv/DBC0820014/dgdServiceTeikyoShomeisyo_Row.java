9bbec830f52f71037a0d580beb26ed1bc647179c
chenxin2 <chenxin2@liandisys.com.cn>
2016-11-23 11:36:45 +0800

    1030仕様変更対応

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0820014/dgdServiceTeikyoShomeisyo_Row.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/entity/parentdiv/DBC0820014/dgdServiceTeikyoShomeisyo_Row.java
@@ -3,47 +3,65 @@ package jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC0820014;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Thu May 12 11:02:17 CST 2016
+ * Tue Nov 22 11:43:08 CST 2016 
  */
 
-import jp.co.ndensan.reams.uz.uza.lang.RString;
+
+
 import jp.co.ndensan.reams.uz.uza.ui.binding.DataRow;
+import jp.co.ndensan.reams.uz.uza.lang.RString;
+
 
 /**
- * dgdServiceTeikyoShomeisyo_Row のクラスファイル
- *
- * @reamsid_L DBC-1030-130 gongliang
+ * dgdServiceTeikyoShomeisyo_Row のクラスファイル 
+ * 
+ * @author 自動生成
  */
-public class dgdServiceTeikyoShomeisyo_Row extends DataRow {
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-03-22_14-06-37">
 
+public class dgdServiceTeikyoShomeisyo_Row extends DataRow {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-11-04_20-51-13">
+
+    private RString shori;
     private RString data1;
     private RString data2;
     private RString data3;
     private RString data4;
+    private RString shoriKubun;
 
     public dgdServiceTeikyoShomeisyo_Row() {
         super();
+        this.shori = RString.EMPTY;
         this.data1 = RString.EMPTY;
         this.data2 = RString.EMPTY;
         this.data3 = RString.EMPTY;
         this.data4 = RString.EMPTY;
+        this.shoriKubun = RString.EMPTY;
+        this.setOriginalData("shori", shori);
         this.setOriginalData("data1", data1);
         this.setOriginalData("data2", data2);
         this.setOriginalData("data3", data3);
         this.setOriginalData("data4", data4);
+        this.setOriginalData("shoriKubun", shoriKubun);
     }
 
-    public dgdServiceTeikyoShomeisyo_Row(RString data1, RString data2, RString data3, RString data4) {
+    public dgdServiceTeikyoShomeisyo_Row(RString shori, RString data1, RString data2, RString data3, RString data4, RString shoriKubun) {
         super();
+        this.setOriginalData("shori", shori);
         this.setOriginalData("data1", data1);
         this.setOriginalData("data2", data2);
         this.setOriginalData("data3", data3);
         this.setOriginalData("data4", data4);
+        this.setOriginalData("shoriKubun", shoriKubun);
+        this.shori = shori;
         this.data1 = data1;
         this.data2 = data2;
         this.data3 = data3;
         this.data4 = data4;
+        this.shoriKubun = shoriKubun;
+    }
+
+    public RString getShori() {
+        return shori;
     }
 
     public RString getData1() {
@@ -60,6 +78,15 @@ public class dgdServiceTeikyoShomeisyo_Row extends DataRow {
 
     public RString getData4() {
         return data4;
+    }
+
+    public RString getShoriKubun() {
+        return shoriKubun;
+    }
+
+    public void setShori(RString shori) {
+        this.setOriginalData("shori", shori);
+        this.shori = shori;
     }
 
     public void setData1(RString data1) {
@@ -80,6 +107,11 @@ public class dgdServiceTeikyoShomeisyo_Row extends DataRow {
     public void setData4(RString data4) {
         this.setOriginalData("data4", data4);
         this.data4 = data4;
+    }
+
+    public void setShoriKubun(RString shoriKubun) {
+        this.setOriginalData("shoriKubun", shoriKubun);
+        this.shoriKubun = shoriKubun;
     }
 
     // </editor-fold>
