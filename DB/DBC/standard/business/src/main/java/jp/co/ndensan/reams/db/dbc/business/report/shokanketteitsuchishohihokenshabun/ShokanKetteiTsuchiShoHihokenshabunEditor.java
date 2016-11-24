0d0f4c2f5750ee68cb86f8f22a7c63680e6c8285
韓旭 <hanxu2@liandisys.com.cn>
2016-11-24 16:33:00 +0800

    受入 #111333対応

--- DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/shokanketteitsuchishohihokenshabun/ShokanKetteiTsuchiShoHihokenshabunEditor.java
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/shokanketteitsuchishohihokenshabun/ShokanKetteiTsuchiShoHihokenshabunEditor.java
@@ -124,7 +124,7 @@ public class ShokanKetteiTsuchiShoHihokenshabunEditor implements IShokanKetteiTs
         source.samaBun1 = item.getSamaBun1();
         source.kakkoRight1 = item.getKakkoRight1();
         source.samabunShimeiSmall1 = item.getSamabunShimeiSmall1();
-
+        source.customerBarCode = item.getCustomerBarCode();
         return source;
     }
 }
