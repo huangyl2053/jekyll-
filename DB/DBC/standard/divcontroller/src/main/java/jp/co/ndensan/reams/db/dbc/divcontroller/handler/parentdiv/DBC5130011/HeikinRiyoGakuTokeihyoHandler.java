e386f77f41aef9145aa9698c74b7bb03c411e862
wanghuafeng <wanghuafeng@liandisys.com.cn>
2016-11-25 11:11:36 +0800

    受入 #111419

--- DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/parentdiv/DBC5130011/HeikinRiyoGakuTokeihyoHandler.java
+++ DB/DBC/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/divcontroller/handler/parentdiv/DBC5130011/HeikinRiyoGakuTokeihyoHandler.java
@@ -6,7 +6,7 @@
 package jp.co.ndensan.reams.db.dbc.divcontroller.handler.parentdiv.DBC5130011;
 
 import java.util.Map;
-import jp.co.ndensan.reams.db.dbc.definition.batchprm.DBC150040.DBC150040_HeikinriyogakuTokeihyoBatchParameter;
+import jp.co.ndensan.reams.db.dbc.definition.batchprm.DBC150040.DBC150040_HeikinriyogakuTokeihyoParameter;
 import jp.co.ndensan.reams.db.dbc.divcontroller.entity.parentdiv.DBC5130011.HeikinRiyoGakuTokeihyoDiv;
 import jp.co.ndensan.reams.uz.uza.batch.parameter.BatchParameterMap;
 import jp.co.ndensan.reams.uz.uza.lang.RDate;
@@ -33,10 +33,10 @@ public class HeikinRiyoGakuTokeihyoHandler {
     /**
      * 平均利用額統計表作成のBatchParameterを作成します。
      *
-     * @return DBC150040_HeikinriyogakuTokeihyoBatchParameter
+     * @return DBC150040_HeikinriyogakuTokeihyoParameter
      */
-    public DBC150040_HeikinriyogakuTokeihyoBatchParameter getBatchParameter() {
-        DBC150040_HeikinriyogakuTokeihyoBatchParameter parameter = new DBC150040_HeikinriyogakuTokeihyoBatchParameter();
+    public DBC150040_HeikinriyogakuTokeihyoParameter getBatchParameter() {
+        DBC150040_HeikinriyogakuTokeihyoParameter parameter = new DBC150040_HeikinriyogakuTokeihyoParameter();
 
         RString key = div.getRadTaishoYM().getSelectedKey();
         parameter.setTaishoNendoYM(key);
@@ -50,7 +50,7 @@ public class HeikinRiyoGakuTokeihyoHandler {
         }
         RString 地区指定 = div.getChikuShitei().getCcdChikuShichosonSelect().get選択対象();
         parameter.setChikuShitei(地区指定);
-        Map<RString, RString> 選択地区Map = div.getChikuShitei().getCcdChikuShichosonSelect().get選択結果();
+        Map<String, String> 選択地区Map = div.getChikuShitei().getCcdChikuShichosonSelect().get選択結果();
         parameter.setSelectorChikulist(選択地区Map);
         parameter.setShichosonCode(div.getChikuShitei().getCcdChikuShichosonSelect().get市町村コード());
         parameter.setShichosonMei(div.getChikuShitei().getCcdChikuShichosonSelect().get市町村名称());
@@ -85,7 +85,7 @@ public class HeikinRiyoGakuTokeihyoHandler {
         RString 地区指定 = restoreBatchParameterMap.getParameterValue(RString.class, new RString("chikuShitei"));
         div.getChikuShitei().getCcdChikuShichosonSelect().set選択対象(地区指定);
         div.getChikuShitei().getCcdChikuShichosonSelect().onChange_選択対象DDL();
-        Map<RString, RString> 選択地区Map = restoreBatchParameterMap.getParameterValue(Map.class, new RString("selectorChikulist"));
+        Map<String, String> 選択地区Map = restoreBatchParameterMap.getParameterValue(Map.class, new RString("selectorChikulist"));
         div.getChikuShitei().getCcdChikuShichosonSelect().set選択結果(選択地区Map);
         RString 市町村コード = restoreBatchParameterMap.getParameterValue(RString.class, new RString("shichosonCode"));
         div.getChikuShitei().getCcdChikuShichosonSelect().set市町村コード(市町村コード);
