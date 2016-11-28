67dfeed946874a160d4bde0a16c4ea0dd0f539b0
陳鉢心 <chenbx@liandisys.com.cn>
2016-11-28 14:36:19 +0800

    不具合修正DBCMNF2002

--- DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/kokuhorenjukyushadaichoichiran/KokuhorenJukyushaDaichoIchiranBodyEditor.java
+++ DB/DBC/standard/business/src/main/java/jp/co/ndensan/reams/db/dbc/business/report/kokuhorenjukyushadaichoichiran/KokuhorenJukyushaDaichoIchiranBodyEditor.java
@@ -260,8 +260,10 @@ public class KokuhorenJukyushaDaichoIchiranBodyEditor
             return RString.EMPTY;
         }
         if (FlexibleDate.canConvert(年月日)) {
-            return new FlexibleDate(年月日).wareki().eraType(EraType.KANJI_RYAKU).firstYear(FirstYear.ICHI_NEN).separator(Separator.PERIOD)
-                    .fillType(FillType.BLANK).toDateString();
+            if (new FlexibleDate(年月日).isWareki()) {
+                return new FlexibleDate(年月日).wareki().eraType(EraType.KANJI_RYAKU).firstYear(FirstYear.ICHI_NEN).separator(Separator.PERIOD)
+                        .fillType(FillType.BLANK).toDateString();
+            }
         }
         return 年月日;
     }
