cd3dfb26bce3cadab6d40cae3908fcd1fd50901c
陳鉢心 <chenbx@liandisys.com.cn>
2016-11-28 09:34:07 +0800

    不具合対応 再審査決定通知書情報取込

--- DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/flow/DBC120190_SaishinsaKetteiHokenshaIn.java
+++ DB/DBC/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbc/batchcontroller/flow/DBC120190_SaishinsaKetteiHokenshaIn.java
@@ -48,7 +48,7 @@ public class DBC120190_SaishinsaKetteiHokenshaIn extends BatchFlowBase<DBC120190
     private static final String 一覧表作成 = "doIchiranhyoSakusei";
     private static final String 処理結果リスト作成 = "doShoriKekkaListSakusei";
     private static final String 取込済ファイル削除 = "deleteReveicedFile";
-    private static final RString マスタ登録BATCHID = new RString("SaishinsaKetteiDBToroku");
+    private static final RString マスタ登録BATCHID = new RString("DBC120191_SaishinsaKetteiDBToroku");
     private static final RString ファイル格納フォルダ名 = new RString("DBC120080");
 
     private KokuhorenKyoutsuuFileGetReturnEntity returnEntity;
