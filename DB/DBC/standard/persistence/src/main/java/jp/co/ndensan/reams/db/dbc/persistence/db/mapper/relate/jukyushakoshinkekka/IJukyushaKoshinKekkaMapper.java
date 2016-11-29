8e9930a763082357e0e5c22f30121e888a4aacfa
王　巍 <wangwei3@liandisys.com.cn>
2016-11-29 17:58:57 +0800

    受入 #111540　バグ対応

--- DB/DBC/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbc/persistence/db/mapper/relate/jukyushakoshinkekka/IJukyushaKoshinKekkaMapper.java
+++ DB/DBC/standard/persistence/src/main/java/jp/co/ndensan/reams/db/dbc/persistence/db/mapper/relate/jukyushakoshinkekka/IJukyushaKoshinKekkaMapper.java
@@ -6,7 +6,9 @@
 package jp.co.ndensan.reams.db.dbc.persistence.db.mapper.relate.jukyushakoshinkekka;
 
 import java.util.List;
+import jp.co.ndensan.reams.db.dbc.definition.mybatisprm.jukyushakoshinkekka.DbT3104KensakuMybatisParameter;
 import jp.co.ndensan.reams.db.dbc.definition.mybatisprm.jukyushakoshinkekka.JukyushaKoshinKekkaMybatisParameter;
+import jp.co.ndensan.reams.db.dbc.entity.db.basic.DbT3104KokuhorenInterfaceKanriEntity;
 import jp.co.ndensan.reams.db.dbc.entity.db.relate.jukyushajoho.JukyushaHihokenshaEntity;
 
 /**
@@ -23,4 +25,12 @@ public interface IJukyushaKoshinKekkaMapper {
      * @return List<JukyushaHihokenshaEntity>
      */
     List<JukyushaHihokenshaEntity> get帳票出力対象データ(JukyushaKoshinKekkaMybatisParameter parameter);
+
+    /**
+     * DbT3104KokuhorenInterfaceKanriを取得する。
+     *
+     * @param parameter DbT3104KensakuMybatisParameter
+     * @return DbT3104KokuhorenInterfaceKanriEntity
+     */
+    DbT3104KokuhorenInterfaceKanriEntity getDbT3104KokuhorenInterfaceKanri(DbT3104KensakuMybatisParameter parameter);
 }
