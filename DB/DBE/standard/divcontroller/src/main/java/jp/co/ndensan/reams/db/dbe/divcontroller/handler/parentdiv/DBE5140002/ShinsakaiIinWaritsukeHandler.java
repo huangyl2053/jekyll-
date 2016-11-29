9348a0e964c19ded41c98edb96ac86e315f587a9
上條　貴大 <n3601@ndensan.co.jp>
2016-11-29 16:56:46 +0900

    受入 #110317【不具合】DBE-0130-DBEMN51003_介護認定審査会開催予定登録　全体を表示するをクリックすると、審査員が重複して表示する。　追加修正

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/handler/parentdiv/DBE5140002/ShinsakaiIinWaritsukeHandler.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/handler/parentdiv/DBE5140002/ShinsakaiIinWaritsukeHandler.java
@@ -146,11 +146,8 @@ public class ShinsakaiIinWaritsukeHandler {
     private List<dgShinsakaiIinIchiran_Row> set重複調整(List<dgShinsakaiIinIchiran_Row> ichiranGridList) {
         List<dgShinsakaiIinIchiran_Row> list = new ArrayList();
         if (ichiranGridList != null && !ichiranGridList.isEmpty()) {
+            list.add(ichiranGridList.get(0));
             for (dgShinsakaiIinIchiran_Row ichiran : ichiranGridList) {
-                if (list.isEmpty()) {
-                    list.add(ichiran);
-                    continue;
-                }
                 for (int i = 0; i < list.size(); i++) {
                     if (!list.get(i).getShinsakaiIinCode().equals(ichiran.getShinsakaiIinCode())) {
                         list.add(ichiran);
