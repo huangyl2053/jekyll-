1047cd31a88d1088bcc871147abbb9f1c62be48b
劉振涛 <n8399@ndensan.co.jp>
2016-11-29 11:53:50 +0900

    受入 #111574 修正内容： No.13.DBE231002_主治医意見書記入用紙で申請日の年月日の「年」が不正に出力されている 　　主治医意見書記入用紙 　　主治医意見書記入用紙OCR 　　⇒「設計書どおり、申請年月日が正常に出力されるようにする」に修正しました。 No.14.介護保険診断命令書の郵便番号と住所の出力が不正。　※診断を行う医療機関の郵便番号と住所が出力されてしまってる。 　　⇒「該当箇所は被保険者の郵便番号と住所が出力されるようにする」に修正しました。

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/controller/parentdiv/DBE2300001/ShujiiIkenshoSakuseiIrai.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/controller/parentdiv/DBE2300001/ShujiiIkenshoSakuseiIrai.java
@@ -692,8 +692,8 @@ public class ShujiiIkenshoSakuseiIrai {
         business.setHihokenshaNo10(hihokenshaNo.substring(数字_9));
         RDate 認定申請日 = row.getShinseiDay().getValue();
         if (認定申請日 != null) {
-            business.setShinseiYY1(認定申請日.getYear().wareki().toDateString().substring(数字_0, 数字_1));
-            business.setShinseiYY2(認定申請日.getYear().wareki().toDateString().substring(数字_1, 数字_2));
+            business.setShinseiYY1(認定申請日.getYear().wareki().toDateString().substring(数字_1, 数字_2));
+            business.setShinseiYY2(認定申請日.getYear().wareki().toDateString().substring(数字_2, 数字_3));
             business.setShinseiMM1(new RString(String.valueOf(認定申請日.getMonthValue() / 数字_10)));
             business.setShinseiMM2(new RString(String.valueOf(認定申請日.getMonthValue() % 数字_10)));
             business.setShinseiDD1(new RString(String.valueOf(認定申請日.getDayValue() / 数字_10)));
@@ -819,8 +819,8 @@ public class ShujiiIkenshoSakuseiIrai {
         item.setJushinArea(div.getMeireisho().getTxtjyushinbasho().getValue());
         item.setHakkoYMD(RDate.getNowDate().wareki().eraType(EraType.KANJI).firstYear(FirstYear.GAN_NEN)
                 .separator(Separator.JAPANESE).fillType(FillType.BLANK).toDateString());
-        item.setJushoText(row.getIryoukikanShozaichi());
-        item.setYubinNo(getEditedYubinNo(row.getIryoKikanYubinNo()));
+        item.setJushoText(row.getJusho());
+        item.setYubinNo(getEditedYubinNo(row.getYubinNo()));
         item.setMeishoFuyo(ChohyoAtesakiKeisho.toValue(
                 DbBusinessConfig.get(ConfigNameDBE.介護保険診断命令書_宛先敬称, RDate.getNowDate(), SubGyomuCode.DBE認定支援)).get名称());
         item.setCustomerBarCode(ReportUtil.getCustomerBarCode(row.getIryoKikanYubinNo(), row.getIryoukikanShozaichi()));
