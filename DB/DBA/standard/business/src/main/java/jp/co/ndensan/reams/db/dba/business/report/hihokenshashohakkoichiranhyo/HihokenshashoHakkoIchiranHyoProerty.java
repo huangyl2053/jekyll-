269583cc3f45002cbef1e1c99b0aad003a8bf922
dongyabin <dongyabin@liandisys.com.cn>
2016-11-29 19:01:23 +0800

    被保険者証発行一覧表 TODO 項の対応

--- DB/DBA/standard/business/src/main/java/jp/co/ndensan/reams/db/dba/business/report/hihokenshashohakkoichiranhyo/HihokenshashoHakkoIchiranHyoProerty.java
+++ DB/DBA/standard/business/src/main/java/jp/co/ndensan/reams/db/dba/business/report/hihokenshashohakkoichiranhyo/HihokenshashoHakkoIchiranHyoProerty.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package jp.co.ndensan.reams.db.dba.business.report.hihokenshashohakkoichiranhyo;
 
 import jp.co.ndensan.reams.db.dba.definition.reportid.ReportIdDBA;
@@ -13,19 +12,19 @@ import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
 import jp.co.ndensan.reams.uz.uza.report.ReportPropertyBase;
 
 /**
- * 
+ *
  * {@link HihokenshashoHakkoIchiranHyoProerty}のプロパティです。
+ *
  * @reamsid_L DBU-0420-060 dongyabin
  */
 public class HihokenshashoHakkoIchiranHyoProerty extends ReportPropertyBase<HihokenshashoHakkoIchiranhyoReportSource> {
-    
-        private static final ReportId ID = ReportIdDBA.DBA200003.getReportId();
-    
+
+    private static final ReportId ID = ReportIdDBA.DBA200003.getReportId();
+
     /**
      * コンストラクタです。
      */
     public HihokenshashoHakkoIchiranHyoProerty() {
         super(SubGyomuCode.DBA介護資格, ID);
     }
-    // TODO Redmine：#73393 (出力順について)
 }
