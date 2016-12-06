97d27731f8222e68b0e2d53f73d41e87c9cbee1f
山邉　大貴 <n3331@ndensan.co.jp>
2016-12-06 20:59:27 +0900

    介護認定審査会資料作成 その他資料（組み合わせ帳票）　イメージデータ出力処理修正

--- DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/step/shiryoshinsakai/IinShinsakaiSiryouKumiawaseA4Process.java
+++ DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/step/shiryoshinsakai/IinShinsakaiSiryouKumiawaseA4Process.java
@@ -51,8 +51,13 @@ import jp.co.ndensan.reams.uz.uza.batch.process.BatchWriter;
 import jp.co.ndensan.reams.uz.uza.batch.process.SimpleBatchProcessBase;
 import jp.co.ndensan.reams.uz.uza.biz.AtenaMeisho;
 import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
+import jp.co.ndensan.reams.uz.uza.cooperation.FilesystemName;
+import jp.co.ndensan.reams.uz.uza.cooperation.FilesystemPath;
+import jp.co.ndensan.reams.uz.uza.cooperation.SharedFile;
+import jp.co.ndensan.reams.uz.uza.cooperation.descriptor.ReadOnlySharedFileEntryDescriptor;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
 import jp.co.ndensan.reams.uz.uza.lang.RDate;
+import jp.co.ndensan.reams.uz.uza.lang.RDateTime;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.lang.RStringBuilder;
 import jp.co.ndensan.reams.uz.uza.report.BreakerCatalog;
@@ -88,6 +93,11 @@ public class IinShinsakaiSiryouKumiawaseA4Process extends SimpleBatchProcessBase
     private int count;
     private int shinsakaiOrder;
     private int 存在ファイルindex;
+    private static final int INDEX_5 = 5;
+    private static final boolean あり = true;
+    private static final boolean 無し = false;
+    private static final RString ファイル名_G0001 = new RString("G0001.png");
+
     @BatchWriter
     private BatchReportWriter<IinShinsakaishiryoA4ReportSource> batchReportWriter;
     private ReportSourceWriter<IinShinsakaishiryoA4ReportSource> reportSourceWriter;
@@ -249,7 +259,14 @@ public class IinShinsakaiSiryouKumiawaseA4Process extends SimpleBatchProcessBase
                 if (shinsakaiOrder != entity.getShinsakaiOrder()) {
                     存在ファイルindex = 0;
                 }
-                business = new JimuSonotashiryoBusiness(entity, new ArrayList(), 存在ファイルindex);
+                List<RString> イメージファイルリスト;
+                if (!entity.isJimukyoku()) {
+                    イメージファイルリスト = getその他資料(entity.getImageSharedFileId(), getその他資料マスキング後イメージファイル名());
+                } else {
+                    イメージファイルリスト = getその他資料(entity.getImageSharedFileId(), getその他資料原本イメージファイル名());
+                }
+                business = new JimuSonotashiryoBusiness(entity, イメージファイルリスト, 存在ファイルindex);
+                business.set事務局概況特記イメージ(共有ファイルを引き出す(entity.getImageSharedFileId(), ファイル名_G0001));
                 存在ファイルindex = business.get存在ファイルIndex();
                 shinsakaiOrder = entity.getShinsakaiOrder();
             }
@@ -333,5 +350,142 @@ public class IinShinsakaiSiryouKumiawaseA4Process extends SimpleBatchProcessBase
         list.add(builder2.toRString());
         list.add(builder3.toRString());
         return list;
+    }
+
+    /**
+     * その他資料を取得します。
+     *
+     * @param sharedFileId 共有ファイルID
+     * @param ファイル名List イメージファイルリスト
+     * @return その他資料
+     */
+    public List<RString> getその他資料(RDateTime sharedFileId, List<RString> ファイル名List) {
+        List<RString> ファイルPathList = new ArrayList<>();
+        if (sharedFileId == null) {
+            return ファイルPathList;
+        }
+        boolean is存在;
+        int index = 0;
+        for (int i = 0; i < ファイル名List.size(); i++) {
+            RString ファイル名 = ファイル名List.get(i);
+            ReadOnlySharedFileEntryDescriptor descriptor
+                    = new ReadOnlySharedFileEntryDescriptor(new FilesystemName(ファイル名), sharedFileId);
+            try {
+                SharedFile.copyToLocal(descriptor, new FilesystemPath(batchReportWriter.getImageFolderPath()));
+                is存在 = あり;
+            } catch (Exception e) {
+                is存在 = 無し;
+            }
+            if (is存在 && index < INDEX_5) {
+                ファイルPathList.add(ファイル名);
+                index = i + 1;
+            }
+            if (INDEX_5 <= index) {
+                return ファイルPathList;
+            }
+        }
+        return ファイルPathList;
+    }
+
+    private List<RString> getその他資料マスキング後イメージファイル名() {
+        List<RString> ファイル名 = new ArrayList<>();
+        ファイル名.add(new RString("F1401A01.png"));
+        ファイル名.add(new RString("F1401A02.png"));
+        ファイル名.add(new RString("F1401A03.png"));
+        ファイル名.add(new RString("F1401A04.png"));
+        ファイル名.add(new RString("F1401A05.png"));
+        ファイル名.add(new RString("F1401A06.png"));
+        ファイル名.add(new RString("F1401B01.png"));
+        ファイル名.add(new RString("F1401B02.png"));
+        ファイル名.add(new RString("F1401B03.png"));
+        ファイル名.add(new RString("F1401B04.png"));
+        ファイル名.add(new RString("F1401B05.png"));
+        ファイル名.add(new RString("F1401B06.png"));
+        ファイル名.add(new RString("F1401C01.png"));
+        ファイル名.add(new RString("F1401C02.png"));
+        ファイル名.add(new RString("F1401C03.png"));
+        ファイル名.add(new RString("F1401C04.png"));
+        ファイル名.add(new RString("F1401C05.png"));
+        ファイル名.add(new RString("F1401C06.png"));
+        ファイル名.add(new RString("F1401D01.png"));
+        ファイル名.add(new RString("F1401D02.png"));
+        ファイル名.add(new RString("F1401D03.png"));
+        ファイル名.add(new RString("F1401D04.png"));
+        ファイル名.add(new RString("F1401D05.png"));
+        ファイル名.add(new RString("F1401D06.png"));
+        ファイル名.add(new RString("F1401E01.png"));
+        ファイル名.add(new RString("F1401E02.png"));
+        ファイル名.add(new RString("F1401E03.png"));
+        ファイル名.add(new RString("F1401E04.png"));
+        ファイル名.add(new RString("F1401E05.png"));
+        ファイル名.add(new RString("F1401E06.png"));
+        ファイル名.add(new RString("F1401F01.png"));
+        ファイル名.add(new RString("F1401F02.png"));
+        ファイル名.add(new RString("F1401F03.png"));
+        ファイル名.add(new RString("F1401F04.png"));
+        ファイル名.add(new RString("F1401F05.png"));
+        ファイル名.add(new RString("F1401F06.png"));
+        return ファイル名;
+    }
+
+    private List<RString> getその他資料原本イメージファイル名() {
+        List<RString> ファイル名 = new ArrayList<>();
+        ファイル名.add(new RString("F1401A01_BAK.png"));
+        ファイル名.add(new RString("F1401A02_BAK.png"));
+        ファイル名.add(new RString("F1401A03_BAK.png"));
+        ファイル名.add(new RString("F1401A04_BAK.png"));
+        ファイル名.add(new RString("F1401A05_BAK.png"));
+        ファイル名.add(new RString("F1401A06_BAK.png"));
+        ファイル名.add(new RString("F1401B01_BAK.png"));
+        ファイル名.add(new RString("F1401B02_BAK.png"));
+        ファイル名.add(new RString("F1401B03_BAK.png"));
+        ファイル名.add(new RString("F1401B04_BAK.png"));
+        ファイル名.add(new RString("F1401B05_BAK.png"));
+        ファイル名.add(new RString("F1401B06_BAK.png"));
+        ファイル名.add(new RString("F1401C01_BAK.png"));
+        ファイル名.add(new RString("F1401C02_BAK.png"));
+        ファイル名.add(new RString("F1401C03_BAK.png"));
+        ファイル名.add(new RString("F1401C04_BAK.png"));
+        ファイル名.add(new RString("F1401C05_BAK.png"));
+        ファイル名.add(new RString("F1401C06_BAK.png"));
+        ファイル名.add(new RString("F1401D01_BAK.png"));
+        ファイル名.add(new RString("F1401D02_BAK.png"));
+        ファイル名.add(new RString("F1401D03_BAK.png"));
+        ファイル名.add(new RString("F1401D04_BAK.png"));
+        ファイル名.add(new RString("F1401D05_BAK.png"));
+        ファイル名.add(new RString("F1401D06_BAK.png"));
+        ファイル名.add(new RString("F1401E01_BAK.png"));
+        ファイル名.add(new RString("F1401E02_BAK.png"));
+        ファイル名.add(new RString("F1401E03_BAK.png"));
+        ファイル名.add(new RString("F1401E04_BAK.png"));
+        ファイル名.add(new RString("F1401E05_BAK.png"));
+        ファイル名.add(new RString("F1401E06_BAK.png"));
+        ファイル名.add(new RString("F1401F01_BAK.png"));
+        ファイル名.add(new RString("F1401F02_BAK.png"));
+        ファイル名.add(new RString("F1401F03_BAK.png"));
+        ファイル名.add(new RString("F1401F04_BAK.png"));
+        ファイル名.add(new RString("F1401F05_BAK.png"));
+        ファイル名.add(new RString("F1401F06_BAK.png"));
+        return ファイル名;
+    }
+
+    private RString 共有ファイルを引き出す(RDateTime イメージID, RString sharedFileName) {
+        RString imagePath = RString.EMPTY;
+        if (イメージID != null) {
+            imagePath = getFilePath(イメージID, sharedFileName);
+        }
+        return imagePath;
+    }
+
+    private RString getFilePath(RDateTime sharedFileId, RString sharedFileName) {
+        ReadOnlySharedFileEntryDescriptor descriptor
+                = new ReadOnlySharedFileEntryDescriptor(new FilesystemName(sharedFileName),
+                        sharedFileId);
+        try {
+            SharedFile.copyToLocal(descriptor, new FilesystemPath(batchReportWriter.getImageFolderPath()));
+        } catch (Exception e) {
+            return RString.EMPTY;
+        }
+        return sharedFileName;
     }
 }
