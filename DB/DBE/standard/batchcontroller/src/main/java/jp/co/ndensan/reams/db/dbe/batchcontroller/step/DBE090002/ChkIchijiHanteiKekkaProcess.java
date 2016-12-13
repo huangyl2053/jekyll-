ab80eba4a309da8b6230e0e532eb0b6fd6902476
山邉　大貴 <n3331@ndensan.co.jp>
2016-12-13 09:33:13 +0900

    0230_要介護認定情報提供　一次判定結果棒グラフエラー再現

--- DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/step/DBE090002/ChkIchijiHanteiKekkaProcess.java
+++ DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/step/DBE090002/ChkIchijiHanteiKekkaProcess.java
@@ -222,6 +222,10 @@ public class ChkIchijiHanteiKekkaProcess extends BatchProcessBase<YokaigoninteiE
     List<DbT5304ShujiiIkenshoIkenItemEntity> 前回意見書項目;
     List<DbT5208NinteichosahyoServiceJokyoFlagEntity> dbt5208Entity;
 
+    static {
+        System.setProperty("java.awt.headless", "true");
+    }
+
     @Override
     protected void initialize() {
         list = new ArrayList<>();
@@ -1600,14 +1604,14 @@ public class ChkIchijiHanteiKekkaProcess extends BatchProcessBase<YokaigoninteiE
         RString 文件名 = 日期.getDate().toDateString().concat(get文件名(日期.getHour()))
                 .concat(get文件名(日期.getSecond())).concat(get文件名(日期.getMicros()));
         //TODO n8178 城間 グラフ機能以外の箇所を動作確認するために一時的にコメントアウト。
-//        System.setProperty("java.awt.headless", "true");
-//        new StackedBarChart(getNumber(entity.get要介護認定等基準時間_食事()),
-//                getNumber(entity.get要介護認定等基準時間_排泄()), getNumber(entity.get要介護認定等基準時間_移動()),
-//                getNumber(entity.get要介護認定等基準時間_清潔保持()), getNumber(entity.get要介護認定等基準時間_間接ケア()),
-//                getNumber(entity.get要介護認定等基準時間_BPSD関連()), getNumber(entity.get要介護認定等基準時間_機能訓練()),
-//                getNumber(entity.get要介護認定等基準時間_医療関連()), getNumber(entity.get要介護認定等基準時間_認知症加算()),
-//                文件名, batchWrite.getImageFolderPath());
-//        ichijiEntity.set要介護認定等基準時間イメージ(new RString(文件名 + ".png"));
+        System.setProperty("java.awt.headless", "true");
+        new StackedBarChart(getNumber(entity.get要介護認定等基準時間_食事()),
+                getNumber(entity.get要介護認定等基準時間_排泄()), getNumber(entity.get要介護認定等基準時間_移動()),
+                getNumber(entity.get要介護認定等基準時間_清潔保持()), getNumber(entity.get要介護認定等基準時間_間接ケア()),
+                getNumber(entity.get要介護認定等基準時間_BPSD関連()), getNumber(entity.get要介護認定等基準時間_機能訓練()),
+                getNumber(entity.get要介護認定等基準時間_医療関連()), getNumber(entity.get要介護認定等基準時間_認知症加算()),
+                文件名, batchWrite.getImageFolderPath());
+        ichijiEntity.set要介護認定等基準時間イメージ(new RString(文件名 + ".png"));
         ichijiEntity.set要介護認定等基準時間イメージ(RString.EMPTY);
         ichijiEntity.set要介護認定等基準時間_食事(entity.get要介護認定等基準時間_食事());
         ichijiEntity.set要介護認定等基準時間_排泄(entity.get要介護認定等基準時間_排泄());
