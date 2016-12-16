8aace271647202abf697202b39e7ecff66efcffb
八木 翼 <n3329@ndensan.co.jp>
2016-12-16 10:00:38 +0900

    認定調査督促状発行バッチ　抽出Mapperの修正

--- DB/DBE/standard/persistence/src/test/java/jp/co/ndensan/reams/db/dbe/persistence/db/mapper/relate/ninteichosatokusokujyo/INinteichosaTokusokujyoRelateMapperTest.java
+++ DB/DBE/standard/persistence/src/test/java/jp/co/ndensan/reams/db/dbe/persistence/db/mapper/relate/ninteichosatokusokujyo/INinteichosaTokusokujyoRelateMapperTest.java
@@ -5,12 +5,15 @@
  */
 package jp.co.ndensan.reams.db.dbe.persistence.db.mapper.relate.ninteichosatokusokujyo;
 
+import java.util.List;
 import jp.co.ndensan.reams.db.dbe.definition.mybatisprm.ninteichosatokusokujyo.NinteiChosaTokusokujoMybatisParameter;
+import jp.co.ndensan.reams.db.dbe.entity.db.relate.ninteichosatokusokujyo.NinteiChosaTokusokujoRelateEntity;
 import jp.co.ndensan.reams.db.dbz.testhelper.DbeTestDacBase;
 import jp.co.ndensan.reams.uz.uza.biz.LasdecCode;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.math.Decimal;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -38,6 +41,16 @@ public class INinteichosaTokusokujyoRelateMapperTest extends DbeTestDacBase {
                 new RString("1"),
                 new RString("1"),
                 new LasdecCode("209007"));
-        sut.select要介護認定調査督促状ByKey(param);
+        NinteiChosaTokusokujoMybatisParameter param2 = new NinteiChosaTokusokujoMybatisParameter(
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null);
+        List<NinteiChosaTokusokujoRelateEntity> result = sut.select要介護認定調査督促状ByKey(param);
+        List<NinteiChosaTokusokujoRelateEntity> result2 = sut.select要介護認定調査督促状ByKey(param2);
+        assertTrue(true);
     }
 }
