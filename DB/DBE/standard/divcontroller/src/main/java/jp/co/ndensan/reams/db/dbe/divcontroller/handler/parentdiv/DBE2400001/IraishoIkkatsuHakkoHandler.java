25b3f1418580b4deb0f5305045a27292d0af23e9
中込隆治 <n3417@ndensan.co.jp>
2016-12-21 08:57:37 +0900

    ビルドエラー修正

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/handler/parentdiv/DBE2400001/IraishoIkkatsuHakkoHandler.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/handler/parentdiv/DBE2400001/IraishoIkkatsuHakkoHandler.java
@@ -252,9 +252,10 @@ public class IraishoIkkatsuHakkoHandler {
             } else {
                 ocrDisabledKeys.add(概況調査);
             }
-            if (OCR.equals(DbBusinessConfig.get(ConfigNameDBE.認定調査票_概況特記_用紙タイプ, 基準日, SubGyomuCode.DBE認定支援))) {
-                ninteiChosahyoDisabledKeys.add(概況特記);
-            } else {
+//            if (OCR.equals(DbBusinessConfig.get(ConfigNameDBE.認定調査票_概況特記_用紙タイプ, 基準日, SubGyomuCode.DBE認定支援))) {
+//                ninteiChosahyoDisabledKeys.add(概況特記);
+//            } 
+            else {
                 ocrDisabledKeys.add(概況特記);
             }
             div.getChkNinteiChosahyoShurui().setDisabledItemsByKey(ninteiChosahyoDisabledKeys);
