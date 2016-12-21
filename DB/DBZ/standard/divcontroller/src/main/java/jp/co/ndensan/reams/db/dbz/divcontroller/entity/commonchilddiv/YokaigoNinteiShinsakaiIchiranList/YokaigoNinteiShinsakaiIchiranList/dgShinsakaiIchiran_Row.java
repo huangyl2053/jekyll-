bc60de9878379a418aa4f44c89b0a7778fa82e77
清水　健一 <n9818@ndensan.co.jp>
2016-12-21 18:48:20 +0900

    審査会一覧  デフォルトボタン割り当て

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/YokaigoNinteiShinsakaiIchiranList/YokaigoNinteiShinsakaiIchiranList/dgShinsakaiIchiran_Row.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/YokaigoNinteiShinsakaiIchiranList/YokaigoNinteiShinsakaiIchiranList/dgShinsakaiIchiran_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.YokaigoNi
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Tue Dec 20 18:23:44 JST 2016 
+ * Wed Dec 21 18:26:37 JST 2016 
  */
 
 
