358631b5f5ab502cc02a2a4b0880e1bc0a8f767f
八木 翼 <n3329@ndensan.co.jp>
2016-12-22 15:48:02 +0900

    #113395 認定調査督促状、主治医意見書督促状　帳票、CSV修正

--- DB/DBE/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbe/definition/mybatisprm/ninteichosatokusokujyo/NinteiChosaTokusokuTaishoshaIchiranhyoMybatisParameter.java
+++ DB/DBE/standard/definition/src/main/java/jp/co/ndensan/reams/db/dbe/definition/mybatisprm/ninteichosatokusokujyo/NinteiChosaTokusokuTaishoshaIchiranhyoMybatisParameter.java
@@ -8,7 +8,6 @@ package jp.co.ndensan.reams.db.dbe.definition.mybatisprm.ninteichosatokusokujyo;
 import jp.co.ndensan.reams.uz.uza.batch.parameter.IMyBatisParameter;
 import jp.co.ndensan.reams.uz.uza.lang.FlexibleDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
-import jp.co.ndensan.reams.uz.uza.math.Decimal;
 import lombok.Getter;
 
 /**
@@ -20,48 +19,33 @@ import lombok.Getter;
 @Getter
 public final class NinteiChosaTokusokuTaishoshaIchiranhyoMybatisParameter implements IMyBatisParameter {
 
-    private final FlexibleDate temp_基準日;
-    private final Decimal temp_認定調査督促期限日数;
-    private final RString temp_印刷済対象者;
     private final RString temp_保険者コード;
     private final RString temp_認定調査委託先コード;
     private final RString temp_認定調査員コード;
     private final FlexibleDate temp_印刷期間開始日;
     private final FlexibleDate temp_印刷期間終了日;
-    private final RString temp_出力順;
 
     /**
      * コンストラクタです。
      *
-     * @param temp_基準日 基準日
-     * @param temp_認定調査督促期限日数 認定調査督促期限日数
-     * @param temp_印刷済対象者 印刷済対象者
      * @param temp_保険者コード 保険者コード
      * @param temp_認定調査委託先コード 認定調査委託先コード
      * @param temp_認定調査員コード 認定調査員コード
      * @param temp_印刷期間開始日 印刷期間開始日
      * @param temp_印刷期間終了日 印刷期間終了日
-     * @param temp_出力順 出力順
      */
-    public NinteiChosaTokusokuTaishoshaIchiranhyoMybatisParameter(FlexibleDate temp_基準日,
-            Decimal temp_認定調査督促期限日数,
-            RString temp_印刷済対象者,
+    public NinteiChosaTokusokuTaishoshaIchiranhyoMybatisParameter(
             RString temp_保険者コード,
             RString temp_認定調査委託先コード,
             RString temp_認定調査員コード,
             FlexibleDate temp_印刷期間開始日,
-            FlexibleDate temp_印刷期間終了日,
-            RString temp_出力順
+            FlexibleDate temp_印刷期間終了日
     ) {
-        this.temp_基準日 = temp_基準日;
-        this.temp_認定調査督促期限日数 = temp_認定調査督促期限日数;
-        this.temp_印刷済対象者 = temp_印刷済対象者;
         this.temp_保険者コード = temp_保険者コード;
         this.temp_認定調査委託先コード = temp_認定調査委託先コード;
         this.temp_認定調査員コード = temp_認定調査員コード;
         this.temp_印刷期間開始日 = temp_印刷期間開始日;
         this.temp_印刷期間終了日 = temp_印刷期間終了日;
-        this.temp_出力順 = temp_出力順;
     }
 
 }
