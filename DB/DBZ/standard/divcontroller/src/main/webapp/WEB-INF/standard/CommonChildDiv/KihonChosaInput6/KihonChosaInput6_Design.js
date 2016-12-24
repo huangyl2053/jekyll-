065085e801aac106ff3f1188cb2e982a60dc8046
張 メイに <n8361@ndensan.co.jp>
2016-12-24 17:28:43 +0900

    受入#103040対応　特別な医療画面改修

--- DB/DBZ/standard/divcontroller/src/main/webapp/WEB-INF/standard/CommonChildDiv/KihonChosaInput6/KihonChosaInput6_Design.js
+++ DB/DBZ/standard/divcontroller/src/main/webapp/WEB-INF/standard/CommonChildDiv/KihonChosaInput6/KihonChosaInput6_Design.js
@@ -4,12 +4,52 @@ var DBZ;
         var Events = (function () {
             function Events() {
             }
-            Events.onBeforeOpenDialog_btnShochiNaiyo = function () {
-                return "onBeforeOpenDialog_btnShochiNaiyo";
+            Events.onBeforeOpenDialog_btnTenteki = function () {
+                return "onBeforeOpenDialog_btnTenteki";
             };
 
-            Events.onBeforeOpenDialog_btnTokiTaiou = function () {
-                return "onBeforeOpenDialog_btnTokiTaiou";
+            Events.onBeforeOpenDialog_btnSeimyaku = function () {
+                return "onBeforeOpenDialog_btnSeimyaku";
+            };
+
+            Events.onBeforeOpenDialog_btnTouseki = function () {
+                return "onBeforeOpenDialog_btnTouseki";
+            };
+
+            Events.onBeforeOpenDialog_btnSutoma = function () {
+                return "onBeforeOpenDialog_btnSutoma";
+            };
+
+            Events.onBeforeOpenDialog_btnSansou = function () {
+                return "onBeforeOpenDialog_btnSansou";
+            };
+
+            Events.onBeforeOpenDialog_btnResupireta = function () {
+                return "onBeforeOpenDialog_btnResupireta";
+            };
+
+            Events.onBeforeOpenDialog_btnKikan = function () {
+                return "onBeforeOpenDialog_btnKikan";
+            };
+
+            Events.onBeforeOpenDialog_btnToutsu = function () {
+                return "onBeforeOpenDialog_btnToutsu";
+            };
+
+            Events.onBeforeOpenDialog_btnKeikan = function () {
+                return "onBeforeOpenDialog_btnKeikan";
+            };
+
+            Events.onBeforeOpenDialog_btnMonita = function () {
+                return "onBeforeOpenDialog_btnMonita";
+            };
+
+            Events.onBeforeOpenDialog_btnJyokuso = function () {
+                return "onBeforeOpenDialog_btnJyokuso";
+            };
+
+            Events.onBeforeOpenDialog_btnKateru = function () {
+                return "onBeforeOpenDialog_btnKateru";
             };
 
             Events.onClick_btnBack = function () {
@@ -55,24 +95,104 @@ var DBZ;
                 return new UZA.Panel(this.convFiledName("ShochiNaiyo"));
             };
 
-            Controls.prototype.btnShochiNaiyo = function () {
-                return new UZA.ButtonDialog(this.convFiledName("btnShochiNaiyo"));
+            Controls.prototype.btnTenteki = function () {
+                return new UZA.ButtonDialog(this.convFiledName("btnTenteki"));
             };
 
-            Controls.prototype.chkShochiNaiyo = function () {
-                return new UZA.CheckBoxList(this.convFiledName("chkShochiNaiyo"));
+            Controls.prototype.chkTenteki = function () {
+                return new UZA.CheckBoxList(this.convFiledName("chkTenteki"));
+            };
+
+            Controls.prototype.btnSeimyaku = function () {
+                return new UZA.ButtonDialog(this.convFiledName("btnSeimyaku"));
+            };
+
+            Controls.prototype.chkSeimyaku = function () {
+                return new UZA.CheckBoxList(this.convFiledName("chkSeimyaku"));
+            };
+
+            Controls.prototype.btnTouseki = function () {
+                return new UZA.ButtonDialog(this.convFiledName("btnTouseki"));
+            };
+
+            Controls.prototype.chkTouseki = function () {
+                return new UZA.CheckBoxList(this.convFiledName("chkTouseki"));
+            };
+
+            Controls.prototype.btnSutoma = function () {
+                return new UZA.ButtonDialog(this.convFiledName("btnSutoma"));
+            };
+
+            Controls.prototype.chkSutoma = function () {
+                return new UZA.CheckBoxList(this.convFiledName("chkSutoma"));
+            };
+
+            Controls.prototype.btnSansou = function () {
+                return new UZA.ButtonDialog(this.convFiledName("btnSansou"));
+            };
+
+            Controls.prototype.chkSansou = function () {
+                return new UZA.CheckBoxList(this.convFiledName("chkSansou"));
+            };
+
+            Controls.prototype.btnResupireta = function () {
+                return new UZA.ButtonDialog(this.convFiledName("btnResupireta"));
+            };
+
+            Controls.prototype.chkResupireta = function () {
+                return new UZA.CheckBoxList(this.convFiledName("chkResupireta"));
+            };
+
+            Controls.prototype.btnKikan = function () {
+                return new UZA.ButtonDialog(this.convFiledName("btnKikan"));
+            };
+
+            Controls.prototype.chkKikan = function () {
+                return new UZA.CheckBoxList(this.convFiledName("chkKikan"));
+            };
+
+            Controls.prototype.btnToutsu = function () {
+                return new UZA.ButtonDialog(this.convFiledName("btnToutsu"));
+            };
+
+            Controls.prototype.chkToutsu = function () {
+                return new UZA.CheckBoxList(this.convFiledName("chkToutsu"));
+            };
+
+            Controls.prototype.btnKeikan = function () {
+                return new UZA.ButtonDialog(this.convFiledName("btnKeikan"));
+            };
+
+            Controls.prototype.chkKeikan = function () {
+                return new UZA.CheckBoxList(this.convFiledName("chkKeikan"));
             };
 
             Controls.prototype.TokiTaiou = function () {
                 return new UZA.Panel(this.convFiledName("TokiTaiou"));
             };
 
-            Controls.prototype.btnTokiTaiou = function () {
-                return new UZA.ButtonDialog(this.convFiledName("btnTokiTaiou"));
+            Controls.prototype.btnMonita = function () {
+                return new UZA.ButtonDialog(this.convFiledName("btnMonita"));
             };
 
-            Controls.prototype.chkTokiTaiou = function () {
-                return new UZA.CheckBoxList(this.convFiledName("chkTokiTaiou"));
+            Controls.prototype.chkMonita = function () {
+                return new UZA.CheckBoxList(this.convFiledName("chkMonita"));
+            };
+
+            Controls.prototype.btnJyokuso = function () {
+                return new UZA.ButtonDialog(this.convFiledName("btnJyokuso"));
+            };
+
+            Controls.prototype.chkJyokuso = function () {
+                return new UZA.CheckBoxList(this.convFiledName("chkJyokuso"));
+            };
+
+            Controls.prototype.btnbtnKateru = function () {
+                return new UZA.ButtonDialog(this.convFiledName("btnbtnKateru"));
+            };
+
+            Controls.prototype.chkKateru = function () {
+                return new UZA.CheckBoxList(this.convFiledName("chkKateru"));
             };
 
             Controls.prototype.btnBack = function () {
