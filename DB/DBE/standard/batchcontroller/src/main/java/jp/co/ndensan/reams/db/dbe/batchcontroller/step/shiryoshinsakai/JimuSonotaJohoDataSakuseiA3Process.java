c57deed76517f41f159372c11ec0c58b3d067bea
山邉　大貴 <n3331@ndensan.co.jp>
2016-12-26 17:51:46 +0900

    0150 介護認定審査会資料作成　帳票出力処理修正

--- DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/step/shiryoshinsakai/JimuSonotaJohoDataSakuseiA3Process.java
+++ DB/DBE/standard/batchcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/batchcontroller/step/shiryoshinsakai/JimuSonotaJohoDataSakuseiA3Process.java
@@ -82,14 +82,14 @@ public class JimuSonotaJohoDataSakuseiA3Process extends BatchKeyBreakBase<Shinsa
             存在ファイルindex = 0;
         }
         List<RString> イメージファイルリスト;
+        RString 共有ファイル名 = entity.getShoKisaiHokenshaNo().concat(entity.getHihokenshaNo());
+        path = getFilePath(entity.getImageSharedFileId(), 共有ファイル名);
         if (!entity.isJimukyoku()) {
             イメージファイルリスト = getその他資料(entity.getImageSharedFileId(), getその他資料マスキング後イメージファイル名());
         } else {
             イメージファイルリスト = getその他資料(entity.getImageSharedFileId(), getその他資料原本イメージファイル名());
         }
         business = new JimuSonotashiryoBusiness(entity, イメージファイルリスト, 存在ファイルindex);
-        RString 共有ファイル名 = entity.getShoKisaiHokenshaNo().concat(entity.getHihokenshaNo());
-        path = getFilePath(entity.getImageSharedFileId(), 共有ファイル名);
         business.set事務局概況特記イメージ(共有ファイルを引き出す(path, ファイル名_G0001));
         SonotashiryoA3Report reportA3 = new SonotashiryoA3Report(business);
         reportA3.writeBy(reportSourceWriterA3);
