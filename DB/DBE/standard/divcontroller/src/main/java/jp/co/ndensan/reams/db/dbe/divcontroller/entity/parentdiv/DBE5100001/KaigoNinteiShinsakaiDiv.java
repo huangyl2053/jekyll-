19aa45f5f182542c64b2c7569ac386851e0633fe
清水　健一 <n9818@ndensan.co.jp>
2016-12-26 16:50:48 +0900

    審査会一覧グリッドの選択ボタンを押下したときの動作改善

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE5100001/KaigoNinteiShinsakaiDiv.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE5100001/KaigoNinteiShinsakaiDiv.java
@@ -4,18 +4,19 @@ package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE5100001;
  * このファイルへの変更は、再生成時には損失するため
  * 不正な動作の原因になります。
  */
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.YokaigoNinteiShinsakaiIchiranList.YokaigoNinteiShinsakaiIchiranList.IYokaigoNinteiShinsakaiIchiranListDiv;
 import jp.co.ndensan.reams.db.dbz.divcontroller.entity.commonchilddiv.YokaigoNinteiShinsakaiIchiranList.YokaigoNinteiShinsakaiIchiranList.YokaigoNinteiShinsakaiIchiranListDiv;
 import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
 
 /**
- * KaigoNinteiShinsakai のクラスファイル 
- * 
+ * KaigoNinteiShinsakai のクラスファイル
+ *
  * @author 自動生成
  */
 public class KaigoNinteiShinsakaiDiv extends Panel {
+
     // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-12-02_08-19-39">
     /*
      * [ private の作成 ]
@@ -54,7 +55,7 @@ public class KaigoNinteiShinsakaiDiv extends Panel {
      * [ ショートカットの作成 ]
      */
     @JsonIgnore
-    public IYokaigoNinteiShinsakaiIchiranListDiv getCcdShinsakaiItiran() {
+    public YokaigoNinteiShinsakaiIchiranListDiv getCcdShinsakaiItiran() {
         return this.getShinsakaiIchiran().getCcdShinsakaiItiran();
     }
 
