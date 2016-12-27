8e35b119d3cbe2d790ee72c9e22c941c4c583dd1
高橋　一矢 <n3509@ndensan.co.jp>
2016-12-27 13:08:58 +0900

    イメージ情報マスキング画面　ダイアログの高さを調整

---
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/commonchilddiv/ImageJohoMaskingDaialog/ImageJohoMaskingDialog/IImageJohoMaskingDialogDiv.java
@@ -0,0 +1,10 @@
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.commonchilddiv.ImageJohoMaskingDaialog.ImageJohoMaskingDialog;
+
+import jp.co.ndensan.reams.uz.uza.ui.binding.ICommonChildDivBaseProperties;
+
+/*
+ * このコードはツールによって生成されました。
+ */
+public interface IImageJohoMaskingDialogDiv extends ICommonChildDivBaseProperties {
+
+}
