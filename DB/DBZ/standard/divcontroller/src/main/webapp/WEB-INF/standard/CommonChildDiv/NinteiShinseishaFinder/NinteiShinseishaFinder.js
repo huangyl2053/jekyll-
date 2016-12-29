3ca9643f03ed95020218c333bdae0c82b7a27564
茅野健太郎 <n3415@ndensan.co.jp>
2016-12-29 17:06:46 +0900

    要介護認定申請者検索、主治医意見書作成依頼 raml修正

--- DB/DBZ/standard/divcontroller/src/main/webapp/WEB-INF/standard/CommonChildDiv/NinteiShinseishaFinder/NinteiShinseishaFinder.js
+++ DB/DBZ/standard/divcontroller/src/main/webapp/WEB-INF/standard/CommonChildDiv/NinteiShinseishaFinder/NinteiShinseishaFinder.js
@@ -5,7 +5,6 @@ var DBZ;
             function ModeController(fieldName) {
                 this.fieldName = fieldName;
                 this.controls = new NinteiShinseishaFinder.Controls(fieldName);
-                this.controls.ShosaiJoken().isOpen = false;
             }
             ModeController.prototype.Properties = function () {
                 return new UZA.CommonChildDiv(this.fieldName);
