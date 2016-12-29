2b168aee6e4c6c97fe7e106b11f333f41f99f43f
竹平森悟 <n2708@ndensan.co.jp>
2016-12-29 17:53:27 +0900

    介護認定審査会審査結果登録 不具合修正

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/handler/parentdiv/DBE5230001/ShinsakaiKekkaTorokuValidationHandler.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/handler/parentdiv/DBE5230001/ShinsakaiKekkaTorokuValidationHandler.java
@@ -5,8 +5,8 @@
  */
 package jp.co.ndensan.reams.db.dbe.divcontroller.handler.parentdiv.DBE5230001;
 
+import jp.co.ndensan.reams.db.dbe.definition.core.shinsakai.HanteiKekkaCode;
 import jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE5230001.ShinsakaiKekkaTorokuDiv;
-import jp.co.ndensan.reams.db.dbz.definition.core.yokaigonintei.ichijihantei.IchijiHanteiKekkaCode09;
 import jp.co.ndensan.reams.db.dbz.definition.core.yokaigonintei.shinsei.NinteiShinseiShinseijiKubunCode;
 import jp.co.ndensan.reams.ur.urz.definition.message.UrErrorMessages;
 import jp.co.ndensan.reams.uz.uza.lang.RDate;
@@ -42,12 +42,15 @@ public class ShinsakaiKekkaTorokuValidationHandler {
      */
     public ValidationMessageControlPairs validate個別() {
         ValidationMessageControlPairs validationMessages = new ValidationMessageControlPairs();
-        有効月数チェック(validationMessages);
-        有効月数範囲チェック(validationMessages);
-        認定期間開始チェック(validationMessages);
-        認定期間終了チェック(validationMessages);
-        入力チェック(validationMessages);
-        設定期間Fromと設定期間Toの前後順(validationMessages);
+        ShinsakaiKekkaTorokuHandler handler = getHandler();
+
+        if (handler.get判定結果() == HanteiKekkaCode.認定) {
+            有効月数チェック(validationMessages);
+            有効月数範囲チェック(validationMessages);
+            認定期間開始チェック(validationMessages);
+            認定期間終了チェック(validationMessages);
+            設定期間Fromと設定期間Toの前後順(validationMessages);
+        }
         return validationMessages;
     }
 
@@ -130,20 +133,6 @@ public class ShinsakaiKekkaTorokuValidationHandler {
                 && (ninteiKikanMonth.compareTo(new Decimal(24))) <= 0)) {
             validPairs.add(new ValidationMessageControlPair(
                     new ShinsakaiKekkaTorokuValidationHandler.IdocheckMessages(UrErrorMessages.入力値が不正_追加メッセージあり, "有効月数")));
-        }
-    }
-
-    /**
-     * 入力チェック
-     *
-     * @param validPairs ValidationMessageControlPairs
-     * @return ValidationMessageControlPairs ValidationMessageControlPairs
-     */
-    void 入力チェック(ValidationMessageControlPairs validPairs) {
-        if (IchijiHanteiKekkaCode09.要介護1.get名称().equals(div.getTxtIchijiHantei().getValue())
-                && div.getDdlJotaiZo().getSelectedKey().isEmpty()) {
-            validPairs.add(new ValidationMessageControlPair(
-                    new ShinsakaiKekkaTorokuValidationHandler.IdocheckMessages(UrErrorMessages.必須項目), div.getDdlJotaiZo()));
         }
     }
 
