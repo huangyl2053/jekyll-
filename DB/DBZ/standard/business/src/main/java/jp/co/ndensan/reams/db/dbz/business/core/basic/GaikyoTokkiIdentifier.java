d2b13fcc950b3f56f8d76910943774f5fd30014f
塩入 啓生 <n3010@ndensan.co.jp>
2016-12-29 10:36:34 +0900

    概況特記の原本マスク区分が追加されたことによる修正

--- DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/core/basic/GaikyoTokkiIdentifier.java
+++ DB/DBZ/standard/business/src/main/java/jp/co/ndensan/reams/db/dbz/business/core/basic/GaikyoTokkiIdentifier.java
@@ -28,23 +28,6 @@ public class GaikyoTokkiIdentifier implements Serializable {
      * @param 申請書管理番号 申請書管理番号
      * @param 認定調査依頼履歴番号 認定調査依頼履歴番号
      * @param 概況特記テキストイメージ区分 概況特記テキストイメージ区分
-     * @deprecated 原本マスク区分の主キーが追加になったことによりこのコンストラクタは不要
-     */
-    public GaikyoTokkiIdentifier(ShinseishoKanriNo 申請書管理番号,
-            int 認定調査依頼履歴番号,
-            RString 概況特記テキストイメージ区分) {
-        this.申請書管理番号 = 申請書管理番号;
-        this.認定調査依頼履歴番号 = 認定調査依頼履歴番号;
-        this.概況特記テキストイメージ区分 = 概況特記テキストイメージ区分;
-        this.原本マスク区分 = Code.EMPTY;
-    }
-
-    /**
-     * コンストラクタです。
-     *
-     * @param 申請書管理番号 申請書管理番号
-     * @param 認定調査依頼履歴番号 認定調査依頼履歴番号
-     * @param 概況特記テキストイメージ区分 概況特記テキストイメージ区分
      * @param 原本マスク区分 原本マスク区分
      */
     public GaikyoTokkiIdentifier(ShinseishoKanriNo 申請書管理番号,
--- DB/DBZ/standard/business/src/test/java/jp/co/ndensan/reams/db/dbz/business/core/basic/GaikyoTokkiIdentifierTest.java
+++ DB/DBZ/standard/business/src/test/java/jp/co/ndensan/reams/db/dbz/business/core/basic/GaikyoTokkiIdentifierTest.java
@@ -8,6 +8,7 @@ import static jp.co.ndensan.reams.db.dbz.business.helper.IsSerializable.serializ
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShinseishoKanriNo;
 import jp.co.ndensan.reams.db.dbz.entity.basic.helper.DbT5206GaikyoTokkiEntityGenerator;
 import jp.co.ndensan.reams.db.dbz.testhelper.DbzTestBase;
+import jp.co.ndensan.reams.uz.uza.biz.Code;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -25,19 +26,21 @@ public class GaikyoTokkiIdentifierTest extends DbzTestBase {
     private static ShinseishoKanriNo 申請書管理番号;
     private static int 認定調査依頼履歴番号;
     private static RString 概況特記テキストイメージ区分;
+    private static Code 原本マスク区分;
 
     @BeforeClass
     public static void setUpClass() {
         申請書管理番号 = DbT5206GaikyoTokkiEntityGenerator.DEFAULT_申請書管理番号;
         認定調査依頼履歴番号 = DbT5206GaikyoTokkiEntityGenerator.DEFAULT_認定調査依頼履歴番号;
         概況特記テキストイメージ区分 = DbT5206GaikyoTokkiEntityGenerator.DEFAULT_概況特記テキストイメージ区分;
+        原本マスク区分 = DbT5206GaikyoTokkiEntityGenerator.DEFAULT_原本マスク区分;
     }
 
     public static class シリアライズテスト extends DbzTestBase {
 
         @Test
         public void シリアライズできる() {
-            GaikyoTokkiIdentifier sut = new GaikyoTokkiIdentifier(申請書管理番号, 認定調査依頼履歴番号, 概況特記テキストイメージ区分);
+            GaikyoTokkiIdentifier sut = new GaikyoTokkiIdentifier(申請書管理番号, 認定調査依頼履歴番号, 概況特記テキストイメージ区分, 原本マスク区分);
             assertThat(sut, is(serializable()));
         }
     }
