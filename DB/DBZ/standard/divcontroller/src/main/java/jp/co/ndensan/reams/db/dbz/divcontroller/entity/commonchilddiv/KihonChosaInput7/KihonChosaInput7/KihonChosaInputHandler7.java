9b50ac8732a0cad174aff90a6ed33d48b24e42d5
嵩西　健太 <n3213@ndensan.co.jp>
2017-01-07 14:28:46 +0900

    認定調査結果登録 課題管理一覧表 項番73 基本調査の特記事項がない場合、各ボタンは非活性になるよう修正

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/KihonChosaInput7/KihonChosaInput7/KihonChosaInputHandler7.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/KihonChosaInput7/KihonChosaInput7/KihonChosaInputHandler7.java
@@ -10,10 +10,13 @@ import java.util.List;
 import jp.co.ndensan.reams.db.dbx.definition.core.configkeys.ConfigNameDBE;
 import jp.co.ndensan.reams.db.dbx.definition.core.dbbusinessconfig.DbBusinessConfig;
 import jp.co.ndensan.reams.db.dbx.definition.core.valueobject.domain.ShinseishoKanriNo;
+import jp.co.ndensan.reams.db.dbz.business.core.basic.NinteichosahyoTokkijiko;
 import jp.co.ndensan.reams.db.dbz.business.core.kihonchosainput.KihonChosaInput;
 import jp.co.ndensan.reams.db.dbz.business.core.kihonchosainput.KihonChosaSpecial;
+import jp.co.ndensan.reams.db.dbz.definition.core.ninteichosatokkijikou.NinteiChosaTokkiJikou;
 import jp.co.ndensan.reams.db.dbz.definition.core.yokaigonintei.chosain.NinchishoNichijoSeikatsuJiritsudoCode;
 import jp.co.ndensan.reams.db.dbz.definition.core.yokaigonintei.chosain.ShogaiNichijoSeikatsuJiritsudoCode;
+import jp.co.ndensan.reams.db.dbz.service.core.basic.NinteichosahyoTokkijikoManager;
 import jp.co.ndensan.reams.db.dbz.service.core.kihonchosainput.KihonChosaInputFinder;
 import jp.co.ndensan.reams.uz.uza.biz.Code;
 import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
@@ -21,6 +24,7 @@ import jp.co.ndensan.reams.uz.uza.lang.RDate;
 import jp.co.ndensan.reams.uz.uza.lang.RString;
 import jp.co.ndensan.reams.uz.uza.ui.binding.IconType;
 import jp.co.ndensan.reams.uz.uza.ui.binding.ListControlTextIcon;
+import jp.co.ndensan.reams.uz.uza.util.di.InstanceProvider;
 import jp.co.ndensan.reams.uz.uza.util.serialization.DataPassingConverter;
 
 /**
@@ -149,6 +153,37 @@ public class KihonChosaInputHandler7 {
         ArrayList<RString> 認定調査特記情報ArrayList = new ArrayList<>(認定調査特記情報List);
         div.getJiritsudo().setNinteichosaTokkijikoNoList(DataPassingConverter.serialize(認定調査特記情報ArrayList));
         onLoad第七群自立度(認定調査基本情報リスト, 認定調査前回結果表示);
+        setTokkiJikouDisabled(申請書管理番号, 認定調査依頼履歴番号);
+    }
+    
+    private void setTokkiJikouDisabled(ShinseishoKanriNo 申請書管理番号, RString 認定調査依頼履歴番号) {
+        ArrayList<RString> tokkiJikouNoList = setTokkiJikouNoList();
+        NinteichosahyoTokkijikoManager manager = InstanceProvider.create(NinteichosahyoTokkijikoManager.class);
+        ArrayList<RString> gaitouTokkiJikouNoList = new ArrayList<>();
+        for (RString tokkiJikouNo : tokkiJikouNoList) {
+            gaitouTokkiJikouNoList.add(tokkiJikouNo);
+            ArrayList<NinteichosahyoTokkijiko> list = manager.get調査特記事項(申請書管理番号, Integer.parseInt(認定調査依頼履歴番号.toString()), gaitouTokkiJikouNoList);
+            if (list.isEmpty()
+                    || RString.isNullOrEmpty(tokkiJikouNo)) {
+                setDisabled(tokkiJikouNo);
+            }
+            gaitouTokkiJikouNoList.clear();
+        }
+    }
+    
+    private ArrayList<RString> setTokkiJikouNoList() {
+        ArrayList<RString> tokkiJikouNoList = new ArrayList<>();
+        tokkiJikouNoList.add(NinteiChosaTokkiJikou.障害高齢者自立度.get認定調査票_特記情報_認定調査特記事項番号());
+        tokkiJikouNoList.add(NinteiChosaTokkiJikou.認知症高齢者自立度.get認定調査票_特記情報_認定調査特記事項番号());
+        return tokkiJikouNoList;
+    }
+    
+    private void setDisabled(RString tokkiJikouNo) {
+        if(NinteiChosaTokkiJikou.障害高齢者自立度.get認定調査票_特記情報_認定調査特記事項番号().equals(tokkiJikouNo)) {
+            div.getBtnShogaiKoreisha().setDisabled(true);
+        } else if (NinteiChosaTokkiJikou.認知症高齢者自立度.get認定調査票_特記情報_認定調査特記事項番号().equals(tokkiJikouNo)) {
+            div.getBtnNinchishaJiritsudo().setDisabled(true);
+        }
     }
 
     private List<RString> get特記事項番号List(ShinseishoKanriNo 申請書管理番号) {
