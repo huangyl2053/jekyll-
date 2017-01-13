9bb1fa3185d9290f4f435b1075d11d9a9af99adb
陳　少軒 <n8407@densan.co.jp>
2017-01-13 09:53:52 +0900

    【認定】運用テスト不具合・課題管理一覧表 項番126、203追加

---
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE1010001/ServiceDelDiv.java
@@ -0,0 +1,72 @@
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE1010001;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+
+/**
+ * ServiceDel のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class ServiceDelDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-12-14_20-51-56">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("btnServiceDelTeikeibunGuide")
+    private ButtonDialog btnServiceDelTeikeibunGuide;
+    @JsonProperty("txtServiceDeleteRiyu")
+    private TextBoxMultiLine txtServiceDeleteRiyu;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * getbtnServiceDelTeikeibunGuide
+     * @return btnServiceDelTeikeibunGuide
+     */
+    @JsonProperty("btnServiceDelTeikeibunGuide")
+    public ButtonDialog getBtnServiceDelTeikeibunGuide() {
+        return btnServiceDelTeikeibunGuide;
+    }
+
+    /*
+     * setbtnServiceDelTeikeibunGuide
+     * @param btnServiceDelTeikeibunGuide btnServiceDelTeikeibunGuide
+     */
+    @JsonProperty("btnServiceDelTeikeibunGuide")
+    public void setBtnServiceDelTeikeibunGuide(ButtonDialog btnServiceDelTeikeibunGuide) {
+        this.btnServiceDelTeikeibunGuide = btnServiceDelTeikeibunGuide;
+    }
+
+    /*
+     * gettxtServiceDeleteRiyu
+     * @return txtServiceDeleteRiyu
+     */
+    @JsonProperty("txtServiceDeleteRiyu")
+    public TextBoxMultiLine getTxtServiceDeleteRiyu() {
+        return txtServiceDeleteRiyu;
+    }
+
+    /*
+     * settxtServiceDeleteRiyu
+     * @param txtServiceDeleteRiyu txtServiceDeleteRiyu
+     */
+    @JsonProperty("txtServiceDeleteRiyu")
+    public void setTxtServiceDeleteRiyu(TextBoxMultiLine txtServiceDeleteRiyu) {
+        this.txtServiceDeleteRiyu = txtServiceDeleteRiyu;
+    }
+
+    // </editor-fold>
+}
---
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE1010001/SinseiTorisageDiv.java
@@ -0,0 +1,92 @@
+package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE1010001;
+/*
+ * このコードはツールによって生成されました。
+ * このファイルへの変更は、再生成時には損失するため
+ * 不正な動作の原因になります。
+ */
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jp.co.ndensan.reams.uz.uza.ui.binding.*;
+import jp.co.ndensan.reams.uz.uza.ui.binding.Panel;
+
+/**
+ * SinseiTorisage のクラスファイル 
+ * 
+ * @author 自動生成
+ */
+public class SinseiTorisageDiv extends Panel {
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2016-12-14_20-51-56">
+    /*
+     * [ private の作成 ]
+     * クライアント側から取得した情報を元にを検索を行い
+     * コントロール名とフィールド名を取得する
+     * private + コントロール名 + フィールド名 の文字列を作成
+     */
+    @JsonProperty("ddlTorisageJiyu")
+    private DropDownList ddlTorisageJiyu;
+    @JsonProperty("txtTorisageDate")
+    private TextBoxDate txtTorisageDate;
+    @JsonProperty("txtTorisageJiyu")
+    private TextBox txtTorisageJiyu;
+
+    /*
+     * [ GetterとSetterの作成 ]
+     * クライアント側から取得した情報を元に検索を行い
+     * コントロール名とフィールド名を取得する
+     * フィールド名のGetterとSetter を作成
+     */
+    /*
+     * getddlTorisageJiyu
+     * @return ddlTorisageJiyu
+     */
+    @JsonProperty("ddlTorisageJiyu")
+    public DropDownList getDdlTorisageJiyu() {
+        return ddlTorisageJiyu;
+    }
+
+    /*
+     * setddlTorisageJiyu
+     * @param ddlTorisageJiyu ddlTorisageJiyu
+     */
+    @JsonProperty("ddlTorisageJiyu")
+    public void setDdlTorisageJiyu(DropDownList ddlTorisageJiyu) {
+        this.ddlTorisageJiyu = ddlTorisageJiyu;
+    }
+
+    /*
+     * gettxtTorisageDate
+     * @return txtTorisageDate
+     */
+    @JsonProperty("txtTorisageDate")
+    public TextBoxDate getTxtTorisageDate() {
+        return txtTorisageDate;
+    }
+
+    /*
+     * settxtTorisageDate
+     * @param txtTorisageDate txtTorisageDate
+     */
+    @JsonProperty("txtTorisageDate")
+    public void setTxtTorisageDate(TextBoxDate txtTorisageDate) {
+        this.txtTorisageDate = txtTorisageDate;
+    }
+
+    /*
+     * gettxtTorisageJiyu
+     * @return txtTorisageJiyu
+     */
+    @JsonProperty("txtTorisageJiyu")
+    public TextBox getTxtTorisageJiyu() {
+        return txtTorisageJiyu;
+    }
+
+    /*
+     * settxtTorisageJiyu
+     * @param txtTorisageJiyu txtTorisageJiyu
+     */
+    @JsonProperty("txtTorisageJiyu")
+    public void setTxtTorisageJiyu(TextBox txtTorisageJiyu) {
+        this.txtTorisageJiyu = txtTorisageJiyu;
+    }
+
+    // </editor-fold>
+}
