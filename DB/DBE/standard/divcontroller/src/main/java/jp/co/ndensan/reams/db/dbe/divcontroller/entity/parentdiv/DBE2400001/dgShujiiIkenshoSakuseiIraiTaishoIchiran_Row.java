c057c1c98882fd1bcfdcd3a008120bd5dda63d55
高橋　一矢 <n3509@ndensan.co.jp>
2017-02-14 10:28:06 +0900

    一括発行画面　修正

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE2400001/dgShujiiIkenshoSakuseiIraiTaishoIchiran_Row.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE2400001/dgShujiiIkenshoSakuseiIraiTaishoIchiran_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE2400001;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Mon Jan 30 14:51:09 JST 2017 
+ * Tue Feb 14 09:45:51 JST 2017 
  */
 
 
@@ -20,7 +20,7 @@ import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxNum;
  */
 
 public class dgShujiiIkenshoSakuseiIraiTaishoIchiran_Row extends DataRow {
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2017-01-10_09-26-16">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2017-02-13_05-12-42">
 
     private RString shujiiIryoKikanCode;
     private RString shujiiIryoKikanMeisho;
