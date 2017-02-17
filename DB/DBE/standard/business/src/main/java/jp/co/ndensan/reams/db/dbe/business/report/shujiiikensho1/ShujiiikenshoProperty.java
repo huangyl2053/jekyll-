36306febf5fba2ec7642bb410befbee87ed65bd5
竹内 和紀 <n3212@ndensan.co.jp>
2017-02-17 19:21:33 +0900

    修正

--- DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/shujiiikensho1/ShujiiikenshoProperty.java
+++ DB/DBE/standard/business/src/main/java/jp/co/ndensan/reams/db/dbe/business/report/shujiiikensho1/ShujiiikenshoProperty.java
@@ -8,7 +8,11 @@ package jp.co.ndensan.reams.db.dbe.business.report.shujiiikensho1;
 import jp.co.ndensan.reams.db.dbe.definition.core.reportid.ReportIdDBE;
 import jp.co.ndensan.reams.db.dbe.entity.report.source.shujiiikensho1.ShujiiikenshoReportSource;
 import jp.co.ndensan.reams.uz.uza.biz.SubGyomuCode;
+import jp.co.ndensan.reams.uz.uza.report.BreakerCatalog;
+import jp.co.ndensan.reams.uz.uza.report.Breakers;
+import jp.co.ndensan.reams.uz.uza.report.ReportLineRecord;
 import jp.co.ndensan.reams.uz.uza.report.ReportPropertyBase;
+import jp.co.ndensan.reams.uz.uza.report.data.chart.ReportDynamicChart;
 
 /**
  * 主治医意見書のプロパティです。
@@ -22,5 +26,26 @@ public class ShujiiikenshoProperty extends ReportPropertyBase<ShujiiikenshoRepor
      */
     public ShujiiikenshoProperty() {
         super(SubGyomuCode.DBE認定支援, ReportIdDBE.DBE517151.getReportId());
+    }
+
+    @Override
+    public Breakers<ShujiiikenshoReportSource> defineBreakers(
+            Breakers<ShujiiikenshoReportSource> breakers,
+            BreakerCatalog<ShujiiikenshoReportSource> catalog) {
+        return breakers.add(catalog.new SimplePageBreaker(
+            ShujiiikenshoReportSource.LAYOUTBREAKITEM) {
+            @Override
+            public ReportLineRecord<ShujiiikenshoReportSource> occuredBreak(
+                    ReportLineRecord<ShujiiikenshoReportSource> currentRecord,
+                    ReportLineRecord<ShujiiikenshoReportSource> nextRecord, ReportDynamicChart dynamicChart) {
+                int layout = currentRecord.getSource().layout;
+                currentRecord.setFormGroupIndex(layout);
+                if (nextRecord != null && nextRecord.getSource() != null) {
+                    layout = nextRecord.getSource().layout;
+                    nextRecord.setFormGroupIndex(layout);
+                }
+                return currentRecord;
+            }
+        }).fixed();
     }
 }
