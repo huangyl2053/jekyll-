2bdd76f3a7e4cbc021147f333bffcc3424ebdf48
髙野　暁秀 <n3508@ndensan.co.jp>
2017-02-20 11:14:27 +0900

    介護認定審査会開催予定登録　画面修正

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE5140001/dgShinsakaiKaisaiYoteiIchiran_Row.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/entity/parentdiv/DBE5140001/dgShinsakaiKaisaiYoteiIchiran_Row.java
@@ -3,7 +3,7 @@ package jp.co.ndensan.reams.db.dbe.divcontroller.entity.parentdiv.DBE5140001;
  * このコードはツールによって生成されました。
  * このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
  * コードが再生成されるときに損失したりします。
- * Tue Feb 14 16:27:07 JST 2017 
+ * Mon Feb 20 09:04:45 JST 2017 
  */
 
 
