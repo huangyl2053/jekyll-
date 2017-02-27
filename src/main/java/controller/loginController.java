c1daa6c40f157a1cf45e43405b3c14d00d1e3474
1313 <huangyl@sunjapan.com.cn>
2017-02-27 11:44:39 +0800

    gitignore更新。

--- src/main/java/controller/loginController.java
+++ src/main/java/controller/loginController.java
@@ -2,12 +2,7 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import entity.loginInfo;
 @Controller
-@RequestMapping("/loginController")
 public class loginController {
 	@RequestMapping("/login")
 	public String login() {
