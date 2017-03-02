3d46ea76893ce5d4a7ad30f638da4b43604f67e3
髙野　暁秀 <n3508@ndensan.co.jp>
2017-03-02 19:01:24 +0900

    介護認定審査会委員割付　ラジオボタンの項目名を修正

--- DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/YokaigoNinteiShinsakaiIchiranList/YokaigoNinteiShinsakaiIchiranList/YokaigoNinteiShinsakaiIchiranListDiv.java
+++ DB/DBZ/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbz/divcontroller/entity/commonchilddiv/YokaigoNinteiShinsakaiIchiranList/YokaigoNinteiShinsakaiIchiranList/YokaigoNinteiShinsakaiIchiranListDiv.java
@@ -27,7 +27,7 @@ import jp.co.ndensan.reams.uz.uza.ui.binding.TextBoxDate;
  * @reamsid_L DBE-0120-010 lishengli
  */
 public class YokaigoNinteiShinsakaiIchiranListDiv extends Panel implements IYokaigoNinteiShinsakaiIchiranListDiv {
-    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2017-02-23_06-32-36">
+    // <editor-fold defaultstate="collapsed" desc="Created By UIDesigner ver：UZ-deploy-2017-02-15_05-21-09">
     /*
      * [ private の作成 ]
      * クライアント側から取得した情報を元にを検索を行い
