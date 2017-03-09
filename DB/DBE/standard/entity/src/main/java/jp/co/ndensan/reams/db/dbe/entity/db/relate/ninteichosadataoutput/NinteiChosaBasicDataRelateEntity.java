564809813a565111df7bb6778b4ac287ca0296d4
Kubo Satoshi <n2810@ndensan.co.jp>
2017-03-09 19:11:50 +0900

    認定調査データ出力（モバイル）　項番1091　不具合修正

--- DB/DBE/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbe/entity/db/relate/ninteichosadataoutput/NinteiChosaBasicDataRelateEntity.java
+++ DB/DBE/standard/entity/src/main/java/jp/co/ndensan/reams/db/dbe/entity/db/relate/ninteichosadataoutput/NinteiChosaBasicDataRelateEntity.java
@@ -17,7 +17,7 @@ import jp.co.ndensan.reams.uz.uza.util.db.IDbAccessable;
 public class NinteiChosaBasicDataRelateEntity implements IDbAccessable {
 
     private NinteiChosaDataOutputBatchRelateEntity 今回分Entity;
-    private NinteiChosaDataOutputBatchRelateEntity 前回分Entity;
+    private NinteiChosaDataOutputBatchRelateZenkaiEntity 前回分Entity;
     private RString 認定日_前回;
     private RString 認定結果コード_前回;
 }
