47390177ac5b4efeebcdf3fe9b3190625f90941a
髙野　暁秀 <n3508@ndensan.co.jp>
2017-03-23 19:00:16 +0900

    基本運用 認定調査依頼　CSV出力時のアクセスログ対応

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/controller/parentdiv/DBE2010001/NinteichosaIrai.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/controller/parentdiv/DBE2010001/NinteichosaIrai.java
@@ -168,7 +168,7 @@ public class NinteichosaIrai {
      */
     public IDownLoadServletResponse onClick_btnDataOutput(NinteichosaIraiDiv requestDiv, IDownLoadServletResponse response) {
         FileSpoolManager spoolManager = new FileSpoolManager(UzUDE0835SpoolOutputType.EucOther,
-                CSVファイルID_認定調査依頼一覧, UzUDE0831EucAccesslogFileType.Other);
+                CSVファイルID_認定調査依頼一覧, UzUDE0831EucAccesslogFileType.Csv);
         RString 出力名 = EucOtherInfo.getDisplayName(SubGyomuCode.DBE認定支援, CSVファイルID_認定調査依頼一覧);
         RString filePath = Path.combinePath(spoolManager.getEucOutputDirectry(), 出力名);
 
@@ -189,9 +189,9 @@ public class NinteichosaIrai {
                 Optional<PersonalData> personalData = getHandler(requestDiv).getPersonalData(row.getGetShoKisaiHokenshaNo(), row.getHokenshaCode(), row.getShinseishoKanriNo());
                 personalDataList.add(personalData.get());
             }
+            csvWriter.close();
             AccessLogUUID accessLogUUID = AccessLogger.logEUC(UzUDE0835SpoolOutputType.EucOther, personalDataList);
             spoolManager.spool(filePath, accessLogUUID);
-            csvWriter.close();
         }
         SharedFileDescriptor sfd = new SharedFileDescriptor(GyomuCode.DB介護保険, FilesystemName.fromString(出力名));
         sfd = SharedFile.defineSharedFile(sfd);
