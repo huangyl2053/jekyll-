2974c2de25d1930248ad4e2253d67362c3972815
1313 <huangyl@sunjapan.com.cn>
2017-03-28 15:22:39 +0800

    增加数据库连接功能。

---
+++ src/main/java/controller/dbLinkController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class dbLinkController {
+	Statement _Stmt;
+	Connection _Conn;
+
+	public Statement getStatement(){
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			String url="jdbc:mysql://localhost:3306/nature";
+			_Conn = DriverManager.getConnection(url,"root","root");
+			_Stmt = _Conn.createStatement(); 
+			return _Stmt;
+		}catch(Exception e1){
+			System.out.print("数据库连接失败。");
+			return null;
+		}
+	}
+}
--- src/main/java/controller/loginController.java
+++ src/main/java/controller/loginController.java
@@ -1,5 +1,11 @@
 package controller;
 
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
@@ -7,9 +13,32 @@ public class loginController {
 	@RequestMapping("/login")
 	public String login(String psd) {
 		System.out.println(psd);
-		if(psd!=null&&psd.equals("2053")){
+		if(psd!=null&&psd.equals(getPSD())){
 			return "main";
 		}
 		return "index.jsp";
+	}
+
+	private String getPSD() {
+		String psd=new String();
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			String url="jdbc:mysql://localhost:3306/nature";
+			Connection conn;
+			conn = DriverManager.getConnection(url,"root","root");
+			Statement stmt = conn.createStatement(); 
+			
+			String sql = "select * from user"; 
+			ResultSet rs = stmt.executeQuery(sql);
+			if(rs.next()){
+				psd=rs.getString(4);
+				}
+			rs.close();
+			stmt.close();
+			conn.close();
+		}catch(Exception e1){
+			System.out.print("数据库连接失败。");
+		}
+		return psd;
 	}
 }
