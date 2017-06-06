0c90ea59f194f200665d611ab975b4d1a4280ccc
三浦 凌 <n3327@ndensan.co.jp>
2017-06-06 20:45:12 +0900

    資料作成：実行するボタンクリック時のチェック処理を修正（却下・取下の申請は検査に利用しない）

---
+++ DB/DBZ/standard/persistence/src/test/java/jp/co/ndensan/reams/db/dbz/persistence/db/mapper/basic/IDbT5502ShinsakaiWariateJohoMapperTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.ndensan.reams.db.dbz.persistence.db.mapper.basic;
+
+import jp.co.ndensan.reams.db.dbz.testhelper.DbzTestDacBase;
+import org.junit.BeforeClass;
+
+/**
+ * {@link IDbT5502ShinsakaiWariateJohoMapper}のテストです。
+ */
+public class IDbT5502ShinsakaiWariateJohoMapperTest extends DbzTestDacBase {
+
+    public IDbT5502ShinsakaiWariateJohoMapperTest() {
+    }
+
+    @BeforeClass
+    public static void setUp() {
+        setUpClass();
+    }
+
+//    @Test
+//    public void testSomeMethod() {
+//        IDbT5502ShinsakaiWariateJohoMapper sut = sqlSession.getMapper(IDbT5502ShinsakaiWariateJohoMapper.class);
+//        System.out.println(sut.findEffective(new RString("20170054")).size());
+//    }
+}
