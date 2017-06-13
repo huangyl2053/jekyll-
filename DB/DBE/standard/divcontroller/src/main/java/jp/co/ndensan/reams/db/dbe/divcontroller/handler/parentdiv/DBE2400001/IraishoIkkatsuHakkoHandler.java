ed03a778197aaf6e96be3d15edc286c3276b56eb
嵩西　健太 <n3213@ndensan.co.jp>
2017-06-13 15:33:54 +0900

    懸案事項一覧表（木曽広域） 項番：4 請求書の発行日をカスタムコンフィグにより、日付表示・非表示を変更できるよう修正 一括発行で出力される請求書の意見書作成料種別を表示・非表示の不具合を修正

--- DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/handler/parentdiv/DBE2400001/IraishoIkkatsuHakkoHandler.java
+++ DB/DBE/standard/divcontroller/src/main/java/jp/co/ndensan/reams/db/dbe/divcontroller/handler/parentdiv/DBE2400001/IraishoIkkatsuHakkoHandler.java
@@ -667,7 +667,7 @@ public class IraishoIkkatsuHakkoHandler {
             param.setIkenshoSakuseiSeikyusho(請求書選択selectedKeys.contains(意見書チェックボックス_請求書));
             if (請求書選択selectedKeys.contains(意見書チェックボックス_請求書)) {
                 RString 発行日印字有無 = DbBusinessConfig.get(ConfigNameDBE.主治医意見書作成料請求書_発行日_印字有無, RDate.getNowDate(), SubGyomuCode.DBE認定支援,
-                        div.getCcdNinteiChosaHokensha().getSelectedItem().get市町村コード().value());
+                        div.getCcdShujiiIkenshoHokensha().getSelectedItem().get市町村コード().value());
                 param.setSeikyushoHakkobiInjiumu(発行日印字有無.equals(CONFIGVALUE1));
             }
         } else {
